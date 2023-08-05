7242ed5005a3ee5de2f52caf0f98d464a86a3e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:32 +0000

    Add package flowclient/1.9.0.post0.dev9/flowclient-1.9.0.post0.dev9-py3-none-any.whl

---
+++ packages/flowclient/flowclient-1.9.0.post0.dev9-py3-none-any.whl/flowclient/__init__.py
@@ -0,0 +1,107 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+FlowClient is a Python client to FlowAPI.
+"""
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
+
+from .client import (
+    Connection,
+    connect,
+    daily_location_spec,
+    modal_location_spec,
+    modal_location_from_dates_spec,
+    get_geography,
+    get_result,
+    get_result_by_query_id,
+    get_geojson_result,
+    get_status,
+    query_is_ready,
+    run_query,
+    get_available_dates,
+    radius_of_gyration_spec,
+    unique_location_counts_spec,
+    subscriber_degree_spec,
+    topup_amount_spec,
+    topup_balance_spec,
+    event_count_spec,
+    displacement_spec,
+    pareto_interactions_spec,
+    nocturnal_events_spec,
+    handset_spec,
+    random_sample_spec,
+    unique_locations_spec,
+)
+from .api_query import APIQuery
+from . import aggregates
+from .aggregates import (
+    location_event_counts,
+    meaningful_locations_aggregate,
+    meaningful_locations_between_label_od_matrix,
+    meaningful_locations_between_dates_od_matrix,
+    flows,
+    unique_subscriber_counts,
+    location_introversion,
+    total_network_objects,
+    aggregate_network_objects,
+    spatial_aggregate,
+    joined_spatial_aggregate,
+    histogram_aggregate,
+    active_at_reference_location_counts,
+    unmoving_at_reference_location_counts,
+    unmoving_counts,
+    consecutive_trips_od_matrix,
+    trips_od_matrix,
+)
+
+__all__ = [
+    "aggregates",
+    "Connection",
+    "connect",
+    "daily_location_spec",
+    "modal_location_spec",
+    "modal_location_from_dates_spec",
+    "get_geography",
+    "get_result",
+    "get_result_by_query_id",
+    "get_geojson_result",
+    "get_status",
+    "query_is_ready",
+    "run_query",
+    "get_available_dates",
+    "radius_of_gyration_spec",
+    "unique_location_counts_spec",
+    "subscriber_degree_spec",
+    "topup_amount_spec",
+    "topup_balance_spec",
+    "event_count_spec",
+    "displacement_spec",
+    "pareto_interactions_spec",
+    "nocturnal_events_spec",
+    "handset_spec",
+    "random_sample_spec",
+    "APIQuery",
+    "location_event_counts",
+    "meaningful_locations_aggregate",
+    "meaningful_locations_between_label_od_matrix",
+    "meaningful_locations_between_dates_od_matrix",
+    "flows",
+    "unique_subscriber_counts",
+    "location_introversion",
+    "total_network_objects",
+    "aggregate_network_objects",
+    "spatial_aggregate",
+    "joined_spatial_aggregate",
+    "histogram_aggregate",
+    "active_at_reference_location_counts",
+    "unique_locations_spec",
+    "unmoving_at_reference_location_counts",
+    "unmoving_counts",
+    "consecutive_trips_od_matrix",
+    "trips_od_matrix",
+]
