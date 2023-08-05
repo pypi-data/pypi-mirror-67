5dd01c2a443553e0d9c59578427cea2f7cae62e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:43 +0000

    Add package kodexa/1.1.139/kodexa-1.1.139-py3-none-any.whl

---
+++ packages/kodexa/kodexa-1.1.139-py3-none-any.whl/kodexa/__init__.py
@@ -0,0 +1,16 @@
+"""
+    Kodexa is a Python framework to enable flexible data engineering with semi-structured and unstructured documents and
+    data.
+
+
+"""
+
+from .connectors import *
+from .extractors import *
+from .cloud import *
+from .mixins import *
+from .model import *
+from .pipeline import *
+from .sinks import *
+from .steps import *
+from .stores import *
