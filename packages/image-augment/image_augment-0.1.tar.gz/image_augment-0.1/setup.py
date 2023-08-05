33e463a244e7021c7a29af08032b2de0645dff8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:43 +0000

    Add package image-augment/0.1/image_augment-0.1.tar.gz

---
+++ packages/image-augment/image_augment-0.1.tar.gz/image_augment-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='image_augment',
+    version='0.1',
+    packages=['ImageAugment'],
+    url='https://github.com/lewis-morris/image_augment',
+    license='MIT',
+    author='lewis',
+    author_email='lewis.morris@gmail.com',
+    description='Basic image augmentor',
+    download_url='https://github.com/lewis-morris/image_augment/archive/0.1.tar.gz'
+)
