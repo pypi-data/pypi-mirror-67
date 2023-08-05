586ce981294942172d9765dab6700a165116db55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:13 +0000

    Add package supermath112/1.0/supermath112-1.0.tar.gz

---
+++ packages/supermath112/supermath112-1.0.tar.gz/supermath112-1.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup (
+    name="supermath112",   #对外模块的名字
+    version="1.0",      #版本号
+    description="这是第er 个发布的模块",    #描述
+    author="xiedongdong",  #作者
+    author_email="381610444@qq.com",
+    py_modules=["supermath.demo1","supermath.demo2"]  #大部的模块
+
+
+
+
+
+
+
+
+
+)
