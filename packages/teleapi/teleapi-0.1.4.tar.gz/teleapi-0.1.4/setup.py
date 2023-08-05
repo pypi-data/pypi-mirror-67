10ace3ebe5ac11a7032fb0f9840f939714db0dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:22 +0000

    Add package teleapi/0.1.4/teleapi-0.1.4.tar.gz

---
+++ packages/teleapi/teleapi-0.1.4.tar.gz/teleapi-0.1.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='teleapi',
+    version='0.1.4',
+    author='DesSolo',
+    author_email='dessolo@mail.ru',
+    url='https://github.com/DesSolo/teleapi',
+    description='Simple telegram api',
+    license='GPLv3+',
+    keywords='telegram bot api',
+    packages=find_packages(),
+    install_requires=['requests', 'pysocks']
+)
