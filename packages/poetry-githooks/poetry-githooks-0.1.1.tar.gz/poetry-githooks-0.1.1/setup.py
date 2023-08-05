752541729fb7423c2335631c6bb42bd1fe3b54dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:02 +0000

    Add package poetry-githooks/0.1.1/poetry-githooks-0.1.1.tar.gz

---
+++ packages/poetry-githooks/poetry-githooks-0.1.1.tar.gz/poetry-githooks-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['poetry_githooks']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{u'poetry_githooks': ['main = poetry_githooks:main']}
+
+setup_kwargs = {
+    'name': 'poetry-githooks',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Thomas Thiebaud',
+    'author_email': 'thiebaud.tom@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
