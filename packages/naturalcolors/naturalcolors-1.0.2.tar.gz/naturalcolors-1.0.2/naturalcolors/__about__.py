74088ce9a7a6b2f2520795f85bea3ae51daca9df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:15 +0000

    Add package naturalcolors/1.0.2/naturalcolors-1.0.2.tar.gz

---
+++ packages/naturalcolors/naturalcolors-1.0.2.tar.gz/naturalcolors-1.0.2/naturalcolors/__about__.py
@@ -0,0 +1,13 @@
+__title__ = 'naturalcolors'
+__version__ = '1.0.2'
+__license__ = 'MIT'
+__author__ = 'Fabio D. Steffen'
+__description__ = 'Nature color palette'
+__keywords__ = 'colormap, colors, nature'
+__url__ = 'https://github.com/fdsteffen/naturalcolors'
+__copyright__ = '2020 {}'.format(__author__)
+__classifiers__ = [
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+    ]
