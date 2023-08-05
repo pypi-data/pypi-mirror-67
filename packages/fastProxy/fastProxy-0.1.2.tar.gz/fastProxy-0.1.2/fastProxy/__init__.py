b86585d892739eec4d24170f5f9858199c9972d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:10 +0000

    Add package fastProxy/0.1.2/fastProxy-0.1.2.tar.gz

---
+++ packages/fastProxy/fastProxy-0.1.2.tar.gz/fastProxy-0.1.2/fastProxy/__init__.py
@@ -0,0 +1,6 @@
+from .fastProxy import (THREAD_COUNT,
+                       REQUEST_TIMEOUT,
+                       GENERATE_CSV,
+                       ALL_IPS,
+                       printer,
+                       fetch_proxies)
