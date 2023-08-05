3d8b3ae89783a0a2ce22de8abb97f565ec497b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:32 +0000

    Add package xtracthub/0.0.1/xtracthub-0.0.1.tar.gz

---
+++ packages/xtracthub/xtracthub-0.0.1.tar.gz/xtracthub-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="xtracthub",
+    version="0.0.1",
+    author="Ryan Wong",
+    author_email="rewong03@gmail.com",
+    description="SDK for xtract-container-service",
+    long_description_content_type="text/markdown",
+    url="https://github.com/xtracthub/xtract-container-service",
+    packages=setuptools.find_packages(),
+)
