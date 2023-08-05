ed1b6f91aa127766d9b00caee9281d2948f0520a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:58 +0000

    Add package epyk-materials/1.0.0/epyk-materials-1.0.0.tar.gz

---
+++ packages/epyk-materials/epyk-materials-1.0.0.tar.gz/epyk-materials-1.0.0/README.md
@@ -0,0 +1,27 @@
+
+![](https://raw.githubusercontent.com/epykure/epyk-materials/master/epyk_materials/static/images/epyklogo_whole_big.png)
+
+### First Version out now!
+
+An easy way to use Epyk with the Material Components extension.
+
+Presentation
+================================
+This package will make a simple interface between most of the components of Materials components
+and Epyk. This addon will allow you to benefit from the two world and to keep write Interactive and modern web pages from Python.
+
+https://material-ui.com/
+
+Compatibility
+================================
+epyk is compatible with the most common Web Python Frameworks (Flask and Django).
+By default, the server package embeds a Flask app as it is easier to install and ready to use.
+
+The Extension (with Epyk-UI) can be included within a Jupyter or JupyterLab project. But this will lead to some limitations - for example Ajax and Socket will not be available.
+
+The Material extension will add extra components with a more advanced design. Those components are integrated within
+the framework and all the underlying event, interactivities and styling features can be reused.
+
+Also it is possible to mix the components and to use Epyk-UI ecosystem with Material UI.
+
+  
---
+++ packages/epyk-materials/epyk-materials-1.0.0.tar.gz/epyk-materials-1.0.0/setup.py
@@ -0,0 +1,39 @@
+"""
+packaging module to install: python.exe -m pip install --upgrade setuptools wheel
+to create the tar.gz: python.exe setup.py sdist
+to create the weels: python.exe setup.py sdist bdist_wheel --universal
+"""
+
+import setuptools
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+def install_required():
+  return [line for line in open('requirements.txt')]
+
+setuptools.setup(
+    name="epyk-materials",
+    author="epykure",
+    version="1.0.0",
+    author_email="smith.pyotr@gmail.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/epykure/epyk-materials",
+    project_urls={
+        "Documentation": "http://www.epyk.io",
+        "Code": "https://github.com/epykure/epyk-materials",
+        "Issue tracker": "https://github.com/epykure/epyk-materials/issues"
+    },
+    packages=setuptools.find_packages(),
+    install_requires=install_required(),
+    python_requires=">=2.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+)
