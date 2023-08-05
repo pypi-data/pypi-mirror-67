731189ba42320de28a3530c2eb69b5c8e52f0c6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:42 +0000

    Add package bitbucket2github/1.3/bitbucket2github-1.3.tar.gz

---
+++ packages/bitbucket2github/bitbucket2github-1.3.tar.gz/bitbucket2github-1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='bitbucket2github',
+    version='1.3',
+    description='Simple tool to migrate your Bitbucket repositories to GitHub',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/brunogfranca/bitbucket2github',
+    author='Bruno França',
+    author_email='bgfranca@gmail.com',
+    license='MIT',
+    packages=['bitbucket2github'],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': ['bitbucket2github=bitbucket2github.cli:migrate'],
+    },
+    install_requires=[
+        'click==7.1.1',
+        'requests==2.23.0',
+        'PyGithub==1.47'
+    ]
+)
