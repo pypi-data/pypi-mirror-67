26a45a1b0c357c8a272117395eb087d7bdcb50f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:22 +0000

    Add package superdesk-planning/1.33.0/superdesk-planning-1.33.0.tar.gz

---
+++ packages/superdesk-planning/superdesk-planning-1.33.0.tar.gz/superdesk-planning-1.33.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+package_data = {
+    'planning': [
+        'templates/*.txt',
+        'templates/*.html'
+    ]
+}
+
+setup(
+    name="superdesk-planning",
+    version="1.33.0",
+    package_dir={'': 'server'},
+    packages=find_packages('server'),
+    package_data=package_data,
+    include_package_data=True,
+    author='Edouard Richard',
+    author_email='edouard.richard@sourcefabric.org',
+    license='MIT',
+    install_requires=[
+        'icalendar==3.11.1',
+        'deepdiff==3.3.0'
+    ],
+    url='',
+)
