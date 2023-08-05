a28af831fe0b562e450c60fc0a1c75241f6e88af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:07 +0000

    Add package image-titler/1.8.3/image-titler-1.8.3.tar.gz

---
+++ packages/image-titler/image-titler-1.8.3.tar.gz/image-titler-1.8.3/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="image-titler",
+    version="1.8.3",
+    author="The Renegade Coder",
+    author_email="jeremy.grifski@therenegadecoder.com",
+    description="Adds a title and logo to an image using The Renegade Coder Featured Image style",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TheRenegadeCoder/image-titler",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires=">=3.8",
+    entry_points={
+        "console_scripts": [
+            'image_titler = image_titler.trc_image_titler:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'titlecase',
+        'pillow>=6.0.0',
+        'pathvalidate'
+    ],
+)
