b0a674735400a2aa0848f8b5676c4993ae0ac9fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:03 +0000

    Add package mingdongnlp/2.0.0a42/mingdongnlp-2.0.0a42.tar.gz

---
+++ packages/mingdongnlp/mingdongnlp-2.0.0a42.tar.gz/mingdongnlp-2.0.0a42/mingdongnlp/components/pos.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# Author: hankcs
+# Date: 2019-12-05 23:05
+from mingdongnlp.components.taggers.cnn_tagger import CNNTagger
+from mingdongnlp.components.taggers.rnn_tagger import RNNTagger
+
+
+class CNNPartOfSpeechTagger(CNNTagger):
+    pass
+
+
+class RNNPartOfSpeechTagger(RNNTagger):
+    pass
