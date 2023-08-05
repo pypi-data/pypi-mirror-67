d525444a54ad81820ceb5525389d0cc23a837a48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:40 +0000

    Add package SCCAF/0.0.10/SCCAF-0.0.10.tar.gz

---
+++ packages/SCCAF/SCCAF-0.0.10.tar.gz/SCCAF-0.0.10/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+print(find_packages())
+
+setup(
+        name='SCCAF',
+        version='0.0.10',
+        description='Single-Cell Clustering Assessment Framework',
+        long_description=readme(),
+        long_description_content_type='text/markdown',
+        packages=find_packages(),
+        install_requires=[
+            'numpy',
+            'pandas',
+            'louvain',
+            'scikit-learn',
+            'psutil',
+            'scanpy==1.4.6'],
+        scripts=['cli/sccaf', 'cli/sccaf-assess', 'cli/sccaf-assess-merger', 'cli/sccaf-regress-out'],
+        author='Chichau Miau',
+        author_email='zmiao@ebi.ac.uk',
+        license='MIT'
+    )
