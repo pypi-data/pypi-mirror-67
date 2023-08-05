d524e41e7a9db2dc1937d8bbd0e1198944c538af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:33 +0000

    Add package collective.anysurfer/1.2.2/collective.anysurfer-1.2.2.tar.gz

---
+++ packages/collective.anysurfer/collective.anysurfer-1.2.2.tar.gz/collective.anysurfer-1.2.2/CHANGES.rst
@@ -0,0 +1,56 @@
+Changelog
+=========
+
+1.2.2 (2020-04-29)
+------------------
+
+- Fix view titles calculation in multilingual websites : #1
+  [laulaz]
+
+- Add missing plone.api dependency
+  [laulaz]
+
+
+1.2.1 (2019-11-20)
+------------------
+
+- Harmonization of '(Required)' syntax for fields
+  [laulaz]
+
+
+1.2 (2019-10-04)
+----------------
+
+- Add documentation, contributors, fix setup.py & use RST
+  [laulaz]
+
+- Handle more Anysurfer usecases : empty lists, bad alts, required labels, ...
+  [laulaz]
+
+- Move h1, add search results count in search page title & handle JS refresh
+  [laulaz]
+
+- Handle 404 page
+  [vpiret]
+
+- Migrate to plone 5
+  [oxydedefer]
+
+- Add missing translations for accessibility view title
+  [laulaz]
+
+- Add missing translations for contact-info view title
+  [laulaz]
+
+
+1.1 (2012-11-12)
+----------------
+
+- Nothing changed yet.
+
+
+1.0 (2012-11-09)
+----------------
+
+- Package created using zopeskel
+  []
---
+++ packages/collective.anysurfer/collective.anysurfer-1.2.2.tar.gz/collective.anysurfer-1.2.2/src/collective/anysurfer/viewlets.py
@@ -0,0 +1,84 @@
+import logging
+from cgi import escape
+
+from zope.component import getMultiAdapter
+from zope.annotation.interfaces import IAnnotations
+from zope.i18n import translate
+
+from Products.CMFPlone.utils import safe_unicode
+
+from plone.app.layout.viewlets import common
+from plone import api
+
+from collective.anysurfer.layout import SKIN_TEMPLATE_KEY
+
+logger = logging.getLogger('collective.anysurfer')
+
+# In some cases (ex: multilingual websites) we cannot check correctly that
+# page_title == portal_title to translate view names
+VIEWS_TO_HANDLE = [
+    "accessibility-info",
+    "sitemap",
+    "search",
+    "contact-info",
+]
+
+
+class TitleViewlet(common.TitleViewlet):
+
+    def update(self):
+        portal_state = getMultiAdapter((self.context, self.request),
+                                        name=u'plone_portal_state')
+        context_state = getMultiAdapter((self.context, self.request),
+                                         name=u'plone_context_state')
+        page_title = escape(safe_unicode(context_state.object_title()))
+        portal_title = escape(safe_unicode(portal_state.portal_title()))
+        view_name = self.view.__name__
+        if page_title == portal_title or view_name in VIEWS_TO_HANDLE:
+            logger.warn('View without explicit title: %s' % self.request.URL)
+            view_title = translate(view_name, 'plone', context=self.request)
+            if view_name == "search":
+                results_nb = self.view.results().sequence_length
+                self.site_title = u"%s %s &mdash; %s" % (
+                    results_nb,
+                    view_title,
+                    portal_title,
+                )
+            elif view_name != view_title:
+                self.site_title = u"%s &mdash; %s" % (view_title, portal_title)
+            else:
+                try:
+                    portal = api.portal.get()
+                    portal.unrestrictedTraverse(
+                        self.request.getURL().lstrip(
+                            portal.absolute_url()
+                        )
+                    )
+                except KeyError:
+                    self.site_title = u"%s &mdash; %s" % (
+                        translate(u'404-error', 'plone', context=self.request),
+                        portal_title,
+                    )
+                else:
+                    self.site_title = portal_title
+
+        else:
+            self.site_title = u"%s &mdash; %s" % (page_title, portal_title)
+
+
+class TitleViewletForSkinTemplateView(common.TitleViewlet):
+
+    def update(self):
+        portal_state = getMultiAdapter((self.context, self.request),
+                                        name=u'plone_portal_state')
+        portal_title = escape(safe_unicode(portal_state.portal_title()))
+        template = IAnnotations(self.view)[SKIN_TEMPLATE_KEY]
+        title = template.title or template.id
+        if title:
+            view_title = translate(title, 'plone',
+                    context=self.request)
+            self.site_title = u"%s &mdash; %s" % (view_title, portal_title)
+        else:
+            logger.warn('Skin template without explicit title: %s'
+                    % template.id)
+            self.site_title = portal_title
