55bac671ef49fd611f36c7c5f88b8d1365b2f02c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:22 +0000

    Add package ScreenRecorder/0.4/ScreenRecorder-0.4.tar.gz

---
+++ packages/ScreenRecorder/ScreenRecorder-0.4.tar.gz/ScreenRecorder-0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='ScreenRecorder',
+    version='0.4',
+    author='foo',
+    author_email='foo@foo.com',
+    url='https://pypi.org/project/ScreenRecorder',
+    description='A screen recorder.',
+    install_requires=["keyboard", "moviepy", "pyaudio", "pyautogui", "pygame"],
+    python_requires='>=3.6',
+    py_modules=['ScreenRecorder'],)
