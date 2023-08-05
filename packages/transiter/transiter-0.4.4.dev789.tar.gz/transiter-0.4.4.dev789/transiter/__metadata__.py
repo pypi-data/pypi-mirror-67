d24eae3c254c2ae0e749c6ef8194808a77bfdc99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:33 +0000

    Add package transiter/0.4.4.dev789/transiter-0.4.4.dev789.tar.gz

---
+++ packages/transiter/transiter-0.4.4.dev789.tar.gz/transiter-0.4.4.dev789/transiter/__metadata__.py
@@ -0,0 +1,15 @@
+# When updating the version, also update it in the docs.
+__version__ = "0.4.4"
+
+# The following are set by Travis CI at build time.
+__build_number__ = None
+__build_timestamp__ = None
+__build_href__ = None
+__git_commit_hash__ = None
+
+# NOTE: the following were set by Travis CI
+__version__ = "0.4.4.dev789"
+__build_number__ = 789
+__build_timestamp__ = 1588123143
+__build_href__ = "https://travis-ci.org/jamespfennell/transiter/jobs/680817609"
+__git_commit_hash__ = "af83866b2a6d75bc6806239843e0ae805d228453"
---
+++ packages/transiter/transiter-0.4.4.dev789.tar.gz/transiter-0.4.4.dev789/transiter/services/feedservice.py
@@ -0,0 +1,163 @@
+"""
+The feed service is used to retrieve data about feeds and perform feed update
+operations.
+"""
+
+import datetime
+import logging
+import typing
+from dataclasses import dataclass
+from typing import Iterable
+
+from transiter import exceptions, models
+from transiter.data import dbconnection
+from transiter.data.dams import feeddam, systemdam
+from transiter.services import views
+from transiter.services.update import updatemanager
+
+logger = logging.getLogger(__name__)
+
+
+# TODO: just use the view instead
+@dataclass
+class Feed:
+    system_id: str
+    feed_id: str
+    auto_update_period: int
+
+
+@dbconnection.unit_of_work
+def list_all_auto_updating() -> Iterable[Feed]:
+    """
+    List all auto updating feeds.
+
+    This method is designed for use by the scheduler.
+    """
+    return [
+        Feed(feed.system.id, feed.id, feed.auto_update_period)
+        for feed in feeddam.list_all_auto_updating()
+    ]
+
+
+@dbconnection.unit_of_work
+def list_all_in_system(system_id) -> typing.List[views.Feed]:
+    """
+    Get data on all feeds in a system.
+    """
+    system = systemdam.get_by_id(system_id, only_return_active=True)
+    if system is None:
+        raise exceptions.IdNotFoundError(models.System, system_id=system_id)
+    return list(map(views.Feed.from_model, feeddam.list_all_in_system(system_id)))
+
+
+@dbconnection.unit_of_work
+def get_in_system_by_id(system_id, feed_id) -> views.Feed:
+    """
+    Get data on a specific feed in a system.
+    """
+    feed = feeddam.get_in_system_by_id(system_id, feed_id)
+    if feed is None:
+        raise exceptions.IdNotFoundError(
+            models.Feed, system_id=system_id, feed_id=feed_id
+        )
+    response = views.FeedLarge.from_model(feed)
+    response.updates = views.UpdatesInFeedLink.from_model(feed)
+    return response
+
+
+def create_and_execute_feed_update(
+    system_id, feed_id, content=None, execute_async=False
+):
+    return _create_and_execute_feed_update_helper(
+        system_id,
+        feed_id,
+        update_manager_function=updatemanager.create_feed_update,
+        content=content,
+        execute_async=execute_async,
+    )
+
+
+def create_and_execute_feed_flush(system_id, feed_id, execute_async=False):
+    return _create_and_execute_feed_update_helper(
+        system_id,
+        feed_id,
+        update_manager_function=updatemanager.create_feed_flush,
+        content=None,
+        execute_async=execute_async,
+    )
+
+
+def _create_and_execute_feed_update_helper(
+    system_id, feed_id, update_manager_function, content=None, execute_async=False,
+):
+    """
+    Create a feed update for a feed in a system.
+    """
+    feed_update_pk = update_manager_function(system_id, feed_id)
+    if feed_update_pk is None:
+        raise exceptions.IdNotFoundError(
+            models.Feed, system_id=system_id, feed_id=feed_id
+        )
+    if execute_async:
+        updatemanager.execute_feed_update_async.delay(feed_update_pk, content)
+    else:
+        updatemanager.execute_feed_update(feed_update_pk, content)
+    return feed_update_pk
+
+
+@dbconnection.unit_of_work
+def list_updates_in_feed(system_id, feed_id):
+    """
+    List all of the updates for a feed.
+    """
+    feed = feeddam.get_in_system_by_id(system_id, feed_id)
+    if feed is None:
+        raise exceptions.IdNotFoundError(
+            models.Feed, system_id=system_id, feed_id=feed_id
+        )
+    response = []
+    for feed_update in feeddam.list_updates_in_feed(feed.pk):
+        response.append(feed_update.to_dict())
+    return response
+
+
+@dbconnection.unit_of_work
+def get_update_in_feed_by_pk(system_id, feed_id, feed_update_pk):
+    feed_update = feeddam.get_update_by_pk(feed_update_pk)
+    if feed_update is None:
+        raise exceptions.IdNotFoundError(
+            models.Feed,
+            system_id=system_id,
+            feed_id=feed_id,
+            feed_update_id=str(feed_update_pk),
+        )
+    return feed_update.to_dict()
+
+
+def trim_feed_updates():
+    """
+    Delete old feed updates.
+
+    This method is designed to be called hourly by the task server.
+    """
+
+    @dbconnection.unit_of_work
+    def _list_all_feed_pks():
+        return feeddam.list_all_feed_pks()
+
+    @dbconnection.unit_of_work
+    def _trim_feed_updates_helper(feed_pk_, before_datetime_):
+        logger.info(
+            "Deleting feed updates with feed_pk={} and last updated before {}".format(
+                feed_pk_, before_datetime_
+            )
+        )
+        feeddam.trim_feed_updates(feed_pk_, before_datetime_)
+
+    logger.info("Trimming old feed updates.")
+    before_datetime = (
+        datetime.datetime.now() - datetime.timedelta(minutes=60)
+    ).replace(microsecond=0, second=0)
+
+    for feed_pk in _list_all_feed_pks():
+        _trim_feed_updates_helper(feed_pk, before_datetime)
