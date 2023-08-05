7f451fa995857b5db08bc1d9eca8239a0840fc96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:54 +0000

    Add package scrapy-do-heroku/0.4.1/scrapy_do_heroku-0.4.1-py2.py3-none-any.whl

---
+++ packages/scrapy-do-heroku/scrapy_do_heroku-0.4.1-py2.py3-none-any.whl/scrapy_do/ui/precache-manifest.b07bf6aa92181b2b8ecfbd20c7e653f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c70b74379dd47eb014faa82a0bb7fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e8fd756cc646dc35850",
+    "url": "/static/css/2.d194689a.chunk.css"
+  },
+  {
+    "revision": "a451a8d78a93afe80774",
+    "url": "/static/css/main.91a93399.chunk.css"
+  },
+  {
+    "revision": "0e8fd756cc646dc35850",
+    "url": "/static/js/2.0129959e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0129959e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a451a8d78a93afe80774",
+    "url": "/static/js/main.fcf8f966.chunk.js"
+  },
+  {
+    "revision": "dde8f637dc425c8b4c75",
+    "url": "/static/js/runtime-main.4c1ec1e9.js"
+  }
+]);
