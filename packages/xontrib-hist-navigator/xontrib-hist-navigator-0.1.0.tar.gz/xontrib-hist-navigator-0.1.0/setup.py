1c91e8aef3ffe943edc4c8db3d11d39936ce4964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:57 +0000

    Add package xontrib-hist-navigator/0.1.0/xontrib-hist-navigator-0.1.0.tar.gz

---
+++ packages/xontrib-hist-navigator/xontrib-hist-navigator-0.1.0.tar.gz/xontrib-hist-navigator-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['xontrib']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['prompt-toolkit', 'xonsh>=0.9.17']
+
+setup_kwargs = {
+    'name': 'xontrib-hist-navigator',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Noortheen Raja',
+    'author_email': 'jnoortheen@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
