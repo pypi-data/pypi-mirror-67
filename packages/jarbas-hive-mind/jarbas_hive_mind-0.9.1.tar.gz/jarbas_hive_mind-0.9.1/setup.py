41c71afb1e52fb3bea9705173506f8ef8c8d77e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:33 +0000

    Add package jarbas-hive-mind/0.9.1/jarbas_hive_mind-0.9.1.tar.gz

---
+++ packages/jarbas-hive-mind/jarbas_hive_mind-0.9.1.tar.gz/jarbas_hive_mind-0.9.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='jarbas_hive_mind',
+    version='0.9.1',
+    packages=['jarbas_hive_mind',
+              'jarbas_hive_mind.master',
+              'jarbas_hive_mind.slave',
+              'jarbas_hive_mind.configuration',
+              'jarbas_hive_mind.database',
+              'jarbas_hive_mind.utils'],
+    include_package_data=True,
+    install_requires=["pyopenssl",
+                      "service_identity",
+                      "autobahn",
+                      "twisted",
+                      "jarbas_utils>=0.5.0",
+                      "json_database",
+                      "pycryptodome"],
+    url='https://github.com/JarbasAl/hive_mind',
+    license='MIT',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    description='Mesh Networking utilities for mycroft core'
+)
