ce028f704f6c8cb7a41f68d32da69eb8275d0b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:28 +0000

    Add package cli2gui/2020.8/cli2gui-2020.8-py3-none-any.whl

---
+++ packages/cli2gui/cli2gui-2020.8-py3-none-any.whl/cli2gui/decorators.py
@@ -0,0 +1,244 @@
+"""Decorator and entry point for the program
+"""
+# pylint: disable=import-outside-toplevel
+
+from os import path
+import sys
+import warnings
+from functools import reduce
+from copy import deepcopy
+
+# Parsers - these are all default so will not crash everything
+from argparse import ArgumentParser
+from optparse import OptionParser
+import getopt
+
+# Import module
+from cli2gui.tojson import (
+	argparse2json,
+	getopt2json,
+	optparse2json,
+	docopt2json,
+	click2json
+)
+from cli2gui.application import application
+
+DO_COMMAND = '--cli2gui'
+DO_NOT_COMMAND = '--disable-cli2gui'
+
+if sys.platform.startswith("win"):
+	def quote(value):
+		return u'"{}"'.format(u'{}'.format(value).replace(u'"', u'""'))
+else:	# POSIX shell
+	def quote(value):
+		return u"'{}'".format(u'{}'.format(value).replace(u"'", u"'\\''"))
+
+
+def merge(*maps):
+	"""Merge all maps left to right"""
+	copies = map(deepcopy, maps)
+	return reduce(lambda acc, val: acc.update(val) or acc, copies)
+
+
+def create_from_parser(self_parser, args_parser, kwargs_parser, source_path, **kwargs):
+	"""Generate a build_spec from a parser
+
+	Args:
+		self_parser (obj): A parser that acts on self. eg. ArgumentParser.parse_args
+		args_parser (tuple): A parser that acts on function arguments. eg. getopt.getopt
+		kwargs_parser (dict): A parser that acts on named params
+		source_path (str): Program source path
+
+	Returns:
+		dict: build_spec to be used by the application
+	"""
+	run_cmd = kwargs.get('target')
+	if run_cmd is None:
+		if hasattr(sys, 'frozen'):
+			run_cmd = quote(source_path)
+		else:
+			run_cmd = '{} -u {}'.format(quote(sys.executable), quote(source_path))
+
+	parser = kwargs.get("parser", "argparse")
+
+	build_spec = {
+		'run_function': kwargs.get('run_function'),
+		"parser": parser,
+		"gui": kwargs.get('gui'),
+		"theme": kwargs.get("theme", None),
+		"darkTheme": kwargs.get("darkTheme", None),
+		"sizes": kwargs.get("sizes", None),
+		'image': kwargs.get('image', None),
+		'program_name':	kwargs.get('program_name') or path.basename(sys.argv[0]).replace('.py', ''),
+		'program_description': kwargs.get('program_description', None),
+		'max_args_shown':	kwargs.get('max_args_shown', 5),
+		'menu': kwargs.get('menu', None),
+	}
+
+	# Select parser
+	if parser == "optparse":
+		build_spec.update(optparse2json.convert(self_parser))
+	if parser == "getopt":
+		build_spec.update(getopt2json.convert(args_parser))
+	if parser in ["argparse", "dephell_argparse"]:
+		build_spec.update(argparse2json.convert(self_parser))
+	if parser == "docopt":
+		build_spec.update(docopt2json.convert(self_parser))
+	if parser == "click":
+		build_spec.update(click2json.convert(build_spec["run_function"]))
+
+	return build_spec
+
+
+def Click2Gui(run_function, gui="pysimplegui", theme=None, darkTheme=None,
+ sizes=None, image=None, program_name=None, program_description=None,
+ max_args_shown=5, menu=None, **kwargs):
+	"""Decorator to use in the function that contains the argument parser
+	Serialises data to JSON and launches the Cli2Gui application
+
+	Args:
+		run_function (def, optional): The name of the function to call eg.
+		main(args). Defaults to None. If not specified, program continues as
+		normal (can only run once)
+		gui (str, optional): Override the gui to use. Current options are:
+		"pysimplegui", "pysimpleguiqt","pysimpleguiweb". Defaults to
+		"pysimplegui".
+		theme (str[], optional): Set a base24 theme. Can also pass a base24
+		scheme file. eg. one-light.yaml. Defaults to None.
+		darkTheme (str[], optional): Set a base24 dark theme variant. Can also
+		pass a base24 scheme file. eg. one-dark.yaml. Defaults to None.
+		sizes (dict, optional): Set the UI sizes such as the button size.
+		Defaults to None.
+		image (string, optional): Set the program icon. File extensions can be
+		any that PIL supports. Defaults to None.
+		program_name (string, optional): Override the program name. Defaults to
+		None.
+		program_description (string, optional): Override the program
+		description. Defaults to None.
+		max_args_shown (int, optional): Maximum number of args shown before
+		using a scrollbar. Defaults to 5.
+		menu (dict, optional): Add a menu to the program. Defaults to None. eg.
+		THIS_DIR = str(Path(__file__).resolve().parent)
+		menu={"File": THIS_DIR + "/file.md"}
+
+	Returns:
+		void: Runs the application
+	"""
+	parser = "click"
+	params = merge(locals(), locals()['kwargs'])
+	build_spec = create_from_parser(
+		None, None, kwargs,
+		sys.argv[0],
+		**params
+	)
+	return application.run(build_spec)
+
+
+def Cli2Gui(run_function=None, auto_enable=False, parser="argparse",
+gui="pysimplegui", theme=None, darkTheme=None, sizes=None, image=None,
+program_name=None, program_description=None, max_args_shown=5, menu=None,
+**kwargs):
+	"""Decorator to use in the function that contains the argument parser
+	Serialises data to JSON and launches the Cli2Gui application
+
+	Args:
+		run_function (def, optional): The name of the function to call eg.
+		main(args). Defaults to None. If not specified, program continues as
+		normal (can only run once)
+		auto_enable (bool, optional): Enable the GUI by default. If enabled by
+		default requires `--disable-cli2gui`, otherwise requires `--cli2gui`.
+		Defaults to False.
+		parser (str, optional): Override the parser to use. Current
+		options are: "argparse", "getopt", "optparse", "docopt",
+		"dephell_argparse". Defaults to "argparse".
+		gui (str, optional): Override the gui to use. Current options are:
+		"pysimplegui", "pysimpleguiqt","pysimpleguiweb". Defaults to
+		"pysimplegui".
+		theme (str[], optional): Set a base24 theme. Can also pass a base24
+		scheme file. eg. one-light.yaml. Defaults to None.
+		darkTheme (str[], optional): Set a base24 dark theme variant. Can also
+		pass a base24 scheme file. eg. one-dark.yaml. Defaults to None.
+		sizes (dict, optional): Set the UI sizes such as the button size.
+		Defaults to None.
+		image (string, optional): Set the program icon. File extensions can be
+		any that PIL supports. Defaults to None.
+		program_name (string, optional): Override the program name. Defaults to
+		None.
+		program_description (string, optional): Override the program
+		description. Defaults to None.
+		max_args_shown (int, optional): Maximum number of args shown before
+		using a scrollbar. Defaults to 5.
+		menu (dict, optional): Add a menu to the program. Defaults to None. eg.
+		THIS_DIR = str(Path(__file__).resolve().parent)
+		menu={"File": THIS_DIR + "/file.md"}
+
+	Returns:
+		void: Runs the application
+	"""
+	params = merge(locals(), locals()['kwargs'])
+
+	def build(calling_function):
+		def run_cli2gui(self, *args, **kwargs):
+			"""Generate the buildspec and run the GUI
+
+			Args:
+				calling_function (def): The calling function eg.
+				ArgumentParser.parse_args
+			"""
+			source_path = sys.argv[0]
+			build_spec = create_from_parser(
+				self, args, kwargs,
+				source_path,
+				payload_name=calling_function.__name__,
+				**params)
+			return application.run(build_spec)
+
+		def inner(*args, **kwargs):
+			"""Replace the inner functions with run_cli2gui. eg. When
+			ArgumentParser.parse_args is called, do run_cli2gui
+
+			Returns:
+				func(): Do the calling_function
+			"""
+			getopt.getopt = run_cli2gui
+			getopt.gnu_getopt = run_cli2gui
+			OptionParser.parse_args = run_cli2gui
+			ArgumentParser.parse_args = run_cli2gui
+			try:
+				import docopt
+				docopt.docopt = run_cli2gui
+			except ImportError:
+				pass
+			try:
+				import dephell_argparse
+				dephell_argparse.Parser.parse_args = run_cli2gui
+			except ImportError:
+				pass
+			# Using type=argparse.FileType('r') leads to a resource warning
+			with warnings.catch_warnings():
+				warnings.filterwarnings("ignore", category=ResourceWarning)
+				return calling_function(*args, **kwargs)
+
+		inner.__name__ = calling_function.__name__
+		return inner
+
+
+	def run_without_cli2gui(calling_function):
+		def inner(*args, **kwargs):
+			# Using type=argparse.FileType('r') leads to a resource warning
+			with warnings.catch_warnings():
+				warnings.filterwarnings("ignore", category=ResourceWarning)
+				return calling_function(*args, **kwargs)
+		inner.__name__ = calling_function.__name__
+		return inner
+
+	"""If enabled by default requires do_not_command, otherwise requires do_command """
+	if (not auto_enable and DO_COMMAND not in sys.argv) or (auto_enable
+	and DO_NOT_COMMAND in sys.argv):
+		if DO_NOT_COMMAND in sys.argv:
+			sys.argv.remove(DO_NOT_COMMAND)
+		return run_without_cli2gui
+	return build
+
+if __name__ == '__main__':
+	pass
---
+++ packages/cli2gui/cli2gui-2020.8-py3-none-any.whl/cli2gui/tojson/argparse2json.py
@@ -0,0 +1,164 @@
+"""Generate a dict describing argparse arguments
+"""
+# pylint: disable=protected-access
+import argparse
+from os import path
+from sys import argv
+from argparse import (
+	_CountAction,
+	_HelpAction,
+	_StoreFalseAction,
+	_StoreTrueAction,
+	_SubParsersAction,
+	_MutuallyExclusiveGroup)
+
+
+def iter_parsers(parser):
+	''' Iterate over name, parser pairs '''
+	try:
+		# Get the actions from the subparser
+		return [action for action in parser._actions if isinstance(
+			action, _SubParsersAction)][0].choices.items()
+	except IndexError:
+		# There is no subparser
+		return list([('::cli2gui/default', parser)])
+
+def is_default_progname(name, subparser):
+	'''Identify if the passed name is the default program name '''
+	return subparser.prog == '{} {}'.format(path.split(argv[0])[-1], name)
+
+
+def choose_name(name, subparser):
+	'''Get the program name	'''
+	return name if is_default_progname(name, subparser) else subparser.prog
+
+def contains_actions(a, b):
+	''' check if any actions(a) are present in actions(b) '''
+	return set(a).intersection(set(b))
+
+def reapply_mutex_groups(mutex_groups, action_groups):
+	'''
+	argparse stores mutually exclusive groups independently
+	of all other groups. So, they must be manually re-combined
+	with the groups/subgroups to which they were originally declared
+	in order to have them appear in the correct location in the UI.
+
+	Order is attempted to be preserved by inserting the MutexGroup
+	into the _actions list at the first occurrence of any item
+	where the two groups intersect
+	'''
+	def swap_actions(actions):
+		for mutexgroup in mutex_groups:
+			mutex_actions = mutexgroup._group_actions
+			if contains_actions(mutex_actions, actions):
+				# make a best guess as to where we should store the group
+				targetindex = actions.index(mutexgroup._group_actions[0])
+				# insert the _ArgumentGroup container
+				actions[targetindex] = mutexgroup
+				# remove the duplicated individual actions
+				actions = [action for action in actions
+						if action not in mutex_actions]
+		return actions
+
+	return [group.update({'items': swap_actions(group['items'])}) or group
+			for group in action_groups]
+
+
+def extract_groups(action_group):
+	'''
+	Recursively extract argument groups and associated actions
+	from ParserGroup objects
+	'''
+	return {
+		'name': action_group.title,
+		# List of items that are not help messages
+		'items': [action for action in action_group._group_actions
+                    if not isinstance(action, _HelpAction)],
+		'groups': [extract_groups(group)
+                    for group in action_group._action_groups],
+	}
+
+def action_to_json(action, widget):
+	'''Generate json for an action and set the widget - used by the application'''
+	return {
+		'type': widget,
+		'data': {
+			'display_name': action.metavar or action.dest,
+			'help': action.help,
+			'commands': action.option_strings,
+			'choices': list(map(str, action.choices)) if action.choices else [],
+			'dest': action.dest,
+		},
+	}
+
+
+def build_radio_group(mutex_group):
+	'''Create a radio group for a mutex group of arguments '''
+	return {
+		'type': 'Group',
+		'data': {
+			'commands': [action.option_strings for action in mutex_group._group_actions],
+			'widgets': list(categorizeItems(mutex_group._group_actions))
+		}
+  	}
+
+
+def categorizeItems(actions):
+	'''Catergorise each action and generate json '''
+	for action in actions:
+		if isinstance(action, _MutuallyExclusiveGroup):
+			yield build_radio_group(action)
+		elif isinstance(action, (_StoreTrueAction, _StoreFalseAction)):
+			yield action_to_json(action, "Bool")
+		elif isinstance(action, _CountAction):
+			yield action_to_json(action, "Counter")
+		elif action.choices:
+			yield action_to_json(action, "Dropdown")
+		elif isinstance(action.type, argparse.FileType):
+			yield action_to_json(action, "File")
+		else:
+			yield action_to_json(action, "TextBox")
+
+
+def categorizeGroups(groups):
+	'''Categorize the parser groups and items '''
+	return [{
+		'name': group['name'],
+		'items': list(categorizeItems(group['items'])),
+		'groups': categorizeGroups(group['groups']),
+	} for group in groups]
+
+
+def strip_empty(groups):
+	'''Remove groups where group['items'] is false '''
+	return [group for group in groups if group['items']]
+
+
+def process(parser):
+	'''Reapply the mutex groups and then categorize them and the items under
+	the parser '''
+	mutex_groups = parser._mutually_exclusive_groups
+	raw_action_groups = [extract_groups(group) for group in parser._action_groups
+                      if group._group_actions]
+	corrected_action_groups = reapply_mutex_groups(mutex_groups, raw_action_groups)
+	return categorizeGroups(strip_empty(corrected_action_groups))
+
+
+def convert(parser):
+	"""Convert argparse to a dict
+
+	Args:
+		parser (argparse): argparse parser
+
+	Returns:
+		dict: dictionary representing parser object
+	"""
+	return {
+		'parser_description': parser.description,
+		'widgets': [
+			{
+				'name': choose_name(name, sub_parser),
+				'contents': process(sub_parser)
+			} for name, sub_parser in iter_parsers(parser)
+		]
+	}
