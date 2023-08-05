30cc843b1834c8c514aaab2f7038dd8b5668338a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:16 +0000

    Add package elasticsearch-sbs/1.1.1/elasticsearch_sbs-1.1.1.tar.gz

---
+++ packages/elasticsearch-sbs/elasticsearch_sbs-1.1.1.tar.gz/elasticsearch_sbs-1.1.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="elasticsearch_sbs",
+    version="1.1.1",
+    author="Sara Ben Shabbat",
+    author_email="sarabenshabbat@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["elasticsearch_sbs"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
