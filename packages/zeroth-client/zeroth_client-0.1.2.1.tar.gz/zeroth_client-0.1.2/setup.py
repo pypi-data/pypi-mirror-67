1f3ee6fcba61e768d538a87b31a4150c077f8780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:12 +0000

    Add package zeroth-client/0.1.2/zeroth_client-0.1.2.1.tar.gz

---
+++ packages/zeroth-client/zeroth_client-0.1.2.1.tar.gz/zeroth_client-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name                = 'zeroth_client',
+    version             = '0.1.2',
+    description         = 'python client package for zeroth',
+    author              = 'henry.kim',
+    author_email        = 'henry@atlaslabs.ai',
+    install_requires    =  [],
+    packages            = find_packages(exclude = []),
+    keywords            = ['pypi deploy'],
+    python_requires     = '>=3',
+    package_data        = {},
+    zip_safe            = False,
+    classifiers         = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    scripts=['bin/zeroth_client'],
+)
