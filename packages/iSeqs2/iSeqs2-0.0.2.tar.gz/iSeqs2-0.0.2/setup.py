b9cfb87af47a55fed4626b755ec66f4a0c4a1a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:44 +0000

    Add package iSeqs2/0.0.2/iSeqs2-0.0.2.tar.gz

---
+++ packages/iSeqs2/iSeqs2-0.0.2.tar.gz/iSeqs2-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+#scripts=['change_set_subfield'] ,
+
+setuptools.setup(
+     name='iSeqs2',  
+     version='0.0.2',
+     author="Alessandro Coppe",
+     author_email="",
+     description="Another bunch of scripts I use in my Next Generation Sequencing Bioinformatics Analyses",
+     url="https://github.com/alexcoppe/iSeqs2",
+     packages=["iseqs2"],
+     scripts=["scripts/build_leukemia_genes_list/build_leukemia_genes_list.py",
+         "scripts/filter_by_normal_expression/filter_by_normal_expression.py",
+         "scripts/filter_strelka2/filter_strelka2.py",
+         "scripts/remove_snps_from_cosmic/remove_snps_from_cosmic.py",
+         "scripts/filter_vcf_by_gene_names/filter_vcf_by_gene_names.py",
+         "scripts/filter_varscan2_vcfs/filter_varscan2_vcfs.py",
+         "scripts/join_tables_from_snpsift_extractFields/join_tables_from_snpsift_extractFields.py"],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+)
