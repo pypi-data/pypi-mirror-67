0268c4590bd35aaad759bfbc8e794dc82ce1d4e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:31 +0000

    Add package sbs-SaraBenShabbat/0.0.9/sbs_SaraBenShabbat-0.0.9-py3-none-any.whl

---
+++ packages/sbs-SaraBenShabbat/sbs_SaraBenShabbat-0.0.9-py3-none-any.whl/sbs/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 28 22:37:23 2020
+
+@author: Sara Ben Shabbat
+"""
+
+#import sbs
+
+
+# Define a variable
+author_name = 'Sara Ben Shabbat'
+
+# Define a function
+def hello_world() -> None:
+    print('Hello World !')
+    
+# Define a class
+class fruit:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def print_fruit_attributes(self):
+        print('The fruit name is = ' + self.name)
+        print('The fruit color is = ' + self.color)
+
