b6d1898e689f128dee011abafef463e7109b8ee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:14 +0000

    Add package jif/2.0.2/jif-2.0.2.tar.gz

---
+++ packages/jif/jif-2.0.2.tar.gz/jif-2.0.2/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="jif",
+    version="2.0.2",
+    description="CLI tool to run scipts",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JasonAtallah/jif",
+    download_url="https://github.com/JasonAtallah/jif/archive/v2.0.2.tar.gz",
+    author="Jason Atallah",
+    author_email="jason.atallah@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["jif"],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={"console_scripts": ["jif=jif.__main__:main",]},
+)
