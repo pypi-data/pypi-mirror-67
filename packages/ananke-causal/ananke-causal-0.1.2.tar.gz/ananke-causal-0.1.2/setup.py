9383e4db21c75f7796acd7a2814e7a858b505e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:11 +0000

    Add package ananke-causal/0.1.2/ananke-causal-0.1.2.tar.gz

---
+++ packages/ananke-causal/ananke-causal-0.1.2.tar.gz/ananke-causal-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(name='ananke-causal',
+      version='0.1.2',
+      description='Python',
+      url='',
+      author='Rohit Bhattacharya',
+      author_email='rbhatta8@jhu.edu',
+      install_requires=requirements,
+      packages=['ananke'],
+      zip_safe=False)
