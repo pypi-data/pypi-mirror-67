a9801fb0f89700eccbb0cea44c5aca90248cce0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:17 +0000

    Add package cpmd-cube-tools/0.992/cpmd_cube_tools-0.992-py3-none-any.whl

---
+++ packages/cpmd-cube-tools/cpmd_cube_tools-0.992-py3-none-any.whl/cpmd_cube_tools/_citation.py
@@ -0,0 +1,3 @@
+Acknowledgement="This code is partially developed based on the Cube-Toolz @ https://github.com/funkymunkycool/Cube-Toolz/blob/master/cube_tools.py"
+citation="If you want to publish your results obtained (partially) from this software, \
+please cite this paper:'On the accuracy of molecular simulation-based predictions of koff values: a Metadynamics study', DOI: https://doi.org/10.1101/2020.03.30.015396"
