207da616e0a86c8c89f19560380267a5edfe50eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:05 +0000

    Add package mbed-project/1.0.0/mbed_project-1.0.0-py3-none-any.whl

---
+++ packages/mbed-project/mbed_project-1.0.0-py3-none-any.whl/mbed_project/__init__.py
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2020 Arm Mbed. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+"""Exposes the primary interfaces for the library."""
+
+from mbed_project._version import __version__
+from mbed_project.mbed_project import initialise_project, clone_project, checkout_project_revision, get_known_libs
+from mbed_project.mbed_program import MbedProgram
