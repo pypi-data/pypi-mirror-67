ac6f90ad3c7e3cb763ce556d8195b049a2bf0cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:56 +0000

    Add package gids/1.1/gids-1.1.tar.gz

---
+++ packages/gids/gids-1.1.tar.gz/gids-1.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='gids',
+    version='1.1',
+    license='',
+    author_email="gkswlsrb95@gmail.com",
+    description="Google-images-downloader module using selenium",
+    long_description=open('README.md').read(), 
+    url="https://github.com/jinkyuhan/google-images-download",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
