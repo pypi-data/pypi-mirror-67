0c57af2aedca4760e9ad931c857a463fa6c29a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:17 +0000

    Add package nteu-translation-engine/0.1/nteu_translation_engine-0.1.tar.gz

---
+++ packages/nteu-translation-engine/nteu_translation_engine-0.1.tar.gz/nteu_translation_engine-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='nteu_translation_engine',
+    version='0.1',
+    description='NTEU translation engine',
+    url='https://github.com/Pangeamt/nteu-translation-engine',
+    author='PangeaMT',
+    author_email='a.cerda@pangeanic.es',
+    license='MIT',
+    packages=[
+        'nteu_translation_engine'
+    ],
+    zip_safe=False
+)
