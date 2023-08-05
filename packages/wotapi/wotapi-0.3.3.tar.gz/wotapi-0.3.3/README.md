8f0e0e053ffcd95f3725779b4e104f9e9f506e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:17 +0000

    Add package wotapi/0.3.3/wotapi-0.3.3.tar.gz

---
+++ packages/wotapi/wotapi-0.3.3.tar.gz/wotapi-0.3.3/README.md
@@ -0,0 +1,127 @@
+[![pipeline status](https://gitlab.com/gabriel_oana/worldoftanks/badges/master/pipeline.svg)](https://gitlab.com/gabriel_oana/worldoftanks)
+[![pipeline status](https://gitlab.com/gabriel_oana/worldoftanks/badges/master/coverage.svg)](https://gitlab.com/gabriel_oana/worldoftanks)
+# World of Tanks - API (PC)
+
+### 1. Description
+This package will extract data from the Wargaming World of Tanks API.      
+Currently, this works only for the PC version with the rest of the platforms to be implemented in future iterations.
+
+The package will require the following from the official [World of Tanks Developer API](https://developers.wargaming.net/) page.
+* application id
+* account id
+* access token
+
+All data extracted will be written to a local sqlite database ready to be accessed. The database is automatically created
+at the location where the script is executed.    
+The name of the database is ```world_of_tanks.db``` of type sqlite.
+
+
+### 2. Install
+
+```
+pip install WotAPI
+```
+
+### 3. Usage
+
+```
+from worldoftanks import WotAPI
+
+wot = WotAPI(application_id='############',
+             account_id='##########',
+             token='#########')
+
+# Create the local database
+wot.db_init()
+```
+
+```
+# Extract Account Data
+wot.player_personal()
+wot.player_vehicles()
+wot.player_achievements()
+
+# Extract Tankopedia Data
+wot.tankopedia_vehicles(load_once=True)
+wot.tankopedia_achievements(load_once=True)
+wot.tankopedia_information(load_once=True)
+wot.tankopedia_maps(load_once=True)
+```
+
+
+All data from the Tankopedia part of the API needs to be loaded only once in the database, otherwise this will be duplicated. 
+For ease, the argument ```load_once``` is by default set to True. 
+
+The data can be accessed from the ```wot``` objects for further development. The response is a list of dictionaries.
+```
+achievements = wot.player_achivements(load_once=True)
+print(achievements)
+
+[{
+'name': 'medalBobAmway921', 
+'outdated': True, 
+'section': 'action', 
+'section_order': 6, 
+'image_big': 'http://api.worldoftanks.eu/static/2.66.0/wot/encyclopedia/achievement/big/medalBobAmway921.png', 
+'hero_info': None, 
+'name_i18n': None, 
+'order': 1443, 
+'type': 'single', 
+'image': 'http://api.worldoftanks.eu/static/2.66.0/wot/encyclopedia/achievement/medalBobAmway921.png', 
+'condition': 'None', 
+'description': None
+} ... 
+]
+```
+
+
+
+### 4. Left To Do
+
+| API Part      | Name                      | Date Completed    | Version   |
+| ---           | ---                       | ---               | ---       |
+| Accounts      | Player Personal Data      | 2020-04-24        | 0.0.1     |
+| Accounts      | Player Vehicles           | 2020-04-24        | 0.0.1     |
+| Accounts      | Player Achievements       | 2020-04-24        | 0.0.1     |
+| Tankopedia    | Vehicles                  | 2020-04-25        | 0.0.2     |
+| Tankopedia    | Achievements              | 2020-04-25        | 0.0.2     |
+| Tankopedia    | Tankopedia Information    | 2020-04-25        | 0.0.2     |
+| Tankopedia    | Maps                      | 2020-04-25        | 0.0.2     |
+| Tankopedia    | Badges                    |                   |           |
+| Tankopedia    | Vehicle characteristics   |                   |           |
+| Tankopedia    | Engines                   |                   |           |
+| Tankopedia    | Turrets                   |                   |           |
+| Tankopedia    | Radios                    |                   |           |
+| Tankopedia    | Suspensions               |                   |           | 
+| Tankopedia    | Guns                      |                   |           |
+| Tankopedia    | Equipment and Consumables |                   |           |
+| Tankopedia    | Personal Missions         |                   |           |
+| Tankopedia    | Personal Reserves         |                   |           |
+| Tankopedia    | Vehicle Configurations    |                   |           |
+| Tankopedia    | Modules                   |                   |           |
+| Tankopedia    | Badges                    |                   |           |
+| Tankopedia    | Crew Qualifications       |                   |           |
+| Tankopedia    | Crew Skills               |                   |           |
+| Vehicles      | Vehicle statistics        | 2020-04-27        | 0.3.2     |
+| Vehicles      | Vehicle achievements      | 2020-04-27        | 0.3.2     |
+| Clans         |                           |                   |           |
+| Clan ratings  |                           |                   |           |
+| Strongholds   |                           |                   |           |
+| Global Map    |                           |                   |           |
+    
+ 
+### 5. Development
+
+##### Unittesting
+For development purposes, the unittests can be executed via: 
+
+```
+python3 -m unittest discover -v worldoftanks/tests
+```
+
+##### Coverage Tests
+
+```
+coverage run --source=worldoftanks -m unittest discover -s worldoftanks/tests
+coverage report -m
+```
---
+++ packages/wotapi/wotapi-0.3.3.tar.gz/wotapi-0.3.3/worldoftanks/WotAPI.py
@@ -0,0 +1,177 @@
+import os
+
+from worldoftanks.helper.validators import Validators
+from worldoftanks.helper.create_engine import create_db_engine
+from worldoftanks.helper.logger import create_logger
+from worldoftanks.orm.data_model import DataModel
+from worldoftanks.action.player_personal_data import PlayerPersonalData
+from worldoftanks.action.player_vehicles_data import PlayerVehiclesData
+from worldoftanks.action.player_achievements import PlayerAchievementsData
+from worldoftanks.action.tankopedia_vehicles import TankopediaVehiclesData
+from worldoftanks.action.tankopedia_achievements import TankopediaAchievementsData
+from worldoftanks.action.tankopedia_info import TankopediaInfoData
+from worldoftanks.action.tankopedia_maps import TankopediaMapsData
+from worldoftanks.action.vehicles_statistics import VehicleStatisticsData
+from worldoftanks.action.vehicles_achievements import VehicleAchievementsData
+
+
+class WotAPI:
+
+    def __init__(self, application_id: str, account_id: str, token: str, realm: str, quietly=False,
+                 log_level="WARNING"):
+        self.log_level = create_logger(log_level)
+        self.quietly = quietly
+        self.application_id = application_id
+        self.account_id = account_id
+        self.token = token
+        self.realm = realm
+
+    def _check_parameters(self):
+        """
+        Validates if the application id, account_id and token exist.
+        """
+        Validators.check_if_param_exists(self.token, 'token')
+        Validators.check_if_param_exists(self.account_id, 'account_id')
+        Validators.check_if_param_exists(self.application_id, 'application_id')
+        Validators.check_realm(self.realm)
+
+    def db_init(self):
+        """
+        Creates the sqlite database and all tables for data to be populated.
+        """
+        engine = create_db_engine(path=os.getcwd())
+        DataModel.create_tables(engine=engine)
+        if not self.quietly:
+            print('Database created')
+
+    def player_personal(self, load_to_db=True) -> list:
+        """
+        Handles the extraction, transformation and loading of personal data into the database.
+        Requires a personal access token.
+        """
+
+        self._check_parameters()
+
+        personal_data = PlayerPersonalData()
+        data = personal_data.etl_data(application_id=self.application_id, account_id=self.account_id, token=self.token,
+                                      load_to_db=load_to_db, realm=self.realm)
+
+        if not self.quietly:
+            print('Player personal data has been extracted')
+
+        return data
+
+    def player_vehicles(self, load_to_db=True) -> list:
+        """
+        Handles the extraction, transformation and loading of player vehicle data into the database.
+        Requires a personal access token.
+        """
+        self._check_parameters()
+
+        vehicles_data = PlayerVehiclesData()
+        data = vehicles_data.etl_data(application_id=self.application_id, account_id=self.account_id, token=self.token,
+                                      load_to_db=load_to_db, realm=self.realm)
+
+        if not self.quietly:
+            print("Player personal vehicles data has been extracted")
+        return data
+
+    def player_achievements(self, load_to_db=True) -> list:
+        """
+        Handles the extraction, transformation and loading of player achievements data into the database.
+        Requires a personal access token.
+        """
+        self._check_parameters()
+
+        vehicles_data = PlayerAchievementsData()
+        data = vehicles_data.etl_data(application_id=self.application_id, account_id=self.account_id, token=self.token,
+                                      load_to_db=load_to_db, realm=self.realm)
+
+        if not self.quietly:
+            print("Player personal achievements data has been extracted")
+        return data
+
+    def tankopedia_vehicles(self, load_once: bool, load_to_db=True) -> list:
+        """
+        Handles the extraction, transformation and loading of tankopedia vehicles data into the database.
+        """
+        self._check_parameters()
+
+        vehicles = TankopediaVehiclesData()
+        data = vehicles.etl_data(application_id=self.application_id, account_id=self.account_id, token=self.token,
+                                 load_to_db=load_to_db, load_once=load_once, realm=self.realm)
+
+        if not self.quietly:
+            print("Player tankopedia vehicles data has been extracted")
+        return data
+
+    def tankopedia_achievements(self, load_once: bool, load_to_db=True) -> list:
+        """
+        Handles the extraction, transformation and loading of tankopedia achievements data into the database.
+        """
+        self._check_parameters()
+
+        vehicles = TankopediaAchievementsData()
+        data = vehicles.etl_data(application_id=self.application_id, account_id=self.account_id, token=self.token,
+                                 load_to_db=load_to_db, load_once=load_once, realm=self.realm)
+
+        if not self.quietly:
+            print("Player tankopedia achievements has been extracted")
+        return data
+
+    def tankopedia_information(self, load_once: bool, load_to_db=True) -> list:
+        """
+        Handles the extraction, transformation and loading of tankopedia information data into the database.
+        """
+        self._check_parameters()
+
+        info = TankopediaInfoData()
+        data = info.etl_data(application_id=self.application_id, account_id=self.account_id, token=self.token,
+                             load_to_db=load_to_db, load_once=load_once, realm=self.realm)
+
+        if not self.quietly:
+            print("Player tankopedia information has been extracted")
+        return data
+
+    def tankopedia_maps(self, load_once: bool, load_to_db=True) -> list:
+        """
+        Handles the extraction, transformation and loading of tankopedia maps data into the database.
+        """
+        self._check_parameters()
+
+        info = TankopediaMapsData()
+        data = info.etl_data(application_id=self.application_id, account_id=self.account_id, token=self.token,
+                             load_to_db=load_to_db, load_once=load_once, realm=self.realm)
+
+        if not self.quietly:
+            print("Player tankopedia maps has been extracted")
+        return data
+
+    def vehicle_statistics(self, load_to_db=True) -> list:
+        """
+        Handles the extraction, transformation and loading of vehicle statistics data into the database.
+        """
+        self._check_parameters()
+
+        vehicles = VehicleStatisticsData()
+        data = vehicles.etl_data(application_id=self.application_id, account_id=self.account_id, token=self.token,
+                                 load_to_db=load_to_db, realm=self.realm)
+
+        if not self.quietly:
+            print("Vehicle statistics has been extracted")
+        return data
+
+    def vehicle_achievements(self, load_to_db=True) -> list:
+        """
+        Handles the extraction, transformation and loading of vehicle statistics data into the database.
+        """
+        self._check_parameters()
+
+        vehicles = VehicleAchievementsData()
+        data = vehicles.etl_data(application_id=self.application_id, account_id=self.account_id, token=self.token,
+                                 load_to_db=load_to_db, realm=self.realm)
+
+        if not self.quietly:
+            print("Vehicle achievements has been extracted")
+        return data
+
