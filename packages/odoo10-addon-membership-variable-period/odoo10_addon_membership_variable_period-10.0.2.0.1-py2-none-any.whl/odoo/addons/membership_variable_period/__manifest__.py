85980e53cb2bf2d19ef32e91c5e5c68a7827a0c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:43 +0000

    Add package odoo10-addon-membership-variable-period/10.0.2.0.1/odoo10_addon_membership_variable_period-10.0.2.0.1-py2-none-any.whl

---
+++ packages/odoo10-addon-membership-variable-period/odoo10_addon_membership_variable_period-10.0.2.0.1-py2-none-any.whl/odoo/addons/membership_variable_period/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2017 David Vidal <david.vidal@tecnativa.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    'name': 'Variable period for memberships',
+    'version': '10.0.2.0.1',
+    'license': 'AGPL-3',
+    'category': 'Association',
+    'author': 'Tecnativa, '
+              'Odoo Community Association (OCA)',
+    'website': 'https://odoo-community.org/',
+    'depends': [
+        'membership_extension',
+    ],
+    'data': [
+        'views/product_template_views.xml',
+    ],
+    "installable": True,
+}
