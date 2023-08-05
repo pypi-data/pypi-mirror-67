7f4006b1f7cf9898fbaf785ae374a4b36b1e922b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:44 +0000

    Add package bingoset/0.1.2/bingoset-0.1.2.tar.gz

---
+++ packages/bingoset/bingoset-0.1.2.tar.gz/bingoset-0.1.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['bingoset', 'bingoset.utilities']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Pillow>=7.1.2,<8.0.0', 'requests>=2.23.0,<3.0.0', 'typer[all]>=0.1.1,<0.2.0']
+
+entry_points = \
+{'console_scripts': ['bingoset = bingoset.main:app']}
+
+setup_kwargs = {
+    'name': 'bingoset',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': '<h4 align="center">\n    <a href="https://github.com/Akshay090/bingoset">\n        <img src="https://raw.githubusercontent.com/Akshay090/bingoset/master/.github/bingoset-banner.png" alt="bingoset" />\n    </a>\n    <br>\n    <br>\n     CLI Toolkit to quickly create image dataset using Bing Image Search API\n\n![PyPI - License](https://img.shields.io/pypi/l/bingoset?style=plastic)\n![PyPI - Python Version](https://img.shields.io/pypi/pyversions/bingoset)\n![Twitter Follow](https://img.shields.io/twitter/follow/aks2899?style=social) \n\n</h4>\n\n# Welcome to BingoSet \n\n\n## Install\n\n```sh\npip3 install bingoset\n```\n## Set-up\n\nGet your Bingo Image Search API key and execute below command\n\n```sh\nbingoset set-api-key YOUR_BING_API_KEY_HERE\n```\n\n## Usage \n\n```sh\nbingoset q pikachu\n```\nThis will download 250 (default) images of pikachu into a directory called dataset (default)\n\n## Additional/Optional Config\n\n```sh\nbingoset set-max-results 100\n```\nChange the maximum number of images per request by the Bing API, eg : 100\nDEFAULT : 250\n\n```sh\nbingoset set-group-size 20\n```\nChange the group size of images you want to download, eg : 100\ngroupsize is number of search results to return “per page”\nDEFAULT : 50\n\n\n```sh\nbingoset set-base-folder-name mydatast\n```\nChange the base folder name\nDEFAULT : dataset\n',
+    'author': 'Akshay Ashok',
+    'author_email': 'aks28id@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Akshay090/bingoset',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
