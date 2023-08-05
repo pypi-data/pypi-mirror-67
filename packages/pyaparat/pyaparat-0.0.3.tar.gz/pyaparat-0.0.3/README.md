b61464d348c75bd769266f1a1ddd1597c854ce79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:53 +0000

    Add package pyaparat/0.0.3/pyaparat-0.0.3.tar.gz

---
+++ packages/pyaparat/pyaparat-0.0.3.tar.gz/pyaparat-0.0.3/README.md
@@ -0,0 +1,15 @@
+## Download 'aparat.com' videos with python
+
+
+install:
+```python
+pip install pyaparat
+```
+usage:
+
+```python
+from pyaparat import Main
+
+a = Main('url', 'quality')
+a.download()
+```
---
+++ packages/pyaparat/pyaparat-0.0.3.tar.gz/pyaparat-0.0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="pyaparat",
+	version="0.0.3",
+	author="amirhosseing bigdelu",
+	author_email="amirbig44@gmail.com",
+	description="download aparat.com videos",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/amirbigg/pyaparat",
+	packages=setuptools.find_packages(),
+	install_requires=[
+		'requests', 'beautifulsoup4',
+	],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
