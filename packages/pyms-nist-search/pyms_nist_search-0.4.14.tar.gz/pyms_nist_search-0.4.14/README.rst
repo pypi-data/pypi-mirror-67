37c724ab42ac9ec3b638c430ee6f4e1742f8a82a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:52 +0000

    Add package pyms-nist-search/0.4.14/pyms_nist_search-0.4.14.tar.gz

---
+++ packages/pyms-nist-search/pyms_nist_search-0.4.14.tar.gz/pyms_nist_search-0.4.14/README.rst
@@ -0,0 +1,80 @@
+************************
+PyMassSpec NIST Search
+************************
+
+.. image:: https://travis-ci.com/domdfcoding/pynist.svg?branch=master
+	:target: https://travis-ci.com/domdfcoding/pynist
+	:alt: Linux Build Status
+.. image:: https://ci.appveyor.com/api/projects/status/82cs9prucypd1igb?svg=true
+	:target: https://ci.appveyor.com/project/domdfcoding/pyms-nist-search/branch/master
+	:alt: Windows Build Status
+.. image:: https://readthedocs.org/projects/pymassspec/badge/?version=latest
+	:target: https://pymassspec.readthedocs.io/en/latest/?badge=latest
+	:alt: Documentation Status
+.. image:: https://img.shields.io/pypi/v/pyms_nist_search.svg
+	:target: https://pypi.org/project/pyms_nist_search/
+	:alt: PyPI
+.. image:: https://img.shields.io/pypi/pyversions/pyms_nist_search.svg
+	:target: https://pypi.org/project/pyms_nist_search/
+	:alt: PyPI - Python Version
+.. image:: https://img.shields.io/pypi/l/pyms-nist-search
+	:alt: PyPI - License
+	:target: https://www.gnu.org/licenses/lgpl-3.0.en.html
+
+
+
+PyMassSpec extension for searching mass spectra using NIST's Spectrum Search Engine
+
+PyMassSpec NIST Search is Free Software licensed under the `GNU Lesser General Public License Version 3 <https://www.gnu.org/licenses/lgpl-3.0.en.html>`_
+
+A copy of the MassBank of North America database, in JSON, MSP and NIST Library formats, is included for the purposes of these tests.
+This library was created on 22 April 2020 using the "parse_mona_json.py" script and Lib2Nist.
+Licensed under the CC BY 4.0 License.
+For a list of contributors, see the file `MoNA_GCMS_Library/AUTHORS`
+.. TODO: add links.
+
+Usage
+########
+
+You will need to supply your own copy of the NIST Mass Spectral library to use this software.
+
+The main class in this library is the `Engine` class. This class performs the actual searching. Start by initialising the search engine as follows:
+
+.. code-block:: python
+
+	search = pyms_nist_search.Engine(
+			FULL_PATH_TO_MAIN_LIBRARY,
+			pyms_nist_search.NISTMS_MAIN_LIB,
+			FULL_PATH_TO_WORK_DIR,
+			)
+
+Where ``FULL_PATH_TO_MAIN_LIBRARY`` is the path to the location of your mass spectral library, and ``FULL_PATH_TO_WORK_DIR`` is the path to the working directory to be used by the search engine.
+
+A `MassSpectrum` object can then be searched as follows:
+
+.. code-block:: python
+
+	search.full_search_with_ref_data(mass_spec)
+
+This will return a list of tuples consisting of `SearchResult` and `ReferenceData` objects for the possible identities of the mass spectrum.
+
+A list of just the `SearchResult` objects can be obtained with this method:
+
+.. code-block:: python
+
+	hit_list = search.full_search(mass_spec)
+
+For each of these hits, the reference data can be obtained as follows:
+
+.. code-block:: python
+
+	for hit in hit_list:
+		ref_data = search.get_reference_data(hit.spec_loc)
+
+
+TODO
+#####
+
+1. Build a user library for use in tests
+2. Allow user libraries to be used. IIRC they can't be currently.
+3. Write comprehensive tests using pytest
---
+++ packages/pyms-nist-search/pyms_nist_search-0.4.14.tar.gz/pyms_nist_search-0.4.14/setup.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+"""Setup script"""
+
+import pathlib
+import sys
+
+from setuptools import Extension, find_packages, setup
+
+print(pathlib.Path.cwd())
+
+copyright = """
+2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+"""
+
+VERSION = "0.4.14"
+
+modname = "pyms_nist_search"
+
+short_desc = "PyMassSpec extension for searching mass spectra using NIST's Mass Spectrum Search Engine"
+
+author = "Dominic Davis-Foster"
+author_email = "dominic@davis-foster.co.uk"
+github_username = "domdfcoding"
+
+classifiers = [
+		"Development Status :: 4 - Beta",
+		# "Development Status :: 5 - Production/Stable",
+		# "Development Status :: 6 - Mature",
+		# "Development Status :: 7 - Inactive",
+
+		"Operating System :: Microsoft :: Windows",
+		"Operating System :: Microsoft :: Windows :: Windows 10",
+		"Operating System :: Microsoft :: Windows :: Windows 7",
+		"Operating System :: Microsoft :: Windows :: Windows 8.1",
+
+		"Operating System :: POSIX :: Linux",
+		# "Operating System :: OS Independent",
+
+		"Intended Audience :: Developers",
+		"Intended Audience :: Science/Research",
+
+		"License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+
+		"Programming Language :: C",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Programming Language :: Python :: 3 :: Only",
+		"Programming Language :: Python :: Implementation :: CPython",
+
+		# "Topic :: Database :: Front-Ends",
+		"Topic :: Scientific/Engineering :: Chemistry",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		]
+
+
+common_kwargs = dict(
+		author=author,
+		author_email=author_email,
+		classifiers=classifiers,
+		description=short_desc,
+		license='LGPLv3',
+		name=modname,
+		version=VERSION,
+		packages=["pyms_nist_search"],
+		package_dir={'pyms_nist_search': 'src/pyms_nist_search'},
+		python_requires=">=3.6",
+		package_data={modname: ['pyms_nist_search/templates/*']},
+		include_package_data=True,
+		url=f"https://github.com/{github_username}/{modname}",
+		)
+
+if pathlib.Path.cwd().name == "doc-source":
+	print(pathlib.Path.cwd().parent / "README.rst")
+	install_requires = (pathlib.Path.cwd().parent / "requirements.txt").read_text().split("\n")
+	common_kwargs["long_description"] = (pathlib.Path.cwd().parent / "README.rst").read_text() + '\n'
+else:
+	print(pathlib.Path("README.rst"))
+	install_requires = pathlib.Path("requirements.txt").read_text().split("\n")
+	common_kwargs["long_description"] = pathlib.Path("README.rst").read_text() + '\n'
+
+docker_only_reqs = [
+		"docker>=4.2.0",
+		"requests>=2.22.0",
+		]
+
+build_macros = [
+		('INTERNALBUILD', '1'),
+		('WIN32', '1'),
+		('MSTXTDATA', '1'),
+		]
+
+##############################
+
+if __name__ == '__main__':
+	
+	if sys.platform == "win32":
+		
+		if sys.maxsize > 2 ** 32:
+			x = 64
+			bit = 64
+			ctree = "ctNt66_64"
+			# libraries = ['src/pyms_nist_search/x64/nistdl64']
+			# data_files = [('', ['src/pyms_nist_search/x64/nistdl64.dll', 'src/pyms_nist_search/x64/ctNt66_64.dll'])]
+		else:
+			x = 86
+			bit = 32
+			ctree = "ctNt66"
+			# libraries = ['src/pyms_nist_search/x86/nistdl32']
+			# data_files = [('', ['src/pyms_nist_search/x86/nistdl32.dll', 'src/pyms_nist_search/x86/ctNt66.dll'])]
+		libraries = [f'src/pyms_nist_search/x{x}/nistdl{bit}']
+		data_files = [('', [f'src/pyms_nist_search/x{x}/nistdl{bit}.dll', f'src/pyms_nist_search/x{x}/{ctree}.dll'])]
+		
+		extension = Extension(
+				name='pyms_nist_search._core',
+				define_macros=build_macros,
+				libraries=libraries,
+				sources=['src/pyms_nist_search/pyms_nist_search.c'],
+				include_dirs=["src/pyms_nist_search"],
+				language="c",
+				)
+		
+		setup(
+				**common_kwargs,
+				install_requires=[req for req in install_requires if req not in docker_only_reqs],
+				ext_modules=[extension],
+				data_files=data_files
+				)
+	
+	else:
+		# On platforms other than windows, build the minimal C extension that just contains the variables,
+		# as well as the Python files that are required for cross compatibility.
+		
+		min_extension = Extension(
+				name='pyms_nist_search._core',
+				define_macros=build_macros,
+				sources=['src/pyms_nist_search/pyms_nist_search_min.c'],
+				include_dirs=["src/pyms_nist_search"],
+				)
+	
+		setup(
+				**common_kwargs,
+				install_requires=install_requires,
+				ext_modules=[min_extension],
+				)
