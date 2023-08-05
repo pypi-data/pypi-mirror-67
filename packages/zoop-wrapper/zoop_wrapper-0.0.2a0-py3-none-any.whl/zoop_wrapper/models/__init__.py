24d120a24340d966bb7de8ebdea29dc87853d95a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:10 +0000

    Add package zoop-wrapper/0.0.2a0/zoop_wrapper-0.0.2a0-py3-none-any.whl

---
+++ packages/zoop-wrapper/zoop_wrapper-0.0.2a0-py3-none-any.whl/zoop_wrapper/models/__init__.py
@@ -0,0 +1,7 @@
+from .base import Address  # noqa
+from .bank_account import BankAccount  # noqa
+from .buyer import Buyer  # noqa
+from .invoice import Invoice  # noqa
+from .seller import Seller  # noqa
+from .token import Token  # noqa
+from .transaction import Transaction  # noqa
