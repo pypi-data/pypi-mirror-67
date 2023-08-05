95fc37e4174af3875d8b1edd0dd8cfbf3bb7fc25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:34 +0000

    Add package bbbmon/0.1.1/bbbmon-0.1.1.tar.gz

---
+++ packages/bbbmon/bbbmon-0.1.1.tar.gz/bbbmon-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['bbbmon']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['appdirs>=1.4.3,<2.0.0', 'requests>=2.23.0,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['run = bbbmon.bbbmon:main']}
+
+setup_kwargs = {
+    'name': 'bbbmon',
+    'version': '0.1.1',
+    'description': 'A small CLI utility to monitor bbb usage',
+    'long_description': None,
+    'author': 'David Huss',
+    'author_email': 'david.huss@hfbk-hamburg.de',
+    'maintainer': 'David Huss',
+    'maintainer_email': 'david.huss@hfbk-hamburg.de',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
