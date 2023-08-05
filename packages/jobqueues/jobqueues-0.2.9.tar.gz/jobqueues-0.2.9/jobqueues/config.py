36ebacbff9426aa4d3e894145c2c3197137628a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:49 +0000

    Add package jobqueues/0.2.9/jobqueues-0.2.9.tar.gz

---
+++ packages/jobqueues/jobqueues-0.2.9.tar.gz/jobqueues-0.2.9/jobqueues/config.py
@@ -0,0 +1,26 @@
+# (c) 2015-2018 Acellera Ltd http://www.acellera.com
+# All Rights Reserved
+# Distributed under HTMD Software License Agreement
+# No redistribution in whole or part
+#
+import os
+import logging
+
+logger = logging.getLogger(__file__)
+
+_config = {"lsf": None, "slurm": None}
+
+
+def config(lsf=_config["lsf"], slurm=_config["slurm"]):
+    """
+    Function to temporarily change configuration variables.
+
+    Parameters
+    ----------
+    lsf : str
+        Defines a YAML file that can contain default profile configurations for an LsfQueue
+    slurm : str
+        Defines a YAML file that can contain default profile configurations for an SlurmQueue
+    """
+    _config["lsf"] = lsf
+    _config["slurm"] = slurm
