958156d853e61b665bf359021eea222a1de902de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:10 +0000

    Add package click-config-file/0.6.0/click_config_file-0.6.0.tar.gz

---
+++ packages/click-config-file/click_config_file-0.6.0.tar.gz/click_config_file-0.6.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+setup(
+    name='click_config_file',
+    author='Philipp Hack',
+    author_email='philipp.hack@gmail.com',
+    url='http://github.com/phha/click_config_file',
+    version='0.6.0',
+    license='MIT',
+    description='Configuration file support for click applications.',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    py_modules=['click_config_file'],
+    install_requires=['click >= 6.7', 'configobj >= 5.0.6'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'twine'],
+)
