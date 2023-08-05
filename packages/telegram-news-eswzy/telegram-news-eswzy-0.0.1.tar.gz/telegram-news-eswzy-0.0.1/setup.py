5d00972ea70b8a950e2803880ea0ec0ce54fe228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:40 +0000

    Add package telegram-news-eswzy/0.0.1/telegram-news-eswzy-0.0.1.tar.gz

---
+++ packages/telegram-news-eswzy/telegram-news-eswzy-0.0.1.tar.gz/telegram-news-eswzy-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='telegram-news-eswzy',
+    version='0.0.1',
+    packages=['template'],
+    url='https://github.com/ESWZY/telegram-news',
+    author='ESWZY',
+    author_email='',
+    description='For the news.',
+    python_requires='>=3.5'
+)
