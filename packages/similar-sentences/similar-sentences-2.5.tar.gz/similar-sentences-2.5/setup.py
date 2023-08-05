f63655f93b1313c8b85e07fdd7c020bfeaf03983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:22 +0000

    Add package similar-sentences/2.5/similar-sentences-2.5.tar.gz

---
+++ packages/similar-sentences/similar-sentences-2.5.tar.gz/similar-sentences-2.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="similar-sentences",
+    version="2.5",
+    author="Shankar Ganesh Jayaraman",
+    author_email="shangan.23@gmail.com",
+    description="Similar sentence prediction",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shangan23/similar-sentences",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "sentence-transformers",
+        "xlsxwriter"
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
