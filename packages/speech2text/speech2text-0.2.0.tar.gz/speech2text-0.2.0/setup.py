8c5e03cf05f1efcf7ba8fb133256a69b7ddb44ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:13 +0000

    Add package speech2text/0.2.0/speech2text-0.2.0.tar.gz

---
+++ packages/speech2text/speech2text-0.2.0.tar.gz/speech2text-0.2.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='speech2text',
+    version='0.2.0',
+    packages=['speech2text', 'speech2text.engines'],
+    url='https://github.com/JarbasAl/speech2text',
+    install_requires=["requests", "SpeechRecognition==3.8.1"],
+    license='Apache2.0',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    description='Mycroft STT engine wrappers'
+)
