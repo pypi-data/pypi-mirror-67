589fec8ae616ba66dc8aedadbe9903bb90e7f2bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:03 +0000

    Add package mingdongnlp/2.0.0a42/mingdongnlp-2.0.0a42-py3-none-any.whl

---
+++ packages/mingdongnlp/mingdongnlp-2.0.0a42-py3-none-any.whl/mingdongnlp/pretrained/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+# Author: hankcs
+# Date: 2019-12-28 19:10
+from mingdongnlp.pretrained import cws
+from mingdongnlp.pretrained import dep
+from mingdongnlp.pretrained import sdp
+from mingdongnlp.pretrained import glove
+from mingdongnlp.pretrained import pos
+from mingdongnlp.pretrained import rnnlm
+from mingdongnlp.pretrained import word2vec
+from mingdongnlp.pretrained import ner
+from mingdongnlp.pretrained import classifiers
+
+# Will be filled up during runtime
+ALL = {}
