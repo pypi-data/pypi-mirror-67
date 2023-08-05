107893f9010002268da4c4dfdda8f33e29a89778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:54 +0000

    Add package sbol2/1.0b2/sbol2-1.0b2.tar.gz

---
+++ packages/sbol2/sbol2-1.0b2.tar.gz/sbol2-1.0b2/README.md
@@ -0,0 +1,55 @@
+# pySBOL2
+[![Build Status](https://travis-ci.org/SynBioDex/pySBOL2.svg?branch=master)](https://travis-ci.org/SynBioDex/pySBOL2)
+
+**pySBOL2** is a pure Python implementation of the SBOL 2.3.0 standard [Synthetic Biology Open Language (SBOL)](http://www.sbolstandard.org/) - 
+an open standard for the representation of *in silico* biological designs. It draws from [libSBOL](https://github.com/SynBioDex/libSBOL)
+and [pySBOL](https://github.com/SynBioDex/pySBOL).
+
+This native Python library is intended to eventually supplant the original [pySBOL](https://github.com/SynBioDex/pySBOL) based on C++ wrappers.
+
+For developers, please see [CONTRIBUTING.md](CONTRIBUTING.md).
+
+## INSTALLATION
+
+This library is currently under development. Please consider using a
+virtual environment for your installation.
+
+```shell
+# Name the virtual environment
+DIR=myenv
+
+# Create the virtual environment
+python3 -m venv "${DIR}"
+
+# Activate the virtual environment
+source "${DIR}"/bin/activate
+
+# Recommended: upgrade pip
+pip install -U pip
+
+# Install the sbol module
+pip install sbol2
+```
+
+## CODE EXAMPLE
+
+```
+import sbol2
+
+doc = sbol2.Document()
+```
+
+## DOCUMENTATION
+
+This module should be largely compatible with
+[pySBOL](https://github.com/SynBioDex/pySBOL).
+The [pySBOL documentation](https://pysbol2.readthedocs.io/en/latest/index.html)
+should be used until this module has its own documentation.
+
+## ACKNOWLEDGEMENTS
+
+Development of this library has been supported by the DARPA Synergistic Discovery & Design program and BBN Technologies.
+
+<p align="center">
+  <img src="./logo.jpg" height="100" />
+</p>
---
+++ packages/sbol2/sbol2-1.0b2.tar.gz/sbol2-1.0b2/sbol2/__init__.py
@@ -0,0 +1,76 @@
+__version__ = '1.0b2'
+
+# Anything imported here is part of the public API. Limit what gets
+# imported to only those things that are actually needed.
+#
+# We should be using `__all__` for the list of exported items for this
+# module, but the list of items in `constants.py` is extensive. So
+# instead of `__all__`, we will try to be careful about what gets
+# imported into this file. In the absence of __all__, all imported
+# symbols are also exported.
+from .collection import Collection
+from .combinatorialderivation import CombinatorialDerivation
+from .component import Component
+from .component import FunctionalComponent
+from .componentdefinition import ComponentDefinition
+from .config import Config, ConfigOptions
+from .config import getHomespace, hasHomespace, setHomespace
+from .constants import *
+from .dbtl import Design
+from .document import Document
+from .identified import Identified
+from .implementation import Implementation
+from .interaction import Interaction
+from .location import Location, Range, Cut, GenericLocation
+from .module import Module
+from .moduledefinition import ModuleDefinition
+from .object import SBOLObject
+from .participation import Participation
+from .partshop import PartShop
+from .property import IntProperty
+from .property import LiteralProperty
+from .property import TextProperty
+from .provo import Activity
+from .provo import Agent
+from .provo import Association
+from .provo import Plan
+from .provo import Usage
+from .sbolerror import SBOLError, SBOLErrorCode
+from .sequence import Sequence
+from .sequenceannotation import SequenceAnnotation
+from .validation import is_alphanumeric_or_underscore
+from .validation import is_not_alphanumeric_or_underscore
+
+
+def testSBOL():
+    """
+    Function to test pySBOL API.
+    """
+    import sbol2.test as unit_tests
+    unit_tests.runTests()
+
+
+def testRoundTrip():
+    """
+    Function to run test suite for pySBOL
+    """
+    import sbol2.test as unit_tests
+    result = unit_tests.runRoundTripTests()
+    if result.wasSuccessful():
+        exit(0)
+    else:
+        exit(1)
+
+
+def testSBOLHook():
+    """
+    Function to test pySBOL API. Exits 1 or 0 depending on whether
+    all tests pass.
+    :return:
+    """
+    import sbol2.test as unit_tests
+    result = unit_tests.runTests()
+    if result.wasSuccessful():
+        exit(0)
+    else:
+        exit(1)
