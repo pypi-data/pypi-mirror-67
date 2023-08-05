8f0b35453f1ac87a2b2022d3d08301f8dacda662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:53 +0000

    Add package pyjsonmodel/1.0.1/pyjsonmodel-1.0.1.tar.gz

---
+++ packages/pyjsonmodel/pyjsonmodel-1.0.1.tar.gz/pyjsonmodel-1.0.1/setup.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pyjsonmodel",
+    version="1.0.1",
+    author="Sun Yu",
+    author_email="18610863095@163.com",
+    description="json转实体类",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/1102210843/jsonModel",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Environment :: Web Environment",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    install_requires=[
+    ],
+    zip_safe=True,
+    keywords="json",
+)
+
+
---
+++ packages/pyjsonmodel/pyjsonmodel-1.0.1.tar.gz/pyjsonmodel-1.0.1/test/test.py
@@ -0,0 +1,44 @@
+import sys
+sys.path.append(".")
+from jsonModel import loads, dumps
+
+
+class GModel:
+    sss = None
+
+
+class ZModel:
+    aaa = None
+
+
+class TestModel:
+    name = None
+    age = None
+    girlFriends = None
+    zzz = None
+
+    # 这里指定属性类型
+    __doc__ = {
+        "girlFriends": GModel,
+        "zzz": ZModel
+    }
+
+
+if __name__ == '__main__':
+    data = '[{"name":"jack","age":13,"girlFriends":[{"sss":"111"},{"sss":"222"}],"zzz":{"aaa":"aaa"}},{"name":"jone","age":21,"girlFriends":[{"sss":"111"},{"sss":"222"}],"zzz":{"aaa":"aaa"}}]'
+
+    res = loads(data, TestModel)
+    for r in res:
+        print(r.name)
+        print(r.age)
+        print([i.sss for i in r.girlFriends])
+        print(r.zzz.aaa)
+    jsonStr = dumps(res)
+    print(jsonStr)
+
+
+
+
+
+
+
