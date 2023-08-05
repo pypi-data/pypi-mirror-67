a245e5d32b0f44ea75a67877525c82c37c161ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:19 +0000

    Add package isort-black-compat/4.3.22/isort_black_compat-4.3.22.tar.gz

---
+++ packages/isort-black-compat/isort_black_compat-4.3.22.tar.gz/isort_black_compat-4.3.22/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(name='isort_black_compat',
+      version='4.3.22',
+      description='A Python utility / library to sort Python imports written by Timothy Crosley',
+      long_description=readme,
+      author='Sam Shleifer',
+      author_email='sshleifer@gmail.com',
+      url='https://github.com/sshleifer/isort',
+      license="MIT",
+      entry_points={
+        'console_scripts': [
+            'isort = isort.main:main',
+        ],
+        'distutils.commands': ['isort = isort.main:ISortCommand'],
+        'pylama.linter': ['isort = isort.pylama_isort:Linter'],
+      },
+      packages=['isort'],
+      extras_require={
+          'pipfile': ['pipreqs', 'requirementslib'],
+          'pyproject': ['toml'],
+          'requirements': ['pipreqs', 'pip-api'],
+          'xdg_home': ['appdirs>=1.4.0'],
+      },
+      install_requires=[
+          'futures; python_version < "3.2"',
+          'backports.functools_lru_cache; python_version < "3.2"',
+      ],
+      python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+      keywords='Refactor, Python, Python2, Python3, Refactoring, Imports, Sort, Clean',
+      classifiers=['Development Status :: 6 - Mature',
+                   'Intended Audience :: Developers',
+                   'Natural Language :: English',
+                   'Environment :: Console',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: Implementation :: CPython',
+                   'Programming Language :: Python :: Implementation :: PyPy',
+                   'Topic :: Software Development :: Libraries',
+                   'Topic :: Utilities'])
