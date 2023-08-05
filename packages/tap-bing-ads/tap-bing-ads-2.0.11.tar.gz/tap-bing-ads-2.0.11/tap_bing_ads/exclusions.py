2686a5a8d1004e1d88e18bde96497788d594f594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:08 +0000

    Add package tap-bing-ads/2.0.11/tap-bing-ads-2.0.11.tar.gz

---
+++ packages/tap-bing-ads/tap-bing-ads-2.0.11.tar.gz/tap-bing-ads-2.0.11/tap_bing_ads/exclusions.py
@@ -0,0 +1,117 @@
+EXCLUSIONS = {
+    'AccountPerformanceReport': {
+        'Attributes': [
+            'BidMatchType',
+            'DeviceOS',
+            'TopVsOther'
+        ],
+        'ImpressionSharePerformanceStatistics': [
+            'AbsoluteTopImpressionSharePercent',
+            'ClickSharePercent',
+            'ExactMatchImpressionSharePercent',
+            'ImpressionLostToAdRelevancePercent',
+            'ImpressionLostToBidPercent',
+            'ImpressionLostToBudgetPercent',
+            'ImpressionLostToExpectedCtrPercent',
+            'ImpressionLostToRankPercent',
+            'ImpressionSharePercent',
+            'TopImpressionRatePercent',
+            'TopImpressionShareLostToBudgetPercent',
+            'TopImpressionShareLostToRankPercent',
+            'TopImpressionSharePercent'
+        ]
+    },
+    'AdGroupPerformanceReport': {
+        'Attributes': [
+            'BidMatchType',
+            'DeviceOS',
+            'TopVsOther',
+            'Goal',
+            'GoalType'
+        ],
+        'ImpressionSharePerformanceStatistics': [
+            'AbsoluteTopImpressionSharePercent',
+            'ClickSharePercent',
+            'ExactMatchImpressionSharePercent',
+            'ImpressionLostToAdRelevancePercent',
+            'ImpressionLostToBidPercent',
+            'ImpressionLostToBudgetPercent',
+            'ImpressionLostToExpectedCtrPercent',
+            'ImpressionLostToRankPercent',
+            'ImpressionSharePercent',
+            'TopImpressionRatePercent',
+            'TopImpressionShareLostToBudgetPercent',
+            'TopImpressionShareLostToRankPercent',
+            'TopImpressionSharePercent'
+        ]
+    },
+    'CampaignPerformanceReport': {
+        'Attributes': [
+            'BidMatchType',
+            'DeviceOS',
+            'TopVsOther',
+            'BudgetAssociationStatus',
+            'BudgetName',
+            'BudgetStatus'
+        ],
+        'ImpressionSharePerformanceStatistics': [
+            'AbsoluteTopImpressionSharePercent',
+            'ClickSharePercent',
+            'ExactMatchImpressionSharePercent',
+            'ImpressionLostToAdRelevancePercent',
+            'ImpressionLostToBidPercent',
+            'ImpressionLostToBudgetPercent',
+            'ImpressionLostToExpectedCtrPercent',
+            'ImpressionLostToRankPercent',
+            'ImpressionSharePercent',
+            'TopImpressionRatePercent',
+            'TopImpressionShareLostToBudgetPercent',
+            'TopImpressionShareLostToRankPercent',
+            'TopImpressionSharePercent'
+        ]
+    },
+    'ProductDimensionPerformanceReport': {
+        'Attributes': [
+            'AdDistribution',
+            'AdId',
+            'AdStatus',
+            'ClickType',
+            'Language',
+            'LocalStoreCode',
+            'Network',
+            'TopVsOther'
+        ],
+        'ImpressionSharePerformanceStatistics': [
+            'AbsoluteTopImpressionSharePercent',
+            'BenchmarkBid',
+            'BenchmarkCtr',
+            'ClickSharePercent',
+            'ImpressionLostToBudgetPercent',
+            'ImpressionLostToRankPercent',
+            'ImpressionSharePercent'
+        ]
+    },
+    'ProductPartitionPerformanceReport': {
+        'Attributes': [
+            'AdDistribution',
+            'AdId',
+            'AdStatus',
+            'BidMatchType',
+            'ClickType',
+            'DeliveredMatchType',
+            'Language',
+            'LocalStoreCode',
+            'Network',
+            'TopVsOther'
+        ],
+        'ImpressionSharePerformanceStatistics': [
+            'AbsoluteTopImpressionSharePercent',
+            'BenchmarkBid',
+            'BenchmarkCtr',
+            'ClickSharePercent',
+            'ImpressionLostToBudgetPercent',
+            'ImpressionLostToRankPercent',
+            'ImpressionSharePercent'
+        ]
+    }
+}
