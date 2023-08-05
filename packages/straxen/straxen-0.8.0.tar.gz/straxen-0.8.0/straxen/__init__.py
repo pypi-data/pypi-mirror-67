e553db095c802c86e47de37588ef675f6bf648fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:48 +0000

    Add package straxen/0.8.0/straxen-0.8.0.tar.gz

---
+++ packages/straxen/straxen-0.8.0.tar.gz/straxen-0.8.0/straxen/__init__.py
@@ -0,0 +1,16 @@
+__version__ = '0.8.0'
+
+from .common import *
+from .itp_map import *
+from .rundb import *
+from .matplotlib_utils import *
+from .mini_analysis import *
+
+from . import plugins
+from .plugins import *
+
+from . import analyses
+
+# Do not make all contexts directly available under straxen.
+# Otherwise we have straxen.demo() etc.
+from . import contexts
