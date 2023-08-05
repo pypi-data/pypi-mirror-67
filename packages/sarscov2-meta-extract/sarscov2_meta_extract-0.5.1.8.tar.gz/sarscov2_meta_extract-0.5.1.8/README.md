2042c2a12e08ce50d83f4c6049ba177936963aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:33 +0000

    Add package sarscov2-meta-extract/0.5.1.8/sarscov2_meta_extract-0.5.1.8.tar.gz

---
+++ packages/sarscov2-meta-extract/sarscov2_meta_extract-0.5.1.8.tar.gz/sarscov2_meta_extract-0.5.1.8/README.md
@@ -0,0 +1,3 @@
+# sarscov2_meta_extract
+
+Tool for extracting and formating metadata for Galaxy SARS-CoV-2 selection analysis pipeline.
---
+++ packages/sarscov2-meta-extract/sarscov2_meta_extract-0.5.1.8.tar.gz/sarscov2_meta_extract-0.5.1.8/sarscov2metaextract/cli.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import argparse
+from sarscov2metaextract import convertor
+
+def main():
+	parser = argparse.ArgumentParser(description='Metadata extractor for SARS-CoV-2 selection analysis pipeline in Galaxy')
+	parser.add_argument('-a', '--alignment', dest='alignment', action='store', help='Mulitple sequence alignment file', required=True)
+	parser.add_argument('-m', '--metadata', dest='metadata', action='store', help='Metadata source (Use "ncbi" if using NCBI SARS-CoV-2 data, otherwise supply tabular file of the correct format)', required=True)
+	args = parser.parse_args()
+
+	convertor(args.alignment, args.metadata)
+
+if __name__ == '__main__':
+    exit(main())
