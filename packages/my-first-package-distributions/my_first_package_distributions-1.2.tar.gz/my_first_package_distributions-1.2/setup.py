38725966cbdcc0087cc330c209b2095aa8e013a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:13 +0000

    Add package my-first-package-distributions/1.2/my_first_package_distributions-1.2.tar.gz

---
+++ packages/my-first-package-distributions/my_first_package_distributions-1.2.tar.gz/my_first_package_distributions-1.2/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+
+setup(name='my_first_package_distributions', version = '1.2', description = 'Gaussian and Binomial dsitribution', 
+          package = ['distributions'], author = 'Andrew Paster', author_email = 'a.nicolau16@gmail.com', zip_safe = False)
