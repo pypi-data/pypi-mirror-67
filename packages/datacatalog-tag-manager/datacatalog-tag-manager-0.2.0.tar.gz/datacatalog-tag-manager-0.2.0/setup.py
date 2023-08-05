69323c5433da4eb6ffb2cf858581cbeba049596e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:14 +0000

    Add package datacatalog-tag-manager/0.2.0/datacatalog-tag-manager-0.2.0.tar.gz

---
+++ packages/datacatalog-tag-manager/datacatalog-tag-manager-0.2.0.tar.gz/datacatalog-tag-manager-0.2.0/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+setuptools.setup(
+    name='datacatalog-tag-manager',
+    version='0.2.0',
+    author='Ricardo Mendes',
+    author_email='ricardolsmendes@gmail.com',
+    description='A package to manage Google Cloud Data Catalog tags,'
+    ' loading metadata from external sources',
+    platforms='Posix; MacOS X; Windows',
+    packages=setuptools.find_packages(where='./src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'datacatalog-tag-manager = datacatalog_tag_manager:main',
+        ],
+    },
+    include_package_data=True,
+    install_requires=(
+        'google-cloud-datacatalog',
+        'pandas',
+    ),
+    setup_requires=('pytest-runner', ),
+    tests_require=('pytest-cov', ),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
