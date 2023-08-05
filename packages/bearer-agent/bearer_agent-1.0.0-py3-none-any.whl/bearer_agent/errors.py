0606628c936d1102dcc9550ea7a0b4977d356e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:22 +0000

    Add package bearer-agent/1.0.0/bearer_agent-1.0.0-py3-none-any.whl

---
+++ packages/bearer-agent/bearer_agent-1.0.0-py3-none-any.whl/bearer_agent/errors.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -
+#
+# This file is part of bearer-agent released under the Apache License 2.
+# See the NOTICE for more information.
+
+
+class ConfigError(Exception):
+    """ Exception raised on config error """
