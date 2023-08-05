b748fe06bd77e92b117cefc23ef2f72a8265c508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:15 +0000

    Add package mypy-boto3-kinesis-video-archived-media/1.12.47.0/mypy-boto3-kinesis-video-archived-media-1.12.47.0.tar.gz

---
+++ packages/mypy-boto3-kinesis-video-archived-media/mypy-boto3-kinesis-video-archived-media-1.12.47.0.tar.gz/mypy-boto3-kinesis-video-archived-media-1.12.47.0/mypy_boto3_kinesis_video_archived_media/type_defs.py
@@ -0,0 +1,111 @@
+"""
+Main interface for kinesis-video-archived-media service type definitions.
+
+Usage::
+
+    from mypy_boto3.kinesis_video_archived_media.type_defs import DASHTimestampRangeTypeDef
+
+    data: DASHTimestampRangeTypeDef = {...}
+"""
+from datetime import datetime
+import sys
+from typing import IO, List, Union
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = (
+    "DASHTimestampRangeTypeDef",
+    "DASHFragmentSelectorTypeDef",
+    "TimestampRangeTypeDef",
+    "FragmentSelectorTypeDef",
+    "GetDASHStreamingSessionURLOutputTypeDef",
+    "GetHLSStreamingSessionURLOutputTypeDef",
+    "GetMediaForFragmentListOutputTypeDef",
+    "HLSTimestampRangeTypeDef",
+    "HLSFragmentSelectorTypeDef",
+    "FragmentTypeDef",
+    "ListFragmentsOutputTypeDef",
+    "PaginatorConfigTypeDef",
+)
+
+DASHTimestampRangeTypeDef = TypedDict(
+    "DASHTimestampRangeTypeDef", {"StartTimestamp": datetime, "EndTimestamp": datetime}, total=False
+)
+
+DASHFragmentSelectorTypeDef = TypedDict(
+    "DASHFragmentSelectorTypeDef",
+    {
+        "FragmentSelectorType": Literal["PRODUCER_TIMESTAMP", "SERVER_TIMESTAMP"],
+        "TimestampRange": DASHTimestampRangeTypeDef,
+    },
+    total=False,
+)
+
+TimestampRangeTypeDef = TypedDict(
+    "TimestampRangeTypeDef", {"StartTimestamp": datetime, "EndTimestamp": datetime}
+)
+
+FragmentSelectorTypeDef = TypedDict(
+    "FragmentSelectorTypeDef",
+    {
+        "FragmentSelectorType": Literal["PRODUCER_TIMESTAMP", "SERVER_TIMESTAMP"],
+        "TimestampRange": TimestampRangeTypeDef,
+    },
+)
+
+GetDASHStreamingSessionURLOutputTypeDef = TypedDict(
+    "GetDASHStreamingSessionURLOutputTypeDef", {"DASHStreamingSessionURL": str}, total=False
+)
+
+GetHLSStreamingSessionURLOutputTypeDef = TypedDict(
+    "GetHLSStreamingSessionURLOutputTypeDef", {"HLSStreamingSessionURL": str}, total=False
+)
+
+GetMediaForFragmentListOutputTypeDef = TypedDict(
+    "GetMediaForFragmentListOutputTypeDef",
+    {"ContentType": str, "Payload": Union[bytes, IO]},
+    total=False,
+)
+
+HLSTimestampRangeTypeDef = TypedDict(
+    "HLSTimestampRangeTypeDef", {"StartTimestamp": datetime, "EndTimestamp": datetime}, total=False
+)
+
+HLSFragmentSelectorTypeDef = TypedDict(
+    "HLSFragmentSelectorTypeDef",
+    {
+        "FragmentSelectorType": Literal["PRODUCER_TIMESTAMP", "SERVER_TIMESTAMP"],
+        "TimestampRange": HLSTimestampRangeTypeDef,
+    },
+    total=False,
+)
+
+FragmentTypeDef = TypedDict(
+    "FragmentTypeDef",
+    {
+        "FragmentNumber": str,
+        "FragmentSizeInBytes": int,
+        "ProducerTimestamp": datetime,
+        "ServerTimestamp": datetime,
+        "FragmentLengthInMilliseconds": int,
+    },
+    total=False,
+)
+
+ListFragmentsOutputTypeDef = TypedDict(
+    "ListFragmentsOutputTypeDef",
+    {"Fragments": List[FragmentTypeDef], "NextToken": str},
+    total=False,
+)
+
+PaginatorConfigTypeDef = TypedDict(
+    "PaginatorConfigTypeDef", {"MaxItems": int, "PageSize": int, "StartingToken": str}, total=False
+)
