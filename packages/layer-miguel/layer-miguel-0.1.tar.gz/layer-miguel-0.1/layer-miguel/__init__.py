a7e44d46951f3bb0fd2283439170464a4641d5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:13 +0000

    Add package layer-miguel/0.1/layer-miguel-0.1.tar.gz

---
+++ packages/layer-miguel/layer-miguel-0.1.tar.gz/layer-miguel-0.1/layer-miguel/__init__.py
@@ -0,0 +1,1 @@
+from fun1 import cust_fun
---
+++ packages/layer-miguel/layer-miguel-0.1.tar.gz/layer-miguel-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'layer-miguel',         # How you named your package folder (MyLib)
+  packages = ['layer-miguel'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Package de prueba',   # Give a short description about your library
+  author = 'Miguel Garcia Burgos',                   # Type in your name
+  author_email = 'mgarcibu@everis.com',      # Type in your E-Mail
+  url = 'https://ic.correos.es/git/cdp/minerva/ri/prueba.git',   # Provide either the link to your github or to your website
+  download_url = 'https://ic.correos.es/git/cdp/minerva/ri/prueba/-/archive/v_01/prueba-v_01.tar.gz',    # I explain this later on
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+)
