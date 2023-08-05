a43b3311b7a53e7270cc0f7cacb983e7c5de3e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:57 +0000

    Add package gbd-stat/0.1/gbd_stat-0.1.tar.gz

---
+++ packages/gbd-stat/gbd_stat-0.1.tar.gz/gbd_stat-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gbd_stat',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gbd_stat'],
+      author = 'Arjun Maini',
+      author_email = 'arjunmaini007@gmail.com',
+      zip_safe=False)
