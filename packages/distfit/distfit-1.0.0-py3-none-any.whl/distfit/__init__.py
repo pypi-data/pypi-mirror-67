6dbcbb6e575f29b97b9c98df1a3aeb53c78f5352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:33 +0000

    Add package distfit/1.0.0/distfit-1.0.0-py3-none-any.whl

---
+++ packages/distfit/distfit-1.0.0-py3-none-any.whl/distfit/__init__.py
@@ -0,0 +1,40 @@
+from distfit.distfit import (
+    distfit,
+)
+
+__author__ = 'Erdogan Tasksen'
+__email__ = 'erdogant@gmail.com'
+__version__ = '1.0.0'
+
+
+# module level doc-string
+__doc__ = """
+distfit
+=====================================================================
+
+Description
+-----------
+Probability density function fitting and hypothesis testing.
+Computes best fit to the input emperical distribution for 89 theoretical
+distributions using the Sum of Squared errors (SSE) estimates.
+
+Example
+-------
+>>> from distfit import distfit
+>>>
+>>> X = np.random.normal(0, 2, 1000)
+>>> y = [-8,-6,0,1,2,3,4,5,6]
+>>>
+>>> dist = distfit()
+>>> dist.fit_transform(X)
+>>> dist.plot()
+>>>
+>>> # Make prediction
+>>> dist.predict(y)
+>>> dist.plot()
+
+References
+----------
+    https://github.com/erdogant/distfit
+
+"""
