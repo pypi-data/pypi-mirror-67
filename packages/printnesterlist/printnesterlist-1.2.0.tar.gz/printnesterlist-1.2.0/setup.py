0e474afd32648768fabf40ee85e8ae98f33975c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:09 +0000

    Add package printnesterlist/1.2.0/printnesterlist-1.2.0.tar.gz

---
+++ packages/printnesterlist/printnesterlist-1.2.0.tar.gz/printnesterlist-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name = 'printnesterlist',
+	version = '1.2.0',
+	py_modules = ['printnesterlist'],
+	author = 'lando',
+	author_email = 'landochan3@gmail.com',
+	url = '',
+	description = 'A simple printer of nested lists', 
+)
