000727ef3218dd0bd9cbf281ae8707796d541069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:22 +0000

    Add package validated-dc/0.2.2/validated-dc-0.2.2.tar.gz

---
+++ packages/validated-dc/validated-dc-0.2.2.tar.gz/validated-dc-0.2.2/tests/test_basic_validation.py
@@ -0,0 +1,62 @@
+from dataclasses import dataclass
+
+from validated_dc import BasicValidation
+
+
+class СustomСlass:
+
+    foo = 'foo'
+
+    def bar(self):
+        pass
+
+
+@dataclass
+class Foo(BasicValidation):
+    i: int
+    s: str
+    l: list
+    # ... You can use all other standard python classes.
+    cc: СustomСlass
+
+
+correct_input = {
+    'i': 1,
+    's': '2',
+    'l': [3, '4'],
+    'cc': СustomСlass()
+}
+
+
+def test_basic_validation_valid():
+
+    instance = Foo(**correct_input)
+
+    assert instance.get_errors() is None
+
+
+def test_basic_validation_not_valid():
+
+    nocorrect_input = correct_input
+    nocorrect_input['s'] = 2
+
+    instance = Foo(**nocorrect_input)
+
+    assert instance.get_errors()
+
+
+def test__post_init__():
+    """
+        Тест запуска валидации после создания экземпляра
+        (в методе self.__post_init__())
+    """
+    @dataclass
+    class FakeBasicValidation(BasicValidation):
+        """
+            Подменим метод старта валидаци
+        """
+        def _run_validation(self):
+            self.validation_run = True
+
+    instance = FakeBasicValidation()
+    assert instance.validation_run
