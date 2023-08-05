e994dfaa4ca7019db33dbf288a3ace12c7dfc36c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:06 +0000

    Add package gapipy/2.26.3/gapipy-2.26.3.tar.gz

---
+++ packages/gapipy/gapipy-2.26.3.tar.gz/gapipy-2.26.3/gapipy/resources/booking/agency.py
@@ -0,0 +1,65 @@
+# Python 2 and 3
+from __future__ import unicode_literals
+
+from gapipy.models import Address, AgencyDocument
+from gapipy.models.base import BaseModel
+from gapipy.resources.base import Resource
+from gapipy.resources.booking_company import BookingCompany
+from gapipy.resources.tour import Promotion
+
+from .agency_chain import AgencyChain
+
+
+class AgencyEmail(BaseModel):
+    _as_is_fields = [
+        'type',
+        'address',
+    ]
+
+class AgencyChainConfigurations(BaseModel):
+    _as_is_fields = ['flags']
+    _resource_fields = [
+        ('agency_chain', AgencyChain),
+        ('booking_company', BookingCompany),
+    ]
+
+
+class Agency(Resource):
+    _resource_name = 'agencies'
+    _is_listable = False
+    _is_parent_resource = True
+
+    _as_is_fields = [
+        'id',
+        'href',
+        'name',
+        'booking_currencies',
+        'latitude',
+        'longitude',
+        'transactional_email',
+        'communication_preferences',
+        'override_agency_secondary',
+        'passenger_notifications',
+        'agent_notifications',
+        'preferred_display_name',
+    ]
+    _date_time_fields_local = [
+        'date_created',
+    ]
+    _model_fields = [
+        ('address', Address),
+    ]
+    _resource_fields = [
+        ('agency_chain', AgencyChain),
+    ]
+    _model_collection_fields = [
+        ('agency_chains', 'AgencyChain'),
+        ('booking_companies', BookingCompany),
+        ('documents', AgencyDocument),
+        ('emails', AgencyEmail),
+        ('agency_chain_configurations', AgencyChainConfigurations),
+    ]
+    _resource_collection_fields = [
+        ('agents', 'Agent'),
+        ('promotions', Promotion),
+    ]
