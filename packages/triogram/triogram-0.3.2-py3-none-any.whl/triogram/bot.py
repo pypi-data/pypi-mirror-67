3b015b18fff1c5b74b84732e7081bf90d8093a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:29 +0000

    Add package triogram/0.3.2/triogram-0.3.2-py3-none-any.whl

---
+++ packages/triogram/triogram-0.3.2-py3-none-any.whl/triogram/bot.py
@@ -0,0 +1,46 @@
+import os
+
+import attr
+
+from .api import Api
+from .dispatcher import Dispatcher
+from .poller import Poller
+from .http import http_client
+from .config import TOKEN_ENV_VAR
+
+
+def make_bot(token=None):
+    if token is None:
+        token = os.environ[TOKEN_ENV_VAR]
+
+    http = http_client(token=token)
+    api = Api(http=http)
+    poller = Poller(api=api)
+    dispatcher = Dispatcher()
+
+    return Bot(http=http, api=api, poller=poller, dispatcher=dispatcher)
+
+
+@attr.s
+class Bot:
+
+    _http = attr.ib()
+    api = attr.ib()
+    _poller = attr.ib()
+    _dispatcher = attr.ib()
+
+    async def run(self):
+        async with self._http:
+            while True:
+                for update in await self._poller.get_updates():
+                    await self._dispatcher.pub(update)
+
+    __call__ = run
+
+    @property
+    def sub(self):
+        return self._dispatcher.sub
+
+    async def wait(self, *args, **kwargs):
+        async with self.sub(*args, **kwargs) as updates:
+            return await updates.receive()
