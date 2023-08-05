ac41fea4d921183bf6c4fa95726159e282690983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:40 +0000

    Add package fleet-rec/0.0.3/fleet_rec-0.0.3-py2.py3-none-any.whl

---
+++ packages/fleet-rec/fleet_rec-0.0.3-py2.py3-none-any.whl/fleetrec/core/model.py
@@ -0,0 +1,43 @@
+import abc
+from fleetrec.core.utils import envs
+
+class Model(object):
+    """R
+    """
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, config):
+        """R
+        """
+        self._cost = None
+        self._metrics = {}
+        self._data_var = []
+        self._data_loader = None
+        self._fetch_interval = 20
+        self._namespace = "train.model"
+        self._platform = envs.get_platform()
+
+    def get_inputs(self):
+        return self._data_var
+
+    def get_cost_op(self):
+        """R
+        """
+        return self._cost
+
+    def get_metrics(self):
+        """R
+        """
+        return self._metrics
+
+    def get_fetch_period(self):
+        return self._fetch_interval
+
+    @abc.abstractmethod
+    def train_net(self):
+        """R
+        """
+        pass
+
+    def infer_net(self):
+        pass
