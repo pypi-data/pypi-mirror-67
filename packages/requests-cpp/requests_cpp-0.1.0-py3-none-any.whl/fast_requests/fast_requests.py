2f6f8931bf9a786d03b4179cc70b0dab549a5067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:27 +0000

    Add package requests-cpp/0.1.0/requests_cpp-0.1.0-py3-none-any.whl

---
+++ packages/requests-cpp/requests_cpp-0.1.0-py3-none-any.whl/fast_requests/fast_requests.py
@@ -0,0 +1,10 @@
+from . import fast_requetst_cpp
+
+
+# timeOut 单位为毫秒
+def get(urls, headers={}, nThread=5, isDebug=False, timeOut=3000, verifySsl=True):
+    return fast_requetst_cpp.run("get", urls, headers, nThread, isDebug, timeOut, verifySsl)
+
+
+def sessionGet(urls, headers={}, nThread=5, isDebug=False, timeOut=3000, verifySsl=True):
+    return fast_requetst_cpp.run("session", urls, headers, nThread, isDebug, timeOut, verifySsl)
