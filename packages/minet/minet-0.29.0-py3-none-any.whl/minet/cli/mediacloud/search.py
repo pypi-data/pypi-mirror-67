e4ed5bac572aba788ca3d5abb45059de574983db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:36 +0000

    Add package minet/0.29.0/minet-0.29.0-py3-none-any.whl

---
+++ packages/minet/minet-0.29.0-py3-none-any.whl/minet/cli/mediacloud/search.py
@@ -0,0 +1,44 @@
+# =============================================================================
+# Minet Mediacloud Search CLI Action
+# =============================================================================
+#
+# Logic of the `mc search` action.
+#
+import csv
+from tqdm import tqdm
+
+from minet.mediacloud import MediacloudClient
+from minet.mediacloud.constants import MEDIACLOUD_STORIES_CSV_HEADER
+
+
+def mediacloud_search_action(namespace, output_file):
+    writer = csv.writer(output_file)
+    writer.writerow(MEDIACLOUD_STORIES_CSV_HEADER)
+
+    client = MediacloudClient(namespace.token)
+
+    kwargs = {
+        'collections': namespace.collections
+    }
+
+    count = client.count(
+        namespace.query,
+        **kwargs
+    )
+
+    loading_bar = tqdm(
+        desc='Searching stories',
+        dynamic_ncols=True,
+        total=count,
+        unit=' stories'
+    )
+
+    iterator = client.search(
+        namespace.query,
+        format='csv_row',
+        **kwargs
+    )
+
+    for story in iterator:
+        writer.writerow(story)
+        loading_bar.update()
