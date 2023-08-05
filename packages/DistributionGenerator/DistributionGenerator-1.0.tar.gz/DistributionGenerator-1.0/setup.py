533ffdc738b2803fc98bde2ab711f67e4add2656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:09 +0000

    Add package DistributionGenerator/1.0/DistributionGenerator-1.0.tar.gz

---
+++ packages/DistributionGenerator/DistributionGenerator-1.0.tar.gz/DistributionGenerator-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='DistributionGenerator',
+      version='1.0',
+      description='Distribution generators',
+      packages=['DistributionGenerator'],
+      zip_safe=False)
