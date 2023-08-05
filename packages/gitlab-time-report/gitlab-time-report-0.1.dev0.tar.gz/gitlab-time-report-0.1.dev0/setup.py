b8f884cd0bc0d3b65703a5f27a7b0e9622976dd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:56 +0000

    Add package gitlab-time-report/0.1.dev0/gitlab-time-report-0.1.dev0.tar.gz

---
+++ packages/gitlab-time-report/gitlab-time-report-0.1.dev0.tar.gz/gitlab-time-report-0.1.dev0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['gitlab_time_report']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'gitlab-time-report',
+    'version': '0.1.dev0',
+    'description': '',
+    'long_description': None,
+    'author': 'Johannes Wildermuth',
+    'author_email': 'johannes.wildermuth@hsr.ch',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
