cb5b1c6ca153ae080b602c4c780eee4f3b56a41b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:42 +0000

    Add package go2scope/0.0.1/go2scope-0.0.1.tar.gz

---
+++ packages/go2scope/go2scope-0.0.1.tar.gz/go2scope-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="go2scope",
+    version="0.0.1",
+    author="Nenad Amodaj",
+    author_email="nenad@go2scope.com",
+    description="go2scope support functions for python: data I/O",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    url="https://github.com/go2scope/micro-manager-plus/tree/master/python/go2scope",
+    install_requires=[
+        'numpy==1.18.2', 'opencv-python==4.2.0.34',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
