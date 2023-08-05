3b63e8a0c3c4207d20e726759038ee4375943605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:00 +0000

    Add package meboot/0.1/meboot-0.1.tar.gz

---
+++ packages/meboot/meboot-0.1.tar.gz/meboot-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(	name='meboot',
+      	version='0.1',
+      	packages=['meboot'],
+	install_requires=[	'numpy>=1.18.3',
+				'scipy>=1.4.1'
+				],
+	author='Christian Bongiorno',
+	author_email='pvofeta@gmail.com',
+	license='GPL',
+	zip_safe=False
+
+      )
+
