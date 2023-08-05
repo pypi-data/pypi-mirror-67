4ba331ffeb152d1a96bc16e1e907d94ae9b414b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:38 +0000

    Add package libutils/0.0.1/libutils-0.0.1.tar.gz

---
+++ packages/libutils/libutils-0.0.1.tar.gz/libutils-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+	name="libutils",
+	version="0.0.1",
+	author="aztecrabbit",
+	author_email="ars.xda@gmail.com",
+	description="Libutils",
+	long_description="...",
+	long_description_content_type="text/markdown",
+	url="https://github.com/aztecrabbit/python-libutils",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+	entry_points={
+	},
+)
