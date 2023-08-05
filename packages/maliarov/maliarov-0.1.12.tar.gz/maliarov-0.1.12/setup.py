9e543aac0817cc90aad32cceccc78300f5be847d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:14 +0000

    Add package maliarov/0.1.12/maliarov-0.1.12.tar.gz

---
+++ packages/maliarov/maliarov-0.1.12.tar.gz/maliarov-0.1.12/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="maliarov",
+    version="0.1.12",
+    description="Routine automation",
+
+    packages=['maliarov'],
+    include_package_data = True,
+    package_data = {
+        '': ['*.txt'],
+        'maliarov': ['data/*.*'],
+    },
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires = [
+        "selenium ~= 3.1",
+        "datetime ~= 4.3",
+        "beautifulsoup4 ~= 4.8.2",
+        "urllib3 ~= 1.25.8"
+    ],
+    author="Anatolii Maliarov",
+    author_email="tly.mov@gmail.com",
+    url="https://github.com/tlmy/webscraping",
+)
