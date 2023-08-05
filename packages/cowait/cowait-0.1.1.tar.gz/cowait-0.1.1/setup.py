13b9aada9d7afc32a4397cf1f419feb60835f4be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:58 +0000

    Add package cowait/0.1.1/cowait-0.1.1.tar.gz

---
+++ packages/cowait/cowait-0.1.1.tar.gz/cowait-0.1.1/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cowait",
+    version="0.1.1",
+    author="Backtick Technologies",
+    author_email="hello@backtick.se",
+    description="Cowait Core",
+
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/backtick-se/cowait",
+
+    packages=setuptools.find_packages(),
+    scripts=['bin/cowait'],
+
+    classifiers=[],
+
+    python_requires='>=3.6',
+)
+
+"""
+install_requires=[
+    'click',
+    'docker',
+    'requests',
+    'pyyaml',
+    'dask',
+    'distributed',
+    'kubernetes',
+],
+"""
