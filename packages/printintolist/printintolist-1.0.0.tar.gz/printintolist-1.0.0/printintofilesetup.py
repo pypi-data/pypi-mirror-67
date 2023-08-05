ecb10903811818baaee3a4b2896c2c0e8dd9a51a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:01 +0000

    Add package printintolist/1.0.0/printintolist-1.0.0.tar.gz

---
+++ packages/printintolist/printintolist-1.0.0.tar.gz/printintolist-1.0.0/printintofilesetup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name = 'printintolist',
+        version = '1.0.0',
+        py_modulus = ['printintolist'],
+        author = 'lando',
+        email = 'landochan3@gmail.com',
+        url = '',
+        description = 'print_lol function to file'
+)
