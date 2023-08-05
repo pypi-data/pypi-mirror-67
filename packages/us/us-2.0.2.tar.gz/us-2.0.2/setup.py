22ad8b87803dc71d73a6e7c0b54d7214edeceab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:38 +0000

    Add package us/2.0.2/us-2.0.2.tar.gz

---
+++ packages/us/us-2.0.2.tar.gz/us-2.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+long_description = open("README.rst").read()
+
+setup(
+    name="us",
+    version="2.0.2",
+    author="Jeremy Carbaugh",
+    author_email="jeremy@jcarbaugh.com",
+    url="https://github.com/unitedstates/python-us",
+    description="US state meta information and other fun stuff",
+    long_description=long_description,
+    license="BSD",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["jellyfish==0.6.1"],
+    entry_points={"console_scripts": ["states = us.cli.states:main"]},
+    platforms=["any"],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
