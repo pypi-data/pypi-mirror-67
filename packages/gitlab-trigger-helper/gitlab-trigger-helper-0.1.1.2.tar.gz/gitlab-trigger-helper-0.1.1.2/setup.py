bfdbb495b91e02e744952a70775f6266ae2f0f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:33 +0000

    Add package gitlab-trigger-helper/0.1.1.2/gitlab-trigger-helper-0.1.1.2.tar.gz

---
+++ packages/gitlab-trigger-helper/gitlab-trigger-helper-0.1.1.2.tar.gz/gitlab-trigger-helper-0.1.1.2/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['gitlab_trigger_helper']
+entry_points = \
+{'console_scripts': ['gitlab-trigger = gilab_trigger_helper:__main__']}
+
+setup_kwargs = {
+    'name': 'gitlab-trigger-helper',
+    'version': '0.1.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Fabio Lima',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'entry_points': entry_points,
+    'python_requires': '>=2.7,<3.0',
+}
+
+
+setup(**setup_kwargs)
