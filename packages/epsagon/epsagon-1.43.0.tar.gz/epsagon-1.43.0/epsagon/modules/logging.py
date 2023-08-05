6648c1c8354bce557a522fda6ffd9677e8fa1781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:55 +0000

    Add package epsagon/1.43.0/epsagon-1.43.0.tar.gz

---
+++ packages/epsagon/epsagon-1.43.0.tar.gz/epsagon-1.43.0/epsagon/modules/logging.py
@@ -0,0 +1,33 @@
+"""
+logging patcher module.
+"""
+
+from __future__ import absolute_import
+import os
+import wrapt
+from ..trace import trace_factory
+
+
+def _wrapper(wrapped, _instance, args, kwargs):
+    """
+    Wrapper for logging module.
+    :param wrapped: wrapt's wrapped
+    :param _instance: wrapt's instance, unused.
+    :param args: wrapt's args
+    :param kwargs: wrapt's kwargs
+    :return: None
+    """
+    if (os.getenv(
+            'EPSAGON_DISABLE_LOGGING_ERRORS'
+    ) or '').upper() == 'TRUE':
+        trace_factory.set_error(*args)
+    return wrapped(*args, **kwargs)
+
+
+def patch():
+    """
+    Patch module.
+    :return: None
+    """
+    wrapt.wrap_function_wrapper('logging', 'exception', _wrapper)
+    wrapt.wrap_function_wrapper('logging', 'Logger.exception', _wrapper)
