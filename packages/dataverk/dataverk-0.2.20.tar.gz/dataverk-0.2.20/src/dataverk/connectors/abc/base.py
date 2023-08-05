5ad7797654748bf93910c347f940bd906b800e58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:25 +0000

    Add package dataverk/0.2.20/dataverk-0.2.20.tar.gz

---
+++ packages/dataverk/dataverk-0.2.20.tar.gz/dataverk-0.2.20/src/dataverk/connectors/abc/base.py
@@ -0,0 +1,19 @@
+from abc import ABC
+
+from dataverk.mixins.logger_mixin import LoggerMixin
+from dataverk.mixins.auth_mixin import AuthMixin, AuthError
+from prometheus_client import Summary
+
+
+# Create a metric to track time spent and requests made.
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+
+
+class BaseConnector(ABC, AuthMixin, LoggerMixin):
+    """Common connection methods
+    
+    """
+    def __init__(self):
+        self.user = self.get_user()
+        if self._is_authorized() is not True:
+            raise AuthError("auth", "not authorized")
---
+++ packages/dataverk/dataverk-0.2.20.tar.gz/dataverk-0.2.20/src/dataverk/connectors/abc/db_base.py
@@ -0,0 +1,22 @@
+from abc import abstractmethod
+from dataverk.connectors import BaseConnector
+
+
+class DBBaseConnector(BaseConnector):
+
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def get_pandas_df(self, query, verbose_output):
+        """Get Pandas dataframe
+
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def persist_pandas_df(self, table, schema=None, df=None, chunksize=10000, if_exists='replace'):
+        """ Write Pandas dataframe
+
+        """
+        raise NotImplementedError()
