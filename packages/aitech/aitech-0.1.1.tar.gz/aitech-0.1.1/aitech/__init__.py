2f08093c9cb130119d08b54385630d28dd23d8cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:57 +0000

    Add package aitech/0.1.1/aitech-0.1.1.tar.gz

---
+++ packages/aitech/aitech-0.1.1.tar.gz/aitech-0.1.1/aitech/__init__.py
@@ -0,0 +1,5 @@
+from aitech.infrared import IR as IR
+from aitech.masspsec import MassSpec as MS
+from aitech.tg_dsc import TgDsc as STA
+import aitech.utils as utils
+from aitech.utils import topic_directories, method_directories
