4989805fe00b0fab452bb1b4d8a13a7bfb98b62c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:04 +0000

    Add package pyatv/0.6.1/pyatv-0.6.1.tar.gz

---
+++ packages/pyatv/pyatv-0.6.1.tar.gz/pyatv-0.6.1/pyatv/dmap/pairing.py
@@ -0,0 +1,166 @@
+"""Module used for pairing pyatv with a device."""
+
+import socket
+import hashlib
+import logging
+import random
+import ipaddress
+from io import StringIO
+
+from aiohttp import web
+from aiozeroconf import ServiceInfo, Zeroconf
+import netifaces
+
+from pyatv.const import Protocol
+from pyatv.dmap import tags
+from pyatv.interface import PairingHandler
+
+_LOGGER = logging.getLogger(__name__)
+
+
+# Maybe replace with pyatv.net.get_local_address_reaching?
+def _get_private_ip_addresses():
+    for iface in netifaces.interfaces():
+        addresses = netifaces.ifaddresses(iface)
+        if netifaces.AF_INET not in addresses:
+            continue
+
+        for addr in addresses[netifaces.AF_INET]:
+            ipaddr = ipaddress.ip_address(addr["addr"])
+            if ipaddr.is_private and not ipaddr.is_loopback:
+                yield ipaddr
+
+
+def _generate_random_guid():
+    return hex(random.getrandbits(64)).upper()
+
+
+class DmapPairingHandler(
+    PairingHandler
+):  # pylint: disable=too-many-instance-attributes  # noqa
+    """Handle the pairing process.
+
+    This class will publish a bonjour service and configure a webserver
+    that responds to pairing requests.
+    """
+
+    def __init__(self, config, session, loop, **kwargs):
+        """Initialize a new instance."""
+        super().__init__(session, config.get_service(Protocol.DMAP))
+        self._loop = loop
+        self._zeroconf = kwargs.get(
+            "zeroconf", Zeroconf(loop, address_family=[netifaces.AF_INET])
+        )
+        self._name = kwargs.get("name", "pyatv")
+        self._web_server = None
+        self._server = None
+        self._pin_code = None
+        self._has_paired = False
+        self._pairing_guid = (
+            kwargs.get("pairing_guid", None) or _generate_random_guid()
+        )[2:].upper()
+
+    async def close(self):
+        """Call to free allocated resources after pairing."""
+        await self._zeroconf.close()
+        if self._web_server is not None:
+            await self._web_server.shutdown()
+            self._server.close()
+
+        if self._server is not None:
+            await self._server.wait_closed()
+
+        await super().close()
+
+    @property
+    def has_paired(self):
+        """If a successful pairing has been performed.
+
+        The value will be reset when stop() is called.
+        """
+        return self._has_paired
+
+    async def begin(self):
+        """Start the pairing server and publish service."""
+        self._web_server = web.Server(self.handle_request, loop=self._loop)
+        self._server = await self._loop.create_server(self._web_server, "0.0.0.0")
+
+        # Get the allocated (random port) and include it in zeroconf service
+        allocated_port = self._server.sockets[0].getsockname()[1]
+        _LOGGER.debug("Started pairing web server at port %d", allocated_port)
+
+        for ipaddr in _get_private_ip_addresses():
+            await self._publish_service(ipaddr, allocated_port)
+
+    async def finish(self):
+        """Stop pairing server and unpublish service."""
+        if self._has_paired:
+            _LOGGER.debug("Saving updated credentials")
+            self.service.credentials = "0x" + self._pairing_guid
+
+    def pin(self, pin):
+        """Pin code used for pairing."""
+        self._pin_code = pin
+        _LOGGER.debug("DMAP PIN changed to %s", self._pin_code)
+
+    @property
+    def device_provides_pin(self):
+        """Return True if remote device presents PIN code, else False."""
+        return False
+
+    async def _publish_service(self, address, port):
+        props = {
+            b"DvNm": self._name,
+            b"RemV": b"10000",
+            b"DvTy": b"iPod",
+            b"RemN": b"Remote",
+            b"txtvers": b"1",
+            b"Pair": self._pairing_guid,
+        }
+
+        service = ServiceInfo(
+            "_touch-remote._tcp.local.",
+            "{0:040d}._touch-remote._tcp.local.".format(int(address)),
+            address=socket.inet_aton(str(address)),
+            port=port,
+            weight=0,
+            priority=0,
+            properties=props,
+        )
+        await self._zeroconf.register_service(service)
+
+        _LOGGER.debug("Published zeroconf service: %s", service)
+
+    async def handle_request(self, request):
+        """Respond to request if PIN is correct."""
+        service_name = request.rel_url.query["servicename"]
+        received_code = request.rel_url.query["pairingcode"].lower()
+        _LOGGER.info(
+            "Got pairing request from %s with code %s", service_name, received_code
+        )
+
+        if self._verify_pin(received_code):
+            cmpg = tags.uint64_tag("cmpg", int(self._pairing_guid, 16))
+            cmnm = tags.string_tag("cmnm", self._name)
+            cmty = tags.string_tag("cmty", "iPhone")
+            response = tags.container_tag("cmpa", cmpg + cmnm + cmty)
+            self._has_paired = True
+            return web.Response(body=response)
+
+        # Code did not match, generate an error
+        return web.Response(status=500)
+
+    def _verify_pin(self, received_code):
+        # If no particular pin code is specified, allow any pin
+        if self._pin_code is None:
+            return True
+
+        merged = StringIO()
+        merged.write(self._pairing_guid)
+        for char in str(self._pin_code).zfill(4):
+            merged.write(char)
+            merged.write("\x00")
+
+        expected_code = hashlib.md5(merged.getvalue().encode()).hexdigest()
+        _LOGGER.debug("Got code %s, expects %s", received_code, expected_code)
+        return received_code == expected_code
---
+++ packages/pyatv/pyatv-0.6.1.tar.gz/pyatv-0.6.1/tests/utils.py
@@ -0,0 +1,103 @@
+"""Various helper methods used by test cases."""
+
+import time
+import asyncio
+import inspect
+
+from datetime import datetime
+from importlib import import_module
+from asyncio import sleep as real_sleep
+
+from aiohttp import ClientSession
+
+
+def stub_sleep():
+    """Stub asyncio.sleep to not make tests slow."""
+    # This is a special "hack" to schedule the sleep at the end of the
+    # event queue in order to give other possibility to run.
+    async def fake_sleep(time=None, loop=None):
+        async def dummy():
+            pass
+
+        await asyncio.ensure_future(dummy())
+
+    asyncio.sleep = fake_sleep
+
+
+async def simple_get(url):
+    """Perform a GET-request to a specified URL."""
+    session = ClientSession()
+    response = await session.request("GET", url)
+    if response.status < 200 or response.status >= 300:
+        response.close()
+        await session.close()
+        return None, response.status
+
+    data = await response.content.read()
+    response.close()
+    await session.close()
+    return data, response.status
+
+
+async def until(pred, timeout=5, **kwargs):
+    """Wait until a predicate is fulfilled.
+
+    Simple method of "waiting" for asynchronous code to finish.
+    """
+    deadline = time.time() + timeout
+    while True:
+        value = pred(**kwargs)
+        if inspect.iscoroutinefunction(pred):
+            value = await value
+
+        if isinstance(value, tuple):
+            cond, retvalue = value
+            if cond:
+                return retvalue
+        elif value:
+            return None
+
+        if timeout is not None:
+            if deadline - time.time() <= 0:
+                raise asyncio.TimeoutError()
+
+        await real_sleep(0.01)
+
+
+def faketime(module_name, *times):
+    """Monkey patch datetime.now to return fake time."""
+
+    class FakeDatetime:
+        def __init__(self, times):
+            self.times = times
+
+        def __enter__(self):
+            module = import_module(module_name)
+            setattr(module.datetime, "datetime", self)
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            module = import_module(module_name)
+            setattr(module.datetime, "datetime", datetime)
+
+        def now(self, tz=None):
+            """Replace times from now to fake values."""
+            if not self.time:
+                return datetime.now(tz=tz)
+
+            next_time = self.times[0]
+            if len(self.times) > 1:
+                self.times = self.times[1:]
+
+            time = datetime.fromtimestamp(next_time)
+            if tz:
+                time = time.replace(tzinfo=tz)
+            return time
+
+        def __call__(self, *args, **kwargs):
+            return datetime(*args, **kwargs)
+
+        def __getattr__(self, attr):
+            """Redirect non-stubbed functions to original module."""
+            return getattr(datetime, attr)
+
+    return FakeDatetime(list(times))
