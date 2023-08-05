3c97bc6428b804acfb7c25338b580923c7706996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:48 +0000

    Add package skidtools/0.0.1a2/skidtools-0.0.1a2.tar.gz

---
+++ packages/skidtools/skidtools-0.0.1a2.tar.gz/skidtools-0.0.1a2/skidtools/config_template.py
@@ -0,0 +1,11 @@
+# Default config for skidtool
+# See documumention for details docs.clast.dev/skidtools/home/config/
+# https://github.com/Abd0s/skidtools/
+
+# General
+version: str = "0.0.1"
+help_text: str = "A simple help accesed through --help"
+
+# slogging
+log_to_file: bool = False
+debug: bool = False
