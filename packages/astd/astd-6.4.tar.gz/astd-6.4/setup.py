6ce10489df0f9c39413ea2a1e5c60c587f0df610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:51 +0000

    Add package astd/6.4/astd-6.4.tar.gz

---
+++ packages/astd/astd-6.4.tar.gz/astd-6.4/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'astd',
+  packages = ['astd'],
+  version = '6.4',
+  license='MIT',
+  description = 'TYPE YOUR DESCRIPTION HERE',
+  author = 'YOUR NAME',
+  author_email = 'your.email@domain.com',
+  url = 'https://gitlab.com/fez1/async_state_transition_decorator',
+  download_url = 'https://gitlab.com/fez1/async_state_transition_decorator/archive/v_01.tar.gz',
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],
+  setup_requires=['wheel'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
