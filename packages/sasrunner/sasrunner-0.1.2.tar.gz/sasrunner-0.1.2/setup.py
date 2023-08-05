f6e3ec82059e7efec45c591b621798bbb638e7ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:01 +0000

    Add package sasrunner/0.1.2/sasrunner-0.1.2.tar.gz

---
+++ packages/sasrunner/sasrunner-0.1.2.tar.gz/sasrunner-0.1.2/setup.py
@@ -0,0 +1,15 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distuils.core import setup
+
+setup(
+    name="sasrunner",
+    version='0.1.2',
+    description="SAS runner",
+    author="Joe Dougherty",
+    author_email="josepd@wharton.upenn.edu",
+    url="https://wrds-web.wharton.upenn.edu/wrds/",
+    packages=['sasrunner'],
+    keywords = ['SAS', 'etl'],
+)
