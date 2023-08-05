45cae9466bc87ad3ff3cbaef3fd6378ff9fe1baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:56 +0000

    Add package tkinterx/0.0.7/tkinterx-0.0.7.tar.gz

---
+++ packages/tkinterx/tkinterx-0.0.7.tar.gz/tkinterx-0.0.7/tkinterx/graph/canvas.py
@@ -0,0 +1,261 @@
+'''Some of the actions related to the graph.
+'''
+from tkinter import Canvas, StringVar
+
+
+class CanvasMeta(Canvas):
+    '''Graphic elements are composed of line(segment), rectangle, ellipse, and arc.
+    '''
+
+    def __init__(self, master=None, cnf={}, **kw):
+        '''The base class of all graphics frames.
+
+        :param master: a widget of tkinter or tkinter.ttk.
+        '''
+        super().__init__(master, cnf, **kw)
+
+    def layout(self, row=0, column=0):
+        '''Layout graphic elements with Grid'''
+        # Layout canvas space
+        self.grid(row=row, column=column, sticky='nwes')
+
+    def draw_graph(self, graph_type, direction, color='blue', width=1, tags=None, **kwargs):
+        '''Draw basic graphic elements.
+
+        :param direction: Specifies the orientation of the graphic element. 
+            Union[int, float] -> (x_0,y_0,x_,y_1), (x_0, y_0) refers to the starting point of 
+            the reference brush (i.e., the left mouse button is pressed), and (x_1, y_1) refers to 
+            the end position of the reference brush (i.e., release the left mouse button).
+            Multipoint sequences are supported for 'line' and 'polygon',
+             for example ((x_0, y_0), (x_1, y_1), (x_2, y_2)).
+        :param graph_type: Types of graphic elements.
+            (str) 'rectangle', 'oval', 'line', 'arc'(That is, segment), 'polygon'.
+            Note that 'line' can no longer pass in the parameter 'fill', and 
+            the remaining graph_type cannot pass in the parameter 'outline'.
+        :param color: The color of the graphic element.
+        :param width: The width of the graphic element.(That is, center fill)
+        :param tags: The tags of the graphic element. 
+            It cannot be a pure number (such as 1 or '1' or '1 2 3'), it can be a list, a tuple, 
+            or a string separated by a space(is converted to String tupers separated by a blank space). 
+            The collection or dictionary is converted to a string.
+            Example:
+                ['line', 'graph'], ('test', 'g'), 'line',
+                ' line kind '(The blanks at both ends are automatically removed), and so on.
+        :param style: Style of the arc in {'arc', 'chord', or 'pieslice'}.
+
+        :return: Unique identifier solely for graphic elements.
+        '''
+        if tags is None:
+            if graph_type in ('rectangle', 'oval', 'line', 'arc'):
+                tags = f"{color} {graph_type}"
+            else:
+                tags = f'{color} graph'
+
+        com_kw = {'width': width, 'tags': tags}
+        kw = {**com_kw, 'outline': color}
+        line_kw = {**com_kw, 'fill': color}
+        if graph_type == 'line':
+            kwargs.update(line_kw)
+        else:
+            kwargs.update(kw)
+        func = eval(f"self.create_{graph_type}")
+        graph_id = func(*direction, **kwargs)
+        return graph_id
+
+
+class GraphMeta(dict):
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+
+    def __set__(self, instance, value):
+        print('===> set', instance, value)
+        self[instance] = value
+
+    def __get__(self, instance, owner):
+        return self[instance]
+
+
+class SimpleGraph(CanvasMeta):
+    color = GraphMeta()
+    shape = GraphMeta()
+
+    def __init__(self, master, shape, color, cnf={}, **kw):
+        '''The base class of all graphics frames.
+
+        :param master: a widget of tkinter or tkinter.ttk.
+        '''
+        super().__init__(master, cnf, **kw)
+        self.color = color
+        self.shape = shape
+
+    def draw(self, direction, width=1, tags=None, **kw):
+        return self.draw_graph(self.shape, direction, color=self.color, width=width, tags=tags, **kw)
+
+    def add_row(self, direction, num, stride=10, width=1, tags=None, **kw):
+        x0, y0, x1, y1 = direction
+        stride = x1 - x0 + stride
+        for k in range(num):
+            direction = [x0+stride*k, y0, x1+stride*k, y1]
+            self.draw(direction, width=width, tags=tags, **kw)
+
+    def add_column(self, direction, num, stride=5, width=1, tags=None, **kw):
+        x0, y0, x1, y1 = direction
+        stride = y1 - y0 + stride
+        for k in range(num):
+            direction = [x0, y0+stride*k, x1, y1+stride*k]
+            self.draw(direction, width=width, tags=tags, **kw)
+
+
+class Drawing(CanvasMeta):
+    '''Create graphic elements (graph) including rectangular boxes (which can be square points), 
+        ovals (circular points), and segments.
+        Press the left mouse button to start painting, release the left 
+            mouse button for the end of the painting.
+    Example
+    ===============
+    from tkinterx.graph.canvas_design import SelectorFrame
+    from tkinterx.graph.canvas import Drawing
+    from tkinter import Tk
+    root = Tk()
+    selector_frame = SelectorFrame(root, 'rectangle', 'yellow')
+    self = Drawing(root, selector_frame, width=800, height=800, background='lightgray')
+    self.layout(0, 0)
+    selector_frame.layout(0, 1)
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+    root.mainloop()
+    '''
+
+    def __init__(self, master, selector_frame, after_time=160, cnf={}, **kw):
+        '''Click the left mouse button to start painting, release
+            the left mouse button to complete the painting.
+
+        :param selector: The graphics selector, which is an instance of Selector.
+        '''
+        super().__init__(master, cnf, **kw)
+        self.selector_frame = selector_frame
+        self.after_time = after_time
+        self.x = self.y = 0
+        self.reset()
+        self._draw_bind()
+
+    def reset(self):
+        self.first_x, self.first_y = 0, 0
+        self.last_x, self.last_y = 0, 0
+        self.on = False # Used to record whether a painting is being made
+
+    def update_xy(self, event):
+        '''Press the left mouse button to record the coordinates of the left mouse button'''
+        self.x = self.canvasx(event.x)
+        self.y = self.canvasy(event.y)
+
+    def update_bbox(self, event):
+        x0, y0 = self.x, self.y  # The upper-left coordinates of the graph
+        self.update_xy(event)
+        x1, y1 = self.x, self.y  # Lower-right coordinates of the graph
+        bbox = x0, y0, x1, y1
+        return bbox
+
+    def get_bbox(self, event):
+        self.update_xy(event)
+        return self.first_x, self.first_y, self.x, self.y
+
+    def strat_draw(self, event):
+        self.update_xy(event)
+        self.first_x, self.first_y = self.x, self.y
+
+    def mouse_draw(self, event):
+        '''Release the left mouse button to finish painting.'''
+        self.configure(cursor="arrow")
+        self.after(self.after_time)
+        bbox = self.get_bbox(event)
+        self.create_graph(bbox)
+
+    def mouse_move(self, event):
+        self.on = True
+        self.mouse_draw(event)
+
+    def mouse_release(self, event):
+        self.delete('temp')
+        self.on = False
+        self.mouse_draw(event)
+        
+    def create_graph(self, bbox):
+        '''Create a graphic.
+
+        :param bbox: (x0,y0,x1,y1)
+        '''
+        color, shape = self.selector_frame._selector.color, self.selector_frame._selector.shape
+        x0, y0, x1, y1 = bbox
+        cond1 = x0 == x1 and y0 == y1 and 'point' not in shape
+        cond2 = 'point' in shape and (x0 != x1 or y0 != y1)
+        if self.on:
+            tags = (color, shape, 'temp')
+        else:
+            tags = (color, shape)
+        kw = {
+            'direction': bbox,
+            'color': color,
+            'tags': tags
+        }
+        if cond1 or cond2:
+            return
+        else:
+            return self.draw_graph(shape.split('_')[0], **kw)
+
+    def tune_graph(self, event):
+        '''Release the left mouse button to finish painting.'''
+        self.configure(cursor="arrow")
+        x0, y0, x1, y1 = self.get_bbox(event)
+        graph_id = self.find_closest(x0, y0)
+        bbox = self.bbox(graph_id)
+        self.coords(graph_id, *bbox[:2], x1, y1)
+
+    def get_xy(self, event):
+        self.configure(cursor="target")
+        self.update_xy(event)
+
+    def _draw_bind(self):
+        self.bind("<1>", self.strat_draw)
+        self.bind("<B1-Motion>",  self.mouse_move)
+        self.bind("<ButtonRelease-1>", self.mouse_release)
+        self.bind("<B3-Motion>", self.get_xy)
+        self.bind("<3>", self.get_xy)
+        self.bind("<ButtonRelease-3>", self.tune_graph)
+
+    def layout(self, row=0, column=0):
+        self.grid(row=row, column=column, sticky='nwes')
+
+
+class TrajectoryDrawing(Drawing):
+    '''Draw based on the mouse's trajectory.
+
+    Click the left mouse button to start painting, move the 
+        left mouse button 'after_time' after the completion of painting.
+
+    Example
+    ===================
+    root = Tk()
+    selector = SelectorFrame(root)
+    meta = TrajectoryDrawing(root, selector, after_time=370, background='lightgray')
+    # Makes the master widget change as the canvas size
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+    meta.layout(0, 0)
+    selector.layout(0, 1)
+    root.mainloop()
+    '''
+
+    def __init__(self, master,  selector, after_time=200, cnf={}, **kw):
+        super().__init__(master, selector, cnf, **kw)
+        self.after_time = after_time
+        self.bind("<1>", self.update_xy)
+        self.bind("<ButtonRelease-1>", self.update_xy)
+        self.bind("<Button1-Motion>", self.mouse_draw)
+
+    def mouse_draw(self, event):
+        '''Release the left mouse button to finish painting.'''
+        self.configure(cursor="arrow")
+        self.after(self.after_time)
+        bbox = self.get_bbox(event)
+        return self.create_graph(bbox)
---
+++ packages/tkinterx/tkinterx-0.0.7.tar.gz/tkinterx-0.0.7/tkinterx/window.py
@@ -0,0 +1,122 @@
+from tkinter import ttk, Tk, StringVar
+
+from .graph.canvas_design import SelectorFrame
+from .graph.canvas import Drawing, CanvasMeta
+from .utils import save_bunch, load_bunch, mkdir
+
+
+class DrawingWindow(Tk):
+    def __init__(self, screenName=None, baseName=None, className='Tk', useTk=1, sync=0, use=None):
+        super().__init__(screenName, baseName, className, useTk, sync, use)
+        self.bunch = {}
+        self._selected_tags = set()
+        self.selector_frame = SelectorFrame(self, 'rectangle', 'blue')
+        self.drawing = Drawing(self, self.selector_frame,
+                               width=800, height=600, background='lightgray')
+        self.create_notebook()
+        self.tip_var = StringVar(self)
+        self.tip_label = ttk.Label(self, textvariable=self.tip_var,
+                            foreground='blue', background='yellow')
+        self.tip_var.set("Start your creation!")
+        self.bind('<Motion>', self.update_info)
+        self.bind_move()
+
+    def update_info(self, *args):
+        if not self.drawing.on:
+            self.drawing.update_xy(*args)
+        xy = self.drawing.x, self.drawing.y
+        self.tip_var.set(xy)
+
+    def bind_move(self):
+        self.bind('<Up>', lambda event: self.move_graph(event, 0, -1))
+        self.bind('<Down>', lambda event: self.move_graph(event, 0, 1))
+        self.bind('<Left>', lambda event: self.move_graph(event, -1, 0))
+        self.bind('<Right>', lambda event: self.move_graph(event, 1, 0))
+        self.bind('<F1>', self.clear_graph)
+        self.bind('<F2>', self.fill_normal)
+        self.bind('<Delete>', self.delete_graph)
+
+    def find_closest(self):
+        xy = self.drawing.x, self.drawing.y
+        graph_id = self.drawing.find_closest(*xy)
+        return graph_id
+
+    def delete_graph(self, *args):
+        graph_id = self.find_closest()
+        self.drawing.delete(graph_id)
+
+    def clear_graph(self, *args):
+        self.drawing.delete('all')
+
+    def move_graph(self, event, x, y):
+        graph_id = self.find_closest()
+        self.drawing.move(graph_id, x, y)
+        
+    def create_notebook(self):
+        self.notebook = ttk.Notebook(
+            self.selector_frame, width=200, height=200, padding=(5, 5, 5, 5))
+        # first page, which would get widgets gridded into it
+        self.normal = ttk.Frame(self.notebook, width=200,
+                                height=200, padding=(5, 5, 5, 5))
+        self.save_normal_button = ttk.Button(
+            self.normal, text='Save', command=self.save_normal)
+        self.load_normal_button = ttk.Button(
+            self.normal, text='Load', command=self.load_normal)
+        self.annotation = ttk.Frame(
+            self.notebook, width=200, height=200, padding=(5, 5, 5, 5))
+        self.save_annotation_button = ttk.Button(self.annotation, text='Save')
+        self.load_annotation_button = ttk.Button(self.annotation, text='Load')
+        self.next_annotation_button = ttk.Button(self.annotation, text='Next')
+        self.prev_annotation_button = ttk.Button(self.annotation, text='Prev')
+        self.notebook.add(self.normal, text='Normal')
+        self.notebook.add(self.annotation, text='Annotation')
+
+    def all_graph(self):
+        cats = {}
+        for graph_id in self.drawing.find_withtag('all'):
+            tags = self.drawing.gettags(graph_id)
+            bbox = self.drawing.bbox(graph_id)
+            cats[graph_id] = {'tags': tags, 'bbox': bbox}
+        return cats
+
+    def save_normal(self):
+        all_graph = self.all_graph()
+        mkdir('data')
+        save_bunch(all_graph, path='data/normal.json')
+
+    def bunch2params(self, bunch):
+        params = {}
+        for graph_id, cats in bunch.items():
+            tags = cats['tags']
+            color, shape = tags
+            graph_type = shape.split('_')[0]
+            bbox = cats['bbox']
+            params[graph_id] = {'tags': tags, 'color': color,
+                                'graph_type': graph_type, 'direction': bbox}
+        return params
+
+    def load_normal(self):
+        self.bunch = load_bunch('data/normal.json')
+        params = self.bunch2params(self.bunch)
+        self.clear_graph()
+        for param in params.values():
+            self.drawing.draw_graph(**param)
+
+    def fill_normal(self, *args):
+        graph_id = self.find_closest()
+        color = self.drawing.selector_frame._selector.color
+        self.drawing.itemconfigure(graph_id, fill=color)
+
+    def layout(self, row=0):
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.drawing.layout(row=row, column=0)
+        self.selector_frame.layout(row=row, column=1)
+        self.tip_label.grid(row=row+1, sticky='we')
+        self.notebook.grid(row=row+2, column=0)
+        self.save_normal_button.grid(row=0, column=0, padx=2, pady=2)
+        self.load_normal_button.grid(row=0, column=1, padx=2, pady=2)
+        self.save_annotation_button.grid(row=0, column=0, padx=2, pady=2)
+        self.load_annotation_button.grid(row=0, column=1, padx=2, pady=2)
+        self.prev_annotation_button.grid(row=1, column=0, padx=2, pady=2)
+        self.next_annotation_button.grid(row=1, column=1, padx=2, pady=2)
