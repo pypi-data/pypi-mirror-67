3c0243f388999fc0343b3284da4df59ade429fe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:15 +0000

    Add package igePAL/0.0.3/igePAL-0.0.3.tar.gz

---
+++ packages/igePAL/igePAL-0.0.3.tar.gz/igePAL-0.0.3/win32/PALImpl.cpp
@@ -0,0 +1,23 @@
+#include "PAL.h"
+
+void PALImpl::Init()
+{
+}
+
+void PALImpl::Release()
+{
+}
+
+bool PALImpl::openAppSettings()
+{
+    return true;
+}
+
+void PALImpl::saveImageToGallery(const char *name)
+{
+}
+
+bool PALImpl::checkPermission(int permission)
+{
+	return true;
+}
