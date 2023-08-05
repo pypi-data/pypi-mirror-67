eca66816c1da706bc2053ee1af267ba155d41080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:52 +0000

    Add package distribution-s/1.4/distribution_s-1.4.tar.gz

---
+++ packages/distribution-s/distribution_s-1.4.tar.gz/distribution_s-1.4/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distribution_s',
+      version='1.4',
+      description='Gaussian distributions',
+      packages=['distribution_s'],
+      zip_safe=False)
