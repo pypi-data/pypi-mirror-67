976f16637d54d69643a8e5d556d385ea0a344838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:45 +0000

    Add package ezdnac/0.0.27/ezdnac-0.0.27.tar.gz

---
+++ packages/ezdnac/ezdnac-0.0.27.tar.gz/ezdnac-0.0.27/ezdnac/__init__.py
@@ -0,0 +1,12 @@
+from ezdnac.apic import *
+from ezdnac.device import *
+import warnings
+
+warnings.filterwarnings('ignore', message='Unverified HTTPS request')
+
+
+#####################################################
+## Written by Johan Lahti, CCIE60702                #
+## https://github.com/johan-lahti                   #
+## shiproute.net                                    #
+#####################################################
