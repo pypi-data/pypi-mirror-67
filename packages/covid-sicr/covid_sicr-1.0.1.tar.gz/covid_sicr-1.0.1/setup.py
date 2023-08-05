f3241d0ef4a4bdfe2e965c8751ee235218fae03f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:43 +0000

    Add package covid-sicr/1.0.1/covid_sicr-1.0.1.tar.gz

---
+++ packages/covid-sicr/covid_sicr-1.0.1.tar.gz/covid_sicr-1.0.1/setup.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+try:
+    from pip.req import parse_requirements
+    from pip.download import PipSession
+except ImportError:
+    from pip._internal.req import parse_requirements
+    try:
+        from pip._internal.download import PipSession
+    except ImportError:
+        from pip._internal.network.session import PipSession
+
+from setuptools import setup, find_packages
+
+
+def read_requirements():
+    '''parses requirements from requirements.txt'''
+    reqs_path = Path(__file__).parent / 'requirements.txt'
+    install_reqs = parse_requirements(str(reqs_path), session=PipSession())
+    reqs = [str(ir.req) for ir in install_reqs]
+    return reqs
+
+
+setup(
+    name='covid_sicr',
+    version='1.0.1',
+    author='NIDDK',
+    packages=find_packages(),
+    license='MIT',
+    description='Global estimation of unobserved COVID-19 infection',
+    install_requires=read_requirements(),
+    )
