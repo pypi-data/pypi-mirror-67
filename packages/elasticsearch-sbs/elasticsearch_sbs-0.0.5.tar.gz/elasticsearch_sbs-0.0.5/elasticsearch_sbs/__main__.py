b898f0452c5f3f7e539c57b2f9f611748d8ac0fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:45 +0000

    Add package elasticsearch-sbs/0.0.5/elasticsearch_sbs-0.0.5.tar.gz

---
+++ packages/elasticsearch-sbs/elasticsearch_sbs-0.0.5.tar.gz/elasticsearch_sbs-0.0.5/elasticsearch_sbs/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 04:10:19 2020
+
+@author: Sara Ben Shabbat
+"""
+
+
+from elasticsearch_sbs import part_one
+from elasticsearch_sbs import part_two
+
