1f0b2f8b94070d15806b73239a7c1ce90a1abec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:43 +0000

    Add package moco-wrapper/0.6.3/moco_wrapper-0.6.3-py2.py3-none-any.whl

---
+++ packages/moco-wrapper/moco_wrapper-0.6.3-py2.py3-none-any.whl/moco_wrapper/models/objector_models/project.py
@@ -0,0 +1,31 @@
+from moco_wrapper.models import objector_models as obj
+
+class Project(object):
+    def __init__(
+        self,
+        **kwargs
+    ):
+        nk = kwargs
+
+        if "customer" in kwargs.keys() and kwargs["customer"] is not None:
+            c = obj.Company(**kwargs["customer"])
+            nk["customer"] = c
+
+        if "leader" in kwargs.keys() and kwargs["leader"] is not None:
+            u = obj.User(**kwargs["leader"])
+            nk["leader"] = u
+
+        if "contracts" in kwargs.keys() and kwargs["contracts"] is not None:
+            items = []
+            for c in kwargs["contracts"]:
+                items.append(obj.ProjectContract(**c))
+            nk["contracts"] = items
+
+        if "tasks" in kwargs.keys() and kwargs["tasks"] is not None:
+            items = []
+            for t in kwargs["tasks"]:
+                items.append(obj.ProjectTask(**t))
+            nk["tasks"] = items
+
+        self.__dict__.update(nk)
+        
