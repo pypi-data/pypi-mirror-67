5fa04aa8f3df6aac39dea7a65a5a8546f61768ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:09 +0000

    Add package django-kubernetes-manager/0.4.5/django_kubernetes_manager-0.4.5-py3-none-any.whl

---
+++ packages/django-kubernetes-manager/django_kubernetes_manager-0.4.5-py3-none-any.whl/kubernetes_manager/serializers/__init__.py
@@ -0,0 +1,13 @@
+from .base import (
+    KubernetesConfigMapSerializer,
+    KubernetesContainerSerializer,
+    KubernetesDeploymentSerializer,
+    KubernetesIngressSerializer,
+    KubernetesJobSerializer,
+    KubernetesNamespaceSerializer,
+    KubernetesPodTemplateSerializer,
+    KubernetesServiceSerializer,
+    KubernetesVolumeMountSerializer,
+    KubernetesVolumeSerializer,
+    TargetClusterSerializer,
+)
---
+++ packages/django-kubernetes-manager/django_kubernetes_manager-0.4.5-py3-none-any.whl/kubernetes_manager/views/__init__.py
@@ -0,0 +1,13 @@
+from .model_views import (
+    KubernetesConfigMapViewSet,
+    KubernetesContainerViewSet,
+    KubernetesDeploymentViewSet,
+    KubernetesIngressViewSet,
+    KubernetesJobViewSet,
+    KubernetesNamespaceViewSet,
+    KubernetesPodTemplateViewSet,
+    KubernetesServiceViewSet,
+    KubernetesVolumeMountViewSet,
+    KubernetesVolumeViewSet,
+    TargetClusterViewSet,
+)
