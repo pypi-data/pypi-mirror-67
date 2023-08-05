ecefd3b30496536804a5d9655e8ee4f163c72736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:37 +0000

    Add package biotite/0.21.0/biotite-0.21.0-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/biotite/biotite-0.21.0-cp37-cp37m-manylinux1_x86_64.whl/biotite/structure/error.py
@@ -0,0 +1,18 @@
+# This source code is part of the Biotite package and is distributed
+# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
+# information..
+
+"""
+This module contains all possible errors of the `structure` subpackage.
+"""
+
+__name__ = "biotite.structure"
+__author__ = "Patrick Kunzmann"
+__all__ = ["BadStructureError"]
+
+
+class BadStructureError(Exception):
+    """
+    Indicates that a structure is not suitable for a certain operation.
+    """
+    pass
