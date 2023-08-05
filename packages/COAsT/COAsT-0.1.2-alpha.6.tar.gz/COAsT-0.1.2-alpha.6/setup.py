8d24924a3a4a56f0a334cd1c196a6a526987ad24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:42 +0000

    Add package COAsT/0.1.2-alpha.6/COAsT-0.1.2-alpha.6.tar.gz

---
+++ packages/COAsT/COAsT-0.1.2-alpha.6.tar.gz/COAsT-0.1.2-alpha.6/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+setup(
+    name="COAsT",
+    version="0.1.2-alpha.6",
+    description="This is the Coastal Ocean Assessment Tool",
+    url="https://www.bodc.ac.uk",
+    download_url = 'https://github.com/British-Oceanographic-Data-Centre/COAsT/archive/0.1.2-alpha.6.tar.gz',
+    author="British Oceanographic Data Centre (BODC)",
+    author_email="bodcsoft@bodc.ac.uk",
+    license="Put something here",  # TODO,
+
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Hydrology",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords=["NEMO", "shallow water", "ocean assessment"],
+    project_urls={"documentation":"https://british-oceanographic-data-centre.github.io/COAsT/"},
+    install_requires=[
+        'dask[complete]',
+        'xarray',
+        'matplotlib',
+        'netCDF4',
+    ],
+    python_requires=">=3",
+    packages=["COAsT"],
+    include_package_data=True
+)
