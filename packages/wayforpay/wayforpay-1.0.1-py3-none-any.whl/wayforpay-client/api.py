f3990efba421a5c6954a0f104afe7958793ff025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:55 +0000

    Add package wayforpay/1.0.1/wayforpay-1.0.1-py3-none-any.whl

---
+++ packages/wayforpay/wayforpay-1.0.1-py3-none-any.whl/wayforpay-client/api.py
@@ -0,0 +1,6 @@
+# noinspection PyMethodMayBeStatic
+class Api:
+    def __init__(self, merchant_account, merchant_key, merchant_domain):
+        self.merchant_account = merchant_account
+        self.merchant_key = merchant_key
+        self.merchant_key = merchant_domain
