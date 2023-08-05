66313d9dae976b09db4dec3b0701af8b6afd0ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:01 +0000

    Add package cdsdashboards/0.0.5/cdsdashboards-0.0.5.tar.gz

---
+++ packages/cdsdashboards/cdsdashboards-0.0.5.tar.gz/cdsdashboards-0.0.5/setup.py
@@ -0,0 +1,52 @@
+import os
+pjoin = os.path.join
+
+from setuptools import setup
+
+here = os.path.dirname(__file__)
+
+with open(pjoin(here, 'requirements.txt')) as f:
+    requirements = [
+        l.strip() for l in f.readlines()
+        if not l.strip().startswith('#')
+    ]
+
+# Data files e.g. templates and static js
+
+here = os.path.abspath(os.path.dirname(__file__))
+share_cdsdashboards = pjoin(here, 'share', 'cdsdashboards')
+
+def get_data_files():
+    """Get data files in share/cdsdashboards"""
+
+    data_files = []
+    ntrim = len(here + os.path.sep)
+
+    for (d, dir, filenames) in os.walk(share_cdsdashboards):
+        data_files.append((d[ntrim:], [pjoin(d, f)[ntrim:] for f in filenames]))
+    return data_files
+
+setup(
+    name='cdsdashboards',
+    packages=['cdsdashboards'],
+    version='0.0.5',
+    python_requires='>=3.6',
+    author='Ideonate',
+    author_email='dan@containds.com',
+    license='BSD',
+    url='https://containds.com/',
+    # this should be a whitespace separated string of keywords, not a list
+    keywords="containds jupyterhub",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    platforms="Linux, Mac OS X",
+    description="ContainDS Dashboards extension for JupyterHub",
+    data_files=get_data_files(),
+    install_requires=requirements,
+)
