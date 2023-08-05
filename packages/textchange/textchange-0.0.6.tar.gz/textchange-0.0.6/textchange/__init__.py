38f824bd6b756d8785a9ffb136947105199ec6ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:42 +0000

    Add package textchange/0.0.6/textchange-0.0.6.tar.gz

---
+++ packages/textchange/textchange-0.0.6.tar.gz/textchange-0.0.6/textchange/__init__.py
@@ -0,0 +1,77 @@
+#this library can change text
+
+if True:
+    name = 'textchange'
+    version = '0.0.6'
+    developer = 'Yanwentao'
+    update_date = '2020/4/28'
+
+def __before__():
+    print('==========textchange==========')
+    print('full name : text change')
+    print('version : '+version)
+    print('developer : '+developer)
+    print('update date : '+update_date)
+    print('You can type "help()" for more information.')
+
+__before__()
+
+def help():
+    print('''This library has two functions, namely, reading and writing documents.
+You only need to enter "write ()" to write to the document, and you need to enter "read ()" to read the document.''')
+
+def read():
+    global text
+    asktextname = input('Please enter the text location(Type v to view):')
+    ion = True
+    if asktextname == 'v':
+        print('This feature requires the installation of the tkinter library.')
+        import time
+        time.sleep(1)
+        del time
+        from tkinter.filedialog import askopenfilename
+        asktextname=askopenfilename(filetypes=[('text','*.txt')])
+    elif asktextname == '':
+        print('This is not a location!')
+        text = ''
+        ion = 'False'
+    if ion:
+        try:
+            with open(asktextname,'r',encoding='UTF-8') as t:
+                text = t.read()
+            print()
+            print(text)
+            print()
+            print('The literal variable name is "text".')
+        except:
+            print('This is not a location!')
+            text = ''
+
+def write():
+    asktextname = input('Please enter the text location(Type v to view):')
+    ion = True
+    if asktextname == 'v':
+        print('This feature requires the installation of the tkinter library.')
+        import time
+        time.sleep(1)
+        del time
+        from tkinter.filedialog import askopenfilename
+        asktextname=askopenfilename(filetypes=[('text','*.txt')])
+    elif asktextname == '':
+        print('This is not a location!')
+        text = ''
+        ion = 'False'
+    if ion:
+        try:
+            with open(asktextname,'w+',encoding='UTF-8') as t:
+                text = t.read()
+                text.seek(0)
+                text.truncate()
+            print()
+            print(text)
+            print()
+            wt = input('Please enter the changed text:')
+            text.write(wt)
+        except:
+            print('This is not a location!')
+            text = ''
