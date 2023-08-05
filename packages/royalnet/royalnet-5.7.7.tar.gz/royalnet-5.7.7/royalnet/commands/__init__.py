41c1eb7c9ddcf44af7d193ec936d3b96ecb5db32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:44 +0000

    Add package royalnet/5.7.7/royalnet-5.7.7.tar.gz

---
+++ packages/royalnet/royalnet-5.7.7.tar.gz/royalnet-5.7.7/royalnet/commands/__init__.py
@@ -0,0 +1,26 @@
+"""The subpackage providing all classes related to Royalnet commands."""
+
+from .commandinterface import CommandInterface
+from .command import Command
+from .commanddata import CommandData
+from .commandargs import CommandArgs
+from .event import Event
+from .errors import \
+    CommandError, InvalidInputError, UnsupportedError, ConfigurationError, ExternalError, UserError, ProgramError
+from .keyboardkey import KeyboardKey
+
+__all__ = [
+    "CommandInterface",
+    "Command",
+    "CommandData",
+    "CommandArgs",
+    "CommandError",
+    "InvalidInputError",
+    "UnsupportedError",
+    "ConfigurationError",
+    "ExternalError",
+    "UserError",
+    "ProgramError",
+    "Event",
+    "KeyboardKey",
+]
