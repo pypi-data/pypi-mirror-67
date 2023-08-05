91eb5bc3bbcf9057dcd785ddf49528aa9ab88dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:28 +0000

    Add package dbdaora/0.5.1/dbdaora-0.5.1-py3-none-any.whl

---
+++ packages/dbdaora/dbdaora-0.5.1-py3-none-any.whl/dbdaora/hash/repositories/datastore.py
@@ -0,0 +1,35 @@
+from typing import Any, ClassVar, Optional, Sequence, Type, get_type_hints
+
+from google.cloud.datastore import Key
+
+from . import HashRepository
+
+
+class DatastoreHashRepository(HashRepository[Key]):
+    __skip_cls_validation__ = ('DatastoreHashRepository',)
+    exclude_from_indexes: ClassVar[Sequence[str]] = ()
+    exclude_all_from_indexes: ClassVar[bool] = False
+
+    def __init_subclass__(
+        cls,
+        entity_cls: Optional[Type[Any]] = None,
+        name: Optional[str] = None,
+        id_name: Optional[str] = None,
+        key_attrs: Optional[Sequence[str]] = None,
+        many_key_attrs: Optional[Sequence[str]] = None,
+    ):
+        super().__init_subclass__(
+            entity_cls, name, id_name, key_attrs, many_key_attrs,
+        )
+
+        if cls.exclude_all_from_indexes:
+            cls.exclude_from_indexes = tuple(
+                get_type_hints(cls.entity_cls).keys()
+            )
+
+    async def add_fallback(
+        self, entity: Any, *entities: Any, **kwargs: Any
+    ) -> None:
+        await super().add_fallback(
+            entity, *entities, exclude_from_indexes=self.exclude_from_indexes
+        )
