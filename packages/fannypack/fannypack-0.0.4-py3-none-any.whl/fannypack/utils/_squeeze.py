60076ccd38eb59d9c22ac1de3ea94694c0b21138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:24 +0000

    Add package fannypack/0.0.4/fannypack-0.0.4-py3-none-any.whl

---
+++ packages/fannypack/fannypack-0.0.4-py3-none-any.whl/fannypack/utils/_squeeze.py
@@ -0,0 +1,21 @@
+from typing import Iterable, List, Tuple, Union, cast
+
+
+def squeeze(x, axis: Union[int, Tuple[int]] = None):
+    """Generic squeeze function.
+    """
+    if type(axis) == int:
+        axis = cast(Tuple[int], (axis,))
+    else:
+        axis = cast(Tuple[int], axis)
+
+    slices: List[Union[int, slice]] = []
+    for i, dim in enumerate(x.shape):
+        if dim == 1 and (axis is None or i in axis):
+            slices.append(0)
+        elif axis is not None and i in axis:
+            assert False, "Desired axis can't be squeezed"
+        else:
+            slices.append(slice(None))
+
+    return x[tuple(slices)]
