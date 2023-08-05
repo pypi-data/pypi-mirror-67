90a01cc2641df88da8caa5c70cbefdd8b132fdea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:58 +0000

    Add package anole/0.1.5/anole-0.1.5-py3-none-any.whl

---
+++ packages/anole/anole-0.1.5-py3-none-any.whl/anole/setting.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# @author: leesoar
+
+"""settings"""
+
+import logging
+import os
+import tempfile
+
+__version__ = "0.1.5"
+
+logger = logging.getLogger(__package__)
+
+CACHE_PATH = os.path.join(tempfile.gettempdir(), f"anole_{__version__}.json")
+
+CACHE_URL = f"https://www.leesoar.com/file/anole_{__version__}.json"
+
+SHORTCUTS = {
+    'internet explorer': 'internetexplorer',
+    'ie': 'internetexplorer',
+    'msie': 'internetexplorer',
+    'edge': 'internetexplorer',
+    'gg': 'chrome',
+    'google': 'chrome',
+    'googlechrome': 'chrome',
+    'ff': 'firefox',
+}
+
+HTTP_RETRY = 5
