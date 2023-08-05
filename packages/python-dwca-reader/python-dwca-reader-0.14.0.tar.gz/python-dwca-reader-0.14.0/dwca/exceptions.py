590f2328b35254878f506c81de803fd21bd3be29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:09 +0000

    Add package python-dwca-reader/0.14.0/python-dwca-reader-0.14.0.tar.gz

---
+++ packages/python-dwca-reader/python-dwca-reader-0.14.0.tar.gz/python-dwca-reader-0.14.0/dwca/exceptions.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""Exceptions for the whole package."""
+
+
+class RowNotFound(Exception):
+    """The DwC-A Row cannot be found."""
+
+
+class InvalidArchive(Exception):
+    """The archive appears to be invalid."""
+
+
+class InvalidSimpleArchive(InvalidArchive):
+    """The simple archive appears to be invalid."""
+
+
+class NotADataFile(Exception):
+    """The file doesn't exists or is not a data file."""
