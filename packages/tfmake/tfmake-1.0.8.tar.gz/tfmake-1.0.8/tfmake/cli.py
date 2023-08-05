384c1091f12390e77bd7f42aaa2ed9781779d0f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:28 +0000

    Add package tfmake/1.0.8/tfmake-1.0.8.tar.gz

---
+++ packages/tfmake/tfmake-1.0.8.tar.gz/tfmake-1.0.8/tfmake/cli.py
@@ -0,0 +1,68 @@
+import os
+import click
+from jinja2 import Environment, PackageLoader
+import yaml
+import json
+
+from tfmake import __version__
+
+from tfmake.custom import DefaultCommandHandler, DefaultCommandGroup
+
+env = Environment(
+    loader=PackageLoader('tfmake', 'templates')
+)
+
+@click.group(cls=DefaultCommandGroup)
+@click.version_option(version=__version__)
+def main():
+    pass
+
+@main.command(name='azure')
+@click.argument('target', default='help')
+@click.argument('args', nargs=-1)
+def azure(target, args):
+    '''
+    Use Azure provider
+    '''
+    DefaultCommandHandler('azure').call(target, args)
+
+@main.command(name='guess', default_command=True)
+@click.argument('target', default='help')
+@click.argument('args', nargs=-1)
+def guess(target, args):
+    '''
+    Default command that guesses what provider you're using.
+    '''
+    DefaultCommandHandler().call(target, args)
+
+@main.command(name='aws')
+@click.argument('target', default='help')
+@click.argument('args', nargs=-1)
+def aws(target, args):
+    '''
+    Use AWS provider.
+    '''
+    DefaultCommandHandler('aws').call(target, args)
+
+@main.command()
+@click.argument('provider', default='aws')
+def init(provider):
+    '''
+    Create configuration for provider.
+    '''
+    cwd = os.path.join(os.getcwd(), '.tfmake')
+    if os.path.isfile(cwd):
+        click.confirm('Existing configuration found! Do you want to continue?', abort=True)
+
+    # Get provider-specific template
+    # NOTE: this is for future use ... not used yet
+    template = env.get_template('tfmake.{}.j2'.format(provider))
+
+    with open(cwd, 'w+') as f:
+        f.write(template.render(provider=provider))
+
+    click.echo("\nConfiguration written to '{}'".format(cwd))
+
+if __name__ == '__main__':
+    main()
+
