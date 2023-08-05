362de029d7db1fcbc65e93c8f4a14edeed31ba08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:55 +0000

    Add package anole/0.1.4/anole-0.1.4-py3-none-any.whl

---
+++ packages/anole/anole-0.1.4-py3-none-any.whl/anole/error.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @author: leesoar
+
+"""errors"""
+
+AnoleUserAgnetError = type("AnoleUserAgnetError", (Exception, ), {})
+AnoleCrawlError = type("AnoleCrawlError", (Exception, ), {})
