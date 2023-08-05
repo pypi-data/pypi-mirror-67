18e955b8044895927bc20ba371c7db16662b1ae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:52 +0000

    Add package engagevoice-sdk-wrapper/0.5.1.dev0/engagevoice-sdk-wrapper-0.5.1.dev0.tar.gz

---
+++ packages/engagevoice-sdk-wrapper/engagevoice-sdk-wrapper-0.5.1.dev0.tar.gz/engagevoice-sdk-wrapper-0.5.1.dev0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.5.1'
+
+setup(name='engagevoice-sdk-wrapper',
+      version=version,
+      description="RingCentral Engage Voice SDK Wrapper for Python",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='RingCentral EngageVoice Engage Contact Center',
+      author='Phong Vu',
+      author_email='phong.vu@ringcentral.com',
+      url='http://engage.ringcentral.com',
+      license='',
+      packages=find_packages(exclude=[]),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+            'requests'
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
