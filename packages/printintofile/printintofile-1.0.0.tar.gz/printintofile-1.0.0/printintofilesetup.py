44e88f8753e32ddbcad6044bf903be79b672483c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:38 +0000

    Add package printintofile/1.0.0/printintofile-1.0.0.tar.gz

---
+++ packages/printintofile/printintofile-1.0.0.tar.gz/printintofile-1.0.0/printintofilesetup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name = 'printintofile',
+        version = '1.0.0',
+        py_modulus = ['printintofile'],
+        author = 'lando',
+        email = 'landochan3@gmail.com',
+        url = '',
+        description = 'print_lol function to file'
+)
