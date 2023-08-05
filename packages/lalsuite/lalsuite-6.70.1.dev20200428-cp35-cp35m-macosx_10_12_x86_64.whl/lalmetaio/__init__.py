adaeabed8a92e1676de5103ef3e95672113cb45c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:52 +0000

    Add package lalsuite/6.70.1.dev20200428/lalsuite-6.70.1.dev20200428-cp35-cp35m-macosx_10_12_x86_64.whl

---
+++ packages/lalsuite/lalsuite-6.70.1.dev20200428-cp35-cp35m-macosx_10_12_x86_64.whl/lalmetaio/__init__.py
@@ -0,0 +1,21 @@
+# Import SWIG wrappings, if available
+from .lalmetaio import *
+
+__version__ = "1.6.0.1"
+
+## \addtogroup lalmetaio_python
+"""This package provides Python wrappings and extensions to LALMetaIO"""
+
+
+
+#
+# This section was added automatically to support using LALSuite as a wheel.
+#
+import os
+import pkg_resources
+new_path = pkg_resources.resource_filename('lalapps', 'data')
+path = os.environ.get('LAL_DATA_PATH')
+path = path.split(':') if path else []
+if new_path not in path:
+    path.append(new_path)
+os.environ['LAL_DATA_PATH'] = ':'.join(path)
