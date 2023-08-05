2f667638b1dc6a4626ca692cba35536c75ecd96c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:00 +0000

    Add package bitvaluta-rpc/1.31/bitvaluta_rpc-1.31.tar.gz

---
+++ packages/bitvaluta-rpc/bitvaluta_rpc-1.31.tar.gz/bitvaluta_rpc-1.31/bitvaluta_rpc/__init__.py
@@ -0,0 +1,7 @@
+
+try:
+    from bitvaluta_rpc.bitvaluta_rpc import Client
+except ImportError:
+    from bitvaluta_rpc import Client
+
+__version__ = 1.31
---
+++ packages/bitvaluta-rpc/bitvaluta_rpc-1.31.tar.gz/bitvaluta_rpc-1.31/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from os import path
+from io import open
+
+classifiers = [
+  'Development Status :: 4 - Beta',
+  'Intended Audience :: Financial and Insurance Industry',
+  'Programming Language :: Python',
+  'Operating System :: OS Independent',
+  'Natural Language :: English',
+  'License :: OSI Approved :: MIT License'
+]
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='bitvaluta_rpc',
+      version='1.31',
+      description='Library to communicate with bitvaluta daemon via JSON-RPC protocol.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/justluce/bitvaluta_rpc',
+      author='justluce',
+      author_email='support@justinluce.com',
+      license='MIT',
+      packages=['bitvaluta_rpc'],
+      install_requires=['requests'],
+      keywords=['bitvaluta', 'json-rpc', 'cryptocurrency', 'blockchain'],
+      classifiers=classifiers,
+      platforms="any")
