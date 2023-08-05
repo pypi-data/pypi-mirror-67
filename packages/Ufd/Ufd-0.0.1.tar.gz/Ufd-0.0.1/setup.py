2d5ac9fbd8df9a330436d4f02b0c8dd71967d34a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:43 +0000

    Add package Ufd/0.0.1/Ufd-0.0.1.tar.gz

---
+++ packages/Ufd/Ufd-0.0.1.tar.gz/Ufd-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("readme.md") as readme:
+    long_description = readme.read()
+
+
+setup(
+    name="Ufd",
+    url="https://github.com/emboiko/Ufd",
+    author="Emboiko",
+    author_email="ed@emboiko.com",
+    version="0.0.1",
+    description="Unopinionated, minimalist, reusable, slightly configurable, general-purpose file-dialog.",
+    py_modules=["Ufd"],
+    package_dir={"":"src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows"
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
