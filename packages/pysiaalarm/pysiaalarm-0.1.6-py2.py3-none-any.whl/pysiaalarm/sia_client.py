88dfa5a59187d95f03b89bf78ccaf9b7c32cf7cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:50 +0000

    Add package pysiaalarm/0.1.6/pysiaalarm-0.1.6-py2.py3-none-any.whl

---
+++ packages/pysiaalarm/pysiaalarm-0.1.6-py2.py3-none-any.whl/pysiaalarm/sia_client.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+"""This is a the main class for the SIA Client."""
+
+from typing import Callable
+from binascii import hexlify, unhexlify
+import socket
+import threading
+import logging
+from Crypto import Random
+from Crypto.Cipher import AES
+
+from pysiaalarm import __version__
+from pysiaalarm.sia_event import SIAEvent
+from pysiaalarm.sia_tcp_handler import SIAServer, SIATCPHandler
+from pysiaalarm.sia_errors import (
+    InvalidAccountFormatError,
+    InvalidAccountLengthError,
+    InvalidKeyFormatError,
+    InvalidKeyLengthError,
+    PortInUseError,
+)
+
+__author__ = "E.A. van Valkenburg"
+__copyright__ = "E.A. van Valkenburg"
+__license__ = "mit"
+__version__ = __version__
+
+logging.getLogger(__name__)  # .addHandler(logging.NullHandler())
+
+global_process_event = None
+ending = ""
+
+
+class SIAClient(threading.Thread):
+    """Class for SIA Clients."""
+
+    def __init__(
+        self,
+        host: str,
+        port: int,
+        account_id: str,
+        function: Callable[[SIAEvent], None],
+        key: str = None,
+    ):
+        """Initialize the SIA Client."""
+        threading.Thread.__init__(self)
+        self._encrypted = False
+        self._ending = "]"
+        self._account_id = account_id
+        self._key = key
+        self._func = function
+        self._host = host
+        self._port = port
+        if self._key:
+            logging.debug("Hub: init: encryption is enabled.")
+            self._encrypted = True
+            self._key = self._key.encode("utf8")
+            try:
+                self._decrypter = AES.new(
+                    self._key,
+                    AES.MODE_CBC,
+                    unhexlify("00000000000000000000000000000000"),
+                )
+                _encrypter = AES.new(
+                    self._key, AES.MODE_CBC, Random.new().read(AES.block_size)
+                )
+            except ValueError:
+                raise InvalidKeyLengthError
+            self._ending = (
+                hexlify(_encrypter.encrypt("00000000000000|]".encode("utf8")))
+                .decode(encoding="UTF-8")
+                .upper()
+            )
+        global ending
+        ending = self._ending
+        self.server = SIAServer((self._host, self._port), SIATCPHandler)
+
+    def test_port(self):
+        """Test if the port is in use."""
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            sock.bind((self._host, self._port))
+            logging.debug("No error in bind.")
+            return True
+        except socket.error as exp:
+            logging.debug(exp)
+            logging.debug("Error in bind.")
+            raise PortInUseError
+        finally:
+            sock.close()
+
+    def validate(self):
+        """Validate the settings by the user."""
+        if self._key:
+            try:
+                int(self._key, 16)
+            except ValueError:
+                raise InvalidKeyFormatError
+            try:
+                assert len(self._key) in (16, 24, 32)
+            except AssertionError:
+                raise InvalidKeyLengthError
+        try:
+            int(self._account_id, 16)
+        except ValueError:
+            raise InvalidAccountFormatError
+        try:
+            assert 3 <= len(self._account_id) <= 16
+        except AssertionError:
+            raise InvalidAccountLengthError
+
+        return True
+
+    def start(self):
+        """Start the SIA TCP Handler."""
+        logging.debug("Hub: start: starting thread.")
+        global global_process_event
+        global_process_event = self.process_event
+        self.server_thread = threading.Thread(target=self.server.serve_forever)
+        self.server_thread.daemon = True
+        self.server_thread.start()
+
+    def stop(self):
+        """Stop the SIA TCP Handler thread."""
+        logging.debug("Hub: stop: stopping thread.")
+        self.server.shutdown()
+        self.server.server_close()
+        self.server_thread.join()
+
+    def process_event(self, event: SIAEvent):
+        """Process the Event that comes from the TCP handler."""
+        try:
+            logging.debug(f"Hub: Process event: {event}")
+            if self._encrypted:
+                event = self._decrypt_string(event)
+                logging.debug(f"Hub: Process event, after decrypt: {event}")
+            self._func(event)
+        except Exception as exc:
+            logging.error(f"Hub: Process Event: {event} gave error {exc}")
+
+    def set_alarm(self, state):
+        """Set the alarm to a specified state."""
+
+    def _decrypt_string(self, event: SIAEvent) -> SIAEvent:
+        """Decrypt the encrypted event content and parse it."""
+        logging.debug("Hub: Decrypt String: Original: %s", str(event.encrypted_content))
+        resmsg = self._decrypter.decrypt(
+            unhexlify(event.encrypted_content.encode("utf8"))
+        ).decode(encoding="UTF-8", errors="replace")
+        logging.debug(f"Hub: Decrypt String: Decrypted: {resmsg}")
+        event.parse_decrypted(resmsg)
+        return event
---
+++ packages/pysiaalarm/pysiaalarm-0.1.6-py2.py3-none-any.whl/pysiaalarm/sia_tcp_handler.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+"""This is the class for the actual TCP handler override of the handle method."""
+
+from datetime import datetime
+import time
+import socketserver
+import logging
+from pysiaalarm.sia_event import SIAEvent
+from pysiaalarm import sia_client
+from pysiaalarm.sia_errors import CRCMismatchError
+
+logging.getLogger(__name__)
+
+
+class SIAServer(socketserver.TCPServer):  # socketserver.ThreadingMixIn,
+    """Class for a SIA Server."""
+
+    daemon_threads = True
+    allow_reuse_address = True
+
+    def __init__(self, server_address, RequestHandlerClass):
+        """Initialize the server."""
+        self.request_handler = RequestHandlerClass
+        socketserver.TCPServer.__init__(self, server_address, self.request_handler)
+
+
+class SIATCPHandler(socketserver.BaseRequestHandler):
+    """Class for the TCP Handler."""
+
+    _received_data = "".encode()
+
+    def handle_line(self, line: str):
+        """Handle the line in detail and call event processing code."""
+        logging.debug(f"TCP: Handle Line: Income raw string: {line}")
+        print(f"Handle line: {line}")
+        # parse the event
+        try:
+            event = SIAEvent(line)
+            logging.debug(f"TCP: Handle Line: event: {event}")
+            if not event.valid_message:
+                logging.error(
+                    f"TCP: Handle Line: CRC mismatch, received: {event.msg_crc}, calculated: {event.calc_crc}"
+                )
+                raise CRCMismatchError("CRC mismatch in event, check the logs.")
+            response = f'"ACK"{event.sequence}L0#{event.account}[{sia_client.ending}'
+        except Exception as exc:
+            logging.error(f"TCP: Handle Line: error: {exc}")
+            timestamp = datetime.fromtimestamp(time.time()).strftime(
+                "_%H:%M:%S,%m-%d-%Y"
+            )
+            response = f'"NAK"0000L0R0A0[]{timestamp}'
+            # if the line could not be parsed or the crc does not match, the event is invalid and ignored, security risk otherwise.
+            event = None
+
+        # send the response.
+        try:
+            logging.debug(f"TCP: Handle Line: response: {response}")
+            header = ("%04x" % len(response)).upper()
+            res = f"\n{SIATCPHandler.crc_calc(response)}{header}{response}\r"
+            byte_response = str.encode(res)
+            self.request.sendall(byte_response)
+        except Exception as exp:
+            logging.info(f"Could not send response, error: {exp}")
+
+        # finally run the event process code if the event is valid.
+        if event:
+            sia_client.global_process_event(event=event)
+
+    def handle(self):
+        """Overridden method for handle lines."""
+        line = b""
+        try:
+            while True:
+                raw = self.request.recv(1024)
+                print(f"Raw: {raw}")
+                if not raw:
+                    return
+                raw = bytearray(raw)
+                while True:
+                    splitter = raw.find(b"\r")
+                    if splitter > -1:
+                        line = raw[1:splitter]
+                        raw = raw[splitter + 1 :]
+                    else:
+                        break
+                    print(f"Line handle: {line.decode()}")
+                    self.handle_line(line.decode())
+        except Exception as exc:
+            logging.error(f"TCP: Handle: last line {line.decode()} gave error: {exc}")
+            return
+
+    @staticmethod
+    def crc_calc(msg: str) -> str:
+        """Calculate the CRC of the response."""
+        new_crc = 0
+        for letter in msg:
+            temp = ord(letter)
+            for _ in range(0, 8):
+                temp ^= new_crc & 1
+                new_crc >>= 1
+                if (temp & 1) != 0:
+                    new_crc ^= 0xA001
+                temp >>= 1
+
+        return ("%x" % new_crc).upper().zfill(4)
