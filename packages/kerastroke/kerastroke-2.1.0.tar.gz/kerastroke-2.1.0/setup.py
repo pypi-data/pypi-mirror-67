3a29d68595000aeda04d1a7ad6bf33ab62b8c860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:31 +0000

    Add package kerastroke/2.1.0/kerastroke-2.1.0.tar.gz

---
+++ packages/kerastroke/kerastroke-2.1.0.tar.gz/kerastroke-2.1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='kerastroke',
+    version='2.1.0',
+    scripts=['./kerastroke/__init__.py'],
+    author="Charles Averill",
+    author_email="charlesaverill20@gmail.com",
+    description="A suite of the generalization-improvement techniques Stroke, Pruning, and NeuroPlast",
+    long_description=long_description,
+    install_requires=['tensorflow', 'numpy'],
+    long_description_content_type="text/markdown",
+    url="https://github.com/CharlesAverill/kerastroke/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
