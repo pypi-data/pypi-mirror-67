293c69f42114fb3b499301dbe1d578ed832ea6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:49 +0000

    Add package mbed-build/1.0.0b30/mbed_build-1.0.0b30-py3-none-any.whl

---
+++ packages/mbed-build/mbed_build-1.0.0b30-py3-none-any.whl/mbed_build/_internal/config/source.py
@@ -0,0 +1,127 @@
+#
+# Copyright (C) 2020 Arm Mbed. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+"""Configuration source abstraction."""
+from dataclasses import dataclass
+import json
+from pathlib import Path
+from typing import Iterable, Tuple
+
+from mbed_targets import get_target_by_board_type
+from mbed_build._internal.config.cumulative_data import ALL_CUMULATIVE_FIELDS
+
+
+@dataclass
+class Source:
+    """Configuration source abstraction.
+
+    MbedOS build configuration is assembled from various sources:
+    - targets.json
+    - mbed_lib.json
+    - mbed_app.json
+
+    This class provides a common interface to configuration data.
+    """
+
+    human_name: str
+    config: dict
+    config_overrides: dict
+    cumulative_overrides: dict
+    macros: Iterable[str]
+
+    @classmethod
+    def from_mbed_lib(cls, file: Path, target_labels: Iterable[str]) -> "Source":
+        """Build Source from mbed_lib.json file.
+
+        Args:
+            file: Path to mbed_lib.json file
+            target_labels: Labels for which "target_overrides" should apply
+        """
+        data = json.loads(file.read_text())
+        namespace = data["name"]
+
+        config = data.get("config", {})
+        config = _namespace_data(config, namespace)
+
+        target_overrides = data.get("target_overrides", {})
+        target_specific_overrides = _filter_target_overrides(target_overrides, target_labels)
+        target_specific_overrides = _namespace_data(target_specific_overrides, namespace)
+        config_overrides, cumulative_overrides = _split_target_overrides_by_type(target_specific_overrides)
+
+        macros = data.get("macros", [])
+
+        return cls(
+            human_name=f"File: {file}",
+            config=config,
+            cumulative_overrides=cumulative_overrides,
+            config_overrides=config_overrides,
+            macros=macros,
+        )
+
+    @classmethod
+    def from_target(cls, mbed_target: str, mbed_program_directory: Path) -> "Source":
+        """Build Source from retrieved mbed_targets.Target data."""
+        target = get_target_by_board_type(mbed_target, mbed_program_directory)
+        namespace = "target"
+        config = _namespace_data(target.config, namespace)
+
+        cumulative_overrides = {
+            "features": target.features,
+            "components": target.components,
+            "labels": target.labels,
+        }
+        cumulative_overrides = _namespace_data(cumulative_overrides, namespace)
+
+        return cls(
+            human_name=f"mbed_target.Target for {mbed_target}",
+            config=config,
+            config_overrides={},
+            cumulative_overrides=cumulative_overrides,
+            macros=[],
+        )
+
+
+def _filter_target_overrides(data: dict, allowed_labels: Iterable[str]) -> dict:
+    """Flatten and filter target overrides.
+
+    Ensures returned dictionary only contains configuration settings applicable to given allowed labels.
+    """
+    flattened = {}
+    for target_label, overrides in data.items():
+        if target_label == "*" or target_label in allowed_labels:
+            flattened.update(overrides)
+    return flattened
+
+
+def _split_target_overrides_by_type(data: dict) -> Tuple[dict, dict]:
+    """Split target override data into config overrides and cumulative overrides."""
+    config_overrides = {}
+    cumulative_overrides = {}
+    for key, value in data.items():
+        if key in ALL_CUMULATIVE_FIELDS:
+            cumulative_overrides[key] = value
+        else:
+            config_overrides[key] = value
+    return config_overrides, cumulative_overrides
+
+
+def _namespace_data(data: dict, namespace: str) -> dict:
+    """Prefix configuration key with a namespace.
+
+    Namespace is ConfigSource wide, and is resolved at source build time.
+
+    It should be one of:
+    - "target"
+    - "app"
+    - library name (where "mbed_lib.json" comes from)
+
+    If given key is already namespaced, return it as is - this is going to be the case for
+    keys from "target_overrides" entries. Keys from "config" usually need namespacing.
+    """
+    namespaced = {}
+    for key, value in data.items():
+        if "." not in key:
+            key = f"{namespace}.{key}"
+        namespaced[key] = value
+    return namespaced
---
+++ packages/mbed-build/mbed_build-1.0.0b30-py3-none-any.whl/mbed_build/_internal/mbed_tools/export.py
@@ -0,0 +1,56 @@
+#
+# Copyright (C) 2020 Arm Mbed. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+"""Export top-level CMakeLists.txt command."""
+import pathlib
+
+import click
+
+from mbed_build.mbed_build import generate_cmakelists_file, write_cmakelists_file
+
+
+@click.command(
+    help="This is an unfinished implementation of export. It only exports the initial Mbed OS 'key folder names'."
+)
+@click.option(
+    "-o",
+    "--output-directory",
+    type=click.Path(),
+    required=True,
+    help="Destination for exported CMakeLists.txt file containing directory keys.",
+)
+@click.option(
+    "-t",
+    "--toolchain",
+    type=click.Choice(["ARM6", "GCC"]),
+    required=True,
+    help="The toolchain you are using to build your app.",
+)
+@click.option("-m", "--mbed_target", required=True, help="A build target for an Mbed-enabled device, eg. K64F")
+@click.option(
+    "-p",
+    "--project-path",
+    type=click.Path(),
+    default=".",
+    help="Path to local Mbed project. By default is the current working directory.",
+)
+def export(output_directory: str, toolchain: str, mbed_target: str, project_path: str) -> None:
+    """Exports a top-level CMakeLists.txt file to the specified directory.
+
+    The parameters set in the CMake file will be dependent on the combination of
+    toolchain and Mbed target provided and these can then control which parts of
+    Mbed OS is included in the build.
+
+    Args:
+        output_directory: where the top-level CMakeLists.txt should be exported to
+        toolchain: the toolchain you are using to build your app (eg. GCC, ARM5 etc.)
+        mbed_target: the build target you are wanting to run your app (eg. K64F)
+        project_path: the path to the local Mbed project
+
+    Raises:
+        InvalidExportOutputDirectory: it's not possible to export to the output directory provided
+    """
+    cmake_file_contents = generate_cmakelists_file(mbed_target, project_path, toolchain)
+    write_cmakelists_file(pathlib.Path(output_directory), cmake_file_contents)
+    click.echo(f"The program-level CMake file has been successfully exported to directory '{str(output_directory)}'")
