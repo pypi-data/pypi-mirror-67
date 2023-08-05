af8efb758739e0e196d3b7d5af4afac9c8733498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:50 +0000

    Add package re-common/0.0.37/re_common-0.0.37.tar.gz

---
+++ packages/re-common/re_common-0.0.37.tar.gz/re_common-0.0.37/re_common/facade/lazy_import.py
@@ -0,0 +1,7 @@
+# 延迟导入　主要是为了使用时不去安装多余的
+from re_common.baselibrary import IniConfig
+
+
+def get_TomlConfig():
+    from re_common.baselibrary.readconfig.toml_config import TomlConfig
+    return TomlConfig
