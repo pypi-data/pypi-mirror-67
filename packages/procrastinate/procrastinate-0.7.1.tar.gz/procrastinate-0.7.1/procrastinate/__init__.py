850327321e5c22c658e1d0e7c6c4f7f43a1e7357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:34 +0000

    Add package procrastinate/0.7.1/procrastinate-0.7.1.tar.gz

---
+++ packages/procrastinate/procrastinate-0.7.1.tar.gz/procrastinate-0.7.1/procrastinate/__init__.py
@@ -0,0 +1,19 @@
+from procrastinate import metadata as _metadata_module
+from procrastinate.aiopg_connector import PostgresConnector
+from procrastinate.app import App
+from procrastinate.retry import BaseRetryStrategy, RetryStrategy
+
+__all__ = [
+    "App",
+    "BaseRetryStrategy",
+    "PostgresConnector",
+    "RetryStrategy",
+]
+
+
+_metadata = _metadata_module.extract_metadata()
+__author__ = _metadata["author"]
+__author_email__ = _metadata["email"]
+__license__ = _metadata["license"]
+__url__ = _metadata["url"]
+__version__ = _metadata["version"]
---
+++ packages/procrastinate/procrastinate-0.7.1.tar.gz/procrastinate-0.7.1/procrastinate/builtin_tasks.py
@@ -0,0 +1,39 @@
+import functools
+from typing import Optional
+
+from procrastinate import store
+
+
+async def remove_old_jobs(
+    job_store: store.JobStore,
+    max_hours: int,
+    queue: Optional[str] = None,
+    remove_error: Optional[bool] = False,
+) -> None:
+    """
+    This task cleans your database by removing old jobs. Note that jobs and linked
+    events will be irreversibly removed from the database when running this task.
+
+    Parameters
+    ----------
+    max_hours :
+        Only jobs which were finished more than ``max_hours`` ago will be deleted.
+    queue :
+        The name of the queue in which jobs will be deleted. If not specified, the
+        task will delete jobs from all queues.
+    remove_error :
+        By default only successful jobs will be removed. When this parameter is True
+        failed jobs will also be deleted.
+    """
+    await job_store.delete_old_jobs(
+        nb_hours=max_hours, queue=queue, include_error=remove_error
+    )
+
+
+# Register your builtin tasks here
+def register_builtin_tasks(app) -> None:
+    app.builtin_tasks["remove_old_jobs"] = app.task(
+        functools.partial(remove_old_jobs, app.job_store),
+        queue="builtin",
+        name="procrastinate.builtin_tasks.remove_old_jobs",
+    )
