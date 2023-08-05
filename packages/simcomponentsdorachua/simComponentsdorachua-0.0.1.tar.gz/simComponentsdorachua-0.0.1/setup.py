7c9d4e515e8f3803231b7f3efa1f6b1ae5e76b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:18 +0000

    Add package simcomponentsdorachua/0.0.1/simComponentsdorachua-0.0.1.tar.gz

---
+++ packages/simcomponentsdorachua/simComponentsdorachua-0.0.1.tar.gz/simComponentsdorachua-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simComponentsdorachua", # Replace with your own username
+    version="0.0.1",
+    author="Dora Chua",
+    author_email="msdorachua@gmail.com",
+    description="A small package for simulating RaspberryPi and Arduino hardware components on a Windows PC",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
