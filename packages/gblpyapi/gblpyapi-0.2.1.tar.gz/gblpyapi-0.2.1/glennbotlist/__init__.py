73e4e4beaeb5ee66cfec00b4c415bd52eacd5e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:42 +0000

    Add package gblpyapi/0.2.1/gblpyapi-0.2.1.tar.gz

---
+++ packages/gblpyapi/gblpyapi-0.2.1.tar.gz/gblpyapi-0.2.1/glennbotlist/__init__.py
@@ -0,0 +1,7 @@
+from .GBL import GBL
+from .GBL import errors
+
+__copyright__ = 'Copyright 2020 Gavyn Stanley'
+__author__ = "Gavyn Stanley (A Trash Coder)"
+__license__ = "MIT"
+__title__ = "gblapi.py"
