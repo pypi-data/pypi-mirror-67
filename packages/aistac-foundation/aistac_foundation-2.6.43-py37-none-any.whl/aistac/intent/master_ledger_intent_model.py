4b63aab55462d111614a5868764d958149327275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:10 +0000

    Add package aistac-foundation/2.6.43/aistac_foundation-2.6.43-py37-none-any.whl

---
+++ packages/aistac-foundation/aistac_foundation-2.6.43-py37-none-any.whl/aistac/intent/master_ledger_intent_model.py
@@ -0,0 +1,34 @@
+import inspect
+
+from aistac.properties.master_ledger_property_manager import LedgerPropertyManager
+from aistac.intent.abstract_intent import AbstractIntentModel
+
+__author__ = 'Darryl Oatridge'
+
+
+class MasterLedgerIntentModel(AbstractIntentModel):
+
+    def __init__(self, property_manager: LedgerPropertyManager, default_save_intent: bool=None,
+                 default_intent_level: [str, int, float]=None, order_next_available: bool=None,
+                 default_replace_intent: bool=None):
+        """initialisation of the Intent class.
+
+        :param property_manager: the property manager class that references the intent contract.
+        :param default_save_intent: (optional) The default action for saving intent in the property manager
+        :param default_intent_level: (optional) the default level intent should be saved at
+        :param order_next_available: (optional) if the default behaviour for the order should be next available order
+        :param default_replace_intent: (optional) the default replace existing intent behaviour
+        """
+        default_save_intent = default_save_intent if isinstance(default_save_intent, bool) else True
+        default_replace_intent = default_replace_intent if isinstance(default_replace_intent, bool) else True
+        default_intent_level = default_intent_level if isinstance(default_intent_level, (str, int, float)) else 'A'
+        default_intent_order = -1 if isinstance(order_next_available, bool) and order_next_available else 0
+        intent_param_exclude = []
+        intent_type_additions = []
+        super().__init__(property_manager=property_manager, default_save_intent=default_save_intent,
+                         intent_param_exclude=intent_param_exclude, default_intent_level=default_intent_level,
+                         default_intent_order=default_intent_order, default_replace_intent=default_replace_intent,
+                         intent_type_additions=intent_type_additions)
+
+    def run_intent_pipeline(self, intent_levels: [int, str, list], **kwargs):
+        pass
