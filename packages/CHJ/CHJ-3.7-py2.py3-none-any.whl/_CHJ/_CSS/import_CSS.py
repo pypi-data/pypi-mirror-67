7278fa174e2f6a7adfaf0787e119c9e762800711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:46 +0000

    Add package CHJ/3.7/CHJ-3.7-py2.py3-none-any.whl

---
+++ packages/CHJ/CHJ-3.7-py2.py3-none-any.whl/_CHJ/_CSS/import_CSS.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2011-2017 Mag. Christian Tanzer All rights reserved
+# Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
+# ****************************************************************************
+# This module is part of the package CHJ.CSS.
+#
+# This module is licensed under the terms of the BSD 3-Clause License
+# <http://www.c-tanzer.at/license/bsd_3c.html>.
+# ****************************************************************************
+#
+#++
+# Name
+#    CHJ.CSS.import_CSS
+#
+# Purpose
+#    Import all CSS classes necessary to define CSS fragments
+#
+#    Usage::
+#
+#        from _CHJ._CSS.import_CSS import *
+#
+# Revision Dates
+#     1-Jan-2011 (CT) Creation
+#    21-Feb-2011 (CT) `Border` added
+#    17-Jan-2012 (CT) Import `GTW.CSS.Color` instead of `TFL.Color`
+#    12-Apr-2014 (CT) Import `Property`, not `Border`
+#    11-Oct-2016 (CT) Move from `GTW` to `CHJ`
+#     5-Jan-2017 (CT) Add `CS`
+#    18-Jan-2017 (CT) Add `Shape`
+#    ««revision-date»»···
+#--
+
+from   _CHJ._CSS             import CS
+from   _CHJ._CSS             import Media
+from   _CHJ._CSS             import Shape
+from   _CHJ._CSS.Color       import *
+from   _CHJ._CSS.Length      import *
+from   _CHJ._CSS.Property    import *
+from   _CHJ._CSS.Rule        import *
+from   _CHJ._CSS.Style_Sheet import *
+
+### __END__ CHJ.CSS.import_CSS
