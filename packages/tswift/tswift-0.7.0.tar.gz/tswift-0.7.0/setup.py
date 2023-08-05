dc345fb27c9a184d11a947ecb8cb0f07017d24f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:20 +0000

    Add package tswift/0.7.0/tswift-0.7.0.tar.gz

---
+++ packages/tswift/tswift-0.7.0.tar.gz/tswift-0.7.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name='tswift',
+    version='0.7.0',
+    description='MetroLyrics API',
+    long_description=long_description,
+    install_requires=['lxml', 'requests', 'google>=2.0'],
+    url='https://github.com/brenns10/tswift',
+    author='Stephen Brennan',
+    author_email='stephen@brennan.io',
+    license='Revised BSD',
+    py_modules=['tswift'],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Development Status :: 4 - Beta',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Natural Language :: English',
+    ],
+    keywords='lyrics metrolyrics scrape',
+    entry_points={
+        'console_scripts': [
+            'tswift=tswift:main',
+        ],
+    },
+)
