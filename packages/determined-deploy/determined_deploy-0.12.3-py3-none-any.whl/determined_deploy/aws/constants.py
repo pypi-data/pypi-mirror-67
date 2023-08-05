dd4edc7e86af646bbf30abfc8d00c053cb1241fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:53 +0000

    Add package determined-deploy/0.12.3/determined_deploy-0.12.3-py3-none-any.whl

---
+++ packages/determined-deploy/determined_deploy-0.12.3-py3-none-any.whl/determined_deploy/aws/constants.py
@@ -0,0 +1,49 @@
+class deployment_types:
+    SIMPLE = "simple"
+    SECURE = "secure"
+    VPC = "vpc"
+    DEPLOYMENT_TYPES = [SIMPLE, SECURE, VPC]
+
+
+class defaults:
+    DEPLOYMENT_TYPE = deployment_types.SIMPLE
+    DB_PASSWORD = "postgres"
+    HASURA_SECRET = "hasura"
+    REGION = "us-west-2"
+
+
+class cloudformation:
+    CLUSTER_ID = "ClusterId"
+    KEYPAIR = "Keypair"
+    VPC = "VPC"
+    PUBLIC_SUBNET = "PublicSubnetId"
+    PRIVATE_SUBNET = "PrivateSubnetId"
+    BASTION_AMI = "BastionAmiId"
+    MASTER_AMI = "MasterAmiId"
+    AGENT_AMI = "AgentAmiId"
+    AGENT_INSTANCE_PROFILE_KEY = "AgentInstanceProfile"
+    AGENT_SECURITY_GROUP_ID_KEY = "AgentSecurityGroupId"
+    MASTER_ID = "MasterId"
+    BASTION_ID = "BastionId"
+    CHECKPOINT_BUCKET = "CheckpointBucket"
+    MASTER_INSTANCE_TYPE = "MasterInstanceType"
+    AGENT_INSTANCE_TYPE = "AgentInstanceType"
+    PUBLIC_IP_ADDRESS = "PublicIpAddress"
+    PRIVATE_IP_ADDRESS = "PrivateIpAddress"
+    SUBNET_ID_KEY = "SubnetId"
+    INBOUND_CIDR = "InboundCIDRRange"
+    DET_ADDRESS = "DeterminedAddress"
+    VERSION = "Version"
+    DB_PASSWORD = "DBPassword"
+    HASURA_SECRET = "HasuraSecret"
+    BOTO3_SESSION = "Boto3Session"
+    AGENT_TAG_NAME = "AgentTagName"
+    MAX_IDLE_AGENT_PERIOD = "MaxIdleAgentPeriod"
+    MAX_INSTANCES = "MaxInstances"
+    LOG_GROUP = "LogGroup"
+    REGION = "Region"
+
+
+class misc:
+    TEMPLATE_PATH = "determined_deploy.aws.templates"
+    CLOUDFORMATION_REGEX = r"^[a-zA-Z][-a-zA-Z0-9]*$"
---
+++ packages/determined-deploy/determined_deploy-0.12.3-py3-none-any.whl/determined_deploy/gcp/constants.py
@@ -0,0 +1,16 @@
+class defaults:
+
+    AGENT_INSTANCE_TYPE = "n1-standard-32"
+    DB_PASSWORD = "postgres"
+    ENVIRONMENT_IMAGE = "pedl-environments-4bd937a"
+    GPU_NUM = 8
+    GPU_TYPE = "nvidia-tesla-k80"
+    HASURA_SECRET = "secret"
+    MASTER_INSTANCE_TYPE = "n1-standard-2"
+    MAX_IDLE_AGENT_PERIOD = "10m"
+    MAX_INSTANCES = 5
+    MIN_CPU_PLATFORM_MASTER = "Intel Skylake"
+    MIN_CPU_PLATFORM_AGENT = "Intel Broadwell"
+    NETWORK = "det-default"
+    PORT = 8080
+    REGION = "us-west1"
