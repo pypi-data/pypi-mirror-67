59934d7df7cd46c3551666016a684047148dc159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:23 +0000

    Add package vk-distributions/0.1/vk_distributions-0.1.tar.gz

---
+++ packages/vk-distributions/vk_distributions-0.1.tar.gz/vk_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='vk_distributions',
+      version='0.1',
+      description='Gaussian and Normal distributions',
+      packages=['distributions'],
+      author='Vivek Kandasamy',
+      author_email='vivekkandasmy@yahoo.in',
+      zip_safe=False)
