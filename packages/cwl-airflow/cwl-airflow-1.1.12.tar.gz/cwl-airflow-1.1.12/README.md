c26ce32938beda85dbed044be5a108bfdf27317f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:50 +0000

    Add package cwl-airflow/1.1.12/cwl-airflow-1.1.12.tar.gz

---
+++ packages/cwl-airflow/cwl-airflow-1.1.12.tar.gz/cwl-airflow-1.1.12/README.md
@@ -0,0 +1,27 @@
+[![DOI](https://zenodo.org/badge/103197335.svg)](https://zenodo.org/badge/latestdoi/103197335)
+[![Python 3.6](https://img.shields.io/badge/python-3.6-green.svg)](https://www.python.org/downloads/release/python-365/)
+[![Build Status](https://ci.commonwl.org/buildStatus/icon?job=airflow-conformance)](https://ci.commonwl.org/job/airflow-conformance)
+
+
+# **CWL-Airflow**
+
+Python package to extend **[Apache-Airflow 1.10.5](https://github.com/apache/incubator-airflow)**
+functionality with **[CWL v1.1](https://www.commonwl.org/v1.1/)** support.
+
+
+## **Cite as**
+
+Michael Kotliar, Andrey V Kartashov, Artem Barski, CWL-Airflow: a lightweight pipeline manager supporting Common Workflow Language, GigaScience, Volume 8, Issue 7, July 2019, giz084, https://doi.org/10.1093/gigascience/giz084
+
+## **Get the latest version**
+```
+pip install cwl-airflow
+```
+Latest version [documentation](https://cwl-airflow.readthedocs.io/en/latest/)
+
+
+## **Get published version**
+```
+pip install cwl-airflow==1.0.16
+```
+Published version [documentation](https://cwl-airflow.readthedocs.io/en/1.0.18/)
---
+++ packages/cwl-airflow/cwl-airflow-1.1.12.tar.gz/cwl-airflow-1.1.12/setup.py
@@ -0,0 +1,155 @@
+#! /usr/bin/env python3
+import os
+import subprocess
+import time
+from setuptools import setup, find_packages
+
+
+GIT_VERSION_FILE = os.path.join('cwl_airflow','git_version')
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_git_tag():
+    return subprocess.check_output(['git', 'describe', '--contains'], text=True).split("^")[0].strip()
+
+
+def get_git_timestamp():
+    gitinfo = subprocess.check_output(
+        ['git', 'log', '--first-parent', '--max-count=1', '--format=format:%ct', '.'], text=True).strip()
+    return time.strftime('%Y%m%d%H%M%S', time.gmtime(int(gitinfo)))
+
+
+def get_description():
+    README = os.path.join(HERE, 'README.md')
+    with open(README, 'r') as f:
+        return f.read()
+
+
+def get_version():
+    '''
+    Tries to get pachage version with following order:
+    0. default version
+    1. from git_version file - when installing from pip, this is the only source to get version
+    2. from tag
+    3. from commit timestamp
+    Updates/creates git_version file with the package version
+    :return: package version
+    '''
+    version = '1.1.0'                                      # set default version
+    try:
+        with open(GIT_VERSION_FILE, 'r') as input_stream:  # try to get version info from file
+            version = input_stream.read()
+    except Exception:
+        pass
+    try:
+        version = get_git_tag()                            # try to get version info from the closest tag
+    except Exception:
+        try:
+            version = '1.1.' + get_git_timestamp()         # try to get version info from commit date
+        except Exception:
+            pass
+    try:
+        with open(GIT_VERSION_FILE, 'w') as output_stream: # save updated version to file (or the same)
+            output_stream.write(version)
+    except Exception:
+        pass
+    return version
+
+
+setup(
+    name='cwl-airflow',
+    description='Python package to extend Airflow functionality with CWL v1.0 support',
+    long_description=get_description(),
+    long_description_content_type="text/markdown",
+    version=get_version(),
+    url='https://github.com/Barski-lab/cwl-airflow',
+    download_url='https://github.com/Barski-lab/cwl-airflow',
+    author='Michael Kotliar',
+    author_email='misha.kotliar@gmail.com',
+    license='Apache-2.0',
+    include_package_data=True,
+    packages=find_packages(
+        exclude=[
+            'docs', 'tests',
+            'windows', 'macOS', 'linux',
+            'iOS', 'android',
+            'django'
+        ]
+    ),
+    install_requires=[
+        'flask-appbuilder == 2.2.4',
+        'jsonschema == 3.0.1',
+        'urllib3',
+        'future==0.16.0',
+        'cryptography',
+        'cwltool == 2.0.20200224214940',
+        'schema-salad==5.0.20200220195218',
+        'jsonmerge',
+        'ruamel.yaml == 0.15.97',
+        'apache-airflow == 1.10.10',
+        'pyjwt',
+        'connexion==2.6.0',
+        'werkzeug==0.16.1'
+    ],
+    options={
+        'app': {
+            'formal_name': 'cwl-airflow',
+            'bundle': 'python.org'
+        },
+        'macos': {
+            'app_requires': [
+                'toga-cocoa==0.3.0.dev14',
+            ]
+        },
+        'linux': {
+            'app_requires': [
+                'toga-gtk==0.3.0.dev14',
+            ]
+        },
+        'windows': {
+            'app_requires': [
+                'toga-winforms==0.3.0.dev14',
+            ]
+        },
+        'ios': {
+            'app_requires': [
+                'toga-ios==0.3.0.dev14',
+            ]
+        },
+        'android': {
+            'app_requires': [
+                'toga-android==0.3.0.dev14',
+            ]
+        },
+        'django': {
+            'app_requires': [
+                'toga-django==0.3.0.dev14',
+            ]
+        },
+    },
+    zip_safe=False,
+    scripts=["cwl_airflow/bin/cwl-airflow"],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Healthcare Industry',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: OS Independent',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'Operating System :: Microsoft :: Windows :: Windows 8.1',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.'
+    ]
+)
