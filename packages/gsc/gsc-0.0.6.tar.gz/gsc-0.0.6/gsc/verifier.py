e6a1266f51470ccd83e57215baf74cb34ffcfb83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:16 +0000

    Add package gsc/0.0.6/gsc-0.0.6.tar.gz

---
+++ packages/gsc/gsc-0.0.6.tar.gz/gsc-0.0.6/gsc/verifier.py
@@ -0,0 +1,40 @@
+import os
+from gsc import cli, client
+import gsc.exercises.my_first_commit, gsc.exercises.push_and_pull, gsc.exercises.ssh_clone
+
+
+class VerifyError(Exception):
+    pass
+
+
+def verify(exercise: str = None):
+    while not os.path.exists(".git"):
+        os.chdir("..")
+        if os.getcwd() == "/":
+            raise VerifyError("This is not a git repo.")
+
+    if exercise == "ssh":
+        cli.title(f"Verifying ssh")
+        gsc.exercises.ssh_clone.verify()
+        client.complete_exercise("ssh_clone")
+        return
+    elif exercise:
+        raise VerifyError("Unknown Git Scientist exercise. Try updating gsc.")
+
+    if os.getcwd().endswith(gsc.exercises.push_and_pull.PULL_SUFFIX):
+        gsc_id = "push_and_pull"
+    elif not os.path.exists(".gsc_id"):
+        raise VerifyError("This repo is not a Git Scientist exercise.")
+    else:
+        gsc_id = open(".gsc_id", "r").read().strip()
+
+    cli.title(f"Verifying {gsc_id}")
+
+    if gsc_id == "my_first_commit":
+        gsc.exercises.my_first_commit.verify()
+        client.complete_exercise(gsc_id)
+    elif gsc_id == "push_and_pull":
+        gsc.exercises.push_and_pull.verify()
+        client.complete_exercise(gsc_id)
+    else:
+        raise VerifyError("Unknown Git Scientist exercise. Try updating gsc.")
