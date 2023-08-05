30c024e283ae376ed20bf11ac99a5691322f45cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:02 +0000

    Add package shipmnts-utils/0.0.58/shipmnts_utils-0.0.58-py3-none-any.whl

---
+++ packages/shipmnts-utils/shipmnts_utils-0.0.58-py3-none-any.whl/shipmnts_utils/auth/token_auth.py
@@ -0,0 +1,27 @@
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.exceptions import AuthenticationFailed
+
+from shipmnts_utils.auth.token_handler import token_expire_handler
+
+
+class ExpiringTokenAuthentication(TokenAuthentication):
+    """
+    If token is expired then it will be removed
+    and new one with different key will be created
+    """
+
+    def authenticate_credentials(self, key):
+        try:
+            token = Token.objects.get(key=key)
+        except Token.DoesNotExist:
+            raise AuthenticationFailed("Invalid Token")
+
+        if not token.user.is_active:
+            raise AuthenticationFailed("User is not active")
+
+        is_expired, token = token_expire_handler(token)
+        if is_expired:
+            raise AuthenticationFailed("The Token is expired")
+
+        return token.user, token
