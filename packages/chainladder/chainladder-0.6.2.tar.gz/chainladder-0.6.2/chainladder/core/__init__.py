f84bf7efa4845b3be340c4ea3b2d380b97efc09d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:14 +0000

    Add package chainladder/0.6.2/chainladder-0.6.2.tar.gz

---
+++ packages/chainladder/chainladder-0.6.2.tar.gz/chainladder-0.6.2/chainladder/core/__init__.py
@@ -0,0 +1,4 @@
+""" core should store the core data structure functionality.
+"""
+from chainladder.core.triangle import Triangle # noqa (API import)
+from chainladder.core.io import EstimatorIO # noqa (API import)
