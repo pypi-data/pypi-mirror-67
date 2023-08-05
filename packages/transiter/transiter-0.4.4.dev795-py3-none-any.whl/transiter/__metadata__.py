81c1637125314228176ea09d3924df921ff71c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:17 +0000

    Add package transiter/0.4.4.dev795/transiter-0.4.4.dev795-py3-none-any.whl

---
+++ packages/transiter/transiter-0.4.4.dev795-py3-none-any.whl/transiter/__metadata__.py
@@ -0,0 +1,15 @@
+# When updating the version, also update it in the docs.
+__version__ = "0.4.4"
+
+# The following are set by Travis CI at build time.
+__build_number__ = None
+__build_timestamp__ = None
+__build_href__ = None
+__git_commit_hash__ = None
+
+# NOTE: the following were set by Travis CI
+__version__ = "0.4.4.dev795"
+__build_number__ = 795
+__build_timestamp__ = 1588132079
+__build_href__ = "https://travis-ci.org/jamespfennell/transiter/jobs/680846400"
+__git_commit_hash__ = "adb48e5dbf0235f96251ec9f51d94f7b578f25e9"
