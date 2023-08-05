41b339f0ee2f2a24982beea70bef8b8e4c0f7e5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:35 +0000

    Add package textchange/0.0.5/textchange-0.0.5.tar.gz

---
+++ packages/textchange/textchange-0.0.5.tar.gz/textchange-0.0.5/setup.py
@@ -0,0 +1,7 @@
+import setuptools
+setuptools.setup(name='textchange',
+      version='0.0.5',
+      author='Yanwentao',
+      author_email='3504802487@qq.com',
+      description='This library can change text.',
+      packages=setuptools.find_packages())
