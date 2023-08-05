e2285484338561d59a88067a21fd6a286dc96498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:54 +0000

    Add package pysy/0.0.20/pysy-0.0.20-py3-none-any.whl

---
+++ packages/pysy/pysy-0.0.20-py3-none-any.whl/pysy/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+__all__ = ["scigeo", "scigee", "fluxlib", "toolbox"]
+
+# from pysy.scigeo import *
+# from pysy.scigee import *
+# from pysy.fluxlib import *
+# from pysy.toolbox import *
