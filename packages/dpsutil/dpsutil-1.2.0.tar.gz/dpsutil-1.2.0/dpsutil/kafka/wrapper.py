b7a6b5a62cff5de2b653482a45a1c778614fae28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:08 +0000

    Add package dpsutil/1.2.0/dpsutil-1.2.0.tar.gz

---
+++ packages/dpsutil/dpsutil-1.2.0.tar.gz/dpsutil-1.2.0/dpsutil/kafka/wrapper.py
@@ -0,0 +1,58 @@
+import ssl
+
+from kafka import KafkaProducer, KafkaConsumer
+
+# TODO | Kafka Confluent. https://github.com/confluentinc/confluent-kafka-python
+
+# Implement it with Optimize Kafka Config Tutorial |
+# https://assets.confluent.io/m/6b6d4f8910691700/original/20190626-WP-Optimizing_Your_Apache_Kafka_Deployment.pdf?_ga=2.212964180.1913133577.1587956580-480563997.1585304727
+
+
+def initial_ssl(username):
+    if username is None:
+        return None, 'PLAINTEXT', None
+
+    sasl_mechanism = 'PLAIN'
+    security_protocol = 'SASL_PLAINTEXT'
+    ssl_context = ssl.create_default_context()
+    ssl_context.options &= ssl.OP_NO_TLSv1
+    ssl_context.options &= ssl.OP_NO_TLSv1_1
+    return sasl_mechanism, security_protocol, ssl_context
+
+
+def initial_consumer(*topic, bootstrap_servers='localhost', group_id=None, auto_offset_reset="earliest",
+                     enable_auto_commit=True, sasl_plain_username=None, sasl_plain_password=None,
+                     consumer_timeout_ms=float('inf'), value_deserializer=None):
+    sasl_mechanism, security_protocol, ssl_context = initial_ssl(sasl_plain_username)
+
+    return KafkaConsumer(
+        *topic,
+        bootstrap_servers=bootstrap_servers,
+        group_id=group_id,
+        auto_offset_reset=auto_offset_reset,
+        enable_auto_commit=enable_auto_commit,
+        sasl_plain_username=sasl_plain_username,
+        sasl_plain_password=sasl_plain_password,
+        security_protocol=security_protocol,
+        ssl_context=ssl_context,
+        sasl_mechanism=sasl_mechanism,
+        value_deserializer=value_deserializer,
+        consumer_timeout_ms=consumer_timeout_ms
+    )
+
+
+def initial_producer(bootstrap_servers='localhost', compression_type="lz4",
+                     sasl_plain_username=None, sasl_plain_password=None, value_serializer=None,
+                     max_request_size=1*1024**2):
+    sasl_mechanism, security_protocol, context = initial_ssl(sasl_plain_username)
+    return KafkaProducer(
+        bootstrap_servers=bootstrap_servers,
+        compression_type=compression_type,
+        sasl_plain_username=sasl_plain_username,
+        sasl_plain_password=sasl_plain_password,
+        security_protocol=security_protocol,
+        ssl_context=context,
+        sasl_mechanism=sasl_mechanism,
+        value_serializer=value_serializer,
+        max_request_size=max_request_size
+    )
