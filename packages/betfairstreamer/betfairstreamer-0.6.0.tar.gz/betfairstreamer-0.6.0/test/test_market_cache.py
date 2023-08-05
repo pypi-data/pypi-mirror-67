9797ce1c2d574de238b1dac2177579252e4418f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:33 +0000

    Add package betfairstreamer/0.6.0/betfairstreamer-0.6.0.tar.gz

---
+++ packages/betfairstreamer/betfairstreamer-0.6.0.tar.gz/betfairstreamer-0.6.0/test/test_market_cache.py
@@ -0,0 +1,22 @@
+import hypothesis.strategies as st
+from hypothesis import given
+
+from betfairstreamer.models.betfair_api import BetfairMarketDefinition
+from test.generators import market_definition, generate_market_definition_from_prev_version
+
+
+@given(st.data(), market_definition())
+def test_market_definition_generator(data, mdf: BetfairMarketDefinition):
+    prev_runner_sort = 0
+
+    md2 = data.draw(generate_market_definition_from_prev_version(mdf))
+
+    assert len(md2["runners"]) == len(mdf["runners"])
+
+    for r in mdf["runners"]:
+        assert r["sortPriority"] == prev_runner_sort + 1
+
+        assert r["id"] is not None
+        assert r["sortPriority"] is not None
+
+        prev_runner_sort = r["sortPriority"]
