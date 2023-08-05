9b81cd839bf0b56d791b91b9c4aeaa4a29bc8c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:13 +0000

    Add package flynt/0.46.1/flynt-0.46.1.tar.gz

---
+++ packages/flynt/flynt-0.46.1.tar.gz/flynt-0.46.1/src/flynt/state.py
@@ -0,0 +1,14 @@
+"""This module keeps global state of flynt application instance."""
+verbose = False
+quiet = False
+
+percent_candidates = 0
+percent_transforms = 0
+
+call_candidates = 0
+call_transforms = 0
+
+invalid_conversions = 0
+
+concat_candidates = 0
+concat_changes = 0
