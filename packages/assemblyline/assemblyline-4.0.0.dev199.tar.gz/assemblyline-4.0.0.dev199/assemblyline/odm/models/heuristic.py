b5dbe80dcb4d0c5339ff18e808a2ea3325f0f903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:08 +0000

    Add package assemblyline/4.0.0.dev199/assemblyline-4.0.0.dev199.tar.gz

---
+++ packages/assemblyline/assemblyline-4.0.0.dev199.tar.gz/assemblyline-4.0.0.dev199/assemblyline/odm/models/heuristic.py
@@ -0,0 +1,17 @@
+from assemblyline import odm
+from assemblyline.common import forge
+from assemblyline.common.attack_map import attack_map
+
+Classification = forge.get_classification()
+PATTERNS = list(attack_map.keys())
+
+
+@odm.model(index=True, store=True)
+class Heuristic(odm.Model):
+    attack_id = odm.Optional(odm.Enum(values=PATTERNS, copyto="__text__"))       # Att&ck matrix pattern
+    classification = odm.Classification(default=Classification.UNRESTRICTED)     # Classification of the heuristic
+    description = odm.Text(copyto="__text__")                                    # Description of the heuristic
+    filetype = odm.Keyword(copyto="__text__")                                    # Type of file targeted
+    heur_id = odm.Keyword(copyto="__text__")                                     # Heuristic ID
+    name = odm.Keyword(copyto="__text__")                                        # Name of the heuristic
+    score = odm.Integer()                                                        # Score of the heuristic
