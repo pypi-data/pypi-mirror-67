a49d1c4e0806cf2a1dcc69a2bf802fe83f437c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:49 +0000

    Add package undmainchain/0.0.11/undmainchain-0.0.11.tar.gz

---
+++ packages/undmainchain/undmainchain-0.0.11.tar.gz/undmainchain-0.0.11/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requires = [
+    'awscli>=1.16.211',
+    'boto3>=1.9.200',
+    'click>=7.1.1',
+    'pytest>=3.5.1',
+    'requests>=2.23.0',
+    'tabulate>=0.8.7'
+]
+
+setuptools.setup(
+    name="undmainchain",
+    packages=['undmainchain'],
+    version="0.0.11",
+    author="Unification Foundation",
+    author_email="indika@unification.com",
+    description="Helper tools for administering the Unification Mainchain",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/unification-com/mainchain-helpers",
+    include_package_data=True,
+    install_requires=requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ packages/undmainchain/undmainchain-0.0.11.tar.gz/undmainchain-0.0.11/undmainchain/const.py
@@ -0,0 +1,58 @@
+import os
+from pathlib import Path
+
+GENESIS = 'https://raw.githubusercontent.com/unification-com/testnet/master' \
+          '/latest/genesis.json'
+
+DEFAULT_BUCKET = 'genesis-export'
+
+COMMON = {
+    'und': '/usr/local/bin/und',
+    'undcli': '/usr/local/bin/undcli',
+    'export_bucket': DEFAULT_BUCKET
+}
+
+MACHINES = {
+    'default': {
+        'service': 'und',
+        'home': Path(os.path.expanduser("~")) / '.und_mainchain',
+        'user': 'root'
+    },
+    'shark': {
+        'service': 'und',
+        'home': Path('/home/deploy/.und_mainchain'),
+        'user': 'deploy'
+    },
+    'node1': {
+        'service': 'node1',
+        'home': Path('/home/deploy/node1/.und_mainchain'),
+        'user': 'deploy'
+    },
+    'node2': {
+        'service': 'node2',
+        'home': Path('/home/deploy/node2/.und_mainchain'),
+        'user': 'deploy'
+    },
+    'node3': {
+        'service': 'node3',
+        'home': Path('/home/deploy/node3/.und_mainchain'),
+        'user': 'deploy'
+    },
+    'seed1': {
+        'service': 'seed1',
+        'home': Path('/home/deploy/seed1/.und_mainchain'),
+        'user': 'deploy'
+    }
+    'sentinel1': {
+        'service': 'sentinel1',
+        'home': Path('/home/deploy/sentinel1/.und_mainchain'),
+        'user': 'deploy'
+    }
+}
+
+
+def get_defaults():
+    d = {}
+    for key, value in MACHINES.items():
+        d[key] = {**value, **COMMON}
+    return d
