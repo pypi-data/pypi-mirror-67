2a9d4f62373140537decd10582d729ba6930c7a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:39 +0000

    Add package cidan/0.1.6/CIDAN-0.1.6.tar.gz

---
+++ packages/cidan/CIDAN-0.1.6.tar.gz/CIDAN-0.1.6/README.md
@@ -0,0 +1,16 @@
+# CIDAN
+
+# Install Instructions
+1. Make sure you have anaconda installed.
+2. Then download the requirements.txt file.
+3. In the same directory where it is located
+run these commands:
+
+```
+conda create -n CIDAN python=3.7
+conda activate CIDAN
+pip install -r requirements.txt
+python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps CIDAN
+python -m CIDAN
+```
+ 
---
+++ packages/cidan/CIDAN-0.1.6.tar.gz/CIDAN-0.1.6/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='CIDAN',
+    version='0.1.6',
+    description='CIDAN-Calcium Imaging Data ANalysis',
+    license="MIT",
+    long_description=long_description,
+    author='Sam Schickler',
+    author_email='sschickl@ucsd.edu',
+    # url="http://www.foopackage.com/",
+    packages=find_packages(),
+    install_requires=["QtPy","QDarkStyle", "Pyside2","numpy", "dask[complete]", "matplotlib","scipy","tiffile","scikit-image","hnswlib", "pillow"],  # TODO add external packages as dependencies
+    scripts=[
+        "CIDAN/LSSC/process_data.py",
+        "CIDAN/LSSC/SpatialBox.py",
+        'CIDAN/LSSC/functions/data_manipulation.py',
+        "CIDAN/LSSC/functions/eigen.py",
+        "CIDAN/LSSC/functions/embeddings.py",
+        "CIDAN/LSSC/functions/pickle_funcs.py",
+        "CIDAN/LSSC/functions/roi_extraction.py",
+        "CIDAN/LSSC/functions/save_test_images.py",
+        "CIDAN/LSSC/functions/temporal_correlation.py",
+        "CIDAN/ConsoleWidget.py",
+        "CIDAN/DataHandler.py",
+        'CIDAN/DataHandlerWrapper.py',
+        'CIDAN/fileHandling.py',
+        'CIDAN/FileOpenTab.py',
+        'CIDAN/ImageViewModule.py',
+        'CIDAN/Input.py',
+        'CIDAN/MainWindow.py',
+        'CIDAN/PreprocessingTab.py',
+        'CIDAN/ROIExtractionTab.py',
+        'CIDAN/ROIItemModule.py',
+        'CIDAN/ROIListModule.py',
+        'CIDAN/roiTools.py',
+        'CIDAN/SettingBlockModule.py',
+        'CIDAN/SettingsModule.py',
+        'CIDAN/Tab.py'
+    ]
+)
