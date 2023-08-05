6d517bbad66a3b9dd663b54bfe22aef7f38b1ef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:24 +0000

    Add package jupyterhub-idle-culler/1.0/jupyterhub-idle-culler-1.0.tar.gz

---
+++ packages/jupyterhub-idle-culler/jupyterhub-idle-culler-1.0.tar.gz/jupyterhub-idle-culler-1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jupyterhub-idle-culler',
+    version='1.0',
+    packages=find_packages(),
+    license='3-BSD',
+    long_description=open('README.rst').read(),
+    entry_points={
+        'console_scripts': [
+            'cull_idle_servers.py = jupyterhub_idle_culler:main',
+            'jupyterhub-idle-culler = jupyterhub_idle_culler:main'
+        ]
+    },
+    install_requires=[
+        'tornado',
+        'python-dateutil'
+    ]
+)
