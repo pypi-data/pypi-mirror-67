80089d0476a4e23461e1c42173e4383ee1517689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:45 +0000

    Add package buildflaskapp/0.2.5.3/buildflaskapp-0.2.5.3.tar.gz

---
+++ packages/buildflaskapp/buildflaskapp-0.2.5.3.tar.gz/buildflaskapp-0.2.5.3/buildflaskapp/scripts/manual.py
@@ -0,0 +1,23 @@
+from tabulate import tabulate
+
+def print_manual():
+    print('Usage: buildflaskapp [app_name] [options]\n')
+
+    print('Usage: buildflaskapp [app_name] -[arguments]\n')
+
+    print(tabulate(
+        [
+            ['-d, --debugger', 'Debugger mode on'],
+            ['-sl3, --sqlite3', 'Sqlite3 mode on'],
+            ['-cj, --css-js', 'Import Stylesheet and script tag'],
+            ['-bs, --bootstrap', 'Import bootstrap'],
+            ['-jq, --jQuery', 'Import jQuery'],
+            ['-gsap, --gsap', 'Import gsap'],
+            ['-fa, --font-awesome', 'Import font awesome'],
+            ['-dc, --docker-container', 'Generate Dockerfile and docker-compose.yml'],
+            ['-h, --help', 'Print help'],
+            ['-v, --version', 'Print version']
+        ],
+        headers=['Options', 'Description']))
+
+    print('Example: build-flask-app hello-world -d -bs\n')
