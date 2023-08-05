b8d6f920575b790ad51065e113628a29ed27e792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:41 +0000

    Add package mfiles/0.4.1/mfiles-0.4.1.tar.gz

---
+++ packages/mfiles/mfiles-0.4.1.tar.gz/mfiles-0.4.1/setup.py
@@ -0,0 +1,33 @@
+# pylint: disable=missing-module-docstring
+
+from setuptools import setup, find_packages
+
+PYPI_DESCRIPTION = """
+Python wrapper around the M-Files API.
+
+Documentation: https://mfiles.readthedocs.io/en/latest/
+"""
+
+setup(
+    name='mfiles',
+    packages=find_packages(),
+    version='0.4.1',
+    license='MIT',
+    description='M-Files API wrapper',
+    long_description=PYPI_DESCRIPTION,
+    author='Emil Hjelm',
+    author_email='emil.hjelm@climeon.com',
+    url='https://github.com/afcmrp/mfiles',
+    keywords=['M-Files', 'mfiles', 'REST', 'API'],
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
