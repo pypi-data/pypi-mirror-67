3205282376e71e3e1862319601a434deb13ebc1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:04 +0000

    Add package liblog/0.0.1/liblog-0.0.1.tar.gz

---
+++ packages/liblog/liblog-0.0.1.tar.gz/liblog-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+	name="liblog",
+	version="0.0.1",
+	author="aztecrabbit",
+	author_email="ars.xda@gmail.com",
+	description="Liblog",
+	long_description="Liblog",
+	long_description_content_type="text/markdown",
+	url="https://github.com/aztecrabbit/python-liblog",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+	entry_points={
+	},
+)
