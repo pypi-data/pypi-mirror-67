5a6994ae5c3ecce3a8fffe3a646b24afa91900a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:04 +0000

    Add package pyatv/0.6.1/pyatv-0.6.1-py3-none-any.whl

---
+++ packages/pyatv/pyatv-0.6.1-py3-none-any.whl/pyatv/__init__.py
@@ -0,0 +1,347 @@
+"""Main routines for interacting with an Apple TV."""
+
+import os
+import asyncio
+import logging
+import datetime  # noqa
+from abc import ABC, abstractmethod
+from ipaddress import IPv4Address, ip_address
+from typing import List, Dict
+
+import aiohttp
+from aiozeroconf import ServiceBrowser, Zeroconf
+import netifaces
+
+from pyatv import conf, exceptions, interface
+from pyatv.airplay import AirPlayStreamAPI
+from pyatv.const import Protocol
+from pyatv.dmap import DmapAppleTV
+from pyatv.dmap.pairing import DmapPairingHandler
+from pyatv.mrp import MrpAppleTV
+from pyatv.mrp.pairing import MrpPairingHandler
+from pyatv.airplay.pairing import AirPlayPairingHandler
+from pyatv.support import net, knock, udns
+
+
+_LOGGER = logging.getLogger(__name__)
+
+HOMESHARING_SERVICE = "_appletv-v2._tcp.local."
+DEVICE_SERVICE = "_touch-able._tcp.local."
+MEDIAREMOTE_SERVICE = "_mediaremotetv._tcp.local."
+AIRPLAY_SERVICE = "_airplay._tcp.local."
+
+ALL_SERVICES = [
+    HOMESHARING_SERVICE,
+    DEVICE_SERVICE,
+    MEDIAREMOTE_SERVICE,
+    AIRPLAY_SERVICE,
+]
+
+# These ports have been "arbitrarily" chosen (see issue #580) because a device normally
+# listen on them (more or less). They are used as best-effort when for unicast scanning
+# to try to wake up a device. Both issue #580 and #595 are good references to read.
+KNOCK_PORTS = [3689, 7000, 49152, 32498]
+
+
+def _decode_properties(properties) -> Dict[str, str]:
+    def _decode(value: bytes):
+        try:
+            # Remove non-breaking-spaces (0xA2A0, 0x00A0) before decoding
+            return (
+                value.replace(b"\xC2\xA0", b" ")
+                .replace(b"\x00\xA0", b" ")
+                .decode("utf-8")
+            )
+        except Exception:  # pylint: disable=broad-except
+            return str(value)
+
+    return {k.decode("utf-8"): _decode(v) for k, v in properties.items()}
+
+
+class BaseScanner(ABC):  # pylint: disable=too-few-public-methods
+    """Base scanner for service discovery."""
+
+    def __init__(self) -> None:
+        """Initialize a new BaseScanner."""
+        self._found_devices = {}  # type: Dict[IPv4Address, conf.BaseService]
+
+    @abstractmethod
+    async def discover(self, timeout: int):
+        """Start discovery of devices and services."""
+
+    def service_discovered(  # pylint: disable=too-many-arguments
+        self, service_type, service_name, address, port, properties
+    ):
+        """Call when a service was discovered."""
+        supported_types = {
+            HOMESHARING_SERVICE: self._hs_service,
+            DEVICE_SERVICE: self._non_hs_service,
+            MEDIAREMOTE_SERVICE: self._mrp_service,
+            AIRPLAY_SERVICE: self._airplay_service,
+        }
+
+        handler = supported_types.get(service_type)
+        if handler:
+            handler(service_name, address, port, properties)
+        else:
+            _LOGGER.warning("Discovered unknown device: %s", service_type)
+
+    def _hs_service(self, service_name, address, port, properties):
+        """Add a new device to discovered list."""
+        identifier = service_name.split(".")[0]
+        name = properties.get("Name")
+        hsgid = properties.get("hG")
+        service = conf.DmapService(identifier, hsgid, port=port, properties=properties)
+        self._handle_service(address, name, service)
+
+    def _non_hs_service(self, service_name, address, port, properties):
+        """Add a new device without Home Sharing to discovered list."""
+        identifier = service_name.split(".")[0]
+        name = properties.get("CtlN")
+        service = conf.DmapService(identifier, None, port=port, properties=properties)
+        self._handle_service(address, name, service)
+
+    def _mrp_service(self, _, address, port, properties):
+        """Add a new MediaRemoteProtocol device to discovered list."""
+        identifier = properties.get("UniqueIdentifier")
+        name = properties.get("Name")
+        service = conf.MrpService(identifier, port, properties=properties)
+        self._handle_service(address, name, service)
+
+    def _airplay_service(self, service_name, address, port, properties):
+        """Add a new AirPlay device to discovered list."""
+        identifier = properties.get("deviceid")
+        name = service_name.replace("." + AIRPLAY_SERVICE, "")
+        service = conf.AirPlayService(identifier, port, properties=properties)
+        self._handle_service(address, name, service)
+
+    def _handle_service(self, address, name, service):
+        if address not in self._found_devices:
+            self._found_devices[address] = conf.AppleTV(address, name)
+
+        _LOGGER.debug(
+            "Auto-discovered %s at %s:%d (%s)",
+            name,
+            address,
+            service.port,
+            service.protocol,
+        )
+
+        atv = self._found_devices[address]
+        atv.add_service(service)
+
+
+class ZeroconfScanner(BaseScanner):
+    """Service discovery based on Zeroconf."""
+
+    def __init__(self, loop):
+        """Initialize a new ZeroconfScanner."""
+        super().__init__()
+        self.loop = loop
+        self.pending = set()
+
+    async def discover(self, timeout):
+        """Start discovery of devices and services."""
+        zeroconf = Zeroconf(self.loop, address_family=[netifaces.AF_INET])
+        browsers = []
+        try:
+            browsers += [
+                ServiceBrowser(zeroconf, HOMESHARING_SERVICE, self),
+                ServiceBrowser(zeroconf, DEVICE_SERVICE, self),
+                ServiceBrowser(zeroconf, MEDIAREMOTE_SERVICE, self),
+                ServiceBrowser(zeroconf, AIRPLAY_SERVICE, self),
+            ]
+            _LOGGER.debug("Discovering devices for %d seconds", timeout)
+            await asyncio.sleep(timeout)
+
+            if self.pending:
+                await asyncio.wait(self.pending)
+        finally:
+            for browser in browsers:
+                browser.cancel()
+            await zeroconf.close()
+        return self._found_devices
+
+    def add_service(self, zeroconf, service_type, name):
+        """Handle callback from zeroconf when a service has been discovered."""
+        self.pending.add(
+            asyncio.ensure_future(self._internal_add(zeroconf, service_type, name))
+        )
+
+    def remove_service(self, zeroconf, service_type, name):
+        """Handle callback when a service is removed."""
+
+    async def _internal_add(self, zeroconf, service_type, name):
+        info = await zeroconf.get_service_info(service_type, name, timeout=2000)
+        if info.address is None:
+            _LOGGER.debug("Failed to resolve %s (%s)", service_type, name)
+            return
+
+        address = ip_address(info.address)
+        self.service_discovered(
+            info.type,
+            info.name,
+            address,
+            info.port,
+            _decode_properties(info.properties),
+        )
+
+
+class UnicastMdnsScanner(BaseScanner):
+    """Service discovery based on unicast MDNS."""
+
+    def __init__(self, hosts: List[IPv4Address], loop: asyncio.AbstractEventLoop):
+        """Initialize a new UnicastMdnsScanner."""
+        super().__init__()
+        self.hosts = hosts
+        self.loop = loop
+
+    async def discover(self, timeout: int):
+        """Start discovery of devices and services."""
+        results = await asyncio.gather(
+            *[self._get_services(host, timeout) for host in self.hosts]
+        )
+        for host, response in results:
+            if response is not None:
+                self._handle_response(host, response)
+        return self._found_devices
+
+    async def _get_services(self, host: IPv4Address, timeout: int):
+        port = int(os.environ.get("PYATV_UDNS_PORT", 5353))  # For testing purposes
+        services = [s[0:-1] for s in ALL_SERVICES]
+        knocker = None
+        try:
+            knocker = await knock.knocker(host, KNOCK_PORTS, self.loop, timeout=timeout)
+            response = await udns.request(
+                self.loop, str(host), services, port=port, timeout=timeout
+            )
+        except asyncio.TimeoutError:
+            response = None
+        finally:
+            if knocker:
+                knocker.cancel()
+        return host, response
+
+    def _handle_response(self, host: IPv4Address, response: udns.DnsMessage):
+        for resource in response.resources:
+            if resource.qtype != udns.QTYPE_TXT:
+                continue
+
+            service_name = ".".join(resource.qname.split(".")[1:]) + "."
+            if service_name not in ALL_SERVICES:
+                continue
+
+            port = UnicastMdnsScanner._get_port(response, resource.qname)
+            if not port:
+                _LOGGER.warning("Missing port for %s", resource.qname)
+                continue
+
+            self.service_discovered(
+                service_name,
+                resource.qname + ".",
+                host,
+                port,
+                _decode_properties(resource.rd),
+            )
+
+    @staticmethod
+    def _get_port(response, qname):
+        for resource in response.resources:
+            if resource.qtype != udns.QTYPE_SRV:
+                continue
+
+            if resource.qname == qname:
+                return resource.rd.get("port")
+
+        return None
+
+
+async def scan(
+    loop: asyncio.AbstractEventLoop,
+    timeout: int = 5,
+    identifier: str = None,
+    protocol: Protocol = None,
+    hosts: List[str] = None,
+) -> List[conf.AppleTV]:
+    """Scan for Apple TVs on network and return their configurations."""
+
+    def _should_include(atv):
+        if not atv.ready:
+            return False
+
+        if identifier and identifier not in atv.all_identifiers:
+            return False
+
+        if protocol and atv.get_service(protocol) is None:
+            return False
+
+        return True
+
+    scanner: BaseScanner
+    if hosts:
+        scanner = UnicastMdnsScanner([IPv4Address(host) for host in hosts], loop)
+    else:
+        scanner = ZeroconfScanner(loop)
+
+    devices = (await scanner.discover(timeout)).values()
+    return [device for device in devices if _should_include(device)]
+
+
+async def connect(
+    config: conf.AppleTV,
+    loop: asyncio.AbstractEventLoop,
+    protocol: Protocol = None,
+    session: aiohttp.ClientSession = None,
+) -> interface.AppleTV:
+    """Connect to a device based on a configuration."""
+    if config.identifier is None:
+        raise exceptions.DeviceIdMissingError("no device identifier")
+
+    service = config.main_service(protocol=protocol)
+
+    implementation = {Protocol.DMAP: DmapAppleTV, Protocol.MRP: MrpAppleTV}.get(
+        service.protocol
+    )
+
+    if not implementation:
+        raise exceptions.UnsupportedProtocolError(str(service.protocol))
+
+    # If no session is given, create a default one
+    if session is None:
+        session = await net.create_session(loop=loop)
+
+    # AirPlay stream API is the same for both DMAP and MRP
+    airplay = AirPlayStreamAPI(config, loop)
+
+    atv = implementation(loop, session, config, airplay)
+    await atv.connect()
+    return atv
+
+
+async def pair(
+    config: conf.AppleTV,
+    protocol: Protocol,
+    loop: asyncio.AbstractEventLoop,
+    session: aiohttp.ClientSession = None,
+    **kwargs
+):
+    """Pair a protocol for an Apple TV."""
+    service = config.get_service(protocol)
+    if not service:
+        raise exceptions.NoServiceError(
+            "no service available for protocol " + str(protocol)
+        )
+
+    handler = {
+        Protocol.DMAP: DmapPairingHandler,
+        Protocol.MRP: MrpPairingHandler,
+        Protocol.AirPlay: AirPlayPairingHandler,
+    }.get(protocol)
+
+    if handler is None:
+        raise exceptions.UnsupportedProtocolError(str(protocol))
+
+    if session is None:
+        session = await net.create_session(loop)
+
+    return handler(config, session, loop, **kwargs)
---
+++ packages/pyatv/pyatv-0.6.1-py3-none-any.whl/pyatv/scripts/atvproxy.py
@@ -0,0 +1,248 @@
+#!/usr/bin/env python3
+#
+# This is a hack to sort-of intercept traffic between an Apple TV and the iOS
+# app. It will establish a connection to the ATV-of-interest (using code from
+# pyatv to do so), publish a fake device called "ATVProxy" that can be paired
+# with the app and forward messages between the two devices. Two sets of
+# encryption keys are used: one set between ATV and this proxy and a second set
+# between this proxy and the app. So all messages are "re-encrypted".
+#
+# What you need is:
+#
+# * Credentials to device of interest (atvremote -a --id <device> pair)
+# * IP-address and port to the Apple TV of interest
+# * IP-address of an interface that is on the same network as the Apple TV
+#
+# Then you just call this script like:
+#
+#   python ./scripts/proxy.py `cat credentials` 10.0.0.20 10.0.10.30 49152
+#
+# Argument order: <credentials> <local ip> <atv ip> <atv port>
+#
+# It shoulfd be possible to pair with your phone using pin "1111". When the
+# proxy receives a connection, it will start by connecting to the Apple TV and
+# then continue with setting up encryption and relaying messages. The same
+# static key pair is hardcoded, so it is possible to reconnect again layer
+# without having to re-pair.
+#
+# Please note that this script is perhaps not a 100% accurate MITM of all
+# traffic. It takes shortcuts and doesn't imitate everything correctly, so some
+# traffic might be missed.
+#
+# Some suggestions for improvements:
+#
+# * Use pyatv to discover device (based on device id) to not have to enter all
+#   details on command line
+# * Base proxy device name on real device (e.g. Bedroom -> Bedroom Proxy)
+# * Re-work logging to make it more clear what is what
+#
+# Help to improve the proxy is greatly appreciated! I will only make
+# improvements in case I personally see any benefits of doing so.
+"""Simple MRP proxy server to intercept traffic."""
+
+import sys
+import socket
+import asyncio
+import logging
+import argparse
+
+from aiozeroconf import Zeroconf, ServiceInfo
+
+from pyatv.conf import MrpService
+from pyatv.mrp.srp import SRPAuthHandler
+from pyatv.mrp.connection import MrpConnection
+from pyatv.mrp.protocol import MrpProtocol
+from pyatv.mrp import chacha20, protobuf, variant
+from pyatv.mrp.server_auth import MrpServerAuth, SERVER_IDENTIFIER
+from pyatv.support import log_binary
+
+_LOGGER = logging.getLogger(__name__)
+
+DEVICE_NAME = "Proxy"
+AIRPLAY_IDENTIFIER = "4D797FD3-3538-427E-A47B-A32FC6CF3A6A"
+
+
+class MrpAppleTVProxy(MrpServerAuth, asyncio.Protocol):
+    """Implementation of a fake MRP Apple TV."""
+
+    def __init__(self, loop):
+        """Initialize a new instance of ProxyMrpAppleTV."""
+        super().__init__(self, DEVICE_NAME)
+        self.loop = loop
+        self.buffer = b""
+        self.transport = None
+        self.chacha = None
+        self.connection = None
+
+    async def start(self, address, port, credentials):
+        """Start the proxy instance."""
+        self.connection = MrpConnection(address, port, self.loop)
+        protocol = MrpProtocol(
+            self.connection,
+            SRPAuthHandler(),
+            MrpService(None, port, credentials=credentials),
+        )
+        await protocol.start(skip_initial_messages=True)
+        self.connection.listener = self
+        self._process_buffer()
+
+    def connection_made(self, transport):
+        """Client did connect to proxy."""
+        self.transport = transport
+
+    def enable_encryption(self, input_key, output_key):
+        """Enable encryption with specified keys."""
+        self.chacha = chacha20.Chacha20Cipher(input_key, output_key)
+
+    def send(self, message):
+        """Send protobuf message to client."""
+        data = message.SerializeToString()
+        _LOGGER.info("<<(DECRYPTED): %s", message)
+        if self.chacha:
+            data = self.chacha.encrypt(data)
+            log_binary(_LOGGER, "<<(ENCRYPTED)", Message=message)
+
+        length = variant.write_variant(len(data))
+        self.transport.write(length + data)
+
+    def send_raw(self, raw):
+        """Send raw data to client."""
+        parsed = protobuf.ProtocolMessage()
+        parsed.ParseFromString(raw)
+
+        log_binary(_LOGGER, "ATV->APP", Raw=raw)
+        _LOGGER.info("ATV->APP Parsed: %s", parsed)
+        if self.chacha:
+            raw = self.chacha.encrypt(raw)
+            log_binary(_LOGGER, "ATV->APP", Encrypted=raw)
+
+        length = variant.write_variant(len(raw))
+        try:
+            self.transport.write(length + raw)
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Failed to send to app")
+
+    def message_received(self, _, raw):
+        """Message received from ATV."""
+        self.send_raw(raw)
+
+    def data_received(self, data):
+        """Message received from iOS app/client."""
+        self.buffer += data
+        if self.connection.connected:
+            self._process_buffer()
+
+    def _process_buffer(self):
+        while self.buffer:
+            length, raw = variant.read_variant(self.buffer)
+            if len(raw) < length:
+                break
+
+            data = raw[:length]
+            self.buffer = raw[length:]
+            if self.chacha:
+                log_binary(_LOGGER, "ENC Phone->ATV", Encrypted=data)
+                data = self.chacha.decrypt(data)
+
+            message = protobuf.ProtocolMessage()
+            message.ParseFromString(data)
+            _LOGGER.info("(DEC Phone->ATV): %s", message)
+
+            try:
+                if message.type == protobuf.DEVICE_INFO_MESSAGE:
+                    self.handle_device_info(message, message.inner())
+                elif message.type == protobuf.CRYPTO_PAIRING_MESSAGE:
+                    self.handle_crypto_pairing(message, message.inner())
+                else:
+                    self.connection.send_raw(data)
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Error while dispatching message")
+
+
+async def publish_zeroconf(zconf, ip_address, port):
+    """Publish zeroconf service for ATV proxy instance."""
+    props = {
+        b"ModelName": "Apple TV",
+        b"AllowPairing": b"YES",
+        b"macAddress": b"40:cb:c0:12:34:56",
+        b"BluetoothAddress": False,
+        b"Name": DEVICE_NAME.encode(),
+        b"UniqueIdentifier": SERVER_IDENTIFIER.encode(),
+        b"SystemBuildVersion": b"17K499",
+        b"LocalAirPlayReceiverPairingIdentity": AIRPLAY_IDENTIFIER.encode(),
+    }
+
+    service = ServiceInfo(
+        "_mediaremotetv._tcp.local.",
+        DEVICE_NAME + "._mediaremotetv._tcp.local.",
+        address=socket.inet_aton(ip_address),
+        port=port,
+        weight=0,
+        priority=0,
+        properties=props,
+    )
+    await zconf.register_service(service)
+    _LOGGER.debug("Published zeroconf service: %s", service)
+
+    return service
+
+
+async def _start_mrp_proxy(loop, args):
+    def proxy_factory():
+        try:
+            proxy = MrpAppleTVProxy(loop)
+            asyncio.ensure_future(
+                proxy.start(args.remote_ip, args.remote_port, args.credentials),
+                loop=loop,
+            )
+        except Exception:
+            _LOGGER.exception("failed to start proxy")
+        return proxy
+
+    zconf = Zeroconf(loop)
+
+    # Setup server used to publish a fake MRP server
+    server = await loop.create_server(proxy_factory, "0.0.0.0")
+    port = server.sockets[0].getsockname()[1]
+    _LOGGER.info("Started MRP server at port %d", port)
+
+    service = await publish_zeroconf(zconf, args.local_ip, port)
+
+    print("Press ENTER to quit")
+    await loop.run_in_executor(None, sys.stdin.readline)
+
+    await zconf.unregister_service(service)
+
+
+async def appstart(loop):
+    """Start the asyncio event loop and runs the application."""
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(title="sub-commands", dest="command")
+    mrp = subparsers.add_parser("mrp", help="MRP proxy")
+    mrp.add_argument("credentials", help="MRP credentials")
+    mrp.add_argument("local_ip", help="local IP address")
+    mrp.add_argument("remote_ip", help="Apple TV IP address")
+    mrp.add_argument("remote_port", help="MRP port")
+
+    args = parser.parse_args()
+    if not args.command:
+        parser.error("No command specified")
+        return 1
+
+    # To get logging from pyatv
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+
+    if args.command == "mrp":
+        await _start_mrp_proxy(loop, args)
+
+    return 0
+
+
+def main():
+    """Application start here."""
+    loop = asyncio.get_event_loop()
+    return loop.run_until_complete(appstart(loop))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
