5bbf190ba34a915f1cc836cebce52709a5663e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:28 +0000

    Add package kawpow/0.9.4.1/kawpow-0.9.4.1.tar.gz

---
+++ packages/kawpow/kawpow-0.9.4.1.tar.gz/kawpow-0.9.4.1/include/kawpow/hash_types.h
@@ -0,0 +1,50 @@
+/* kawpow: C/C++ implementation of Kawpow, the Ravencoin Proof of Work algorithm.
+ * Copyright 2018-2019 Pawel Bylica.
+ * Licensed under the Apache License, Version 2.0.
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+union kawpow_hash256
+{
+    uint64_t word64s[4];
+    uint32_t word32s[8];
+    uint8_t bytes[32];
+    char str[32];
+};
+
+union kawpow_hash512
+{
+    uint64_t word64s[8];
+    uint32_t word32s[16];
+    uint8_t bytes[64];
+    char str[64];
+};
+
+union kawpow_hash1024
+{
+    union kawpow_hash512 hash512s[2];
+    uint64_t word64s[16];
+    uint32_t word32s[32];
+    uint8_t bytes[128];
+    char str[128];
+};
+
+union kawpow_hash2048
+{
+    union kawpow_hash512 hash512s[4];
+    uint64_t word64s[32];
+    uint32_t word32s[64];
+    uint8_t bytes[256];
+    char str[256];
+};
+
+#ifdef __cplusplus
+}
+#endif
