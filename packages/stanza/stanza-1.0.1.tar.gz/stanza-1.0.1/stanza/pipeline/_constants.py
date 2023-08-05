4263d14c264370bd443ac6ca3f64f6409ae152ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:40 +0000

    Add package stanza/1.0.1/stanza-1.0.1.tar.gz

---
+++ packages/stanza/stanza-1.0.1.tar.gz/stanza-1.0.1/stanza/pipeline/_constants.py
@@ -0,0 +1,12 @@
+""" Module defining constants """
+
+# string constants for processor names
+TOKENIZE = 'tokenize'
+MWT = 'mwt'
+POS = 'pos'
+LEMMA = 'lemma'
+DEPPARSE = 'depparse'
+NER = 'ner'
+
+# supported external packages
+SUPPORTED_TOKENIZERS = ['spacy', 'jieba']
