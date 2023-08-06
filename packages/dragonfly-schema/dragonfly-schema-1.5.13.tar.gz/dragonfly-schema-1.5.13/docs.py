4fef2ee279119612b86842761bdd3fec1dea7541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:53 +0000

    Add package dragonfly-schema/1.5.13/dragonfly-schema-1.5.13.tar.gz

---
+++ packages/dragonfly-schema/dragonfly-schema-1.5.13.tar.gz/dragonfly-schema-1.5.13/docs.py
@@ -0,0 +1,76 @@
+"""generate openapi docs."""
+from honeybee_schema._openapi import get_openapi, get_model_mapper
+from dragonfly_schema.model import Model
+
+import json
+import argparse
+from pkg_resources import get_distribution
+
+parser = argparse.ArgumentParser(description='Generate OpenAPI JSON schemas')
+
+parser.add_argument('--version',
+                    help='Set the version of the new OpenAPI Schema')
+
+args = parser.parse_args()
+
+VERSION = None
+
+if args.version:
+    VERSION = args.version.replace('v', '')
+else:
+    VERSION = '.'.join(get_distribution('dragonfly_schema').version.split('.')[:3])
+
+info = {
+        "description": "",
+        "version": VERSION,
+        "title": "",
+        "contact": {
+            "name": "Ladybug Tools",
+            "email": "info@ladybug.tools",
+            "url": "https://github.com/ladybug-tools/dragonfly-core"
+        },
+        "x-logo": {
+            "url": "https://www.ladybug.tools/assets/img/dragonfly-large.png",
+            "altText": "Dragonfly logo"
+        },
+        "license": {
+            "name": "MIT",
+            "url": "https://github.com/ladybug-tools/dragonfly-schema/blob/master/LICENSE"
+        }
+}
+
+# generate Model open api schema
+print('Generating Model documentation...')
+
+external_docs = {
+    "description": "OpenAPI Specification with Inheritance",
+    "url": "./model_inheritance.json"
+}
+
+openapi = get_openapi(
+    [Model],
+    title='Dragonfly Model Schema',
+    description='This is the documentation for Dragonfly model schema.',
+    version=VERSION, info=info,
+    external_docs=external_docs)
+with open('./docs/model.json', 'w') as out_file:
+    json.dump(openapi, out_file, indent=2)
+
+# with inheritance
+openapi = get_openapi(
+    [Model],
+    title='Dragonfly Model Schema',
+    description='This is the documentation for Dragonfly model schema.',
+    version=VERSION, info=info,
+    inheritance=True,
+    external_docs=external_docs
+)
+
+with open('./docs/model_inheritance.json', 'w') as out_file:
+    json.dump(openapi, out_file, indent=2)
+
+# add the mapper file
+mapper = get_model_mapper(Model)
+module_mapper = {k: c.__module__ for k, c in mapper.items()}
+with open('./docs/model_mapper.json', 'w') as out_file:
+    json.dump(module_mapper, out_file, indent=2)
