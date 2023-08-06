b5becb3a0fbc49ab942c1307e4abcb6ceb0fcf64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:50 +0000

    Add package docking-py/0.2.1/docking_py-0.2.1.tar.gz

---
+++ packages/docking-py/docking_py-0.2.1.tar.gz/docking_py-0.2.1/README.rst
@@ -0,0 +1,41 @@
+==============
+Docking Python
+==============
+
+
+.. image:: https://travis-ci.org/samuelmurail/docking_py.svg?branch=master
+    :target: https://travis-ci.org/samuelmurail/docking_py
+
+.. image:: https://readthedocs.org/projects/docking-py/badge/?version=latest
+        :target: https://docking-py.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+.. image:: https://codecov.io/gh/samuelmurail/docking_py/branch/master/graph/badge.svg
+    :target: https://codecov.io/gh/samuelmurail/docking_py
+
+
+
+Docking_py is a python library allowing a simplified use of the Smina, vina, qvina2 and qvinaw docking software. Docking_py can be easily automatize and scripted.
+
+
+* Free software: GNU General Public License v2 (GPLv2)
+* Documentation: https://docking-py.readthedocs.io.
+
+
+Features
+--------
+
+* Prepare receptors and ligands.
+* Launch docking using:
+    * vina
+    * smina
+    * qvina2
+    * qvinaw
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ packages/docking-py/docking_py-0.2.1.tar.gz/docking_py-0.2.1/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['numpy',
+                'os_command_py',
+                'pdb_manip_py',
+                'pdb2pqr-htmd-propka30']
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    version='0.2.1',
+    author="Samuel Murail",
+    author_email='samuel.murail@u-paris.fr',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+    description="Docking_py is a python library allowing a simplified use of the Smina, vina, qvina2 and qvinaw docking software. Docking_py can be easily automatize and scripted.",
+    entry_points={
+        'console_scripts': [
+            'docking_py=docking_py.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="GNU General Public License v2 (GPLv2)",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='docking_py',
+    name='docking_py',
+    packages=find_packages(include=['docking_py', 'docking_py.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/samuelmurail/docking_py',
+    zip_safe=False,
+)
