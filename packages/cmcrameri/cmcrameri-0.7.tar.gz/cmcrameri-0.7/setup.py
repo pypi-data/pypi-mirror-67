391f8dd083a5d1ba30e2a31cf6964513e002aee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:24 +0000

    Add package cmcrameri/0.7/cmcrameri-0.7.tar.gz

---
+++ packages/cmcrameri/cmcrameri-0.7.tar.gz/cmcrameri-0.7/setup.py
@@ -0,0 +1,23 @@
+"""
+setup.py for cmcrameri
+
+"""
+from distutils.core import setup
+setup(
+  name = 'cmcrameri',        
+  packages = ['cmcrameri'],  
+  version = '0.7',  
+  license='MIT',   
+  description = 'Perceptually uniform colourmaps',   
+  author = 'Callum Rollo',         
+  author_email = 'c.rollo@outlook.com',      
+  url = 'https://github.com/callumrollo/cmcrameri',  
+  download_url = 'https://github.com/callumrollo/cmcrameri/v_01.tar.gz',  
+  keywords = ['colormaps', 'oceanography', 'plotting', 'visualization'],
+  install_requires=[         
+          'matplotlib',
+          'numpy',
+      ],
+  package_dir={'mypkg': 'cmcrameri'},     
+  package_data={'cmcrameri': ['cmaps/*.txt']},
+)
