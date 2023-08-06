7329fc332ae8706650e632464ad665f810ea09d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:19 +0000

    Add package wolk-connect/3.3.3/wolk-connect-3.3.3.tar.gz

---
+++ packages/wolk-connect/wolk-connect-3.3.3.tar.gz/wolk-connect-3.3.3/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="wolk-connect",
+    version="3.3.3",
+    install_requires=["paho_mqtt==1.4.0", "python-persistent-queue==1.3.0"],
+    include_package_data=True,
+    license="Apache License 2.0",
+    author="WolkAbout",
+    author_email="info@wolkabout.com",
+    description="Python 3 library for connecting to WolkAbout IoT Platform",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=["IoT", "WolkAbout", "Internet of Things"],
+    url="https://github.com/Wolkabout/WolkConnect-Python",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3 :: Only",
+        "Operating System :: OS Independent",
+        "Topic :: Internet",
+    ),
+)
