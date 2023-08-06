d161086370743abe7c0685e515b5504baee98db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:23 +0000

    Add package cs46-flora-trees/1.0.0/cs46_flora_trees-1.0.0.tar.gz

---
+++ packages/cs46-flora-trees/cs46_flora_trees-1.0.0.tar.gz/cs46_flora_trees-1.0.0/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="cs46_flora_trees",
+    version="1.0.0",
+    description="This is a tree data structure implementation in Python covering Binary Search Trees, AVL Trees and Heaps.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/florazhang98/trees",
+    author="Flora Zhang",
+    author_email="zzhang20@cmc.edm",
+    license="GPLv3",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["Trees"],
+    install_requires=["pytest", "hypothesis"],
+)
