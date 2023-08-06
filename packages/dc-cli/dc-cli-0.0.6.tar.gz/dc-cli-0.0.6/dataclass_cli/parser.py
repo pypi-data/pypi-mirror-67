7a74cb0584eb7eba1103a92f78c63a2579f147ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:03 +0000

    Add package dc-cli/0.0.6/dc-cli-0.0.6.tar.gz

---
+++ packages/dc-cli/dc-cli-0.0.6.tar.gz/dc-cli-0.0.6/dataclass_cli/parser.py
@@ -0,0 +1,60 @@
+import argparse
+import dataclasses
+import enum
+import logging
+from functools import partial
+from typing import Dict, List, Union
+
+
+class Options(str, enum.Enum):
+    POSSIBLE_VALUES = enum.auto()
+    HELP_TEXT = enum.auto()
+
+
+def add(cls=None, *, name=None, **kwargs):
+    if cls is None:
+        return partial(_add, name=name, **kwargs)
+    return _add(cls, name=name, **kwargs)
+
+
+def _add(
+    cls,
+    *,
+    name: str = "",
+    _classes: Dict[str, List[str]] = {},
+    _parser=argparse.ArgumentParser(),
+):
+    assert dataclasses.is_dataclass(cls)
+
+    name = name or cls.__name__.lower()
+    assert name not in _classes
+    _classes[name] = [arg.name for arg in dataclasses.fields(cls)]
+
+    group = _parser.add_argument_group(name)
+    for field in dataclasses.fields(cls):
+        group.add_argument(
+            f"--{name}_{field.name}",
+            type=field.type,
+            default=None if field.default is dataclasses.MISSING else field.default,
+            required=field.default is dataclasses.MISSING,
+            choices=field.metadata.get(Options.POSSIBLE_VALUES, None),
+            help=field.metadata.get(Options.HELP_TEXT, None),
+        )
+
+    original_init = cls.__init__
+
+    def __init__(self, **kwargs):
+        _parsed_args = vars(_parser.parse_args())
+
+        cli_args = [f"{name}_{arg}" for arg in _classes[name]]
+        args = {
+            arg_name: _parsed_args[cli_arg]
+            for arg_name, cli_arg in zip(_classes[name], cli_args)
+        }
+        args.update(kwargs)
+
+        original_init(self, **args)
+
+    cls.__init__ = __init__
+
+    return cls
