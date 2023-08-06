4aa86cbc51fc65342bc2ebb0bd85cb89ea18b7a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:27 +0000

    Add package oli/0.0.17/oli-0.0.17.tar.gz

---
+++ packages/oli/oli-0.0.17.tar.gz/oli-0.0.17/oli/run.py
@@ -0,0 +1,17 @@
+import platform
+import signal
+import sys
+from oli.macos import mac
+
+def signal_handler(signal, frame):
+    print("\nGracefully exiting")
+    sys.exit(0)
+
+def run_as_command():
+    platform_name = platform.system()
+    if platform_name == "Darwin":
+        mac.run_macos()
+        
+
+signal.signal(signal.SIGINT, signal_handler)
+
