b6daf67d3877a29a58790e71ed27abaee9582b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:01 +0000

    Add package pytest-soft-assertions/0.1.2/pytest-soft-assertions-0.1.2.tar.gz

---
+++ packages/pytest-soft-assertions/pytest-soft-assertions-0.1.2.tar.gz/pytest-soft-assertions-0.1.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+requirements = [
+    'pytest',
+    'astor',
+    'texttable',
+    'pytest-ast-transformer>=1.0.3'
+]
+
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='pytest-soft-assertions',
+    version='0.1.2',
+    packages=find_packages(exclude=['examples', 'tests']),
+    entry_points={
+        "pytest11": ["pytest_soft_asserts = pytest_soft_asserts.plugin"],
+    },
+    install_requires=requirements,
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='okorolev',
+    author_email='johnnyprobel@gmail.com',
+    keywords=['pytest', 'ast', 'soft-asserts', 'asserts', 'testing', 'debug'],
+    classifiers=[
+        "Framework :: Pytest",
+        'Programming Language :: Python :: 3',
+        'Development Status :: 3 - Alpha',
+    ],
+    setup_requires=['pytest-runner'],
+    project_urls={
+        'Source': 'https://github.com/okorolev/pytest-soft-assertions',
+    },
+)
