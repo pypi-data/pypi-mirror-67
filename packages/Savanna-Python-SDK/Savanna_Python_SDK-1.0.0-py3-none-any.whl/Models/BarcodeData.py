1fc23e404c46c9173a58a0d8ad2e2f121de5e870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:00 +0000

    Add package Savanna-Python-SDK/1.0.0/Savanna_Python_SDK-1.0.0-py3-none-any.whl

---
+++ packages/Savanna-Python-SDK/Savanna_Python_SDK-1.0.0-py3-none-any.whl/Models/BarcodeData.py
@@ -0,0 +1,38 @@
+"""
+Response object for UPC lookup
+"""
+class BarcodeData:
+
+    def __init__(self, code, total, offset, items):
+        self.code = code
+        self.total = total
+        self.offset = offset
+        self.items = items
+    
+    """
+    The status code returned by the API.
+    """
+    @property
+    def code(self):
+        return self.__code
+
+    """
+    The total number of records matching the search criteria.
+    """
+    @property
+    def total(self):
+        return self.__total
+    
+    """
+    The Offset (page) of results.
+    """
+    @property
+    def offset(self):
+        return self.__offset
+    
+    """
+    Collection of items matching the UPC lookup query.
+    """
+    @property
+    def items(self):
+        return self.__item
