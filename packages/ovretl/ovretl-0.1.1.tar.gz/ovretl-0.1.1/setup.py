9d931fd7f46597634d958942c634422db4af1733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:34 +0000

    Add package ovretl/0.1.1/ovretl-0.1.1.tar.gz

---
+++ packages/ovretl/ovretl-0.1.1.tar.gz/ovretl-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ovretl', 'ovretl.prices_utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pandas>=1.0.3,<2.0.0']
+
+setup_kwargs = {
+    'name': 'ovretl',
+    'version': '0.1.1',
+    'description': 'Python package for Ovrsea ETL',
+    'long_description': '',
+    'author': 'nicolas67',
+    'author_email': 'nicolas@ovrsea.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.1',
+}
+
+
+setup(**setup_kwargs)
