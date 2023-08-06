ede5a7cc36f7103eff1d45cca72e08f9c284c4d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:34 +0000

    Add package pymotiva/0.1.1/pymotiva-0.1.1.tar.gz

---
+++ packages/pymotiva/pymotiva-0.1.1.tar.gz/pymotiva-0.1.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pymotiva',
+    version = '0.1.1',
+    license = 'MIT',
+    description = 'Python library for Emotiva XMC-1',
+    author = 'Dima Zavin',
+    author_email = 'thecynic@gmail.com',
+    url = 'http://github.com/thecynic/pymotiva',
+    packages=find_packages(),
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=[],
+    zip_safe=True,
+)
