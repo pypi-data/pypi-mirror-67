105510057d7db3e815dcd3c3592e88f9fdd5b872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:37 +0000

    Add package axdscfg/1.0.1/axdscfg-1.0.1-py3-none-any.whl

---
+++ packages/axdscfg/axdscfg-1.0.1-py3-none-any.whl/axdscfg/__init__.py
@@ -0,0 +1,19 @@
+
+from axdscfg.axdscfg import main
+
+__all__ = [
+    "__title__", "__summary__", "__version__", "__author__",
+    "__email__", "__license__",
+]
+
+__title__ = "axdscfg"
+__summary__ = ("Python lib to get credentials from encrypted file.")
+
+__version__ = "1.0.1"
+
+__uri__ = "https://pypi.org/project/axdscfg"
+
+__author__ = "Axiomas Data Science"
+__email__ = "suporte@axiomas.tech"
+
+__license__ = "GNU General Public License v3 (GPLv3)"
