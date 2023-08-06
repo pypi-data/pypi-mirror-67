2a64850b154dbc303878b744ce5db07b287179a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:02 +0000

    Add package itk-iomeshstl/2.1.0/itk_iomeshstl-2.1.0-cp37-cp37m-win_amd64.whl

---
+++ packages/itk-iomeshstl/itk_iomeshstl-2.1.0-cp37-cp37m-win_amd64.whl/itk/Configuration/IOMeshSTLConfig.py
@@ -0,0 +1,6 @@
+depends = ('ITKPyBase', 'ITKIOMeshBase', 'ITKCommon', )
+templates = (
+  ('STLMeshIO', 'itk::STLMeshIO', 'itkSTLMeshIO', True),
+  ('STLMeshIOFactory', 'itk::STLMeshIOFactory', 'itkSTLMeshIOFactory', True),
+)
+snake_case_functions = ()
