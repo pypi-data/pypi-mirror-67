d8d106585c5b707b9a7c65899a44fd56ca034eb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:12 +0000

    Add package synthpops/0.1.0/synthpops-0.1.0-py3-none-any.whl

---
+++ packages/synthpops/synthpops-0.1.0-py3-none-any.whl/synthpops/__init__.py
@@ -0,0 +1,12 @@
+msg = 'SynthPops is currently being prepared for public release. This version is not currently operation. The full version will be published on pypi, and its source code released on GitHub, as soon as possible. For more information, please email covid@idmod.org.'
+raise NotImplementedError(msg)
+
+# from .config import *
+# from .base import *
+# from .data_distributions import *
+# from .sampling import *
+# from .contacts import *
+# from .contact_networks import *
+# from .contact_networks_industries import *
+# from .plot_tools import *
+# from .api import *
