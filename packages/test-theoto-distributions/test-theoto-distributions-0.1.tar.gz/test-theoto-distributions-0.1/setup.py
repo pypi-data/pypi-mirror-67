d7702e9d2cf9d079ced5fa1789e5cd812090cf5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:32 +0000

    Add package test-theoto-distributions/0.1/test-theoto-distributions-0.1.tar.gz

---
+++ packages/test-theoto-distributions/test-theoto-distributions-0.1.tar.gz/test-theoto-distributions-0.1/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  7 09:36:38 2020
+
+@author: Tati
+"""
+
+from setuptools import setup
+
+setup(name='test-theoto-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      author = 'Tatiana Theoto',
+      packages=['test-theoto-distributions'],
+      zip_safe=False)
