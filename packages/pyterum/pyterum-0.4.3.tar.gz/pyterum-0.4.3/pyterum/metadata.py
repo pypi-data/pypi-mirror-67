3060da782c15c1223487cf21d5cc6b93363884d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:55 +0000

    Add package pyterum/0.4.3/pyterum-0.4.3.tar.gz

---
+++ packages/pyterum/pyterum-0.4.3.tar.gz/pyterum-0.4.3/pyterum/metadata.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+from typing import List
+
+class Metadata:
+    def __init__(self, output_channel:str=None):
+        self.output_channel = output_channel
+    
+    def __str__(self):
+        return str(self.__dict__)
+
+    def to_json(self) -> dict:
+        return self.__dict__
+
+    @classmethod
+    def from_json(cls, d:dict) -> cls:
+        if not isinstance(d, dict):
+            raise TypeError("Argument 'd' is not of type 'dict'")
+        result = cls()
+        for key in d.keys():
+            result.__setattr__(key, d[key])
+
+        return result
