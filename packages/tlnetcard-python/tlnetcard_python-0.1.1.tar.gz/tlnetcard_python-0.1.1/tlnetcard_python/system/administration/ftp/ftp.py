582612b219456227c6e1ad097ebf961180a66190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:19 +0000

    Add package tlnetcard-python/0.1.1/tlnetcard_python-0.1.1.tar.gz

---
+++ packages/tlnetcard-python/tlnetcard_python-0.1.1.tar.gz/tlnetcard_python-0.1.1/tlnetcard_python/system/administration/ftp/ftp.py
@@ -0,0 +1,9 @@
+# ftp.py
+# Ethan Guthrie
+# DATE TBD
+""" Allows FTP settings to be configured. """
+
+class Ftp:
+    """ Class for the Ftp object. """
+    def __init__(self):
+        """ Initialize Ftp object. """
