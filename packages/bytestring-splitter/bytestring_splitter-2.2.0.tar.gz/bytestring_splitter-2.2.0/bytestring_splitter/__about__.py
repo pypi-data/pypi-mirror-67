739c8962ee1818026d8d7ddaca301f7ad9318250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:57 +0000

    Add package bytestring-splitter/2.2.0/bytestring_splitter-2.2.0.tar.gz

---
+++ packages/bytestring-splitter/bytestring_splitter-2.2.0.tar.gz/bytestring_splitter-2.2.0/bytestring_splitter/__about__.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import, division, print_function
+
+__all__ = [
+    "__title__", "__summary__", "__version__", "__author__", "__license__",
+]
+
+__title__ = "bytestring_splitter"
+
+__url__ = "https://github.com/nucypher/bytestringSplitter"
+
+__summary__ = 'Among the simplest imaginable ways to serialize: concatenate and split.'
+
+__version__ = "2.2.0"
+
+__author__ = "NuCypher"
+
+__email__ = "dev@nucypher.com"
+
+__license__ = "Apache Software License v2.0"
+
+__copyright__ = 'Copyright (C) 2018 NuCypher'
+
