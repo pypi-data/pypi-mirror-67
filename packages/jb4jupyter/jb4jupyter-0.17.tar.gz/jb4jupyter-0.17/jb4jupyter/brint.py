17391b494a4330c5ca94c33a3aeb0a19ff1732f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:59 +0000

    Add package jb4jupyter/0.17/jb4jupyter-0.17.tar.gz

---
+++ packages/jb4jupyter/jb4jupyter-0.17.tar.gz/jb4jupyter-0.17/jb4jupyter/brint.py
@@ -0,0 +1,20 @@
+from IPython.display import display, Latex
+
+def latexdisplay(varname,varval,roundto=3):
+    return display(Latex('$' + varname + '= $' + str(round(varval,roundto))))
+
+def latexdisplayunits(varname,varval,units,roundto=3):
+    return display(Latex('$' + varname + '= $' + str(round(varval,roundto)) + ' $' + units + '$'))
+
+def variablename(var):
+    import itertools
+    return str(([tpl[0] for tpl in filter(lambda x: var is x[1], globals().items())])[0])
+
+def brint(var,units='',roundto=3):
+    if units == '':
+        if var == str(var):
+            return display(Latex(var))
+        else:
+            return latexdisplay(variablename(var),var,roundto)
+    else:
+        return latexdisplayunits(variablename(var),var,units,roundto)
---
+++ packages/jb4jupyter/jb4jupyter-0.17.tar.gz/jb4jupyter-0.17/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='jb4jupyter',
+	version='0.17',
+	description='jupyter bettifier by @jb4earth',
+	url='http://gitlab.com/jb4earth/jb4jupyter',
+	author='@jb4earth',
+	author_email='jb4earth@gmail.com',
+	license='GPL',
+	packages=find_packages(),
+	long_description='Out of this world beautiful plotting and reports for jupyter, with dark mode ;)',
+    long_description_content_type="text/markdown"
+	)
