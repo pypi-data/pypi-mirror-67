47b7c86b93456351a6c54d6734f6f085bfc40aa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:07 +0000

    Add package matflow/0.1.1/matflow-0.1.1.tar.gz

---
+++ packages/matflow/matflow-0.1.1.tar.gz/matflow-0.1.1/matflow/models/__init__.py
@@ -0,0 +1,5 @@
+"""`matflow.models.__init__.py`"""
+
+from matflow.models.command import Command, CommandGroup
+from matflow.models.task import Task, TaskSchema
+from matflow.models.workflow import Workflow
