815a1bfa5654c446bd6d47d793b23734b8150462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:27 +0000

    Add package dsnd-probability-cbarr/1.0/dsnd_probability_cbarr-1.0.tar.gz

---
+++ packages/dsnd-probability-cbarr/dsnd_probability_cbarr-1.0.tar.gz/dsnd_probability_cbarr-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_cbarr',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_cbarr'],
+      author = 'Cindy Barrientos',
+      author_email = 'cbarrien@ucsd.edu',
+      zip_safe=False)
