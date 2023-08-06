ba055d70046fdbb48cd8d76d19554f0f2799ea8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:43 +0000

    Add package lsf-runner/0.0.2/lsf_runner-0.0.2-py3-none-any.whl

---
+++ packages/lsf-runner/lsf_runner-0.0.2-py3-none-any.whl/tests/test_runner.py
@@ -0,0 +1,74 @@
+from lsf_runner import init_runner, make_commands
+from lsf_runner.runners import IBMRunner
+import pytest
+import sys
+import itertools
+
+
+def test_make_commands():
+    script = 'tests/script.py'
+
+    cmds = make_commands(
+        script, base_args={'threads': 2},
+        fixed_hyper_args={'lr': 0.1, 'print': True, 'layers': [64, 128]},
+        common_hyper_args={'seed': [0, 1, 2]},
+        algorithm_hyper_args={'wd': [0.01, 0.1]}
+    )
+
+    assert cmds == [
+        '{} {} --threads 2 --lr 0.1 --print --layers 64 128 --seed {} --wd {}'.format(
+            sys.executable, script, seed, wd) for seed, wd in itertools.product(
+            [0, 1, 2], [0.01, 0.1])]
+
+
+def test_runner():
+    script = 'tests/script.py'
+
+    cmds = make_commands(
+        script, base_args={'threads': 2}, fixed_hyper_args={'lr': 0.1, 'print': True},
+        common_hyper_args={'seed': [0, 1, 2]},
+        algorithm_hyper_args={'wd': [0.01, 0.1]}
+    )
+
+    runner = init_runner('test')
+    runner.run(cmds)
+    runner.run_batch(cmds)
+
+
+@pytest.fixture(params=[1, 4])
+def num_threads(request):
+    return request.param
+
+
+@pytest.fixture(params=[True, False])
+def use_gpu(request):
+    return request.param
+
+
+@pytest.fixture(params=[None, 720])
+def wall_time(request):
+    return request.param
+
+
+@pytest.fixture(params=[None, 4096])
+def memory(request):
+    return request.param
+
+
+def test_ibm(num_threads, use_gpu, wall_time, memory):
+    runner = IBMRunner(num_threads=num_threads, use_gpu=use_gpu, wall_time=wall_time,
+                       memory=memory)
+
+    base_cmd = runner._build_base_cmd()
+
+    cmd = "bsub "
+    if wall_time:
+        cmd += '-W {} '.format(wall_time)
+    if memory:
+        cmd += '-R "rusage[mem={}]" '.format(memory)
+    if use_gpu:
+        cmd += '-R "rusage[ngpus_excl_p=1]" '
+
+    cmd += '-n {} '.format(num_threads)
+
+    assert cmd == base_cmd
