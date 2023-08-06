e00ab7adc3229c04c6cb52ca554c51a12efdce94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:18 +0000

    Add package chenliang/1.0.0/chenliang-1.0.0.tar.gz

---
+++ packages/chenliang/chenliang-1.0.0.tar.gz/chenliang-1.0.0/setup.py
@@ -0,0 +1,2 @@
+import setuptools as tt
+tt.setup(name='chenliang',version='1.0.0',description='chenchenliangliang',packages=['chenliang'],py_modules=['tools'],author='cl')
---
+++ packages/chenliang/chenliang-1.0.0.tar.gz/chenliang-1.0.0/tools.py
@@ -0,0 +1,2 @@
+import chenliang.s1 as ss
+ss.run()
