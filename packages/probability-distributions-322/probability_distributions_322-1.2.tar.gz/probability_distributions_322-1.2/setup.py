ff84a16eb41977819af391e77cef926d038ef821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:00 +0000

    Add package probability-distributions-322/1.2/probability_distributions_322-1.2.tar.gz

---
+++ packages/probability-distributions-322/probability_distributions_322-1.2.tar.gz/probability_distributions_322-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_distributions_322',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['probability_distributions_322'],
+      author='Muhammad Qasim Khan',
+      author_email='qasimkhan3.124@gmail.com',
+      zip_safe=False)
