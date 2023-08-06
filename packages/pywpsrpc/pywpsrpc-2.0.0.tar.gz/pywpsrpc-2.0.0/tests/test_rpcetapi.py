4d5714c0ca98f115b63b46202efd1f776afed828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:42 +0000

    Add package pywpsrpc/2.0.0/pywpsrpc-2.0.0.tar.gz

---
+++ packages/pywpsrpc/pywpsrpc-2.0.0.tar.gz/pywpsrpc-2.0.0/tests/test_rpcetapi.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/../build")
+
+from pywpsrpc.rpcetapi import etapi
+from pywpsrpc import (rpcetapi, common)
+
+
+def check_call(funcName, hr, value=None):
+    if common.FAILED(hr):
+        print("call {} failed with code: {}".format(funcName, hr))
+        sys.exit(-1)
+    if value != None:
+        print("{}: {}".format(funcName, value))
+    else:
+        print("{}: <ok>".format(funcName))
+
+
+def test():
+    hr, rpc = rpcetapi.createEtRpcInstance()
+    check_call("rpcetapi.createEtRpcInstance", hr, rpc)
+
+    hr, app = rpc.getEtApplication()
+    check_call("rpc.getEtApplication", hr, app)
+
+    hr, pid = rpc.getProcessPid()
+    check_call("rpc.getProcessPid", hr, pid)
+
+    hr, workbooks = app.get_Workbooks()
+    check_call("app.get_Workbooks", hr ,workbooks)
+
+    hr, workbook = workbooks.Add()
+    check_call("workbooks.Add", hr, workbook)
+
+    hr = workbook.Close()
+    check_call("workbook.Close", hr)
+
+
+    app.Quit()
+
+
+if __name__ == "__main__":
+    test()
---
+++ packages/pywpsrpc/pywpsrpc-2.0.0.tar.gz/pywpsrpc-2.0.0/tests/test_rpcwppapi.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/../build")
+
+from pywpsrpc.rpcwppapi import wppapi
+from pywpsrpc import (rpcwppapi, common)
+
+
+def check_call(funcName, hr, value=None):
+    if not common.SUCCEEDED(hr):
+        print("call {} failed with code: {}".format(funcName, hr))
+        sys.exit(-1)
+    if value != None:
+        print("{}: {}".format(funcName, value))
+    else:
+        print("{}: <ok>".format(funcName))
+
+
+def test():
+    hr, rpc = rpcwppapi.createWppRpcInstance()
+    check_call("rpcwppapi.createWppRpcInstance", hr, rpc)
+
+    hr, app = rpc.getWppApplication()
+    check_call("rpc.getWppApplication", hr, app)
+
+    hr, pid = rpc.getProcessPid()
+    check_call("rpc.getProcessPid", hr, pid)
+
+    hr, preses = app.get_Presentations()
+    check_call("app.get_Presentations", hr, preses)
+
+    hr, pres = preses.Add(wppapi.msoTrue)
+    check_call("preses.Add", hr, pres)
+
+    hr, pres = app.get_ActivePresentation()
+    check_call("app.get_ActivePresentation", hr, pres)
+
+    hr, name = pres.get_FullName()
+    check_call("pres.get_FullName", hr, name)
+
+    hr = pres.Close()
+    check_call("pres.Close", hr)
+
+    hr = app.Quit()
+    check_call("ap.Quit", hr)
+
+
+if __name__ == "__main__":
+    test()
