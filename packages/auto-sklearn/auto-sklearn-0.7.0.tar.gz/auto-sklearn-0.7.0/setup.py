1acea6a580d5373f6d256ee0720d87ba79a00c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:01 +0000

    Add package auto-sklearn/0.7.0/auto-sklearn-0.7.0.tar.gz

---
+++ packages/auto-sklearn/auto-sklearn-0.7.0.tar.gz/auto-sklearn-0.7.0/setup.py
@@ -0,0 +1,94 @@
+# -*- encoding: utf-8 -*-
+import os
+import sys
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+from setuptools.command.build_ext import build_ext
+
+
+# Check if Auto-sklearn *could* run on the given system
+if os.name != 'posix':
+    raise ValueError(
+        'Detected unsupported operating system: %s. Please check '
+        'the compability information of auto-sklearn: http://automl.github.io'
+        '/auto-sklearn/stable/installation.html#windows-osx-compability' %
+        sys.platform
+    )
+
+if sys.version_info < (3, 5):
+    raise ValueError(
+        'Unsupported Python version %d.%d.%d found. Auto-sklearn requires Python '
+        '3.5 or higher.' % (sys.version_info.major, sys.version_info.minor, sys.version_info.micro)
+    )
+
+
+class BuildExt(build_ext):
+    """ build_ext command for use when numpy headers are needed.
+    SEE tutorial: https://stackoverflow.com/questions/2379898
+    SEE fix: https://stackoverflow.com/questions/19919905
+    """
+
+    def finalize_options(self):
+        build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        # __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+
+extensions = [
+    Extension('autosklearn.data.competition_c_functions',
+              sources=['autosklearn/data/competition_c_functions.pyx'],
+              language='c')
+]
+
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+setup_reqs = ['Cython', 'numpy']
+with open(os.path.join(HERE, 'requirements.txt')) as fp:
+    install_reqs = [r.rstrip() for r in fp.readlines()
+                    if not r.startswith('#') and not r.startswith('git+')]
+
+with open("autosklearn/__version__.py") as fh:
+    version = fh.readlines()[-1].split()[-1].strip("\"'")
+
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='auto-sklearn',
+    author='Matthias Feurer',
+    author_email='feurerm@informatik.uni-freiburg.de',
+    description='Automated machine learning.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version=version,
+    cmdclass={'build_ext': BuildExt},
+    ext_modules=extensions,
+    packages=find_packages(exclude=['test', 'scripts', 'examples']),
+    setup_requires=setup_reqs,
+    install_requires=install_reqs,
+    include_package_data=True,
+    license='BSD',
+    platforms=['Linux'],
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.5.*',
+    url='https://automl.github.io/auto-sklearn',
+)
---
+++ packages/auto-sklearn/auto-sklearn-0.7.0.tar.gz/auto-sklearn-0.7.0/test/test_automl/test_automl.py
@@ -0,0 +1,395 @@
+# -*- encoding: utf-8 -*-
+import os
+import pickle
+import sys
+import time
+import glob
+import unittest
+import unittest.mock
+
+import numpy as np
+import sklearn.datasets
+from smac.scenario.scenario import Scenario
+from smac.facade.roar_facade import ROAR
+
+from autosklearn.util.backend import Backend
+from autosklearn.automl import AutoML
+import autosklearn.automl
+from autosklearn.metrics import accuracy
+import autosklearn.pipeline.util as putil
+from autosklearn.util.logging_ import setup_logger, get_logger
+from autosklearn.constants import MULTICLASS_CLASSIFICATION, BINARY_CLASSIFICATION
+from autosklearn.smbo import load_data
+from smac.tae.execute_ta_run import StatusType
+
+sys.path.append(os.path.dirname(__file__))
+from base import Base  # noqa (E402: module level import not at top of file)
+
+
+class AutoMLStub(AutoML):
+    def __init__(self):
+        self.__class__ = AutoML
+
+
+class AutoMLTest(Base, unittest.TestCase):
+    _multiprocess_can_split_ = True
+
+    def setUp(self):
+        super().setUp()
+
+        self.automl = AutoMLStub()
+
+        self.automl._shared_mode = False
+        self.automl._seed = 42
+        self.automl._backend = unittest.mock.Mock(spec=Backend)
+        self.automl._delete_output_directories = lambda: 0
+
+    def test_refit_shuffle_on_fail(self):
+        backend_api = self._create_backend('test_refit_shuffle_on_fail')
+
+        failing_model = unittest.mock.Mock()
+        failing_model.fit.side_effect = [ValueError(), ValueError(), None]
+        failing_model.fit_transformer.side_effect = [
+            ValueError(), ValueError(), (None, {})]
+        failing_model.get_max_iter.return_value = 100
+
+        auto = AutoML(backend_api, 20, 5)
+        ensemble_mock = unittest.mock.Mock()
+        ensemble_mock.get_selected_model_identifiers.return_value = [(1, 1, 50.0)]
+        auto.ensemble_ = ensemble_mock
+        for budget_type in [None, 'iterations']:
+            auto._budget_type = budget_type
+
+            auto.models_ = {(1, 1, 50.0): failing_model}
+
+            X = np.array([1, 2, 3])
+            y = np.array([1, 2, 3])
+            auto.refit(X, y)
+
+            self.assertEqual(failing_model.fit.call_count, 3)
+        self.assertEqual(failing_model.fit_transformer.call_count, 3)
+
+        del auto
+        self._tearDown(backend_api.temporary_directory)
+        self._tearDown(backend_api.output_directory)
+
+    def test_only_loads_ensemble_models(self):
+        identifiers = [(1, 2), (3, 4)]
+
+        models = [42]
+        self.automl._backend.load_ensemble.return_value.identifiers_ \
+            = identifiers
+        self.automl._backend.load_models_by_identifiers.side_effect \
+            = lambda ids: models if ids is identifiers else None
+
+        self.automl._load_models()
+
+        self.assertEqual(models, self.automl.models_)
+
+    def test_check_for_models_if_no_ensemble(self):
+        models = [42]
+        self.automl._backend.load_ensemble.return_value = None
+        self.automl._backend.list_all_models.return_value = models
+        self.automl._disable_evaluator_output = False
+
+        self.automl._load_models()
+
+    def test_raises_if_no_models(self):
+        self.automl._backend.load_ensemble.return_value = None
+        self.automl._backend.list_all_models.return_value = []
+        self.automl._resampling_strategy = 'holdout'
+
+        self.automl._disable_evaluator_output = False
+        self.assertRaises(ValueError, self.automl._load_models)
+
+        self.automl._disable_evaluator_output = True
+        self.automl._load_models()
+
+    def test_fit(self):
+        backend_api = self._create_backend('test_fit')
+
+        X_train, Y_train, X_test, Y_test = putil.get_dataset('iris')
+        automl = autosklearn.automl.AutoML(backend_api, 20, 5)
+        automl.fit(
+            X_train, Y_train, metric=accuracy, task=MULTICLASS_CLASSIFICATION,
+        )
+        score = automl.score(X_test, Y_test)
+        self.assertGreaterEqual(score, 0.8)
+        self.assertEqual(automl._task, MULTICLASS_CLASSIFICATION)
+
+        del automl
+        self._tearDown(backend_api.temporary_directory)
+        self._tearDown(backend_api.output_directory)
+
+    def test_delete_non_candidate_models(self):
+        backend_api = self._create_backend(
+            'test_delete', delete_tmp_folder_after_terminate=False)
+
+        seed = 555
+        X, Y, _, _ = putil.get_dataset('iris')
+        automl = autosklearn.automl.AutoML(
+            backend_api,
+            time_left_for_this_task=30,
+            per_run_time_limit=5,
+            ensemble_nbest=3,
+            seed=seed,
+            initial_configurations_via_metalearning=0,
+            resampling_strategy='holdout',
+            include_estimators=['sgd'],
+            include_preprocessors=['no_preprocessing']
+        )
+
+        automl.fit(X, Y, metric=accuracy, task=MULTICLASS_CLASSIFICATION,
+                   X_test=X, y_test=Y)
+
+        # Assert at least one model file has been deleted and that there were no
+        # deletion errors
+        log_file_path = glob.glob(os.path.join(
+            backend_api.temporary_directory, 'AutoML(' + str(seed) + '):*.log'))
+        with open(log_file_path[0]) as log_file:
+            log_content = log_file.read()
+            self.assertIn('Deleted files of non-candidate model', log_content)
+            self.assertNotIn('Failed to delete files of non-candidate model', log_content)
+            self.assertNotIn('Failed to lock model', log_content)
+
+        # Assert that the files of the models used by the ensemble weren't deleted
+        model_files = backend_api.list_all_models(seed=seed)
+        model_files_idx = set()
+        for m_file in model_files:
+            # Extract the model identifiers from the filename
+            m_file = os.path.split(m_file)[1].replace('.model', '').split('.', 2)
+            model_files_idx.add((int(m_file[0]), int(m_file[1]), float(m_file[2])))
+        ensemble_members_idx = set(automl.ensemble_.identifiers_)
+        self.assertTrue(ensemble_members_idx.issubset(model_files_idx))
+
+        del automl
+        self._tearDown(backend_api.temporary_directory)
+        self._tearDown(backend_api.output_directory)
+
+    def test_fit_roar(self):
+        def get_roar_object_callback(
+                scenario_dict,
+                seed,
+                ta,
+                ta_kwargs,
+                **kwargs
+        ):
+            """Random online adaptive racing.
+
+            http://ml.informatik.uni-freiburg.de/papers/11-LION5-SMAC.pdf"""
+            scenario = Scenario(scenario_dict)
+            return ROAR(
+                scenario=scenario,
+                rng=seed,
+                tae_runner=ta,
+                tae_runner_kwargs=ta_kwargs,
+            )
+
+        backend_api = self._create_backend('test_fit_roar')
+
+        X_train, Y_train, X_test, Y_test = putil.get_dataset('iris')
+        automl = autosklearn.automl.AutoML(
+            backend=backend_api,
+            time_left_for_this_task=20,
+            per_run_time_limit=5,
+            initial_configurations_via_metalearning=0,
+            get_smac_object_callback=get_roar_object_callback,
+        )
+        automl.fit(
+            X_train, Y_train, metric=accuracy, task=MULTICLASS_CLASSIFICATION,
+        )
+        score = automl.score(X_test, Y_test)
+        self.assertGreaterEqual(score, 0.8)
+        self.assertEqual(automl._task, MULTICLASS_CLASSIFICATION)
+
+        del automl
+        self._tearDown(backend_api.temporary_directory)
+        self._tearDown(backend_api.output_directory)
+
+    def test_binary_score_and_include(self):
+        """
+        Test fix for binary classification prediction
+        taking the index 1 of second dimension in prediction matrix
+        """
+        backend_api = self._create_backend('test_binary_score_and_include')
+
+        data = sklearn.datasets.make_classification(
+            n_samples=400, n_features=10, n_redundant=1, n_informative=3,
+            n_repeated=1, n_clusters_per_class=2, random_state=1)
+        X_train = data[0][:200]
+        Y_train = data[1][:200]
+        X_test = data[0][200:]
+        Y_test = data[1][200:]
+
+        automl = autosklearn.automl.AutoML(backend_api, 20, 5,
+                                           include_estimators=['sgd'],
+                                           include_preprocessors=['no_preprocessing'])
+        automl.fit(X_train, Y_train, task=BINARY_CLASSIFICATION,
+                   metric=accuracy)
+        self.assertEqual(automl._task, BINARY_CLASSIFICATION)
+
+        # TODO, the assumption from above is not really tested here
+        # Also, the score method should be removed, it only makes little sense
+        score = automl.score(X_test, Y_test)
+        self.assertGreaterEqual(score, 0.4)
+
+        del automl
+        self._tearDown(backend_api.temporary_directory)
+        self._tearDown(backend_api.output_directory)
+
+    def test_automl_outputs(self):
+        backend_api = self._create_backend('test_automl_outputs')
+
+        name = '31_bac'
+        dataset = os.path.join(self.test_dir, '..', '.data', name)
+        data_manager_file = os.path.join(backend_api.temporary_directory, '.auto-sklearn',
+                                         'datamanager.pkl')
+
+        auto = autosklearn.automl.AutoML(
+            backend_api, 20, 5,
+            initial_configurations_via_metalearning=0,
+            seed=100,
+        )
+        auto.fit_automl_dataset(dataset, accuracy)
+
+        # pickled data manager (without one hot encoding!)
+        with open(data_manager_file, 'rb') as fh:
+            D = pickle.load(fh)
+            self.assertTrue(np.allclose(D.data['X_train'][0, :3],
+                                        [1., 12., 2.]))
+
+        # Check that all directories are there
+        fixture = ['predictions_valid', 'true_targets_ensemble.npy',
+                   'start_time_100', 'datamanager.pkl',
+                   'predictions_ensemble',
+                   'ensembles', 'predictions_test', 'models']
+        self.assertEqual(sorted(os.listdir(os.path.join(backend_api.temporary_directory,
+                                                        '.auto-sklearn'))),
+                         sorted(fixture))
+
+        # At least one ensemble, one validation, one test prediction and one
+        # model and one ensemble
+        fixture = os.listdir(os.path.join(backend_api.temporary_directory,
+                                          '.auto-sklearn', 'predictions_ensemble'))
+        self.assertGreater(len(fixture), 0)
+
+        fixture = glob.glob(os.path.join(backend_api.temporary_directory, '.auto-sklearn',
+                                         'models', '100.*.model'))
+        self.assertGreater(len(fixture), 0)
+
+        fixture = os.listdir(os.path.join(backend_api.temporary_directory,
+                                          '.auto-sklearn', 'ensembles'))
+        self.assertIn('100.0000000001.ensemble', fixture)
+
+        # Start time
+        start_time_file_path = os.path.join(backend_api.temporary_directory,
+                                            '.auto-sklearn', "start_time_100")
+        with open(start_time_file_path, 'r') as fh:
+            start_time = float(fh.read())
+        self.assertGreaterEqual(time.time() - start_time, 10)
+
+        del auto
+        self._tearDown(backend_api.temporary_directory)
+        self._tearDown(backend_api.output_directory)
+
+    def test_do_dummy_prediction(self):
+        for name in ['401_bac', '31_bac', 'adult', 'cadata']:
+            backend_api = self._create_backend('test_do_dummy_prediction')
+
+            dataset = os.path.join(self.test_dir, '..', '.data', name)
+
+            auto = autosklearn.automl.AutoML(
+                backend_api, 20, 5,
+                initial_configurations_via_metalearning=25)
+            setup_logger()
+            auto._logger = get_logger('test_do_dummy_predictions')
+            auto._backend._make_internals_directory()
+            D = load_data(dataset, backend_api)
+            auto._backend.save_datamanager(D)
+            auto._do_dummy_prediction(D, 1)
+
+            # Ensure that the dummy predictions are not in the current working
+            # directory, but in the temporary directory.
+            self.assertFalse(os.path.exists(os.path.join(os.getcwd(),
+                                                         '.auto-sklearn')))
+            self.assertTrue(os.path.exists(os.path.join(
+                backend_api.temporary_directory, '.auto-sklearn', 'predictions_ensemble',
+                'predictions_ensemble_1_1_0.0.npy')))
+
+            del auto
+            self._tearDown(backend_api.temporary_directory)
+            self._tearDown(backend_api.output_directory)
+
+    @unittest.mock.patch('autosklearn.evaluation.ExecuteTaFuncWithQueue.run')
+    def test_fail_if_dummy_prediction_fails(self, ta_run_mock):
+        backend_api = self._create_backend('test_fail_if_dummy_prediction_fails')
+
+        dataset = os.path.join(self.test_dir, '..', '.data', '401_bac')
+
+        time_for_this_task = 30
+        per_run_time = 10
+        auto = autosklearn.automl.AutoML(backend_api,
+                                         time_for_this_task,
+                                         per_run_time,
+                                         initial_configurations_via_metalearning=25,
+                                         )
+        setup_logger()
+        auto._logger = get_logger('test_fail_if_dummy_prediction_fails')
+        auto._backend._make_internals_directory()
+        D = load_data(dataset, backend_api)
+        auto._backend.save_datamanager(D)
+
+        # First of all, check that ta.run() is actually called.
+        ta_run_mock.return_value = StatusType.SUCCESS, None, None, "test"
+        auto._do_dummy_prediction(D, 1)
+        ta_run_mock.assert_called_once_with(1, cutoff=time_for_this_task)
+
+        # Case 1. Check that function raises no error when statustype == success.
+        # ta.run() returns status, cost, runtime, and additional info.
+        ta_run_mock.return_value = StatusType.SUCCESS, None, None, "test"
+        raised = False
+        try:
+            auto._do_dummy_prediction(D, 1)
+        except ValueError:
+            raised = True
+        self.assertFalse(raised, 'Exception raised')
+
+        # Case 2. Check that if statustype returned by ta.run() != success,
+        # the function raises error.
+        ta_run_mock.return_value = StatusType.CRASHED, None, None, "test"
+        self.assertRaisesRegex(ValueError,
+                               'Dummy prediction failed: test',
+                               auto._do_dummy_prediction,
+                               D, 1,
+                               )
+        ta_run_mock.return_value = StatusType.ABORT, None, None, "test"
+        self.assertRaisesRegex(ValueError,
+                               'Dummy prediction failed: test',
+                               auto._do_dummy_prediction,
+                               D, 1,
+                               )
+        ta_run_mock.return_value = StatusType.TIMEOUT, None, None, "test"
+        self.assertRaisesRegex(ValueError,
+                               'Dummy prediction failed: test',
+                               auto._do_dummy_prediction,
+                               D, 1,
+                               )
+        ta_run_mock.return_value = StatusType.MEMOUT, None, None, "test"
+        self.assertRaisesRegex(ValueError,
+                               'Dummy prediction failed: test',
+                               auto._do_dummy_prediction,
+                               D, 1,
+                               )
+        ta_run_mock.return_value = StatusType.CAPPED, None, None, "test"
+        self.assertRaisesRegex(ValueError,
+                               'Dummy prediction failed: test',
+                               auto._do_dummy_prediction,
+                               D, 1,
+                               )
+
+        self._tearDown(backend_api.temporary_directory)
+        self._tearDown(backend_api.output_directory)
+
+
+if __name__ == "__main__":
+    unittest.main()
