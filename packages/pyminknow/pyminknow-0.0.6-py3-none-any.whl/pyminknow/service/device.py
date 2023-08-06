c360d58f52544974a75ae35dfcae887ba3e7916e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:46 +0000

    Add package pyminknow/0.0.6/pyminknow-0.0.6-py3-none-any.whl

---
+++ packages/pyminknow/pyminknow-0.0.6-py3-none-any.whl/pyminknow/service/device.py
@@ -0,0 +1,29 @@
+import logging
+import random
+
+import minknow.rpc.device_pb2
+import minknow.rpc.device_pb2_grpc
+
+import pyminknow.config
+
+LOGGER = logging.getLogger(__name__)
+
+
+class DeviceService(minknow.rpc.device_pb2_grpc.DeviceServiceServicer):
+    """
+    Device service
+    """
+    add_to_server = minknow.rpc.device_pb2_grpc.add_DeviceServiceServicer_to_server
+
+    def get_device_state(self, request, context):
+        # Pick a random state
+        possible_states = minknow.rpc.device_pb2.GetDeviceStateResponse.DeviceState.keys()
+        device_state = random.choice(possible_states)
+
+        return minknow.rpc.device_pb2.GetDeviceStateResponse(device_state=device_state)
+
+    def get_flow_cell_info(self, request, context):
+        return minknow.rpc.device_pb2.GetFlowCellInfoResponse(
+            has_flow_cell=True,
+            flow_cell_id=pyminknow.config.FLOW_CELL_ID,
+        )
