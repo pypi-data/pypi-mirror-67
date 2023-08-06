7680498f562fd23f8232f87c2b32810e4084af1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:54 +0000

    Add package bullsandcows/1.4/bullsandcows-1.4.tar.gz

---
+++ packages/bullsandcows/bullsandcows-1.4.tar.gz/bullsandcows-1.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md","r")as f:
+    long_description = f.read()
+setup(
+    name = "bullsandcows",
+    version = "1.4",
+    description = "Bulls and Cows in Python.",
+    py_modules = ["bullsandcows"],
+    package_dir = {"":"src"},
+    author = "ryancflam",
+    author_email = "ryancflam@hotmail.com",
+    url = "https://github.com/ryancflam/Bulls-and-Cows",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
