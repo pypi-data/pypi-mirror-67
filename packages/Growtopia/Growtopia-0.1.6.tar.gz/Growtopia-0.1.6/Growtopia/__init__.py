1469dd63b0c21b37cb7f18ce8d8043b3b8fd0b5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:40 +0000

    Add package Growtopia/0.1.6/Growtopia-0.1.6.tar.gz

---
+++ packages/Growtopia/Growtopia-0.1.6.tar.gz/Growtopia-0.1.6/Growtopia/__init__.py
@@ -0,0 +1,40 @@
+import os, random, socket, requests, struct
+from enet import *
+__version__ = '0.1.6'
+
+# V0.1.0
+# Name taking
+
+# V0.1.1
+# Bug Fixing
+
+# V0.1.2
+# Bug Fixing
+
+# V0.1.3
+# Bug Fixing
+
+# v0.1.4
+# Package fixing
+
+# v0.1.5
+# Bug Fixing
+
+# v0.1.6
+# Exception solved
+
+"""
+UNOFFICIAL GROWTOPIA PYTHON PACKAGE
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+
+
+#######################################
+##	Importing The Required Things	 ##
+#######################################
+
+from .generate import *
+from .exceptions import *
+from .packet import *
+from .player import *
