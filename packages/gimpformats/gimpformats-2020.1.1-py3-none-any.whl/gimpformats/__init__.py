cde1cfdc49b9003453a7a75e26550b022cc9fa0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:21 +0000

    Add package gimpformats/2020.1.1/gimpformats-2020.1.1-py3-none-any.whl

---
+++ packages/gimpformats/gimpformats-2020.1.1-py3-none-any.whl/gimpformats/__init__.py
@@ -0,0 +1,22 @@
+""" gimpformats
+
+Forked from https://github.com/TheHeadlessSourceMan/gimpFormats
+
+A pure python implementation of the GIMP XCF image format.
+
+Use this to interact with GIMP image formats
+"""
+from .gimpFormat import *
+from .gimpGbrBrush import *
+from .gimpGgrGradient import *
+from .gimpGihBrushSet import *
+from .gimpGpbBrush import *
+from .gimpGtpToolPreset import *
+from .gimpImageInternals import *
+from .gimpIOBase import *
+from .gimpParasites import *
+from .gimpPatPattern import *
+from .gimpVbrBrush import *
+from .gimpVectors import *
+from .gimpXcfDocument import *
+from .gimpGplPalette import *
