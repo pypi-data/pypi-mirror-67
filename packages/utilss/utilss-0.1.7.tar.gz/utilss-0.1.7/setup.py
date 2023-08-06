a3152da6ddeefa89ae542f1cba58f14416b6b397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:08 +0000

    Add package utilss/0.1.7/utilss-0.1.7.tar.gz

---
+++ packages/utilss/utilss-0.1.7.tar.gz/utilss-0.1.7/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup_args = dict(
+    name='utilss',
+    version='0.1.7',
+    description='Useful tools to work with text mining in Python',
+    long_description_content_type="text/markdown",
+    long_description=README,
+    include_package_data=True,
+    license='MIT',
+    packages=find_packages(),
+    author='Xiaoou WANG',
+    author_email='xiaoouwangfrance@gmail.com',
+    keywords=['text mining', 'npl', 'corpus'],
+    url='https://github.com/xiaoouwang/utilss',
+    download_url='https://pypi.org/project/utilss',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+install_requires = [
+    ''
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
