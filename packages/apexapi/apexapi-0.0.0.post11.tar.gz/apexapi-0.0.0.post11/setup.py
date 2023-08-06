83d5f26aabaf53b24e115ab4baf4958ae852a4cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:54 +0000

    Add package apexapi/0.0.0.post11/apexapi-0.0.0.post11.tar.gz

---
+++ packages/apexapi/apexapi-0.0.0.post11.tar.gz/apexapi-0.0.0.post11/setup.py
@@ -0,0 +1,38 @@
+"""
+Setup.py for ApexAPI
+"""
+
+from setuptools import setup
+version = "0.0.0-11" #NOTE: please blame pypi for the weird version numbers...
+
+setup(
+    name='apexapi',
+    version=version,
+    description="Apex is a modern microservice framework written in Python.",
+    url='https://gitlab.com/apexapi/apexapi',
+    author='Dan Sikes',
+    author_email='dansikes7@gmail.com',
+    keywords='python, framework, microservices, modern, api',
+
+    packages=[
+        'apexapi',
+        'apexapi.core.handlers.apex',
+        # 'apexapi.core.templates'
+    ],
+
+    install_requires=[
+        'click',
+        'pyaml',
+        'munch',
+        'werkzeug',
+        'requests'
+    ],
+
+    entry_points = {
+        'console_scripts': ['apex=apexapi:main'],
+    },
+    
+    project_urls={
+        'Source': 'https://gitlab.com/dsikes/apex',
+    },
+)
