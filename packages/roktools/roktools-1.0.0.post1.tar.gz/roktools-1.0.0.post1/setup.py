8725b470a4f8afb23e4f9c8bd5e8f890fa406cf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:13 +0000

    Add package roktools/1.0.0.post1/roktools-1.0.0.post1.tar.gz

---
+++ packages/roktools/roktools-1.0.0.post1.tar.gz/roktools-1.0.0.post1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='roktools',
+    version_cc='{version}',
+    setup_requires=['setuptools-git-version-cc'],
+    author='Rokubun',
+    author_email='info@rokubun.cat',
+    description='Set of tools used in internal Rokubun projects',
+    license='http://opensource.org/licenses/MIT',
+    url="https://github.com/rokubun/py-roktools",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
+    py_modules=['rooktools'],
+    install_requires=[
+        'setuptools >= 8.0',
+    ]
+)
