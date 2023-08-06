bcab5130da272402da0d0e7664823404773274fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:50 +0000

    Add package tlnetcard-python/0.1.4/tlnetcard_python-0.1.4-py3-none-any.whl

---
+++ packages/tlnetcard-python/tlnetcard_python-0.1.4-py3-none-any.whl/tlnetcard_python/system/administration/tcp_ip/tcp_ip.py
@@ -0,0 +1,9 @@
+# tcp_ip.py
+# Ethan Guthrie
+# DATE TBD
+""" Allows TCP/IP settings for IPv4/IPv6 to be configured. """
+
+class TcpIp:
+    """ Class for the TcpIp object. """
+    def __init__(self):
+        """ Initializes the TcpIp object. """
---
+++ packages/tlnetcard-python/tlnetcard_python-0.1.4-py3-none-any.whl/tlnetcard_python/system/administration/user_manager/user_manager.py
@@ -0,0 +1,9 @@
+# user_manager.py
+# Ethan Guthrie
+# DATE TBD
+""" Allows user and permission settings to be configured. """
+
+class UserManager:
+    """ Class for the UserManager object. """
+    def __init__(self):
+        """ Initializes the UserManager object. """
