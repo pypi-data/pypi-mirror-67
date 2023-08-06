75921d8106184ebee124d0a8b5fea1058d038c4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:34 +0000

    Add package hspf-utils/5.1.1/hspf_utils-5.1.1.tar.gz

---
+++ packages/hspf-utils/hspf_utils-5.1.1.tar.gz/hspf_utils-5.1.1/setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from setuptools import setup
+
+pkg_name = "hspf_utils"
+
+# temporarily redirect config directory to prevent matplotlib importing
+# testing that for writeable directory which results in sandbox error in
+# certain easy_install versions
+os.environ["MPLCONFIGDIR"] = "."
+
+version = open("VERSION").readline().strip()
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist")
+    os.system("twine upload dist/{pkg_name}-{version}.tar.gz".format(**locals()))
+    sys.exit()
+
+README = open("README.rst").read()
+
+install_requires = [
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+    "tstoolbox",
+    "hspfbintoolbox",
+]
+
+setup(
+    name=pkg_name,
+    version=version,
+    description="Utilities to work with Hydrological Simulation Program - FORTRAN (HSPF) models.",
+    long_description=README,
+    classifiers=[
+        # Get strings from
+        # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "Environment :: Console",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords="time_series",
+    author="Tim Cera, P.E.",
+    author_email="tim@cerazone.net",
+    url="http://timcera.bitbucket.io/{pkg_name}/docsrc/index.html".format(**locals()),
+    packages=[pkg_name],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        "console_scripts": ["{pkg_name}={pkg_name}.{pkg_name}:main".format(**locals())]
+    },
+    test_suite="tests",
+    python_requires=">=3.6",
+)
