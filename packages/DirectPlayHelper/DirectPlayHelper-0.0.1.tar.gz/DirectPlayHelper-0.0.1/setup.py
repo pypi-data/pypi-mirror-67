e1c23e1879779bab9b6467084633ce0c0809de48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:56 +0000

    Add package DirectPlayHelper/0.0.1/DirectPlayHelper-0.0.1.tar.gz

---
+++ packages/DirectPlayHelper/DirectPlayHelper-0.0.1.tar.gz/DirectPlayHelper-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+        name="DirectPlayHelper",
+        version="0.0.1",
+        author="Keivn Davis",
+        author_email="kevincarrolldavis@gmail.com",
+        description="Server and Client application for DirectPlay" +
+                    "communication",
+        long_description="Longer description",
+        url="https://github.com/kevincar/DirectPlayHelper",
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent"
+            ]
+        )
