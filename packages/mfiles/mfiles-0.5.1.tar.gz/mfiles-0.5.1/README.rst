2098a3ab748885a079f7a22d8dba6804483c9bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:21 +0000

    Add package mfiles/0.5.1/mfiles-0.5.1.tar.gz

---
+++ packages/mfiles/mfiles-0.5.1.tar.gz/mfiles-0.5.1/README.rst
@@ -0,0 +1,70 @@
+=============
+python-mfiles
+=============
+
+.. include:: doc/badges.rst
+
+Introduction
+-------------
+Python wrapper around the M-Files API. Simple examples for how to use it can be
+found under ``examples/``. Documentation can be found at
+https://mfiles.readthedocs.io/en/latest/
+
+Install
+-------------
+
+To use this library, install the python requirements:
+
+::
+
+    pip install mfiles
+
+
+You can also use Git to clone the repository from GitHub to install the latest
+development version:
+
+::
+
+    git clone https://github.com/afcmrp/python-mfiles.git
+    cd python-mfiles
+    pip install .
+
+Development
+-------------
+
+Code follows PEP-8. Docstrings follow Google Python style guide docstring
+format. To develop and test this library, install the python dev requirements:
+
+::
+
+    pip install -r dev_requirements.txt
+
+Testing
+-------------
+
+Testing is done through ``pytest`` and code coverage is done through
+``coverage.py``.
+
+Run tests
+
+::
+
+    pytest
+
+Run tests and check coverage
+
+::
+
+    coverage run -m pytest
+
+Check coverage report
+
+::
+
+    coverage report
+
+For html report run
+
+::
+
+    coverage html
---
+++ packages/mfiles/mfiles-0.5.1.tar.gz/mfiles-0.5.1/setup.py
@@ -0,0 +1,39 @@
+# pylint: disable=missing-module-docstring
+
+from setuptools import setup, find_packages
+
+PYPI_DESCRIPTION = """
+Python wrapper around the M-Files API.
+
+Documentation: https://mfiles.readthedocs.io/en/latest/
+"""
+
+setup(
+    name='mfiles',
+    packages=find_packages(),
+    version='0.5.1',
+    license='MIT',
+    description='M-Files API wrapper',
+    long_description=PYPI_DESCRIPTION,
+    author='Emil Hjelm',
+    author_email='emil.hjelm@climeon.com',
+    url='https://github.com/afcmrp/python-mfiles',
+    keywords=['M-Files', 'mfiles', 'REST', 'API'],
+    python_requires='!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent',
+    ],
+)
