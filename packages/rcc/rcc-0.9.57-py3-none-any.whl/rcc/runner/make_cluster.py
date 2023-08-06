8f4c4cb07e5c548928ac5a0607ffa7af059bf550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:35 +0000

    Add package rcc/0.9.57/rcc-0.9.57-py3-none-any.whl

---
+++ packages/rcc/rcc-0.9.57-py3-none-any.whl/rcc/runner/make_cluster.py
@@ -0,0 +1,28 @@
+'''Tools to help initialize a redis cluster on kubernete
+
+Copyright (c) 2020 Machine Zone, Inc. All rights reserved.
+'''
+
+import asyncio
+import logging
+import tempfile
+
+import click
+
+from rcc.cluster.init_cluster import runNewCluster
+
+
+@click.command()
+@click.option('--size', default=3, type=int)
+@click.option('--start_port', default=11000, type=int)
+@click.option('--password', '-a')
+@click.option('--user')
+def make_cluster(size, start_port, password, user):
+    '''Create, configure, initialize and run a redis cluste  r
+    and a redis cluster proxy'''
+    root = tempfile.mkdtemp()
+
+    try:
+        asyncio.run(runNewCluster(root, start_port, size, password, user))
+    except Exception as e:
+        logging.error(f'cluster_nodes error: {e}')
