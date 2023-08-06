689ca86e33336aa5eb664f6c75a7562bf58e12fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:39 +0000

    Add package cleaning-scripts/0.2.18/cleaning-scripts-0.2.18.tar.gz

---
+++ packages/cleaning-scripts/cleaning-scripts-0.2.18.tar.gz/cleaning-scripts-0.2.18/scripts/custom/clean_codes.py
@@ -0,0 +1,19 @@
+from scripts import utils
+import re
+
+
+terminologies = ["HL7:", "UMLS:", "LOINC:", "ATC:", "FINESS:", "MedDRA:", "CIM10:", "RECIST:"]
+
+
+def clean_codes(raw_input):
+    """Remove terminology system from code ("HL7:male") to ("male")
+    """
+    if utils.is_empty(raw_input):
+        return None
+
+    code = re.match(r"([A-z0-9]*:)(.*)", raw_input)
+
+    if not code:
+        return None
+    elif code.group(1) in terminologies:
+        return code.group(2)
