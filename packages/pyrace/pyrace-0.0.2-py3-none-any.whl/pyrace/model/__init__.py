ee2a0ba1225a3ed079f7439218bba5f4c6be3982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:36 +0000

    Add package pyrace/0.0.2/pyrace-0.0.2-py3-none-any.whl

---
+++ packages/pyrace/pyrace-0.0.2-py3-none-any.whl/pyrace/model/__init__.py
@@ -0,0 +1,51 @@
+from pyrace.setting import Setting
+from pyrace.model.modelregistery import ModelRegistery
+from pyrace.model.car import Car
+
+class Model:
+    def __init__(self, model_name, vue):
+
+        self.metadata = ModelRegistery.METADATA[model_name]
+
+        self.vue = vue
+        self.car = Car(self.vue.metadata, self.metadata)
+        
+        self.progress = 0
+        
+        self.state = self.car.get_state(self.vue)
+        self.reward = 0
+        self.done = False
+        self.info = {}
+        self.history = [(self.state, self.reward, self.done, self.info)]
+
+
+    def step(self, action):
+        
+        if self.car.step(action) :
+
+            self.state = self.car.get_state(self.vue)
+
+            progress = self.vue.get_progress(self.car.position)
+            self.reward = progress - self.progress
+            if self.reward > 1000 :
+                self.reward = -self.reward  # avoid going directly back during after start. 
+            self.progress = progress
+
+            if not self.vue.is_road(self.car.position):
+                self.done = True
+
+            self.info = {}
+
+            self.history.append((self.state, self.reward, self.done, self.info))
+            return self.state, self.reward, self.done, self.info
+
+        else :
+            return None
+
+
+
+
+
+
+
+    
