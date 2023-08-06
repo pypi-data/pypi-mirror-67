4ed213ab6a8c47089b640b786db06b83a94fc40f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:30 +0000

    Add package geohash-to-geojson/1.3/geohash_to_geojson-1.3.tar.gz

---
+++ packages/geohash-to-geojson/geohash_to_geojson-1.3.tar.gz/geohash_to_geojson-1.3/README.md
@@ -0,0 +1,6 @@
+# Geohash to geojson: 
+
+This package convert a set of geohashes into a geojson file with the corresponding boundaries
+
+## How it works
+
---
+++ packages/geohash-to-geojson/geohash_to_geojson-1.3.tar.gz/geohash_to_geojson-1.3/setup.py
@@ -0,0 +1,27 @@
+
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='geohash_to_geojson',  
+     version='1.3',
+     license="MIT",
+     author="Afraoucene Chakib",
+     author_email="fc_afraoucene@esi.dz",
+     description="convert set of geohashes into a geojson boundaries files of the corresponding geohashes",
+     long_description=long_description,
+     url="https://github.com/brings123/geohash-to-geojson",
+     packages=setuptools.find_packages(),
+     long_description_content_type="text/markdown",
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+         
+     ],
+     install_requires=[
+       "polygon_geohasher",
+       "geopandas",
+       "shapely"
+   ]
+ )
