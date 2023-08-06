bedc3c24afc88decde32ef49e3cee9bde3057e8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:43 +0000

    Add package pymelcloud/2.8.0/pymelcloud-2.8.0-py3-none-any.whl

---
+++ packages/pymelcloud/pymelcloud-2.8.0-py3-none-any.whl/pymelcloud/const.py
@@ -0,0 +1,13 @@
+"""Constants for pymelcloud."""
+
+DEVICE_TYPE_ATA = "ata"
+DEVICE_TYPE_ATW = "atw"
+DEVICE_TYPE_UNKNOWN = "unknown"
+
+DEVICE_TYPE_LOOKUP = {
+    0: DEVICE_TYPE_ATA,
+    1: DEVICE_TYPE_ATW,
+}
+
+UNIT_TEMP_CELSIUS = "celsius"
+UNIT_TEMP_FAHRENHEIT = "fahrenheit"
