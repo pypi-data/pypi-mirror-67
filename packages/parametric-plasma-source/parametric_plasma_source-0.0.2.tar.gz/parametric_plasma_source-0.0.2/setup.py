e0017052a438ab55b6be48ed2184903e51228a49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:47 +0000

    Add package parametric-plasma-source/0.0.2/parametric_plasma_source-0.0.2.tar.gz

---
+++ packages/parametric-plasma-source/parametric_plasma_source-0.0.2.tar.gz/parametric_plasma_source-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="parametric_plasma_source",
+    version="0.0.2",
+    author="Jonathan Shimwell",
+    author_email="jonathan.shimwell@ukaea.uk",
+    description="Parametric plasma source for fusion simulations in OpenMC",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shimwell/parametric_plasma_source",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
