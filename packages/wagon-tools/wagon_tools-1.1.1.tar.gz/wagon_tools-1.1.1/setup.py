9d7786b60b5f9c03d29437c8bfa91b0c2d6ac00e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:27 +0000

    Add package wagon-tools/1.1.1/wagon_tools-1.1.1.tar.gz

---
+++ packages/wagon-tools/wagon_tools-1.1.1.tar.gz/wagon_tools-1.1.1/setup.py
@@ -0,0 +1,26 @@
+import pathlib
+from setuptools import setup, find_packages
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+
+setup(name='wagon_tools',
+      version="1.1.1",
+      # Below to deal with cleaner versionning
+      #setup_requires=['setuptools_scm'],
+      #install_requires=['setuptools_scm'],
+      #use_scm_version={'write_to': 'wagon_tools/version.txt'},
+      description="Package builder with CI included",
+      long_description=README,
+      long_description_content_type="text/markdown",
+      url="https://github.com/lologibus2/wagon_tools",
+      author='Jean Bizot', author_email="jea@gmail.com",
+      packages=find_packages(),
+      test_suite='tests',
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      scripts=['scripts/wagon-make-package'],
+      zip_safe=False)
