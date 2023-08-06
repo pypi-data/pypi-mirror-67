502334b1b8eb7cf65287dbb744a1ed42c5000605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:15 +0000

    Add package r2k/0.3.2/r2k-0.3.2.tar.gz

---
+++ packages/r2k/r2k-0.3.2.tar.gz/r2k-0.3.2/r2k/constants.py
@@ -0,0 +1,24 @@
+from enum import Enum
+from os.path import dirname, expanduser, join
+
+CONFIG_ENV_VAR = "RSS_TO_KINDLE_CONFIG"
+
+HTML_HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3"
+}
+
+DEFAULT_APP_PATH = expanduser("~/.r2k")
+DEFAULT_CONFIG_PATH = join(DEFAULT_APP_PATH, "config.yml")
+
+PACKAGE_DIR = dirname(__file__)
+TOP_LEVEL_DIR = dirname(PACKAGE_DIR)
+TEMPLATES_DIR = join(TOP_LEVEL_DIR, "templates")
+
+
+class Parser(Enum):
+    """A convenience class to represent the available parsing options"""
+
+    PUSH_TO_KINDLE = "pushtokindle"
+    MERCURY = "mercury"
+
+    __values__ = PUSH_TO_KINDLE, MERCURY
