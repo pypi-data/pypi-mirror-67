cfed65680c31e767c791b84b06df8f311fe1281d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:20 +0000

    Add package XTCryptoSignals/0.5.0/XTCryptoSignals-0.5.0.tar.gz

---
+++ packages/XTCryptoSignals/XTCryptoSignals-0.5.0.tar.gz/XTCryptoSignals-0.5.0/xtcryptosignals/client/api/contact/views.py
@@ -0,0 +1,28 @@
+__author__ = "Paulo Antunes"
+__copyright__ = "Copyright 2018, XTCryptoSignals"
+__credits__ = [
+    "Paulo Antunes",
+]
+__license__ = "GPL"
+__maintainer__ = "Paulo Antunes"
+__email__ = "pjmlantunes@gmail.com"
+
+
+import requests
+from flask import (
+    request,
+    Blueprint,
+    current_app,
+)
+
+
+bp = Blueprint("contact", __name__)
+
+
+@bp.route("/contact", methods=["POST"])
+def contact():
+    response = requests.post(
+        url="{}contact".format(current_app.config["SERVER_API_BASE_URL"]),
+        data=request.form,
+    )
+    return response.text, response.status_code
---
+++ packages/XTCryptoSignals/XTCryptoSignals-0.5.0.tar.gz/XTCryptoSignals-0.5.0/xtcryptosignals/client/static/js/charts/twitter.js
@@ -0,0 +1,60 @@
+function create_chart_twitter(chart_id, pt) {
+      return Highcharts.chart(chart_id, {
+        chart: {
+            zoomType: 'x'
+        },
+        title: {
+            text: `Twitter ${pt} 1d`
+        },
+        subtitle: {
+            text: document.ontouchstart === undefined ?
+                'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+        },
+          credits: {
+            enabled: false
+        },
+        time: {
+            timezone: moment.tz.guess(true)
+        },
+        xAxis: [{
+            type: 'datetime',
+            title: {
+                text: `Date (${moment.tz.guess(true)})`
+            }
+        }],
+        yAxis: {
+            title: {
+                text: 'Number of followers'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                    },
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                },
+                marker: {
+                    radius: 2
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
+    });
+}
