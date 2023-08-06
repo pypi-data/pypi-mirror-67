590f4974aa1fa3d12c912e5999040ccea27c5406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:45 +0000

    Add package bow-distributions/1.9/bow_distributions-1.9.tar.gz

---
+++ packages/bow-distributions/bow_distributions-1.9.tar.gz/bow_distributions-1.9/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='bow_distributions',
+      version='1.9',
+      description='Gaussian distributions & Binomial distributions',
+      packages=['bow_distributions'],
+      author = 'Waliyat Olayiwola',
+      author_email = 'analystbow@gmail.com',
+      url='https://github.com/AnalystBow/myPythonPackages',
+      zip_safe=False)
