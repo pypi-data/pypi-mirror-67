eb43ad3f1ea66c7ea791368b221a8ea8d2fc2b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:56 +0000

    Add package sysapi/0.0.51/sysapi-0.0.51-py3-none-any.whl

---
+++ packages/sysapi/sysapi-0.0.51-py3-none-any.whl/sysapi/user.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+
+## Based on tutorial example at 
+## https://fastapi.tiangolo.com/tutorial/extra-models/
+
+class BaseUser(BaseModel):
+    username: str
+    email: str
+    full_name: str = None
+    role: str
+
+class UserIn(BaseUser):
+    password: str
+
+class UserOut(BaseUser):
+
+class UserInDB(BaseUser):
+    hashed_password: str
