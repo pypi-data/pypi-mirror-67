11148077c019e5a128602f46a839b1211af51b6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:28 +0000

    Add package pyleniumio/1.6.1/pyleniumio-1.6.1.tar.gz

---
+++ packages/pyleniumio/pyleniumio-1.6.1.tar.gz/pyleniumio-1.6.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='pyleniumio',
+    version='1.6.1',
+    packages=[
+        'pylenium'
+    ],
+    package_dir={'pylenium': 'pylenium'},
+    url='https://github.com/ElSnoMan/pyleniumio',
+    license='MIT',
+    author='Carlos Kidman',
+    author_email='carlos@qap.dev',
+    description='The best of Selenium and Cypress in a single Python Package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=['selenium', 'pytest', 'pytest-xdist', 'pydantic', 'faker', 'requests', 'webdriver-manager'],
+    data_files=[('..', ['conftest.py'])]
+)
