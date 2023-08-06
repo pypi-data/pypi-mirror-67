9683b64cb9758089b354b9cbac77117d89974f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:52 +0000

    Add package mbitutils/0.0.4/mbitutils-0.0.4-py3-none-any.whl

---
+++ packages/mbitutils/mbitutils-0.0.4-py3-none-any.whl/music.py
@@ -0,0 +1,34 @@
+"""
+Author: Øivind Rønning, Statped
+Github: https://github.com/oivron/mbitutils
+"""
+
+class __Music:
+    """Music stuff"""
+
+    def __init__(self):
+        self.DADADADUM = "DADADADUM"
+        self.ENTERTAINER = "ENTERTAINER"
+        self.PRELUDE = "PRELUDE"
+        self.ODE = "ODE"
+        self.RINGTONE = "RINGTONE"
+        self.FUNK = "FUNK"
+        self.BIRTHDAY = "BIRTHDAY"
+        self.WEDDING = "WEDDING"
+        self.FUNERAL = "FUNERAL"
+        self.PUNCHLINE = "PUNCHLINE"
+        self.PYTHON = "PYTHON"
+        self.BADDY = "BADDY"
+        self.CHASE = "CHASE"
+        self.BA_DING = "BA_DING"
+        self.WAWAWAWAA = "WAWAWAWAA"
+        self.JUMP_UP = "JUMP_UP"
+        self.JUMP_DOWN = "JUMP_DOWN"
+        self.POWER_UP = "POWER_UP"
+        self.POWER_DOWN = "POWER_DOWN"
+
+    def play(self, music):
+        """Plays a melody."""
+
+
+music = __Music()
---
+++ packages/mbitutils/mbitutils-0.0.4-py3-none-any.whl/speech.py
@@ -0,0 +1,15 @@
+"""
+Author: Øivind Rønning, Statped
+Github: https://github.com/oivron/mbitutils
+"""
+
+class __Speech:
+    """Speech synthesis"""
+
+    def __init__(self):
+        """Init"""
+    
+    def say(self, text):
+        """Read aloud selected text."""
+    
+speech = __Speech()
