a21bfc89f6dc97cbccae3e4e1427208b6a37429a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:27 +0000

    Add package TracComponentDependency/0.3/TracComponentDependency-0.3-py2-none-any.whl

---
+++ packages/TracComponentDependency/TracComponentDependency-0.3-py2-none-any.whl/componentdependencies/interface.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from trac.core import Interface
+
+
+class IRequireComponents(Interface):
+    def requires():
+        """list of component classes that this component depends on"""
