72bfc7491dbf14efd51b2a9d8e1e5b1986178ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:58 +0000

    Add package Electrum-Zcash-Random-Fork/3.1.3b5/Electrum-Zcash-Random-Fork-3.1.3b5.tar.gz

---
+++ packages/Electrum-Zcash-Random-Fork/Electrum-Zcash-Random-Fork-3.1.3b5.tar.gz/Electrum-Zcash-Random-Fork-3.1.3b5/README.rst
@@ -0,0 +1,94 @@
+Electrum-Zcash - Lightweight Zcash client
+=====================================
+
+::
+
+  Licence: MIT Licence
+  Author: Thomas Voegtlin
+  Language: Python
+  Homepage: https://github.com/zebra-lucky/electrum-zcash
+
+
+.. image:: https://travis-ci.org/zebra-lucky/electrum-zcash.svg?branch=master
+    :target: https://travis-ci.org/zebra-lucky/electrum-zcash
+    :alt: Build Status
+
+
+
+
+
+Getting started
+===============
+
+Electrum-Zcash is a pure python application. If you want to use the
+Qt interface, install the Qt dependencies::
+
+    sudo apt-get install python3-pyqt5
+
+If you downloaded the official package (tar.gz), you can run
+Electrum-Zcash from its root directory, without installing it on your
+system; all the python dependencies are included in the 'packages'
+directory. To run Electrum-Zcash from its root directory, just do::
+
+    ./electrum-zcash
+
+You can also install Electrum-Zcash on your system, by running this command::
+
+    sudo apt-get install python3-setuptools
+    pip3 install .[full]
+
+This will download and install the Python dependencies used by
+Electrum-Zcash, instead of using the 'packages' directory.
+The 'full' extra contains some optional dependencies that we think
+are often useful but they are not strictly needed.
+
+If you cloned the git repository, you need to compile extra files
+before you can run Electrum-Zcash. Read the next section, "Development
+Version".
+
+
+
+Development version
+===================
+
+Check out the code from GitHub::
+
+    git clone git://github.com/zebra-lucky/electrum-zcash.git
+    cd electrum-zcash
+
+Run install (this should install dependencies)::
+
+    pip3 install .[full]
+
+Compile the icons file for Qt::
+
+    sudo apt-get install pyqt5-dev-tools
+    pyrcc5 icons.qrc -o gui/qt/icons_rc.py
+
+Compile the protobuf description file::
+
+    sudo apt-get install protobuf-compiler
+    protoc --proto_path=lib/ --python_out=lib/ lib/paymentrequest.proto
+
+Create translations (optional)::
+
+    sudo apt-get install python-requests gettext
+    ./contrib/make_locale
+
+
+
+
+Creating Binaries
+=================
+
+
+To create binaries, create the 'packages' directory::
+
+    ./contrib/make_packages
+
+This directory contains the python dependencies used by Electrum-Zcash.
+
+Android
+-------
+
+See `gui/kivy/Readme.txt` file.
---
+++ packages/Electrum-Zcash-Random-Fork/Electrum-Zcash-Random-Fork-3.1.3b5.tar.gz/Electrum-Zcash-Random-Fork-3.1.3b5/setup.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+# python setup.py sdist --format=zip,gztar
+
+from setuptools import setup
+import os
+import sys
+import platform
+import imp
+import argparse
+
+with open('contrib/requirements/requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open('contrib/requirements/requirements-hw.txt') as f:
+    requirements_hw = f.read().splitlines()
+
+version = imp.load_source('version', 'lib/version.py')
+
+if sys.version_info[:3] < (3, 4, 0):
+    sys.exit("Error: Electrum-Zcash requires Python version >= 3.4.0...")
+
+data_files = []
+
+if platform.system() in ['Linux', 'FreeBSD', 'DragonFly']:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root=', dest='root_path', metavar='dir', default='/')
+    opts, _ = parser.parse_known_args(sys.argv[1:])
+    usr_share = os.path.join(sys.prefix, "share")
+    icons_dirname = 'pixmaps'
+    if not os.access(opts.root_path + usr_share, os.W_OK) and \
+       not os.access(opts.root_path, os.W_OK):
+        icons_dirname = 'icons'
+        if 'XDG_DATA_HOME' in os.environ.keys():
+            usr_share = os.environ['XDG_DATA_HOME']
+        else:
+            usr_share = os.path.expanduser('~/.local/share')
+    data_files += [
+        (os.path.join(usr_share, 'applications/'), ['electrum-zcash.desktop']),
+        (os.path.join(usr_share, icons_dirname), ['icons/electrum-zcash.png'])
+    ]
+
+setup(
+    name="Electrum-Zcash-Random-Fork",
+    version=version.ELECTRUM_VERSION,
+    install_requires=requirements,
+    extras_require={
+        'full': requirements_hw + ['pycryptodomex'],
+    },
+    packages=[
+        'electrum_zcash',
+        'electrum_zcash_gui',
+        'electrum_zcash_gui.qt',
+        'electrum_zcash_plugins',
+        'electrum_zcash_plugins.audio_modem',
+        'electrum_zcash_plugins.cosigner_pool',
+        'electrum_zcash_plugins.email_requests',
+        'electrum_zcash_plugins.hw_wallet',
+        'electrum_zcash_plugins.keepkey',
+        'electrum_zcash_plugins.labels',
+        'electrum_zcash_plugins.ledger',
+        'electrum_zcash_plugins.trezor',
+        'electrum_zcash_plugins.digitalbitbox',
+        'electrum_zcash_plugins.virtualkeyboard',
+    ],
+    package_dir={
+        'electrum_zcash': 'lib',
+        'electrum_zcash_gui': 'gui',
+        'electrum_zcash_plugins': 'plugins',
+    },
+    package_data={
+        'electrum_zcash_random_fork': [
+            'servers.json',
+            'servers_testnet.json',
+            'servers_regtest.json',
+            'currencies.json',
+            'wordlist/*.txt',
+            'locale/*/LC_MESSAGES/electrum.mo',
+        ]
+    },
+    scripts=['electrum-zcash'],
+    data_files=data_files,
+    description="Lightweight Zcash Wallet",
+    author="Thomas Voegtlin",
+    author_email="thomasv@electrum.org",
+    license="MIT License",
+    url="https://github.com/zebra-lucky/electrum-zcash",
+    long_description="""Lightweight Zcash Wallet"""
+)
