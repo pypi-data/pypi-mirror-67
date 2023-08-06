fbad19ccf3cfc45e7eca19c36427f40e91770fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:53 +0000

    Add package le-pycaption/2.2.0a1/le-pycaption-2.2.0a1.tar.gz

---
+++ packages/le-pycaption/le-pycaption-2.2.0a1.tar.gz/le-pycaption-2.2.0a1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from __future__ import unicode_literals
+import os
+from setuptools import setup, find_packages
+
+README_PATH = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)),
+    'README.rst')
+
+dependencies = [
+    'beautifulsoup4>=4.6.3,<4.9.0',
+    'lxml>=3.2.3',
+    'cssutils>=0.9.10',
+    'future',
+    'six>=1.9.0'
+]
+
+setup(
+    name='le-pycaption',
+    version='2.2.0-alpha1',
+    description='Closed caption converter',
+    long_description=open(README_PATH).read(),
+    author='Joe Norton <joey@nortoncrew.com>',
+    author_email='blaine@learningequality.org',
+    url='https://github.com/learningequality/pycaption',
+    license='Apache License, Version 2.0',
+    install_requires=dependencies,
+    py_modules=["enum_compat"],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Multimedia :: Video',
+    ],
+    test_suite="tests",
+)
