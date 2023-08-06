a8fe05dfd39131d062de41b04db240ccf1c3107f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:48 +0000

    Add package Cybernator/0.1/Cybernator-0.1.tar.gz

---
+++ packages/Cybernator/Cybernator-0.1.tar.gz/Cybernator-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Cybernator',
+      version='0.1',
+      description='Paginator для discord.py',
+      packages=['Cybernator'],
+      author_email='rucybernetic@yandex.ru',
+      zip_safe=False)
