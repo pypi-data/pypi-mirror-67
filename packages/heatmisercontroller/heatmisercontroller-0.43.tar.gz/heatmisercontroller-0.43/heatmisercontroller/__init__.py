461cdcaa5c03c9f7b416f2ed26292f3014a9b812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:17 +0000

    Add package heatmisercontroller/0.43/heatmisercontroller-0.43.tar.gz

---
+++ packages/heatmisercontroller/heatmisercontroller-0.43.tar.gz/heatmisercontroller-0.43/heatmisercontroller/__init__.py
@@ -0,0 +1,2 @@
+"""Library for getting and setting values on Heatmiser thermstats and devices"""
+name = "heatmisercontroller"
---
+++ packages/heatmisercontroller/heatmisercontroller-0.43.tar.gz/heatmisercontroller-0.43/setup.py
@@ -0,0 +1,37 @@
+"""Build setup for package"""
+from setuptools import setup
+
+def readme():
+    """Method to add readme file as long_description."""
+    with open('README.rst') as fhandle:
+        return fhandle.read()
+
+setup(name='heatmisercontroller',
+      version='0.43',
+      description='Python implementation of Heatmiser protocol for serial connected thermostats ',
+      long_description=readme(),
+      classifiers=[
+        'Programming Language :: Python :: 2.7',
+      ],
+      url='https://github.com/ianhorsley/heatmisercontroller',
+      author='Ian Horsley',
+      #author_email='flyingcircus@example.com',
+      license='GNU v3.0',
+      packages=['heatmisercontroller'],
+      install_requires=[
+        'datetime',
+        'logging',
+        'pyserial',
+        'configobj',
+        'transitions'
+      ],
+      test_suite="tests",
+      scripts=[
+        'bin/hm_get_example.py',
+        'bin/hm_set_example.py',
+        'bin/hm_find_example.py',
+        'bin/hm_check_time_example.py'
+      ],
+
+      include_package_data=True,
+      zip_safe=False)
