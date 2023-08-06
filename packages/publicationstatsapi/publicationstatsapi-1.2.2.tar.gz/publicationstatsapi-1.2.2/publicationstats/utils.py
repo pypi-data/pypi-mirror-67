60b60c81fdaab1a83a7fea01bdc7a3ff79d3f213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:12 +0000

    Add package publicationstatsapi/1.2.2/publicationstatsapi-1.2.2.tar.gz

---
+++ packages/publicationstatsapi/publicationstatsapi-1.2.2.tar.gz/publicationstatsapi-1.2.2/publicationstats/utils.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+CITABLE_DOCUMENT_TYPES = (
+    u'article-commentary',
+    u'brief-report',
+    u'case-report',
+    u'data-article',
+    u'rapid-communication',
+    u'research-article',
+    u'review-article'
+)
---
+++ packages/publicationstatsapi/publicationstatsapi-1.2.2.tar.gz/publicationstatsapi-1.2.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+install_requires = [
+    'thriftpy2',
+    'requests'
+]
+
+setup(
+    name="publicationstatsapi",
+    version="1.2.2",
+    description="SciELO PublicationStats service SDK for Python",
+    author="SciELO",
+    author_email="scielo-dev@googlegroups.com",
+    url="http://github.com/scieloorg/publicationstatsapi",
+    packages=find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ],
+    test_suite='tests',
+    install_requires=install_requires
+)
