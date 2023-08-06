b1093ccee6b9c3b0284655e034c1b577b905a193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:16 +0000

    Add package mici/0.1.5/mici-0.1.5-py3-none-any.whl

---
+++ packages/mici/mici-0.1.5-py3-none-any.whl/mici/errors.py
@@ -0,0 +1,25 @@
+"""Exception types."""
+
+
+class Error(RuntimeError):
+    """Base class for errors."""
+
+
+class IntegratorError(Error):
+    """Error raised when integrator step fails."""
+
+
+class NonReversibleStepError(IntegratorError):
+    """Error raised when integrator step fails reversibility check."""
+
+
+class ConvergenceError(IntegratorError):
+    """Error raised when solver fails to converge within allowed iterations."""
+
+
+class LinAlgError(Error):
+    """Error raised when a matrix operation raises a linear algebra error."""
+
+
+class HamiltonianDivergenceError(Error):
+    """Error raised when integration of Hamiltonian dynamics diverges."""
