7f6295de8082b5f81e617954687fc656756db4f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:58 +0000

    Add package tamplar/0.1.19/tamplar-0.1.19-py3-none-any.whl

---
+++ packages/tamplar/tamplar-0.1.19-py3-none-any.whl/tamplar/api/methods.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import subprocess
+
+from setuptools import sandbox as setuptools_
+from compose.cli import main as compose
+from loguru import logger
+
+from tamplar.internal import utils
+
+
+class DuplicatePathException(Exception):
+    """
+    There are not the only path to library
+    """
+    pass
+
+
+logger.add(sys.stdout, backtrace=False, diagnose=True)
+
+
+def deps():
+    logger.info('install dependencies')
+    subprocess.call(['pip', 'install', '-r', 'requirements'])
+
+
+def init():
+    logger.info('initialize new service')
+    file_path = os.path.realpath(__file__)
+    print(file_path)
+    root_lib_path = os.path.dirname(file_path).split('/')[:-3]
+    path = '/'.join(root_lib_path)
+    src = f'{path}/tamplar/template/'
+    dst = os.path.abspath('./')
+    utils.copytree(src=src, dst=dst)
+    deps()
+
+
+def run(mode='local', daemon=None):
+    logger.info('run service')
+    # compose.TopLevelCommand()
+
+
+def upload(pypi=None, docker=None):
+    args = ['bdist_wheel', 'upload']
+    if pypi is not None:
+        args += ['-r', pypi]
+    setuptools_.run_setup('setup.py', pypi)
+    if docker is None:
+        return
+
+
+def test(mode):
+    pass
