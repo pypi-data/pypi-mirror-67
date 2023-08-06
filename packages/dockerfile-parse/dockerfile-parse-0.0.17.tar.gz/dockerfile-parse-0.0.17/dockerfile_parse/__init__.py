ac46aebef9dc2df93a20470ad1156a559a53a23c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:49 +0000

    Add package dockerfile-parse/0.0.17/dockerfile-parse-0.0.17.tar.gz

---
+++ packages/dockerfile-parse/dockerfile-parse-0.0.17.tar.gz/dockerfile-parse-0.0.17/dockerfile_parse/__init__.py
@@ -0,0 +1,15 @@
+"""
+Copyright (c) 2015, 2020 Red Hat, Inc
+All rights reserved.
+
+This software may be modified and distributed under the terms
+of the BSD license. See the LICENSE file for details.
+"""
+
+#
+# This file is generated from template: rel-eng/version_init.template
+#
+
+from .parser import DockerfileParser
+
+__version__ = "0.0.17"
