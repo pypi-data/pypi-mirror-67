37a0b69e654b79e578ccfca10f91bc13fe15dfa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:46 +0000

    Add package honeybee-schema/1.27.4/honeybee_schema-1.27.4-py2.py3-none-any.whl

---
+++ packages/honeybee-schema/honeybee_schema-1.27.4-py2.py3-none-any.whl/honeybee_schema/radiance/properties.py
@@ -0,0 +1,91 @@
+"""Properties Schema"""
+from pydantic import Field, constr
+from typing import List, Union
+
+from .modifier import _REFERENCE_UNION_MODIFIERS
+from .modifierset import ModifierSet, ModifierSetAbridged
+from .._base import NoExtraBaseModel
+
+
+class _PropertiesBaseAbridged(NoExtraBaseModel):
+    """Base class of Abridged Radiance Properties."""
+
+    modifier: str = Field(
+        default=None,
+        description='A string for a Honeybee Radiance Modifier.'
+        )
+
+    modifier_blk: str = Field(
+        default=None,
+        description='A string for a Honeybee Radiance Modifier to be used '
+                    'in direct solar simulations and in isolation studies (assessing'
+                    'the contribution of individual objects).'
+        )
+
+
+class ApertureRadiancePropertiesAbridged(_PropertiesBaseAbridged):
+    """Radiance Properties for Honeybee Aperture Abridged."""
+
+    type: constr(regex='^ApertureRadiancePropertiesAbridged$') = \
+        'ApertureRadiancePropertiesAbridged'
+
+
+class DoorRadiancePropertiesAbridged(_PropertiesBaseAbridged):
+    """Radiance Properties for Honeybee Door Abridged."""
+
+    type: constr(regex='^DoorRadiancePropertiesAbridged$') = \
+        'DoorRadiancePropertiesAbridged'
+
+
+class FaceRadiancePropertiesAbridged(_PropertiesBaseAbridged):
+    """Radiance Properties for Honeybee Face Abridged."""
+
+    type: constr(regex='^FaceRadiancePropertiesAbridged$') = \
+        'FaceRadiancePropertiesAbridged'
+
+
+class ShadeRadiancePropertiesAbridged(_PropertiesBaseAbridged):
+    """Radiance Properties for Honeybee Shade Abridged."""
+
+    type: constr(regex='^ShadeRadiancePropertiesAbridged$') = \
+        'ShadeRadiancePropertiesAbridged'
+
+
+class RoomRadiancePropertiesAbridged(NoExtraBaseModel):
+    """Abridged Radiance Properties for Honeybee Room."""
+
+    type: constr(regex='^RoomRadiancePropertiesAbridged$') = \
+        'RoomRadiancePropertiesAbridged'
+
+    modifier_set: str = Field(
+        default=None,
+        description='An identifier for a unique Room-Assigned ModifierSet '
+                    '(default: None).'
+        )
+
+
+class ModelRadianceProperties(NoExtraBaseModel):
+    """Radiance Properties for Honeybee Model."""
+
+    type: constr(regex='^ModelRadianceProperties$') = 'ModelRadianceProperties'
+
+    modifiers: List[_REFERENCE_UNION_MODIFIERS] = Field(
+        default=[],
+        description='A list of all unique modifiers in the model. '
+                    'This includes modifiers across all Faces, Apertures, Doors, Shades, '
+                    'Room ModifierSets, and the global_modifier_set. (default: []).'
+        )
+
+    modifier_sets: List[Union[ModifierSet, ModifierSetAbridged]] = Field(
+        default=[],
+        description='A list of all unique Room-Assigned ModifierSets in the Model '
+                    '(default: []).'
+        )
+
+    global_modifier_set: str = Field(
+        default=None,
+        description='Identifier of a ModifierSet or ModifierSetAbridged object to be used as '
+                    'as a default object for all unassigned objects in the Model '
+                    '(default: None).'
+    )
+
