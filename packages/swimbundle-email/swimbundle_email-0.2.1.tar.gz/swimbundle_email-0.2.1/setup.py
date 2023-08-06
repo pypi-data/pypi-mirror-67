00ba99aa34f4104192dcf6a6f0f9e6535a71b0d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:50 +0000

    Add package swimbundle-email/0.2.1/swimbundle_email-0.2.1.tar.gz

---
+++ packages/swimbundle-email/swimbundle_email-0.2.1.tar.gz/swimbundle_email-0.2.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='swimbundle_email',
+    packages=['swimbundle_email'],
+    version='0.2.1',
+    description='Swimlane Email Utility Package',
+    author='Swimlane',
+    author_email="info@swimlane.com",
+    long_description="# swimbundle-email",
+    install_requires=["python-magic==0.4.15"],
+    keywords=['utilities', 'email', 'parsing'],
+    classifiers=[],
+)
---
+++ packages/swimbundle-email/swimbundle_email-0.2.1.tar.gz/swimbundle_email-0.2.1/swimbundle_email/__init__.py
@@ -0,0 +1,1 @@
+from swimbundle_email.util import *
