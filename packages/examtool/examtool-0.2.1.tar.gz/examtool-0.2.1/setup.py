a0ff1cf6cf75d6deaa467f6df709c87350ca7ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:44 +0000

    Add package examtool/0.2.1/examtool-0.2.1.tar.gz

---
+++ packages/examtool/examtool-0.2.1.tar.gz/examtool-0.2.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    readme = f.read()
+
+
+setup(
+    name="examtool",
+    version="0.2.1",
+    author="Rahul Arya",
+    author_email="rahularya@berkeley.edu",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    licence="MIT",
+    packages=find_packages(include=["examtool.api", "examtool.cli"]),
+    entry_points={
+        "console_scripts": [
+            "examtool=examtool.cli.__main__:cli",
+        ]
+    },
+    python_requires='>=3.6',
+    install_requires=['cryptography'],
+    extras_require={
+        "cli": ['pikepdf', 'pytz', 'requests', 'fpdf', 'pypandoc']
+    }
+)
