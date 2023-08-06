a2a062c933b48fb9c3b73dc861b307ca16fdb139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:51 +0000

    Add package humanizer-portugues/2.2.0/humanizer-portugues-2.2.0.tar.gz

---
+++ packages/humanizer-portugues/humanizer-portugues-2.2.0.tar.gz/humanizer-portugues-2.2.0/src/humanizer_portugues/list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Lists related humanization."""
+
+__all__ = ["natural_list"]
+
+
+def natural_list(itens, separator, conjunction=None):
+    """
+    Returns human readable list separated by separator
+    Optional argument is conjuntion that substitutes the last separator
+    """
+    len_itens = len(itens)
+    if len_itens == 0:
+        return ""
+    if len_itens == 1:
+        return itens[0]
+    phrase = itens[0]
+    if conjunction:
+        for i in range(1, len_itens - 1):
+            phrase += "%s %s" % (separator, itens[i])
+        phrase += " %s %s" % (conjunction, itens[len_itens - 1])
+    else:
+        for i in range(1, len_itens):
+            phrase += "%s %s" % (separator, itens[i])
+    return phrase
