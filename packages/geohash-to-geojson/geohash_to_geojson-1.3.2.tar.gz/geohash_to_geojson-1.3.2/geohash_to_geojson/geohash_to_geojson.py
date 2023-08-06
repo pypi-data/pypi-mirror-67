45fc75ef22fc5cd3ef12924f87c31283a58c3c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:12 +0000

    Add package geohash-to-geojson/1.3.2/geohash_to_geojson-1.3.2.tar.gz

---
+++ packages/geohash-to-geojson/geohash_to_geojson-1.3.2.tar.gz/geohash_to_geojson-1.3.2/geohash_to_geojson/geohash_to_geojson.py
@@ -0,0 +1,21 @@
+import geohash as gh
+import shapely.geometry
+import geopandas
+from polygon_geohasher.polygon_geohasher import geohashes_to_polygon
+import json
+
+def geohash_to_geojson(geohashes=['snd1j0']):
+	geojson = []
+	for i in range(0,len(geohashes)):
+		geojson.append(geopandas.GeoSeries([geohashes_to_polygon([geohashes[i]])]).to_json())
+    
+	d = json.loads(geojson[0])
+	for i in range(1,len(geojson)):
+		data=json.loads(geojson[i])
+		feature = {}
+		feature["type"] = "Feature"
+		feature["properties"] = {"id":geohashes[i]}
+		feature["geometry"] = {"type": "Polygon","coordinates":data['features'][0]['geometry']['coordinates'] ,}
+		d['features'].append(feature)
+	y = json.dumps(d)
+	return y
