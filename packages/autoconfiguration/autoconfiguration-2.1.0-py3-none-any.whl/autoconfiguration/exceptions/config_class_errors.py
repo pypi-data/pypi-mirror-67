18ab8dd40dd7072995d5e1e5dec07fe90d3f7509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:34 +0000

    Add package autoconfiguration/2.1.0/autoconfiguration-2.1.0-py3-none-any.whl

---
+++ packages/autoconfiguration/autoconfiguration-2.1.0-py3-none-any.whl/autoconfiguration/exceptions/config_class_errors.py
@@ -0,0 +1,34 @@
+"""Contains exceptions that are raised when errors occur with the Config class."""
+from autoconfiguration.exceptions.base_error import ConfigBaseError
+
+
+class ConfigNotInitializedError(ConfigBaseError):
+    """Raised if the Config class was not initialized."""
+
+    def __init__(self):
+        super().__init__(
+            "The Config class was not initialized! Call the init function first."
+        )
+
+
+class ConfigClassIsNotDataclassError(ConfigBaseError):
+    """Raised if a config class is not a dataclass."""
+
+    def __init__(self):
+        super().__init__(
+            "The Config class or one of its sub classes is not a dataclass!"
+        )
+
+
+class ConfigClassAttributeError(ConfigBaseError):
+    """Raised if an attribute of a config class has no value."""
+
+    def __init__(self, attribute: str):
+        super().__init__(f"The Config class has no attribute '{attribute}'")
+
+
+class InvalidDefaultValueInConfigClassError(ConfigBaseError):
+    """Raised if the default value of a field of the config class was not recognized."""
+
+    def __init__(self, field_name: str):
+        super().__init__(f"The field '{field_name}' of the config class is invalid")
