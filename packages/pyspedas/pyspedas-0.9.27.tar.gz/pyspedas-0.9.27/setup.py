ebe94153be80f6fd0f27ce97fd5413aadbc02275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:46 +0000

    Add package pyspedas/0.9.27/pyspedas-0.9.27.tar.gz

---
+++ packages/pyspedas/pyspedas-0.9.27.tar.gz/pyspedas-0.9.27/setup.py
@@ -0,0 +1,36 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+
+setup(
+    name='pyspedas',
+    version='0.9.27',
+    description='Python Space Physics Environment Data Analysis Software (SPEDAS)',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/spedas/pyspedas',
+    author='Nick Hatzigeorgiu, Eric Grimes',
+    author_email='nikos@berkeley.edu',
+    license='MIT',
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Science/Research',
+                 'Topic :: Scientific/Engineering',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 3',
+                 ],
+    keywords='spedas data tools',
+    project_urls={'Information': 'http://spedas.org/wiki/',
+                  },
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=['requests', 'pytplot>=1.6.3', 'cdflib>=0.3.18',
+                      'cdasws>=1.7.17', 'netCDF4'],
+    python_requires='>=3.5',
+    include_package_data=True,
+)
