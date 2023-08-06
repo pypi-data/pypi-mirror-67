01fe1762e76e636f8cf3dcd6457d7aa386a32338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:34 +0000

    Add package wtc/1.2.1/wtc-1.2.1.tar.gz

---
+++ packages/wtc/wtc-1.2.1.tar.gz/wtc-1.2.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from setuptools import find_packages
+from wtc.__init__ import __version__
+
+with open("README.md", "r", encoding="utf8") as readme:
+    long_description = readme.read()
+
+setup(
+    name = "wtc",
+    version = __version__,
+    description = "Compress osu replay lzma bytestrings",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords = ["osu!, compression, lzma, python, replay, osr"],
+    author = "Chris Miuchiz",
+    author_email = "chrismiuchiz@gmail.com",
+    url = "https://github.com/circleguard/wtc-lzma-compressor",
+    download_url = "https://github.com/circleguard/wtc-lzma-compressor/tarball/v" + __version__,
+    license = "MIT",
+    packages = find_packages()
+)
---
+++ packages/wtc/wtc-1.2.1.tar.gz/wtc-1.2.1/wtc/__init__.py
@@ -0,0 +1,3 @@
+from .wtc import compress, decompress
+
+__version__ = "1.2.1"
