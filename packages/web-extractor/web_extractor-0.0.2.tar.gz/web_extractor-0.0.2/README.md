8d8ad63d1d592ff348733dbd43898b3c67ffdfde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:32 +0000

    Add package web-extractor/0.0.2/web_extractor-0.0.2.tar.gz

---
+++ packages/web-extractor/web_extractor-0.0.2.tar.gz/web_extractor-0.0.2/README.md
@@ -0,0 +1,2 @@
+# WEB Content Extractor
+ 
---
+++ packages/web-extractor/web_extractor-0.0.2.tar.gz/web_extractor-0.0.2/web_extractor/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/2 12:17
+# @Author  : justin.郑 3907721@qq.com
+# @File    : test.py
+# @Desc    :
+
+def cs_test():
+    return "CE测试成功"
