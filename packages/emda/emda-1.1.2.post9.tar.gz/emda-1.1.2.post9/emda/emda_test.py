b2bd0eb0978b9e3c6f5a8dd7914a39c565c34627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:35 +0000

    Add package emda/1.1.2.post9/emda-1.1.2.post9.tar.gz

---
+++ packages/emda/emda-1.1.2.post9.tar.gz/emda-1.1.2.post9/emda/emda_test.py
@@ -0,0 +1,19 @@
+"""
+Author: "Rangana Warshamanage, Garib N. Murshudov"
+MRC Laboratory of Molecular Biology
+
+This software is released under the
+Mozilla Public License, version 2.0; see LICENSE.
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from emda import iotools
+import fcodes_fast
+
+def main():
+    iotools.test()
+    fcodes_fast.test()
+
+if __name__ == '__main__':
+    main()
