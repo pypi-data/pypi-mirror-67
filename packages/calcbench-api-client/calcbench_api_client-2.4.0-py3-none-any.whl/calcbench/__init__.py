d6bd4132ec1d16101a1e2604bd6a9541993beeec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:53 +0000

    Add package calcbench-api-client/2.4.0/calcbench_api_client-2.4.0-py3-none-any.whl

---
+++ packages/calcbench-api-client/calcbench_api_client-2.4.0-py3-none-any.whl/calcbench/__init__.py
@@ -0,0 +1,43 @@
+"""
+Client for the Calcbench API
+
+To turn on verbose logging
+import logging
+logging.getLogger('calcbench.api_client').setLevel(logging.INFO)
+"""
+__version__ = "2.4.0"
+from .api_client import (
+    normalized_data,
+    normalized_dataframe,
+    standardized_data,
+    tickers,
+    set_credentials,
+    set_proxies,
+    companies,
+    normalized_raw,
+    as_reported_raw,
+    dimensional_raw,
+    companies_raw,
+    company_disclosures,
+    disclosure_text,
+    available_metrics,
+    document_search,
+    filings,
+    mapped_raw,
+    point_in_time,
+    document_contents,
+    tag_contents,
+    business_combinations,
+    document_types,
+    html_diff,
+    press_release_raw,
+    document_dataframe,
+    enable_backoff,
+    raw_xbrl_raw,
+    raw_xbrl,
+    DocumentSearchResults,
+    raw_data,
+    raw_data_raw,
+)
+
+from .listener import handle_filings
