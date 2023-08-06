6e8340b97cef21791381e7c4b55bcbd84f211a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:14 +0000

    Add package batcave/38.1.1/BatCave-38.1.1-py3-none-any.whl

---
+++ packages/batcave/BatCave-38.1.1-py3-none-any.whl/batcave/__init__.py
@@ -0,0 +1,19 @@
+﻿'Module initializer and generic utilities'
+__all__ = ('__title__', '__summary__', '__uri__',
+           '__version__', '__buildname__', '__builddate__',
+           '__author__', '__email__',
+           '__license__', '__copyright__')
+
+__title__ = 'BatCave'
+__summary__ = 'Python Programming Toolkit'
+__uri__ = 'https://gitlab.com/arisilon/batcave/'
+
+__version__ = '38.1.1'
+__buildname__ = 'BatCave_38.1.1_0'
+__builddate__ = '2020-05-05 00:43:13.256551'
+
+__author__ = 'Jeff Smith'
+__email__ = 'web@pobox.com'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2019 Jeffery G. Smith'
