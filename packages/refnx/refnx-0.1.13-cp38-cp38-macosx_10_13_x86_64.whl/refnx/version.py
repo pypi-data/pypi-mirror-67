c16ac479d8dd6c3d414b4222012635dbb55d9105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:32 +0000

    Add package refnx/0.1.13/refnx-0.1.13-cp38-cp38-macosx_10_13_x86_64.whl

---
+++ packages/refnx/refnx-0.1.13-cp38-cp38-macosx_10_13_x86_64.whl/refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.1.13'
+version = '0.1.13'
+full_version = '0.1.13'
+git_revision = 'b16b78904f9b12121ae9c8a5ed7afa7ae372f1b4'
+release = True
+if not release:
+    version = full_version
