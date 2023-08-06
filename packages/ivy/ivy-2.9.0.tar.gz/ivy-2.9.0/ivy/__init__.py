9ec6d35115897a0eaf29990f248f7ef6ab965d43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:10 +0000

    Add package ivy/2.9.0/ivy-2.9.0.tar.gz

---
+++ packages/ivy/ivy-2.9.0.tar.gz/ivy-2.9.0/ivy/__init__.py
@@ -0,0 +1,85 @@
+# ------------------------------------------------------------------------------
+# Ivy: a static website generator.
+#
+# Author: Darren Mulholland <dmulholl@tcd.ie>
+# License: Public Domain
+# ------------------------------------------------------------------------------
+
+import sys
+
+
+# Application version number.
+__version__ = '2.9.0'
+
+
+# Ivy requires at least Python 3.6.
+if sys.version_info < (3, 6):
+    sys.exit('Error: Ivy requires Python 3.6 or later.')
+
+
+# Template for error messages informing the user of any missing dependencies.
+errorstr = """Error: Ivy requires the %s library. Try:
+
+    $ pip install %s"""
+
+
+# Check that the application's required dependencies are available.
+try:
+    import janus
+except ImportError:
+    sys.exit(errorstr % ('Janus', 'libjanus'))
+try:
+    import shortcodes
+except ImportError:
+    sys.exit(errorstr % ('Shortcodes', 'shortcodes'))
+
+
+# On Windows, use colorama to support ANSI terminal codes.
+if sys.platform == 'win32':
+    try:
+        import colorama
+    except ImportError:
+        pass
+    else:
+        colorama.init()
+
+
+# We import the package's modules so users can access 'ivy.foo' via a simple
+# 'import ivy' statement. Otherwise the user would have to import each module
+# individually as 'import ivy.foo'.
+from . import cli
+from . import extensions
+from . import hashes
+from . import hooks
+from . import includes
+from . import loader
+from . import nodes
+from . import pages
+from . import renderers
+from . import site
+
+
+# Application entry point. Calling main() initializes the site model, loads
+# the site's plugins, and then fires a sequence of event hooks. All of Ivy's
+# functionality is handled by callbacks registered on these hooks.
+def main():
+
+    # Initialize the site model.
+    site.init()
+
+    # Load bundled plugins, plugins listed in the site's configuration file,
+    # and plugins in the site's 'ext' directory.
+    extensions.load_bundled_extensions()
+    extensions.load_installed_extensions()
+    extensions.load_site_extensions()
+
+    # Process the application's command-line arguments.
+    cli.parse()
+
+    # Load any plugins bundled with the active theme.
+    extensions.load_theme_extensions()
+
+    # Fire the primary sequence of event hooks.
+    hooks.event('init')
+    hooks.event('main')
+    hooks.event('exit')
---
+++ packages/ivy/ivy-2.9.0.tar.gz/ivy-2.9.0/ivy/skeleton/inc/menu.md
@@ -0,0 +1,10 @@
+---
+# This is a sample menu file. This file should contain a list of links,
+# optionally with nested sub-lists.
+---
+
+* [Home](@root/)
+* [Parent Page](@root/parent//)
+    * [Child One](@root/parent/child-one//)
+    * [Child Two](@root/parent/child-two//)
+* [About](@root/about//)
