4c5bbeb95227e8ce06d088036c646148e24137db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:18 +0000

    Add package facile-new-business-lib/0.0.7.1.22/facile-new-business-lib-0.0.7.1.22.tar.gz

---
+++ packages/facile-new-business-lib/facile-new-business-lib-0.0.7.1.22.tar.gz/facile-new-business-lib-0.0.7.1.22/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='facile-new-business-lib',
+    version='0.0.7.1.22',
+    description='Keywords for facile newbusiness project!',
+    package_dir={'': 'src'},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/f46io",
+    author="Fabio Lima",
+    author_email="f46io@icloud.com",
+    install_requires=[
+        "selenium ~= 3.141.0",
+        "robotframework ~= 3.2",
+    ],
+    Classfiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: MIT",
+        "Operating System  ::  OS Independent",
+        "Facile.it New Business help out lib "
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=3.7",
+        ],
+    },
+)
