404056e9e0c9599b6e6c20ec789d1e73f2740aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:22 +0000

    Add package pytruelayer/0.3.0/pytruelayer-0.3.0-py3-none-any.whl

---
+++ packages/pytruelayer/pytruelayer-0.3.0-py3-none-any.whl/truelayer/auth/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from truelayer.auth.api.auth_api import AuthApi
+from truelayer.auth.api.data_api import DataApi
