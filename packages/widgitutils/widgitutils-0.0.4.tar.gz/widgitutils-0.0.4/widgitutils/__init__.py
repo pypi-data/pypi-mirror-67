77de7253f11043eaa0a0506b78e9e350c0398f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:45 +0000

    Add package widgitutils/0.0.4/widgitutils-0.0.4.tar.gz

---
+++ packages/widgitutils/widgitutils-0.0.4.tar.gz/widgitutils-0.0.4/widgitutils/__init__.py
@@ -0,0 +1,17 @@
+"""
+WidgitUtils
+===========
+
+A suite of common-use utilities for our cogs.
+
+Current methods include:
+
+- embeds.post
+- color.from_hex
+- color.get
+- color.pick
+
+
+:copyright: (c) 2020 Widgit Labs
+:license: MIT, see LICENSE for more details.
+"""
