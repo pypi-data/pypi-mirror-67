bf62ab87543662d89576ed78ed2c78d35f4ca81d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:51 +0000

    Add package metadsl_core/0.4.0/metadsl_core-0.4.0-py2.py3-none-any.whl

---
+++ packages/metadsl_core/metadsl_core-0.4.0-py2.py3-none-any.whl/metadsl_core/boolean_test.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+
+from metadsl import *
+from metadsl_rewrite import *
+from .boolean import *
+
+
+class TestBoolean:
+    def test_if(self):
+        assert execute(Boolean.create(True).if_(0, 1)) == 0
+        assert execute(Boolean.create(False).if_(0, 1)) == 1
---
+++ packages/metadsl_core/metadsl_core-0.4.0-py2.py3-none-any.whl/metadsl_core/pair_test.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from metadsl import *
+from metadsl_rewrite import *
+
+from .integer import *
+from .pair import *
+
+
+class TestPair:
+    def test_left(self):
+        assert execute(Pair.create(1, 2).left) == 1
+
+    def test_right(self):
+        assert execute(Pair.create(1, 2).right) == 2
+
+    def test_left_type(self):
+        assert execute(
+            Pair.create(Integer.from_int(1), 2).left + Integer.from_int(1)
+        ) == Integer.from_int(2)
