12b9b6826a35d448bc8bb2355421fff3646aafe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:48 +0000

    Add package ezephys/0.4.1/ezephys-0.4.1.tar.gz

---
+++ packages/ezephys/ezephys-0.4.1.tar.gz/ezephys-0.4.1/setup.py
@@ -0,0 +1,36 @@
+"""Install ezephys."""
+
+import setuptools
+import os
+import re
+
+currdir = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(currdir, 'ezephys', '__init__.py'), 'r') as f:
+    version = re.search(r"__version__ = '([^']+)'", f.read()).group(1)
+
+setuptools.setup(
+    name='ezephys',
+    version=version,
+    description='Tools for working with electrophysiological data.',
+    license='MIT',
+    author='Emerson Harkin',
+    author_email='emerson.f.harkin@gmail.com',
+    url='https://github.com/efharkin/ez-ephys',
+    download_url='https://pypi.org/project/ez-ephys/',
+    keywords='neuroscience,electrophysiology',
+    packages=['ezephys'],
+    install_requires=[
+        'matplotlib',
+        'seaborn',
+        'numpy',
+        'neo',
+        'numba==0.43.1',
+        'llvmlite==0.28.0',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+)
