fbb49876f2112e0f7033ecbc53db4db0128f1159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:14 +0000

    Add package minrest/0.0.61/minrest-0.0.61-py2.py3-none-any.whl

---
+++ packages/minrest/minrest-0.0.61-py2.py3-none-any.whl/minrest/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for Generic REST Client."""
+
+__author__ = """Leonardo Diegues"""
+__email__ = 'dscleo@gmail.com'
+__version__ = '0.0.1'
---
+++ packages/minrest/minrest-0.0.61-py2.py3-none-any.whl/minrest/minrest.py
@@ -0,0 +1,34 @@
+"""Main module."""
+import re
+import inspect
+import requests
+from urllib.parse import urljoin
+
+from minrest import parser
+
+def build_url(api_url, *args):
+    api_url = re.sub(r'\/$', '', api_url)
+    endpoint = '/'.join(list(map(lambda x: re.sub(r'^\/|\/$', '', x), args)))
+    return api_url + '/' + endpoint
+
+
+class GenericClient(object):
+
+    def __init__(self, api_url, endpoints):
+        self.api_url = api_url
+        self.endpoints = endpoints
+
+    def call(self, endpoint, *args, **kwargs):
+        if not self.is_valid_endpoint(endpoint):
+            raise ValueError(f'Invalid endpoint: {endpoint}')
+
+        return (
+            requests
+            .request(
+                urljoin(self.api_url, endpoint),
+                *args, **kwargs
+            )
+        )
+
+    def _is_valid_endpoint(self, endpoint):
+        return endpoint in self.endpoints
