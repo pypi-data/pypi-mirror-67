364d5baf08ca5a8b10372cd607a51707ace8d63b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:24 +0000

    Add package pettingzoo/0.1.2/PettingZoo-0.1.2.tar.gz

---
+++ packages/pettingzoo/PettingZoo-0.1.2.tar.gz/PettingZoo-0.1.2/pettingzoo/mpe/simple_crypto.py
@@ -0,0 +1,9 @@
+from ._mpe_utils.simple_env import SimpleEnv
+from .scenarios.simple_crypto import Scenario
+
+
+class env(SimpleEnv):
+    def __init__(self, seed=None, max_frames=100):
+        scenario = Scenario()
+        world = scenario.make_world()
+        super(env, self).__init__(scenario, world, max_frames, seed)
