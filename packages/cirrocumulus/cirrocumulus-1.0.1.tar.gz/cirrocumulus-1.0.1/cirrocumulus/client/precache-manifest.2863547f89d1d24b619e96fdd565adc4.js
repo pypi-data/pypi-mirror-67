de784fd741a0d37d4eb15b8d335df574b5edad2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:59 +0000

    Add package cirrocumulus/1.0.1/cirrocumulus-1.0.1.tar.gz

---
+++ packages/cirrocumulus/cirrocumulus-1.0.1.tar.gz/cirrocumulus-1.0.1/cirrocumulus/client/precache-manifest.2863547f89d1d24b619e96fdd565adc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1165f6b3fdb9281384e34984a5626f3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c01ea2847c29a8303e49",
+    "url": "/static/js/2.fdf16ebc.chunk.js"
+  },
+  {
+    "revision": "0b26c0824b12d8dcb9041afb66cff21c",
+    "url": "/static/js/2.fdf16ebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69ddc7c8aa7f0a9c66cb",
+    "url": "/static/js/main.d1546825.chunk.js"
+  },
+  {
+    "revision": "9e766b7ee20fa086f639",
+    "url": "/static/js/runtime-main.e853a34d.js"
+  }
+]);
