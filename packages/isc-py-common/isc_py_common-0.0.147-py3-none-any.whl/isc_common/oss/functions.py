0954a94966dae8c9e1003105ae9e2299826afca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:33 +0000

    Add package isc-py-common/0.0.147/isc_py_common-0.0.147-py3-none-any.whl

---
+++ packages/isc-py-common/isc_py_common-0.0.147-py3-none-any.whl/isc_common/oss/functions.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+
+def mekeDirs(name, mode=0o777, exist_ok=False):
+    os.makedirs(name=name, exist_ok=exist_ok, mode=mode)
+    if not os.path.exists(name):
+        raise Exception(f'dirs: {name} not created.')
+
+
+def copyTwo(src, dst, follow_symlinks=True):
+    dir, _file = os.path.split(dst)
+    os.chdir(dir)
+    try:
+        shutil.copy2(src=src, dst=_file, follow_symlinks=follow_symlinks)
+        return True
+    except FileNotFoundError:
+        return False
