9923328c7e188fdd97382a69de324e9ea18bae8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:38 +0000

    Add package grapl-analyzerlib/0.2.57/grapl_analyzerlib-0.2.57.tar.gz

---
+++ packages/grapl-analyzerlib/grapl_analyzerlib-0.2.57.tar.gz/grapl_analyzerlib-0.2.57/README.md
@@ -0,0 +1,4 @@
+# grapl_analyzerlib
+A library for working with [Grapl](https://github.com/insanitybit/grapl)
+
+[readthedocs](https://grapl-analyzerlib.readthedocs.io/en/latest/)
---
+++ packages/grapl-analyzerlib/grapl_analyzerlib-0.2.57.tar.gz/grapl_analyzerlib-0.2.57/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="grapl_analyzerlib",
+    version="0.2.57",
+    description="Library for Grapl Analyzers",
+    url="https://github.com/insanitybit/grapl_analyzerlib/",
+    author="insanitybit",
+    author_email="insanitybit@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    zip_safe=False,
+    packages=find_packages(),
+    package_data={
+        'grapl_analyzerlib': ["py.typed"],
+        'grapl_analyzerlib.nodes': ["nodes/py.typed"],
+        'grapl_analyzerlib.schemas': ["schemas/py.typed"],
+    },
+    include_package_data=True,
+    install_requires=["pydgraph", "typing_extensions"],
+)
