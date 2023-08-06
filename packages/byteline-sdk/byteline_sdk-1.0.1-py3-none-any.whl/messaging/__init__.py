53a2f79dd1df5582a49bd8fa628176fd3c89b51b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:25 +0000

    Add package byteline-sdk/1.0.1/byteline_sdk-1.0.1-py3-none-any.whl

---
+++ packages/byteline-sdk/byteline_sdk-1.0.1-py3-none-any.whl/messaging/__init__.py
@@ -0,0 +1,37 @@
+import requests
+
+
+def send_email(apiKey, subject, body, emails, sender='default', reply_addresses=''):
+    resp = requests.post(_url('email'), auth={}, json={
+        'to': {
+            'addresses': emails.split(',')
+        },
+        'subject': subject,
+        'body': {
+            'html': body
+        },
+        'replyToAddresses': reply_addresses.split(','),
+        'sender': sender
+    })
+    if resp.status_code != 200:
+        raise RuntimeError('Cannot send email - HTTP status {}'.format(resp.status_code))
+    else:
+        print('Email sent successfully!')
+
+
+def send_templated_email(apiKey, templateId, emails, params={}):
+    resp = requests.post(_url('email/templated'), auth={}, json={
+        'to': {
+            'addresses': emails.split(',')
+        },
+        'templateId': templateId,
+        'parameters': params
+    })
+    if resp.status_code != 200:
+        raise RuntimeError('Cannot send templated email - HTTP status {}'.format(resp.status_code))
+    else:
+        print('Email sent successfully!')
+
+
+def _url(path):
+    return 'https://api.byteline.io/' + path
