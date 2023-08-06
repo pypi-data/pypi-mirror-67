f6c6dd88b36f4e638775a649d53e4c62ede493ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:39 +0000

    Add package async-lichess-sdk/1.1.0.7/async_lichess_sdk-1.1.0.7-py3-none-any.whl

---
+++ packages/async-lichess-sdk/async_lichess_sdk-1.1.0.7-py3-none-any.whl/lichess_client/abstract_endpoints/abstract_chess_bot.py
@@ -0,0 +1,6 @@
+from abc import ABC
+
+
+class AbstractChessBot(ABC):
+    """An abstract class for Chess Bot API Endpoint"""
+    pass
---
+++ packages/async-lichess-sdk/async_lichess_sdk-1.1.0.7-py3-none-any.whl/tests/utils/__init__.py
@@ -0,0 +1,1 @@
+from tests.utils.utils import async_test, get_token_from_config
