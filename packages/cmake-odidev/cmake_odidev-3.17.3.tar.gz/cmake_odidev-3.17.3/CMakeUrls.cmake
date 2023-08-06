3d17e675a75690769e7a88283d89b3e370f02d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:05 +0000

    Add package cmake-odidev/3.17.3/cmake_odidev-3.17.3.tar.gz

---
+++ packages/cmake-odidev/cmake_odidev-3.17.3.tar.gz/cmake_odidev-3.17.3/CMakeUrls.cmake
@@ -0,0 +1,26 @@
+
+#-----------------------------------------------------------------------------
+# CMake sources
+set(unix_source_url       "https://github.com/Kitware/CMake/releases/download/v3.16.3/cmake-3.16.3.tar.gz")
+set(unix_source_sha256    "e54f16df9b53dac30fd626415833a6e75b0e47915393843da1825b096ee60668")
+
+set(windows_source_url    "https://github.com/Kitware/CMake/releases/download/v3.16.3/cmake-3.16.3.zip")
+set(windows_source_sha256 "f66fd5a506df762b7f0de4c8e5c5b1f05b01a0d3831c1303911f273cd16dcc12")
+
+#-----------------------------------------------------------------------------
+# CMake binaries
+
+set(linux32_binary_url    "NA")  # Linux 32-bit binaries not available
+set(linux32_binary_sha256 "NA")
+
+set(linux64_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.16.3/cmake-3.16.3-Linux-x86_64.tar.gz")
+set(linux64_binary_sha256 "3e15dadfec8d54eda39c2f266fc1e571c1b88bf32f9d221c8a039b07234206fa")
+
+set(macosx_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.16.3/cmake-3.16.3-Darwin-x86_64.tar.gz")
+set(macosx_binary_sha256 "655d6ed41a1c276676ca6a1ec381c179d394420c489f2d39b3cf2ef26bfae799")
+
+set(win32_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.16.3/cmake-3.16.3-win32-x86.zip")
+set(win32_binary_sha256 "6c6ca229b2c7b503aed90ac01fa75b9165e2cdbf18b5e0629658c13c658e3647")
+
+set(win64_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.16.3/cmake-3.16.3-win64-x64.zip")
+set(win64_binary_sha256 "4b1370b3252acda0850d26c75e9bc6b8e019daaa7978a19f5d8dc008450d3548")
---
+++ packages/cmake-odidev/cmake_odidev-3.17.3.tar.gz/cmake_odidev-3.17.3/README.rst
@@ -0,0 +1,19 @@
+==========================
+CMake Python Distributions
+==========================
+
+* I am working on a generatting aarch64 cmake wheel
+* This project is where I am testing the things
+* You must not use this project to grab numpy features
+* I am expecting it to go away at some point in the future
+* Please visit official cmake pypi page: https://pypi.org/project/cmake/ to install cmake
+
+
+License
+-------
+
+This project is maintained by Jean-Christophe Fillion-Robin from Kitware Inc.
+It is covered by the `Apache License, Version 2.0 <http://www.apache.org/licenses/LICENSE-2.0>`_.
+
+CMake is distributed under the OSI-approved BSD 3-clause License.
+For more information about CMake, visit http://cmake.org
