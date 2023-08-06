bd88b62959b27190d04cbd4ff812006c3424a9fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:34 +0000

    Add package pyglobal/0.0.1/pyglobal-0.0.1-cp36-cp36m-win32.whl

---
+++ packages/pyglobal/pyglobal-0.0.1-cp36-cp36m-win32.whl/pyglobal/__meta__.py
@@ -0,0 +1,6 @@
+name = 'pyglobal'
+version = '0.0.1'
+description = 'Global accessors for things like settings. This could be used across applications and modules to share data.'
+url = 'https://github.com/justengel/pyglobal'
+author = 'Justin Engel'
+author_email = 'jtengel08@gmail.com'
