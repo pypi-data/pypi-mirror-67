d3b0057d8b3dcd10aff8bb3cb80fde81622a732b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:48 +0000

    Add package texthero/1.0.6/texthero-1.0.6.tar.gz

---
+++ packages/texthero/texthero-1.0.6.tar.gz/texthero-1.0.6/texthero/__init__.py
@@ -0,0 +1,17 @@
+"""Texthero: python toolkit for text preprocessing, representation and visualization.
+
+
+
+"""
+
+from . import preprocessing
+from .preprocessing import *
+
+from . import representation
+from .representation import *
+
+from . import visualization
+from .visualization import *
+
+from . import __about__
+from .__about__ import __version__
