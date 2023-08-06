40d022bf48f4b09e8b5f3c9f0000acbab7ad8f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:30 +0000

    Add package pyotr/0.5.0/pyotr-0.5.0-py3-none-any.whl

---
+++ packages/pyotr/pyotr-0.5.0-py3-none-any.whl/pyotr/validation/responses/client.py
@@ -0,0 +1,9 @@
+from openapi_core.validation.response.datatypes import OpenAPIResponse
+
+
+class ClientOpenAPIResponseFactory(object):
+    @classmethod
+    def create(cls, response):
+        return OpenAPIResponse(
+            data=response.content, status_code=response.status_code, mimetype=response.headers.get("content-type"),
+        )
