8647afba2ae5935faa507c7c5f8d56bd7187954c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:40 +0000

    Add package chromwindow/1.1/chromwindow-1.1.tar.gz

---
+++ packages/chromwindow/chromwindow-1.1.tar.gz/chromwindow-1.1/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(name='chromwindow',
+      version='1.1',
+      description='Utilities for working with windows on a chromosome.',
+      url='https://github.com/kaspermunch/chromwindow',
+      author='Kasper Munch',
+      packages=['chromwindow'],
+      zip_safe=False)
+
