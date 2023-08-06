19292775f156944b81302827edf186607125654b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:52 +0000

    Add package pysample55321/1.0.6/pysample55321-1.0.6.tar.gz

---
+++ packages/pysample55321/pysample55321-1.0.6.tar.gz/pysample55321-1.0.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+setup(
+    name = "pysample55321",
+    version = "1.0.6",
+    description = "A sample python package with cpp",
+    long_description = "README.md",
+    classifiers = ["Development Status :: 1 - Planning", "Environment :: Console", "Intended Audience :: Developers", "License :: OSI Approved :: MIT License", "Operating System :: POSIX", "Programming Language :: Python :: 3.7"],
+    keywords = "sample hello test",
+    url = "http://github.com/horia141/tabletest",
+    author = "Mansoor Nasir",
+    author_email = "cheema@fortiss.org",
+    license = "MIT",
+    packages=["pysample55321"],
+    install_requires=[],
+    test_suite = "nose.collector",
+    tests_require=["nose"],
+    package_data={"pysample55321": ["../main/pyhellocpp.so"]},
+    zip_safe=False,
+)
+
