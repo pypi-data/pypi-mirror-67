d10a31dd38e1a600226d23e74bc9c8f723896200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:17 +0000

    Add package sds/2.0/sds-2.0.tar.gz

---
+++ packages/sds/sds-2.0.tar.gz/sds-2.0/sds/__init__.py
@@ -0,0 +1,23 @@
+from sds.standard import (
+    Agent,
+    D_passive,
+    DH_uniform,
+    H_fixed,
+    I_sync,
+    SDS,
+    Swarm,
+    T_boolean,
+    TM_uniform,
+)
+
+__all__ = [
+    "Agent",
+    "D_passive",
+    "DH_uniform",
+    "H_fixed",
+    "I_sync",
+    "SDS",
+    "Swarm",
+    "T_boolean",
+    "TM_uniform",
+]
