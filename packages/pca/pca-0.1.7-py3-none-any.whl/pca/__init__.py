2dd438c5954648d68837cd4fe0190d2b949c787d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:49 +0000

    Add package pca/0.1.7/pca-0.1.7-py3-none-any.whl

---
+++ packages/pca/pca-0.1.7-py3-none-any.whl/pca/__init__.py
@@ -0,0 +1,41 @@
+from pca.pca import (
+    fit,
+    biplot,
+    biplot3d,
+    plot,
+    norm,
+)
+
+__author__ = 'Erdogan Tasksen'
+__email__ = 'erdogant@gmail.com'
+__version__ = '0.1.7'
+
+# module level doc-string
+__doc__ = """
+pca
+=====================================================================
+
+Description
+-----------
+pca is a python package that performs the principal component analysis and to make insightful plots.
+
+Examples
+--------
+>>> # Load example data
+>>> X = load_iris().data
+>>> labels = load_iris().feature_names
+>>> y = load_iris().target
+>>> Fit using PCA
+>>> import pca
+>>> model = pca.fit(X, row_labels=y, col_labels=labels)
+>>> ax = pca.biplot(model) 
+>>> ax = pca.biplot3d(model)
+>>> ax = pca.plot(model)
+>>> X_norm = pca.norm(X)
+
+References
+----------
+* https://github.com/erdogant/pca
+* https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.SparsePCA.html
+
+"""
