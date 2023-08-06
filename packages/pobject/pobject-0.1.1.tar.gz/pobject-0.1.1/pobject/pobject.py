55bb50bdaabff2c5097fed1952801b1b1fdcb7ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:06 +0000

    Add package pobject/0.1.1/pobject-0.1.1.tar.gz

---
+++ packages/pobject/pobject-0.1.1.tar.gz/pobject-0.1.1/pobject/pobject.py
@@ -0,0 +1,35 @@
+from .file import File
+
+
+class P:
+
+    def __init__(self, input):
+        self._input = input
+
+    @property
+    def input(self):
+        return self._input
+
+    @property
+    def is_string(self):
+        return isinstance(self.input, str)
+
+    @property
+    def file(self):
+        """
+        :rtype: pobject.file.File
+        """
+        return File(self.input)
+
+    @property
+    def is_file_path(self):
+        if self.is_string and '.yaml' in self.input:
+            return True
+        else:
+            raise NotImplementedError
+
+    def to_dict(self):
+        if self.is_file_path:
+            return self.file.to_dict()
+
+
