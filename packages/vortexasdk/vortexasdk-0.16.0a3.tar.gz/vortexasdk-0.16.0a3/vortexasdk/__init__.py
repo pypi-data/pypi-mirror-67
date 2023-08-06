fcaaf197ee0bd67add5d40cd5219e9b2c94ef9e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:16 +0000

    Add package vortexasdk/0.16.0a3/vortexasdk-0.16.0a3.tar.gz

---
+++ packages/vortexasdk/vortexasdk-0.16.0a3.tar.gz/vortexasdk-0.16.0a3/vortexasdk/__init__.py
@@ -0,0 +1,10 @@
+"""Vortexa Python SDK."""
+from vortexasdk.endpoints import (
+    CargoMovements,
+    CargoTimeSeries,
+    Corporations,
+    Geographies,
+    Products,
+    VesselMovements,
+    Vessels,
+)
---
+++ packages/vortexasdk/vortexasdk-0.16.0a3.tar.gz/vortexasdk-0.16.0a3/vortexasdk/utils.py
@@ -0,0 +1,16 @@
+from typing import Dict, List
+
+
+def convert_to_list(a) -> List:
+    """Convert wraps element in list if element isn't a list already."""
+    if a is None:
+        return []
+    elif isinstance(a, list):
+        return a
+    else:
+        return [a]
+
+
+def convert_values_to_list(data: Dict) -> Dict:
+    """Convert each value to a list."""
+    return {k: convert_to_list(v) for k, v in data.items()}
