93f6e91acae3b22ca5ef729834a4597af92e620d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:08 +0000

    Add package vim-unitex/0.0.1/vim_unitex-0.0.1-py3-none-any.whl

---
+++ packages/vim-unitex/vim_unitex-0.0.1-py3-none-any.whl/vim_unitex/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ["subsuperscripts_table", "combiningmarks_table",
+           "replacements_table", "replace"]
+
+from .unitex import subsuperscripts_table, combiningmarks_table
+from .unitex import replacements_table, replace
