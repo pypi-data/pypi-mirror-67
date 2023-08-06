1201a66407b60231c17600e4d91449c8c290ce21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:26 +0000

    Add package Snookey2/3.0/Snookey2-3.0.tar.gz

---
+++ packages/Snookey2/Snookey2-3.0.tar.gz/Snookey2-3.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='Snookey2',
+      author='IOnlyPlayAsDrift',
+      version='3.0',
+      description='Stream on RPAN on PC easily using OBS and this module!',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/IOnlyPlayAsDrift/Snookey2',
+      install_requires=[
+          'pytz',
+          'requests', 
+          'pypresence'
+      ],
+      py_modules = ['snookey2'],
+      entry_points={'console_scripts': ['snookey=snookey2:main']}
+      ) 
