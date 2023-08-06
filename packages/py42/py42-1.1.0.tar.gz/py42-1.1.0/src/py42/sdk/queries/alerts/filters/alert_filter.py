8dd40583583532ae024d17d5827df38920cd7eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:25 +0000

    Add package py42/1.1.0/py42-1.1.0.tar.gz

---
+++ packages/py42/py42-1.1.0.tar.gz/py42-1.1.0/src/py42/sdk/queries/alerts/filters/alert_filter.py
@@ -0,0 +1,81 @@
+from py42.sdk.queries.query_filter import (
+    QueryFilterStringField,
+    QueryFilterTimestampField,
+    create_filter_group,
+    create_query_filter,
+)
+
+
+def create_contains_filter_group(term, value):
+    filter_list = [create_query_filter(term, u"CONTAINS", value)]
+    return create_filter_group(filter_list, u"AND")
+
+
+def create_not_contains_filter_group(term, value):
+    filter_list = [create_query_filter(term, u"DOES_NOT_CONTAIN", value)]
+    return create_filter_group(filter_list, u"AND")
+
+
+class AlertQueryFilterStringField(QueryFilterStringField):
+    @classmethod
+    def contains(cls, value):
+        return create_contains_filter_group(cls._term, value)
+
+    @classmethod
+    def not_contains(cls, value):
+        return create_not_contains_filter_group(cls._term, value)
+
+
+class DateObserved(QueryFilterTimestampField):
+    """Class that filters alerts based on the timestamp the alert was triggered."""
+
+    _term = u"createdAt"
+
+
+class Actor(AlertQueryFilterStringField):
+    """Class that filters alerts based on the username that originated the event(s) that
+    triggered the alert."""
+
+    _term = u"actor"
+
+
+class RuleName(AlertQueryFilterStringField):
+    """Class that filters alerts based on rule name."""
+
+    _term = u"name"
+
+
+class Description(AlertQueryFilterStringField):
+    """Class that filters alerts based on rule description text."""
+
+    _term = u"description"
+
+
+class Severity(QueryFilterStringField):
+    """Class that filters alerts based on severity.
+
+    Available options are:
+        - :attr:`Severity.HIGH`
+        - :attr:`Severity.MEDIUM`
+        - :attr:`Severity.LOW`
+    """
+
+    _term = u"severity"
+
+    HIGH = u"HIGH"
+    MEDIUM = u"MEDIUM"
+    LOW = u"LOW"
+
+
+class AlertState(QueryFilterStringField):
+    """Class that filters alerts based on alert state.
+
+    Available options are:
+        - :attr:`AlertState.OPEN`
+        - :attr:`AlertState.DISMISSED`
+    """
+
+    _term = u"state"
+
+    OPEN = u"OPEN"
+    DISMISSED = u"RESOLVED"
