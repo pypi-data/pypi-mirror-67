a0bee549d43cdf8b206b281122fc8962e60c87b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:32 +0000

    Add package eflow/0.2.41/eflow-0.2.41.tar.gz

---
+++ packages/eflow/eflow-0.2.41.tar.gz/eflow-0.2.41/eflow/data_pipeline_segments/__init__.py
@@ -0,0 +1,4 @@
+from .feature_transformer import FeatureTransformer
+from .type_fixer import TypeFixer
+from .data_encoder import DataEncoder
+from .feature_data_cleaner import FeatureDataCleaner
---
+++ packages/eflow/eflow-0.2.41.tar.gz/eflow-0.2.41/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+import os
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# raise Exception(os.getcwd())
+
+MAIN_PACKAGES = [i.strip() for i in open("extra_setup/main_packages.txt").readlines()]
+
+setuptools.setup(
+     name='eflow',
+     version='0.2.41',
+     scripts=['init_env'],
+     author="Eric Cacciavillani",
+     author_email="eric.cacciavillani@gmail.com",
+     description="",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/EricCacciavillani/eFlow",
+     packages=setuptools.find_packages(),
+     install_requires=MAIN_PACKAGES,
+     classifiers=[
+         "Programming Language :: Python :: 3.7",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
