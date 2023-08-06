1a1823bf113bdc406f689f210878880ff7367a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:57 +0000

    Add package pydocsearch/1.0.2/pydocsearch-1.0.2.tar.gz

---
+++ packages/pydocsearch/pydocsearch-1.0.2.tar.gz/pydocsearch-1.0.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="pydocsearch",
+    version="1.0.2",
+    description="Search a page in docs.python.org",
+    long_description=open("README.rst", "rt").read(),
+    author="Julien Palard",
+    author_email="julien@palard.fr",
+    keywords=["pydoc", "docs.python.org", "search"],
+    url="https://github.com/JulienPalard/pydocsearch",
+    download_url="https://github.com/julienpalard/pydocsearch/tarball/1.0.2",
+    py_modules=["pydocsearch"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+    ],
+    entry_points={"console_scripts": ["pydocsearch=pydocsearch:main"]},
+)
