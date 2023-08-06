d4bb59549113121ef6ce5a9302d80a71e8cbdc2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:57 +0000

    Add package black-mamba/0.3.4/black-mamba-0.3.4.tar.gz

---
+++ packages/black-mamba/black-mamba-0.3.4.tar.gz/black-mamba-0.3.4/black_mamba/initialization/init_files/settings_sample.py
@@ -0,0 +1,44 @@
+import os
+
+
+networks = {
+    "development": {
+        "mode": "HTTP",
+        "host": "localhost",
+        "port": 7545,
+        "network_id": "*"
+    }
+
+    #"development": {
+    #    "mode": "IPC",
+    #    "url": "/home/sarahconnor/ethereum/data/geth.ipc"
+    #}
+
+    #"development": {
+    #    "mode": "Websocket",
+    #    "host": "localhost",
+    #    "port": 8546,
+    #    "network_id": "*"
+    #}
+
+    #"development": {
+    #    "mode": "IPC",
+    #    "url": "/home/sarahconnor/ethereum/data/geth.ipc"
+    #}
+
+    #"development": {
+    #    "mode": "Infura",
+    #    "project_id": "FILL_IT_WITH_PROJECT_ID",
+    #    "api_secret": "FILL_IT_WITH_API_SECRET",
+    #    "scheme": "https / wss",
+    #    "endpoints": "mainnet / ropsten / goerly / rinkeby / kovan"
+    #}
+}
+
+auth = {
+    "development": {
+        #"private_key": os.environ("MY_PRIVATE_KEY")
+        "password": "password_to_unlock_keyfile",
+        "keyfile": "keyfile.json"
+    }
+}
---
+++ packages/black-mamba/black-mamba-0.3.4.tar.gz/black-mamba-0.3.4/black_mamba/testlib/test_with_contracts.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+
+
+class TestWithContracts():
+
+    tmp_path = Path('/tmp') # TODO: Change this into dynamic temp directory
+
+    def setup_method(self, method):
+        self.fixtures_dir = Path('.') / Path('test') / Path('fixtures')
+        self.build_dir = self.tmp_path / Path('build')
+        self.migrations_dir = self.tmp_path / Path("migrations")
+        self.decentralized_app_dir = self.tmp_path / Path("decentralized_app")
+        self.build_contracts_dir = self.tmp_path / Path('build') / Path('contracts')
+        self.json_compiled_fixture = self.fixtures_dir / Path("HelloWorld.json")
+        self.json_compiled_file = self.build_contracts_dir / Path('HelloWorld.json')
+        self.migration_file = self.migrations_dir / Path("deploy_HelloWorld.py")
+        self.migration2_file = self.migrations_dir / Path("deploy_HelloParameters.py")
+        self.json2_compiled_fixture = self.fixtures_dir / Path("HelloParameters.json")
+        self.json2_compiled_file = self.build_contracts_dir / Path('HelloParameters.json')
+        self.keyfile2_file = self.tmp_path / Path('keyfile2.json')
+
+        if not self.build_dir.exists():
+            self.build_dir.mkdir()
+        if not self.build_contracts_dir.exists():
+            self.build_contracts_dir.mkdir()
+        if not self.migrations_dir.exists():
+            self.migrations_dir.mkdir()
+        if not self.decentralized_app_dir.exists():
+            self.decentralized_app_dir.mkdir()
+
+    def teardown_method(self, method):
+        if self.json_compiled_file.exists():
+            self.json_compiled_file.unlink()
+        if self.json2_compiled_file.exists():
+            self.json2_compiled_file.unlink()
+        if self.migration_file.exists():
+            self.migration_file.unlink()
+        if self.migration2_file.exists():
+            self.migration2_file.unlink()
+        if self.build_contracts_dir.exists():
+            self.build_contracts_dir.rmdir()
+        if self.build_dir.exists():
+            self.build_dir.rmdir()
+        if self.migrations_dir.exists():
+            self.migrations_dir.rmdir()
+        if self.decentralized_app_dir.exists():
+            self.decentralized_app_dir.rmdir()
+        if self.keyfile2_file.exists():
+            self.keyfile2_file.unlink()
