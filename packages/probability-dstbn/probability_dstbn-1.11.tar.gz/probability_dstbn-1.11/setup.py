077b06299694a83ca8c3ad15e128e9c207b90873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:38 +0000

    Add package probability-dstbn/1.11/probability_dstbn-1.11.tar.gz

---
+++ packages/probability-dstbn/probability_dstbn-1.11.tar.gz/probability_dstbn-1.11/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_dstbn',
+      version='1.11',
+      description='Gaussian and Binomial distribution simplified',
+      packages=['probability_dstbn'],
+      author= 'Pratik Poudel',
+      author_email= 'xpresspratik@gmail.com',
+      zip_safe=False)
