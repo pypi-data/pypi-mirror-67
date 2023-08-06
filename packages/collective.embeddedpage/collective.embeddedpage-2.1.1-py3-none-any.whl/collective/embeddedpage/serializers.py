9133bff7f586ad0794ad52b5c470974502c01074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:57 +0000

    Add package collective.embeddedpage/2.1.1/collective.embeddedpage-2.1.1-py3-none-any.whl

---
+++ packages/collective.embeddedpage/collective.embeddedpage-2.1.1-py3-none-any.whl/collective/embeddedpage/serializers.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from collective.embeddedpage.interfaces import ICollectiveEmbeddedpageLayer
+from collective.embeddedpage.interfaces import IEmbeddedPage
+from plone.restapi.interfaces import ISerializeToJson
+from plone.restapi.serializer.dxcontent import SerializeToJson
+from Products.CMFPlone.utils import safe_unicode
+from zope.component import adapter
+from zope.component import getMultiAdapter
+from zope.interface import implementer
+
+
+@implementer(ISerializeToJson)
+@adapter(IEmbeddedPage, ICollectiveEmbeddedpageLayer)
+class CustomSerializeToJson(SerializeToJson):
+
+    def __call__(self, version=None, include_items=True):
+        serialization = super(CustomSerializeToJson, self).__call__(
+            version=version, include_items=include_items)
+        view = getMultiAdapter((self.context, self.request), name="view")
+        serialization.update({
+            "text": {
+                "data": safe_unicode(view.process_page()),
+                "content-type": "text/html",
+                "encoding": "utf-8",
+            },
+        })
+        return serialization
