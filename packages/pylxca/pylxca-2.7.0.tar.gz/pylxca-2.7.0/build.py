6bac66b7be2f51ec0ce3d96f860dc53c8fbe6541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:03 +0000

    Add package pylxca/2.7.0/pylxca-2.7.0.tar.gz

---
+++ packages/pylxca/pylxca-2.7.0.tar.gz/pylxca-2.7.0/build.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# Build Script
+import sys,os
+import subprocess
+
+if len(sys.argv) < 2 :
+	print ("Building Repository")
+	ret = subprocess.call(sys.executable + " setup.py sdist bdist_egg --exclude-source-file bdist_wheel",shell = True)
+	print ("Done ", ret)
+	print ("Build drop location is: ",  os.getcwd() + "/dist")
+elif sys.argv[1] == "clean":
+	ret = subprocess.call(sys.executable + " setup.py clean --all",shell = True)
+	print ("Done ", ret)
+sys.exit(ret)
