146c13189f1a5437ab8786cf41d1454ca92cb159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:28 +0000

    Add package tlnetcard-python/0.1.3/tlnetcard_python-0.1.3.tar.gz

---
+++ packages/tlnetcard-python/tlnetcard_python-0.1.3.tar.gz/tlnetcard_python-0.1.3/tlnetcard_python/system/administration/console/console.py
@@ -0,0 +1,9 @@
+# console.py
+# Ethan Guthrie
+# DATE TBD
+""" Allows for console settings to be configured. """
+
+class Console:
+    """ Class for the Console object. """
+    def __init__(self):
+        """ Initializes the Console object. """
