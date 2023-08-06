6ffa443cb927bbe7fe7b08df217bbc1bbc6ff5eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:25 +0000

    Add package moclaphar/0.0.9/moclaphar-0.0.9.tar.gz

---
+++ packages/moclaphar/moclaphar-0.0.9.tar.gz/moclaphar-0.0.9/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="moclaphar",
+    version="0.0.9",
+    license='MIT',
+    author="Jongkuk Lim",
+    author_email="lim.jeikei@gmail.com",
+    description="This packages mainly aims to make an easy process for dataset manipulation.",
+    long_description=open('README.md').read(),
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # 패키지에 대한 태그
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
