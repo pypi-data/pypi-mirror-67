448a97a1f983a5db8a0d6aa926d1c8506ffaf237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:52 +0000

    Add package prob-distribution/0.1/prob_distribution-0.1.tar.gz

---
+++ packages/prob-distribution/prob_distribution-0.1.tar.gz/prob_distribution-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='prob_distribution',
+      version='0.1',
+      description='Probability distributions',
+      packages=['prob_distribution'],
+      zip_safe=False)
