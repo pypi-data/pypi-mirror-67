66e2faedace69e846a4f220ebedd2ca6edd4cd5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:21 +0000

    Add package raveberry/0.5.1/raveberry-0.5.1-py3-none-any.whl

---
+++ packages/raveberry/raveberry-0.5.1-py3-none-any.whl/raveberry/setup/user_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Performing Migrations"
+DJANGO_DEBUG=1 DJANGO_MOCK=1 python3 manage.py migrate
+echo "Creating Users"
+DJANGO_DEBUG=1 DJANGO_MOCK=1 python3 manage.py shell <<-EOF
+	from django.contrib.auth.models import User
+	User.objects.create_superuser('admin', email='', password='admin')
+	User.objects.create_user('mod', password='mod')
+	User.objects.create_user('pad', password='pad')
+EOF
+if [[ ! -d static/libs ]]; then
+	echo "Installing frontend libraries"
+	HOME= yarn install
+fi
+echo "Compiling SCSS Files"
+DJANGO_MOCK=1 DJANGO_DEBUG=1 python3 manage.py compilescss
