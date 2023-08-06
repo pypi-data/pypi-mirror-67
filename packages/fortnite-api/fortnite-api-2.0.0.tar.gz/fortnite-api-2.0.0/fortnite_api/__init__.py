06e00b071b400b8edf8f4d111aaaaf1105c075ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:59 +0000

    Add package fortnite-api/2.0.0/fortnite-api-2.0.0.tar.gz

---
+++ packages/fortnite-api/fortnite-api-2.0.0.tar.gz/fortnite-api-2.0.0/fortnite_api/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '2.0.0'
+
+from .aes import *
+from .api import FortniteAPI
+from .cosmetics import *
+from .creator_code import *
+from .endpoints import *
+from .enums import *
+from .errors import *
+from .news import *
+from .shop import *
+from .user import *
---
+++ packages/fortnite-api/fortnite-api-2.0.0.tar.gz/fortnite-api-2.0.0/fortnite_api/errors.py
@@ -0,0 +1,26 @@
+class FortniteAPIException(Exception):
+    pass
+
+
+class NotFound(FortniteAPIException):
+    pass
+
+
+class MissingSearchParameter(FortniteAPIException):
+    pass
+
+
+class MissingIDParameter(FortniteAPIException):
+    pass
+
+
+class ServiceUnavailable(FortniteAPIException):
+    pass
+
+
+class RateLimited(FortniteAPIException):
+    pass
+
+
+class Unauthorized(FortniteAPIException):
+    pass
