c1101d5a533125c8b14fdf703da6d7b54a9add7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:17 +0000

    Add package zaius-export/1.4.2/zaius_export-1.4.2.tar.gz

---
+++ packages/zaius-export/zaius_export-1.4.2.tar.gz/zaius_export-1.4.2/zaius/reports/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""Pre-baked reports
+
+Set of reports that can be called from code or executed using
+the zaius-export command line utility.
+
+    Example:
+        zaius-export demo
+
+"""
+from .spec import ReportSpec
+from . import demo
+from . import product_attribution
+from . import lifecycle_progress
+from . import email_metrics
+
+SPECS = ReportSpec.specs
