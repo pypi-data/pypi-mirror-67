fc8cdd9d13aad313ddd1e849ab509603def6edeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:10 +0000

    Add package gallery-dl/1.13.6/gallery_dl-1.13.6-py3-none-any.whl

---
+++ packages/gallery-dl/gallery_dl-1.13.6-py3-none-any.whl/gallery_dl/postprocessor/common.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2018-2019 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Common classes and constants used by postprocessor modules."""
+
+import logging
+
+
+class PostProcessor():
+    """Base class for postprocessors"""
+
+    def __init__(self):
+        name = self.__class__.__name__[:-2].lower()
+        self.log = logging.getLogger("postprocessor." + name)
+
+    @staticmethod
+    def prepare(pathfmt):
+        """Update file paths, etc."""
+
+    @staticmethod
+    def run(pathfmt):
+        """Execute the postprocessor for a file"""
+
+    @staticmethod
+    def run_metadata(pathfmt):
+        """Execute the postprocessor for a file"""
+
+    @staticmethod
+    def run_after(pathfmt):
+        """Execute postprocessor after moving a file to its target location"""
+
+    @staticmethod
+    def run_final(pathfmt, status):
+        """Postprocessor finalization after all files have been downloaded"""
+
+    def __repr__(self):
+        return self.__class__.__name__
