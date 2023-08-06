49a2b762d6bae03fe3ee71c199856f06e498c2cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:03 +0000

    Add package megahal/0.3.5/megahal-0.3.5.tar.gz

---
+++ packages/megahal/megahal-0.3.5.tar.gz/megahal-0.3.5/megahal/__init__.py
@@ -0,0 +1,9 @@
+from megahal.megahal import (
+    DEFAULT_BANWORDS, DEFAULT_BRAINFILE, DEFAULT_ORDER, DEFAULT_TIMEOUT,
+    Brain, Dictionary, MegaHAL, Tree, __version__,
+)
+
+__all__ = [
+    "MegaHAL", "Dictionary", "Tree", "__version__", "DEFAULT_ORDER", "Brain",
+    "DEFAULT_BRAINFILE", "DEFAULT_BANWORDS", "DEFAULT_TIMEOUT",
+]
