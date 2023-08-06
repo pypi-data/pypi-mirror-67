14cced63979723b2d159875837a7f0b1e17522eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:12 +0000

    Add package pyminknow/0.0.15/pyminknow-0.0.15.tar.gz

---
+++ packages/pyminknow/pyminknow-0.0.15.tar.gz/pyminknow-0.0.15/pyminknow/client.py
@@ -0,0 +1,279 @@
+import argparse
+import logging
+import warnings
+
+import grpc
+import google.protobuf.wrappers_pb2
+
+import minknow.rpc.device_pb2
+import minknow.rpc.device_pb2_grpc
+import minknow.rpc.manager_pb2
+import minknow.rpc.manager_pb2_grpc
+import minknow.rpc.protocol_pb2
+import minknow.rpc.protocol_pb2_grpc
+
+import pyminknow.config
+
+LOGGER = logging.getLogger(__name__)
+
+DESCRIPTION = """
+This is a client to test a Nanopore minKNOW gene sequencing device by using its gRPC interface.
+"""
+
+USAGE = "python -m pyminknow.client"
+
+
+class RpcClient:
+    """
+    gRPC client
+    """
+
+    # TODO timeouts
+    # https://groups.google.com/forum/#!topic/grpc-io/kNBQlQjxQVU
+
+    stub_name = None
+    _stub = None
+
+    def __init__(self, channel):
+        self.channel = channel
+
+    @staticmethod
+    def get_stub(service: str, channel):
+        """
+        Retrieve the appropriate stub (client) class for the specified service
+        """
+
+        # Map services to stubs
+        stubs = dict(
+            device=minknow.rpc.device_pb2_grpc.DeviceServiceStub,
+            protocol=minknow.rpc.protocol_pb2_grpc.ProtocolServiceStub,
+            manager=minknow.rpc.manager_pb2_grpc.ManagerServiceStub,
+        )
+
+        Stub = stubs[service]
+
+        stub = Stub(channel=channel)
+
+        return stub
+
+    @property
+    def stub(self):
+        """Initialise stub"""
+
+        if not self._stub:
+            self._stub = self.get_stub(self.stub_name, self.channel)
+
+        return self._stub
+
+
+class ManagerClient(RpcClient):
+    """
+    Manager client
+    """
+
+    stub_name = 'manager'
+
+    def describe_host(self, **kwargs) -> dict:
+        request = minknow.rpc.manager_pb2.DescribeHostRequest()
+        response = self.stub.describe_host(request, **kwargs)
+
+        # return dict(
+        #     product_code=response.product_code,
+        #     description=response.description,
+        #     serial=response.serial,
+        #     network_name=response.network_name,
+        # )
+
+        return response
+
+    def list_devices(self, **kwargs):
+        warnings.warn('DEPRECATED: use `flow_cell_positions` instead', DeprecationWarning)
+
+        request = minknow.rpc.manager_pb2.ListDevicesRequest()
+        return self.stub.list_devices(request, **kwargs)
+
+    def flow_cell_positions(self, **kwargs) -> iter:
+        request = minknow.rpc.manager_pb2.FlowCellPositionsRequest()
+        for response in self.stub.flow_cell_positions(request, **kwargs):
+            yield from response.positions
+
+
+class ProtocolClient(RpcClient):
+    """
+    Protocol client
+    """
+
+    stub_name = 'protocol'
+
+    def list_protocols(self):
+        request = minknow.rpc.protocol_pb2.ListProtocolsRequest()
+        return self.stub.list_protocols(request)
+
+    def start_protocol(self, identifier: str, user_info: dict = None, args: list = None) -> str:
+        """
+        :param identifier: Protocol ID
+        :param user_info: User input describing the protocol (keys: protocol_group_id, sample_id)
+        :param args: The arguments to pass to the protocol
+        :returns: Run ID
+        """
+
+        user_info = user_info or dict()
+
+        # StringValue may be null
+        _user_info = minknow.rpc.protocol_pb2.ProtocolRunUserInfo(
+            protocol_group_id=google.protobuf.wrappers_pb2.StringValue(value=user_info.get('protocol_group_id')),
+            sample_id=google.protobuf.wrappers_pb2.StringValue(value=user_info.get('sample_id')),
+        )
+
+        request = minknow.rpc.protocol_pb2.StartProtocolRequest(
+            identifier=identifier,
+            user_info=_user_info,
+            args=args,
+        )
+
+        response = self.stub.start_protocol(request)
+
+        return response.run_id
+
+    def list_protocol_runs(self) -> list:
+        request = minknow.rpc.protocol_pb2.ListProtocolRunsRequest()
+        response = self.stub.list_protocol_runs(request)
+        return response.run_ids
+
+    @property
+    def latest_run_id(self) -> int:
+        return self.list_protocol_runs()[0]
+
+    def get_run_info_by_id(self, run_id: str = None):
+        # Get latest run ID
+        if run_id is None:
+            run_id = self.latest_run_id
+
+        request = minknow.rpc.protocol_pb2.GetRunInfoRequest(run_id=run_id)
+        return self.stub.get_run_info(request)
+
+
+class DeviceClient(RpcClient):
+    """
+    Device client
+    """
+
+    stub_name = 'device'
+
+    def get_device_state(self) -> str:
+        request = minknow.rpc.device_pb2.GetDeviceStateRequest()
+        response = self.stub.get_device_state(request)
+
+        # Get human-readable state
+        state_name = minknow.rpc.device_pb2.GetDeviceStateResponse.DeviceState.Name(response.device_state)
+        LOGGER.info("Device status: %s => '%s'", response.device_state, state_name)
+
+        return state_name
+
+
+def get_args():
+    parser = argparse.ArgumentParser(usage=USAGE, description=DESCRIPTION)
+
+    parser.add_argument('-v', '--verbose', action='store_true', help='Debug logging')
+    parser.add_argument('-o', '--host', default=pyminknow.config.DEFAULT_HOST, help='Connect to this host')
+    parser.add_argument('-p', '--port', type=int, default=pyminknow.config.DEFAULT_PORT, help='Connect to this port')
+    parser.add_argument('-s', '--device_state', action='store_true', help='Get device state')
+    parser.add_argument('-l', '--list_protocols', action='store_true', help='List available protocols')
+    parser.add_argument('-d', '--list_devices', action='store_true', help='List available devices')
+    parser.add_argument('-f', '--flow_cell_positions', action='store_true',
+                        help='List all known positions where flow cells can be inserted')
+    parser.add_argument('-i', '--start_protocol', help='Start a protocol given by this identifier')
+    parser.add_argument('-g', '--protocol_group_id', help='The group which the experiment should be held in')
+    parser.add_argument('-r', '--list_protocol_runs', action='store_true',
+                        help='List previously started protocol run IDs, in order of starting')
+
+    return parser, parser.parse_args()
+
+
+def connect(host: str, port: int):
+    """
+    Connect to the server
+    """
+
+    target = '{host}:{port}'.format(host=host, port=port)
+
+    LOGGER.info("Connecting to '%s'...", target)
+
+    channel = grpc.insecure_channel(target=target)
+
+    return channel
+
+
+def configure_logging(verbose: bool = False):
+    logging.basicConfig(level=logging.DEBUG if verbose else logging.INFO)
+    logging.captureWarnings(capture=True)
+
+
+def main():
+    parser, args = get_args()
+    configure_logging(verbose=args.verbose)
+
+    with connect(host=args.host, port=args.port) as channel:
+
+        manager_client = ManagerClient(channel)
+
+        LOGGER.info(manager_client.describe_host())
+
+        # Device
+        if args.device_state:
+            client = DeviceClient(channel)
+            print(client.get_device_state())
+
+        # Protocol
+        elif args.list_protocols or args.start_protocol or args.list_protocol_runs:
+            client = ProtocolClient(channel)
+
+            if args.list_protocols:
+                for protocol in client.list_protocols().protocols:
+                    print(protocol)
+
+            # Launch new protocol run
+            elif args.start_protocol:
+                run_id = client.start_protocol(
+                    identifier=args.start_protocol,
+                    user_info=dict(protocol_group_id=args.protocol_group_id),
+                    args=[
+                        "--fast5=on",
+                        "--fast5_data", "trace_table", "fastq", "raw", "zlib_compress",
+                        "--base_calling=on",
+                        "--fastq=on",
+                        "--barcoding_kits", "EXP-NBD114", "EXP-NBD104",
+                        "--experiment_time=24"
+                    ],
+                )
+                LOGGER.info("Started run ID: %s", run_id)
+                run_info = client.get_run_info_by_id(run_id)
+                print(run_info)
+            elif args.list_protocol_runs:
+                run_ids = client.list_protocol_runs()
+                print(run_ids)
+            else:
+                raise ValueError('Unknown command')
+
+        # Manager
+        elif args.list_devices or args.flow_cell_positions:
+
+            if args.list_devices:
+                devices = manager_client.list_devices()
+                LOGGER.info("Found %s active devices", len(devices.active))
+                for device in devices.active:
+                    print(device)
+
+            elif args.flow_cell_positions:
+                for item in manager_client.flow_cell_positions():
+                    print(item)
+
+            else:
+                raise ValueError('Unknown command')
+
+        else:
+            parser.print_help()
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/pyminknow/pyminknow-0.0.15.tar.gz/pyminknow-0.0.15/pyminknow/service/protocol.py
@@ -0,0 +1,320 @@
+import logging
+import uuid
+import time
+import datetime
+import warnings
+import os.path
+import pickle
+import pathlib
+
+import google.protobuf.wrappers_pb2
+import google.protobuf.timestamp_pb2
+
+import minknow.rpc.protocol_pb2
+import minknow.rpc.protocol_pb2_grpc
+
+import pyminknow.config
+
+LOGGER = logging.getLogger(__name__)
+
+
+def build_timestamp(timestamp=None) -> google.protobuf.timestamp_pb2.Timestamp:
+    """Convert Python datetime to Protobuf Timestamp"""
+    # https://github.com/protocolbuffers/protobuf/issues/3986
+    proto_timestamp = google.protobuf.timestamp_pb2.Timestamp()
+    return proto_timestamp.FromDatetime(timestamp or datetime.datetime.utcnow())
+
+
+class Run:
+    """Protocol run (dummy)"""
+    SERIALISATION_EXT = 'pkl'
+
+    def __init__(self, protocol_id: str, user_info: minknow.rpc.protocol_pb2.ProtocolRunUserInfo, args: list):
+        self._state = None
+        self.run_id = self.make_run_id()
+        self.protocol_id = protocol_id
+        self.user_info = user_info
+        self.args = list(args)
+        self.start_time = datetime.datetime.utcnow()
+        self.end_time = None
+
+        LOGGER.debug("Starting run ID: %s", self.run_id)
+        LOGGER.debug("Protocol group ID: %s", user_info.protocol_group_id.value)
+        LOGGER.debug("Sample ID: %s", user_info.sample_id.value)
+
+    @classmethod
+    def build_path(cls, run_id):
+        filename = "{name}.{ext}".format(name=run_id, ext=cls.SERIALISATION_EXT)
+        return os.path.join(pyminknow.config.RUN_DIR, filename)
+
+    @property
+    def as_dict(self) -> dict:
+        return dict(
+            state=self.state,
+            run_id=self.run_id,
+            protocol_id=self.protocol_id,
+            user_info=dict(
+                protocol_group_id=str(self.user_info.protocol_group_id),
+                sample_id=str(self.user_info.sample_id),
+            ),
+            args=self.args,
+            start_time=self.start_time,
+            end_time=self.end_time,
+        )
+
+    @property
+    def path(self):
+        return self.build_path(self.run_id)
+
+    def serialise(self):
+        os.makedirs(pyminknow.config.RUN_DIR, exist_ok=True)
+
+        with open(self.path, 'wb') as file:
+            pickle.dump(self.as_dict, file)
+
+            LOGGER.info("Wrote '%s'", file.name)
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        run = Run(
+            protocol_id=data.pop('protocol_id'),
+            user_info=cls.build_user_info(**data.pop('user_info')),
+            args=data.pop('args'),
+        )
+
+        # run.state = minknow.rpc.protocol_pb2.ProtocolState(data.pop('state'))
+
+        for attr, value in data.items():
+            setattr(run, attr, value)
+
+        return run
+
+    @classmethod
+    def deserialise(cls, run_id: str):
+        path = cls.build_path(run_id)
+        with open(path, 'rb') as file:
+            data = pickle.load(file)
+
+            LOGGER.debug("Read '%s'", file.name)
+
+        run = cls.from_dict(data)
+
+        return run
+
+    def save_data(self):
+        """Write sequence data to disk"""
+
+        filename = 'my_data.txt'
+        path = os.path.join(self.output_directory, filename)
+
+        # Write to disk
+        os.makedirs(self.output_directory, exist_ok=True)
+        with open(path, 'w') as file:
+            # Generate some dummy data
+            file.write('Hello world!\n')
+
+            LOGGER.debug("Wrote '%s'", file.name)
+
+    @property
+    def state(self) -> minknow.rpc.protocol_pb2.ProtocolState:
+        return self._state
+
+    @state.setter
+    def state(self, state):
+        """Change state"""
+        self._state = state
+        self.serialise()
+
+    def start(self):
+        self.state = minknow.rpc.protocol_pb2.ProtocolState.PROTOCOL_RUNNING
+        self.save_data()
+        self.finish()
+
+    def finish(self):
+        self.end_time = datetime.datetime.utcnow()
+        self.state = minknow.rpc.protocol_pb2.ProtocolState.PROTOCOL_COMPLETED
+
+    @property
+    def is_complete(self) -> bool:
+        return self.state == minknow.rpc.protocol_pb2.ProtocolState.PROTOCOL_COMPLETED
+
+    @property
+    def output_directory(self) -> str:
+        """
+        The directory to save the sequencing data
+        e.g. "/data/external_run_id/external_run_id/protcol_run_id"
+        """
+        external_run_id = str(self.user_info.protocol_group_id)
+        return os.path.join(
+            pyminknow.config.DATA_DIR,
+            external_run_id,
+            external_run_id,
+            self.run_id,
+        )
+
+    @classmethod
+    def make_run_id(cls) -> str:
+        """
+        Generate protocol run identifier
+        e.g. "DATE_TIME_DEVICE_FLOWCELLID_PARTOFAQUISITIONID"
+        """
+        device_id = pyminknow.config.PRODUCT_CODE
+        flow_cell_id = pyminknow.config.FLOW_CELL_ID
+        now = datetime.datetime.utcnow()
+        day = now.date().strftime('%Y%m%d')
+        clock_time = now.strftime('%H%M')
+        unique = str(uuid.uuid4()).partition('-')[0]
+        return "{day}_{time}_{device}_{flow_cell}_{acq}".format(day=day, time=clock_time, device=device_id,
+                                                                flow_cell=flow_cell_id, acq=unique)
+
+    @classmethod
+    def build_user_info(cls, protocol_group_id: str, sample_id: str):
+        return minknow.rpc.protocol_pb2.ProtocolRunUserInfo(
+            protocol_group_id=google.protobuf.wrappers_pb2.StringValue(value=protocol_group_id),
+            sample_id=google.protobuf.wrappers_pb2.StringValue(value=sample_id),
+        )
+
+    @property
+    def start_time(self):
+        return build_timestamp(self._start_time)
+
+    @start_time.setter
+    def start_time(self, value):
+        self._start_time = value
+
+    @property
+    def end_time(self):
+        return build_timestamp(self._end_time)
+
+    @end_time.setter
+    def end_time(self, value):
+        self._end_time = value
+
+    @property
+    def info(self) -> minknow.rpc.protocol_pb2.ProtocolRunInfo:
+        return minknow.rpc.protocol_pb2.ProtocolRunInfo(
+            protocol_id=self.protocol_id,
+            args=self.args,
+            output_path=self.output_directory,
+            state=self.state,
+            start_time=self.start_time,
+            end_time=self.end_time,
+            user_info=self.user_info,
+            acquisition_run_ids=self.acquisition_run_ids,
+        )
+
+    @property
+    def acquisition_run_ids(self) -> list:
+        return [str(uuid.uuid4()) for _ in range(3)]
+
+    @classmethod
+    def get_run_ids(cls):
+        """Chronological order (by start time ascending)"""
+        yield from (
+            # remove file ext
+            _path.stem for _path in
+            # Sort by creation time
+            sorted(pathlib.Path(pyminknow.config.RUN_DIR).glob('*.{}'.format(cls.SERIALISATION_EXT)),
+                   key=lambda _path: _path.stat().st_ctime)
+        )
+
+    @classmethod
+    def latest_run_id(cls) -> str:
+        return next(cls.get_run_ids())
+
+
+class ProtocolService(minknow.rpc.protocol_pb2_grpc.ProtocolServiceServicer):
+    """
+    Protocol service
+
+    https://github.com/nanoporetech/minknow_lims_interface/blob/master/minknow/rpc/protocol.proto
+    """
+    add_to_server = minknow.rpc.protocol_pb2_grpc.add_ProtocolServiceServicer_to_server
+
+    def list_protocols(self, request, context):
+        if request.force_reload:
+            self.clear_protocol_cache()
+
+        protocols = self.get_protocol_info()
+
+        return minknow.rpc.protocol_pb2.ListProtocolsResponse(protocols=protocols)
+
+    def clear_protocol_cache(self):
+        raise NotImplementedError
+
+    @staticmethod
+    def get_protocol_info() -> list:
+        """Build collection of ProtocolInfo objects"""
+        return [
+            minknow.rpc.protocol_pb2.ProtocolInfo(
+                identifier=protocol_name,
+                name=protocol_name,
+                tags={
+                    'flow cell': minknow.rpc.protocol_pb2.ProtocolInfo.TagValue(string_value="FLO-MIN106"),
+                    'kit': minknow.rpc.protocol_pb2.ProtocolInfo.TagValue(string_value="SQK-LSK109"),
+                    'experiment type': minknow.rpc.protocol_pb2.ProtocolInfo.TagValue(string_value="sequencing"),
+                }
+            ) for protocol_name in pyminknow.config.PROTOCOLS
+        ]
+
+    @classmethod
+    def _start_protocol(cls, identifier, user_info, args):
+        """Emulate a real process running"""
+        LOGGER.info("Starting protocol %s (Args: %s)", identifier, args)
+
+        run = Run(protocol_id=identifier, user_info=user_info, args=args)
+        run.start()
+
+        return run.run_id
+
+    def start_protocol(self, request, context):
+
+        run_id = self._start_protocol(identifier=request.identifier, user_info=request.user_info, args=request.args)
+
+        return minknow.rpc.protocol_pb2.StartProtocolResponse(run_id=run_id)
+
+    @property
+    def latest_run_id(self):
+        """The identifier of the most recently-started run"""
+        return Run.latest_run_id()
+
+    @property
+    def run_ids(self) -> list:
+        return list(Run.get_run_ids())
+
+    def get_run_info(self, request, context):
+        run_id = request.run_id
+
+        # If no run ID is provided, information about the most recently started protocol run is provided
+        if not run_id:
+            run_id = self.latest_run_id
+
+        run = Run.deserialise(run_id=run_id)
+
+        return run.info
+
+    def set_sample_id(self, request, context):
+        """
+        Specify the sample identifier to set for the next protocol (deprecated)
+        """
+
+        warnings.warn('The sample_id should be set in the request when a protocol starts', DeprecationWarning)
+
+        LOGGER.debug('Sample ID: %s', request.sample_id)
+
+        return minknow.rpc.protocol_pb2.SetSampleIdResponse()
+
+    def list_protocol_runs(self, request, context):
+        """List previously started protocol run ids (including any current protocol), in order of starting."""
+        return minknow.rpc.protocol_pb2.ListProtocolRunsResponse(run_ids=self.run_ids)
+
+    def wait_for_finished(self, request, context) -> minknow.rpc.protocol_pb2.ProtocolRunInfo:
+
+        run = Run.deserialise(request.run_id)
+
+        while True:
+
+            if run.is_complete:
+                return run.info
+
+            time.sleep(1)
