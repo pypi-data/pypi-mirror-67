cd216cd0f2855c6e68e15220e09d1122e0be32f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:20 +0000

    Add package syyDlib/0.0.3/syyDlib-0.0.3.tar.gz

---
+++ packages/syyDlib/syyDlib-0.0.3.tar.gz/syyDlib-0.0.3/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+setuptools.setup(
+    name='syyDlib',
+    version='0.0.3',
+    package=['Models']
+)
