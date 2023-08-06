9b90c51f31bbb7741bab15ab8de40c97ba8a08cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:36 +0000

    Add package sqlalchemy-dremio/1.1.4/sqlalchemy_dremio-1.1.4.tar.gz

---
+++ packages/sqlalchemy-dremio/sqlalchemy_dremio-1.1.4.tar.gz/sqlalchemy_dremio-1.1.4/sqlalchemy_dremio/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '1.1.4'
+from .db import Connection, connect
+from sqlalchemy.dialects import registry
+
+# Register the ODBC and flight end points
+
+registry.register("dremio", "sqlalchemy_dremio.pyodbc", "DremioDialect_pyodbc")
+registry.register("dremio+flight", "sqlalchemy_dremio.flight", "DremioDialect_flight")
