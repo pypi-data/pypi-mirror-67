c8fdacec0320e804bfa2b5797c104cecd2428c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:08 +0000

    Add package distributionsbi/0.1/distributionsbi-0.1.tar.gz

---
+++ packages/distributionsbi/distributionsbi-0.1.tar.gz/distributionsbi-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='distributionsbi',
+      
+      version='0.1',
+      
+      description='Gaussian and binomial distributions',
+      
+      packages=['distributionsbi'],
+      
+      author='Pratik Patil',
+      
+      author_email='pratikmpatil12@gmail.com',
+      
+      zip_safe=False)
