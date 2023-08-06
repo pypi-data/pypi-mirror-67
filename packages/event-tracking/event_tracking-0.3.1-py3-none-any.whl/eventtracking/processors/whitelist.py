feaa47cd7b20d5ad04731ad71e51a4a697e1298a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:30 +0000

    Add package event-tracking/0.3.1/event_tracking-0.3.1-py3-none-any.whl

---
+++ packages/event-tracking/event_tracking-0.3.1-py3-none-any.whl/eventtracking/processors/whitelist.py
@@ -0,0 +1,32 @@
+"""Filter out events whose names aren't on a pre-configured whitelist"""
+
+from __future__ import absolute_import
+from eventtracking.processors.exceptions import EventEmissionExit
+import six
+
+
+class NameWhitelistProcessor(object):
+    """
+
+    Filter out events whose names aren't on a pre-configured whitelist.
+
+    `whitelist` is an iterable collection containing event names that should be allowed to pass.
+    """
+
+    def __init__(self, whitelist=None, **_kwargs):
+        try:
+            if isinstance(whitelist, six.string_types):
+                raise TypeError
+
+            self.whitelist = frozenset(whitelist)
+        except TypeError:
+            raise TypeError(
+                'The NameWhitelistProcessor must be passed a collection of allowed names '
+                'using the "whitelist" parameter'
+            )
+
+    def __call__(self, event):
+        if event['name'] not in self.whitelist:
+            raise EventEmissionExit()
+        else:
+            return event
