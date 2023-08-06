95fc5d78d1a65d5c3462bd2da77bf2596c0b3c02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:34 +0000

    Add package pythoms/1.6.3/pythoms-1.6.3.tar.gz

---
+++ packages/pythoms/pythoms-1.6.3.tar.gz/pythoms-1.6.3/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+import pythoms
+
+NAME = 'pythoms'
+AUTHOR = 'Lars Yunker'
+
+PACKAGES = find_packages()
+KEYWORDS = ', '.join([
+    'mass spectrometry',
+    'mass spec',
+    'mzML',
+    'isotope pattern',
+    'HUPO PSI-MS',
+])
+
+with open('README.MD') as f:
+    long_description = f.read()
+
+setup(
+    name=NAME,
+    version=pythoms.__version__,
+    description='A Python library to aid in the processing and interpretation of mass spectrometric data.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    url='https://github.com/larsyunker/PythoMS',
+    packages=PACKAGES,
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Operating System :: OS Independent',
+        'Natural Language :: English'
+    ],
+    install_requires=[
+        'numpy>=1.14.2',
+        'openpyxl>=2.5.2',
+        'matplotlib>=3.2.1',
+        'scipy>=1.1.0',
+        'sympy>=1.1.1',
+        'obonet==0.2.5',  # they changed attribute names without deprecationwarnings, so only this version is verified
+        'numpy',
+        'isospecpy>=2.0.2',
+    ],
+    keywords=KEYWORDS,
+)
