e562a9dcaa6818795cabbf8ac539a17e303aecc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:22 +0000

    Add package crds/7.5.0.0/crds-7.5.0.0.tar.gz

---
+++ packages/crds/crds-7.5.0.0.tar.gz/crds-7.5.0.0/crds/certify/__init__.py
@@ -0,0 +1,8 @@
+from .certify import *
+from .validators import *
+
+__all__ = [
+    "certify_files",
+    "certify_file",
+    "CertifyScript",
+]
---
+++ packages/crds/crds-7.5.0.0.tar.gz/crds-7.5.0.0/crds/core/git_version.py
@@ -0,0 +1,9 @@
+
+
+__version__ = '7fc216e73bd81d334b5d98d165be2caa3b6175e1'
+
+__full_version_info__ = '''
+branch: b7.5.0.0
+sha1: 7fc216e73bd81d334b5d98d165be2caa3b6175e1
+'''
+    
