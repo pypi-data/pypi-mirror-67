01b366e524ff073bfaadcc6dcdd2ca13179e4325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:11 +0000

    Add package parser-main/4.1/parser_main-4.1.tar.gz

---
+++ packages/parser-main/parser_main-4.1.tar.gz/parser_main-4.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='parser_main',
+    version='4.1',
+    packages=['parser_main'],
+    url='',
+    license='',
+    author='Ivan Bondarenko',
+    author_email='ivan.frinom@gmail.com',
+    description='',
+    install_requires=[
+        'parser_module>=1.1',
+    ]
+)
