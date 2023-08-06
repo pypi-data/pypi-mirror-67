9e747e9dd02000d26a76bdb6649913b7440a0b51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:53 +0000

    Add package custom-format/1.0.1/custom_format-1.0.1.tar.gz

---
+++ packages/custom-format/custom_format-1.0.1.tar.gz/custom_format-1.0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="custom_format",
+    version="1.0.1",
+    author="AndyHsueh",
+    author_email="syt156321@gmail.com",
+    description="A custom format for optional html form",
+    packages=["custom_format"],
+    python_requires='>=3.6',
+    license="MIT",
+    install_requires=["behave"],
+    download_url="https://github.com/andy8927/custom_format/archive/1.0.1.tar.gz"
+)
