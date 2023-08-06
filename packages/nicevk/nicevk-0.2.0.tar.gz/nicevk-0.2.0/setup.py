2639e51255f866c6b04018b123b49d92500d3fed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:31 +0000

    Add package nicevk/0.2.0/nicevk-0.2.0.tar.gz

---
+++ packages/nicevk/nicevk-0.2.0.tar.gz/nicevk-0.2.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nicevk', 'nicevk.plugins']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-dotenv>=0.13.0,<0.14.0', 'vkbottle>=2.7.2,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['nicevk = nicevk:cli.run']}
+
+setup_kwargs = {
+    'name': 'nicevk',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'nm17',
+    'author_email': 'dannevergame@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
