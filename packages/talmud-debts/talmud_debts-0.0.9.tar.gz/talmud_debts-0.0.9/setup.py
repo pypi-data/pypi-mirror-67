2fc69d87b476683d14c67c412a582546a469e4be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:53 +0000

    Add package talmud-debts/0.0.9/talmud_debts-0.0.9.tar.gz

---
+++ packages/talmud-debts/talmud_debts-0.0.9.tar.gz/talmud_debts-0.0.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="talmud_debts",
+    version="0.0.9",
+    author="Data Dolitle",
+    author_email="datadolittle@gmail.com",
+    description="Split estate to array of creditors using talmudic game theory",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Datadolittle/Talmud_Debts",
+    packages=['.'],
+    install_requires=['markdown'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
