a8722b9dfe13d471e187ee47b1c2734323446f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:42 +0000

    Add package visu/2020.5/visu-2020.5-py3-none-any.whl

---
+++ packages/visu/visu-2020.5-py3-none-any.whl/visu/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 18 10:50:06 2019
+Data vizualisation
+@author: juliengautier
+"""
+
+from visu import visual
+from visu import visualLight
+from visu.visualLight import SEELIGHT
+from visu.visual import SEE
+from visu.visual import runVisu
+from visu import andor
+from visu import WinCut
+from visu import winMeas
+from visu import winspec
+from visu import winSuppE
+from visu import winFFT
+from visu import winZoom
+from visu import Win3D
+#from visu import moteurRSAI as RSAI
+__version__='2020.05'
