606306a57e17dafb34157476957651d451b2946f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:43 +0000

    Add package django-pg-queue/0.6.1/django-pg-queue-0.6.1.tar.gz

---
+++ packages/django-pg-queue/django-pg-queue-0.6.1.tar.gz/django-pg-queue-0.6.1/pgq/models.py
@@ -0,0 +1,101 @@
+from typing import Any, Dict, Iterable, Optional, Sequence
+
+from django.db import models
+from django.contrib.postgres.functions import TransactionNow
+from django.contrib.postgres.fields import JSONField
+
+DEFAULT_QUEUE_NAME = "default"
+
+
+class BaseJob(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    created_at = models.DateTimeField(default=TransactionNow)
+    execute_at = models.DateTimeField(default=TransactionNow)
+    priority = models.IntegerField(
+        default=0, help_text="Jobs with higher priority will be processed first."
+    )
+    task = models.CharField(max_length=255)
+    args = JSONField()
+    queue = models.CharField(
+        max_length=32,
+        default=DEFAULT_QUEUE_NAME,
+        help_text="Use a unique name to represent each queue.",
+    )
+
+    class Meta:
+        abstract = True
+        indexes = [
+            models.Index(fields=["-priority", "created_at"]),
+            models.Index(fields=["queue"]),
+        ]
+
+    def __str__(self) -> str:
+        return "%s: %s" % (self.id, self.task)
+
+    @classmethod
+    def dequeue(
+        cls,
+        exclude_ids: Optional[Iterable[int]] = None,
+        tasks: Optional[Sequence[str]] = None,
+        queue: str = DEFAULT_QUEUE_NAME,
+    ) -> Optional["BaseJob"]:
+        """
+        Claims the first available task and returns it. If there are no
+        tasks available, returns None.
+
+        exclude_ids: Iterable[int] - excludes jobs with these ids
+        tasks: Optional[Sequence[str]] - filters by jobs with these tasks.
+
+        For at-most-once delivery, commit the transaction before
+        processing the task. For at-least-once delivery, dequeue and
+        finish processing the task in the same transaction.
+
+        To put a job back in the queue, you can just call
+        .save(force_insert=True) on the returned object.
+        """
+
+        WHERE = "WHERE execute_at <= now() AND NOT id = ANY(%s) AND queue = %s"
+        args = [[] if exclude_ids is None else list(exclude_ids), queue]
+        if tasks is not None:
+            WHERE += " AND TASK = ANY(%s)"
+            args.append(tasks)
+
+        jobs: Sequence[BaseJob] = list(
+            cls.objects.raw(
+                """
+            DELETE FROM pgq_job
+            WHERE id = (
+                SELECT id
+                FROM pgq_job
+                {WHERE}
+                ORDER BY priority DESC, created_at
+                FOR UPDATE SKIP LOCKED
+                LIMIT 1
+            )
+            RETURNING *;
+            """.format(
+                    WHERE=WHERE
+                ),
+                args,
+            )
+        )
+        assert len(jobs) <= 1
+        if jobs:
+            return jobs[0]
+        else:
+            return None
+
+    def to_json(self) -> Dict[str, Any]:
+        return {
+            "id": self.id,
+            "created_at": self.created_at,
+            "execute_at": self.execute_at,
+            "priority": self.priority,
+            "queue": self.queue,
+            "task": self.task,
+            "args": self.args,
+        }
+
+
+class Job(BaseJob):
+    """pgq builtin Job model"""
---
+++ packages/django-pg-queue/django-pg-queue-0.6.1.tar.gz/django-pg-queue-0.6.1/setup.py
@@ -0,0 +1,62 @@
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import setup, Command
+
+VERSION = "0.6.1"
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(HERE, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPi via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(VERSION))
+        os.system("git push --tags")
+
+        sys.exit()
+
+
+setup(
+    name="django-pg-queue",
+    version=VERSION,
+    packages=["pgq", "pgq.migrations",],
+    package_data={"pgq": ["py.typed"]},
+    license="BSD",
+    long_description=open("README.rst").read(),
+    author="SweetProcess",
+    author_email="support@sweetprocess.com",
+    url="https://github.com/SweetProcess/django-pg-queue",
+    install_requires=["Django>=2.1",],
+    # $ setup.py publish support.
+    cmdclass={"upload": UploadCommand,},
+)
