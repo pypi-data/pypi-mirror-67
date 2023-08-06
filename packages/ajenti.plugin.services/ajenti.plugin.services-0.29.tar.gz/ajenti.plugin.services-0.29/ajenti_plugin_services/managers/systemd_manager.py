7c80d53187c124e3b957603d7e47e8857a54a0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:35 +0000

    Add package ajenti.plugin.services/0.29/ajenti.plugin.services-0.29.tar.gz

---
+++ packages/ajenti.plugin.services/ajenti.plugin.services-0.29.tar.gz/ajenti.plugin.services-0.29/ajenti_plugin_services/managers/systemd_manager.py
@@ -0,0 +1,60 @@
+import subprocess
+
+from jadi import component
+from aj.plugins.services.api import ServiceManager, Service
+
+
+@component(ServiceManager)
+class SystemdServiceManager(ServiceManager):
+    id = 'systemd'
+    name = 'systemd'
+
+    @classmethod
+    def __verify__(cls):
+        return subprocess.call(['which', 'systemctl']) == 0
+
+    def __init__(self, context):
+        pass
+
+    def list(self, units=None):
+        if not units:
+            units = [x.split()[0] for x in subprocess.check_output(['systemctl', 'list-unit-files', '--no-legend', '--no-pager', '-la']).decode().splitlines() if x]
+            units = [x for x in units if x.endswith('.service') and '@' not in x]
+            units = list(set(units))
+
+        cmd = ['systemctl', 'show', '-o', 'json', '--full', '--all'] + units
+
+        used_names = set()
+        unit = {}
+        for l in subprocess.check_output(cmd).decode().splitlines() + [None]:
+            if not l:
+                if len(unit) > 0:
+                    svc = Service(self)
+                    svc.id = unit['Id']
+                    svc.name, type = svc.id.rsplit('.', 1)
+
+                    svc.name = svc.name.replace('\\x2d', '\x2d')
+                    svc.running = unit['SubState'] == 'running'
+                    svc.state = 'running' if svc.running else 'stopped'
+
+                    if svc.name not in used_names:
+                        yield svc
+
+                    used_names.add(svc.name)
+                unit = {}
+            elif '=' in l:
+                k, v = l.split('=', 1)
+                unit[k] = v
+
+    def get_service(self, _id):
+        for s in self.list(units=[_id]):
+            return s
+
+    def start(self, _id):
+        subprocess.check_call(['systemctl', 'start', _id], close_fds=True)
+
+    def stop(self, _id):
+        subprocess.check_call(['systemctl', 'stop', _id], close_fds=True)
+
+    def restart(self, _id):
+        subprocess.check_call(['systemctl', 'restart', _id], close_fds=True)
