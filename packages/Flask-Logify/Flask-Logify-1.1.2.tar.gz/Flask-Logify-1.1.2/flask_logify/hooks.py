633b07fc122d1dccf29aeecb21ad1c4f7e552372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:14 +0000

    Add package Flask-Logify/1.1.2/Flask-Logify-1.1.2.tar.gz

---
+++ packages/Flask-Logify/Flask-Logify-1.1.2.tar.gz/Flask-Logify-1.1.2/flask_logify/hooks.py
@@ -0,0 +1,48 @@
+from flask import request
+from flask import current_app as app
+
+
+def hook_log_request():
+    """
+
+    """
+    if request.environ.get('HTTP_X_FORWARDED_FOR'):
+        request.environ['REMOTE_ADDR'] = request.environ['HTTP_X_FORWARDED_FOR']
+    elif request.environ.get('HTTP_X_REAL_IP'):
+        request.environ['REMOTE_ADDR'] = request.environ['HTTP_X_REAL_IP']
+
+    remote_address = request.environ['REMOTE_ADDR']
+    app.logger.info(
+        "INCOMING REQUEST: %s %s %s %s",
+        remote_address,
+        request.method,
+        request.scheme,
+        request.full_path
+    )
+
+    app.logger.debug("REQUEST HEADERS\n%s", request.headers)
+
+    data = request.get_data(as_text=True)
+    if data:
+        app.logger.debug("REQUEST BODY\n{}".format(data))
+
+
+def hook_log_response(response):
+    """
+
+    :param response:
+    :return:
+    """
+    code = int(response.status_code / 100)
+    log = app.logger.debug
+
+    if code in (1, 2, 3):
+        log = app.logger.info
+    if code == 4:
+        log = app.logger.warning
+    if code == 5:
+        log = app.logger.error
+
+    log("RESPONSE STATUS: %s", response.status)
+    app.logger.debug("RESPONSE HEADERS\n%s", response.headers)
+    return response
