97335ce5d27e5c3b079a7f2b4238bc977cd18bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:31 +0000

    Add package findblas/0.1.9/findblas-0.1.9.tar.gz

---
+++ packages/findblas/findblas-0.1.9.tar.gz/findblas-0.1.9/setup.py
@@ -0,0 +1,15 @@
+try:
+	from setuptools import setup
+except:
+	from distutils.core import setup
+
+setup(
+  name = 'findblas',
+  packages = ['findblas'],
+  version = '0.1.9',
+  author = 'David Cortes',
+  url = 'https://github.com/david-cortes/findblas',
+  classifiers = [],
+  data_files=[('include', ['findblas/findblas.h', 'findblas/rtd_mock.h'])],
+  include_package_data=True
+) 
