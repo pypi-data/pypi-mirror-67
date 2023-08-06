c30cf0500ae32789c8365189ad25b353d70ac64a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:52 +0000

    Add package aiopulse/0.4.0/aiopulse-0.4.0.tar.gz

---
+++ packages/aiopulse/aiopulse-0.4.0.tar.gz/aiopulse-0.4.0/setup.py
@@ -0,0 +1,24 @@
+"""Pip setup file for aiopulse library."""
+from distutils.core import setup
+
+setup(
+    name="aiopulse",
+    packages=["aiopulse"],
+    version="0.4.0",
+    license="apache-2.0",
+    description="Python module for Rollease Acmeda Automate integration.",
+    author="Alan Murray",
+    author_email="pypi@atmurray.net",
+    url="https://github.com/atmurray/aiopulse",
+    download_url="https://github.com/atmurray/aiopulse/archive/v0.4.0.tar.gz",
+    keywords=["automation"],
+    install_requires=["asyncio", "async_timeout"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+    ],
+)
