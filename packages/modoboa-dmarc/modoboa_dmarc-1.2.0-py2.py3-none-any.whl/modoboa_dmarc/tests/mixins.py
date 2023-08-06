ab29d1db4a86eb7cb6fd8881c981de6f44e2ac19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:26 +0000

    Add package modoboa-dmarc/1.2.0/modoboa_dmarc-1.2.0-py2.py3-none-any.whl

---
+++ packages/modoboa-dmarc/modoboa_dmarc-1.2.0-py2.py3-none-any.whl/modoboa_dmarc/tests/mixins.py
@@ -0,0 +1,49 @@
+"""Test mixins."""
+
+import os
+import sys
+
+import six
+
+from django.core.management import call_command
+from django.utils.six import StringIO
+
+
+class CallCommandMixin(object):
+    """A mixin to provide command execution shortcuts."""
+
+    def setUp(self):
+        """Replace stdin"""
+        super(CallCommandMixin, self).setUp()
+        self.stdin = sys.stdin
+
+    def tearDown(self):
+        sys.stdin = self.stdin
+
+    def import_report(self, path):
+        """Import test report from file."""
+        with open(path) as fp:
+            buf = six.StringIO(fp.read())
+        sys.stdin = buf
+        out = StringIO()
+        call_command("import_aggregated_report", "--pipe", stdout=out)
+        return out.getvalue()
+
+    def import_reports(self, folder="reports"):
+        """Import reports from folder."""
+        path = os.path.join(os.path.dirname(__file__), folder)
+        for f in os.listdir(path):
+            fpath = os.path.join(path, f)
+            if f.startswith(".") or not os.path.isfile(fpath):
+                continue
+            self.import_report(fpath)
+
+    def import_fail_reports(self, folder="fail-reports"):
+        """Import failed reports from folder."""
+        path = os.path.join(os.path.dirname(__file__), folder)
+        for f in os.listdir(path):
+            fpath = os.path.join(path, f)
+            if f.startswith(".") or not os.path.isfile(fpath):
+                continue
+            with self.assertRaisesMessage(SystemExit, '65'):
+                self.import_report(fpath)
---
+++ packages/modoboa-dmarc/modoboa_dmarc-1.2.0-py2.py3-none-any.whl/modoboa_dmarc/tests/test_management_command.py
@@ -0,0 +1,25 @@
+"""Management command tests."""
+
+from modoboa.admin import factories as admin_factories
+from modoboa.lib.tests import ModoTestCase
+
+from . import mixins
+from .. import models
+
+
+class ManagementCommandTestCase(mixins.CallCommandMixin, ModoTestCase):
+    """Test management command."""
+
+    @classmethod
+    def setUpTestData(cls):
+        super(ManagementCommandTestCase, cls).setUpTestData()
+        cls.domain = admin_factories.DomainFactory(name="ngyn.org")
+
+    def test_import_from_archive(self):
+        """Import report from archive."""
+        self.import_reports()
+        self.import_fail_reports()
+        self.assertTrue(self.domain.record_set.exists())
+        self.assertTrue(
+            models.Reporter.objects.filter(
+                org_name="FastMail Pty Ltd").exists())
