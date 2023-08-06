6e0c1e76aee15f557b8c1ee241003d2fb0c19731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:11 +0000

    Add package cozytouchpy/1.5.4/cozytouchpy-1.5.4.tar.gz

---
+++ packages/cozytouchpy/cozytouchpy-1.5.4.tar.gz/cozytouchpy-1.5.4/cozytouchpy/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+"""
+Provides authentification and row access to Cozytouch modules.
+"""
+name = "cozytouchpy"
+__version__ = "1.5.4"
+
+from .client import CozytouchClient
+from .exception import CozytouchException, CozytouchAuthentificationFailed
