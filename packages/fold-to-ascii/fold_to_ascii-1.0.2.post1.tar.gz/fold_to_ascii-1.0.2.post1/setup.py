84ace2f23830aca5e992d9e1f3f885f656f4e1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:22 +0000

    Add package fold-to-ascii/1.0.2.post1/fold_to_ascii-1.0.2.post1.tar.gz

---
+++ packages/fold-to-ascii/fold_to_ascii-1.0.2.post1.tar.gz/fold_to_ascii-1.0.2.post1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+setuptools.setup(
+    name='fold_to_ascii',
+    version='1.0.2.post1',
+    description='A Python port of the Apache Lucene ASCII Folding Filter that converts alphabetic, numeric, and symbolic Unicode characters which are not in the first 127 ASCII characters (the ‘Basic Latin’ Unicode block) into ASCII equivalents, if they exist.',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='William Bert',
+    author_email='william@spanishdict.com',
+    url='https://github.com/spanishdict/fold_to_ascii',
+    license="MIT License",
+    packages=setuptools.find_packages(exclude=('tests')),
+    keywords="ascii unicode sanitize diacritics fold folding ligatures"
+)
