abb40ca43af56655ed095d6ceaacef83a76b56c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:12 +0000

    Add package shield34/1.0.257/shield34-1.0.257-257-py2.py3-none-any.whl

---
+++ packages/shield34/shield34-1.0.257-257-py2.py3-none-any.whl/shield34_reporter/model/csv_rows/basic_csv_row.py
@@ -0,0 +1,17 @@
+import time
+
+
+class BasicCsvRow(object):
+    timestamp = ""
+    header = []
+    csv_file_name = ""
+
+    def __init__(self, header, csv_file_name):
+        self.timestamp = time.time()
+        self.header = header
+        self.csv_file_name = csv_file_name
+
+
+def make_basic_csv_row(header, csv_file_name):
+    basic_csv_row = BasicCsvRow(header, csv_file_name)
+    return basic_csv_row
