872834d20fd2ea754467201811252cf466d103b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:00 +0000

    Add package yamk/0.12.0/yamk-0.12.0.tar.gz

---
+++ packages/yamk/yamk-0.12.0.tar.gz/yamk-0.12.0/src/yamk/lib.py
@@ -0,0 +1,181 @@
+import datetime
+import math
+import re
+import shlex
+
+from yamk.functions import functions
+
+VAR = re.compile(
+    r"(?P<dollars>\$+){(?P<variable>[a-zA-Z0-9_.]+)(?P<sep>:)?(?P<key>[a-zA-Z0-9_.]+)?}"
+)
+OPTIONS = re.compile(r"\[(?P<options>.*?)\](?P<string>.*)")
+FUNCTION = re.compile(r"\$\(\((?P<name>\w+) *(?P<args>.*)\)\)")
+
+
+class Recipe:
+    def __init__(self, target, raw_recipe, base_dir, arg_vars, *, specified=False):
+        self._specified = specified
+        self._raw_recipe = raw_recipe
+        self.base_dir = base_dir
+        self.vars: Variables
+        self.arg_vars = arg_vars
+        self.phony = raw_recipe.get("phony", False)
+        self.requires = raw_recipe.get("requires", [])
+        self.variable_list = raw_recipe.get("vars", [])
+        self.commands = raw_recipe.get("commands", [])
+        self.echo = raw_recipe.get("echo", False)
+        self.regex = raw_recipe.get("regex", False)
+        self.allow_failures = raw_recipe.get("allow_failures", False)
+        self.target = self._target(target)
+        if self.phony:
+            self.keep_ts = raw_recipe.get("keep_ts", False)
+            self.exists_only = False
+            self.recursive = False
+        else:
+            self.keep_ts = False
+            self.exists_only = raw_recipe.get("exists_only", False)
+            self.recursive = raw_recipe.get("recursive", False)
+
+    def __str__(self):
+        if self._specified:
+            return f"Specified recipe for {self.target}"
+        return f"Generic recipe for {self.target}"
+
+    def for_target(self, target, globs):
+        if self._specified:
+            return self
+        new_recipe = self.__class__(
+            target, self._raw_recipe, self.base_dir, self.arg_vars, specified=True
+        )
+        if new_recipe.regex:
+            groups = re.fullmatch(self.target, new_recipe.target).groupdict()
+        else:
+            groups = {}
+        new_recipe._update_variables(globs, groups)
+        new_recipe._update_requirements()
+        new_recipe._update_commands()
+        return new_recipe
+
+    def _update_variables(self, globs, groups):
+        extra_vars = [groups, *self.arg_vars, {".target": self.target}]
+        self.vars = globs.add_batch(self.variable_list).add_batch(extra_vars)
+
+    def _update_requirements(self):
+        parser = Parser(self.vars, self.base_dir)
+        self.requires = parser.evaluate(self.requires)
+
+    def _update_commands(self):
+        extra_vars = [{".requirements": self.requires}]
+        self.vars = self.vars.add_batch(extra_vars)
+        parser = Parser(self.vars, self.base_dir)
+        self.commands = parser.evaluate(self.commands)
+
+    def _target(self, target):
+        if not self.phony:
+            target = self.base_dir.joinpath(target).as_posix()
+        if self.regex and not self._specified:
+            target = re.compile(target)
+        return target
+
+
+class Variables(dict):
+    def __init__(self, base_dir, **kwargs):
+        super().__init__(**kwargs)
+        self.base_dir = base_dir
+
+    def add_batch(self, batch):
+        new_vars = self.__class__(self.base_dir, **self)
+        for var_block in batch:
+            parser = Parser(new_vars, self.base_dir)
+            for key, value in var_block.items():
+                key = parser.evaluate(key)
+                key, options = extract_options(key)
+                if "weak" in options:
+                    continue
+                value = parser.evaluate(value)
+                new_vars[key] = value
+        return new_vars
+
+
+class Parser:
+    def __init__(self, variables, base_dir):
+        self.vars = variables
+        self.base_dir = base_dir
+
+    @staticmethod
+    def _stringify(value):
+        if isinstance(value, list):
+            return " ".join(map(str, value))
+        return str(value)
+
+    def expand_function(self, name, args):
+        args = shlex.split(args)
+        for i, arg in enumerate(args):
+            args[i] = self.evaluate(arg)
+        function = functions.get(name)(self.base_dir)
+        return function(*args)
+
+    def repl(self, matchobj):
+        dollars = matchobj.group("dollars")
+        variable = matchobj.group("variable")
+        key = matchobj.group("key")
+        if len(dollars) % 2:
+            value = self.vars[variable]
+            if key is None:
+                return self._stringify(value)
+            if isinstance(value, list):
+                key = int(key)
+            return self._stringify(value[key])
+        return f"{'$'*(len(dollars)//2)}{{{variable}}}"
+
+    def substitute(self, string):
+        function = re.fullmatch(FUNCTION, string)
+        if function is not None:
+            return self.expand_function(**function.groupdict())
+
+        if (
+            string.startswith("$")
+            and not string.startswith("$$")
+            and re.fullmatch(VAR, string)
+        ):
+            match = re.fullmatch(VAR, string)
+            if match.group("sep") is None:
+                return self.vars[match.group("variable")]
+        return re.sub(VAR, self.repl, string)
+
+    def evaluate(self, obj):
+        if isinstance(obj, str):
+            return self.substitute(obj)
+        if isinstance(obj, list):
+            out = []
+            for string in obj:
+                evaluated = self.evaluate(string)
+                if isinstance(evaluated, list):
+                    out.extend(evaluated)
+                else:
+                    out.append(evaluated)
+            return out
+        if isinstance(obj, dict):
+            return {
+                self.evaluate(key): self.evaluate(value) for key, value in obj.items()
+            }
+        raise TypeError(f"{obj.__class__.__name__} is not supported for evaluation")
+
+
+def extract_options(string):
+    match = re.fullmatch(OPTIONS, string)
+    if match is None:
+        return string.strip(), set()
+
+    options = match.group("options")
+    string = match.group("string")
+    return (
+        string,
+        set(map(lambda s: s.strip(), options.split(","))),
+    )
+
+
+def timestamp_to_dt(timestamp):
+    if math.isinf(timestamp):
+        return datetime.datetime.max
+    return datetime.datetime.utcfromtimestamp(timestamp)
---
+++ packages/yamk/yamk-0.12.0.tar.gz/yamk-0.12.0/src/yamk/make.py
@@ -0,0 +1,216 @@
+import itertools
+import os
+import pathlib
+import re
+import subprocess
+import sys
+
+import tomlkit
+
+from yamk import lib
+
+
+class MakeCommand:
+    def __init__(self, args):
+        self.verbosity = args.verbose
+        if self.verbosity:
+            if self.verbosity > 1:
+                print(args)
+            sys.tracebacklimit = 9999
+        self.regex_recipes = {}
+        self.static_recipes = {}
+        self.aliases = {}
+        self.target = args.target
+        makefile = args.makefile
+        self.arg_vars = [
+            {key: value}
+            for key, value in map(
+                lambda var: var.split("=", maxsplit=1), args.variables
+            )
+        ]
+        self.base_dir = pathlib.Path(makefile).parent.absolute()
+        self.phony_dir = self.base_dir.joinpath(".yamk")
+        with open(makefile) as file:
+            parsed_toml = tomlkit.parse(file.read())
+        file_vars = parsed_toml.pop("$globals", {})
+        self._parse_recipes(parsed_toml)
+        self.vars = (
+            lib.Variables(self.base_dir, **os.environ)
+            .add_batch(self.arg_vars)
+            .add_batch(file_vars.get("vars", []))
+        )
+        self.subprocess_kwargs = {"shell": True, "cwd": self.base_dir}
+
+    def make(self):
+        preprocessed = self._preprocess_target()
+        for info in filter(
+            lambda x: x["should_build"],
+            sorted(preprocessed.values(), key=lambda x: x["priority"], reverse=True),
+        ):
+            self._make_target(info["recipe"])
+
+    def _parse_recipes(self, parsed_toml):
+        for target, raw_recipe in parsed_toml.items():
+            if raw_recipe.get("alias"):
+                self.aliases[target] = raw_recipe["alias"]
+            recipe = lib.Recipe(target, raw_recipe, self.base_dir, self.arg_vars)
+
+            if recipe.regex:
+                self.regex_recipes[recipe.target] = recipe
+            else:
+                self.static_recipes[recipe.target] = recipe
+
+    def _preprocess_target(self):
+        recipe = self._extract_recipe(self.target)
+        if recipe is None:
+            raise ValueError(f"No recipe to build {self.target}")
+
+        unprocessed = {recipe.target: {"recipe": recipe, "priority": 0}}
+        preprocessed = {}
+        while unprocessed:
+            target, info = unprocessed.popitem()
+            priority = info["priority"] + 1
+            target_recipe = info["recipe"]
+            preprocessed[target_recipe.target] = info
+            for index, raw_requirement in enumerate(target_recipe.requires):
+                recipe = self._extract_recipe(raw_requirement)
+                if recipe is None:
+                    requirement_path = self._file_path(raw_requirement)
+                    requirement = requirement_path.as_posix()
+                    if not requirement_path.exists():
+                        raise ValueError(f"No recipe to build {requirement}")
+                else:
+                    requirement = recipe.target
+                target_recipe.requires[index] = requirement
+
+                if requirement in preprocessed:
+                    current_priority = preprocessed[requirement]["priority"]
+                    preprocessed[requirement]["priority"] = max(
+                        priority, current_priority
+                    )
+                elif requirement in unprocessed:
+                    current_priority = unprocessed[requirement]["priority"]
+                    unprocessed[requirement]["priority"] = max(
+                        priority, current_priority
+                    )
+                elif recipe is None:
+                    preprocessed[requirement] = {"priority": priority}
+                else:
+                    unprocessed[requirement] = {
+                        "recipe": recipe,
+                        "priority": priority,
+                    }
+
+        self._mark_unchanged(preprocessed)
+        if self.verbosity > 3:
+            print("=== all targets ===")
+            for target, options in preprocessed.items():
+                print(f"- {target}:")
+                print(f"    priority: {options['priority']}")
+                print(f"    timestamp: {lib.timestamp_to_dt(options['timestamp'])}")
+                print(f"    should_build: {options['should_build']}")
+        return preprocessed
+
+    def _make_target(self, recipe):
+        if self.verbosity > 1:
+            print(f"=== target: {recipe.target} ===")
+
+        for command in recipe.commands:
+            command, options = lib.extract_options(command)
+            if recipe.echo or "echo" in options or self.verbosity > 2:
+                print(command)
+            result = subprocess.run(command, **self.subprocess_kwargs)
+            if (
+                result.returncode
+                and not recipe.allow_failures
+                and "allow_failures" not in options
+            ):
+                sys.exit(result.returncode)
+        if recipe.phony and recipe.keep_ts:
+            path = self._phony_path(recipe.target)
+            self.phony_dir.mkdir(exist_ok=True)
+            path.touch()
+
+    def _extract_recipe(self, target):
+        if target in self.aliases:
+            target = self.aliases[target]
+
+        absolute_path_target = self.base_dir.joinpath(target).as_posix()
+        if target in self.static_recipes:
+            recipe = self.static_recipes[target]
+        elif absolute_path_target in self.static_recipes:
+            recipe = self.static_recipes[absolute_path_target]
+        else:
+            for regex, recipe in self.regex_recipes.items():  # noqa: B007
+                if re.fullmatch(regex, target) or re.fullmatch(
+                    regex, absolute_path_target
+                ):
+                    break
+            else:
+                return None
+
+        return recipe.for_target(target, self.vars)
+
+    def _mark_unchanged(self, preprocessed):
+        for target, info in sorted(
+            preprocessed.items(), key=lambda x: x[1]["priority"], reverse=True
+        ):
+            ts = self._infer_timestamp(target, info)
+            info["timestamp"] = ts
+            should_build = self._should_build(target, preprocessed)
+            info["should_build"] = should_build
+
+    def _phony_path(self, target):
+        encoded_target = target.replace(".", ".46").replace("/", ".47")
+        return self.phony_dir.joinpath(encoded_target)
+
+    def _file_path(self, target):
+        return self.base_dir.joinpath(target)
+
+    def _should_build(self, target, preprocessed):
+        info = preprocessed[target]
+        recipe = info.get("recipe")
+        if recipe is None:
+            return False
+        if recipe.phony:
+            if not recipe.keep_ts:
+                return True
+            path = self._phony_path(target)
+            if not path.exists():
+                return True
+        else:
+            path = self._file_path(target)
+            if not path.exists():
+                return True
+            if recipe.exists_only:
+                return False
+
+        if any(
+            preprocessed[requirement]["should_build"] for requirement in recipe.requires
+        ):
+            return True
+
+        ts = info["timestamp"]
+        req_ts = max(
+            preprocessed[requirement]["timestamp"] for requirement in recipe.requires
+        )
+        return req_ts > ts
+
+    def _infer_timestamp(self, target, info):
+        recipe = info.get("recipe")
+        path = self._file_path(target)
+        if recipe is None:
+            return path.stat().st_mtime
+        if recipe.phony:
+            path = self._phony_path(target)
+            if recipe.keep_ts and path.exists():
+                return path.stat().st_mtime
+            return float("inf")
+        if path.exists():
+            if recipe.recursive:
+                descendants = itertools.chain([path], path.rglob("*"))
+                return max(p.stat().st_mtime for p in descendants)
+            if recipe.exists_only:
+                return 0
+            return path.stat().st_mtime
+        return float("inf")
