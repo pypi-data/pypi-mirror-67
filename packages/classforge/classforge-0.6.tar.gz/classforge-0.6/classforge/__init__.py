bb78da77720cc9804d34d1d71eab7542e9a105e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:05 +0000

    Add package classforge/0.6/classforge-0.6.tar.gz

---
+++ packages/classforge/classforge-0.6.tar.gz/classforge-0.6/classforge/__init__.py
@@ -0,0 +1,8 @@
+
+from .classes import Class as _Class
+from .classes import StrictClass as _StrictClass
+from .fields import Field as _Field
+
+Class = _Class
+StrictClass = _StrictClass
+Field = _Field
