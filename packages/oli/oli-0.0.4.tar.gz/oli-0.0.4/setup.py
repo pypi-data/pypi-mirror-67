c871a48851a11df5006a3ba7d3f354951ccd82ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:41 +0000

    Add package oli/0.0.4/oli-0.0.4.tar.gz

---
+++ packages/oli/oli-0.0.4.tar.gz/oli-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="oli", # Replace with your own username
+    version="0.0.4",
+    author="StanGirard",
+    author_email="stan@primates.dev",
+    description="One Liner installer for Linux & MacOs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/StanGirard/OneLiners",
+    packages=["oli","oli.macos"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
