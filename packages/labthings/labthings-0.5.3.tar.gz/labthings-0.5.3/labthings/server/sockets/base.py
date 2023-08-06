4a69026ee220cd319b03ec808762a0f807cd7a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:02 +0000

    Add package labthings/0.5.3/labthings-0.5.3.tar.gz

---
+++ packages/labthings/labthings-0.5.3.tar.gz/labthings-0.5.3/labthings/server/sockets/base.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+
+from werkzeug.routing import Map, Rule
+from abc import ABC, abstractmethod
+
+from ..representations import encode_json
+
+
+class SocketSubscriber:
+    def __init__(self, ws):
+        self.ws = ws
+
+    def property_notify(self, viewcls):
+        if hasattr(viewcls, "get_value") and callable(viewcls.get_value):
+            property_value = viewcls().get_value()
+        else:
+            property_value = None
+
+        property_name = getattr(viewcls, "endpoint", None) or getattr(
+            viewcls, "__name__", "unknown"
+        )
+
+        response = encode_json(
+            {"messageType": "propertyStatus", "data": {property_name: property_value}}
+        )
+
+        self.ws.send(response)
+
+    def event_notify(self, event_dict: dict):
+        response = encode_json({"messageType": "event", "data": event_dict})
+
+        self.ws.send(response)
+
+
+class BaseSockets(ABC):
+    def __init__(self, app=None):
+        #: Compatibility with 'Flask' application.
+        #: The :class:`~werkzeug.routing.Map` for this instance. You can use
+        #: this to change the routing converters after the class was created
+        #: but before any routes are connected.
+        self.url_map = Map()
+
+        #: Compatibility with 'Flask' application.
+        #: All the attached blueprints in a dictionary by name. Blueprints
+        #: can be attached multiple times so this dictionary does not tell
+        #: you how often they got attached.
+        self.blueprints = {}
+        self._blueprint_order = []
+
+        if app:
+            self.init_app(app)
+
+    @abstractmethod
+    def init_app(self, app):
+        "Registers Flask middleware"
+
+    def route(self, rule, **options):
+        def decorator(f):
+            endpoint = options.pop("endpoint", None)
+            self.add_url_rule(rule, endpoint, f, **options)
+            return f
+
+        return decorator
+
+    def add_url_rule(self, rule, _, f, **options):
+        self.url_map.add(Rule(rule, endpoint=f))
+
+    def add_view(self, rule, f, **options):
+        return self.add_url_rule(rule, None, f, **options)
+
+    def register_blueprint(self, blueprint, **options):
+        """
+        Registers a blueprint for web sockets like for 'Flask' application.
+        Decorator :meth:`~flask.app.setupmethod` is not applied, because it
+        requires ``debug`` and ``_got_first_request`` attributes to be defined.
+        """
+        first_registration = False
+
+        if blueprint.name in self.blueprints:
+            assert self.blueprints[blueprint.name] is blueprint, (
+                "A blueprint's name collision occurred between %r and "
+                '%r.  Both share the same name "%s".  Blueprints that '
+                "are created on the fly need unique names."
+                % (blueprint, self.blueprints[blueprint.name], blueprint.name)
+            )
+        else:
+            self.blueprints[blueprint.name] = blueprint
+            self._blueprint_order.append(blueprint)
+            first_registration = True
+
+        blueprint.register(self, options, first_registration)
+
+
+def process_socket_message(message: str):
+    if message:
+        return None
+    else:
+        return None
---
+++ packages/labthings/labthings-0.5.3.tar.gz/labthings-0.5.3/labthings/server/view/__init__.py
@@ -0,0 +1,68 @@
+from flask.views import MethodView
+from flask import request, make_response
+from werkzeug.wrappers import Response as ResponseBase
+from werkzeug.exceptions import MethodNotAllowed
+
+from collections import OrderedDict
+
+from labthings.server.utilities import unpack
+from labthings.server.representations import DEFAULT_REPRESENTATIONS
+
+
+class View(MethodView):
+    """
+    A LabThing Resource class should make use of functions
+    get(), put(), post(), and delete(), corresponding to HTTP methods.
+
+    These functions will allow for automated documentation generation
+    """
+
+    endpoint = None
+
+    def __init__(self, *args, **kwargs):
+        MethodView.__init__(self, *args, **kwargs)
+
+        # Set the default representations
+        # TODO: Inherit from parent LabThing. See original flask_restful implementation
+        self.representations = OrderedDict(DEFAULT_REPRESENTATIONS)
+
+    def get_value(self):
+        get_method = getattr(self, "get", None)  # Look for this views GET method
+        if get_method is None:
+            return None
+        if not callable(get_method):
+            raise TypeError("Attribute 'get' of View must be a callable")
+        response = get_method()  # pylint: disable=not-callable
+        if isinstance(response, ResponseBase):  # Pluck useful data out of HTTP response
+            return response.json if response.json else response.data.decode()
+        else:  # Unless somehow an HTTP response isn't returned...
+            return response
+
+    def dispatch_request(self, *args, **kwargs):
+        meth = getattr(self, request.method.lower(), None)
+
+        # If the request method is HEAD and we don't have a handler for it
+        # retry with GET.
+        if meth is None and request.method == "HEAD":
+            meth = getattr(self, "get", None)
+
+        # Flask should ensure this is assersion never fails
+        assert meth is not None, f"Unimplemented method {request.method!r}"
+
+        # Generate basic response
+        resp = meth(*args, **kwargs)
+
+        if isinstance(resp, ResponseBase):  # There may be a better way to test
+            return resp
+
+        representations = self.representations or OrderedDict()
+
+        # noinspection PyUnresolvedReferences
+        mediatype = request.accept_mimetypes.best_match(representations, default=None)
+        if mediatype in representations:
+            data, code, headers = unpack(resp)
+            resp = representations[mediatype](data, code, headers)
+            resp.headers["Content-Type"] = mediatype
+            return resp
+
+        return resp
