2c7d8fe25a370e327e5f5ab244f874f93bc1a893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:31 +0000

    Add package ticker-terms/0.1/ticker-terms-0.1.tar.gz

---
+++ packages/ticker-terms/ticker-terms-0.1.tar.gz/ticker-terms-0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ticker_terms']
+
+package_data = \
+{'': ['*'], 'ticker_terms': ['DB/*']}
+
+setup_kwargs = {
+    'name': 'ticker-terms',
+    'version': '0.1',
+    'description': 'Generate searchable terms for public companies based off of the ticker.',
+    'long_description': 'This package is used to generate searchable terms from company ticker.\n\ncompany_obj = Company("AAPL")\ncompany_obj.terms\n\nterms is a dictionary with fields ticker, name, name_alt, kp, kp-alt',
+    'author': 'Henry Becket Trotter',
+    'author_email': 'beckettrotter@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/BecketTrotter/ticker_terms',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
