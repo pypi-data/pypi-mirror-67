b143e71dd068f80af346d361ff15846a86bc3bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:43 +0000

    Add package histore/0.1.0/histore-0.1.0-py3-none-any.whl

---
+++ packages/histore/histore-0.1.0-py3-none-any.whl/histore/__init__.py
@@ -0,0 +1,8 @@
+# This file is part of the History Store (histore).
+#
+# Copyright (C) 2018-2020 New York University.
+#
+# The History Store (histore) is released under the Revised BSD License. See
+# file LICENSE for full license details.
+
+from histore.archive.base import Archive, PersistentArchive  # noqa: F401
