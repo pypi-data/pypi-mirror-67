78744da8bbad4b5af2d64d153aa44e2e5245451c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:05 +0000

    Add package jsondaora/0.13.0/jsondaora-0.13.0-py3-none-any.whl

---
+++ packages/jsondaora/jsondaora-0.13.0-py3-none-any.whl/jsondaora/__init__.py
@@ -0,0 +1,31 @@
+"""Interoperates @dataclass with json objects"""
+
+from jsondaora.dataclasses import asdataclass
+from jsondaora.decorator import jsondaora
+from jsondaora.schema import (
+    IntegerField,
+    StringField,
+    integer,
+    jsonschema_asdataclass,
+    string,
+)
+from jsondaora.serializers import dataclass_asjson, typed_dict_asjson
+from jsondaora.typed_dict import as_typed_dict, as_typed_dict_field
+
+
+__version__ = '0.13.0'
+
+
+__all__ = [
+    'jsondaora',
+    'StringField',
+    'IntegerField',
+    'asdataclass',
+    'as_typed_dict',
+    'as_typed_dict_field',
+    'dataclass_asjson',
+    'typed_dict_asjson',
+    'integer',
+    'string',
+    'jsonschema_asdataclass',
+]
