946a2befba20a2d0e646aaf7276c150bc8a1de59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:25 +0000

    Add package pytest-salt-factories/0.10.3/pytest_salt_factories-0.10.3-py2.py3-none-any.whl

---
+++ packages/pytest-salt-factories/pytest_salt_factories-0.10.3-py2.py3-none-any.whl/tests/functional/factories/conftest.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+tests.functional.factories.conftest
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+import mock
+import pytest
+
+from saltfactories.factories import manager
+
+
+@pytest.fixture
+def salt_factories(
+    request,
+    pytestconfig,
+    tempdir,
+    log_server,
+    log_server_port,
+    log_server_level,
+    salt_factories_config,
+):
+    with mock.patch("saltfactories.utils.event_listener.EventListener"):
+        return manager.SaltFactoriesManager(
+            pytestconfig,
+            tempdir,
+            log_server_port,
+            log_server_level,
+            stats_processes=request.session.stats_processes,
+            **salt_factories_config
+        )
