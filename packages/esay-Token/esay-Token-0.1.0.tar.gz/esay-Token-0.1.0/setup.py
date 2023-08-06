82c8d5c454823137484bbae1223c16979d4a20cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:30 +0000

    Add package esay-Token/0.1.0/esay-Token-0.1.0.tar.gz

---
+++ packages/esay-Token/esay-Token-0.1.0.tar.gz/esay-Token-0.1.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import re
+import setuptools
+
+version = ""
+
+
+setuptools.setup(
+    name="esay-Token",
+    version="0.1.0",
+    author="q2536807",
+    author_email="616566665@qq.com ",
+    description="esayTokenTest",
+    long_description="esayTokenTest",
+    url="http://example.com",
+    install_requires=[],
+    packages=setuptools.find_packages(),
+    include_package_data = True,
+    platforms = "any"
+)
