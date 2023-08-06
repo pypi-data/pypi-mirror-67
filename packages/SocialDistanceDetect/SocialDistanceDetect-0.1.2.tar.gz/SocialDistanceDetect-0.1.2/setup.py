ee965a3ebe56a8414cbc7e161b9ef00bbd969b32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:55 +0000

    Add package SocialDistanceDetect/0.1.2/SocialDistanceDetect-0.1.2.tar.gz

---
+++ packages/SocialDistanceDetect/SocialDistanceDetect-0.1.2.tar.gz/SocialDistanceDetect-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setup(
+    # Metadata
+    name = 'SocialDistanceDetect',
+    version = '0.1.2',
+    description = 'lightweight video detection',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/thomascong121/SocialDistance',
+    author = 'SocialDistance model contributors',
+    author_email = 'thomascong@outlook.com',
+    packages = find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
