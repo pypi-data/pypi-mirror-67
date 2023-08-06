4d957564fb8d7ead6f211ed28c6a51380477ab7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:31 +0000

    Add package bravia-tv/1.0.3/bravia_tv-1.0.3.tar.gz

---
+++ packages/bravia-tv/bravia_tv-1.0.3.tar.gz/bravia_tv-1.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bravia_tv", # Replace with your own username
+    version="1.0.3",
+    maintainer="David Nielsen",
+    maintainer_email="dncielsen90@gmail.com",
+    description="Python Bravia TV remote control",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dcnielsen90/python-bravia-tv.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
