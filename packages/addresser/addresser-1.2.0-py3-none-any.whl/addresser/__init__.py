d315d54e6ba88c90a8f09adb204dcfa92e8053e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:27 +0000

    Add package addresser/1.2.0/addresser-1.2.0-py3-none-any.whl

---
+++ packages/addresser/addresser-1.2.0-py3-none-any.whl/addresser/__init__.py
@@ -0,0 +1,9 @@
+from .parser import (
+    parse_intersection,
+    parse_location,
+    parse_informal_address,
+    parse_address,
+)
+
+__version__ = "1.2.0"
+__all__ = [parse_location, parse_location, parse_informal_address, parse_address]
