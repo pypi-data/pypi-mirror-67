a47d31dcf6a2643e4d5a298f4299d106cfc01ebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:13 +0000

    Add package AJcreatePDF1/1.0/AJcreatePDF1-1.0.tar.gz

---
+++ packages/AJcreatePDF1/AJcreatePDF1-1.0.tar.gz/AJcreatePDF1-1.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+from pathlib import Path
+
+
+setuptools.setup(
+    name="AJcreatePDF1",
+    version=1.0,
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["tests", "data"])
+)                                                           # find_packages excude the given list of modules as they are not part of the source code
