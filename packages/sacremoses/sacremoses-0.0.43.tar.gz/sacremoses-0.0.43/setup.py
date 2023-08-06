44dc36f0f8f3c3d732ec958d0d99d362773aa8ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:30 +0000

    Add package sacremoses/0.0.43/sacremoses-0.0.43.tar.gz

---
+++ packages/sacremoses/sacremoses-0.0.43.tar.gz/sacremoses-0.0.43/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+import setuptools
+
+console_scripts = """
+[console_scripts]
+sacremoses=sacremoses.cli:cli
+"""
+
+setup(
+  name = 'sacremoses',
+  packages = ['sacremoses'],
+  version = '0.0.43',
+  description = 'SacreMoses',
+  long_description = 'MosesTokenizer in Python',
+  author = '',
+  package_data={'sacremoses': ['data/perluniprops/*.txt', 'data/nonbreaking_prefixes/nonbreaking_prefix.*']},
+  url = 'https://github.com/alvations/sacremoses',
+  keywords = [],
+  classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  install_requires = ['regex', 'six', 'click', 'joblib', 'tqdm'],
+  entry_points=console_scripts,
+)
