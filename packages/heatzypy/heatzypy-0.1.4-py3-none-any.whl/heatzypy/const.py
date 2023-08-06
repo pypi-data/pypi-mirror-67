7a59400f816083fa6fafb99f7c4d521daabe5d2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:33 +0000

    Add package heatzypy/0.1.4/heatzypy-0.1.4-py3-none-any.whl

---
+++ packages/heatzypy/heatzypy-0.1.4-py3-none-any.whl/heatzypy/const.py
@@ -0,0 +1,5 @@
+"""Constants for the Heatzy component."""
+HEATZY_APPLICATION_ID = "c70a66ff039d41b4a220e198b0fcc8b3"
+HEATZY_API_URL = "https://euapi.gizwits.com/app"
+HEATZY_PILOTE_V1_PRODUCT_KEY = "9420ae048da545c88fc6274d204dd25f"
+HEATZY_PILOTE_V2_PRODUCT_KEY = "51d16c22a5f74280bc3cfe9ebcdc6402"
