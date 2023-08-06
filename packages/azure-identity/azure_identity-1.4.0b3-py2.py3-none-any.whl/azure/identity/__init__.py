50c17eb1e086f20b84a972707ca3cee6a3821c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:28 +0000

    Add package azure-identity/1.4.0b3/azure_identity-1.4.0b3-py2.py3-none-any.whl

---
+++ packages/azure-identity/azure_identity-1.4.0b3-py2.py3-none-any.whl/azure/identity/__init__.py
@@ -0,0 +1,45 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+"""Credentials for Azure SDK clients."""
+
+from ._auth_record import AuthenticationRecord
+from ._exceptions import AuthenticationRequiredError, CredentialUnavailableError
+from ._constants import KnownAuthorities
+from ._credentials import (
+    AuthorizationCodeCredential,
+    CertificateCredential,
+    ChainedTokenCredential,
+    ClientSecretCredential,
+    DefaultAzureCredential,
+    DeviceCodeCredential,
+    EnvironmentCredential,
+    InteractiveBrowserCredential,
+    ManagedIdentityCredential,
+    SharedTokenCacheCredential,
+    UsernamePasswordCredential,
+)
+
+
+__all__ = [
+    "AuthenticationRecord",
+    "AuthenticationRequiredError",
+    "AuthorizationCodeCredential",
+    "CertificateCredential",
+    "ChainedTokenCredential",
+    "ClientSecretCredential",
+    "CredentialUnavailableError",
+    "DefaultAzureCredential",
+    "DeviceCodeCredential",
+    "EnvironmentCredential",
+    "InteractiveBrowserCredential",
+    "KnownAuthorities",
+    "ManagedIdentityCredential",
+    "SharedTokenCacheCredential",
+    "UsernamePasswordCredential",
+]
+
+from ._version import VERSION
+
+__version__ = VERSION
