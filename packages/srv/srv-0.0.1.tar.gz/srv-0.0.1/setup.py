738008cb181bc8b4fae5d469424dec97a39ab652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:00 +0000

    Add package srv/0.0.1/srv-0.0.1.tar.gz

---
+++ packages/srv/srv-0.0.1.tar.gz/srv-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="srv",
+    version="0.0.1",
+    author="David Teresi",
+    author_email="dkteresi@gmail.com",
+    description="Serve a directory with one command",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dkter/srv",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+        "Typing :: Typed",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'srv = srv:main'
+        ]
+    },
+)
