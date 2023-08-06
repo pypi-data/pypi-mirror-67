679ca16e1f4eed8c2187546f2cb278b4fa542b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:44 +0000

    Add package muarch/0.2.2/muarch-0.2.2-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/muarch/muarch-0.2.2-cp37-cp37m-manylinux1_x86_64.whl/muarch/calibrate/_calibrate_sd.py
@@ -0,0 +1,61 @@
+import numpy as np
+import scipy.optimize as opt
+
+from muarch.funcs import get_annualized_sd
+from ._calibrate_utils import validate_target_sd
+
+
+def calibrate_sd_only(data: np.ndarray, sd: np.ndarray, time_unit: int, tol=1e-6):
+    validate_target_sd(data, sd)
+    curr_sd = get_annualized_sd(data, time_unit)
+
+    sol = [RootFinder(data[..., i], time_unit, tol).find_root(target) for i, target in enumerate(sd)]
+    for i, s in enumerate(sol):
+        if np.isfinite(s) and s > 0:
+            data[..., i] *= s
+        else:
+            data[..., i] *= sd[i] / curr_sd[i]
+
+    return data
+
+
+class RootFinder:
+    def __init__(self, data: np.ndarray, time_unit: int, tol=1e-6):
+        assert data.ndim == 2
+        self.data = data
+        self.time_unit = time_unit
+        self.tol = tol
+
+    def annualized_sd(self, x: float, target: float):
+        return get_annualized_sd(self.data * x, self.time_unit) - target
+
+    def annualized_sd_der(self, x: float, target: float):
+        return (self.annualized_sd(x + self.tol, target) -
+                self.annualized_sd(x - self.tol, target)) / (2 * self.tol)
+
+    def find_root(self, target: float) -> float:
+        if self.is_similar(target):
+            return 0
+
+        return opt.newton(self.annualized_sd, self.initial_guess(target), self.annualized_sd_der,
+                          args=(target,), tol=self.tol, maxiter=1000)
+
+    def initial_guess(self, target: float):
+        def get_by_bisection():
+            i = np.argmin(np.abs(f_space[:-1] - f_space[1:])[mask])  # index with best root character
+            return (space[:-1][mask][i] + space[1:][mask][i]) / 2
+
+        def get_closest_to_0():
+            return float(space[np.argmin(np.abs(f_space))])
+
+        space = 2 ** np.linspace(-15, 6, 30)
+        f_space = np.array([self.annualized_sd(x, target) for x in space])
+        mask: np.ndarray = (f_space[:-1] * f_space[1:]) <= 0
+
+        if any(mask):
+            return get_by_bisection()
+        else:
+            return get_closest_to_0()
+
+    def is_similar(self, target: float):
+        return np.isclose(self.annualized_sd(0, target), 0, atol=self.tol)
---
+++ packages/muarch/muarch-0.2.2-cp37-cp37m-manylinux1_x86_64.whl/muarch/mean/_mean_simulations.pyx
@@ -0,0 +1,104 @@
+import numpy as np
+
+cimport cython
+from libc.stdint cimport int64_t
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+def simulate_harx(
+        double[:] y,
+        int64_t nobs,
+        int64_t k_x,
+        int64_t max_lag,
+        bint constant,
+        double[:, :] x,
+        double[:] errors,
+        double[:] params,
+        int64_t[:, :] lags):
+
+    cdef:
+        int64_t t, i, j, ind, lag_start, lag_end
+        double ar
+        int64_t[:] lag
+
+    for t in range(max_lag, nobs):
+        ind = 0
+        if constant:
+            y[t] = params[ind]
+            ind += 1
+
+        for j in range(len(lags)):
+            ar, lag = 0.0, lags[j]
+            for i in range(t-lag[1], t-lag[0]):
+                ar += y[i]
+            ar /= (lag[1] - lag[0])
+
+            y[t] += params[ind] * ar
+            ind += 1
+
+        for i in range(k_x):
+            y[t] += params[ind] * x[t, i]
+        y[t] += errors[t]
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+def simulate_harx_mc(
+        double[:, :] y,
+        int64_t nobs,
+        int64_t reps,
+        int64_t k_x,
+        int64_t max_lag,
+        bint constant,
+        double[:, :] x,
+        double[:, :] errors,
+        double[:] params,
+        int64_t[:, :] lags):
+
+    cdef double[:, :] data = _simulate_harx_mc(y, nobs, reps, k_x, max_lag, constant, x, errors, params, lags)
+    return np.asarray(data, np.float64)
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+cdef double[:, :] _simulate_harx_mc(
+    double[:, :] y,
+    int64_t nobs,
+    int64_t reps,
+    int64_t k_x,
+    int64_t max_lag,
+    bint constant,
+    double[:, :] x,
+    double[:, :] errors,
+    double[:] params,
+    int64_t[:, :] lags
+):
+    cdef:
+        int64_t i, j, r, t, ind
+        int64_t[:] lag
+        double ar
+
+    for r in range(reps):
+        for t in range(max_lag, nobs):
+            ind = 0
+            if constant:
+                y[t, r] = params[ind]
+                ind += 1
+
+            for j in range(len(lags)):
+                ar, lag = 0.0, lags[j]
+                for i in range(t-lag[1], t-lag[0]):
+                    ar += y[i, r]
+                ar /= (lag[1] - lag[0])
+
+                y[t, r] += params[ind] * ar
+                ind += 1
+
+            for i in range(k_x):
+                y[t, r] += params[ind] * x[t, i]
+            y[t, r] += errors[t, r]
+
+    return y
