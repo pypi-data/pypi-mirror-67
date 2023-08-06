95799858641947fa63af046a561910dbc2d81945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:31 +0000

    Add package pywebapi/0.1a1.dev4/pywebapi-0.1a1.dev4-py2.py3-none-any.whl

---
+++ packages/pywebapi/pywebapi-0.1a1.dev4-py2.py3-none-any.whl/pywebapi/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+    PyWebApi is a micro-framework for Python RESTfulization. It turns Python(3+) module/script into Web API (RESTful Service) out of the box.
+    With PyWebApi, any http client can invoke module level function in a managed way.
+    Input arguments of your function can be supplied in request body by JSON (recommended) or in URL query-string,
+    If the client further wraps a batch of arguments sets into an array as the request JSON,
+    the server will sequentially call the function by each argument set in the array,
+    and wrap all the result objects in a more outer array before return to the client.
+
+    Homepage and documentation: https://github.com/DataBooster/PyWebApi
+    Copyright (c) 2020 Abel Cheng
+    License: MIT
+"""
+
+import bottle
+
+from .func import execute, ModuleImporter, RequestArguments
+from .fmtr import MediaTypeFormatter, MediaTypeFormatterManager
+
+__version__ = "0.1a1.dev4"
