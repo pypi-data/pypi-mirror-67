1af7d8bc271eaf8c1698f4611db7face397d70dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:38 +0000

    Add package Mydemos-pkg/1.0.1.1/Mydemos_pkg-1.0.1.1.tar.gz

---
+++ packages/Mydemos-pkg/Mydemos_pkg-1.0.1.1.tar.gz/Mydemos_pkg-1.0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+ 
+NAME = 'Mydemos_pkg'
+VERSION = '1.00.1.1'
+PACKAGES = ['', 'Mydemos_pkg']
+setup(name = NAME
+        , version = VERSION
+        , packages = PACKAGES
+        ) 
