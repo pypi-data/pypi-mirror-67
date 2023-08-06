18a673471d8dd58b92b651cadecd571c8eda436f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:12 +0000

    Add package drf-firebase-auth/0.0.8/drf_firebase_auth-0.0.8-py3-none-any.whl

---
+++ packages/drf-firebase-auth/drf_firebase_auth-0.0.8-py3-none-any.whl/drf_firebase_auth/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'drf-firebase-auth'
+__version__ = '0.0.8'
+__author__ = 'Gary Burgmann'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020 Gary Burgmann'
+
+# Version synonym
+VERSION = __version__
