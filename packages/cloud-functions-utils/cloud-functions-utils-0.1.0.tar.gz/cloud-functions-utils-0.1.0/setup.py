465261933cc46e789fd0575871a5f26762091f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:44 +0000

    Add package cloud-functions-utils/0.1.0/cloud-functions-utils-0.1.0.tar.gz

---
+++ packages/cloud-functions-utils/cloud-functions-utils-0.1.0.tar.gz/cloud-functions-utils-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cloud_functions_utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['google-cloud-bigquery>=1.24.0,<2.0.0',
+ 'google-cloud-error-reporting>=0.33.0,<0.34.0',
+ 'google-cloud-pubsub>=1.4.3,<2.0.0',
+ 'google-cloud-storage>=1.28.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'cloud-functions-utils',
+    'version': '0.1.0',
+    'description': 'Utilities for Google Cloud Functions.',
+    'long_description': None,
+    'author': 'Jan-Benedikt Jagusch',
+    'author_email': 'jan.jagusch@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
