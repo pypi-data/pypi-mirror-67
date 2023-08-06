ae918c75012c873fb42fcbc8acafe67cb907f44f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:51 +0000

    Add package workadays/1.1.5/workadays-1.1.5.tar.gz

---
+++ packages/workadays/workadays-1.1.5.tar.gz/workadays-1.1.5/setup.py
@@ -0,0 +1,28 @@
+﻿# coding=UTF-8
+
+from setuptools import setup
+
+setup(
+    name='workadays',
+    version='1.1.5',
+    author='Marcelo Horita',
+    author_email='datazeus.tecnologia@gmail.com.br',
+    packages=['workadays'],
+    description='Calendário de dias úteis',
+    long_description='Pacote para adicionar ou subtrair datas em dias úteis. A princípio decidir focar apenas nos seguintes países: Brasil, Estados Unidos e Luxemburgo. No entanto, precisando de incluir novos países, estados ou regiões, será um prazer.',
+    url='https://github.com/mfhorita',
+    license='MIT',
+    keywords='workdays workadays feriados holidays diasuteis uteis',
+    classifiers=[
+        'Natural Language :: Portuguese (Brazilian)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Scientific/Engineering :: Physics'
+    ],
+    install_requires=[
+        'python_dateutil>=2.8.0',
+        'six>=1.11.0',
+        'convertdate>=2.2.0'
+    ]
+
+)
