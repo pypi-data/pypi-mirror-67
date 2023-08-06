32bdf0f1b2367a8cad1c467ebe03a6e08bb66c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:30 +0000

    Add package light-qiwi/1.3.0/Light_Qiwi-1.3.0.tar.gz

---
+++ packages/light-qiwi/Light_Qiwi-1.3.0.tar.gz/Light_Qiwi-1.3.0/Light_Qiwi/Objects.py
@@ -0,0 +1,250 @@
+# -*- coding: utf-8 -*-
+from Light_Qiwi.Enums import OperationType, Currency, PaymentStatus
+from Light_Qiwi.Utils import parse_item_enum, parse_item_multi_value_enum
+
+
+class Payment:
+    """Объект полученный от
+        https://developer.qiwi.com/ru/qiwi-wallet-personal/index.html#payments_list
+    """
+
+    transaction_id: int  # ID транзакции в процессинге QIWI Wallet
+    trm_txn_id: int  # Клиентский ID транзакции
+    type: OperationType  # Тип платежа (см. Enums.OperationType)
+    phone: int  # Номер кошелька
+    error_code: int  # Код ошибки платежа
+    error: str  # Описание ошибки
+    status: PaymentStatus  # Статус платежа (см. Enums.PaymentStatus)
+    status_text: str  # Текстовое описание статуса платежа
+    comment: str  # Комментарий к платежу
+    currency_rate: float  # Курс конвертации (если применяется в транзакции)
+    cheque_ready: bool  # Специальное поле
+    bank_document_available: bool  # Специальное поле
+    bank_document_ready: bool  # Специальное поле
+    repeat_payment_enabled: bool  # Специальное поле
+    favorite_payment_enabled: bool  # Специальное поле
+    regular_payment_enabled: bool  # Специальное поле
+    extras: dict  # Служебная информация
+    amount: float  # Сумма платежа без комиссии
+    currency: Currency  # Валюта платежа (см. Enums.CurrencyStr)
+    commission: float  # Комиссия платежа
+    total: float  # Фактическая сумма платежа или пополнения
+    provider_id: int  # ID провайдера в QIWI Wallet
+    provider_short_name: str  # Краткое наименование провайдера
+    provider_long_name: str  # Развернутое наименование провайдера
+    provider_logo_url: str  # Ссылка на логотип провайдера
+    provider_description: str  # Описание провайдера (HTML)
+    provider_keys: str  # Список ключевых слов
+    provider_site_url: str  # Сайт провайдера
+
+    # Дата/время платежа, во временной зоне запроса (см. параметр startDate).
+    date: str  # Формат даты ГГГГ-ММ-ДД'T'чч:мм:сс+03:00
+
+    # Для платежей - номер счета получателя
+    account: str  # Для пополнений - номер отправителя, терминала или название агента пополнения кошелька
+
+    def __init__(self, raw: dict):
+        self.raw = raw
+
+        self._parse(**raw)
+
+    def _parse(self, txnId: int = None, personId: int = None, date: str = None, errorCode: int = None,
+               error: str = None, type: str = None, status: str = None, statusText: str = None, trmTxnId: str = None,
+               account: str = None, sum: dict = None, commission: dict = None, total: dict = None,
+               provider: dict = None, comment: str = None, currencyRate: float = None, extras: dict = None,
+               chequeReady: bool = None, bankDocumentAvailable: bool = None, bankDocumentReady: bool = None,
+               repeatPaymentEnabled: bool = None, favoritePaymentEnabled: bool = None,
+               regularPaymentEnabled: bool = None):
+        self.transaction_id = txnId
+        self.trm_txn_id = trmTxnId
+        self.type = parse_item_enum(type, OperationType)
+        self.phone = personId
+        self.error_code = errorCode
+        self.error = error
+        self.date = date
+
+        self.status = parse_item_enum(status, PaymentStatus)
+        self.status_text = statusText
+        self.account = account
+
+        self.comment = comment
+        self.currency_rate = currencyRate
+        self.cheque_ready = chequeReady
+        self.bank_document_available = bankDocumentAvailable
+        self.bank_document_ready = bankDocumentReady
+        self.repeat_payment_enabled = repeatPaymentEnabled
+        self.favorite_payment_enabled = favoritePaymentEnabled
+        self.regular_payment_enabled = regularPaymentEnabled
+        self.extras = extras
+
+        if sum:
+            self.amount = sum['amount']
+            self.currency = parse_item_multi_value_enum(sum['currency'], Currency)
+
+        if commission:
+            self.commission = commission['amount']
+
+        if total:
+            self.total = total['amount']
+
+        if provider:
+            self.provider_id = provider['id']
+            self.provider_short_name = provider['shortName']
+            self.provider_long_name = provider['longName']
+            self.provider_logo_url = provider['logoUrl']
+            self.provider_description = provider['description']
+            self.provider_keys = provider['keys']
+            self.provider_site_url = provider['siteUrl']
+
+    def __eq__(self, other):
+        if isinstance(other, Payment):
+            return self.transaction_id == other.transaction_id
+
+    def __str__(self):
+        return str(self.raw)
+
+
+class Balance:
+    """Объект полученный от
+        https://developer.qiwi.com/ru/qiwi-wallet-personal/index.html?http#balances_list
+    """
+
+    alias: str  # Псевдоним пользовательского баланса
+    fs_alias: str  # Псевдоним банковского баланса
+    bank_alias: str  # Псевдоним банка
+    title: str  # Название соответствующего счета кошелька
+    type_id: str  # Описание счета
+    type_title: str  # Описание счета
+    has_balance: bool  # Логический признак реального баланса в системе QIWI Кошелек
+    amount: float  # Текущий баланс данного счета
+    currency: Currency  # Валюта платежа (см. Enums.CurrencyStr)
+
+    def __init__(self, raw: dict):
+        """
+        :param raw: Объект account от balances_list
+        :type raw: dict
+        """
+
+        self.raw = raw
+
+        self._parse(**raw)
+
+    def _parse(self, alias: str = None, fsAlias: str = None, bankAlias: str = None, title: str = None,
+               hasBalance: bool = None, currency: int = None, type: dict = None, balance: dict = None):
+        self.alias = alias
+        self.fs_alias = fsAlias
+        self.bank_alias = bankAlias
+        self.title = title
+        self.has_balance = hasBalance
+        self.currency = parse_item_multi_value_enum(currency, Currency)
+
+        if type:
+            self.type_id = type['id']
+            self.type_title = type['title']
+
+        if balance:
+            self.amount = balance['amount']
+
+    def __str__(self):
+        return str(self.amount)
+
+    def __repr__(self):
+        return "<Balance: {}, Alias: {}, Currency: {}>".format(
+            self.amount, self.alias, self.currency
+        )
+
+
+class QiwiTransferAnswer:
+    """Объект полученный от
+        https://developer.qiwi.com/ru/qiwi-wallet-personal/index.html#p2p
+    """
+
+    id: int  # Копия параметра id из исходного запроса
+    transaction_id: int  # ID транзакции в процессинге QIWI Wallet
+    terms: int  # Константа, 99
+    currency: Currency  # Валюта платежа (см. Enums.CurrencyStr)
+    account: str  # Копия account из исходного запроса
+    amount: float  # Копия amount из исходного запроса
+    source: str  # Константа, account_643
+    comment: str  # Копия параметра comment из исходного запроса
+    status: str  # Текущий статус транзакции, только значение Accepted
+
+    def __init__(self, raw: dict):
+        self.raw = raw
+
+        self._parse(**raw)
+
+    def _parse(self, id: int = None, terms: int = 99, fields: dict = None, sum: dict = None, transaction: dict = None,
+               comment: str = None, source: str = None):
+        self.id = id
+        self.terms = terms
+        self.comment = comment
+        self.source = source
+
+        if fields:
+            self.account = fields['account']
+
+        if sum:
+            self.amount = sum['amount']
+            self.currency = parse_item_multi_value_enum(sum['currency'], Currency)
+
+        if transaction:
+            self.transaction_id = transaction['id']
+            self.status = transaction['state']['code']
+
+    def __str__(self):
+        return str(self.raw)
+
+
+class Bill:
+    """
+
+    """
+
+    id: int  # Идентификатор счета в QIWI Кошельке
+    external_id: str  # Идентификатор счета у мерчанта
+    creation_datetime: int  # Дата/время создания счета, Unix-time
+    expiration_datetime: int  # Дата/время окончания срока действия счета, Unix-time
+    amount: float  # Сумма счета
+    currency: Currency  # Валюта суммы счета (см. Enums.CurrencyStr)
+    comment: str  # Комментарий к счету
+    status: str  # Константа, READY_FOR_PAY
+    repetitive: bool  # Служебное поле
+    pay_url: str  # Ссылка для оплаты счета в интерфейсе QIWI
+    type: str  # Константа, MERCHANT
+    provider_id: int  # ID провайдера в QIWI Wallet
+    provider_short_name: str  # Краткое наименование провайдера
+    provider_long_name: str  # Развернутое наименование провайдера
+    provider_logo_url: str  # Ссылка на логотип провайдера
+
+    def __init__(self, raw: dict):
+        self.raw = raw
+
+        self._parse(**raw)
+
+    def _parse(self, id: int = None, external_id: str = None, creation_datetime: int = None,
+               expiration_datetime: int = None, sum: dict = None, comment: str = None, status: str = None,
+               repetitive: bool = None, provider: dict = None, pay_url: str = None, type: str = None):
+        self.id = id
+        self.external_id = external_id
+        self.creation_datetime = creation_datetime
+        self.expiration_datetime = expiration_datetime
+        self.status = status
+        self.comment = comment
+        self.repetitive = repetitive
+        self.provider = provider
+        self.pay_url = pay_url
+        self.type = type
+
+        if sum:
+            self.amount = sum['amount']
+            self.currency = parse_item_multi_value_enum(sum['currency'], Currency)
+
+        if provider:
+            self.provider_id = provider['id']
+            self.provider_short_name = provider['shortName']
+            self.provider_long_name = provider['longName']
+            self.provider_logo_url = provider['logoUrl']
+
+    def __str__(self):
+        return str(self.raw)
---
+++ packages/light-qiwi/Light_Qiwi-1.3.0.tar.gz/Light_Qiwi-1.3.0/Light_Qiwi/Qiwi.py
@@ -0,0 +1,445 @@
+# -*- coding: utf-8 -*-
+from _thread import start_new_thread
+from time import time, sleep
+from typing import Callable, List
+from urllib.parse import quote
+
+from requests import Session, Response
+
+from Light_Qiwi.Enums import Provider, Field
+from Light_Qiwi.Errors import *
+from Light_Qiwi.Objects import *
+
+
+class Qiwi:
+
+    def __init__(self, token, phone):
+        """
+        :type token: str
+        :type phone: str
+
+        :param token: Токен от API Киви
+        :param phone: Номер для которого был получен токен
+        """
+
+        self._s = Session()
+
+        self._s.headers['Accept'] = 'application/json'
+        self._s.headers['Content-Type'] = 'application/json'
+        self._s.headers['Authorization'] = 'Bearer ' + token
+
+        self._host = 'https://edge.qiwi.com'
+
+        self.phone = phone.replace('+', '')
+
+        self.func = None
+        self.bill_func = None
+        self._bind_payments = []
+        self._bind_bills = []
+
+    @property
+    def balance(self) -> float:
+        """Получить баланс кошелька"""
+        balances = self.get_balance()
+
+        assert balances, 'Список балансов пуст!'
+
+        for balance in balances:
+            if balance.fs_alias == 'qb_wallet':
+                return balance
+
+        return balance
+
+    @property
+    def _transaction_id(self) -> str:
+        """
+        :return: UNIX time * 1000
+        """
+
+        return str(int(time() * 1000))
+
+    def get_pay_url(self, amount: float, comment: str = '', account: str = None,
+                    lock_fields: list = (Field.COMMENT, Field.ACCOUNT, Field.AMOUNT)) -> str:
+        url = 'https://qiwi.com/payment/form/99'
+        amount_int = int(amount)
+        amount_frac = int(round(amount % 1, 2) * 100)
+
+        # if nickname:
+        #    url += f"999?extra['accountType']=nickname&extra['account']={nickname}"
+        # else:
+
+        if not account:
+            account = self.phone
+        url += f"?extra['account']={account}&extra['comment']={comment}" \
+               f"&amountInteger={amount_int}&amountFraction={amount_frac}"
+
+        for i, k in enumerate(lock_fields):
+            url += f"&blocked[{i}]={k.value}"
+
+        return quote(url, '/?=&:')
+
+    @property
+    def identification(self) -> dict:
+        """Получить данные идентефикации аккаунта"""
+        return self._req(f'{self._host}/identification/v1/persons/{self.phone}/identification').json()
+
+    def pay(self, account: str, amount: float = 1.0, comment: str = '',
+            currency: Currency = Currency.RUB, provider: Provider = Provider.QIWI) -> QiwiTransferAnswer:
+        """Перевод денег на Qiwi кошелёк или Карты других банков.
+
+        :type account: str
+        :type amount: float
+        :type comment: str
+        :type currency: Currency
+        :type provider: Provider
+
+        :param comment: Комментарий к переводу
+        :param currency: Валюта. Разрешены только рубли
+        :param amount: Сумма перевода
+        :param provider: Провайдер (см. Enums.Provider). Куда необходимо переводить деньги:
+            на Qiwi, на Карту
+        :param account: Номер получателя
+
+        :raises: APIError
+
+        :returns: QiwiTransferAnswer object
+        :rtype: QiwiTransferAnswer
+        """
+
+        url = f'{self._host}/sinap/api/v2/terms/{provider.value}/payments'
+
+        json = {
+            'id': self._transaction_id,
+            'sum': {
+                'amount': amount,
+                'currency': str(currency.values[1])
+            },
+            'paymentMethod': {
+                'type': 'Account',
+                'accountId': str(currency.values[1])
+            },
+            'fields': {'account': account},
+            'comment': comment
+        }
+
+        if provider != Provider.QIWI:
+            del json['comment']
+
+        response = self._req(url, json=json)
+        data = response.json()
+
+        if 'code' in data or 'errorCode' in data:
+            data["status_code"] = response.status_code
+            raise APIError(data)
+
+        return QiwiTransferAnswer(data)
+
+    def cancel_bill(self, uid: int) -> Response:
+        """Отклонение счёта
+
+        :type uid: int
+
+        :param uid: ID счёта для отклонения
+
+        :rtype: dict
+        """
+
+        return self._req(f'{self._host}/checkout/api/bill/reject', json={
+            'id': uid
+        })
+
+    def pay_bill(self, uid: int, currency: Currency = Currency.RUB) -> dict:
+        """Оплата выставленного счёта
+
+        :param uid: ID счёта для оплаты
+        :param currency: Валюта счёта (см. Enums.Currency)
+        :return:
+        """
+
+        url = f'{self._host}/checkout/invoice/pay/wallet'
+
+        json = {
+            'invoice_uid': uid,
+            'currency': str(currency.values[1])
+        }
+
+        response = self._req(url, json=json)
+        data = response.json()
+
+        if 'code' in data or 'errorCode' in data:
+            data["status_code"] = response.status_code
+            raise APIError(data)
+
+        return data
+
+    def _req(self, url, params: dict = None, json: dict = None) -> Response:
+        """Метод для общения с API
+
+        :type url: str
+        :type params: dict
+        :type json: dict
+
+        :param url: Ссылка для запроса
+        :param params: Для GET запроса
+        :param json: Для POST запроса
+
+        :rtype: Response
+        """
+        if json:
+            response = self._s.post(url, json=json)
+        else:
+            response = self._s.get(url, params=params)
+
+        if response.status_code != 200 and not response.content:
+            RequestError(response.status_code)
+
+        return response
+
+    def get_balance(self, retry=False) -> List[Balance]:
+        """Возвращает список балансов
+
+        :type retry: bool
+
+        :param retry: Служебное
+
+        :rtype: list
+        """
+        url = f'{self._host}/funding-sources/v2/persons/{self.phone}/accounts'
+        params = {'timeout': 1000} if retry else {}
+
+        response = self._req(url, params).json()
+
+        balances = []
+
+        for account in response['accounts']:
+            if account['hasBalance']:
+                if not account['balance']:
+                    return self.get_balance(True)
+                balances.append(Balance(account))
+
+        return balances
+
+    def _person_profile(self, auth_info=True, contract_info=True, user_info=True) -> Response:
+        """Возвращает профиль пользователя
+
+        :type auth_info: bool
+        :type contract_info: bool
+        :type user_info: bool
+
+        :param auth_info: Служебное
+        :param contract_info: Служебное
+        :param user_info: Служебное
+
+        :rtype: Response
+        """
+        url = f'{self._host}/person-profile/v1/profile/current'
+
+        params = {
+            'authInfoEnabled': auth_info,
+            'contractInfoEnabled': contract_info,
+            'userInfoEnabled': user_info
+        }
+
+        return self._req(url, params)
+
+    def get_payments(self, rows: int = 25, operation: OperationType = OperationType.ALL) -> List[Payment]:
+        """Возвращает список последних операций
+
+        :type rows: int
+        :type operation: OperationType
+
+        :param rows: Число платежей в ответе
+        :param operation: Указывает какие типы операций отдать в ответе
+
+        :rtype: list
+        """
+        url = f'{self._host}/payment-history/v2/persons/{self.phone}/payments'
+
+        params = {
+            'rows': min(rows, 50),
+            'operation': operation.value
+        }
+
+        data = self._req(url, params).json()
+
+        if 'error' in data:
+            raise APIError(data['error'])
+        elif 'data' not in data:
+            raise APIError(data)
+        else:
+            return list(map(Payment, data['data']))
+
+    def get_bills(self, rows: int = 25) -> List[Bill]:
+        """Возвращает список выставленных счетов
+
+        :type rows: int
+
+        :param rows: Число счетов в ответе
+
+        :rtype: list
+        """
+
+        url = f'{self._host}/checkout/api/bill/search'
+
+        params = {
+            'rows': rows,
+            'statuses': 'READY_FOR_PAY'
+        }
+
+        data = self._req(url, params).json()
+
+        if 'error' in data:
+            raise APIError(data['error'])
+        elif 'bills' not in data:
+            raise APIError(data)
+        else:
+            return list(map(Bill, data['bills']))
+
+    def get_transaction(self, transaction_id: int) -> Payment:
+        """Получить транзакцию по её id
+        :type transaction_id: int
+
+        :param transaction_id:
+
+        :rtype: Payment
+        """
+        url = f'{self._host}/payment-history/v2/transactions/{transaction_id}'
+
+        return Payment(self._req(url).json())
+
+    def check(self, interval: int = 3, amount: int = 25,
+              operation: OperationType = OperationType.IN) -> Payment:
+        """Проверяет на наличие новых операций
+        :type interval: int
+        :type amount: int
+        :type operation: OperationType
+
+        :param interval: Интервал обновления истории
+        :param amount: Количество получаемых объектов за одно обновление
+        :param operation: Тип операции, который следует получать
+
+        :rtype: Payment
+        """
+        payments = []
+
+        while True:
+            new_payments = self.get_payments(amount, operation)
+
+            for payment in new_payments:
+                if payment not in payments:
+                    yield payment
+
+            payments = new_payments
+
+            sleep(interval)
+
+    def bind_check(self, interval: int = 3, amount: int = 25,
+                   operation: OperationType = OperationType.IN) -> Callable:
+        """
+        :type interval: int
+        :type amount: int
+        :type operation: OperationType
+
+        :param interval: Интервал обновления
+        :param amount: Число получаемых платежей из истории
+        :param operation: Тип операции плажетей
+        """
+
+        def decorator(func):
+            if func.__code__.co_argcount != 1:
+                raise ArgumentError('Функция должна иметь 1 аргумент!')
+
+            def run():
+                for payment in self.check(interval, amount, operation):
+                    func(payment)
+
+            self.func = run
+
+        return decorator
+
+    def start(self):
+        """Запустить блокирующую обработку новых платежей"""
+        return self.func()
+
+    def start_threading(self):
+        """Запустить обработку новых платежей в другом потоке"""
+        return start_new_thread(self.func, ())
+
+    def on_payment_func(self, interval: int = 3, amount: int = 25,
+                        operation: OperationType = OperationType.IN) -> Callable:
+
+        def decorator(func):
+            if func.__code__.co_argcount != 1:
+                raise ArgumentError('Функция должна иметь 1 аргумент!')
+
+            def run():
+                for payment in self.check(interval, amount, operation):
+                    if payment.comment in self._bind_payments:
+                        self._bind_payments.remove(payment.comment)
+                        func(payment)
+
+            self.func = run
+
+        return decorator
+
+    def bind(self, comment: str):
+        """Ожидание платежа с данным комментарием"""
+        self._bind_payments.append(comment)
+
+    def check_bill(self, interval: int = 3, amount: int = 3):
+        bills = []
+
+        while True:
+            new_bills = self.get_bills(amount)
+
+            for bill in new_bills:
+                if bill not in bills:
+                    yield bill
+
+            bills = new_bills
+
+            sleep(interval)
+
+    def bind_check_bill(self, interval: int = 3, amount: int = 3):
+        """
+        :param interval:
+        :param amount:
+        :return decorator:
+        """
+
+        def decorator(func):
+            if func.__code__.co_argcount != 1:
+                raise ArgumentError('Функция должна иметь 1 аргумент!')
+
+            def run():
+                for bill in self.check_bill(interval, amount):
+                    func(bill)
+
+            self.bill_func = run
+
+        return decorator
+
+    def start_bill(self):
+        return self.bill_func()
+
+    def start_threading_bill(self):
+        return start_new_thread(self.bill_func, ())
+
+    def on_bill_func(self, interval: int = 3, amount: int = 3) -> Callable:
+
+        def decorator(func):
+            if func.__code__.co_argcount != 1:
+                raise ArgumentError('Функция должна иметь 1 аргумент!')
+
+            def run():
+                for bill in self.check_bill(interval, amount):
+                    if bill.comment in self._bind_bills:
+                        self._bind_bills.remove(bill.comment)
+                        func(bill)
+
+            self.bill_func = run
+
+        return decorator
+
+    def bind_bill(self, comment):
+        self._bind_bills.append(comment)
