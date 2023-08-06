93de0b6e7a56f10f9378f368867dc3dc6f7b4df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:11 +0000

    Add package rchitect/0.3.25/rchitect-0.3.25-pp36-pypy36_pp73-win32.whl

---
+++ packages/rchitect/rchitect-0.3.25-pp36-pypy36_pp73-win32.whl/rchitect/src/process_events.h
@@ -0,0 +1,14 @@
+#ifndef PROCESS_EVENTS_H__
+#define PROCESS_EVENTS_H__
+
+# include "R.h"
+
+// begin cdef
+
+void process_events(void);
+
+int peek_event(void);
+
+// end cdef
+
+#endif /* end of include guard: PROCESS_EVENTS_H__ */
