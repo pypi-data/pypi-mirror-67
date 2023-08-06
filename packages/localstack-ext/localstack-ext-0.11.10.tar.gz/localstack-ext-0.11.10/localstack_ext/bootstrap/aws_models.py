a405ef5252fb188051f13232f6c9733512d83414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:39 +0000

    Add package localstack-ext/0.11.10/localstack-ext-0.11.10.tar.gz

---
+++ packages/localstack-ext/localstack-ext-0.11.10.tar.gz/localstack-ext-0.11.10/localstack_ext/bootstrap/aws_models.py
@@ -0,0 +1,18 @@
+from localstack.utils.aws import aws_models
+WeftQ=super
+Wefth=None
+WeftM=id
+class LambdaLayer(aws_models.LambdaFunction):
+ def __init__(self,arn):
+  WeftQ(LambdaLayer,self).__init__(arn)
+  self.cwd=Wefth
+  self.runtime=''
+  self.handler=''
+  self.envvars={}
+  self.versions={}
+class RDSDatabase(aws_models.Component):
+ def __init__(self,WeftM,env=Wefth):
+  WeftQ(RDSDatabase,self).__init__(WeftM,env=env)
+ def name(self):
+  return self.WeftM.split(':')[-1]
+# Created by pyminifier (https://github.com/liftoff/pyminifier)
