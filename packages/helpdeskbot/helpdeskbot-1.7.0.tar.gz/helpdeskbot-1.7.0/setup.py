ee193bfce9bf969a20c1b7229f2f8668c6cdc723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:38 +0000

    Add package helpdeskbot/1.7.0/helpdeskbot-1.7.0.tar.gz

---
+++ packages/helpdeskbot/helpdeskbot-1.7.0.tar.gz/helpdeskbot-1.7.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='helpdeskbot',
+    version='1.7.0',
+    description='Discord utility bot for DevDungeon.com',
+    url='https://github.com/DevDungeon/HelpDeskBot',
+    author='DevDungeon',
+    author_email='nanodano@devdungeon.com',
+    license='GPL-3.0',
+    packages=['helpdeskbot'],
+    scripts=[
+        'bin/helpdeskbot',
+        'bin/helpdeskbot.bat',
+    ],
+    zip_safe=False,
+    python_requires='<3.7',
+    install_requires=[
+        'discord.py==0.16.12',
+        'docopt',
+        'aiohttp',
+    ]
+)
