68d310964356fdb1cd701a263f1e658d24222bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:55 +0000

    Add package cidc-api-modules/0.18.0/cidc_api_modules-0.18.0.tar.gz

---
+++ packages/cidc-api-modules/cidc_api_modules-0.18.0.tar.gz/cidc_api_modules-0.18.0/setup.py
@@ -0,0 +1,19 @@
+"""Package up cidc_api/models.py for use in other services."""
+
+from setuptools import setup
+
+with open("requirements.modules.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="cidc_api_modules",
+    description="SQLAlchemy data models and configuration tools used in the CIDC API",
+    python_requires=">=3.6",
+    install_requires=requirements,
+    license="MIT license",
+    packages=["cidc_api.config"],
+    py_modules=["cidc_api.models", "cidc_api.gcloud_client", "cidc_api.emails"],
+    url="https://github.com/CIMAC-CIDC/cidc_api-gae",
+    version="0.18.0",
+    zip_safe=False,
+)
