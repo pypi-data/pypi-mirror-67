c49f691bea2bbb4f9725540b2b46e1b1470b6d5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:51 +0000

    Add package distributionsexerciseupload/1.4/distributionsexerciseupload-1.4.tar.gz

---
+++ packages/distributionsexerciseupload/distributionsexerciseupload-1.4.tar.gz/distributionsexerciseupload-1.4/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsexerciseupload',
+      version='1.4',
+      description='Gaussian distributions',
+      packages=['distributionsexerciseupload'],
+      zip_safe=False)
