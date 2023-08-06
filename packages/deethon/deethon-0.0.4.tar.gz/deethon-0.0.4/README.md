0f4e57303dd341588a90a7ad50f6d16a7c278fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:49 +0000

    Add package deethon/0.0.4/deethon-0.0.4.tar.gz

---
+++ packages/deethon/deethon-0.0.4.tar.gz/deethon-0.0.4/README.md
@@ -0,0 +1,3 @@
+# Deethon
+
+Python3 library to easily download music from Deezer
---
+++ packages/deethon/deethon-0.0.4.tar.gz/deethon-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="deethon",
+    version="0.0.4",
+    author="Aykut Yilmaz",
+    author_email="aykuxt@gmail.com",
+    description="Python3 library to easily download music from Deezer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aykuxt/deethon",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["mutagen", "requests", "Crypto"],
+    python_requires=">=3.6",
+)
