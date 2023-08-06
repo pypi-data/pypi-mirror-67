41a4dc9650444cadd2695c1b459e603db786cbda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:32 +0000

    Add package pdmongo/0.1.0/pdmongo-0.1.0.tar.gz

---
+++ packages/pdmongo/pdmongo-0.1.0.tar.gz/pdmongo-0.1.0/CHANGELOG.rst
@@ -0,0 +1,25 @@
+
+Changelog
+=========
+
+0.1.0 (2020-05-05)
+------------------
+* Added static typing
+* Added mypy to travis CI
+* Removed unecessary params
+
+0.0.2 (2020-05-04)
+------------------
+
+* Dropped support for pypy3
+
+0.0.1 (2020-04-30)
+------------------
+
+* Added read_mongo and basic support for reading MongoDB collections into pandas dataframes
+* Added to_mongo and basic support for writing pandas dataframes in MongoDB collections
+
+0.0.0 (2020-03-22)
+------------------
+
+* First release on PyPI.
---
+++ packages/pdmongo/pdmongo-0.1.0.tar.gz/pdmongo-0.1.0/setup.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+
+import io
+import re
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(*names, **kwargs):
+    with io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ) as fh:
+        return fh.read()
+
+
+setup(
+    name='pdmongo',
+    version='0.1.0',
+    license='MIT',
+    description='Transfer data between pandas dataframes and MongoDB',
+    long_description='%s\n%s' % (
+        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
+        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
+    ),
+    author='Pavlos Kallis',
+    author_email='pakallis@gmail.com',
+    url='https://github.com/pakallis/python-pandas-mongo',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        # uncomment if you test on these interpreters:
+        # 'Programming Language :: Python :: Implementation :: IronPython',
+        # 'Programming Language :: Python :: Implementation :: Jython',
+        # 'Programming Language :: Python :: Implementation :: Stackless',
+        'Topic :: Utilities'
+    ],
+    project_urls={
+        'Documentation': 'https://python-pandas-mongo.readthedocs.io/',
+        'Changelog': 'https://python-pandas-mongo.readthedocs.io/en/latest/changelog.html',
+        'Issue Tracker': 'https://github.com/pakallis/python-pandas-mongo/issues',
+    },
+    keywords=[
+        # eg: 'keyword1', 'keyword2', 'keyword3',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'pymongo>=3.4',
+        'pandas>=0.20'
+    ],
+    extras_require={
+        "test": [
+            "pytest>=4.0.2",
+            "pytest-mock>=1.10.0",
+            "hypothesis>=3.58"
+        ]
+    },
+    setup_requires=[
+        'pytest-runner',
+    ],
+)
