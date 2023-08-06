a252b6419cd677f97392935e638b6bc56b993e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:06 +0000

    Add package screenpy/2.0.0/screenpy-2.0.0-py3-none-any.whl

---
+++ packages/screenpy/screenpy-2.0.0-py3-none-any.whl/screenpy/exceptions.py
@@ -0,0 +1,31 @@
+"""
+Common exceptions for ScreenPy.
+"""
+
+
+class ScreenPyError(Exception):
+    """The base exception for all of ScreenPy."""
+
+
+class UnableToPerform(ScreenPyError):
+    """Raised when an actor lacks the ability to perform an action."""
+
+
+class TargetingError(ScreenPyError):
+    """Raised when there is an issue preventing target acquisition."""
+
+
+class AbilityError(ScreenPyError):
+    """These errors are raised when an ability fails in some way."""
+
+
+class ActionError(ScreenPyError):
+    """These errors are raised when an action fails."""
+
+
+class DeliveryError(ActionError):
+    """Raised when an action encounters an error while being performed."""
+
+
+class UnableToAct(ActionError):
+    """Raised when an action is missing direction."""
