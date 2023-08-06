ea8d9c4f3cf3b2e2ed86444a4de400e7eb3cdd59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:53 +0000

    Add package scene-text/0.2.3/scene-text-0.2.3.tar.gz

---
+++ packages/scene-text/scene-text-0.2.3.tar.gz/scene-text-0.2.3/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scene-text",
+    version="0.2.3",
+    author="Georgios Tsoukas",
+    author_email="georgios@dict.gr",
+    description="Finding text in photos",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gtsoukas/scene_text",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    keywords='scene text detection recognition',
+    python_requires='==3.6.*',
+    include_package_data=True,
+    scripts=['bin/scene-text'],
+    install_requires=[
+          'Colour',
+          'keras==2.2.4',
+          'LMDB',
+          'matplotlib==3.0.2',
+          'opencv-python',
+          'Pillow',
+          'requests',
+          'scipy',
+          'shapely',
+          'tensorflow==1.15.2',
+          'torch==0.3.1',
+          'torchvision==0.2.1',
+      ],
+)
