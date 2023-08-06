8104dc152a42d4094e75f5a110d1b68671eae2cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:00 +0000

    Add package dai175-probability/0.1/dai175_probability-0.1.tar.gz

---
+++ packages/dai175-probability/dai175_probability-0.1.tar.gz/dai175_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dai175_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dai175_probability'],
+      author = 'Daisuke Ohba',
+      author_email = 'dai175dai@gmail.com',
+      zip_safe=False)
