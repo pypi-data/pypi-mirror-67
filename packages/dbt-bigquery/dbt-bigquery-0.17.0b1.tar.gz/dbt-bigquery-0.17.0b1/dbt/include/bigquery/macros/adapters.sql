7f980114e159691a26a2d0c15416ebc4ee94bdab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:30 +0000

    Add package dbt-bigquery/0.17.0b1/dbt-bigquery-0.17.0b1.tar.gz

---
+++ packages/dbt-bigquery/dbt-bigquery-0.17.0b1.tar.gz/dbt-bigquery-0.17.0b1/dbt/include/bigquery/macros/adapters.sql
@@ -0,0 +1,151 @@
+
+{% macro partition_by(partition_config) -%}
+    {%- if partition_config is none -%}
+      {% do return('') %}
+    {%- elif partition_config.data_type | lower in ('date','timestamp','datetime') -%}
+        partition by {{ partition_config.render() }}
+    {%- elif partition_config.data_type | lower in ('int64') -%}
+        {%- set range = partition_config.range -%}
+        partition by range_bucket(
+            {{ partition_config.field }},
+            generate_array({{ range.start}}, {{ range.end }}, {{ range.interval }})
+        )
+    {%- endif -%}
+{%- endmacro -%}
+
+{% macro cluster_by(raw_cluster_by) %}
+  {%- if raw_cluster_by is not none -%}
+  cluster by {% if raw_cluster_by is string -%}
+    {% set raw_cluster_by = [raw_cluster_by] %}
+  {%- endif -%}
+  {%- for cluster in raw_cluster_by -%}
+    {{ cluster }}
+    {%- if not loop.last -%}, {% endif -%}
+  {%- endfor -%}
+
+  {% endif %}
+
+{%- endmacro -%}
+
+
+{%- macro bigquery_escape_comment(comment) -%}
+  {%- if comment is not string -%}
+    {%- do exceptions.raise_compiler_exception('cannot escape a non-string: ' ~ comment) -%}
+  {%- endif -%}
+  {%- do return((comment | tojson)[1:-1]) -%}
+{%- endmacro -%}
+
+
+{% macro bigquery_table_options(persist_docs, temporary, kms_key_name, labels) %}
+  {% set opts = {} -%}
+
+  {%- set description = get_relation_comment(persist_docs, model) -%}
+  {%- if description is not none -%}
+    {%- do opts.update({'description': "'" ~ bigquery_escape_comment(description) ~ "'"}) -%}
+  {%- endif -%}
+  {%- if temporary -%}
+    {% do opts.update({'expiration_timestamp': 'TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL 12 hour)'}) %}
+  {%- endif -%}
+  {%- if kms_key_name -%}
+    {%- do opts.update({'kms_key_name': "'" ~ kms_key_name ~ "'"}) -%}
+  {%- endif -%}
+  {%- if labels -%}
+    {%- set label_list = [] -%}
+    {%- for label, value in labels.items() -%}
+      {%- do label_list.append((label, value)) -%}
+    {%- endfor -%}
+    {%- do opts.update({'labels': label_list}) -%}
+  {%- endif -%}
+
+  {% set options -%}
+    OPTIONS({% for opt_key, opt_val in opts.items() %}
+      {{ opt_key }}={{ opt_val }}{{ "," if not loop.last }}
+    {% endfor %})
+  {%- endset %}
+  {%- do return(options) -%}
+{%- endmacro -%}
+
+{% macro bigquery__create_table_as(temporary, relation, sql) -%}
+  {%- set raw_partition_by = config.get('partition_by', none) -%}
+  {%- set raw_cluster_by = config.get('cluster_by', none) -%}
+  {%- set raw_persist_docs = config.get('persist_docs', {}) -%}
+  {%- set raw_kms_key_name = config.get('kms_key_name', none) -%}
+  {%- set raw_labels = config.get('labels', {}) -%}
+  {%- set sql_header = config.get('sql_header', none) -%}
+
+  {%- set partition_config = adapter.parse_partition_by(raw_partition_by) -%}
+
+  {{ sql_header if sql_header is not none }}
+
+  create or replace table {{ relation }}
+  {{ partition_by(partition_config) }}
+  {{ cluster_by(raw_cluster_by) }}
+  {{ bigquery_table_options(
+      persist_docs=raw_persist_docs,
+      temporary=temporary,
+      kms_key_name=raw_kms_key_name,
+      labels=raw_labels
+  ) }}
+  as (
+    {{ sql }}
+  );
+
+{%- endmacro -%}
+
+{% macro bigquery__create_view_as(relation, sql) -%}
+  {%- set raw_persist_docs = config.get('persist_docs', {}) -%}
+  {%- set raw_labels = config.get('labels', []) -%}
+  {%- set sql_header = config.get('sql_header', none) -%}
+
+  {{ sql_header if sql_header is not none }}
+
+  create or replace view {{ relation }}
+  {{ bigquery_table_options(persist_docs=raw_persist_docs, temporary=false, labels=raw_labels) }}
+  as (
+    {{ sql }}
+  );
+{% endmacro %}
+
+{% macro bigquery__create_schema(database_name, schema_name) -%}
+  {{ adapter.create_schema(database_name, schema_name) }}
+{% endmacro %}
+
+{% macro bigquery__drop_schema(database_name, schema_name) -%}
+  {{ adapter.drop_schema(database_name, schema_name) }}
+{% endmacro %}
+
+{% macro bigquery__drop_relation(relation) -%}
+  {% call statement('drop_relation') -%}
+    drop {{ relation.type }} if exists {{ relation }}
+  {%- endcall %}
+{% endmacro %}
+
+{% macro bigquery__get_columns_in_relation(relation) -%}
+  {{ return(adapter.get_columns_in_relation(relation)) }}
+{% endmacro %}
+
+
+{% macro bigquery__list_relations_without_caching(information_schema, schema) -%}
+  {{ return(adapter.list_relations_without_caching(information_schema, schema)) }}
+{%- endmacro %}
+
+
+{% macro bigquery__current_timestamp() -%}
+  CURRENT_TIMESTAMP()
+{%- endmacro %}
+
+
+{% macro bigquery__snapshot_string_as_time(timestamp) -%}
+    {%- set result = 'TIMESTAMP("' ~ timestamp ~ '")' -%}
+    {{ return(result) }}
+{%- endmacro %}
+
+
+{% macro bigquery__list_schemas(database) -%}
+  {{ return(adapter.list_schemas()) }}
+{% endmacro %}
+
+
+{% macro bigquery__check_schema_exists(information_schema, schema) %}
+  {{ return(adapter.check_schema_exists(information_schema.database, schema)) }}
+{% endmacro %}
