a9ebdef9842ff5a80f754eabd21dc94e02657ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:02 +0000

    Add package libcfinder/1.0.2/libcfinder-1.0.2.tar.gz

---
+++ packages/libcfinder/libcfinder-1.0.2.tar.gz/libcfinder-1.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='libcfinder',
+    version='1.0.2',
+    scripts=[],
+    author='Roberto Pettinau',
+    author_email='roberto.pettinau99@gmail.com',
+    descrption='Package for finding libc version via offsets',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/petitnau/libcfinder',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Security"
+    ]
+)
