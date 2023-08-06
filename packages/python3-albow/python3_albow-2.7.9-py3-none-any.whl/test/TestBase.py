a4fdc6809ecb158553a45f6e39b453f15522a67b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:56 +0000

    Add package python3-albow/2.7.9/python3_albow-2.7.9-py3-none-any.whl

---
+++ packages/python3-albow/python3_albow-2.7.9-py3-none-any.whl/test/TestBase.py
@@ -0,0 +1,40 @@
+
+import os
+import json
+import logging
+import logging.config
+
+from unittest import TestCase
+
+JSON_LOGGING_CONFIG_FILENAME = "testLoggingConfig.json"
+
+
+class TestBase(TestCase):
+    """
+    A base unit test class to initialize some logging stuff we need
+    """
+
+    @classmethod
+    def setUpLogging(cls):
+        """"""
+        loggingConfigFilename: str = cls.findLoggingConfig()
+
+        with open(loggingConfigFilename, 'r') as loggingConfigurationFile:
+            configurationDictionary = json.load(loggingConfigurationFile)
+
+        logging.config.dictConfig(configurationDictionary)
+        logging.logProcesses = False
+        logging.logThreads = False
+
+    @classmethod
+    def findLoggingConfig(cls) -> str:
+        """"""
+        upDir = f'test/{JSON_LOGGING_CONFIG_FILENAME}'
+        if os.path.isfile(upDir):
+            return upDir
+
+        if os.path.isfile(JSON_LOGGING_CONFIG_FILENAME):
+            return JSON_LOGGING_CONFIG_FILENAME
+        else:
+            os.chdir("../")
+            return cls.findLoggingConfig()
