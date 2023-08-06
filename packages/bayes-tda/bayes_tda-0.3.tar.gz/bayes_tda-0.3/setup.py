af8d776d421054ce8ac421322268431b457441ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:12 +0000

    Add package bayes-tda/0.3/bayes_tda-0.3.tar.gz

---
+++ packages/bayes-tda/bayes_tda-0.3.tar.gz/bayes_tda-0.3/setup.py
@@ -0,0 +1,37 @@
+# setup.py 
+
+from setuptools import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name = 'bayes_tda',
+  packages = ['bayes_tda'],   
+  version = '0.3',      
+  license='MIT',        
+  description = 'Point process model for Bayesian inference with persistence diagrams.',
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
+  author = 'Christopher Oballe',
+  author_email = 'coballejr@gmail.com',      
+  url = 'https://github.com/coballejr/bayes_tda/',
+  download_url = 'https://github.com/coballejr/bayes_tda/archive/v_03.tar.gz',
+  keywords = ['Persistent Homology', 'Topological Data Analysis', 'Bayesian','Point process', 'Poisson process'],   # Keywords that define your package best
+  install_requires=[            
+          'numpy',
+          'scipy'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Science/Research',      
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which python versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
+
