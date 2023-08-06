19e996538fb6de18f7fbd3a475118a0f01f1dc04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:16 +0000

    Add package ancalagon/0.0.2a1/Ancalagon-0.0.2a1.tar.gz

---
+++ packages/ancalagon/Ancalagon-0.0.2a1.tar.gz/Ancalagon-0.0.2a1/ancalagon/resolver.py
@@ -0,0 +1,36 @@
+from functools import partial
+
+from collections import defaultdict
+from .responses import Response
+from .request import Request
+
+
+class Resolver:
+    def __init__(self):
+        self.routes = defaultdict(list)
+
+    async def __call__(self, scope, receive, send):
+        scope['request'] = request = Request(scope, receive, send)
+
+        app = scope['app']
+        path = scope['path']
+        method = scope['method']
+
+        for route in self.routes.get(method, []) + self.routes.get('*', []):
+            if route.path == path:
+                handler = route.handler
+                for middleware in app.middlewares[::-1] + route.middlewares:
+                    if middleware not in route.ignore_middlewares:
+                        handler = partial(middleware, handler)
+
+                response = await handler(request)
+
+                break
+        else:
+            response = Response('<h1>404 Not found</h1>', 404)
+
+        await response(scope, receive, send)
+
+    def add_route(self, route):
+        for method in route.methods:
+            self.routes[method].append(route)
