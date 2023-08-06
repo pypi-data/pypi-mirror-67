b70a48422d54f835ee1d1a7065b1715d56099ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:49 +0000

    Add package teams-import/1.0.8.dev11/teams-import-1.0.8.dev11.tar.gz

---
+++ packages/teams-import/teams-import-1.0.8.dev11.tar.gz/teams-import-1.0.8.dev11/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    description="A command line tool for bulk importing students to MicrosoftTeams",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Jake Renzella",
+    author_email="jake.renzella@deakin.edu.au",
+    license="MIT",
+    name='teams-import',
+    version='1.0.8dev11',
+    py_modules=['main'],
+    packages=['teams_import'],
+    install_requires=[
+        'click',
+    ],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "teams-import=teams_import.__main__:cli",
+        ]
+    }
+)
