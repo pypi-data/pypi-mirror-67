5ddcb8bd622fcbf52c2d9fea4830c1e20be7effe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:06 +0000

    Add package jprq/1.0.1/jprq-1.0.1.tar.gz

---
+++ packages/jprq/jprq-1.0.1.tar.gz/jprq-1.0.1/jprq/http.py
@@ -0,0 +1,42 @@
+import base64
+import json
+import sys
+from urllib.parse import urljoin
+
+import aiohttp
+
+
+class Client:
+    def __init__(self, base_uri, token):
+        self.base_uri = base_uri
+        self.token = token
+
+    async def process(self, message, websocket):
+        async with aiohttp.ClientSession() as session:
+            try:
+                response = await session.request(
+                    method=message['method'],
+                    url=urljoin(self.base_uri, message['url']),
+                    headers=message['header'],
+                    data=base64.b64decode(message['body']),
+                )
+            except:
+                sys.stderr.write(f"Error Processing Request At: {message['url']}")
+                return {
+                    'request_id': message['id'],
+                    'token': self.token,
+                    'status': 500,
+                    'header': {},
+                    'body': base64.b64encode(b'request failed').decode('utf-8'),
+                }
+
+            sys.stdout.write(message["method"], message["url"], response.status)
+            body = await response.read()
+            response_message = {
+                'request_id': message['id'],
+                'token': self.token,
+                'status': response.status,
+                'header': dict(response.headers),
+                'body': base64.b64encode(body).decode('utf-8'),
+            }
+            await websocket.send(json.dumps(response_message))
