7f3b632b53645b020c062e60e7769f5ffbc8751e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:54 +0000

    Add package pytraj/2.0.5/pytraj-2.0.5.tar.gz

---
+++ packages/pytraj/pytraj-2.0.5.tar.gz/pytraj-2.0.5/pytraj/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM PYTRAJ SETUP.PY
+short_version = '2.0.5'
+version = '2.0.5'
+full_version = '2.0.5'
+git_revision = 'a1fd384fbcfe03193cd8edaef361fa4d2d422088'
+release = True
+
+if not release:
+    version = full_version
