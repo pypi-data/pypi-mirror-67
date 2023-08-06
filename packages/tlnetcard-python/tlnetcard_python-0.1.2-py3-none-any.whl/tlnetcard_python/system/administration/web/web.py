dd7370782d23fd4e0454ff06d2e311d125e84382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:08 +0000

    Add package tlnetcard-python/0.1.2/tlnetcard_python-0.1.2-py3-none-any.whl

---
+++ packages/tlnetcard-python/tlnetcard_python-0.1.2-py3-none-any.whl/tlnetcard_python/system/administration/web/web.py
@@ -0,0 +1,9 @@
+# web.py
+# Ethan Guthrie
+# DATE TBD
+""" Allows web host settings to be configured. """
+
+class Web:
+    """ Class for the Web object. """
+    def __init__(self):
+        """ Initializes the Web object. """
