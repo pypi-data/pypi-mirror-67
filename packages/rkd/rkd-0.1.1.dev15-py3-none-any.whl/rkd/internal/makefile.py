c6c17308007c447b245ed9782eddba7287bd745b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:08 +0000

    Add package rkd/0.1.1.dev15/rkd-0.1.1.dev15-py3-none-any.whl

---
+++ packages/rkd/rkd-0.1.1.dev15-py3-none-any.whl/rkd/internal/makefile.py
@@ -0,0 +1,25 @@
+#
+# Base RKD Makefile, contains basic commands such as :tasks, :clean or :version
+#
+
+from rkd.syntax import TaskDeclaration, TaskAliasDeclaration
+from rkd.standardlib.python import PublishTask, BuildTask
+from rkd.standardlib.shell import ShellCommand, ExecProcessCommand
+from rkd.standardlib import InitTask, TasksListingTask, CallableTask, VersionTask
+
+
+IMPORTS = [
+    TaskDeclaration(ShellCommand()),
+    TaskDeclaration(ExecProcessCommand()),
+    TaskDeclaration(InitTask()),
+    TaskDeclaration(TasksListingTask()),
+    TaskDeclaration(VersionTask()),
+
+    TaskDeclaration(PublishTask()),
+    TaskDeclaration(BuildTask())
+]
+
+TASKS = [
+    # example:
+    # TaskAliasDeclaration(':env:test', [':py:publish', '--username=...', '--password=...'], env={'DB_PASSWORD': '123'})
+]
