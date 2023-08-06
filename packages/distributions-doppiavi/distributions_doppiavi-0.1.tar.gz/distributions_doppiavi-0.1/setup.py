ad1659659ed77495320e4548220217ef03449e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:09 +0000

    Add package distributions-doppiavi/0.1/distributions_doppiavi-0.1.tar.gz

---
+++ packages/distributions-doppiavi/distributions_doppiavi-0.1.tar.gz/distributions_doppiavi-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_doppiavi',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_doppiavi'],
+      zip_safe=False)
