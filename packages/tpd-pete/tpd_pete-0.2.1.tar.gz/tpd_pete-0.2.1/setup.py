b8ded51c797e25877a86bcb1cf3319dbce5ba7f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:16 +0000

    Add package tpd-pete/0.2.1/tpd_pete-0.2.1.tar.gz

---
+++ packages/tpd-pete/tpd_pete-0.2.1.tar.gz/tpd_pete-0.2.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+setup(
+	name="tpd_pete",
+	version="0.2.1",
+	description="TPD Pete is a AWS Deployment tool for AWS Cloudformation",
+	author="TotalPunch Development",
+	author_email="info@totalpunch.nl",
+	url="https://github.com/totalpunch/TPD-Pete",
+	download_url="https://github.com/totalpunch/TPD-Pete/archive/0.2.1.tar.gz",
+	license="MIT",
+	packages=find_packages(),
+	entry_points={
+		"console_scripts": [
+			"pete = tpd_pete.__main__:main"
+		]
+	},
+	install_requires=[
+		"awscli==1.18.31",
+		"pyinquirer==1.0.3",
+		"halo==0.0.29",
+		"termcolor==1.1.0",
+		"cfn_flip==1.2.2"
+	],
+	zip_safe=False,
+	python_requires=">=3.7"
+)
