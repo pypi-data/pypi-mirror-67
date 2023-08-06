8b914af42ac5b26e44c18b0fc4c4f9317d126f12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:18 +0000

    Add package torch-blocksparse/1.0.2/torch-blocksparse-1.0.2.tar.gz

---
+++ packages/torch-blocksparse/torch-blocksparse-1.0.2.tar.gz/torch-blocksparse-1.0.2/torch_blocksparse/__init__.py
@@ -0,0 +1,5 @@
+from .softmax import Softmax
+from .matmul import MatMul, Linear
+from .conv import _sparse_conv2d, Conv2d
+from .attention import MultiheadAttention
+from .batchnorm import BatchNorm2d
