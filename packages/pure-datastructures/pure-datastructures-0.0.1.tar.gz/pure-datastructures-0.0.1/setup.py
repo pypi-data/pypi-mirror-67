e66cc11b5db4d53b10c6bf03cb067bbf81ec1985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:28 +0000

    Add package pure-datastructures/0.0.1/pure-datastructures-0.0.1.tar.gz

---
+++ packages/pure-datastructures/pure-datastructures-0.0.1.tar.gz/pure-datastructures-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pure-datastructures", # Replace with your own username
+    version="0.0.1",
+    author="Ryan Febriansyah",
+    author_email="15523163@students.uii.ac.id",
+    description="Data Structures with pure python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    #url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Operating System :: POSIX :: Linux',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: Indonesian',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    python_requires='>=3.6',
+)
