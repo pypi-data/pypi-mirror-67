adeb67e8dbeecb47875d2985ef5dc3227345d09b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:13 +0000

    Add package mypackagedorachua/0.0.4/mypackagedorachua-0.0.4.tar.gz

---
+++ packages/mypackagedorachua/mypackagedorachua-0.0.4.tar.gz/mypackagedorachua-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mypackagedorachua", # Replace dorachua with your own username (no space)
+    version="0.0.4",
+    author="Dora Chua",
+    author_email="msdorachua@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
