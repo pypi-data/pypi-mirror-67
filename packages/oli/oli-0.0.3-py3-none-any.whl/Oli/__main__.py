7be5bb2012b0b6d9ec9f342a0b7de8aeae06091b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:40 +0000

    Add package oli/0.0.3/oli-0.0.3-py3-none-any.whl

---
+++ packages/oli/oli-0.0.3-py3-none-any.whl/Oli/__main__.py
@@ -0,0 +1,22 @@
+import platform
+from macos import macos
+import signal
+import sys
+
+def signal_handler(signal, frame):
+    print("\nGracefully exiting")
+    sys.exit(0)
+
+def main():
+    platform_name = platform.system()
+    if platform_name == "Darwin":
+        macos.run()
+        
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
+
+if __name__ == "__main__":
+    
+    main()
