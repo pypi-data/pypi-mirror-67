9b7f55ecebc23d487dc4b75a930431285121983a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:31 +0000

    Add package nester0705/1.0.0/nester0705-1.0.0.tar.gz

---
+++ packages/nester0705/nester0705-1.0.0.tar.gz/nester0705-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester0705',
+    version = '1.0.0',
+    py_modules = ['nester0705'],
+    author= 'mat',
+    author_email= 'mathrewji@qq.com',
+    description='A simple printer of nested lists',
+)
