59c112ba97552f1bee19bc9a8b0e9234d6fc69d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:48 +0000

    Add package mlnd-probdist/0.0.1/mlnd_probdist-0.0.1.tar.gz

---
+++ packages/mlnd-probdist/mlnd_probdist-0.0.1.tar.gz/mlnd_probdist-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mlnd_probdist',
+      version='0.0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mlnd_probdist'],
+      author='Chandrakanth Nethi',
+      author_email='chandrakanth.nethi@gmail.com',
+      zip_safe=False)
