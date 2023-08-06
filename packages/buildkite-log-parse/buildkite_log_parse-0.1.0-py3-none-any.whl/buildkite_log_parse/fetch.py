4ec74e69dc0079a42af8b8094fb722aecd98c80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:57 +0000

    Add package buildkite-log-parse/0.1.0/buildkite_log_parse-0.1.0-py3-none-any.whl

---
+++ packages/buildkite-log-parse/buildkite_log_parse-0.1.0-py3-none-any.whl/buildkite_log_parse/fetch.py
@@ -0,0 +1,31 @@
+""" Util to handle network requests """
+import requests
+
+from buildkite_log_parse import util
+
+API_ROOT = "https://api.buildkite.com/v2"
+
+
+def builds(build_state, organization, pipeline, token):
+    """ Fetch builds from buildkite api """
+    builds_url = f"{API_ROOT}/organizations/{organization}/pipelines/{pipeline}/builds"
+    params = {"state": build_state}
+    builds_response = requests.get(
+        builds_url, params=params, headers=build_headers(token)
+    )
+    util.write_log(builds_response.text, "json", "builds")
+    return builds_response.json()
+
+
+def build_job_log(build, job, organization, pipeline, token):
+    """ Fetch log for the build's job from buildkite api """
+    builds_url = f"{API_ROOT}/organizations/{organization}/pipelines/{pipeline}/builds"
+    log_url = f"{builds_url}/{build['number']}/jobs/{job['id']}/log.txt"
+    log_response = requests.get(log_url, headers=build_headers(token)).text
+    util.write_log(log_response, "txt", "raw_log")
+    return log_response
+
+
+def build_headers(token):
+    """ Build auth headers for network requests """
+    return {"Authorization": f"Bearer {token}"}
