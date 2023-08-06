a3e5e3996d4aad29d515c261a20757a189be68d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:10 +0000

    Add package raveberry/0.5.0/raveberry-0.5.0.tar.gz

---
+++ packages/raveberry/raveberry-0.5.0.tar.gz/raveberry-0.5.0/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("raveberry/README.md", "r") as f:
+    long_description = f.read()
+
+with open("raveberry/requirements/common.txt") as f:
+    required_packages = f.read().splitlines()
+
+with open("raveberry/requirements/screenvis.txt") as f:
+    screenvis_packages = [
+        line for line in f.read().splitlines() if not line.startswith("-r")
+    ]
+
+setuptools.setup(
+    name="raveberry",
+    version="0.5.0",
+    author="Jonathan Hacker",
+    author_email="raveberry@jhacker.de",
+    description="A multi-user music server with a focus on participation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/raveberry/raveberry",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Django :: 2.2",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["raveberry"],
+    include_package_data=True,
+    python_requires=">=3.7",
+    install_requires=required_packages,
+    extras_require={"screenvis": screenvis_packages},
+    scripts=["raveberry/bin/raveberry"],
+)
