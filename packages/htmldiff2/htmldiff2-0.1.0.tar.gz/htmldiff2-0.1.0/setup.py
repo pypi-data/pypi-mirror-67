d512eca9c906609749d75a1d6e3bab102968f267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:16 +0000

    Add package htmldiff2/0.1.0/htmldiff2-0.1.0.tar.gz

---
+++ packages/htmldiff2/htmldiff2-0.1.0.tar.gz/htmldiff2-0.1.0/setup.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='htmldiff2',
+    author='Ed Summers',
+    author_email='ehs@pobox.com',
+    version='0.1.0',
+    url='http://github.com/edsu/htmldiff2',
+    py_modules=['htmldiff2'],
+    description='Diffs arbitrary HTML inline.',
+    zip_safe=False,
+    install_requires=['genshi', "html5lib"],
+    test_suite='test',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python'
+    ]
+)
