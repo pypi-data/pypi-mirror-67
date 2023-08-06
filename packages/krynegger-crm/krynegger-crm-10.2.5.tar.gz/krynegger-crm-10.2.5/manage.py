35acba287bedc6ed6137e6e1debbfe103bdf179c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:01 +0000

    Add package krynegger-crm/10.2.5/krynegger-crm-10.2.5.tar.gz

---
+++ packages/krynegger-crm/krynegger-crm-10.2.5.tar.gz/krynegger-crm-10.2.5/manage.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import logging
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crm.settings")
+    # os.environ['DJANGO_SETTINGS_MODULE'] = "crm.settings"
+    os.environ.setdefault("DJANGO_CONFIGURATION", 'Development')
+
+    try:
+        from dotenv import load_dotenv, find_dotenv
+        from pathlib import Path
+
+        dotenv_filename = '.crm'
+        dotenv_path = find_dotenv(
+            filename=dotenv_filename,
+            raise_error_if_not_found=True,
+            usecwd=False
+        )
+        print('Load dotenv configuration from %s' % dotenv_path)
+        load_dotenv(dotenv_path=dotenv_path, override=True)
+
+        os.environ.setdefault("DJANGO_CONFIGURATION", os.getenv('KRYNEGGER_CONFIGURATION').capitalize())
+    except BaseException as e:
+        logging.error('Dotenv file could not loaded')
+        # logging.exception(e)
+        # traceback.print_exc()
+
+    try:
+        from configurations.management import execute_from_command_line
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
+        try:
+            pass
+
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
+    execute_from_command_line(sys.argv)
---
+++ packages/krynegger-crm/krynegger-crm-10.2.5.tar.gz/krynegger-crm-10.2.5/setup.py
@@ -0,0 +1,84 @@
+import os
+from setuptools import setup, find_packages
+from pip._internal.req import parse_requirements
+from distutils.util import convert_path
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+# ========================================
+# Parse requirements for all configuration
+# ========================================
+install_reqs = parse_requirements(filename=os.path.join('.', 'requirements.txt'), session='update')
+reqs = [str(ir.req) for ir in install_reqs]
+
+# ========================================
+# Readme
+# ========================================
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+PROJECT_NAME = 'crm'
+
+# ========================================
+# Version parsing
+# ========================================
+main_ns = {}
+ver_path = convert_path(PROJECT_NAME + '/version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
+
+data_files = []
+for dirpath, dirnames, filenames in os.walk(PROJECT_NAME):
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'):
+            del dirnames[i]
+    if '__init__.py' in filenames:
+        continue
+    elif filenames:
+        for f in filenames:
+            data_files.append(os.path.join(
+                dirpath[len(PROJECT_NAME) + 1:], f))
+
+
+
+setup(
+    name='krynegger-crm',
+    version=main_ns['__version__'],
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    description='An opensourse CRM developed on django framework',
+    long_description=README,
+    license='Apache 2.0 License',
+    url='https://gitlab.com/Krynegger/Krynegger-CRM.git',
+    author='lordoftheflies',
+    author_email='laszlo.hegedus@cherubits.hu',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Database'
+    ],
+    install_requires=reqs,
+    scripts=['manage.py'],
+    # entry_points={
+    #     'console_scripts': [
+    #         'crm = crm:manage',
+    #     ]
+    # }
+)
