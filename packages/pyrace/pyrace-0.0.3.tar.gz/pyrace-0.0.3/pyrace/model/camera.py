e9e7d8711d60c35cda1d62e1f88aa97c78f02fd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:10 +0000

    Add package pyrace/0.0.3/pyrace-0.0.3.tar.gz

---
+++ packages/pyrace/pyrace-0.0.3.tar.gz/pyrace-0.0.3/pyrace/model/camera.py
@@ -0,0 +1,17 @@
+import numpy
+
+class Camera:
+        def __init__(self, observer_metadata):
+
+            self.metadata = observer_metadata
+            self.nb_cams = observer_metadata['dimension'][0]
+            self.cams_range = observer_metadata['values_range'][1]
+            self.thetas = [int(180/self.nb_cams*i) for i in range(-(self.nb_cams//2), 1+self.nb_cams//2)]
+
+
+        def get_value(self, vue, position, orientation):
+            x, y = position
+            cursor_orientations = [orientation+theta for theta in self.thetas]
+            self.cursor_positions = [(x-int(self.cams_range*numpy.sin(numpy.pi*(angle)/180)), y-int(self.cams_range*numpy.cos(numpy.pi*(angle)/180))) for angle in cursor_orientations]
+            value = [vue.map_img_rgb.getpixel(cursor_position)[2] for cursor_position in self.cursor_positions]
+            return value
