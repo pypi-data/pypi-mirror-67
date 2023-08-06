17bf2c095c7a948241a3cc42381d8ba98187ca34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:49 +0000

    Add package genrl/0.0.1/genrl-0.0.1.tar.gz

---
+++ packages/genrl/genrl-0.0.1.tar.gz/genrl-0.0.1/setup.py
@@ -0,0 +1,88 @@
+import os
+
+import codecs
+from setuptools import setup, find_packages
+
+# Basic information
+NAME = "genrl"
+DESCRIPTION = "A PyTorch reinforcement learning library for generalizable and reproducible implementations."
+VERSION = "0.0.1"
+AUTHOR = "Society for Artificial Intelligence and Deep Learning"
+EMAIL = "ajaysubramanian110@gmail.com"
+LICENSE = "MIT"
+REPOSITORY = "https://github.com/SforAiDl/genrl"
+PACKAGE = "genrl"
+with open("README.md", "r") as f:
+    LONG_DESCRIPTION = f.read()
+
+# Define the keywords
+KEYWORDS = ("reinforcement learning", "pytorch", "machine learning", "deep learning")
+
+# Define the classifiers
+# See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+CLASSIFIERS = (
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Scientific/Engineering :: Artificial Intelligence",
+)
+
+# Important Paths
+PROJECT = os.path.abspath(os.path.dirname(__file__))
+REQUIRE_PATH = "requirements.txt"
+VERSION_PATH = os.path.join(PACKAGE, "version.py")
+PKG_DESCRIBE = "README.md"
+
+# Directories to ignore in find_packages
+EXCLUDES = ()
+
+
+# helper functions
+def read(*parts):
+    """
+    returns contents of file
+    """
+    with codecs.open(os.path.join(PROJECT, *parts), "rb", "utf-8") as file:
+        return file.read()
+
+
+def get_requires(path=REQUIRE_PATH):
+    """
+    generates requirements from file path given as REQUIRE_PATH
+    """
+    for line in read(path).splitlines():
+        line = line.strip()
+        if line and not line.startswith("#"):
+            yield line
+
+
+# Define the configuration
+CONFIG = {
+    "name": NAME,
+    "version": VERSION,
+    "description": DESCRIPTION,
+    "long_description": LONG_DESCRIPTION,
+    "long_description_content_type": "text/markdown",
+    "classifiers": CLASSIFIERS,
+    "keywords": KEYWORDS,
+    "license": LICENSE,
+    "author": AUTHOR,
+    "author_email": EMAIL,
+    "url": REPOSITORY,
+    "project_urls": {"Source": REPOSITORY},
+    "packages": find_packages(where=PROJECT, exclude=EXCLUDES),
+    "install_requires": list(get_requires()),
+    "python_requires": ">=3.6",
+}
+
+if __name__ == "__main__":
+    setup(**CONFIG)
