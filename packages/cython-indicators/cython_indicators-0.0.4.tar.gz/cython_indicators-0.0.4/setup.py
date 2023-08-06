b88aea91293800803d5ef5259240a1e6a0e7f9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:49 +0000

    Add package cython-indicators/0.0.4/cython_indicators-0.0.4.tar.gz

---
+++ packages/cython-indicators/cython_indicators-0.0.4.tar.gz/cython_indicators-0.0.4/setup.py
@@ -0,0 +1,38 @@
+import os 
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = 'requirements.txt'
+install_requires = [] 
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+cmdclass = {}
+
+from Cython.Distutils import build_ext
+import setuptools
+from setuptools import Extension
+ext_modules = [Extension("indicators", ["src/indicators.pyx"])]
+cmdclass.update({'build_ext': build_ext})
+
+setuptools.setup(
+    name="cython_indicators", 
+    version="0.0.4",
+    author="Ethen Pociask",
+    author_email="epociask@volatrade.com",
+    description="Indicator functions using cython",
+    long_description="TO BE ADDED",
+    long_description_content_type="text/markdown",
+    url="https://github.com/epociask/cython_indicator_functions.git",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    cmdclass = cmdclass,
+    ext_modules = ext_modules,
+
+)
+
