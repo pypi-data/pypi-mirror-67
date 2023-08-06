d5b04582107c3acd7c49c2b8071844a85627b044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:38 +0000

    Add package ewah/0.1.7/ewah-0.1.7.tar.gz

---
+++ packages/ewah/ewah-0.1.7.tar.gz/ewah-0.1.7/ewah/dag_factories/dag_factory_full_refresh.py
@@ -0,0 +1,70 @@
+from airflow import DAG
+
+from ewah.ewah_utils.airflow_utils import etl_schema_tasks
+
+from datetime import datetime, timedelta
+from copy import deepcopy
+
+def dag_factory_drop_and_replace(
+        dag_name,
+        dwh_engine,
+        dwh_conn_id,
+        el_operator,
+        operator_config,
+        target_schema_name,
+        target_schema_suffix='_next',
+        target_database_name=None,
+        default_args=None,
+        start_date=datetime(2019, 1, 1),
+        schedule_interval=timedelta(days=1),
+        end_date=None,
+        additional_dag_args={},
+        additional_task_args={},
+    ):
+
+    if not hasattr(el_operator, '_IS_FULL_REFRESH'):
+        raise Exception('Invalid operator supplied!')
+    if not el_operator._IS_FULL_REFRESH:
+        raise Exception('Operator does not support full refreshs!')
+
+    dag = DAG(
+        dag_name,
+        catchup=False,
+        default_args=default_args,
+        max_active_runs=1,
+        schedule_interval=schedule_interval,
+        start_date=start_date,
+        end_date=end_date,
+        **additional_dag_args,
+    )
+
+    kickoff, final = etl_schema_tasks(
+        dag=dag,
+        dwh_engine=dwh_engine,
+        dwh_conn_id=dwh_conn_id,
+        target_schema_name=target_schema_name,
+        target_schema_suffix=target_schema_suffix,
+        target_database_name=target_database_name,
+        copy_schema=False,
+        **additional_task_args
+    )
+
+    with dag:
+        for table in operator_config['tables'].keys():
+            table_config = deepcopy(additional_task_args)
+            table_config.update(operator_config.get('general_config', {}))
+            table_config.update(operator_config['tables'][table] or {})
+            table_config.update({
+                'task_id': 'extract_load_'+table,
+                'dwh_engine': dwh_engine,
+                'dwh_conn_id': dwh_conn_id,
+                'target_table_name': table,
+                'target_schema_name': target_schema_name,
+                'target_schema_suffix': target_schema_suffix,
+                'target_database_name': target_database_name,
+                'drop_and_replace': True,
+            })
+            table_task = el_operator(**table_config)
+            kickoff >> table_task >> final
+
+    return dag
---
+++ packages/ewah/ewah-0.1.7.tar.gz/ewah-0.1.7/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ewah", # Replace with your own username
+    version="0.1.7",
+    author="Bijan Soltani",
+    author_email="bijan.soltani+ewah@gemmaanalytics.com",
+    description="An ELT with airflow helper module: Ewah",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gemmaanalytics.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)
