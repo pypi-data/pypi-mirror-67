486e317804880bed93ee2c1460da4f7aa04cd253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:15 +0000

    Add package xmpplib/1.0.0/xmpplib-1.0.0.tar.gz

---
+++ packages/xmpplib/xmpplib-1.0.0.tar.gz/xmpplib-1.0.0/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="xmpplib",
+    version="1.0.0",
+    author="Hakan Vergil",
+    author_email='hakanvergil@gmail.com',
+    url="https://pypi.org/project/xmpplib/",
+    download_url=None,
+    description="XMPP protocol client (requires sockets).",
+    long_description="xmpplib is a lightweight and useful XMPP/Jabber client.",
+    license="MIT",
+    install_requires=None,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3"
+    ],
+)
