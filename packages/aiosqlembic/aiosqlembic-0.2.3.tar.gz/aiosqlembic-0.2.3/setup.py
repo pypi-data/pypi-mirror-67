a7c7c2c4eba7e83d9c88bd158085252fc7c61ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:07 +0000

    Add package aiosqlembic/0.2.3/aiosqlembic-0.2.3.tar.gz

---
+++ packages/aiosqlembic/aiosqlembic-0.2.3.tar.gz/aiosqlembic-0.2.3/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aiosqlembic', 'aiosqlembic.models']
+
+package_data = \
+{'': ['*'],
+ 'aiosqlembic': ['sql/aiosqlembic/aiosqlite/*',
+                 'sql/aiosqlembic/asyncpg/*',
+                 'sql/metadata/*',
+                 'templates/*']}
+
+install_requires = \
+['aiosql>=3.0.0,<4.0.0',
+ 'aiosqlite>=0.11.0,<0.12.0',
+ 'anyio>=1.2.3,<2.0.0',
+ 'asyncclick>=7.0.9,<8.0.0',
+ 'asyncpg>=0.20.1,<0.21.0',
+ 'colorama>=0.4.3,<0.5.0',
+ 'jinja2>=2.11.0,<3.0.0',
+ 'pydantic>=1.3,<2.0',
+ 'trio>=0.13.0,<0.14.0']
+
+entry_points = \
+{'console_scripts': ['aiosqlembic = aiosqlembic.main:main']}
+
+setup_kwargs = {
+    'name': 'aiosqlembic',
+    'version': '0.2.3',
+    'description': 'Migrations powered by aiosql',
+    'long_description': '<img src="/docs/source/Cigogne_Belon_1555.jpg"  height="200">\n\n**Aiosqlembic, migrations welcome !**\n\n\nReadme\n======\n\n![pipeline](https://gitlab.com/euri10/aiosqlembic/badges/master/pipeline.svg)\n![coverage](https://gitlab.com/euri10/aiosqlembic/badges/master/coverage.svg)\n![documentation](https://readthedocs.org/projects/aiosqlembic/badge/?version=latest)\n\n\nAiosqlembic aims at running database migrations powered by the awesome [aiosql](https://github.com/nackjicholson/aiosql)\n\nIt\'s inspired by goose for those coming from go\n\nIt is in development and likely to break\n\nDocumentation\n-------------\n\nIt\'s [here](https://aiosqlembic.readthedocs.io/en/latest/index.html)\n\nUsage\n-----\n\nRun `aiosqlembic --help`\n',
+    'author': 'euri10',
+    'author_email': 'benoit.barthelet@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://gitlab.com/euri10/aiosqlembic',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
