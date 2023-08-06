fbf7925f587c5deedf1dad0c97b0582a972b9250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:22 +0000

    Add package cli2/2.2.1.dev2/cli2-2.2.1.dev2.tar.gz

---
+++ packages/cli2/cli2-2.2.1.dev2.tar.gz/cli2-2.2.1.dev2/cli2/command.py
@@ -0,0 +1,142 @@
+import asyncio
+import inspect
+
+from .argument import Argument
+from .colors import colors
+from .entry_point import EntryPoint
+
+
+class Command(EntryPoint, dict):
+    def __new__(cls, target, *args, **kwargs):
+        overrides = getattr(target, 'cli2', {})
+        cls = overrides.get('cls', cls)
+        return super().__new__(cls, *args, **kwargs)
+
+    def __init__(self, target, name=None, color=None, doc=None, posix=False):
+        self.target = target
+        self.posix = posix
+
+        overrides = getattr(target, 'cli2', {})
+        for key, value in overrides.items():
+            setattr(self, key, value)
+
+        if name:
+            self.name = name
+        elif 'name' not in overrides:
+            self.name = getattr(target, '__name__', type(target).__name__)
+
+        if doc:
+            self.doc = doc
+        elif 'doc' not in overrides:
+            self.doc = inspect.getdoc(target)
+
+        if color:
+            self.color = color
+        elif 'color' not in overrides:
+            self.color = colors.orange
+
+        if self.color in colors.__dict__:
+            self.color = getattr(colors, self.color)
+
+        self.sig = inspect.signature(target)
+        self.setargs()
+
+    def setargs(self):
+        for name, param in self.sig.parameters.items():
+            overrides = getattr(self.target, 'cli2_' + name, {})
+            cls = overrides.get('cls', Argument)
+            self[name] = cls(self, param)
+            for key, value in overrides.items():
+                setattr(self[name], key, value)
+
+    @classmethod
+    def cmd(cls, *args, **kwargs):
+        def override(target):
+            overrides = getattr(target, 'cli2', {})
+            overrides.update(kwargs)
+            overrides['cls'] = cls
+            target.cli2 = overrides
+
+        if len(args) == 1 and not kwargs:
+            # simple @YourCommand.cmd syntax
+            target = args[0]
+            override(target)
+            return target
+        elif not args:
+            def wrap(cb):
+                override(cb)
+                return cb
+            return wrap
+        else:
+            raise Exception('Only kwargs are supported by Group.cmd')
+
+    def help(self, error=None, short=False):
+        output = []
+
+        if error:
+            output.append(error + '\n')
+
+        if self.doc:
+            if short:
+                # get the first sentence
+                sentence = ''
+                for char in self.doc.replace('\n', ' '):
+                    if char == '.':
+                        break
+                    sentence += char
+                output.append(sentence)
+            else:
+                output.append(self.doc + '\n')
+
+        return '\n'.join(output)
+
+    def parse(self, *argv):
+        self.setargs()
+        self.bound = self.sig.bind_partial()
+        extra = []
+        skip = False
+        for position, current in enumerate(argv):
+            if skip:
+                skip = False
+                continue
+
+            try:
+                next_argv = argv[position + 1]
+            except IndexError:
+                next_argv = None
+
+            taken = False
+            for arg in self.values():
+                taken = arg.take(current, next_argv)
+                skip = taken == 'next'
+                if taken:
+                    break
+
+            if not taken:
+                extra.append(current)
+
+        if extra:
+            return 'No parameters for these arguments: ' + ', '.join(extra)
+
+    def call(self):
+        return self.target(*self.bound.args, **self.bound.kwargs)
+
+    def __call__(self, *argv):
+        self.exit_code = 0
+        error = self.parse(*argv)
+        if error:
+            return self.help(error=error)
+
+        try:
+            result = self.call()
+        except TypeError as exc:
+            self.exit_code = 1
+            rep = getattr(self.target, '__name__')
+            error = str(exc)
+            if error.startswith(rep):
+                return self.help(error=error.replace(rep, self.name))
+            raise
+
+        if inspect.iscoroutine(result):
+            result = asyncio.run(result)
+        return result
---
+++ packages/cli2/cli2-2.2.1.dev2.tar.gz/cli2-2.2.1.dev2/cli2/group.py
@@ -0,0 +1,166 @@
+import inspect
+import os
+import subprocess
+
+from .colors import colors
+from .command import Command
+from .decorators import arg
+from .entry_point import EntryPoint
+from .node import Node
+
+
+def termsize():
+    if 'FORCE_TERMSIZE' in os.environ:
+        return 180, 80
+
+    try:
+        rows, columns = subprocess.check_output(['stty', 'size']).split()
+    except subprocess.CalledProcessError:
+        return 180, 80
+    return int(rows), int(columns)
+
+
+class Group(EntryPoint, dict):
+    def __init__(self, name=None, doc=None, color=None, posix=False):
+        self.name = name
+        self.doc = doc or inspect.getdoc(self)
+        self.color = color or colors.green
+        self.posix = posix
+
+        # make help a group command
+        self.cmd(self.help)
+
+    def add(self, target, *args, **kwargs):
+        cmd = Command(target, *args, **kwargs)
+        self[cmd.name] = cmd
+        return self
+
+    def __setitem__(self, key, value):
+        value.posix = self.posix
+        super().__setitem__(key, value)
+
+    def cmd(self, *args, **kwargs):
+        if len(args) == 1 and not kwargs:
+            # simple @group.cmd syntax
+            target = args[0]
+            self.add(target)
+            return target
+        elif not args:
+            def wrap(cb):
+                self.add(cb, **kwargs)
+                return cb
+            return wrap
+        else:
+            raise Exception('Only kwargs are supported by Group.cmd')
+
+    def arg(self, name, **kwargs):
+        return arg(name, **kwargs)
+
+    def group(self, name, **kwargs):
+        self[name] = Group(name, **kwargs)
+        return self[name]
+
+    def help(self, *args, error=None, short=False):
+        """Get help for a command or group."""
+        if args:
+            target = self
+            for arg in args:
+                if arg in target:
+                    target = target[arg]
+                elif isinstance(target, Command):
+                    return target.help(error=error)
+                else:
+                    error = f'Command {arg} not found in {target}'
+                    error += f'Showing help for {target}'
+                    break
+            return target.help(error=error)
+
+        output = []
+
+        if error:
+            output.append(error + '\n')
+
+        if self.doc:
+            output.append(self.doc + '\n')
+
+        if not len(self) or short:
+            return '\n'.join(output)
+
+        namewidth = 2 + max([len(key) for key in self]) + 2
+        descwidth = termsize()[1] - namewidth
+
+        for name, command in self.items():
+            doc = command.help(short=True)
+            out = ''
+            for char in doc:
+                if len(out) == descwidth and '\n' not in out:
+                    out += '\n' + ' ' * (namewidth)
+                out += char
+
+            line = [
+                '  ',
+                command.color,
+                name,
+                colors.reset,
+                ' ' * (namewidth - len(str(name)) - 4),
+                '  ',
+                out,
+            ]
+            output.append((''.join(map(str, line))).rstrip())
+        return '\n'.join(output)
+    help.cli2 = dict(color='green')
+
+    def load(self, obj, parent=None):
+        if isinstance(obj, str):
+            obj = Node.factory(obj).target
+
+        objpackage = getattr(obj, '__package__', None)
+
+        for name in dir(obj):
+            if name == '__call__':
+                target = obj
+            elif name.startswith('__'):
+                continue
+            else:
+                target = getattr(obj, name)
+
+            targetpackage = getattr(target, '__package__', None)
+            if targetpackage and objpackage:
+                # prevent recursively loading from other packages
+                # and above obj level
+                if not targetpackage.startswith(objpackage):
+                    continue
+
+            if target == parent:
+                # detect and prevent recursive imports
+                continue
+
+            if callable(target):
+                try:
+                    inspect.signature(target)
+                except ValueError:
+                    pass
+                else:
+                    self.add(target)
+                continue
+
+            node = Node(name, target)
+            if node.callables:
+                self.group(name).load(target, parent=obj)
+
+    def __call__(self, *argv):
+        self.exit_code = 0
+        if not argv:
+            return self.help(error='No sub-command provided')
+
+        if argv[0] in self:
+            result = self[argv[0]](*argv[1:])
+            # fetch exit code
+            self.exit_code = self[argv[0]].exit_code
+        else:
+            return self.help(error=f'Command {argv[0]} not found')
+
+        return result
+
+    def __repr__(self):
+        return f'Group({self.name})'
