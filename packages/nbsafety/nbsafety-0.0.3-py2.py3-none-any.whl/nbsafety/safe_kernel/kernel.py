75e8e7065c12ae4b741bc6c7684960c2c5d4c553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:09 +0000

    Add package nbsafety/0.0.3/nbsafety-0.0.3-py2.py3-none-any.whl

---
+++ packages/nbsafety/nbsafety-0.0.3-py2.py3-none-any.whl/nbsafety/safe_kernel/kernel.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from ipykernel.ipkernel import IPythonKernel
+import nbsafety.safety
+from ..safety import DependencySafety
+from ..version import __version__
+import warnings
+#Avoid networkx gives a FutureWarning
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+_SAFETY_STATE = '__SAFETY_STATE'
+_CELL_MAGIC_NAME = '__SAFETY_CELL_MAGIC'
+_LINE_MAGIC_NAME = '__SAFETY_LINE_MAGIC'
+
+
+class SafeKernel(IPythonKernel):
+    implementation = 'kernel'
+    implementation_version = __version__
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.shell.run_cell(f'from {nbsafety.safety.__name__} import {DependencySafety.__name__}')
+        self.shell.run_cell(
+            f'{_SAFETY_STATE} = {DependencySafety.__name__}'
+            f'(cell_magic_name="{_CELL_MAGIC_NAME}", line_magic_name = "{_LINE_MAGIC_NAME}")'
+        )
+
+    def do_execute(self, code, silent, store_history=False,
+                   user_expressions=None, allow_stdin=False):
+        if code.split()[0] == "%safety":
+            code = "%{} {}".format(_LINE_MAGIC_NAME, ' '.join(code.split()[1:]))
+            return super().do_execute(code, silent, False, user_expressions, allow_stdin)
+        code = f"%%{_CELL_MAGIC_NAME}\n{code}"
+        return super().do_execute(code, silent, False, user_expressions, allow_stdin)
