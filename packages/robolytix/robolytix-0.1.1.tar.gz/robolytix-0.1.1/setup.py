4a2d49db75db21ad1954bc04b648a9720c9c137d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:52 +0000

    Add package robolytix/0.1.1/robolytix-0.1.1.tar.gz

---
+++ packages/robolytix/robolytix-0.1.1.tar.gz/robolytix-0.1.1/setup.py
@@ -0,0 +1,29 @@
+"""Setup script for robolytix"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="robolytix",
+    version="0.1.1",
+    author="Robolytix.com",
+    author_email="info@robolytix.com",
+    description="Robolytix is the key online analytic and monitoring tool for Business / Robotic Process Automation.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/robolytix/robolytix-sdk/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Home Automation",
+        "Topic :: System :: Monitoring",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+    ],
+    python_requires='>=3.6',
+)
