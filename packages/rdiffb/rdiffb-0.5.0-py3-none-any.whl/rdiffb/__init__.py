474a05c018ca533df63aa643e8957b676bffdbc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:42 +0000

    Add package rdiffb/0.5.0/rdiffb-0.5.0-py3-none-any.whl

---
+++ packages/rdiffb/rdiffb-0.5.0-py3-none-any.whl/rdiffb/__init__.py
@@ -0,0 +1,7 @@
+"""Module config for rdiffb."""
+
+from .rdiffb import *
+
+# This is the one place the version number for rdiffb is stored,
+# there is a regex for it in setup.py.
+__version__ = '0.5.0'
