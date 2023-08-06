cce32a7484a68091aa9b9ccb5c4e2fb070b8a806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:39 +0000

    Add package functional-itertools/0.9.25/functional-itertools-0.9.25.tar.gz

---
+++ packages/functional-itertools/functional-itertools-0.9.25.tar.gz/functional-itertools-0.9.25/functional_itertools/errors.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+
+class UnsupportVersionError(RuntimeError):
+    """Raised when the version of Python is unsupported."""
+
+
+class EmptyIterableError(ValueError):
+    """Raised when an Iterable is unexpectedly empty."""
+
+
+class MultipleElementsError(ValueError):
+    """Raised when an Iterable unexpectedly contains more than 1 element."""
