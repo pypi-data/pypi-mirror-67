d920f8122fece86bd25d08789382a8ea09757a2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:36 +0000

    Add package tts-wrapper/0.4.2/TTS_Wrapper-0.4.2-py3-none-any.whl

---
+++ packages/tts-wrapper/TTS_Wrapper-0.4.2-py3-none-any.whl/tts_wrapper/tts.py
@@ -0,0 +1,28 @@
+class ModuleNotInstalled(Exception):
+    def __init__(self, module: str) -> None:
+        message = f'Module {module} is not installed. It is not included with tts-wrapper.'
+        super().__init__(message)
+
+
+class TTS(object):
+    def __init__(self, voice_name=None, lang=None) -> None:
+        '''
+        @param voice_name: the voice identifier to use (we have a default value if you don't care).
+        @param lang: language code (should match the voice_name).
+        '''
+        self.voice_name = voice_name
+        self.lang = lang or 'en-US'
+
+    def _wrap_ssml(self, ssml) -> str:
+        return f'<speak>{ssml}</speak>'
+
+    def _synth(self, ssml: str, filename: str) -> None:
+        raise NotImplementedError()
+
+    def synth(self, ssml: str, filename: str) -> None:
+        '''
+        @param ssml: the ssml text to synthesize without the speak tag (will be added automatically).
+        @param filename: the output wave file path.
+        '''
+        wrapped_ssml = self._wrap_ssml(ssml)
+        self._synth(wrapped_ssml, filename)
