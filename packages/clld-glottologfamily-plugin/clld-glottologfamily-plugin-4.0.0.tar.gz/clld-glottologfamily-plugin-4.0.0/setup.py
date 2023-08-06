bc647c3e4a54538cf9523f24aca10dd5222ceba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:09 +0000

    Add package clld-glottologfamily-plugin/4.0.0/clld-glottologfamily-plugin-4.0.0.tar.gz

---
+++ packages/clld-glottologfamily-plugin/clld-glottologfamily-plugin-4.0.0.tar.gz/clld-glottologfamily-plugin-4.0.0/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='clld-glottologfamily-plugin',
+    version='4.0.0',
+    description='clld-glottologfamily-plugin',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+    ],
+    author='Robert Forkel',
+    author_email='forkel@shh.mpg.de',
+    url='https://github.com/clld/clld-glottologfamily-plugin',
+    keywords='web pyramid pylons',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'clld>=7.0',
+        'sqlalchemy',
+        'zope.interface',
+        'pyglottolog>=2.0',
+    ],
+    extras_require={
+        'dev': ['flake8', 'wheel', 'twine'],
+        'test': [
+            'pytest>=3.6',
+            'pytest-mock',
+            'pytest-clld',
+            'mock',
+            'coverage>=4.2',
+            'pytest-cov',
+            'webtest',
+        ],
+    },
+    license="Apache 2.0",
+)
