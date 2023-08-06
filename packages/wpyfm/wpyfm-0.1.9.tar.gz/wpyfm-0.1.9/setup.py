9f26364e32a93ce6911aa396bd496fbb771360a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:45 +0000

    Add package wpyfm/0.1.9/wpyfm-0.1.9.tar.gz

---
+++ packages/wpyfm/wpyfm-0.1.9.tar.gz/wpyfm-0.1.9/setup.py
@@ -0,0 +1,29 @@
+import glob
+import subprocess
+
+from setuptools import find_packages, setup
+from setuptools.command.build_py import build_py
+
+
+class CustomBuildCommand(build_py):
+    def run(self):
+        subprocess.check_call(['make'])
+        super().run()
+
+
+setup(
+    name='wpyfm',
+    version='0.1.9',
+    author='Karl Krauth',
+    author_email='karl.krauth@gmail.com',
+    description='A pybind11 wrapper for libfm.',
+    download_url= 'https://github.com/Karl-Krauth/libfm/archive/v0.1.9.tar.gz',
+    packages=find_packages(),
+    include_package_data=True,
+    cmdclass={
+        'build_py': CustomBuildCommand,
+    },
+    install_requires=[
+        'pybind11',
+    ]
+)
