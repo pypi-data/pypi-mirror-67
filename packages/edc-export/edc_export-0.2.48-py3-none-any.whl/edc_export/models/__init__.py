dc7fc0f9c8f87add589f66928ab0ca97e6e03818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:53 +0000

    Add package edc-export/0.2.48/edc_export-0.2.48-py3-none-any.whl

---
+++ packages/edc-export/edc_export-0.2.48-py3-none-any.whl/edc_export/models/__init__.py
@@ -0,0 +1,14 @@
+import sys
+
+from django.conf import settings
+
+from .data_request import DataRequest
+from .data_request_history import DataRequestHistory
+from .export_receipt import ExportReceipt
+from .file_history import FileHistory
+from .object_history import ObjectHistory
+from .plan import Plan
+from .upload_export_receipt_file import UploadExportReceiptFile
+
+if settings.APP_NAME == "edc_export" and "makemigrations" not in sys.argv:
+    from ..tests import models  # noqa
