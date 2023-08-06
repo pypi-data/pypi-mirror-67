7fc453272c08c004a211c2e2f5ad3ec10d617e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:07 +0000

    Add package objetto/0.1.1/objetto-0.1.1.tar.gz

---
+++ packages/objetto/objetto-0.1.1.tar.gz/objetto-0.1.1/objetto/_base/exceptions.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Base exceptions."""
+
+
+class ObjettoException(Exception):
+    """Objetto exception."""
+
+
+class ObjettoError(ObjettoException):
+    """Objetto error."""
