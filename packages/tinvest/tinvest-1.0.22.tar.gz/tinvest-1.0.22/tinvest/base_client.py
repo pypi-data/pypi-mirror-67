f07cbd122858ff46e356ed909ab2b6518a059441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:59 +0000

    Add package tinvest/1.0.22/tinvest-1.0.22.tar.gz

---
+++ packages/tinvest/tinvest-1.0.22.tar.gz/tinvest-1.0.22/tinvest/base_client.py
@@ -0,0 +1,28 @@
+from typing import Generic, Optional, TypeVar
+
+from .constants import PRODUCTION, SANDBOX
+
+T = TypeVar('T')
+
+
+class BaseClient(Generic[T]):
+    def __init__(
+        self, token: str, *, use_sandbox: bool = False, session: Optional[T] = None
+    ):
+        if not token:
+            raise ValueError('Token can not be empty')
+        self._base_url: str = PRODUCTION
+        if use_sandbox:
+            self._base_url = SANDBOX
+
+        self._token: str = token
+        self._session = session
+
+    @property
+    def session(self) -> T:
+        if self._session:
+            return self._session
+        raise AttributeError
+
+
+__all__ = ('BaseClient',)
---
+++ packages/tinvest/tinvest-1.0.22.tar.gz/tinvest-1.0.22/tinvest/typedefs.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+from typing import Any, Dict, Union
+
+AnyDict = Dict[str, Any]
+
+datetime_or_str = Union[datetime, str]
+
+
+__all__ = 'AnyDict'
