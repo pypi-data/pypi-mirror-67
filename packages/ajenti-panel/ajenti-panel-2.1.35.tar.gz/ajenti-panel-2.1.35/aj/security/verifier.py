a32ebb0e5465e257c04d31b5718c3423d9af6a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:30 +0000

    Add package ajenti-panel/2.1.35/ajenti-panel-2.1.35.tar.gz

---
+++ packages/ajenti-panel/ajenti-panel-2.1.35.tar.gz/ajenti-panel-2.1.35/aj/security/verifier.py
@@ -0,0 +1,19 @@
+from jadi import service
+from six import PY3
+import aj
+
+if PY3:
+    long = int
+
+@service
+class ClientCertificateVerificator():
+    def __init__(self, context):
+        self.context = context
+
+    def verify(self, x509):
+        serial = x509.get_serial_number()
+        digest = x509.digest('sha1')
+        # logging.debug('SSL verify: %s / %s' % (x509.get_subject(), digest))
+        for c in aj.config.data['ssl']['client_auth']['certificates']:
+            if long(c['serial']) == serial and c['digest'].encode('utf-8') == digest:
+                return c['user']
