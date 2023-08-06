ad19cddd366ed8a626345f54417cf8204487b947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:41 +0000

    Add package hdproc/0.1/hdproc-0.1.tar.gz

---
+++ packages/hdproc/hdproc-0.1.tar.gz/hdproc-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hdproc',
+      version='0.1',
+      description='Implementation of Hierarchical LDA',
+      url='http://github.com/datadiarist/hdproc',
+      author='Andrew and Eduardo',
+      author_email='andrew.carr@duke.edu',
+      license='MIT',
+      packages=['hdproc'],
+      zip_safe=False)
