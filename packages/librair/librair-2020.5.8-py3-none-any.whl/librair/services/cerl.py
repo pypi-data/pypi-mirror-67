47e65960ac996e661402fe10e79b582533ae2680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:46 +0000

    Add package librair/2020.5.8/librair-2020.5.8-py3-none-any.whl

---
+++ packages/librair/librair-2020.5.8-py3-none-any.whl/librair/services/cerl.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from ..protocols import http
+
+from urllib.request import urlretrieve
+
+BASE = "https://data.cerl.org/thesaurus"
+SCHEMA = ["rdfxml", "txt", "json"]
+STYLE = ["ttl", "jsonld", "internal"]
+
+CORPORATE = "cnc00019524"
+IMPRINT = "cni00031211"
+LOCATION = "cnl00029460"
+PERSON = "cnp00390358"
+
+
+def address(idn, schema, style):
+    """
+    get url of item given by idn in given schema (and optional style)
+    """
+    if schema not in SCHEMA:
+        print("schema not supported!")
+        print("choose out of:")
+        for s in SCHEMA:
+            print("\t\t", s)
+        return None
+    # case: json (jsonld), txt (ttl), txt (internal)
+    if style:
+        if style not in STYLE:
+            print("style not supported!")
+            print("choose out of:")
+            for s in STYLE:
+                print("\t\t", s)
+            return None
+        if schema == "internal":
+            return "{0}/{1}?format={2}&style={3}&pretty".format(BASE, idn,
+                                                                schema, style)
+        # case: jsonld or ttl
+        return "{0}/{1}?format={2}&style={3}".format(BASE, idn, schema, style)
+    # case: json, rdfxml, txt [= yml]
+    else:
+        if schema == "json":
+            return "{0}/{1}?format={2}&pretty".format(BASE, idn, schema)
+        # case: rdfxml or yml (txt)
+        return "{0}/{1}?format={2}".format(BASE, idn, schema)
+
+
+def request(idn, schema="rdfxml", style=None):
+    """
+    retrieve data specified by idn in given schema
+
+    supported schemas:
+
+        SCHEMA       STYLE          TYPE
+        rdfxml       -              etree.Element
+        json         -              dict
+        json         jsonld         dict
+        txt          ttl            str
+        txt          internal       str
+    """
+    url = address(idn, schema, style=style)
+    if url is not None:
+        res = http.get_request(url)
+        if schema == "rdfxml":
+            return http.response_xml(res)
+        elif schema == "json":
+            return http.response_json(res)
+        else:
+            return http.response_text(res)
+
+
+def store(idn, schema="json", style=None, path="."):
+    """
+    request data specified by idn in given schema
+    and save it to file at path
+    """
+    url = address(idn, schema, style)
+    if url is not None:
+        fp = idn + "." + schema
+        print("file:\t", fp)
+        print("path:\t", path)
+        fp = path + "/" + fp
+        urlretrieve(url, fp)
---
+++ packages/librair/librair-2020.5.8-py3-none-any.whl/librair/services/zdb.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from ..protocols import http
+
+from urllib.request import urlretrieve
+
+BASE = "https://ld.zdb-services.de"
+SCHEMA = ["html", "rdf", "ttl", "jsonld"]
+EXAMPLE = "331668-3"
+
+
+def address(idn, schema):
+    """
+    get url of item specified by idn in given schema
+    """
+    if schema not in SCHEMA:
+        print("schema not supported!\n")
+        print("choose out of:\n")
+        for s in SCHEMA:
+            print("\t\t", s)
+        print("")
+        return None
+    return "{0}/{1}/{2}.{3}".format(BASE, "data", idn, schema)
+
+
+def request(idn, schema="jsonld"):
+    """
+    retrieve data specified by idn in given schema
+
+    supported schemas:
+
+        ID          TYPE
+        html        str
+        rdf         etree.Element
+        ttl         str
+        jsonld      dict
+    """
+    url = address(idn, schema)
+    if url is not None:
+        res = http.get_request(url)
+        if schema == "jsonld":
+            return http.response_json(res)
+        elif schema == "rdf":
+            return http.response_xml(res)
+        else:
+            return http.response_text(res)
+
+
+def store(idn, schema="jsonld", path="."):
+    """
+    request data specified by idn in given schema
+    and save it to file at path
+    """
+    url = address(idn, schema)
+    if url is not None:
+        fp = idn + "." + schema
+        print("file:\t", fp)
+        print("path:\t", path)
+        fp = path + "/" + fp
+        urlretrieve(url, fp)
