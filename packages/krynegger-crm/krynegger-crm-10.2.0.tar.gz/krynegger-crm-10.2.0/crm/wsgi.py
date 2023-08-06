0a235d2da31537eb62dc71266b07438c73f4bdb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:35 +0000

    Add package krynegger-crm/10.2.0/krynegger-crm-10.2.0.tar.gz

---
+++ packages/krynegger-crm/krynegger-crm-10.2.0.tar.gz/krynegger-crm-10.2.0/crm/wsgi.py
@@ -0,0 +1,31 @@
+import logging
+import os
+import sys
+
+from configurations.wsgi import get_wsgi_application
+
+PROJECT_DIR = os.path.abspath(__file__)
+sys.path.append(PROJECT_DIR)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crm.settings")
+
+try:
+    from dotenv import load_dotenv, find_dotenv
+    from pathlib import Path
+
+    dotenv_filename = '.crm'
+    dotenv_path = find_dotenv(
+        filename=dotenv_filename,
+        raise_error_if_not_found=True,
+        usecwd=True
+    )
+    print('Load dotenv configuration from %s' % dotenv_path)
+    load_dotenv(dotenv_path=dotenv_path, override=True)
+
+    os.environ.setdefault("DJANGO_CONFIGURATION", os.getenv('KRYNEGGER_CONFIGURATION').capitalize())
+except BaseException as e:
+    logging.error('Dotenv file could not loaded')
+    # logging.exception(e)
+    # traceback.print_exc()
+
+application = get_wsgi_application()
