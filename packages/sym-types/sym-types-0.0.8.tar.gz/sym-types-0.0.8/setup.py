1864eb2a7ceeb290910d81c6c86012dc36ddccbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:27 +0000

    Add package sym-types/0.0.8/sym-types-0.0.8.tar.gz

---
+++ packages/sym-types/sym-types-0.0.8.tar.gz/sym-types-0.0.8/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sym-types",
+    version="0.0.8",
+    author="Sym Inc",
+    author_email="info@symops.io",
+    description="Generated protobufs for Sym types",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/symopsio/types",
+    packages=setuptools.find_namespace_packages(include=['sym.*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    python_requires='>=3.6',
+)
