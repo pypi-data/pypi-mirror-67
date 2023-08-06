a95c6072cfaa6878196b84db4b392d9f929f0fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:01 +0000

    Add package shutils/0.1.0/shutils-0.1.0.tar.gz

---
+++ packages/shutils/shutils-0.1.0.tar.gz/shutils-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+requires = ['configparser', 'pymysql']
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="shutils",
+    version="0.1.0",
+    author="山南",
+    author_email="zhangpeichaook@163.com",
+    url="https://github.com/shannan1989/py-shutils",
+    description="Tools for python development.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=requires,
+    entry_points={
+        "console_scripts": []
+    }
+)
