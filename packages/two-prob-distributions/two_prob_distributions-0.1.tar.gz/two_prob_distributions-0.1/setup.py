73ed2986b9ff895b016110534796e9cc7cc6f40b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:59 +0000

    Add package two-prob-distributions/0.1/two_prob_distributions-0.1.tar.gz

---
+++ packages/two-prob-distributions/two_prob_distributions-0.1.tar.gz/two_prob_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='two_prob_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['two_prob_distributions'],
+      author = 'Eswar',
+      author_email = 'eswarjgs@gmail.com',
+      zip_safe=False)
