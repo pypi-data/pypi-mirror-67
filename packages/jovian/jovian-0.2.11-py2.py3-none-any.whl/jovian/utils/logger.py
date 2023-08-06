ea5981bfe91bfeb829ac733583c93361d0dce5e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:32 +0000

    Add package jovian/0.2.11/jovian-0.2.11-py2.py3-none-any.whl

---
+++ packages/jovian/jovian-0.2.11-py2.py3-none-any.whl/jovian/utils/logger.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+import click
+
+
+def log(msg, pre=True, error=False, color=None):
+    """Print a message to stdout"""
+    if error:
+        click.secho(('[jovian] ' if pre else '') + 'Error: ' + msg, err=True, fg='red')
+    else:
+        click.echo(('[jovian] ' if pre else '') + click.style(msg, fg=color))
