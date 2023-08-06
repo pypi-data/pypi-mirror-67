5f1615086b00526b4d4fa7e9e89e398e8377c8c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:46 +0000

    Add package zipfly/2.1.0/zipfly-2.1.0.tar.gz

---
+++ packages/zipfly/zipfly-2.1.0.tar.gz/zipfly-2.1.0/zipfly/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Iterable ZIP archive generator.
+
+Derived directly from zipfile.py
+"""
+from __future__ import unicode_literals, print_function, with_statement
+
+__version__ = '2.1.0'
+__author__ = 'Santiago Debus - Grow HQ, Inc.'
+__license__ = 'MIT'
+
+from .zipfly import ZipFly
+from .api import from_one_file
