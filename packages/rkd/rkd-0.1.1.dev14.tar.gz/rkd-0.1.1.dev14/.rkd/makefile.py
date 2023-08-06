54e2f69bac34a3a400305570656dc01b1bac60cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:46 +0000

    Add package rkd/0.1.1.dev14/rkd-0.1.1.dev14.tar.gz

---
+++ packages/rkd/rkd-0.1.1.dev14.tar.gz/rkd-0.1.1.dev14/.rkd/makefile.py
@@ -0,0 +1,14 @@
+
+from rkd.syntax import TaskAliasDeclaration as Task
+from rkd.standardlib.python import imports as PythonImports
+from rkd.standardlib.docker import imports as DockerImports
+
+IMPORTS = [] + PythonImports() + DockerImports()
+
+TASKS = [
+    Task(':release', description='Release RKD to PyPI (snapshot when on master, release on tag)', to_execute=[
+        ':py:build', ':py:publish', '--username=__token__', '--password=${PYPI_TOKEN}'
+    ]),
+
+    Task(':test', [':py:unittest'], description='Run unit tests')
+]
