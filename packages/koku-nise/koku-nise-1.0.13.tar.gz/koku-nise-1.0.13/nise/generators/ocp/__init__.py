ac2f375720e21f4574a607d9b1eb2f9050553e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:21 +0000

    Add package koku-nise/1.0.13/koku-nise-1.0.13.tar.gz

---
+++ packages/koku-nise/koku-nise-1.0.13.tar.gz/koku-nise-1.0.13/nise/generators/ocp/__init__.py
@@ -0,0 +1,22 @@
+#
+# Copyright 2018 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""Module for aws data generators."""
+from nise.generators.ocp.ocp_generator import (OCPGenerator,  # noqa: F401
+                                               OCP_NODE_LABEL,
+                                               OCP_POD_USAGE,
+                                               OCP_REPORT_TYPE_TO_COLS,
+                                               OCP_STORAGE_USAGE)
