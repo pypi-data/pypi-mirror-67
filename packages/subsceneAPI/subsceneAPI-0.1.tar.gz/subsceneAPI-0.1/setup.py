3a0cae94359aba41425cc79fce79cd7ae15c8699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:08 +0000

    Add package subsceneAPI/0.1/subsceneAPI-0.1.tar.gz

---
+++ packages/subsceneAPI/subsceneAPI-0.1.tar.gz/subsceneAPI-0.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+def readme():
+    with open("README.md", "r") as fh:
+        README = fh.read()
+    return README
+setuptools.setup(
+    name="subsceneAPI", 
+    version="0.1",
+    author="Suman Dhakal",
+    author_email="dhakalsumn739@gmail.com",
+    description="python3 subscene api wrapper",
+    keywords ="pypi packages subscene api wrapper subtitle downloader package python3",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/s-4-m-a-n/subscene-API",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    # packages = ["subscene_API","subscene_API.cli","subscene_API.subtitle"],
+    packages = ["subsceneAPI"],
+    include_package_data = True, 
+    install_requires=[
+                    'beautifulsoup4==4.9.0',
+                    'requests==2.23.0'
+                        ],
+    entry_points = {
+        'console_scripts':['subsceneAPI=subsceneAPI.cli:main']
+    }
+)
