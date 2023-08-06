8bd967fcffa4823f3e0e79861a2c55ea702888d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:29 +0000

    Add package t_re2/1.0.1/t_re2-1.0.1.tar.gz

---
+++ packages/t_re2/t_re2-1.0.1.tar.gz/t_re2-1.0.1/re2/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+# copy so file to /usr/local/lib
+dependent_files = '/usr/local/lib/libre2.so.7'
+if not os.path.isfile(dependent_files):
+    if os.path.exists(os.path.dirname(dependent_files)):
+        os.makedirs(os.path.dirname(dependent_files))
+    shutil.copy2('./libre2.so.7', dependent_files)
