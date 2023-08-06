69807c7f842592de457e3de971a58a93710f145f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:58 +0000

    Add package sarscov2-meta-extract/0.5.2.2/sarscov2_meta_extract-0.5.2.2.tar.gz

---
+++ packages/sarscov2-meta-extract/sarscov2_meta_extract-0.5.2.2.tar.gz/sarscov2_meta_extract-0.5.2.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sarscov2_meta_extract",
+    version="0.5.2.2",
+    author="Nick Keener",
+    author_email="nickeener@gmail.com",
+    description="Extracts metadata from MSA for Galaxy workflow",
+    install_requires=['biopython'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nickeener/sarscov2_meta_extract",
+    packages=setuptools.find_packages(),
+    entry_points={'console_scripts': ['sarscov2_meta_extract=sarscov2metaextract.cli:main']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: Academic Free License (AFL)',
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
