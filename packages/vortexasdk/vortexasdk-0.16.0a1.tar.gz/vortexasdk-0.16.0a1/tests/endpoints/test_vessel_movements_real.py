f58398a388a1be3d6a2883b4c8eba61550192c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:41 +0000

    Add package vortexasdk/0.16.0a1/vortexasdk-0.16.0a1.tar.gz

---
+++ packages/vortexasdk/vortexasdk-0.16.0a1.tar.gz/vortexasdk-0.16.0a1/tests/endpoints/test_vessel_movements_real.py
@@ -0,0 +1,92 @@
+from datetime import datetime
+
+from tests.testcases import TestCaseUsingRealAPI
+from vortexasdk import VesselMovements, Geographies
+from vortexasdk.endpoints import vessel_movements_result
+
+
+class TestVesselMovementsReal(TestCaseUsingRealAPI):
+    def test_search(self):
+        rotterdam = [
+            g.id
+            for g in Geographies().search("rotterdam").to_list()
+            if "port" in g.layer
+        ]
+        v = VesselMovements().search(
+            filter_time_min=datetime(2017, 10, 1, 0, 0),
+            filter_time_max=datetime(2017, 10, 1, 0, 10),
+            filter_origins=rotterdam,
+        )
+
+        assert len(v) > 10
+
+    def test_search_vessel_status(self):
+        v = VesselMovements().search(
+            filter_time_min=datetime(2019, 10, 1),
+            filter_time_max=datetime(2019, 10, 15),
+            filter_vessel_classes=["vlcc"],
+            filter_vessel_status="vessel_status_ballast",
+        )
+
+        assert len(v) > 50
+
+    def test_to_df_all_columns(self):
+        rotterdam = [
+            g.id
+            for g in Geographies().search("rotterdam").to_list()
+            if "port" in g.layer
+        ]
+        df = (
+            VesselMovements()
+            .search(
+                filter_time_min=datetime(2017, 10, 1, 0, 0),
+                filter_time_max=datetime(2017, 10, 1, 0, 10),
+                filter_origins=rotterdam,
+            )
+            .to_df(columns="all")
+            .head(2)
+        )
+
+        assert len(df) == 2
+
+    def test_search_to_dataframe(self):
+        rotterdam = [
+            g.id
+            for g in Geographies().search("rotterdam").to_list()
+            if "port" in g.layer
+        ]
+        df = (
+            VesselMovements()
+            .search(
+                filter_time_min=datetime(2017, 10, 1, 0, 0),
+                filter_time_max=datetime(2017, 10, 1, 0, 10),
+                filter_origins=rotterdam,
+            )
+            .to_df()
+            .head(2)
+        )
+
+        assert list(df.columns) == vessel_movements_result.DEFAULT_COLUMNS
+        assert len(df) == 2
+
+    def test_search_to_dataframe_subset_of_columns(self):
+        cols = ["vessel.imo", "vessel.name"]
+
+        rotterdam = [
+            g.id
+            for g in Geographies().search("rotterdam").to_list()
+            if "port" in g.layer
+        ]
+        df = (
+            VesselMovements()
+            .search(
+                filter_time_min=datetime(2017, 10, 1, 0, 0),
+                filter_time_max=datetime(2017, 10, 1, 0, 10),
+                filter_origins=rotterdam,
+            )
+            .to_df(columns=cols)
+            .head(2)
+        )
+
+        assert list(df.columns) == cols
+        assert len(df) == 2
---
+++ packages/vortexasdk/vortexasdk-0.16.0a1.tar.gz/vortexasdk-0.16.0a1/vortexasdk/endpoints/endpoints.py
@@ -0,0 +1,11 @@
+CARGO_TIMESERIES_RESOURCE = "/time-series"
+
+CARGO_MOVEMENTS_RESOURCE = "/cargo-movements/search"
+VESSEL_MOVEMENTS_RESOURCE = "/vessel-movements/search"
+
+GEOGRAPHIES_REFERENCE = "/reference/geographies"
+VESSELS_REFERENCE = "/reference/vessels"
+PRODUCTS_REFERENCE = "/reference/products"
+CORPORATIONS_REFERENCE = "/reference/charterers"
+
+API_URL = "https://api.vortexa.com/v5"
