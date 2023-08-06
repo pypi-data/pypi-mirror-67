746cf95b2086395de33f92530bca8e13dece3147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:27 +0000

    Add package hca-util/0.2.0/hca_util-0.2.0-py3-none-any.whl

---
+++ packages/hca-util/hca_util-0.2.0-py3-none-any.whl/util/bucket_policy.py
@@ -0,0 +1,48 @@
+from settings import AWS_ACCOUNT, IAM_USER
+
+"""
+Permissions include
+u for upload
+d for download
+x for delete
+Possible combinations are 'u', 'ud', 'ux', 'udx'
+default permissions on new directory is 'ux'
+
+"""
+ALLOWED_PERMS = ['u', 'ud', 'ux', 'udx']
+DEFAULT_PERMS = 'ux'
+
+# user groups:
+# admin     (normally) s3 full access
+# user      util bucket (ListBucket except top-level/root directory)
+
+# Permissions for Object operations
+#                       permissions        s3 operations
+s3_permissions = {'u': 's3:PutObject',     # PUT Object, POST Object, Initiate Multipart Upload, Upload Part, Complete Multipart Upload, PUT Object - Copy
+                  'd': 's3:GetObject',     # GET Object, HEAD Object, SELECT Object Content
+                  'x': 's3:DeleteObject'}  # DELETE Object
+
+
+def get_policy_statement_template():
+    return {
+        'Action': [],
+        'Effect': 'Allow',
+        'Resource': 'arn:aws:s3:::BUCKET_NAME/AREA_NAME/*',
+        'Principal': {
+            'AWS': [
+                f'arn:aws:iam::{AWS_ACCOUNT}:user/{IAM_USER}'
+            ]
+        }
+    }.copy()
+
+
+def new_policy_statement(bucket, area, perms=None):
+    statement = get_policy_statement_template()
+    # add permissions/actions
+    if perms is None or perms not in ALLOWED_PERMS:
+        perms = DEFAULT_PERMS
+    for p in perms:
+        statement['Action'].append(s3_permissions.get(p))
+        res = statement['Resource']
+        statement['Resource'] = res.replace('BUCKET_NAME', bucket).replace('AREA_NAME', area)
+    return statement
---
+++ packages/hca-util/hca_util-0.2.0-py3-none-any.whl/util/command/config.py
@@ -0,0 +1,39 @@
+from util.aws_client import Aws
+from util.common import format_err
+from util.local_state import set_bucket
+from settings import DEFAULT_PROFILE, DEFAULT_REGION
+from util.user_profile import set_profile, profile_exists, get_profile
+
+
+class CmdConfig:
+    """
+    both admin and user
+    aws resource or client used in command - sts (to check valid credentials).
+    """
+
+    def __init__(self, args):
+        self.args = args
+
+    def run(self):
+
+        try:
+            profile = self.args.profile if self.args.profile else DEFAULT_PROFILE
+            if self.args.bucket:
+                set_bucket(self.args.bucket)
+
+            set_profile(profile, DEFAULT_REGION, self.args.ACCESS_KEY, self.args.SECRET_KEY)
+
+            # check new profile
+            if profile_exists(profile):
+                user_profile = get_profile(profile)
+                aws = Aws(user_profile)
+
+                if aws.is_valid_credentials():
+                    return True, 'Valid credentials'
+                else:
+                    return False, 'Invalid credentials'
+            else:
+                return False, 'Error setting profile'
+
+        except Exception as e:
+            return False, format_err(e, 'config')
