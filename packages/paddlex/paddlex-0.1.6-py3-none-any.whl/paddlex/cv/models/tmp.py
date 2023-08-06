7269dc663bc6a61e0f1b40274e558d83e4ffccd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:58 +0000

    Add package paddlex/0.1.6/paddlex-0.1.6-py3-none-any.whl

---
+++ packages/paddlex/paddlex-0.1.6-py3-none-any.whl/paddlex/cv/models/tmp.py
@@ -0,0 +1,35 @@
+import paddlex as pdx
+from paddlex.cls import transforms
+train_transforms = transforms.Compose([
+    transforms.RandomCrop(crop_size=224),
+    transforms.RandomHorizontalFlip(),
+    transforms.Normalize()
+])
+eval_transforms = transforms.Compose([
+    transforms.ResizeByShort(short_size=256),
+    transforms.CenterCrop(crop_size=224),
+    transforms.Normalize()
+])
+
+train_dataset = pdx.datasets.ImageNet(
+    data_dir='vegetables_cls',
+    file_list='vegetables_cls/train_list.txt',
+    label_list='vegetables_cls/labels.txt',
+    transforms=train_transforms,
+    shuffle=True)
+eval_dataset = pdx.datasets.ImageNet(
+    data_dir='vegetables_cls',
+    file_list='vegetables_cls/train_list.txt',
+    label_list='vegetables_cls/labels.txt',
+    transforms=train_transforms)
+
+num_classes = len(train_dataset.labels)
+model = pdx.cls.MobileNetV2(num_classes=num_classes)
+model.train(num_epochs=10,
+            train_dataset=train_dataset,
+            train_batch_size=32,
+            eval_dataset=eval_dataset,
+            lr_decay_epochs=[4, 6, 8],
+            learning_rate=0.025,
+            save_dir='output/mobilenetv2',
+            use_vdl=True)
