a457cf41170e641fafb4c9eadef942bed124d995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:39 +0000

    Add package NConsole/1.0/NConsole-1.0.tar.gz

---
+++ packages/NConsole/NConsole-1.0.tar.gz/NConsole-1.0/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name = "NConsole",
+    packages = ["NConsole"],
+    version = "1.0",
+    license = "gpl-3.0",
+    description = "This is a library to emulate a console and create commands for it",
+    author = "Enn3DevPlayer",
+    author_email = "arturpeshko39@gmail.com",
+    url = "https://github.com/Enn3Developer/NConsole",
+    download_url = "https://github.com/Enn3Developer/NConsole/archive/1.0.tar.gz",
+    keywords = ["console", "commands"],
+    install_requires = [],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
