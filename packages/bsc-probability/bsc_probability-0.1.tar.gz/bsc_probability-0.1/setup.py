2e3625a8635637e438cd6e5a89a1f4275f5ce255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:06 +0000

    Add package bsc-probability/0.1/bsc_probability-0.1.tar.gz

---
+++ packages/bsc-probability/bsc_probability-0.1.tar.gz/bsc_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bsc_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['bsc_probability'],
+      author='Mironas Bitinis',
+      author_email='someemal@gmail.com',
+      zip_safe=False)
