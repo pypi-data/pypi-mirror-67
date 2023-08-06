154d51f4724e3ae78d27d3a02c9282676f6bb854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:41 +0000

    Add package SeleniumAuthProxy/0.0/SeleniumAuthProxy-0.0.tar.gz

---
+++ packages/SeleniumAuthProxy/SeleniumAuthProxy-0.0.tar.gz/SeleniumAuthProxy-0.0/SeleniumAuthProxy/SeleniumAuthProxy.py
@@ -0,0 +1,66 @@
+import os
+import zipfile
+
+def get_proxy_object(ip, port, login, password, path_to_extention):
+    manifest_json = """
+    {
+        "version": "1.0.0",
+        "manifest_version": 2,
+        "name": "Chrome Proxy",
+        "permissions": [
+            "proxy",
+            "tabs",
+            "unlimitedStorage",
+            "storage",
+            "<all_urls>",
+            "webRequest",
+            "webRequestBlocking"
+        ],
+        "background": {
+            "scripts": ["background.js"]
+        },
+        "minimum_chrome_version":"22.0.0"
+    }"""
+
+    background_js = """
+    var config = {
+            mode: "fixed_servers",
+            rules: {
+            singleProxy: {
+                scheme: "http",
+                host: "%s",
+                port: parseInt(%s)
+            },
+            bypassList: ["localhost"]
+            }
+        };
+
+    chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+    function callbackFn(details) {
+        return {
+            authCredentials: {
+                username: "%s",
+                password: "%s"
+            }
+        };
+    }
+
+    chrome.webRequest.onAuthRequired.addListener(
+                callbackFn,
+                {urls: ["<all_urls>"]},
+                ['blocking']
+    );
+    """ % (ip, port, login, password)
+
+    pluginfile = f"{path_to_extention}/{ip}+{port}+{login}+{password}.zip"
+
+    with zipfile.ZipFile(pluginfile, 'w') as zp:
+        zp.writestr("manifest.json", manifest_json)
+        zp.writestr("background.js", background_js)
+
+    return pluginfile
+
+
+def delete_proxy_object(path):
+    os.remove(path)
---
+++ packages/SeleniumAuthProxy/SeleniumAuthProxy-0.0.tar.gz/SeleniumAuthProxy-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='SeleniumAuthProxy',
+      version='0.0',
+      description='Library for installing in Selenium proxy servers with authorization',
+      packages=['SeleniumAuthProxy'],
+      author_email='mart-iden@rambler.ru',
+      zip_safe=False)
