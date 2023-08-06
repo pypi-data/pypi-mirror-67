eec9f6401db775474218dd82a58aed305de49abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:13 +0000

    Add package xer2csv/1.0.4/xer2csv-1.0.4.tar.gz

---
+++ packages/xer2csv/xer2csv-1.0.4.tar.gz/xer2csv-1.0.4/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / 'README.md').read_text()
+
+setup_args = dict(
+    name='xer2csv',
+    version='1.0.4',
+    description='A simple XER to CSV converter',
+    long_description_content_type='text/markdown',
+    long_description=README,
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packages=['xer2csv'],
+    author='Constantin Tcacenco',
+    author_email='constantin.tcacenco@gmail.com',
+    keywords=['csv', 'xer', 'converter'],
+    url='https://github.com/constantintcacenco/xer-to-csv-converter',
+    download_url='https://github.com/constantintcacenco/xer-to-csv-converter',
+    install_requires=['pandas']
+)
+
+if __name__ == '__main__':
+    setup(**setup_args)
