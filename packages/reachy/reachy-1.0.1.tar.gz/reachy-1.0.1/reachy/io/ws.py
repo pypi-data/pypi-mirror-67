2bee5f749b1a0d2978391f31da5adbf7316d8241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:02 +0000

    Add package reachy/1.0.1/reachy-1.0.1.tar.gz

---
+++ packages/reachy/reachy-1.0.1.tar.gz/reachy-1.0.1/reachy/io/ws.py
@@ -0,0 +1,172 @@
+"""WebSocket IO definition."""
+
+import json
+import asyncio
+import websockets
+import numpy as np
+
+from threading import Thread, Event
+
+from .io import IO
+
+
+class WsIO(IO):
+    """WebSocket IO implementation."""
+
+    ws = None
+
+    def __init__(self, part_name):
+        """Init an io attached to the given part."""
+        self.part_name = part_name
+        self.motors = []
+
+    @classmethod
+    def shared_server(cls, part_name):
+        """Create a new io with its ws server."""
+        io = cls(part_name)
+
+        if cls.ws is None:
+            cls.ws = WsServer()
+            cls.ws.run_in_background()
+        cls.ws.register(io)
+
+        return io
+
+    def find_module(self, module_name):
+        """Get a specific module from the IO.
+
+        For the moment no module are really implemented. Only placeholders for code compatibility are provided.
+        """
+        if module_name == 'force_gripper':
+            return WsFakeForceSensor()
+
+        raise NotImplementedError
+
+    def find_dxl(self, dxl_name, dxl_config):
+        """Get a specific dynamixel motor from the IO.
+
+        Only goal position is used atm.
+        """
+        pos = dxl_config['offset'] * (-1 if dxl_config['orientation'] == 'indirect' else 1)
+        m = WsMotor(name=f'{self.part_name}.{dxl_name}', initial_position=pos)
+        self.motors.append(m)
+        return m
+
+    def find_orbita_disks(self):
+        """Get a specific orbita module from the IO.
+
+        Not currently supported.
+        """
+        bottomOrb = WsFakeOrbitaDisk()
+        middleOrb = WsFakeOrbitaDisk()
+        topOrb = WsFakeOrbitaDisk()
+        return [bottomOrb, middleOrb, topOrb]
+
+    def close(self):
+        """Close the WS."""
+        self.ws.close()
+
+
+class WsMotor(object):
+    """Motor Placeholder.
+
+    Only the goal position (ie. target_rot_position) is currently used.
+    """
+
+    def __init__(self, name, initial_position):
+        """Init the fake motor."""
+        self.name = name
+
+        self.compliant = False
+        self.target_rot_position = initial_position
+
+    @property
+    def rot_position(self):
+        """Get the present position."""
+        return self.target_rot_position
+
+
+class WsFakeOrbitaDisk(object):
+    """Orbital disk placeholder."""
+
+    def __init__(self):
+        """Create fake Orbita disk."""
+        self.compliant = False
+        self.target_rot_position = 0
+        self.limit_current = 0
+        self.encoder_res = 0
+        self.reduction = 0
+        self.wheel_size = 0
+        self.positionPid = 0
+        self.rot_position_mode = True
+        self.rot_speed_mode = False
+        self.rot_position = True
+        self.rot_speed = False
+
+    def setToZero(self):
+        """Do nothing atm."""
+        pass
+
+
+class WsFakeForceSensor(object):
+    """Force Sensor placeholder.
+
+    Always return a nan as force.
+    """
+
+    def __init__(self):
+        """Init the fake force sensor."""
+        self.load = np.nan
+
+
+class WsServer(object):
+    """WebSocket server, sync value from the modules with their equivalent from the client."""
+
+    def __init__(self, host='0.0.0.0', port=6171):
+        """Prepare the ws server."""
+        self.host, self.port = host, port
+        self.running = Event()
+
+        self.parts = []
+
+    async def sync(self, websocket, path):
+        """Sync loop that exchange modules state with the client."""
+        self.running.set()
+
+        while self.running.is_set():
+            if not websocket.open:
+                break
+
+            msg = json.dumps({
+                'motors': [
+                    {'name': m.name, 'goal_position': m.target_rot_position}
+                    for m in sum([p.motors for p in self.parts], [])
+                ]
+            })
+            await websocket.send(msg.encode('UTF-8'))
+            await asyncio.sleep(0.01)
+
+    def close(self):
+        """Stop the sync loop."""
+        self.running.clear()
+        self.t.join()
+
+    def register(self, io):
+        """Register a new io (and its module) to be synced."""
+        self.parts.append(io)
+
+    def run_forever(self):
+        """Run the sync loop forever."""
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+        serve = websockets.serve(self.sync, self.host, self.port)
+
+        loop.run_until_complete(serve)
+        loop.run_forever()
+
+    def run_in_background(self):
+        """Run the sync loop forever in background."""
+        self.t = Thread(target=self.run_forever)
+        self.t.daemon = True
+        self.t.start()
---
+++ packages/reachy/reachy-1.0.1.tar.gz/reachy-1.0.1/reachy/parts/arm.py
@@ -0,0 +1,204 @@
+"""Arm part module.
+
+Implements a Right and a Left Arm.
+"""
+
+import numpy as np
+
+from collections import OrderedDict
+
+from .hand import LeftForceGripper, RightForceGripper, OrbitaWrist
+from .part import ReachyPart
+
+
+hands = {
+    'force_gripper': {'left': LeftForceGripper, 'right': RightForceGripper},
+    'orbita_wrist': {'left': OrbitaWrist, 'right': OrbitaWrist},
+}
+
+
+class Arm(ReachyPart):
+    """Arm abstraction class.
+
+    Args:
+        side (str): 'right' or 'left'
+        io (str): port name where the modules can be found
+        dxl_motors (dict): config of the dynamixel motors composing the arm
+        hand (str): name of the Hand to attach ('force_gripper', 'orbita_wrist' or it can be None if no hand are attached)
+
+    Provides high-level access to:
+        * ordered list of motors
+        * forward and inverse kinematics
+    """
+
+    def __init__(self, side, io, dxl_motors, hand):
+        """Create a new Arm part."""
+        ReachyPart.__init__(self, name=f'{side}_arm', io=io)
+        self.side = side
+
+        dxl_motors = OrderedDict(dxl_motors)
+
+        self.attach_dxl_motors(dxl_motors)
+
+        if hand is not None and hand not in hands.keys():
+            raise ValueError(f'"hand" must be one of {list(hands.keys())} or None!')
+
+        if hand is not None:
+            hand_cls = hands[hand][side]
+
+            hand_part = hand_cls(root=self, io=io)
+            self.motors += hand_part.motors
+            self.hand = hand_part
+
+            for m, conf in hand_cls.dxl_motors.items():
+                dxl_motors[m] = conf
+
+        else:
+            self.hand = None
+
+        self.attach_kinematic_chain(dxl_motors)
+
+    def teardown(self):
+        """Clean up before closing."""
+        if self.hand is not None:
+            self.hand.teardown()
+        ReachyPart.teardown(self)
+
+    def __repr__(self):
+        """Arm representation."""
+        return f'<{self.side.capitalize()}Arm "motors": {self.motors} "hand": {self.hand}>'
+
+    def forward_kinematics(self, joints_position, use_rad=False):
+        """Compute the forward kinematics of the Arm.
+
+        Args:
+            joints_position (:py:class:`~numpy.ndarray`): angle joints configuration of the arm (in degrees by default)
+            use_rad (bool): whether or not to use radians for joints configuration
+
+        .. note:: the end effector will be the end of the Hand if one is attached.
+
+        """
+        joints_position = np.array(joints_position)
+        if len(joints_position.shape) == 1:
+            joints_position = joints_position.reshape(1, -1)
+
+        if not use_rad:
+            joints_position = np.deg2rad(joints_position)
+
+        M = self.kin_chain.forward(joints_position)
+
+        if joints_position.shape[0] == 1:
+            M = M[0]
+        return M
+
+    def inverse_kinematics(self, target_pose, q0=None, use_rad=False, maxiter=10):
+        """Approximate the inverse kinematics of the Arm.
+
+        Args:
+            target_pose (:py:class:`~numpy.ndarray`): 4x4 homogeneous pose of the target end effector pose
+            q0 (:py:class:`~numpy.ndarray`): joint initial angle configurations (used for bootstraping the optimization)
+            use_rad (bool): whether or not to use radians for joints configuration
+            maxiter (int): maximum number of iteration to run on the optimizer
+
+        .. note:: the end effector will be the end of the Hand if one is attached.
+
+        """
+        if q0 is None:
+            q0 = [m.present_position for m in self.motors]
+
+        q0 = np.array(q0)
+
+        if len(q0.shape) == 1:
+            q0 = q0.reshape(1, -1)
+
+        if len(target_pose.shape) == 2:
+            target_pose = target_pose.reshape(-1, 4, 4)
+
+        if not use_rad:
+            q0 = np.deg2rad(q0)
+
+        J = self.kin_chain.inverse(target_pose, q0, maxiter=maxiter)
+
+        if J.shape[0] == 1:
+            J = J[0]
+
+        if not use_rad:
+            J = np.rad2deg(J)
+
+        return J
+
+
+class LeftArm(Arm):
+    """Left Arm part.
+
+    Args:
+        io (str): port name where the modules can be found
+        hand (str): name of the :py:class:`~reachy.parts.hand.Hand` to attach ('force_gripper', 'orbita_wrist' or it can be None if no hand are attached)
+    """
+
+    dxl_motors = OrderedDict([
+        ('shoulder_pitch', {
+            'id': 20, 'offset': 90.0, 'orientation': 'direct',
+            'angle-limits': [-90, 180],
+            'link-translation': [0, 0.19, 0], 'link-rotation': [0, 1, 0]
+        }),
+        ('shoulder_roll', {
+            'id': 21, 'offset': -90.0, 'orientation': 'indirect',
+            'angle-limits': [-90, 100],
+            'link-translation': [0, 0, 0], 'link-rotation': [1, 0, 0],
+        }),
+        ('arm_yaw', {
+            'id': 22, 'offset': 0.0, 'orientation': 'indirect',
+            'angle-limits': [-90, 90],
+            'link-translation': [0, 0, 0], 'link-rotation': [0, 0, 1],
+        }),
+        ('elbow_pitch', {
+            'id': 23, 'offset': 0.0, 'orientation': 'indirect',
+            'angle-limits': [0, 125],
+            'link-translation': [0, 0, -0.30745], 'link-rotation': [0, 1, 0],
+        }),
+    ])
+
+    def __init__(self, io, hand=None):
+        """Create a new Left Arm part."""
+        Arm.__init__(self, side='left',
+                     io=io, dxl_motors=LeftArm.dxl_motors,
+                     hand=hand)
+
+
+class RightArm(Arm):
+    """Right Arm part.
+
+    Args:
+        io (str): port name where the modules can be found
+        hand (str): name of the :py:class:`~reachy.parts.hand.Hand` to attach ('force_gripper', 'orbita_wrist' or it can be None if no hand are attached)
+    """
+
+    dxl_motors = OrderedDict([
+        ('shoulder_pitch', {
+            'id': 10, 'offset': 90.0, 'orientation': 'indirect',
+            'angle-limits': [-180, 90],
+            'link-translation': [0, -0.19, 0], 'link-rotation': [0, 1, 0],
+        }),
+        ('shoulder_roll', {
+            'id': 11, 'offset': 90.0, 'orientation': 'indirect',
+            'angle-limits': [-100, 90],
+            'link-translation': [0, 0, 0], 'link-rotation': [1, 0, 0],
+        }),
+        ('arm_yaw', {
+            'id': 12, 'offset': 0.0, 'orientation': 'indirect',
+            'angle-limits': [-90, 90],
+            'link-translation': [0, 0, 0], 'link-rotation': [0, 0, 1],
+        }),
+        ('elbow_pitch', {
+            'id': 13, 'offset': 0.0, 'orientation': 'indirect',
+            'angle-limits': [0, 125],
+            'link-translation': [0, 0, -0.30745], 'link-rotation': [0, 1, 0],
+        }),
+    ])
+
+    def __init__(self, io, hand=None):
+        """Create a new Right Arm part."""
+        Arm.__init__(self, side='right',
+                     io=io, dxl_motors=RightArm.dxl_motors,
+                     hand=hand)
