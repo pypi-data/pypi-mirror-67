0e3c7896841914d9bf3d672a8f9633cfb17e9754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:38 +0000

    Add package web-extractor/0.1.0/web_extractor-0.1.0-py3-none-any.whl

---
+++ packages/web-extractor/web_extractor-0.1.0-py3-none-any.whl/web_extractor/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/2 12:16
+# @Author  : justin.郑 3907721@qq.com
+# @File    : __init__.py.py
+# @Desc    :
+
+from web_extractor.test import (
+    cs_test
+)
+
+from web_extractor.new_extractor import NewsExtractor
---
+++ packages/web-extractor/web_extractor-0.1.0-py3-none-any.whl/web_extractor/new_extractor/TitleExtractor.py
@@ -0,0 +1,36 @@
+import re
+from lxml.html import HtmlElement
+from web_extractor.cons import TITLE_SPLIT_CHAR_PATTERN, TITLE_HTAG_XPATH
+from web_extractor.utils import config
+
+
+class TitleExtractor:
+    def extract_by_xpath(self, element, title_xpath):
+        if title_xpath:
+            title_list = element.xpath(title_xpath)
+            if title_list:
+                return title_list[0]
+            else:
+                return ''
+        return ''
+
+    def extract_by_title(self, element):
+        title_list = element.xpath('//title/text()')
+        if not title_list:
+            return ''
+        title = re.split(TITLE_SPLIT_CHAR_PATTERN, title_list[0])
+        if title:
+            return title[0]
+        else:
+            return ''
+
+    def extract_by_htag(self, element):
+        title_list = element.xpath(TITLE_HTAG_XPATH)
+        if not title_list:
+            return ''
+        return title_list[0]
+
+    def extract(self, element: HtmlElement, title_xpath: str = ''):
+        title_xpath = title_xpath or config.get('title', {}).get('xpath')
+        title = self.extract_by_xpath(element, title_xpath) or self.extract_by_title(element) or self.extract_by_htag(element)
+        return title.strip()
