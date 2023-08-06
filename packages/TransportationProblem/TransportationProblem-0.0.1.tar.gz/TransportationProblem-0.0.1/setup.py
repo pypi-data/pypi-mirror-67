041d9f159aaa873dc6b62f31d6ad1fb3925e422a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:24 +0000

    Add package TransportationProblem/0.0.1/TransportationProblem-0.0.1.tar.gz

---
+++ packages/TransportationProblem/TransportationProblem-0.0.1.tar.gz/TransportationProblem-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='TransportationProblem',
+    version='0.0.1',
+    packages=['transportation_problem'],
+    url='https://github.com/cdfmlr/TransportationProblem/',
+    license='MIT',
+    author='CDFMLR',
+    author_email='cdfmlr@163.com',
+    description='求解产销平衡的运输问题。',
+    install_requires=[
+        'numpy'
+    ]
+)
