d48aa8c3400a7668003e91c6189825970838ebd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:03 +0000

    Add package Pysher/1.0.6/Pysher-1.0.6.tar.gz

---
+++ packages/Pysher/Pysher-1.0.6.tar.gz/Pysher-1.0.6/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+VERSION = "1.0.6"
+
+requirements = ["websocket-client!=0.49"]
+
+
+def readme():
+    with open('README.md', encoding="utf8") as f:
+        return f.read()
+
+setup(
+    name="Pysher",
+    version=VERSION,
+    description="Pusher websocket client for python, based on Erik Kulyk's PythonPusherClient",
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    keywords="pusher websocket client",
+    author="Nils Diefenbach",
+    author_email="nlsdfnbch.foss@kolabnow.com",
+    license="MIT",
+    url="https://github.com/nlsdfnbch/Pysher",
+    install_requires=requirements,
+    packages=["pysher"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries ',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
