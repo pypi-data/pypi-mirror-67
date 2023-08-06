2a43f735b0212ff342cc357169fd3fa9cdc759af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:22 +0000

    Add package mingdongtextsim/0.1.5/mingdongtextsim-0.1.5.tar.gz

---
+++ packages/mingdongtextsim/mingdongtextsim-0.1.5.tar.gz/mingdongtextsim-0.1.5/mingdongtextsim/bert/predict.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing（xuming624@qq.com)
+@description: 
+"""
+import os
+
+import tensorflow as tf
+
+import mingdongtextsim
+from mingdongtextsim.bert.model import BertSimilarity
+
+if __name__ == '__main__':
+    sim = BertSimilarity(data_dir='../data/', model_dir=os.path.join(mingdongtextsim.USER_DATA_DIR, 'chinese_L-12_H-768_A-12'),
+                         output_dir=os.path.join(mingdongtextsim.USER_DATA_DIR, 'fine_tuned_bert_similarity'))
+    sim.set_mode(tf.estimator.ModeKeys.PREDICT)
+    while True:
+        print('input start:')
+        sentence1 = input('sentence1: ')
+        sentence2 = input('sentence2: ')
+        predict = sim.predict(sentence1, sentence2)
+        print(f'similarity：{predict[0][1]}')
---
+++ packages/mingdongtextsim/mingdongtextsim-0.1.5.tar.gz/mingdongtextsim-0.1.5/setup.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Avoids IDE errors, but actual version is read from version.py
+__version__ = None
+exec(open('mingdongtextsim/version.py').read())
+
+if sys.version_info < (3,):
+    sys.exit('Sorry, Python3 is required.')
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+with open('LICENSE', 'r', encoding='utf-8') as f:
+    license = f.read()
+
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+    reqs = f.read()
+
+setup(
+    name='mingdongtextsim',
+    version=__version__,
+    description='',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='',
+    author_email='',
+    url='',
+    license="Apache 2.0",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+        'Natural Language :: Chinese (Traditional)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Indexing',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+    keywords='',
+    install_requires=reqs.strip().split('\n'),
+    packages=find_packages(exclude=['tests']),
+    package_dir={'mingdongtextsim': 'mingdongtextsim'},
+    package_data={
+        'mingdongtextsim': ['*.*', 'LICENSE', '../LICENSE', 'README.*', '../*.txt', 'embeddings/*',
+                     'utils/*', 'processors/*', 'bert/*'],
+    }
+)
