f6cbcaba96968aa1247f0b1037ba91804fe6a378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:51 +0000

    Add package dingdinghelper/0.0.5/dingdinghelper-0.0.5.tar.gz

---
+++ packages/dingdinghelper/dingdinghelper-0.0.5.tar.gz/dingdinghelper-0.0.5/setup.py
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+import setuptools
+
+with open("README.md", mode='r') as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="dingdinghelper",
+  version="0.0.5",
+  author="l2m2",
+  author_email="l2m2lq@gmail.com",
+  description="DingDing Helper.",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/l2m2/dingding-helper",
+  packages=setuptools.find_packages(),
+  classifiers=[
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+  ],
+  python_requires='>=3.6',
+)
