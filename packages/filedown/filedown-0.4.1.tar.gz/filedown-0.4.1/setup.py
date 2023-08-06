1998be0c4a12db930195114a7fb75815d51f03dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:45 +0000

    Add package filedown/0.4.1/filedown-0.4.1.tar.gz

---
+++ packages/filedown/filedown-0.4.1.tar.gz/filedown-0.4.1/setup.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+
+from setuptools import setup
+
+version = '0.4.1'
+
+entry_points = {
+    'console_scripts': [
+        'filedown=filedown.filedown:main',
+        'only_download=filedown.only_download:main'
+    ],
+}
+
+setup(
+    name='filedown',
+    packages=['filedown'],
+    version=version,
+    description='filedown = Multi Thread to Download',
+    author='Windard Yang',
+    author_email='windard@qq.com',
+    url='https://github.com/windard/filedown',
+    install_requires=[
+        'Click',
+        'tqdm',
+        'requests',
+        'gevent',
+    ],
+    license="MIT",
+    entry_points=entry_points,
+)
