b07d182e0923a97be2c684beb687d2124e29a511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:40 +0000

    Add package x-python/1.0.1/x_python-1.0.1-py3.2.egg

---
+++ packages/x-python/x_python-1.0.1-py3.2.egg/xpython/byteop/byteop26.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Bytecode Interpreter operations for Python 2.6
+"""
+from __future__ import print_function, division
+
+from xpython.byteop.byteop25 import ByteOp25
+
+class ByteOp26(ByteOp25):
+    def __init__(self, vm):
+        self.vm = vm
+        self.version = 2.6
+
+    # Right now 2.6 is largely the same as 2.5 here. How nice!
