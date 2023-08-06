468e3c694544ba53d63556bd5a54b5edb18cf335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:18 +0000

    Add package flask-hello-world/1.0/flask_hello_world-1.0.tar.gz

---
+++ packages/flask-hello-world/flask_hello_world-1.0.tar.gz/flask_hello_world-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='flask_hello_world',
+      version='1.0',
+      description='Demo Flask hello world',
+      url='https://github.com/davidleeguanwei/flask_hello_world',
+      author='davidleeguanwei',
+      author_email='davidleeguanwei@gmail.com',
+      license='MIT',
+      packages=['flask_hello_world'],
+      zip_safe=False)
