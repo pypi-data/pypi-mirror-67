e2e3be13b9203c9515385bd698929aab5450122e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:58 +0000

    Add package dedebug/0.7/dedebug-0.7.tar.gz

---
+++ packages/dedebug/dedebug-0.7.tar.gz/dedebug-0.7/dedebug/config.py
@@ -0,0 +1,7 @@
+Config = {
+    # Your own heroku host (without slash in back part)
+    'BOT_HOST': 'https://teelgram-service.herokuapp.com',
+
+    # Not matter(for express() internal routing)
+    'BOT_HOST_MSGROUTE': 'message',
+}
