1da502bfe3e2c6773c7402bac84a6846aa929634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:33 +0000

    Add package zains-gaussian-binomial-distributions/1.0.0/zains_gaussian_binomial_distributions-1.0.0.tar.gz

---
+++ packages/zains-gaussian-binomial-distributions/zains_gaussian_binomial_distributions-1.0.0.tar.gz/zains_gaussian_binomial_distributions-1.0.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='zains_gaussian_binomial_distributions',
+      version='1.0.0',
+      description='Gaussian & Binomial distributions',
+      packages=['zains_gaussian_binomial_distributions'],
+      zip_safe=False)
