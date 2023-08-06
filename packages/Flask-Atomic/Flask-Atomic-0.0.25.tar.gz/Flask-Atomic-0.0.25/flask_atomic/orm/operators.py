2d8aa7baa2e11c326128876b6dbedaa140368c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:19 +0000

    Add package Flask-Atomic/0.0.25/Flask-Atomic-0.0.25.tar.gz

---
+++ packages/Flask-Atomic/Flask-Atomic-0.0.25.tar.gz/Flask-Atomic-0.0.25/flask_atomic/orm/operators.py
@@ -0,0 +1,31 @@
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import OperationalError
+from sqlalchemy.exc import DataError
+
+from flask_atomic.logger import getlogger
+from flask_atomic.orm.database import db
+
+DATA_ERROR = 'Value provided for {} is too large.'
+EXCMAP = {
+    '9h9h': lambda x: DATA_ERROR.format(str(str(x).split('\'').pop(1)).capitalize())
+}
+
+
+def __process_error(err, info):
+    db.session.rollback()
+    raise ValueError(info)
+
+
+def commitsession():
+    try:
+        db.session.commit()
+        return
+    except OperationalError as operror:
+        db.session.rollback()
+        db.session.close()
+    except IntegrityError as integerror:
+        raise integerror
+    except DataError as error:
+        return __process_error(error, EXCMAP[error.code](error))
+    except Exception as error:
+        raise Exception
