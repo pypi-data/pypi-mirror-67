35618f9680e6ce38b8b1a87054d2dcd1e5dd45ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:32 +0000

    Add package edx-django-release-util/0.4.4/edx_django_release_util-0.4.4-py2.py3-none-any.whl

---
+++ packages/edx-django-release-util/edx_django_release_util-0.4.4-py2.py3-none-any.whl/release_util/tests/test_check_reserved_keywords/test_app/non_concrete_app/models.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class BasicModel(models.Model):
+    class Meta:
+        app_label = 'non_concrete_app'
+
+    first_name = models.CharField(max_length=20)
+
+
+class ModelMixin(object):
+    age = 100
+
+
+class MixedModel(BasicModel, ModelMixin):
+    class Meta:
+        app_label = 'non_concrete_app'
+
+    last_name = models.CharField(max_length=20)
+
+
+class AbstractModel(models.Model):
+    class Meta:
+        app_label = 'non_concrete_app'
+        abstract = True
+
+    start_date = models.CharField(max_length=20)
+
+
+class ModelWithAbstractParent(AbstractModel):
+    class Meta:
+        app_label = 'non_concrete_app'
+
+    end_date = models.CharField(max_length=20)
+
+
+class ProxyModel(BasicModel):
+    class Meta:
+        app_label = 'non_concrete_app'
+        proxy = True
