92480da620dfa15032b6237d51dd6c94d6eadec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:13 +0000

    Add package picmistandard/0.0.5/picmistandard-0.0.5.tar.gz

---
+++ packages/picmistandard/picmistandard-0.0.5.tar.gz/picmistandard-0.0.5/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# To use:
+#       python setup.py install
+
+from setuptools import setup
+
+setup(name = 'picmistandard',
+      version = '0.0.5',
+      description = 'Python base classes for PICMI standard',
+      platforms = 'any',
+      packages = ['picmistandard'],
+      package_dir = {'picmistandard': '.'},
+      )
