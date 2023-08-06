5152bf6ffeb92a3f47207babebb7c49e63349084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:06 +0000

    Add package mphyspy/0.2.0.5/mphyspy-0.2.0.5-py3-none-any.whl

---
+++ packages/mphyspy/mphyspy-0.2.0.5-py3-none-any.whl/mphyspy/__init__.py
@@ -0,0 +1,15 @@
+from mphyspy.matrix import matrix as mt
+from mphyspy.qmechanic import atom as at
+from mphyspy.qmechanic import constants as ct
+from mphyspy.qmechanic import quantum as qt
+from mphyspy.qmechanic import schrodinger as sd
+from mphyspy.srelativity import srelativity as sr
+from mphyspy.qmechanic import molecules as mc
+from mphyspy.qmechanic import conversion as cv
+import sympy as sy
+import numpy as np
+from sympy.abc import x,r,a
+from sympy import oo
+
+
+print("modules loaded successfully")
