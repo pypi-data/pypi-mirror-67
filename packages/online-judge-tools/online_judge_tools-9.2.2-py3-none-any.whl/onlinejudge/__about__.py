d0c994cf4eddcec3a3f6d17645ac679f614ff159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:23 +0000

    Add package online-judge-tools/9.2.2/online_judge_tools-9.2.2-py3-none-any.whl

---
+++ packages/online-judge-tools/online_judge_tools-9.2.2-py3-none-any.whl/onlinejudge/__about__.py
@@ -0,0 +1,9 @@
+# Python Version: 3.x
+__package_name__ = 'online-judge-tools'
+__author__ = 'Kimiyuki Onaka'
+__email__ = 'kimiyuki95@gmail.com'
+__license__ = 'MIT License'
+__url__ = 'https://github.com/kmyk/online-judge-tools'
+__version_info__ = (9, 2, 2, 'final', 0)
+__version__ = '.'.join(map(str, __version_info__[:3]))
+__description__ = 'Tools for online-judge services'
