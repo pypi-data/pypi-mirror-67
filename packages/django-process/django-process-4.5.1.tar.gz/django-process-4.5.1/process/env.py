2f4238ef07042c30c967270e78d0ea6eaa015f3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:06 +0000

    Add package django-process/4.5.1/django-process-4.5.1.tar.gz

---
+++ packages/django-process/django-process-4.5.1.tar.gz/django-process-4.5.1/process/env.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import json
+import django
+import logging
+
+logger = logging.getLogger('django-process')
+
+location = os.path.dirname(os.path.abspath(__file__))
+file = os.path.join(location, 'env_conf.json')
+
+if not os.path.isfile(file):
+    logger.warning('env for django-process is not configured')
+else:
+    with open(file, 'r') as f:
+        environment = json.load(f)
+
+    if environment['project_path'] not in sys.path:
+        sys.path.insert(0, environment['project_path'])
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", environment['project_settings'])
+    django.setup()
