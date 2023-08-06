9325e4c5d5066325ebbd5ebb6e3b2a6d82896469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:54 +0000

    Add package jiradls/0.17/jiradls-0.17.tar.gz

---
+++ packages/jiradls/jiradls-0.17.tar.gz/jiradls-0.17/setup.py
@@ -0,0 +1,54 @@
+from __future__ import absolute_import, division
+
+import io
+import os
+import re
+import sys
+
+from setuptools import find_packages, setup
+
+# cf.
+# https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-version
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name="jiradls",
+    description="A JIRA command line interface for the DLS instance",
+    license="BSD",
+    url="https://github.com/DiamondLightSource/python-jira",
+    author="Markus Gerstel",
+    author_email="scientificsoftware@diamond.ac.uk",
+    download_url="https://github.com/DiamondLightSource/python-jira/releases",
+    version=find_version("jiradls", "__init__.py"),
+    install_requires=["colorama", "cryptography", "jira", "prompt_toolkit", "pyjwt",],
+    entry_points={"console_scripts": ["jira=jiradls.command_line:main"],},
+    packages=find_packages(),
+    tests_require=["mock", "pytest"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Bug Tracking",
+    ],
+)
