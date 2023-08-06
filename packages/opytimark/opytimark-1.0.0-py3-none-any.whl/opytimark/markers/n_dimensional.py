f7226c40d7497188cbe76572800331ce64193071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:58 +0000

    Add package opytimark/1.0.0/opytimark-1.0.0-py3-none-any.whl

---
+++ packages/opytimark/opytimark-1.0.0-py3-none-any.whl/opytimark/markers/n_dimensional.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+import opytimark.utils.decorator as d
+from opytimark.core import Benchmark
+
+
+class Sphere(Benchmark):
+    """Sphere class implements the Sphere's benchmarking function.
+
+    .. math:: f(\mathbf{x}) = f(x_1, x_2, \ldots, x_n) = \sum_{i=1}^{n} x_i^2
+
+    Domain:
+        The function is commonly evaluated using :math:`x_i \in [-5.12, 5.12] \mid i = \{1, 2, \ldots, n\}`.
+
+    Global Minima:
+        :math:`f(\mathbf{x^*}) = 0 \mid \mathbf{x^*} = (0, 0, \ldots, 0)`.
+
+    """
+
+    def __init__(self, name='Sphere', dims=-1, continuous=True, convex=True,
+                 differentiable=True, multimodal=False, separable=True):
+        """Initialization method.
+
+        Args:
+            name (str): Name of the function.
+            dims (int): Number of allowed dimensions.
+            continuous (bool): Whether the function is continuous.
+            convex (bool): Whether the function is convex.
+            differentiable (bool): Whether the function is differentiable.
+            multimodal (bool): Whether the function is multimodal.
+            separable (bool): Whether the function is separable.
+
+        """
+
+        # Override its parent class
+        super(Sphere, self).__init__(name, dims, continuous,
+                                     convex, differentiable, multimodal, separable)
+
+    @d.check_dimension
+    def __call__(self, x):
+        """This method returns the function's output when the class is called.
+
+        Args:
+            x (np.array): An input array for calculating the function's output.
+
+        Returns:
+            The benchmarking function output `f(x)`.
+
+        """
+
+        # Calculating the sphere's function
+        f = x ** 2
+
+        return np.sum(f)
