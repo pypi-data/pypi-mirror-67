61206f306c7923b5165a105c878741e90a3fd14b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:30 +0000

    Add package micom/0.12.1/micom-0.12.1.tar.gz

---
+++ packages/micom/micom-0.12.1.tar.gz/micom-0.12.1/micom/data/__init__.py
@@ -0,0 +1,35 @@
+"""Submodule including some common data sets."""
+
+from os.path import split, join
+import pandas as pd
+
+__all__ = ("agora", "test_taxonomy")
+this_dir, _ = split(__file__)
+
+agora = pd.read_csv(join(this_dir, "agora.csv"))
+agora["file"] = agora["id"] + ".xml"
+
+
+def test_taxonomy(n=4):
+    """Create a simple test taxonomy.
+
+    Parameters
+    ----------
+    n : positive int
+        How many species to include.
+
+    Returns
+    -------
+    pandas.DataFrame
+        Taxonomy specification for a.
+
+    """
+    ecoli_file = join(this_dir, "e_coli_core.xml.gz")
+    ids = ["Escherichia_coli_{}".format(i) for i in range(1, n + 1)]
+    taxa = pd.DataFrame({"id": ids})
+    taxa["genus"] = "Escherichia"
+    taxa["species"] = "Eschericia coli"
+    taxa["reactions"] = 95
+    taxa["metabolites"] = 72
+    taxa["file"] = ecoli_file
+    return taxa
