376a9d13774d1db922784abc1f0c7f117251bc77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:52 +0000

    Add package async-couch/0.1a11/async-couch-0.1a11.tar.gz

---
+++ packages/async-couch/async-couch-0.1a11.tar.gz/async-couch-0.1a11/async_couch/exc.py
@@ -0,0 +1,16 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class HttpError(Exception):
+    """
+    Base Http Error
+    """
+    code: int
+    message: bytes
+
+
+class UnexpectedStatusCode(HttpError):
+    def __str__(self):
+        return f'Unexpected status code {self.code} with message:' \
+               f' {self.message}'
