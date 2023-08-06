0a2ccf413e59ef123d507d94c6670ef539250b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:09 +0000

    Add package django-comments-xtd/2.5.1/django-comments-xtd-2.5.1.tar.gz

---
+++ packages/django-comments-xtd/django-comments-xtd-2.5.1.tar.gz/django-comments-xtd-2.5.1/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const STATIC_DIR = path.resolve(__dirname,
+                                'django_comments_xtd', 'static',
+                                'django_comments_xtd', 'js');
+const SOURCE_DIR = path.resolve(STATIC_DIR, 'src');
+
+module.exports = {
+  mode: "none",
+  devtool: 'source-map',
+  entry: {
+    plugin: path.resolve(SOURCE_DIR, 'index.js')
+  },
+  output: {
+    filename: '[name]-2.5.1.js',
+    path: STATIC_DIR
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        vendors: false,
+        
+        vendor: {
+          chunks: 'all',
+          test: /node_modules/
+        }
+      }
+    },
+    minimize: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: SOURCE_DIR,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  externals: {
+    bootstrap: 'bootstrap',
+    django: 'django',
+    jquery: 'jQuery',
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  }
+};
