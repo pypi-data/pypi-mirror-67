b3326ab0e69ac6b1abf3a1390895a38ce0cd661f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:04 +0000

    Add package hackgame/0.0.21/hackgame-0.0.21-py2.py3-none-any.whl

---
+++ packages/hackgame/hackgame-0.0.21-py2.py3-none-any.whl/hackgame/cli/update_notice.py
@@ -0,0 +1,26 @@
+from distutils.version import StrictVersion
+from cachetools import TTLCache, cached
+from getversion import get_module_version
+
+import requests
+
+import hackgame
+
+
+@cached(cache=TTLCache(maxsize=1024, ttl=3600))
+def versions(package_name):
+    url = "https://pypi.python.org/pypi/%s/json" % (package_name,)
+    response = requests.get(url)
+    data = response.json()
+    versions = list(data["releases"].keys())
+    versions.sort(key=StrictVersion)
+    return versions
+
+
+def hackgame_update_notice():
+    hackgame_versions = versions("hackgame")
+    current_version = get_module_version(hackgame)[0]
+    latest_version = hackgame_versions[-1]
+    if current_version != latest_version and current_version < latest_version:
+        return f"this version of hackgame client is out-of-date" \
+               f" ({current_version} -> {latest_version})"
