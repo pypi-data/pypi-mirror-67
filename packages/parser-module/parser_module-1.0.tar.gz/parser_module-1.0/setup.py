5da8b37a10914365bd2ce5771678196b8b2ef86f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:08 +0000

    Add package parser-module/1.0/parser_module-1.0.tar.gz

---
+++ packages/parser-module/parser_module-1.0.tar.gz/parser_module-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='parser_module',
+    version='1.0',
+    url='',
+    license='',
+    pachages=['parser_module'],
+    author='Ivan Bondarenko',
+    author_email='ivan.frinom@gmail.com',
+    description='',
+    install_requires=[
+        'parser_libraries>=1.4'
+    ]
+)
