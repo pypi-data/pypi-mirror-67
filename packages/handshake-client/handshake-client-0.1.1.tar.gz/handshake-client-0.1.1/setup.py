54e8ee51900a843a96b3a8ac283c5f95081766ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:20 +0000

    Add package handshake-client/0.1.1/handshake-client-0.1.1.tar.gz

---
+++ packages/handshake-client/handshake-client-0.1.1.tar.gz/handshake-client-0.1.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['handshake_client']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp>=3.6.2,<4.0.0',
+ 'python-bitcoinrpc>=1.0,<2.0',
+ 'python-socketio[client]>=4.5.1,<5.0.0',
+ 'requests>=2.23.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'handshake-client',
+    'version': '0.1.1',
+    'description': 'Handshake API client by Python',
+    'long_description': None,
+    'author': 'naoki-maeda',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
