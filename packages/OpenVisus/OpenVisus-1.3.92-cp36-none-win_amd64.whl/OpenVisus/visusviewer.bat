381f22470875d801d2f50c604b7cb2fa5f8f1ccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:37 +0000

    Add package OpenVisus/1.3.92/OpenVisus-1.3.92-cp36-none-win_amd64.whl

---
+++ packages/OpenVisus/OpenVisus-1.3.92-cp36-none-win_amd64.whl/OpenVisus/visusviewer.bat
@@ -0,0 +1,8 @@
+
+cd %~dp0
+set PYTHON=C:\Python36-x64\python.exe
+set PATH=%PYTHON%\..;%PATH%;.\bin 
+set Qt5_DIR=bin\Qt
+set PATH=%Qt5_DIR%\bin;%PATH%
+set QT_PLUGIN_PATH=%Qt5_DIR%\plugins
+"bin\visusviewer.exe" %*
