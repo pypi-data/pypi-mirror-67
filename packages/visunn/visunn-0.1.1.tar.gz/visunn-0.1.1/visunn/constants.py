732b0ecb30db70f5c8cfba9abd16062e2d7dfb21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:55 +0000

    Add package visunn/0.1.1/visunn-0.1.1.tar.gz

---
+++ packages/visunn/visunn-0.1.1.tar.gz/visunn-0.1.1/visunn/constants.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+''' some constants shared across backend python api's '''
+
+__author__ = 'Vincent Liu'
+__email__ = 'vliu15@stanford.edu'
+
+__all__ = ['DATA_DIR',
+           'LOG_DIR', 'MODU_EXT',
+           'MODU_ROOT']
+
+# train config
+DATA_DIR = 'data'
+
+# visu config
+LOG_DIR = 'logs'
+MODU_EXT = '.pt'
+
+# modu config
+MODU_ROOT = ''
---
+++ packages/visunn/visunn-0.1.1.tar.gz/visunn-0.1.1/visunn/frontend/build/precache-manifest.10febd8ecf9ba159664f081a5cd31944.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc27e59ea30313057a583703b01064c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33ced16e5baedc66ee2c",
+    "url": "/static/css/main.5b292645.chunk.css"
+  },
+  {
+    "revision": "b49fd4eed39327ba0a40",
+    "url": "/static/js/2.5c24ba71.chunk.js"
+  },
+  {
+    "revision": "6e900953f82e75bb7ccced89e6806918",
+    "url": "/static/js/2.5c24ba71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33ced16e5baedc66ee2c",
+    "url": "/static/js/main.76cd4042.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
