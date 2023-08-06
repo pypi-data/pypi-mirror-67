3fb9cbf1608473570e6e28bb82e447f3fa269191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:18 +0000

    Add package parametrization-clean-cdaksha/1.0.0.9.7/parametrization_clean-cdaksha-1.0.0.9.7.tar.gz

---
+++ packages/parametrization-clean-cdaksha/parametrization_clean-cdaksha-1.0.0.9.7.tar.gz/parametrization_clean-cdaksha-1.0.0.9.7/parametrization_clean/domain/selection/factory.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""Factory for selection algorithms allowed for usage."""
+
+# Standard library
+
+# 3rd party packages
+
+# Local source
+from domain.selection.strategy import ISelectionStrategy
+
+
+class SelectionFactory:
+    """Factory class for creating selection algorithm executor."""
+
+    # Internal registry for available crossover methods
+    registry = {}
+
+    @classmethod
+    def register(cls, algorithm_name: str):
+        def inner_wrapper(wrapped_class: ISelectionStrategy):
+            # Register algorithm only if it doesn't already exist in the registry
+            if algorithm_name not in cls.registry:
+                cls.registry[algorithm_name] = wrapped_class
+            return wrapped_class
+        return inner_wrapper
+
+    @classmethod
+    def create_executor(cls, algorithm_name: str) -> ISelectionStrategy:
+        return cls.registry[algorithm_name]
