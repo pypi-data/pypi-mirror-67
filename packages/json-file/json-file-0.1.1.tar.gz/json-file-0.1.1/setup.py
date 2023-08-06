63495c8ec3dcffb2a634463a452d50ff214975b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:07 +0000

    Add package json-file/0.1.1/json-file-0.1.1.tar.gz

---
+++ packages/json-file/json-file-0.1.1.tar.gz/json-file-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['json_file']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['file-util>=0.1.4,<0.2.0']
+
+setup_kwargs = {
+    'name': 'json-file',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Eyal Levin',
+    'author_email': 'eyalev@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
