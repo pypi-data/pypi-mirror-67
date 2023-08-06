fcbfbff4021994292cf7a8c69c35c25a531ea31e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:19 +0000

    Add package jupyterhubutils/0.17.4/jupyterhubutils-0.17.4.tar.gz

---
+++ packages/jupyterhubutils/jupyterhubutils-0.17.4.tar.gz/jupyterhubutils-0.17.4/jupyterhubutils/loggable/loggable.py
@@ -0,0 +1,9 @@
+from ..utils import make_logger
+
+
+class Loggable(object):
+    log = None
+
+    def __init__(self, *args, **kwargs):
+        self.log = make_logger()
+        self.log.debug("Creating {}".format(self.__class__.__name__))
