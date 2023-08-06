d3d983c3f74953e9390182a55f2ffa2bb8efe489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:23 +0000

    Add package ovretl/0.2.1/ovretl-0.2.1.tar.gz

---
+++ packages/ovretl/ovretl-0.2.1.tar.gz/ovretl-0.2.1/ovretl/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.0"
+
+from ovretl.prices_utils.find_most_relevant_prices import (
+    find_most_relevant_prices_factory,
+)
+
+from ovretl.prices_utils.sum_category_price import sum_category_price
+
+from ovretl.billings_utils.compute_billing_status import compute_billing_status
+
+from ovretl.billings_utils.extract_billing_numbers import extract_billing_numbers
---
+++ packages/ovretl/ovretl-0.2.1.tar.gz/ovretl-0.2.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ovretl',
+ 'ovretl.billings_utils',
+ 'ovretl.billings_utils.tests',
+ 'ovretl.prices_utils',
+ 'ovretl.prices_utils.tests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pandas>=1.0.3,<2.0.0']
+
+setup_kwargs = {
+    'name': 'ovretl',
+    'version': '0.2.1',
+    'description': 'Python package for Ovrsea ETL',
+    'long_description': '',
+    'author': 'nicolas67',
+    'author_email': 'nicolas@ovrsea.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.1',
+}
+
+
+setup(**setup_kwargs)
