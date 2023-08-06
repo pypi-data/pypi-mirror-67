1bab5bf48f35d5f070c93c4465866455217f6b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:51 +0000

    Add package spatialmath-python/0.5/spatialmath-python-0.5.tar.gz

---
+++ packages/spatialmath-python/spatialmath-python-0.5.tar.gz/spatialmath-python-0.5/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='spatialmath-python', 
+
+    version='0.5', #TODO 
+
+    # This is a one-line description or tagline of what your project does. This
+    # corresponds to the "Summary" metadata field:
+    description='Provides spatial maths capability for Python.', #TODO
+    
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    classifiers=[
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 4 - Beta',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+    # Pick your license as you wish (should match "license" above)
+     'License :: OSI Approved :: MIT License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 3 :: Only'],
+
+    project_urls={
+        'Documentation': 'https://petercorke.github.io/spatialmath-python',
+        'Source': 'https://github.com/petercorke/spatialmath-python',
+        'Tracker': 'https://github.com/petercorke/spatialmath-python/issues',
+    },
+
+    url='https://github.com/petercorke/spatialmath-python',
+
+    author='Peter Corke',
+
+    author_email='rvc@petercorke.com', #TODO
+
+    keywords='python SO2 SE2 SO3 SE3 rotation euler roll-pitch-yaw quaternion transforms robotics vision pose',
+
+    license='MIT', #TODO
+
+    python_requires='>=3.2',
+
+    packages=find_packages(),
+
+    install_requires=['numpy', 'scipy', 'matplotlib']
+    
+)
