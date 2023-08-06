cbb5d82e126ad802669b335e7aeef4bda33b01b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:01 +0000

    Add package pywood/0.0.5/pywood-0.0.5-py3-none-any.whl

---
+++ packages/pywood/pywood-0.0.5-py3-none-any.whl/pywood/eventlib.py
@@ -0,0 +1,52 @@
+"""Модуль определяющий стандартные реализованные события."""
+from pywood.events import BaseEvent
+from pywood.updates.commands import is_command, is_start_command
+from pywood.updates.numbers import is_int_number
+from pywood.updates.texts import is_msg_w_text_to_bot
+
+__all__ = [
+    'IncomingText',
+    'IncomingCommand',
+    'IncomingIntNumber',
+    'IncomingUpdate',
+    'IncomingStartCommand',
+]
+
+
+class IncomingText(BaseEvent):
+    """Входящий текст."""
+
+    @staticmethod
+    def happened(update) -> bool:
+        return is_msg_w_text_to_bot(update)
+
+
+class IncomingUpdate(BaseEvent):
+    """Любое входящее обновление."""
+
+    @staticmethod
+    def happened(update) -> bool:
+        return True
+
+
+class IncomingCommand(BaseEvent):
+    """Входящая команда."""
+
+    @staticmethod
+    def happened(update) -> bool:
+        return is_command(update)
+
+class IncomingStartCommand(BaseEvent):
+    """Входящая команда `start`."""
+
+    @staticmethod
+    def happened(update) -> bool:
+        return is_start_command(update)
+
+
+class IncomingIntNumber(BaseEvent):
+    """Число."""
+
+    @staticmethod
+    def happened(update) -> bool:
+        return is_int_number(update)
