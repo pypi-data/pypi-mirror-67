9fb3eabf3b03f305345609b9738b58d81afb2836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:24 +0000

    Add package lapras/0.0.10/lapras-0.0.10-py3-none-any.whl

---
+++ packages/lapras/lapras-0.0.10-py3-none-any.whl/lapras/__init__.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+
+from .detector import detect
+from .stats import quality, IV, VIF, WOE
+from .selection import select
+from .transform import Combiner
+from .metrics import KS, F1, PSI
+from .plot import bin_plot,score_plot
+from .model import Model
+from .performance import performance
+
