400039437fb0f62191a84959213c661d414d4555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:03 +0000

    Add package enamlx/0.4.2/enamlx-0.4.2.tar.gz

---
+++ packages/enamlx/enamlx-0.4.2.tar.gz/enamlx-0.4.2/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""
+Copyright (c) 2015, Jairus Martin.
+Distributed under the terms of the MIT License.
+The full license is in the file COPYING.txt, distributed with this software.
+Created on Mar 2, 2016
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='enamlx',
+    version='0.4.2',
+    description='Additional Qt Widgets for Enaml',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='frmdstryr',
+    author_email='frmdstryr@gmail.com',
+    url='https://github.com/frmdstryr/enamlx',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['enaml'],
+    keywords=['enaml', 'qt', 'tree', 'table', 'widgets'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
