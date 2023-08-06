fbe7dd53b4c8465b4e75b4a9dbbe5eb5f5eab70e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:37 +0000

    Add package cell2cell/0.1.0/cell2cell-0.1.0-py3-none-any.whl

---
+++ packages/cell2cell/cell2cell-0.1.0-py3-none-any.whl/cell2cell/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from cell2cell import datasets
+
+__version__ = "0.1.0"
---
+++ packages/cell2cell/cell2cell-0.1.0-py3-none-any.whl/cell2cell/datasets/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from cell2cell.datasets.toy_data import (generate_toy_distance, generate_toy_rnaseq, generate_toy_ppi, genera_toy_metadata)
