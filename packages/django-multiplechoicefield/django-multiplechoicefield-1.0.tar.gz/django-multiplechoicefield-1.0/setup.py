57de1ad66443f5db314d0306ade4730a0cb4174b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:20 +0000

    Add package django-multiplechoicefield/1.0/django-multiplechoicefield-1.0.tar.gz

---
+++ packages/django-multiplechoicefield/django-multiplechoicefield-1.0.tar.gz/django-multiplechoicefield-1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name="django-multiplechoicefield",
+        version="1.0",
+        author="Olaf Ruehenbeck",
+        author_email="olaf+pip@ruehenbeck.org",
+        description="A Package for Django which adds a MultipleChoiceField for Models.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/oruehenbeck/django-multiplechoicefield",
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Framework :: Django",
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.6',
+        )
