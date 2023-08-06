be2caea46f95d7723e29e178061ac01becf6c651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:53 +0000

    Add package gospl/0.1.3/gospl-0.1.3.tar.gz

---
+++ packages/gospl/gospl-0.1.3.tar.gz/gospl-0.1.3/fortran/functionsmodule.h
@@ -0,0 +1,39 @@
+#define  defineTIN   definetin
+#define  defineTIN_   definetin_
+#define  _defineTIN  _definetin
+#define  _defineTIN_   _definetin_
+
+#define  defineGTIN   definegtin
+#define  defineGTIN_   definegtin_
+#define  _defineGTIN  _definegtin
+#define  _defineGTIN_   _definegtin_
+
+#define  fillPIT   fillpit
+#define  fillPIT_   fillpit_
+#define  _fillPIT  _fillpit
+#define  _fillPIT_  _fillpit_
+
+#define  ngbGlob   ngbglob
+#define  ngbGlob_   ngbglob_
+#define  _ngbGlob  _ngbglob
+#define  _ngbGlob_  _ngbglob_
+
+#define  MFDreceivers   mfdreceivers
+#define  MFDreceivers_   mfdreceivers_
+#define  _MFDreceivers  _mfdreceivers
+#define  _MFDreceivers_  _mfdreceivers_
+
+#define  setMaxNb  setmaxnb
+#define  setMaxNb_   setmaxnb_
+#define  _setMaxNb   _setmaxnb
+#define  _setMaxNb_   _setmaxnb_
+
+#define  setHillslopeCoeff  sethillslopecoeff
+#define  setHillslopeCoeff_   sethillslopecoeff_
+#define  _setHillslopeCoeff   _sethillslopecoeff
+#define  _setHillslopeCoeff_   _sethillslopecoeff_
+
+#define  marineCoeff  marinecoeff
+#define  marineCoeff_   marinecoeff_
+#define  _marineCoeff   _marinecoeff
+#define  _marineCoeff_   _marinecoeff_
