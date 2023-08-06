72600dc5041480576fbd8e932a13c6859a309909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:17 +0000

    Add package simpleocr/0.0.14/simpleocr-0.0.14.tar.gz

---
+++ packages/simpleocr/simpleocr-0.0.14.tar.gz/simpleocr-0.0.14/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+long_description = open('Readme.md', 'r').read()
+
+setuptools.setup(
+    name="simpleocr",  #
+    version="0.0.14",
+    author="xt1800i",
+    author_email="xt1800i@gmail.com",
+    description="A ocr tool for traditional chinese",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xianyuntang/simpleocr",
+    packages=setuptools.find_packages(include=['simpleocr']),
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "opencv-python",
+        "tensorflow-cpu",
+        "requests",
+        "tqdm"
+    ],
+    include_package_data=True,
+
+)
