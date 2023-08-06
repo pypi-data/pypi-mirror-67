a53209bdf18c4ce7ceb330fdacd21c858e4cba82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:23 +0000

    Add package python-interface/1.5.5/python-interface-1.5.5.tar.gz

---
+++ packages/python-interface/python-interface-1.5.5.tar.gz/python-interface-1.5.5/interface/tests/test_utils.py
@@ -0,0 +1,22 @@
+from ..compat import unwrap, wraps
+from ..utils import is_a, unique
+
+
+def test_unique():
+    assert list(unique(iter([1, 3, 1, 2, 3]))) == [1, 3, 2]
+
+
+def test_is_a():
+    assert is_a(int)(5)
+    assert not is_a(str)(5)
+
+
+def test_wrap_and_unwrap():
+    def f(a, b, c):  # pragma: nocover
+        pass
+
+    @wraps(f)
+    def g(*args):  # pragma: nocover
+        pass
+
+    assert unwrap(g) is f
