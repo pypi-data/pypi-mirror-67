489fe7f4dfecc494c0d040caae041a0ff6f49b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:08 +0000

    Add package ip-model/1.0.1/ip_model-1.0.1-py2-none-any.whl

---
+++ packages/ip-model/ip_model-1.0.1-py2-none-any.whl/ip_model/Ipv4.py
@@ -0,0 +1,35 @@
+"""
+@Copyrights 2020 Rakesh Kumar T
+author: Rakesh Kumar T
+github: rakesht2499
+"""
+
+from ip_model.util.RadixTree import RadixTree
+from ip_model.util.Validator import Validator
+validate = Validator.validate
+
+
+class Ipv4:
+    def __init__(self):
+        self.head = RadixTree("N")
+
+    @validate
+    def add(self, ip) -> bool:
+        bin_ip = self._to_binary(ip)
+        return self.head.add(bin_ip)
+
+    @validate
+    def remove(self, ip) -> bool:
+        bin_ip = self._to_binary(ip)
+        return self.head.remove(bin_ip)
+
+    @validate
+    def is_present(self, ip) -> bool:
+        bin_ip = self._to_binary(ip)
+        return self.head.is_present(bin_ip)
+
+    def _to_binary(self, ip):
+        binary_ip = ""
+        for x in ip.split("."):
+            binary_ip += f'{int(x):08b}'
+        return binary_ip
