5a5d4a139f899216e94ac0244e2c9946f7fc0b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:35 +0000

    Add package django-contactforms/1.2/django-contactforms-1.2.tar.gz

---
+++ packages/django-contactforms/django-contactforms-1.2.tar.gz/django-contactforms-1.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='django-contactforms',  
+     version='1.2',
+     scripts=['django-contactforms'] ,
+     author="Sajib Hossain",
+     author_email="sajib1066@gmail.com",
+     description="Django Contact Form Package",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/sajib1066/django-contactforms",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Framework :: Django",
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
