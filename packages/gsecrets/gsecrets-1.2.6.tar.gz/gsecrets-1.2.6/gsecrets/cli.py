5bb5d9d28e399f27109c8525ba98aa9127d40919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:01 +0000

    Add package gsecrets/1.2.6/gsecrets-1.2.6.tar.gz

---
+++ packages/gsecrets/gsecrets-1.2.6.tar.gz/gsecrets-1.2.6/gsecrets/cli.py
@@ -0,0 +1,38 @@
+import json
+import sys
+import click
+from .client import Client
+from .exceptions import SecretNotFound
+
+@click.group()
+def cli():
+    pass
+
+def parse_vault_location(path):
+    sp = path.split("/")
+    vault_location = sp[0] + "/" + sp[1]
+    path = "/".join(sp[2:])
+    return (vault_location, path)
+
+@cli.command()
+@click.argument("path")
+@click.argument("content")
+@click.option("--replace/--no-replace", default=False)
+def put(path, content, replace):
+    vault_location, path = parse_vault_location(path)
+    client = Client(vault_location)
+    client.put(path, content, replace)
+
+@cli.command()
+@click.argument("path")
+def get(path):
+    vault_location, path = parse_vault_location(path)
+    client = Client(vault_location)
+    try:
+        secret = client.get(path)
+        if type(secret) is dict:
+            print(json.dumps(secret))
+        else:
+            print(secret.decode("utf-8"))
+    except SecretNotFound:
+        sys.exit("Secret not found")
