61e321bca39b277103ea84f8aac9f8667faaa4df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:15 +0000

    Add package dandi/0.4.5/dandi-0.4.5-py3-none-any.whl

---
+++ packages/dandi/dandi-0.4.5-py3-none-any.whl/dandi/__init__.py
@@ -0,0 +1,53 @@
+import logging
+import os
+from . import _version
+
+__version__ = _version.get_versions()["version"]
+
+
+#
+# Basic logger configuration
+#
+
+
+def get_logger(name=None):
+    """Return a logger to use
+    """
+    return logging.getLogger("dandi" + (".%s" % name if name else ""))
+
+
+def set_logger_level(lgr, level):
+    if isinstance(level, int):
+        pass
+    elif level.isnumeric():
+        level = int(level)
+    elif level.isalpha():
+        level = getattr(logging, level)
+    else:
+        lgr.warning("Do not know how to treat loglevel %s" % level)
+        return
+    lgr.setLevel(level)
+
+
+_DEFAULT_LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+lgr = get_logger()
+# Basic settings for output, for now just basic
+set_logger_level(lgr, os.environ.get("DANDI_LOG_LEVEL", logging.INFO))
+FORMAT = "%(asctime)-15s [%(levelname)8s] %(message)s"
+logging.basicConfig(format=FORMAT)
+
+# Run telemetry on import for interactive sessions, such as IPython, Jupyter
+# notebooks, Python REPL
+import __main__
+
+if not hasattr(__main__, "__file__"):
+    try:
+        import etelemetry
+
+        etelemetry.check_available_version("dandi/dandi-cli", __version__, lgr=lgr)
+    except Exception as exc:
+        lgr.warning(
+            "Failed to check for a more recent version available with etelemetry: %s",
+            exc,
+        )
