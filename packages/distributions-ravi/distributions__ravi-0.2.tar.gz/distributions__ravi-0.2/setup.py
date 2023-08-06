50662d2e2bbd967964d036dbcb95b0f755237e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:28 +0000

    Add package distributions-ravi/0.2/distributions__ravi-0.2.tar.gz

---
+++ packages/distributions-ravi/distributions__ravi-0.2.tar.gz/distributions__ravi-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions__ravi',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
