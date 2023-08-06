9cd4e17977bf9829910c07102c0d5579e6dcc8ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:18 +0000

    Add package gb-distributions-probab/0.1/gb_distributions_probab-0.1.tar.gz

---
+++ packages/gb-distributions-probab/gb_distributions_probab-0.1.tar.gz/gb_distributions_probab-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gb_distributions_probab',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gb_distributions_probab'],
+      zip_safe=False)
