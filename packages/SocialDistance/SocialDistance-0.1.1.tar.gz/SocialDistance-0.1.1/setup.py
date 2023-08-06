af9c340822353cfb2a3a57c1cbc8edc2a53003bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:39 +0000

    Add package SocialDistance/0.1.1/SocialDistance-0.1.1.tar.gz

---
+++ packages/SocialDistance/SocialDistance-0.1.1.tar.gz/SocialDistance-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    # Metadata
+    name = 'SocialDistance',
+    version = '0.1.1',
+    description = 'lightweight video detection',
+    url = 'https://github.com/thomascong121/SocialDistance',
+    author = 'SocialDistance model contributors',
+    author_email = 'thomascong@outlook.com',
+    packages = find_packages(),
+    license='MIT'
+)
