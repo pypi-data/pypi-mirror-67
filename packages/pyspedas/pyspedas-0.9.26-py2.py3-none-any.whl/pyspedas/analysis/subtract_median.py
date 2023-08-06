e7797321d0a0d22563d2f0f7369061de09a5c9ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:19 +0000

    Add package pyspedas/0.9.26/pyspedas-0.9.26-py2.py3-none-any.whl

---
+++ packages/pyspedas/pyspedas-0.9.26-py2.py3-none-any.whl/pyspedas/analysis/subtract_median.py
@@ -0,0 +1,31 @@
+
+"""
+File:
+    subtract_median.py
+
+Description:
+    Subtracts the median from the data.
+
+Parameters:
+    names: str/list of str
+        List of pytplot names.
+    new_names: str/list of str
+        List of new_names for pytplot variables.
+        If not given, then a suffix is applied.
+    suffix:
+        A suffix to apply. Default is '-d'.
+    overwrite:
+        If set, then pytplot variables are replaced.
+
+Notes:
+    Allowed wildcards are ? for a single character, * from multiple characters.
+"""
+
+from .subtract_average import subtract_average
+
+
+def subtract_median(names, new_names=None, suffix=None, overwrite=None):
+
+    subtract_average(names, new_names=None, suffix=None, overwrite=None,
+                     median=1)
+    return 1
