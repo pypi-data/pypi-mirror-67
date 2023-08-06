d311060809ee42c1f4033dd87b5bbf863b45ec2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:06 +0000

    Add package vortexasdk/0.19.0/vortexasdk-0.19.0-py3-none-any.whl

---
+++ packages/vortexasdk/vortexasdk-0.19.0-py3-none-any.whl/vortexasdk/__init__.py
@@ -0,0 +1,14 @@
+"""Vortexa Python SDK."""
+# noinspection PyUnresolvedReferences
+from vortexasdk.endpoints import (
+    CargoMovements,
+    CargoTimeSeries,
+    Corporations,
+    Geographies,
+    Products,
+    VesselMovements,
+    Vessels,
+)
+
+# noinspection PyUnresolvedReferences
+from vortexasdk.version import __version__
