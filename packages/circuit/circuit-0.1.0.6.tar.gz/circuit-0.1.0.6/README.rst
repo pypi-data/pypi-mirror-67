3f48f4a58cc6d88f868ac96949558b9cd5efd08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:43 +0000

    Add package circuit/0.1.0.6/circuit-0.1.0.6.tar.gz

---
+++ packages/circuit/circuit-0.1.0.6.tar.gz/circuit-0.1.0.6/README.rst
@@ -0,0 +1,20 @@
+=======
+circuit
+=======
+
+Minimal native Python library for building and working with logical circuits.
+
+.. image:: https://badge.fury.io/py/circuit.svg
+   :target: https://badge.fury.io/py/circuit
+   :alt: PyPI version and link.
+
+Package Installation and Usage
+------------------------------
+The package is available on PyPI::
+
+    python -m pip install circuit
+
+The library can be imported in the usual way::
+
+    import circuit
+    from circuit import *
---
+++ packages/circuit/circuit-0.1.0.6.tar.gz/circuit-0.1.0.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="circuit",
+    version="0.1.0.6",
+    packages=["circuit",],
+    install_requires=["parts",],
+    license="MIT",
+    url="https://github.com/lapets/circuit",
+    author="Andrei Lapets",
+    author_email="a@lapets.io",
+    description="Minimal native Python library for building " +\
+                "and working with logical circuits.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
