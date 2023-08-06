0d754900a207d6d7e8bc298cd16951e4826fe56e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:34 +0000

    Add package Cynthia/1.0.3/Cynthia-1.0.3-py3-none-any.whl

---
+++ packages/Cynthia/Cynthia-1.0.3-py3-none-any.whl/hellopy/opts/const.py
@@ -0,0 +1,4 @@
+# -*- coddng: utf-8 -*-
+# Author: Cynthia
+
+lines = "这里是hellopy包opts子包const模块中的lines变量"
---
+++ packages/Cynthia/Cynthia-1.0.3-py3-none-any.whl/hellopy/utils/tool.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Author: Cynthia
+
+def f():
+    print("这里是hellopy包utils子包tool模块中的f函数")
+
+if __name__ == '__main__':
+    f()
