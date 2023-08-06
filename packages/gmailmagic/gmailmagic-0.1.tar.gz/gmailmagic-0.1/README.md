0fee4a8eb38c606c3d6b1fa4a917d4544d5a2dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:03 +0000

    Add package gmailmagic/0.1/gmailmagic-0.1.tar.gz

---
+++ packages/gmailmagic/gmailmagic-0.1.tar.gz/gmailmagic-0.1/README.md
@@ -0,0 +1,6 @@
+# GmailMagic
+Gmail Api
+
+# Donate
+BTC ```37rkr9cpVVcxg8vUpF65Cp9Mjgu1WrKD3d``` or 
+Paypal [Here](https://paypal.me/dian26?locale.x=id_ID "Donate")
---
+++ packages/gmailmagic/gmailmagic-0.1.tar.gz/gmailmagic-0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gmailmagic",
+    version="0.1",
+    author="Korud",
+    author_email="khorud@yahoo.com",
+    description="Gmail Api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/khorud",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    # include_package_data=True,
+    install_requires= ['selenium'],
+    python_requires= '>=3',
+)
