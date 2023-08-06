1ca0f545dbb7f41c5cf0e9c87b736d0ebad3bb6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:07 +0000

    Add package dli/1.8.0b4/dli-1.8.0b4.tar.gz

---
+++ packages/dli/dli-1.8.0b4.tar.gz/dli-1.8.0b4/dli/__init__.py
@@ -0,0 +1,28 @@
+import os
+import warnings
+
+warnings.filterwarnings(
+    'always', module="dli"
+)
+
+__version__ = '1.8.0b4'
+__product__ = "ihsm-datalake"
+
+
+def connect(api_key=None,
+            root_url="https://catalogue.datalake.ihsmarkit.com/__api",
+            debug=None,
+            strict=None,
+            use_keyring=True):
+
+    from dli.client.session import start_session
+
+    return start_session(
+        api_key,
+        root_url=root_url,
+        host=None,
+        debug=debug,
+        strict=strict,
+        use_keyring=use_keyring
+    )
+
