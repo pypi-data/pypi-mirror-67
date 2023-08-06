e07d5b6893091e0fd88f48031e718052e86e36ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:46 +0000

    Add package pomace/0.0.7/pomace-0.0.7-py3-none-any.whl

---
+++ packages/pomace/pomace-0.0.7-py3-none-any.whl/pomace/tests/test_types.py
@@ -0,0 +1,60 @@
+# pylint: disable=expression-not-assigned,unused-variable,redefined-outer-name,unused-argument
+
+import pytest
+
+from ..types import URL
+
+
+@pytest.fixture
+def url():
+    return URL('http://www.example.com/foo/bar')
+
+
+def describe_url():
+    def describe_init():
+        def with_url(expect, url):
+            expect(url.value) == 'http://www.example.com/foo/bar'
+
+        def with_url_and_trailing_slash(expect):
+            url = URL('http://example.com/login/error/')
+            expect(url.value) == 'http://example.com/login/error'
+
+        def with_path(expect):
+            url = URL('example.com', 'login')
+            expect(url.value) == 'https://example.com/login'
+
+        def with_path_at_root(expect):
+            url = URL('example.com', '@')
+            expect(url.value) == 'https://example.com'
+
+        def with_path_and_extra_slashes(expect):
+            url = URL('example.com', '/login/error/')
+            expect(url.value) == 'https://example.com/login/error'
+
+    def describe_str():
+        def it_returns_the_value(expect, url):
+            expect(str(url)) == 'http://www.example.com/foo/bar'
+
+    def describe_eq():
+        def it_compares_domain_and_path(expect, url):
+            expect(url) == URL('https://www.example.com/foo/bar/')
+            expect(url) != URL('http://example.com/foo/bar')
+
+        def it_matches_patterns(expect):
+            pattern = URL('http://example.com/p/{name}')
+            expect(pattern) == URL('http://example.com/p/foobar')
+            expect(pattern) != URL('http://example.com/')
+            expect(pattern) != URL('http://example.com/p/foo/bar')
+
+    def describe_path():
+        def when_root(expect, url):
+            url = URL('http://example.com')
+            expect(url.path) == '@'
+
+        def when_single(expect, url):
+            url = URL('http://example.com/login')
+            expect(url.path) == 'login'
+
+        def when_trailing_slash(expect, url):
+            url = URL('http://example.com/login/error/')
+            expect(url.path) == 'login/error'
