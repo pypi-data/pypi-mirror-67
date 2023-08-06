7241f4cc61232ea6d84296bf79c65c577f13e315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:23 +0000

    Add package sophize-datamodel/0.1.2/sophize-datamodel-0.1.2.tar.gz

---
+++ packages/sophize-datamodel/sophize-datamodel-0.1.2.tar.gz/sophize-datamodel-0.1.2/README.md
@@ -0,0 +1,3 @@
+# datamodel-python
+This repository contains the classes that model data that can be ingested by Sophize. Also contains utility functions to read and write these classes as json.
+
---
+++ packages/sophize-datamodel/sophize-datamodel-0.1.2.tar.gz/sophize-datamodel-0.1.2/sophize_datamodel/__init__.py
@@ -0,0 +1,2 @@
+from .resources import Argument, Term, Article, Beliefset, Project, Proposition, Term, Citation, User, Language, MetaLanguage
+from .resource_utils import get_filename, remove_nulls, write_json, resource_from_dict, read_resource
