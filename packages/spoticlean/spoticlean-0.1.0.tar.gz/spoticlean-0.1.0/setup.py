904ab4bcaf9f505ad28a7e49f17547fb8066282b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:13 +0000

    Add package spoticlean/0.1.0/spoticlean-0.1.0.tar.gz

---
+++ packages/spoticlean/spoticlean-0.1.0.tar.gz/spoticlean-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='spoticlean',
+	version='0.1.0',
+	license="MIT",
+	description='Delete Spotify playlists using RegEx',
+	url="https://github.com/kabirvirji/spoticlean",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires = ">= 3.4",
+    author="Kabir Virji",
+    author_email="kabirvirji@gmail.com",
+    scripts=["spoticlean"]
+)
