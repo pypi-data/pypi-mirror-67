14f797b72a061f551ea0aaca3a528817c8301716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:48 +0000

    Add package texthero/1.0.6/texthero-1.0.6-py3-none-any.whl

---
+++ packages/texthero/texthero-1.0.6-py3-none-any.whl/texthero/statistics.py
@@ -0,0 +1,4 @@
+class Insighters:
+    def word_count(data):
+        for d in data:
+            print("Document ", d, " contains ", len(d), "words.")
---
+++ packages/texthero/texthero-1.0.6-py3-none-any.whl/texthero/texthero.py
@@ -0,0 +1,43 @@
+from . import preprocessing
+from . import representation
+from . import visualization
+
+import pandas as pd
+
+"""
+Preprocessing
+"""
+
+defpreprocess(df, text_columns='text', pipeline=None):
+    return preprocessingpreprocess(df,text_columns, pipeline)
+
+"""
+Representation
+"""
+
+deftfidf(df, text_columns='text', max_features=100):
+    return representationtfidf(df, text_columns=text_columns, max_features=max_features)
+
+defpca(df, vector_columns='tfidf_text', n_components=2):
+    return representationpca(df, vector_columns, n_components)
+
+defnmf(df, vector_columns='tfidf_text', n_components=2):
+    return representationnmf(df, vector_columns, n_components)
+
+deftsne(df, vector_columns, n_components=2, perplexity=30, early_exaggeration=12, learning_rate=200, n_iter=1000):
+    return representationtsne(df,
+                        vector_columns,
+                        n_components,
+                        perplexity,
+                        early_exaggeration,
+                        learning_rate,
+                        n_iter)
+
+"""
+Visualization
+"""
+def scatterplot(df, column, color=None, hover_data=None):
+    return visualization.scatterplot(df, column, color, hover_data)
+
+def top_words(s: pd.Series, normalize=False):
+    return visualization.top_words(s, normalize=normalize)
