4bf71e7c20b4b531725817731bf5e9de79a5c616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:21 +0000

    Add package tamplar/0.1.10/tamplar-0.1.10-py3-none-any.whl

---
+++ packages/tamplar/tamplar-0.1.10-py3-none-any.whl/tamplar/api/methods.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import shutil
+import pathlib
+import subprocess
+
+from loguru import logger
+
+
+class DuplicatePathException(Exception):
+    """
+    There are not the only path to library
+    """
+    pass
+
+
+logger.add(sys.stdout, backtrace=False, diagnose=True)
+
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
+
+def deps():
+    print(pathlib.Path(__file__).parent.absolute())
+    subprocess.call(['pip', 'install', '-r', 'requirements'])
+
+
+def init():
+    paths = list(set([p for p in sys.path if p.split('/')[-1] == 'tamplar']))
+    if len(paths) != 1:
+        raise DuplicatePathException()
+    src = paths[0]+'/template/'
+    dst = os.path.abspath('./')
+    copytree(src=src, dst=dst)
