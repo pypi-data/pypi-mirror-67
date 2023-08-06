fc01d8187eac671120fce42a73c755191b54dc30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:53 +0000

    Add package OpenVisus/1.3.94/OpenVisus-1.3.94-cp37-none-manylinux2010_x86_64.whl

---
+++ packages/OpenVisus/OpenVisus-1.3.94-cp37-none-manylinux2010_x86_64.whl/OpenVisus/visus.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+cd $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+PYTHON=/usr/local/bin/python3.8
+export PYTHONPATH=$(${PYTHON} -c "import sys;print(sys.path)"):${PYTHONPATH}
+export LD_LIBRARY_PATH=$(${PYTHON} -c "import os,sysconfig;print(os.path.realpath(sysconfig.get_config_var('LIBDIR')))"):${LD_LIBRARY_PATH}
+bin/visus "$@"
