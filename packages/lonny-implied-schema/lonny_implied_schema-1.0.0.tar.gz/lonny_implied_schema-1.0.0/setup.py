b3cbaf44694e258033553ef778fcca8253d49d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:47 +0000

    Add package lonny-implied-schema/1.0.0/lonny_implied_schema-1.0.0.tar.gz

---
+++ packages/lonny-implied-schema/lonny_implied_schema-1.0.0.tar.gz/lonny_implied_schema-1.0.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name = "lonny_implied_schema",
+    version = "1.0.0",
+    author ="Tim Lonsdale",
+    author_email = "t@lonny.io",
+    description = "Library for validating nested JSON structures",
+    url = "https://gitlab.com/tlonny/flexi-validate",
+    packages = setuptools.find_packages(),
+)
