9ea01b297ee6105af7513aec2c5d84704ed7969b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:07 +0000

    Add package slurm-jupyter/1.0.2/slurm-jupyter-1.0.2.tar.gz

---
+++ packages/slurm-jupyter/slurm-jupyter-1.0.2.tar.gz/slurm-jupyter-1.0.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools, os, sys
+
+# if sys.platform == 'win32':
+#     # make an .exe file beside the actual script
+#     os.environ['SETUPTOOLS_LAUNCHER'] = "executable"
+
+setuptools.setup(
+    name="slurm-jupyter",
+    version="1.0.2",
+    author="Kasper Munch",
+    author_email="kaspermunch@birc.au.dk",
+    description="Utility running jupyter on a slurm cluster.",
+    # long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kaspermunch/slurm-jupyter",
+    packages=setuptools.find_packages(),
+    scripts=['slurm-jupyter.py'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+          'colorama',
+    ],)
+
+# python3 setup.py sdist bdist_wheel
