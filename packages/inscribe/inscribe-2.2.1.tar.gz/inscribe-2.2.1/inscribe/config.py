357b04fc7d1aa356636f939cbae456601a26fc55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:29 +0000

    Add package inscribe/2.2.1/inscribe-2.2.1.tar.gz

---
+++ packages/inscribe/inscribe-2.2.1.tar.gz/inscribe-2.2.1/inscribe/config.py
@@ -0,0 +1,22 @@
+config = {
+  "production": {
+    "api": {
+      "url": "https://app.inscribe.ai",
+    }
+  },
+  "staging": {
+    "api": {
+      "url": "https://staging.inscribe.ai",
+    }
+  },
+  "development": {
+    "api": {
+      "url": "http://127.0.0.1:8001",
+    }
+  },
+  "test": {
+    "api": {
+      "url": "http://127.0.0.1:8001",
+    }
+  }
+}
