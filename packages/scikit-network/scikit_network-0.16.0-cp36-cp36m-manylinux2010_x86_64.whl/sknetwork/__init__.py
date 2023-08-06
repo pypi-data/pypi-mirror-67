c25816c4b98f439c44fb067997f73209eb274b05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:54 +0000

    Add package scikit-network/0.16.0/scikit_network-0.16.0-cp36-cp36m-manylinux2010_x86_64.whl

---
+++ packages/scikit-network/scikit_network-0.16.0-cp36-cp36m-manylinux2010_x86_64.whl/sknetwork/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Top-level package for scikit-network"""
+
+__author__ = """scikit-network team"""
+__email__ = "bonald@enst.fr"
+__version__ = '0.16.0'
+
+import sknetwork.connectivity
+import sknetwork.classification
+import sknetwork.clustering
+import sknetwork.embedding
+import sknetwork.hierarchy
+import sknetwork.linalg
+import sknetwork.ranking
+import sknetwork.data
+import sknetwork.utils
+import sknetwork.visualization
---
+++ packages/scikit-network/scikit_network-0.16.0-cp36-cp36m-manylinux2010_x86_64.whl/sknetwork/utils/__init__.py
@@ -0,0 +1,39 @@
+"""utils module"""
+from sknetwork.utils.co_neighbors import co_neighbors_graph
+from sknetwork.utils.format import *
+from sknetwork.utils.kmeans import KMeansDense
+from sknetwork.utils.knn import KNNDense, PNNDense
+from sknetwork.utils.membership import membership_matrix
+from sknetwork.utils.simplex import projection_simplex
+from sknetwork.utils.ward import WardDense
+
+
+class Bunch(dict):
+    """Container object for datasets.
+    Dictionary-like object that exposes its keys as attributes.
+
+    This code is taken from scikit-learn.
+    >>> b = Bunch(a=1, b=2)
+    >>> b['b']
+    2
+    >>> b.b
+    2
+    >>> b.a = 3
+    >>> b['a']
+    3
+    >>> b.c = 6
+    >>> b['c']
+    6
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(key)
