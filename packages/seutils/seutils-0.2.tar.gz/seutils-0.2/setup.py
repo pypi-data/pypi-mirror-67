6a437cdea25b043ecb44a14e4b572fc49bd26335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:29 +0000

    Add package seutils/0.2/seutils-0.2.tar.gz

---
+++ packages/seutils/seutils-0.2.tar.gz/seutils-0.2/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name          = 'seutils',
+    version       = '0.2',
+    license       = 'BSD 3-Clause License',
+    description   = 'Description text',
+    url           = 'https://github.com/tklijnsma/seutils.git',
+    author        = 'Thomas Klijnsma',
+    author_email  = 'tklijnsm@gmail.com',
+    # packages      = ['seutils'],
+    py_modules    = ['seutils'],
+    zip_safe      = False,
+    scripts       = [],
+    )
