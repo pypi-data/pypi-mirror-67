9ec859458af84dce55a0a93c1a5c9d6f39b28ee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:02 +0000

    Add package shared-image-processing/0.0.1/shared_image_processing-0.0.1.tar.gz

---
+++ packages/shared-image-processing/shared_image_processing-0.0.1.tar.gz/shared_image_processing-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="shared_image_processing", 
+    version="0.0.1",
+    author="Matt-Conrad",
+    author_email="mattgrayconrad@gmail.com",
+    description="Image processing library for sharing across projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Matt-Conrad/SharedImageProcessing",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
