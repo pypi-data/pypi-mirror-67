8896bc057a4a8c6c94d2eb242264c67c5b7c263c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:24 +0000

    Add package inspy-logger/1.0.2/inspy-logger-1.0.2.tar.gz

---
+++ packages/inspy-logger/inspy-logger-1.0.2.tar.gz/inspy-logger-1.0.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+import setuptools_autover
+from distutils.core import setup
+
+setup(
+    name='inspy-logger',
+    version='1.0.2',
+    packages=setuptools.find_packages(),
+    url='https://github.com/Inspyre-Softworks/inspy_logger',
+    license='WTFYW',
+    author='Taylor-Jayde B. Blackstone',
+    author_email='t.blackstone@inspyre.tech',
+    description='A module that gives you a formatted logger that is also far more informative than the default logger'
+)
