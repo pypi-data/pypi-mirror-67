5f3bebd3de9420368e3a71ddcaf1d42a9fca6261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:38 +0000

    Add package CsPy-Uploading/0.1/CsPy_Uploading-0.1.tar.gz

---
+++ packages/CsPy-Uploading/CsPy_Uploading-0.1.tar.gz/CsPy_Uploading-0.1/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+setup(
+  name = 'CsPy_Uploading',         # How you named your package folder (MyLib)
+  packages = ['CsPy_Uploading'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Package useful for THG staff to aid data pipeline implementation',   # Give a short description about your library
+  author = 'James Robinson',                   # Type in your name
+  author_email = 'james.robinson@thehutgroup.com',      # Type in your E-Mail
+  url = 'https://github.com/JamesRobinson-THG',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/JamesRobinson-THG/CsPY_Uploading/archive/0.1.tar.gz',    # I explain this later on
+  keywords = [],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+		'pandas',
+		'numpy',
+		'datetime',
+		'os',
+		'openpyxl',
+		'pyodbc',
+		'humanfriendly',
+		'platform',
+		'win32com.client',
+		'sys',
+		'google.cloud.bigquery',
+		'google.oauth2',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.7'    #Specify which pyhton versions that you want to support
+  ],
+)
