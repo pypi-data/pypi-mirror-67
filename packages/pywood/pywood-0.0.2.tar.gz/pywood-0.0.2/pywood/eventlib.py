fd1a374367ee33b622f40f14ad8f49c8d189cdde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:27 +0000

    Add package pywood/0.0.2/pywood-0.0.2.tar.gz

---
+++ packages/pywood/pywood-0.0.2.tar.gz/pywood-0.0.2/pywood/eventlib.py
@@ -0,0 +1,44 @@
+"""Модуль определяющий стандартные реализованные события."""
+from pywood.events import BaseEvent
+from pywood.updates.commands import is_command
+from pywood.updates.numbers import is_int_number
+from pywood.updates.texts import is_msg_w_text_to_bot
+
+__all__ = [
+    'IncomingText',
+    'IncomingCommand',
+    'IncomingIntNumber',
+    'IncomingUpdate'
+]
+
+
+class IncomingText(BaseEvent):
+    """Входящий текст."""
+
+    @staticmethod
+    def happened(update) -> bool:
+        return is_msg_w_text_to_bot(update)
+
+
+class IncomingUpdate(BaseEvent):
+    """Любое входящее обновление."""
+
+    @staticmethod
+    def happened(update) -> bool:
+        return True
+
+
+class IncomingCommand(BaseEvent):
+    """Входящая команда."""
+
+    @staticmethod
+    def happened(update) -> bool:
+        return is_command(update)
+
+
+class IncomingIntNumber(BaseEvent):
+    """Число."""
+
+    @staticmethod
+    def happened(update) -> bool:
+        return is_int_number(update)
