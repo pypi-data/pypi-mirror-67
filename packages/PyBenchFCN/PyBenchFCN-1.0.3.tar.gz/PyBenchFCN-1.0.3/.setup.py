eddfda1f5ea50f74d8fa6c10d5c39dbb5532d84b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:42 +0000

    Add package PyBenchFCN/1.0.3/PyBenchFCN-1.0.3.tar.gz

---
+++ packages/PyBenchFCN/PyBenchFCN-1.0.3.tar.gz/PyBenchFCN-1.0.3/.setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyBenchFCN", # Replace with your own username
+    version="1.0.3",
+    author="Yifan He",
+    author_email="he.yifan.xs@alumni.tsukuba.ac.jp",
+    description="A python implementation of optimization benchmarks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Y1fanHE/PyBenchFCN",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+        'matplotlib'],
+)
