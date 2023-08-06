156cee002a6db953509ef042563210da37ac3ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:18 +0000

    Add package mc-nag/0.0.1/mc_nag-0.0.1.tar.gz

---
+++ packages/mc-nag/mc_nag-0.0.1.tar.gz/mc_nag-0.0.1/tests/rules/standard/standard_rule.py
@@ -0,0 +1,21 @@
+"""Standard rule for testing."""
+
+from mc_nag.base_utils.models.rule import (
+    BaseRule,
+    WARNING as RULE_WARNING
+)
+
+
+class StandardRule(BaseRule):
+    """Create a standard rule for testing."""
+
+    rule_id = 'standard-rule'
+    description = """Testing standard rules."""
+    severity = RULE_WARNING
+    url = 'https://github.com/stelligent/mc-nag/tests/rules/standard/' + \
+          'standard_rule.py'
+    resolution = 'N/A'
+
+    def evaluate(self):
+        """Return empty list."""
+        return []
