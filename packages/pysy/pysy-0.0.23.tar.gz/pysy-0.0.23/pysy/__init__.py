06a2671150bab7ca52e460d3cf0ab43408acbe28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:02 +0000

    Add package pysy/0.0.23/pysy-0.0.23.tar.gz

---
+++ packages/pysy/pysy-0.0.23.tar.gz/pysy-0.0.23/pysy/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+__all__ = ["scigeo", "scigee", "fluxlib", "toolbox", "gee", "geo", "utils", "viz_params"]
+
+# from pysy.scigeo import *
+# from pysy.scigee import *
+# from pysy.fluxlib import *
+# from pysy.toolbox import *
+import ee
+
+ee.Initialize()
