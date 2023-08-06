e755f8f7793909024b415817cf64c50dfecc7da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:24 +0000

    Add package pygacode/0.52/pygacode-0.52.tar.gz

---
+++ packages/pygacode/pygacode-0.52.tar.gz/pygacode-0.52/setup.py
@@ -0,0 +1,26 @@
+from numpy.distutils.core import setup, Extension
+import sys
+
+wrapper = Extension('gacode_ext',
+                    sources=['expro/expro.f90',
+                             'expro/expro_util.f90',
+                             'expro/expro_pycomm.f90',
+                             'geo/geo.f90',
+                             'vis/vis.f90'])
+
+setup(name='pygacode',
+      version='0.52',
+      description='Python interface to GACODE profile, geometry, and code tools',
+      url='https://gacode.io',
+      author='General Atomics Theory Group',
+      author_email='candy@fusion.gat.com',
+      license='MIT',
+      py_modules=['pygacode.__init__', 'pygacode.gacodefuncs', 'pygacode.gacodeinput'],
+      package_data={'pygacode.test': ['input.gacode']},
+      packages=['pygacode.test', 'pygacode.gyro', 'pygacode.cgyro', 'pygacode.tgyro', 'pygacode.neo', 'pygacode.profiles_gen'],
+      ext_modules=[wrapper]
+      )
+
+# only run tests when installing
+if 'install' in sys.argv:
+    from pygacode.test import test_install
