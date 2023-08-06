5f0d509e4626f314b83bb52cc6c3e7586ef7fd36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:47 +0000

    Add package nb2pdf/0.5.0/nb2pdf-0.5.0.tar.gz

---
+++ packages/nb2pdf/nb2pdf-0.5.0.tar.gz/nb2pdf-0.5.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name = "nb2pdf",
+	version = "0.5.0",
+	author = "Chris Pyles & Yanay Rosen",
+	author_email = "ds-infra@berkeley.edu",
+	description = "Jupyter Notebook to PDF Converter",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	url = "https://github.com/ucbds-infra/nb2pdf",
+	license = "BSD-3-Clause",
+	packages = setuptools.find_packages(),
+	classifiers = [
+		"Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+	],
+	install_requires = [
+		"nbpdfexport"
+	],
+	scripts=["bin/nb2pdf"]
+)
