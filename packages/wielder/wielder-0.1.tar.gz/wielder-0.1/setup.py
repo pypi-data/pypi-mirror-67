0cdedc1a6c8a5b1b3f95b2b339c2a4823af78e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:39 +0000

    Add package wielder/0.1/wielder-0.1.tar.gz

---
+++ packages/wielder/wielder-0.1.tar.gz/wielder-0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='wielder',
+      version='0.1',
+      description='A reactive debuggable CI-CD & orchestration management tool'
+                  ' for local & cloud deployments e.g. kubernetes, airflow & data lakes',
+      url='https://github.com/hamshif/Wielder.git',
+      download_url='https://github.com/hamshif/Wielder/archive/v0.1-beta.tar.gz',
+      author='Hamshif',
+      author_email='hamshif@gmail.com',
+      license='Apache License Version 2.0',
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=['gitpython', 'pyyaml', 'kubernetes', 'rx', 'jprops', 'pyhocon', 'requests'],
+      keywords=['CI-CD', 'Kubernetes', 'Reactive'],
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Build Tools',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+      ],
+)
