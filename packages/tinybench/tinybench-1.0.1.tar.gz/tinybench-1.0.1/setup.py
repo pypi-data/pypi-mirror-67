c845ead3fa24609e8266cbc9deb107f339d7935f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:42 +0000

    Add package tinybench/1.0.1/tinybench-1.0.1.tar.gz

---
+++ packages/tinybench/tinybench-1.0.1.tar.gz/tinybench-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tinybench",
+    version="1.0.1",
+    author="AD Ventures",
+    author_email="abir.dahlin.ventures@gmail.com",
+    description="A microbenchmark for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AD-Ventures/tinybench",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.3',
+)
---
+++ packages/tinybench/tinybench-1.0.1.tar.gz/tinybench-1.0.1/tinybench/__init__.py
@@ -0,0 +1,6 @@
+from .tinybench import benchmark, benchmark_env
+
+__all__ = [
+  'benchmark',
+  'benchmark_env'
+]
