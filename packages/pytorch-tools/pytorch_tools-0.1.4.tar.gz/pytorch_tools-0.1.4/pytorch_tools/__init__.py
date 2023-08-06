37ee8feecf7b801f96bc99e8721804e276104c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:46 +0000

    Add package pytorch-tools/0.1.4/pytorch_tools-0.1.4.tar.gz

---
+++ packages/pytorch-tools/pytorch_tools-0.1.4.tar.gz/pytorch_tools-0.1.4/pytorch_tools/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.4"
+
+from . import fit_wrapper
+from . import losses
+from . import metrics
+from . import models
+from . import modules
+from . import optim
+from . import segmentation_models
+from . import tta_wrapper
+from . import utils
