3f734580f453b39a2b609f8a7230f254e73a6ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:12 +0000

    Add package facile-new-business-lib/0.0.7.2/facile-new-business-lib-0.0.7.2.tar.gz

---
+++ packages/facile-new-business-lib/facile-new-business-lib-0.0.7.2.tar.gz/facile-new-business-lib-0.0.7.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="facile-new-business-lib",
+    version="0.0.7.2",
+    author="Fabio Lima",
+    author_email="f46io@icloud.com",
+    description="testing ",
+    long_description="Package which centralized python newbusiness keywords",
+    long_description_content_type="text/markdown",
+    url="https://www.facile.it",
+    packages=["facilenewbusinesslib"],
+    install_requires = [
+            "selenium ~= 3.141.0",
+            "robotframework ~= 3.2",
+    ],
+    Classfiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: MIT",
+        "Operating System  ::  OS Independent",
+        "Facile.it New Business help out lib "
+    ],
+)
