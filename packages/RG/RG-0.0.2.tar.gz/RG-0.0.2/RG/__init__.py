af8a725d53e932501994c6d7a7aa0750b36509a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:02 +0000

    Add package RG/0.0.2/RG-0.0.2.tar.gz

---
+++ packages/RG/RG-0.0.2.tar.gz/RG-0.0.2/RG/__init__.py
@@ -0,0 +1,16 @@
+from RG.fct_xlsread import *
+from RG.fct_regress import *
+from RG.fct_R2 import *
+
+from RG.fct_PCA import *
+from RG.fct_PLS import *
+
+from RG.fct_colorspectra import *
+
+from RG.fct_snv import *
+
+
+
+
+
+
---
+++ packages/RG/RG-0.0.2.tar.gz/RG-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+setuptools.setup(
+    name="RG",
+    version="0.0.2",
+    author="Ryan Gosselin",
+    author_email="ryan.gosselin@usherbrooke.ca",
+    url="https://www.usherbrooke.ca/gchimiquebiotech/departement/professeurs/ryan-gosselin/",
+    packages=["RG"],
+    description="Ryan's go-to Python functions",
+    long_description="Miscellaneous functions:\
+    \n\nfct_xlsread\
+    \nfct_regress\
+    \nfct_R2\
+    \nfct_PCA\
+    \nfct_PLS\
+    \nfct_snv\
+    \nfct_colorspectra",
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
