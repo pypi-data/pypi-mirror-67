02742655b106c40ddb7f865683b6b71eed99f17f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:21 +0000

    Add package hellotest-eduardo/0.0.6/hellotest_eduardo-0.0.6-py3-none-any.whl

---
+++ packages/hellotest-eduardo/hellotest_eduardo-0.0.6-py3-none-any.whl/PyHydro/dadosgerais.py
@@ -0,0 +1,28 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+class dadosgerais(object):
+
+    # Dados de cadastro das usinas termeletrica (presentes no TERM.DAT)
+    NomedoCaso = None
+    NAnosEstudo = None
+    MesInicioEstudo = None
+    MesInicioPreEstudo = None
+    AnoInicioEstudo = None
+    NAnosPreEstudo = None
+    NAnosPosEstudo = None
+    NAnosPosSimulacao = None
+    NumMaxIter = None
+    NumSimForward = None
+    NumAberturas = None
+    NumSeriesSinteticas = None
+    OrdemMaxParP = None
+    AnoInicHistorico = None
+
+    def __init__(self, nanos):
+        self.NAnosEstudo = nanos
+
+
+
+
+
