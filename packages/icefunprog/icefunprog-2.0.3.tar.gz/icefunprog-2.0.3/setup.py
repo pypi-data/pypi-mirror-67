3920ff340522387bb1a79ecdb6a57e816f795dd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:24 +0000

    Add package icefunprog/2.0.3/icefunprog-2.0.3.tar.gz

---
+++ packages/icefunprog/icefunprog-2.0.3.tar.gz/icefunprog-2.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+setup(
+    name='icefunprog',
+    packages=find_packages(),
+    install_requires=['pyserial'],
+    version='2.0.3',
+    description='Programmer for the iceFUN FPGAs https://www.robot-electronics.co.uk/icefun.html)',
+    author='Petr Otoupal',
+    author_email='petr.otoupal@gmail.com',
+    url='https://github.com/pitrz/icefunprog',
+    keywords=['fpga', 'icefun', 'programmer', 'bitstream'],
+    license='GNU General Public License v3.0',
+    classifiers=[],
+    entry_points={
+        'console_scripts':[
+            'icefunprog = icefunprog.__main__:main'
+        ]
+    },
+)
