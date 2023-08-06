5ee565e910fc89c52f326048fd3bf385487f3514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:53 +0000

    Add package huscy.bookings/0.1.0a1/huscy.bookings-0.1.0a1-py3-none-any.whl

---
+++ packages/huscy.bookings/huscy.bookings-0.1.0a1-py3-none-any.whl/huscy/bookings/urls.py
@@ -0,0 +1,12 @@
+from rest_framework.routers import DefaultRouter
+
+from huscy.bookings import views
+
+
+router = DefaultRouter()
+router.register('timeslots', views.TimeslotViewSet)
+
+urlpatterns = [
+]
+
+urlpatterns += router.urls
