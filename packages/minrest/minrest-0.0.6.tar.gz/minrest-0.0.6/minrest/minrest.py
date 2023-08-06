de11d4e9b0479d2b3aa1195cbc2f3f30860aa40b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:08 +0000

    Add package minrest/0.0.6/minrest-0.0.6.tar.gz

---
+++ packages/minrest/minrest-0.0.6.tar.gz/minrest-0.0.6/minrest/minrest.py
@@ -0,0 +1,29 @@
+"""Main module."""
+import re
+import inspect
+import requests
+from urllib.parse import urljoin
+
+from minrest import parser
+
+
+class GenericClient(object):
+
+    def __init__(self, api_url, endpoints):
+        self.api_url = api_url
+        self.endpoints = endpoints
+
+    def call(self, endpoint, *args, **kwargs):
+        if not self.is_valid_endpoint(endpoint):
+            raise ValueError(f'Invalid endpoint: {endpoint}')
+
+        return (
+            requests
+            .request(
+                urljoin(self.api_url, endpoint),
+                *args, **kwargs
+            )
+        )
+
+    def _is_valid_endpoint(self, endpoint):
+        return endpoint in self.endpoints
