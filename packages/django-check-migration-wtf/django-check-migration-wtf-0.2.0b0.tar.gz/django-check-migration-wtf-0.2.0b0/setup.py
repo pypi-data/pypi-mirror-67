a3152069e81a5185a131e9ed188475b5960d45e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:49 +0000

    Add package django-check-migration-wtf/0.2.0b0/django-check-migration-wtf-0.2.0b0.tar.gz

---
+++ packages/django-check-migration-wtf/django-check-migration-wtf-0.2.0b0.tar.gz/django-check-migration-wtf-0.2.0b0/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+setup(
+    name='django-check-migration-wtf',
+    version='0.2.0-beta',
+    packages=find_packages(),
+    description='A line of description',
+    long_description=README,
+    author='Antonio Irizar',
+    author_email='antonioirizar@gmail.com',
+    url='https://github.com/antonioIrizar/django-check-migration-wtf',
+    license='GLP3',
+    python_requires='>=3.7',
+    install_requires=[
+        'Django>=2.2,<2.3',
+        'PyGithub>=1.50,<2',
+    ]
+)
