54b4e7ff447223c71ac6b6cb0671f23f049de5eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:04 +0000

    Add package scikit-network/0.16.0/scikit_network-0.16.0-cp38-cp38-win32.whl

---
+++ packages/scikit-network/scikit_network-0.16.0-cp38-cp38-win32.whl/sknetwork/ranking/tests/test_pagerank.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""tests for pagerank.py"""
+
+import unittest
+
+import numpy as np
+
+from sknetwork.data.models import cyclic_digraph
+from sknetwork.data.test_graphs import test_bigraph
+from sknetwork.ranking.pagerank import PageRank, CoPageRank
+from sknetwork.utils import co_neighbors_graph
+
+
+class TestPageRank(unittest.TestCase):
+
+    def setUp(self) -> None:
+        """Cycle graph for tests."""
+        self.n = 5
+        self.adjacency = cyclic_digraph(self.n)
+        self.truth = np.ones(self.n) / self.n
+
+    def test_solvers(self):
+        for solver in ['naive', 'lanczos', 'bicgstab']:
+            pr = PageRank(solver=solver)
+            scores = pr.fit_transform(self.adjacency)
+            self.assertAlmostEqual(0, np.linalg.norm(scores - self.truth))
+
+        PageRank(solver='diteration').fit_transform(self.adjacency)
+
+        with self.assertRaises(ValueError):
+            PageRank(solver='toto').fit_transform(self.adjacency)
+
+    def test_seeding(self):
+        pr = PageRank()
+        seeds_array = np.zeros(self.n)
+        seeds_array[0] = 1.
+        seeds_dict = {0: 1}
+
+        scores1 = pr.fit_transform(self.adjacency, seeds_array)
+        scores2 = pr.fit_transform(self.adjacency, seeds_dict)
+        self.assertAlmostEqual(np.linalg.norm(scores1 - scores2), 0.)
+
+    def test_damping(self):
+        pr = PageRank(damping_factor=0.99)
+        scores = pr.fit_transform(self.adjacency)
+        self.assertAlmostEqual(np.linalg.norm(scores - self.truth), 0.)
+
+        pr = PageRank(damping_factor=0.01)
+        scores = pr.fit_transform(self.adjacency)
+        self.assertAlmostEqual(np.linalg.norm(scores - self.truth), 0.)
+
+    def test_copagerank(self):
+        seeds = {0: 1}
+        biadjacency = test_bigraph()
+
+        adjacency = co_neighbors_graph(biadjacency, method='exact')
+        scores1 = CoPageRank().fit_transform(biadjacency, seeds)
+        scores2 = PageRank().fit_transform(adjacency, seeds)
+        self.assertAlmostEqual(np.linalg.norm(scores1 - scores2), 0.)
+
+        adjacency = co_neighbors_graph(biadjacency.T.tocsr(), method='exact')
+        scores1 = CoPageRank().fit(biadjacency, seeds_col=seeds).scores_col_
+        scores2 = PageRank().fit_transform(adjacency, seeds)
+        self.assertAlmostEqual(np.linalg.norm(scores1 - scores2), 0.)
+
+        with self.assertRaises(ValueError):
+            CoPageRank(solver='diteration').fit_transform(biadjacency, seeds)
