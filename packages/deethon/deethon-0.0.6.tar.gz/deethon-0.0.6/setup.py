dbdcb6fc7523f89cea08995898e6706f8fdc7f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:24 +0000

    Add package deethon/0.0.6/deethon-0.0.6.tar.gz

---
+++ packages/deethon/deethon-0.0.6.tar.gz/deethon-0.0.6/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+import shutil
+import os
+import glob
+
+
+CLEAN_FILES = ("./build", "./dist", "./*.egg-info")
+
+for paths in CLEAN_FILES:
+    paths = glob.glob(paths)
+    for path in paths:
+        print("removing", os.path.relpath(path))
+        shutil.rmtree(path)
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="deethon",
+    version="0.0.6",
+    author="Aykut Yilmaz",
+    author_email="aykuxt@gmail.com",
+    description="Python3 library to easily download music from Deezer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aykuxt/deethon",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["mutagen", "requests", "Crypto"],
+    python_requires=">=3.6",
+)
