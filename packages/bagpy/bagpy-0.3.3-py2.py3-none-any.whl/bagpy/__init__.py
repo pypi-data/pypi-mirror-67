997ad28ce858006f6014870a3539b83cd13cc5c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:39 +0000

    Add package bagpy/0.3.3/bagpy-0.3.3-py2.py3-none-any.whl

---
+++ packages/bagpy/bagpy-0.3.3-py2.py3-none-any.whl/bagpy/__init__.py
@@ -0,0 +1,7 @@
+# Initial Date: March 2, 2020
+# Author: Rahul Bhadani
+# Copyright (c)  Rahul Bhadani, Arizona Board of Regents
+# All rights reserved.
+
+from .bagreader import bagreader
+from .bagreader import animate_timeseries
