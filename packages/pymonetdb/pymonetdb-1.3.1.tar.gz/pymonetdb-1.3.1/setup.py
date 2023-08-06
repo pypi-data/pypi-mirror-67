4563ff812333e04ae1e7706c3fe060a1a7faaf38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:47 +0000

    Add package pymonetdb/1.3.1/pymonetdb-1.3.1.tar.gz

---
+++ packages/pymonetdb/pymonetdb-1.3.1.tar.gz/pymonetdb-1.3.1/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0.  If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright 1997 - July 2008 CWI, August 2008 - 2016 MonetDB B.V.
+
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+__version__ = '1.3.1'
+
+setup(name='pymonetdb',
+      version=__version__,
+      description='Native MonetDB client Python API',
+      long_description=read('README.rst'),
+      author='MonetDB BV',
+      author_email='info@monetdb.org',
+      url='http://www.monetdb.org/',
+      packages=['pymonetdb', 'pymonetdb.sql'],
+      download_url='https://github.com/gijzelaerr/pymonetdb',
+      classifiers=[
+          "Topic :: Database",
+          "Topic :: Database :: Database Engines/Servers",
+          "Development Status :: 5 - Production/Stable",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: Implementation :: PyPy",
+      ],
+      install_requires=['six>=1.12.0', 'typing', 'future'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest', 'mock'],
+      test_suite="test",
+      )
