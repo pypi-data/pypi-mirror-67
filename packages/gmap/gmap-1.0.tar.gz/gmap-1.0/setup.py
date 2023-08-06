66dfded854f90d9bc900aa2a6105ae0e286adab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:45 +0000

    Add package gmap/1.0/gmap-1.0.tar.gz

---
+++ packages/gmap/gmap-1.0.tar.gz/gmap-1.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="gmap",
+    version="1.0",
+    long_description="this is description",
+    packages=setuptools.find_packages(exclude=["tests", "data"])
+
+)
