aa4139e22f2443a78077044d23b550e626fe6fe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:38 +0000

    Add package mickey/0.0.5/mickey-0.0.5.tar.gz

---
+++ packages/mickey/mickey-0.0.5.tar.gz/mickey-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mickey", # Replace with your own username
+    version="0.0.5",
+    author="Md Nazmul Hasan",
+    author_email="hasan08sust@gmail.com",
+    description="A Django package to build complete blog site.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/farhapartex/mickey",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
