3a59fb82a8fc27d6e4e93f0eae4bb289f11e536d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:02 +0000

    Add package suffixtree/0.2.0/suffixtree-0.2.0-py2.py3-none-any.whl

---
+++ packages/suffixtree/suffixtree-0.2.0-py2.py3-none-any.whl/suffixtree/__init__.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+"""
+    suffixtree
+    ~~~~~~~~~~
+
+    Yet another suffix tree implementation in Python. See README file for more
+    information.
+
+    :copyright: (c) 2016 by Ali Ghaffaari.
+    :license: MIT, see LICENSE for more details.
+"""
+
+from .tree import Edge              # noqa
+from .tree import Tree              # noqa
+from .suffixtree import SuffixEdge  # noqa
+from .suffixtree import Substr      # noqa
+from .suffixtree import SuffixTree  # noqa
+from . import release               # noqa
