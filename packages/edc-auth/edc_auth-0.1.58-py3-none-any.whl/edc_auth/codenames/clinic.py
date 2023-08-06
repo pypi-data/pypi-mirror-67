fd1b0655621e902fb04dc086067ded7cc95fdc6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:52 +0000

    Add package edc-auth/0.1.58/edc_auth-0.1.58-py3-none-any.whl

---
+++ packages/edc-auth/edc_auth-0.1.58-py3-none-any.whl/edc_auth/codenames/clinic.py
@@ -0,0 +1,18 @@
+from .action_items import action_items
+
+clinic = [
+    "edc_appointment.add_appointment",
+    "edc_appointment.change_appointment",
+    "edc_appointment.view_appointment",
+    "edc_appointment.view_historicalappointment",
+    "edc_navbar.nav_subject_section",
+    "edc_offstudy.add_subjectoffstudy",
+    "edc_offstudy.change_subjectoffstudy",
+    "edc_offstudy.delete_subjectoffstudy",
+    "edc_offstudy.view_historicalsubjectoffstudy",
+    "edc_offstudy.view_subjectoffstudy",
+    "edc_dashboard.view_subject_review_listboard",
+    "edc_dashboard.view_subject_listboard",
+]
+
+clinic.extend(action_items)
