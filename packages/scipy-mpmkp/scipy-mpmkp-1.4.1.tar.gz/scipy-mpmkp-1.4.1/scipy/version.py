d39e9c959a713838f5014dd19e911c07797c5cd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:14 +0000

    Add package scipy-mpmkp/1.4.1/scipy-mpmkp-1.4.1.tar.gz

---
+++ packages/scipy-mpmkp/scipy-mpmkp-1.4.1.tar.gz/scipy-mpmkp-1.4.1/scipy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '1.4.1'
+version = '1.4.1'
+full_version = '1.4.1'
+git_revision = '4d1c671dc52cef5a6e121200b6360a8b6299f606'
+release = True
+
+if not release:
+    version = full_version
