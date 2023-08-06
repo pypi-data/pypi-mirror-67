3cc0b17b41abef8602b548e16dc27a8aa0bf19c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:07 +0000

    Add package subtitle-sync/0.2.0/subtitle-sync-0.2.0.tar.gz

---
+++ packages/subtitle-sync/subtitle-sync-0.2.0.tar.gz/subtitle-sync-0.2.0/subtitle_sync/utils/tagTimestampToScript.py
@@ -0,0 +1,57 @@
+
+
+
+def tagTimestampToScript(pureScript, scriptWithTimestamp):
+    index = 0
+    for pageIndex, page in enumerate(pureScript):
+        for contentIndex, content in enumerate(page["content"]):
+            if "scene_info" in content:
+                for sceneIndex, scene in enumerate(content["scene"]):
+                    if scene["type"] == "CHARACTER" or scene["type"] == "DUAL_DIALOGUE":
+
+                        testDialogues = ""
+                        if scene["type"] == "DUAL_DIALOGUE":
+                            testDialogues = [
+                                scene["content"]["character1"]["dialogue"],
+                                scene["content"]["character2"]["dialogue"],
+                            ]
+                        else:
+                            testDialogues = [scene["content"]["dialogue"]]
+
+                        for line in scriptWithTimestamp:
+                            if (
+                                any(
+                                    [
+                                        line["dialogue"]
+                                        in " ".join(
+                                            list(
+                                                filter(
+                                                    lambda x: "(" != x[0], testDialogue,
+                                                )
+                                            )
+                                        ).lower()
+                                        for testDialogue in testDialogues
+                                    ]
+                                )
+                                and "timestamp" in line
+                            ):
+                                pureScript[pageIndex]["content"][contentIndex]["scene"][
+                                    sceneIndex
+                                ]["timestampOriginal"] = line["timestamp"]
+
+                                isolatedLeftWords = line["dialogue"][0:line["distinct"]["matchIndex"]]
+                                length = len(line["distinct"]["subsDialogue"])
+                                rightSection = line["dialogue"][line["distinct"]["matchIndex"]:line["distinct"]["matchIndex"] + len(line["distinct"]["subsDialogue"]) + 1]
+
+                                isolatedRightWords = line["dialogue"].replace(rightSection, "").replace(isolatedLeftWords, "")
+                                
+                                pureScript[pageIndex]["content"][contentIndex]["scene"][
+                                    sceneIndex
+                                ]["timestamp"] = [line["timestamp"][0] - len(isolatedLeftWords.split(" ")),
+                                line["timestamp"][1] + len(isolatedRightWords.split(" "))]
+                               
+                                """
+                                remaining words in the left bumps the start timestamp
+                                remaining words in the right bumps the end timestamp
+                                """
+    return pureScript
