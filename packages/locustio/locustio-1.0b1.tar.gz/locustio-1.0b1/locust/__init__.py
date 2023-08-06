07df04c5db8163ca4249a5bc84f0b10396c3c54e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:33 +0000

    Add package locustio/1.0b1/locustio-1.0b1.tar.gz

---
+++ packages/locustio/locustio-1.0b1.tar.gz/locustio-1.0b1/locust/__init__.py
@@ -0,0 +1,8 @@
+from .core import HttpUser, User, TaskSet, task
+from .event import Events
+from .sequential_taskset import SequentialTaskSet
+from .wait_time import between, constant, constant_pacing
+
+events = Events()
+
+__version__ = "1.0b1"
