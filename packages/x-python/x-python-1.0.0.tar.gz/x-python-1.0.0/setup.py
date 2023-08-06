18252585873079da15074b07c3645f55b5f959b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:26 +0000

    Add package x-python/1.0.0/x-python-1.0.0.tar.gz

---
+++ packages/x-python/x-python-1.0.0.tar.gz/x-python-1.0.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from setuptools import setup
+from __pkginfo__ import (
+    author,
+    author_email,
+    entry_points,
+    install_requires,
+    long_description,
+    classifiers,
+    short_desc,
+    VERSION,
+    url,
+)
+
+setup(
+    name="x-python",
+    version=VERSION,
+    author=author,
+    author_email=author_email,
+    description=short_desc,
+    entry_points=entry_points,
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    packages=["xpython"],
+    install_requires=install_requires,
+    url=url
+)
