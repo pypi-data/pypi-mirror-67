7004470d6aae4fd1cc53f0d19bcff959bb97ed03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:57 +0000

    Add package theoto-distributions/0.1/theoto-distributions-0.1.tar.gz

---
+++ packages/theoto-distributions/theoto-distributions-0.1.tar.gz/theoto-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='theoto-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['theoto-distributions'],
+      zip_safe=False)
