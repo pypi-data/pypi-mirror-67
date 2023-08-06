6494e6c9c2c3bbe82d27b37fa2f522022285e231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:45 +0000

    Add package libjpg-bins/0.0.9/libjpg-bins-0.0.9.tar.gz

---
+++ packages/libjpg-bins/libjpg-bins-0.0.9.tar.gz/libjpg-bins-0.0.9/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='libjpg-bins',
+      description='Libraries for LibJPG Turbo on Alpine Linux',
+      author='Spencer Hanson',
+      url="https://github.com/spencer-hanson/libjpeg-turbo",
+      version='0.0.9',
+      package_data={
+          'libjpg': ['*.so*']
+      },
+      zip_safe=False,
+      packages=['libjpg'],
+      long_description="""Libraries for LibJPG Turbo on Alpine Linux""",
+      keywords="libjpg"
+      )
