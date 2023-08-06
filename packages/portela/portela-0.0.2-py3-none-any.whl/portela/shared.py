104b7eb4d006031c714ddaa43c3924ebc60478e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:07 +0000

    Add package portela/0.0.2/portela-0.0.2-py3-none-any.whl

---
+++ packages/portela/portela-0.0.2-py3-none-any.whl/portela/shared.py
@@ -0,0 +1,9 @@
+class Color:
+    GREEN = '\x1b[0;32;40m' 
+    BLUE = '\x1b[0;34;40m' 
+    YELLOW = '\x1b[0;33;40m'
+    TEAL = '\x1b[0;36;40m'
+    WHITE = '\x1b[0;37;40m'
+    RED = '\x1b[1;31;40m'
+    GRAY = '\x1b[2;37;40m'
+    END = '\x1b[0m'
