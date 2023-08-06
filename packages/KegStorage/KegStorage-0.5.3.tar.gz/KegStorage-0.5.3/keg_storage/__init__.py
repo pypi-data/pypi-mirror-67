2a2241accdd9bd5db486eb43a183adc0eb0ce154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:41 +0000

    Add package KegStorage/0.5.3/KegStorage-0.5.3.tar.gz

---
+++ packages/KegStorage/KegStorage-0.5.3.tar.gz/KegStorage-0.5.3/keg_storage/__init__.py
@@ -0,0 +1,6 @@
+from keg_storage.version import VERSION as __version__  # noqa
+from importlib.util import find_spec as _find_spec
+
+if _find_spec('keg'):
+    from keg_storage.plugin import Storage  # noqa
+from keg_storage.backends import *  # noqa
