ee52fd1e15349f92b32961cc167b34c543bcb24a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:17 +0000

    Add package heatmisercontroller/0.43/heatmisercontroller-0.43-py2-none-any.whl

---
+++ packages/heatmisercontroller/heatmisercontroller-0.43-py2-none-any.whl/heatmisercontroller/adaptor.py
@@ -0,0 +1,252 @@
+"""Heatmiser Adaptor handles serial connection and basic framing for the Heatmiser Protocol"""
+
+import time
+import logging
+import serial
+
+from .hm_constants import MAX_FRAME_RESP_LENGTH, MIN_FRAME_READ_RESP_LENGTH, DCB_START, FUNC_WRITE, FUNC_READ, BROADCAST_ADDR, FRAME_WRITE_RESP_LENGTH, FR_CONTENTS, RW_LENGTH_ALL, CRC_LENGTH
+import framing
+from .exceptions import HeatmiserResponseError, HeatmiserResponseErrorCRC
+from .logging_setup import csvlist
+
+def retryer(max_retries=3):
+    """Decorates reading from and writing to devices, rerunning the methods on failure"""
+    def wraps(func):
+        """Part of decorator"""
+        def inner(*args, **kwargs):
+            """Part of decorator"""
+            lasterror = None
+            for i in range(max_retries):
+                if i is not 0:
+                    logging.warn("Gen retrying due to %s"%str(lasterror))
+                try:
+                    result = func(*args, **kwargs)
+                except HeatmiserResponseError as err:
+                    lasterror = err
+                    continue
+                else:
+                    return result
+            raise HeatmiserResponseError("Failed after %i retries on %s"%(max_retries, str(lasterror)))
+        return inner
+    return wraps
+
+class HeatmiserAdaptor(object):
+    """Handles configuration serial port and provides low level read and write functions"""
+    def __init__(self, setup):
+
+        # Initialize setup and get settings
+        self._setup = setup
+        settings = self._setup.settings
+
+        self.serport = serial.Serial()
+        self.serport.bytesize = serial.EIGHTBITS #COM_SIZE
+        self.serport.parity = serial.PARITY_NONE #COM_PARITY
+        self.serport.stopbits = serial.STOPBITS_ONE #COM_STOP
+
+        self.lastsendtime = None
+        self.creationtime = time.time()
+
+        self._update_settings(settings)
+
+        self.lastreceivetime = self.creationtime - self.serport.COM_BUS_RESET_TIME # so that system will get on with sending straight away
+        
+        if self.auto_connect:
+            self.connect()
+        
+    def __del__(self):
+        self._disconnect()
+
+    def _update_settings(self, settings):
+        """Check settings and update if needed."""
+        for name, value in settings['controller'].iteritems():
+            setattr(self, name, value)
+
+        # Configure serial settings after closing if required
+        wasopen = False
+        if self.serport.isOpen():
+            wasopen = True
+            self.serport.close() # close port
+
+        for name, value in settings['serial'].iteritems():
+            setattr(self.serport, name, value)
+
+        if not self.serport.isOpen() and wasopen:
+            self._open_port()
+
+### low level serial commands
+
+    def connect(self):
+        """If not open, open serial port and log settings"""
+        if not self.serport.isOpen():
+            self._open_port()
+
+            logging.info("Gen %s port opened"% (self.serport.portstr))
+            logging.debug("Gen %s baud, %s bit, %s parity, with %s stopbits, timeout %s seconds" % (self.serport.baudrate, self.serport.bytesize, self.serport.parity, self.serport.stopbits, self.serport.timeout))
+        else:
+            logging.warn("Gen serial port was already open")
+    
+    def _open_port(self):
+        """open serial port and logging errors"""
+        try:
+            self.serport.open()
+        except serial.SerialException as err:
+            logging.error("Could not open serial port %s: %s" % (self.serport.portstr, err))
+            raise
+    
+    def _disconnect(self):
+        """check if serial port is open and if so close"""
+        #shouldn't need to called directly because handled by destructor
+        if self.serport.isOpen():
+            self.serport.close() # close port
+            logging.info("Gen serial port closed")
+        else:
+            logging.warn("Gen serial port was already closed")
+    
+    def _send_message(self, message):
+        """Send message to serial port and log errors"""
+        if not self.serport.isOpen():
+            self.connect()
+
+        #check time since last received to make sure bus has settled.
+        waittime = self.serport.COM_BUS_RESET_TIME - (time.time() - self.lastreceivetime)
+        if waittime > 0:
+            logging.debug("Gen waiting before sending %.2f"% (waittime))
+            time.sleep(waittime)
+        
+        # http://stackoverflow.com/questions/180606/how-do-i-convert-a-list-of-ascii-values-to-a-string-in-python
+        string = ''.join(map(chr, message))
+
+        try:
+            self.serport.write(string)    # Write a string
+        except serial.SerialTimeoutException as err:
+            self.serport.close() #need to close so that isOpen works correctly.
+            logging.warning("Write timeout error: %s, sending %s" % (err, csvlist(message)))
+            raise
+        except serial.SerialException as err:
+            self.serport.close() #need to close so that isOpen works correctly.
+            logging.warning("Write error: %s, sending %s" % (err, csvlist(message)))
+            raise
+
+        self.lastsendtime = time.strftime("%d %b %Y %H:%M:%S +0000", time.localtime(time.time())) #timezone is wrong
+        logging.debug("Gen sent %s", csvlist(message))
+
+    def _clear_input_buffer(self):
+        """Clears input buffer
+        
+        Used after CRC check wrong; in case more data was sent than expected."""
+    
+        time.sleep(self.serport.COM_TIMEOUT) #wait for read timeout to ensure slave finished sending
+        try:
+            if self.serport.isOpen():
+                self.serport.reset_input_buffer() #reset input buffer and dump any contents
+            logging.warning("Input buffer cleared")
+        except serial.SerialException:
+            self.serport.close()
+            logging.warning("Failed to clear input buffer")
+            raise
+    
+    def _read_bytes(self, length):
+        """read from serial port and log errors"""
+        try:
+            return self.serport.read(length)
+        except serial.SerialException as err:
+            #There is no new data from serial port (or port missing) (Doesn't include no response from stat)
+            logging.warning("Gen serial port error: %s" % str(err))
+            self.serport.close()
+            raise
+        finally:
+            self.serport.timeout = self.serport.COM_TIMEOUT #make sure timeout is reverted
+            self.lastreceivetime = time.time() #record last read time. Used to manage bus settling.
+    
+    def _receive_message(self, length=MAX_FRAME_RESP_LENGTH):
+        """Receive message from serial port and log errors
+        
+        Uses two time outs, one on the first byte and another for full data"""
+        if not self.serport.isOpen():
+            self.connect()
+        logging.debug("Gen listening for %d"%length)
+        
+        # Listen for the first byte
+        timereadstart = time.time()
+        self.serport.timeout = self.serport.COM_START_TIMEOUT #wait for start of response
+        
+        firstbyteread = self._read_bytes(1)
+
+        timereadfirstbyte = time.time()-timereadstart
+        logging.debug("Gen waited %.2fs for first byte"%timereadfirstbyte)
+        if len(firstbyteread) == 0:
+            raise HeatmiserResponseError("No Response")
+        
+        # Listen for the rest of the response
+        self.serport.timeout = max(self.serport.COM_MIN_TIMEOUT, self.serport.COM_TIMEOUT - timereadfirstbyte) #wait for full time out for rest of response, but not less than COM_MIN_TIMEOUT)
+        byteread = self._read_bytes(length - 1)
+
+        #Convert back to array
+        data = map(ord, firstbyteread) + map(ord, byteread)
+
+        return data
+
+### protocol functions
+    
+    @retryer(max_retries=3)
+    def write_to_device(self, network_address, protocol, unique_address, length, payload):
+        """Forms write frame and sends to serial link checking the acknowledgement"""
+        #Payload must be list
+        msg = framing.form_frame(network_address, protocol, self.my_master_addr,
+                                 FUNC_WRITE, unique_address, length, payload)
+        try:
+            self._send_message(msg)
+        except Exception:
+            logging.warn("C%i writing to address, no message sent"%(network_address))
+            raise
+
+        logging.debug("C%i written to address %i length %i payload %s"%(network_address, unique_address, length, csvlist(payload)))
+        if network_address == BROADCAST_ADDR: # if broadcasting force it to wait longer until next send
+            self.lastreceivetime = time.time() + self.serport.COM_SEND_MIN_TIME - self.serport.COM_BUS_RESET_TIME
+        else: #else listen for acknowledgement
+            response = self._receive_message(FRAME_WRITE_RESP_LENGTH)
+            try:
+                framing.verify_write_ack(protocol, network_address, self.my_master_addr, response)
+            except HeatmiserResponseErrorCRC:
+                self._clear_input_buffer()
+                raise
+
+    def min_time_between_reads(self):
+        """Computes the minimum time that adaptor leaves between read commands"""
+        return self.serport.COM_BUS_RESET_TIME
+
+    @retryer(max_retries=2)
+    def read_from_device(self, network_address, protocol, unique_start_address, expected_length, readall=False):
+        """Forms read frame and sends to serial link checking the response"""
+        if readall:
+            msg = framing.form_read_frame(network_address, protocol, self.my_master_addr, DCB_START, RW_LENGTH_ALL)
+            logging.debug("C %i read request to address %i length %i"%(network_address, DCB_START, RW_LENGTH_ALL))
+        else:
+            msg = framing.form_read_frame(network_address, protocol, self.my_master_addr, unique_start_address, expected_length)
+            logging.debug("C %i read request to address %i length %i"%(network_address, unique_start_address, expected_length))
+        try: #sending request
+            self._send_message(msg)
+        except:
+            logging.warn("C%i address, read message not sent"%(network_address))
+            raise
+
+        time1 = time.time()
+
+        try: #listening for response
+            response = self._receive_message(MIN_FRAME_READ_RESP_LENGTH + expected_length)
+        except Exception as err:
+            logging.warn("C%i read failed from address %i length %i due to %s"%(network_address, unique_start_address, expected_length, str(err)))
+            raise
+
+        logging.debug("C%i read in %.2f s from address %i length %i response %s"%(network_address, time.time()-time1, unique_start_address, expected_length, csvlist(response)))
+
+        try: #processing response
+            framing.verify_response(protocol, network_address, self.my_master_addr, FUNC_READ, expected_length, response)
+        except HeatmiserResponseErrorCRC:
+            self._clear_input_buffer()
+            raise
+        return response[FR_CONTENTS:-CRC_LENGTH]
+
+    def read_all_from_device(self, network_address, protocol, expected_length):
+        """Forms read all frame using read_from_device"""
+        return self.read_from_device(network_address, protocol, DCB_START, expected_length, True)
---
+++ packages/heatmisercontroller/heatmisercontroller-0.43-py2-none-any.whl/heatmisercontroller/fields.py
@@ -0,0 +1,254 @@
+"""generic field definitions for Heatmiser protocol"""
+import logging
+import time
+
+from hm_constants import BYTEMASK
+from .exceptions import HeatmiserResponseError
+from .observer import Notifier
+
+VALUES_ON_OFF = {'ON': 1, 'OFF': 0} #assusme that default comes first, need to swtich to ordered dictionary to make it possible to get default value
+VALUES_OFF_ON = {'OFF': 0, 'ON': 1}
+VALUES_OFF = {'OFF': 0}
+
+class HeatmiserFieldUnknown(Notifier):
+    """Class for variable length unknown read only field"""
+    writeable = False
+    divisor = 1
+
+    def __init__(self, name, address, max_age, length):
+        super(HeatmiserFieldUnknown, self).__init__()
+        self.name = name
+        self.address = address
+        self.dcbaddress = address
+        self.max_age = max_age
+        self.fieldlength = length
+        self._reset()
+
+    def __eq__(self, other):
+        return self.value == other
+
+    def __repr__(self):
+        return str(self.value)
+
+    def __cmp__(self, value):
+        return cmp(self.value, value)
+
+    def _reset(self):
+        """Reset data and values to unknown."""
+        self.data = None
+        self.value = None
+        self.lastreadtime = None #used to record when the field was last read
+
+    def last_dcb_byte_address(self):
+        """returns the address of the last dcb byte"""
+        return self.dcbaddress + self.fieldlength - 1
+
+    def update_data(self, data, readtime):
+        """update stored data and readtime. Don't compute value because don't know how to map"""
+        self.data = data
+        self.lastreadtime = readtime
+
+    def update_value(self, value, writetime):
+        """Don't update because don't know how to map to data."""
+        raise NotImplementedError
+
+    def get_value(self):
+        """Return value."""
+        return self.value
+        
+    def is_writable(self):
+        """Checks if field is writable"""
+        if not self.writeable:
+            raise ValueError("set_field: field isn't writeable")
+
+    def check_values(self, values):
+        """check a payload matches field spec"""
+        raise NotImplementedError
+
+    def check_data_valid(self):
+        """check whether data has been set"""
+        data_not_valid = self.lastreadtime is None
+        if data_not_valid:
+            logging.debug("Data item %s not available"%(self.name))
+        return not data_not_valid
+
+    def check_data_fresh(self, maxagein=None):
+        """check whether data is fresh
+
+        Check field data age is not more than maxage (in seconds)
+        maxagein = None, use the default self.maxage
+        maxagein = -1, only check if present
+        maxagein >=0, use maxagein (0 is effectively always False)
+        return False if old, True if recent"""
+        if not self.check_data_valid():
+            return False
+        elif maxagein == -1: #only check present
+            return True
+        elif maxagein is None: #if none use field defaults
+            maxage = self.max_age
+        else:
+            maxage = maxagein
+        #now check time
+        if time.time() - self.lastreadtime > maxage:
+            logging.debug("Data item %s too old"%(self.name))
+            return False
+        return True
+
+class HeatmiserField(HeatmiserFieldUnknown):
+    """Base class for fields providing basic method calls"""
+    #single value and hence single range
+    writeable = True
+    fieldlength = 0
+
+    def __init__(self, name, address, validrange, max_age, readvalues=None):
+        ###valid range list can be [], [min, max], [list of valid values]
+        super(HeatmiserField, self).__init__(name, address, max_age, self.fieldlength)
+        self.validrange = validrange
+        self.value = None
+        self.expectedvalue = None
+        self.writevalues = self.readvalues = readvalues
+        #check isinstance(fieldrange[0], (int, long)) and isinstance(fieldrange[1], (int, long))
+        if len(validrange) < 2:
+            self.validrange = [0, self.maxdatavalue / self.divisor]
+
+    def is_value(self, name):
+        """Returns true if value matches read value."""
+        return self.value == self.readvalues[name]
+
+    def is_unknown(self):
+        """Returns true if value isn't set."""
+        return self.value is None
+
+    def read_value_text(self):
+        """returns value converting to label if known"""
+        if self.readvalues is None:
+            return self.value
+        else:
+            return self.readvalues.keys()[self.readvalues.values().index(self.value)]
+
+    def write_value_from_text(self, value):
+        """maps text to value, otherwise returns input"""
+        if self.writevalues is None:
+            return value
+        else:
+            return self.writevalues.get(value, value)
+
+    def update_data(self, data, readtime):
+        """update stored data and readtime if data valid. Compute and store value from data."""
+        value = self._calculate_value(data)
+        if self.expectedvalue is not None and value != self.expectedvalue:
+            raise HeatmiserResponseError('Value %i is unexpected for %s, expected %i'%(value, self.name, self.expectedvalue))
+        self._validate_range(value)
+        self.data = data
+        self.value = value
+        self.lastreadtime = readtime
+        self.notify_value_change(value)
+
+    def update_value(self, value, writetime):
+        """Update the field value once successfully written to network"""
+        self._validate_range(value, ValueError)
+        data = self.format_data_from_value(value)
+        self.data = data
+        self.value = value
+        self.lastreadtime = writetime
+        self.notify_value_change(value)
+
+    def _validate_range(self, values, errortype=HeatmiserResponseError, expectedrange=None):
+        """validate the value is within range or in list."""
+        if expectedrange is None:
+            expectedrange = self.validrange
+
+        if len(expectedrange) == 2:
+            if values < expectedrange[0] or values > expectedrange[1]:
+                raise errortype("Value %.1f  outside expected range (%.1f, %.1f) for %s"%(values, expectedrange[0], expectedrange[1], self.name))
+        elif len(expectedrange) > 2:
+            if values not in expectedrange:
+                raise errortype("Value %.1f  outside expected range %s for %s"%(values, ','.join(map(str, expectedrange)), self.name))
+        else:
+            raise errortype("Expected range not defined for %s"%(self.name))
+
+    def _calculate_value(self, data):
+        """Calculate value from payload bytes"""
+        raise NotImplementedError
+
+    def format_data_from_value(self, value):
+        """Convert field to byte form for writting to device"""
+        raise NotImplementedError
+
+    def check_values(self, values):
+        """check a single or double byte field value matches field spec"""
+        if not isinstance(values, (int, long)):
+            #one or two byte field, not single length values
+            raise TypeError("set_field: invalid requested value")
+
+        #checks the values matches the ranges if ranges are defined
+        self._validate_range(values, ValueError)
+
+class HeatmiserFieldSingle(HeatmiserField):
+    """Class for writable 1 byte field"""
+    maxdatavalue = 255
+    fieldlength = 1
+
+    def _calculate_value(self, data):
+        """Calculate value from payload bytes"""
+        return data[0]/self.divisor
+
+    def format_data_from_value(self, value):
+        """Convert field to byte form for writting to device"""
+        return [value]
+
+class HeatmiserFieldSingleReadOnly(HeatmiserFieldSingle):
+    """Class for read only 1 byte field"""
+    writeable = False
+
+class HeatmiserFieldDouble(HeatmiserField):
+    """Class for writable 2 byte field"""
+    maxdatavalue = 65535
+    fieldlength = 2
+
+    def _calculate_value(self, data):
+        """Calculate value from payload bytes"""
+        val_high = data[0]
+        val_low = data[1]
+        return 1.0*(val_high*256 + val_low)/self.divisor #force float, although always returns integer temps.
+
+    def format_data_from_value(self, value):
+        """Convert field to byte form for writting to device"""
+        pay_lo = (value & BYTEMASK)
+        pay_hi = (value >> 8) & BYTEMASK
+        return [pay_lo, pay_hi]
+
+class HeatmiserFieldDoubleReadOnly(HeatmiserFieldDouble):
+    """Class for read only 2 byte field"""
+    writeable = False
+
+class HeatmiserFieldDoubleReadOnlyTenths(HeatmiserFieldDoubleReadOnly):
+    """Class for read only 2 byte field"""
+    divisor = 10.0
+
+class HeatmiserFieldMulti(HeatmiserField):
+    """Base class for writable multi byte field"""
+    maxdatavalue = None
+
+    def _validate_range(self, values, errortype=HeatmiserResponseError, expectedrange=None):
+        """validate the value is within range or in list. cyles through list of ranges"""
+        for i, item in enumerate(values):
+            expectedrange = self.validrange[i % len(self.validrange)]
+            super(HeatmiserFieldMulti, self)._validate_range(item, errortype, expectedrange)
+
+    def _calculate_value(self, data):
+        """Calculate value from payload bytes"""
+        return data
+
+    def format_data_from_value(self, value):
+        """Convert field to byte form for writting to device"""
+        return list(value) #force a copy
+
+    def check_values(self, values):
+        """check a values matches field spec"""
+        if len(values) != self.fieldlength:
+            #greater than two byte field, values length must match field length
+            raise ValueError("set_field: invalid payload length")
+
+        #checks the values matches the ranges if ranges are defined
+        self._validate_range(values, ValueError)
