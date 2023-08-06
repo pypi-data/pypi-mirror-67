8ec40c4742074f50d4bb1e85aa6116daab8e6c7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:22 +0000

    Add package fundamental-distributions/0.1/fundamental_distributions-0.1.tar.gz

---
+++ packages/fundamental-distributions/fundamental_distributions-0.1.tar.gz/fundamental_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fundamental_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['fundamental_distributions'],
+      author='Rohan Gawade',
+      author_email='rohangawade.18@gmail.com',
+      zip_safe=False)
