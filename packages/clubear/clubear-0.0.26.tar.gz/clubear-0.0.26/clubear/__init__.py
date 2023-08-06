6ab6bfc902bc994adc6a4ab55a5cc27e2f5f104c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:30 +0000

    Add package clubear/0.0.26/clubear-0.0.26.tar.gz

---
+++ packages/clubear/clubear-0.0.26.tar.gz/clubear-0.0.26/clubear/__init__.py
@@ -0,0 +1,14 @@
+import time
+author="clubear Reserch Group"
+version="2020-04-30 19:30:04"
+print("Welcome to clubear with version No. =",version,":)")
+from .dm import manager
+from .pp import pump
+from .pp import tank
+from .ck import check
+from .plt import plot
+from .md import model
+from .sf import shuffle
+from .sim import simulator
+from .fun import demo
+from .fun import ispump
