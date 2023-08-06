7c6a41674b6504e15de84614146f13e48caec7b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:02 +0000

    Add package jsmb/1.0.2/jsmb-1.0.2.tar.gz

---
+++ packages/jsmb/jsmb-1.0.2.tar.gz/jsmb-1.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jsmb",
+    version="1.0.2",
+    author="Jonatan dos Santos da Silva",
+    author_email="xjhonn@gmail.com",
+    description="Paquete de ayuda para usar SAMBA con Python con smb.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xjhonn/jsmb",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
