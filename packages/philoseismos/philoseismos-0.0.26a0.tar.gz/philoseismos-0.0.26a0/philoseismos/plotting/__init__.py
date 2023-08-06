c9b3bddcabfd5d4c1d0ad391afffd359c77d88ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:58 +0000

    Add package philoseismos/0.0.26a0/philoseismos-0.0.26a0.tar.gz

---
+++ packages/philoseismos/philoseismos-0.0.26a0.tar.gz/philoseismos-0.0.26a0/philoseismos/plotting/__init__.py
@@ -0,0 +1,8 @@
+""" philoseismos: engineering seismologist's toolbox.
+
+author: Ivan Dubrovin
+e-mail: io.dubrovin@icloud.com """
+
+from philoseismos.plotting.wiggle import wiggle_dm_into
+from philoseismos.plotting.imshow import imshow_dm_into
+from philoseismos.plotting.spectra import plot_average_spectrum_of_dm_into, imshow_dispersion_image_of_dm_into
