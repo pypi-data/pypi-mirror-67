fc0ddab787d87d2d8938e0ae3e5e2258070dceb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:45 +0000

    Add package vzmi/1.0.0/vzmi-1.0.0.tar.gz

---
+++ packages/vzmi/vzmi-1.0.0.tar.gz/vzmi-1.0.0/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# Copyright 2019, Oath Inc.
+# Licensed under the terms of the Apache 2.0 license.  See the LICENSE file in the project root for terms
+"""
+Package setup file for python namespace package vision
+"""
+import setuptools
+
+
+setuptools.setup()
