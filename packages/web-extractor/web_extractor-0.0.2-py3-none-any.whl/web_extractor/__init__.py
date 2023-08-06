bab501cc8c21d0574f300834bb64ac2d23625f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:32 +0000

    Add package web-extractor/0.0.2/web_extractor-0.0.2-py3-none-any.whl

---
+++ packages/web-extractor/web_extractor-0.0.2-py3-none-any.whl/web_extractor/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/2 12:16
+# @Author  : justin.郑 3907721@qq.com
+# @File    : __init__.py.py
+# @Desc    :
+
+from web_extractor.test import (
+    cs_test
+)
