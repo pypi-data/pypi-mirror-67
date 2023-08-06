b33881851b1cc33c170b21e9eba03dcad4cb56d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:07 +0000

    Add package rchitect/0.3.25/rchitect-0.3.25-cp27-cp27m-win32.whl

---
+++ packages/rchitect/rchitect-0.3.25-cp27-cp27m-win32.whl/rchitect/src/process_events.c
@@ -0,0 +1,44 @@
+#include "process_events.h"
+
+static void _process_events(void* n) {
+    R_ProcessEvents();
+
+#if defined(__APPLE__)
+    void* what = R_checkActivity(0, 1);
+    if (what != NULL)
+       R_runHandlers(R_InputHandlers, what);
+#elif defined(unix) || defined(__unix__) || defined(__unix)
+    void* what = R_checkActivity(0, 1);
+    R_runHandlers(R_InputHandlers, what);
+#endif
+
+}
+
+void process_events() {
+    R_ToplevelExec(_process_events, NULL);
+}
+
+#if defined(_WIN32)
+
+int peek_event(void) {
+    return GA_peekevent();
+}
+
+#else
+
+
+static void Call_R_checkActivity(int *d) {
+    *d = R_checkActivity(0, 1);
+}
+
+int peek_event(void) {
+    Rboolean ok;
+    int d;
+    ok = R_ToplevelExec(Call_R_checkActivity, &d);
+    if (ok == FALSE) {
+        return 0;
+    }
+    return d;
+}
+
+#endif
