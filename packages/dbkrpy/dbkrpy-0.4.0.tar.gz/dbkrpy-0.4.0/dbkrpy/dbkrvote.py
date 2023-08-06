d56924a239799792951d86b68af0c117cc7549d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:33 +0000

    Add package dbkrpy/0.4.0/dbkrpy-0.4.0.tar.gz

---
+++ packages/dbkrpy/dbkrpy-0.4.0.tar.gz/dbkrpy-0.4.0/dbkrpy/dbkrvote.py
@@ -0,0 +1,24 @@
+import aiohttp
+import asyncio
+
+class CheckVote:
+
+    def __init__(self, response):
+        self.check = self.checkvote(response)
+
+    @classmethod
+    async def get_response(cls, token, user_id):
+        URL = f'https://api.koreanbots.cf/bots/voted/{user_id}'
+        headers = {"token":token,"content-type":"application/json"}
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(URL,headers=headers) as r:
+                response = await r.json()
+                if response['code'] == 200:
+                    return response
+                else:
+                    raise Exception(f"{response}")
+
+    def checkvote(self, response):
+            return response['voted']
+
+            
