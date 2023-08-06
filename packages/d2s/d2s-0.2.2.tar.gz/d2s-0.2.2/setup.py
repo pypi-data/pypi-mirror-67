8800721254be714c1ad88e8ebe07237e443e9f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:20 +0000

    Add package d2s/0.2.2/d2s-0.2.2.tar.gz

---
+++ packages/d2s/d2s-0.2.2.tar.gz/d2s-0.2.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import setuptools
+
+setup(
+    name='d2s',
+    version='0.2.2',
+    license="MIT License",
+    url="https://github.com/MaastrichtU-IDS/d2s-cli",
+    author="Vincent Emonet",
+    author_email="vincent.emonet@gmail.com",
+    description="A Command Line Interface to orchestrate the integration of heterogenous data and the deployment of services consuming the integrated data. See https://d2s.semanticscience.org",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    py_modules=['d2s'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    install_requires=[
+        'Click', 'cwltool'
+    ],
+    entry_points={
+        'console_scripts': [
+            'd2s=d2s:cli'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
