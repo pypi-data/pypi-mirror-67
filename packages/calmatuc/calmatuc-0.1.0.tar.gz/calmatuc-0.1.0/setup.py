43efd57042569edfb34ba9c4a108632bec1e7aca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:39 +0000

    Add package calmatuc/0.1.0/calmatuc-0.1.0.tar.gz

---
+++ packages/calmatuc/calmatuc-0.1.0.tar.gz/calmatuc-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+setuptools.setup(
+  name = 'calmatuc',
+  version = '0.1.0',
+  author = 'Nicolle Murcia, Karen Pulido, Miguel Ángel Camargo, Mateo Ruíz, Jorge Ali Pastran, Carlos Trujillo, Isaac Zainea',
+  author_email = 'czaineam@ucentral.edu.co',  
+  description = 'Calculadora de Funciones Universidad Central',
+  url = 'https://github.com/MathMaxUC/calmatuc', # use the URL to the github repo
+  install_requires=REQUIREMENTS,
+  include_package_data=True,
+  keywords = ['calculadora', 'matematicas', 'calculo', 'algebra lineal', 'estadistica'],
+  packages=setuptools.find_packages(),
+  classifiers=[
+	"Programming Language :: Python :: 3",
+	"License :: OSI Approved :: MIT License",
+	"Operating System :: OS Independent",
+  ],
+)
