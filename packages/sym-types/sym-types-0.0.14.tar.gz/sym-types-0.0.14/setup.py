ce82d3800ae0a2182be183404779cee20d62bad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:05 +0000

    Add package sym-types/0.0.14/sym-types-0.0.14.tar.gz

---
+++ packages/sym-types/sym-types-0.0.14.tar.gz/sym-types-0.0.14/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sym-types",
+    version="0.0.14",
+    author="Sym Inc",
+    author_email="info@symops.io",
+    description="Generated protobufs for Sym types",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/symopsio/types",
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    python_requires='>=3.6',
+)
