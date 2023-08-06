44c23d8462a5bb2776d451db4350c102e989a71a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:23 +0000

    Add package labrepo-test/0.2.1/labrepo_test-0.2.1-py2.py3-none-any.whl

---
+++ packages/labrepo-test/labrepo_test-0.2.1-py2.py3-none-any.whl/labrepo_test/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for labrepo_test."""
+
+__author__ = """Luigi Petrucco"""
+__email__ = 'luigi.petrucco@gmail.com'
+__version__ = '0.2.1'
+
+
+from labrepo_test.SubPackage1.MyClass1 import MyClass1
+from labrepo_test.SubPackage2.MyClass2 import MyClass2
+from labrepo_test.SubPackage2.MyClass3 import MyClass3
