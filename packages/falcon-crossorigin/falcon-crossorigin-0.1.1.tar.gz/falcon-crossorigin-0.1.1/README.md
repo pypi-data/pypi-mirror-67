24a86204bce41176dccbb78baf1ae5e6494a7c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:14 +0000

    Add package falcon-crossorigin/0.1.1/falcon-crossorigin-0.1.1.tar.gz

---
+++ packages/falcon-crossorigin/falcon-crossorigin-0.1.1.tar.gz/falcon-crossorigin-0.1.1/README.md
@@ -0,0 +1,19 @@
+# falcon-crossorigin [![Build Status](https://travis-ci.org/admiralobvious/falcon-crossorigin.svg?branch=master)](https://travis-ci.org/admiralobvious/falcon-crossorigin)
+
+A simple [Falcon](https://github.com/falconry/falcon) module for Cross-Origin Resource Sharing (CORS).
+
+
+## Install
+```shell script
+pip install falcon-crossorigin
+```
+
+## Usage
+```python
+import falcon
+from falcon_crossorigin import CrossOrigin
+
+cross_origin = CrossOrigin(allow_origins="https://app.example.com")
+
+api = falcon.API(middleware=[cross_origin])
+```
---
+++ packages/falcon-crossorigin/falcon-crossorigin-0.1.1.tar.gz/falcon-crossorigin-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="falcon-crossorigin",
+    version="0.1.1",
+    description="Falcon cross-origin middleware",
+    url="http://github.com/admiralobviou/falcon-crossorigin",
+    author="Alexandre Ferland",
+    author_email="aferlandqc@gmail.com",
+    license="MIT",
+    packages=["falcon_crossorigin"],
+    zip_safe=False,
+    install_requires=["falcon>=1.4.1"],
+    setup_requires=["pytest-runner>=5.2"],
+    tests_require=["pytest>=5.4.1"],
+    platforms="any",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
