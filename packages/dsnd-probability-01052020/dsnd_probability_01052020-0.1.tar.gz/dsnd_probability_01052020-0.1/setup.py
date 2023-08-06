d80319077f0e7aa994d461e336f996478a1dd8ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:08 +0000

    Add package dsnd-probability-01052020/0.1/dsnd_probability_01052020-0.1.tar.gz

---
+++ packages/dsnd-probability-01052020/dsnd_probability_01052020-0.1.tar.gz/dsnd_probability_01052020-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_01052020',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_01052020'],
+      author='Sergio Perez',
+      author_email='sergio.perezcarriche@gmail.com',
+      zip_safe=False)
