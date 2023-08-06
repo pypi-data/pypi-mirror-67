b695ce587ecb0b7a516bbfa4499fcafa2abc8aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:53 +0000

    Add package sprockets.mixins.mediatype/2.2.3/sprockets.mixins.mediatype-2.2.3.tar.gz

---
+++ packages/sprockets.mixins.mediatype/sprockets.mixins.mediatype-2.2.3.tar.gz/sprockets.mixins.mediatype-2.2.3/docs/conf.py
@@ -0,0 +1,39 @@
+import alabaster
+from sprockets.mixins.mediatype import __version__
+
+needs_sphinx = '1.0'
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.intersphinx',
+              'sphinxcontrib.autohttp.tornado']
+source_suffix = '.rst'
+master_doc = 'index'
+project = 'sprockets.mixins.mediatype'
+copyright = '2015-2020, AWeber Communications'
+release = __version__
+version = '.'.join(release.split('.')[0:1])
+
+pygments_style = 'sphinx'
+html_theme = 'alabaster'
+html_style = 'custom.css'
+html_static_path = ['static']
+html_theme_path = [alabaster.get_path()]
+html_sidebars = {
+    '**': ['about.html', 'navigation.html'],
+}
+html_theme_options = {
+    'github_user': 'sprockets',
+    'github_repo': 'sprockets.mixins.media_type',
+    'description': 'Content-Type negotation mix-in',
+    'github_banner': True,
+    'travis_button': True,
+    'sidebar_width': '230px',
+    'codecov_button': True,
+}
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'requests': ('https://requests.readthedocs.org/en/latest/', None),
+    'sprockets': ('https://sprockets.readthedocs.org/en/latest/', None),
+    'tornado': ('http://tornadoweb.org/en/latest/', None),
+}
---
+++ packages/sprockets.mixins.mediatype/sprockets.mixins.mediatype-2.2.3.tar.gz/sprockets.mixins.mediatype-2.2.3/setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+#
+
+import os
+import setuptools
+
+from sprockets.mixins import mediatype
+
+
+def read_requirements(file_name):
+    requirements = []
+    try:
+        with open(os.path.join('requires', file_name)) as req_file:
+            for req_line in req_file:
+                req_line = req_line.strip()
+                if '#' in req_line:
+                    req_line = req_line[0:req_line.find('#')].strip()
+                if req_line.startswith('-r'):
+                    req_line = req_line[2:].strip()
+                    requirements.extend(read_requirements(req_line))
+                else:
+                    requirements.append(req_line)
+    except IOError:
+        pass
+    return requirements
+
+
+install_requires = read_requirements('installation.txt')
+tests_require = read_requirements('testing.txt')
+
+setuptools.setup(
+    name='sprockets.mixins.mediatype',
+    version=mediatype.__version__,
+    description='A mixin for reporting handling content-type/accept headers',
+    long_description='\n' + open('README.rst').read(),
+    url='https://github.com/sprockets/sprockets.mixins.media_type',
+    author='AWeber Communications',
+    author_email='api@aweber.com',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    tests_require=tests_require,
+    namespace_packages=['sprockets', 'sprockets.mixins'],
+    test_suite='nose.collector',
+    zip_safe=False)
