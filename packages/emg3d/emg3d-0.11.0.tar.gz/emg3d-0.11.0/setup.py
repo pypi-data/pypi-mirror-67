d3994a332c3894e772dc96df6964fbdaaadd9ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:34 +0000

    Add package emg3d/0.11.0/emg3d-0.11.0.tar.gz

---
+++ packages/emg3d/emg3d-0.11.0.tar.gz/emg3d-0.11.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+from setuptools import setup
+
+# Get README and remove badges.
+readme = open('README.rst').read()
+readme = re.sub('----.*marker', '----', readme, flags=re.DOTALL)
+
+description = 'A multigrid solver for 3D electromagnetic diffusion.'
+
+setup(
+    name='emg3d',
+    description=description,
+    long_description=readme,
+    author='The emg3d Developers',
+    author_email='dieter@werthmuller.org',
+    url='https://empymod.github.io',
+    license='Apache License V2.0',
+    packages=['emg3d'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.7',
+    ],
+    install_requires=[
+        'numpy',
+        'scipy>=1.4.0',
+        'numba>=0.45.0',
+        'empymod>=2.0.0',
+    ],
+    use_scm_version={
+        'root': '.',
+        'relative_to': __file__,
+        'write_to': os.path.join('emg3d', 'version.py'),
+    },
+    setup_requires=['setuptools_scm'],
+)
