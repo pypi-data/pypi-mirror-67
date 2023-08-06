b51ab78d67e5b87540857a0b2bd52fce58e0aa96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:12 +0000

    Add package bq-fake-pii-table-creator/0.0.1/bq-fake-pii-table-creator-0.0.1.tar.gz

---
+++ packages/bq-fake-pii-table-creator/bq-fake-pii-table-creator-0.0.1.tar.gz/bq-fake-pii-table-creator-0.0.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import find_namespace_packages, setup
+
+packages = [package for package in find_namespace_packages(where='./src',
+                                                           include='bq_fake_pii_table_creator.*')]
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    history = history_file.read()
+
+setup(
+    name='bq-fake-pii-table-creator',
+    version='0.0.1',
+    author='Marcelo Costa',
+    author_email='mesmacosta@gmail.com',
+    description='Library for creating BQ tables with fake sensible data',
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
+    platforms='Posix; MacOS X; Windows',
+    packages=packages,
+    package_dir={
+        '': 'src'
+    },
+    include_package_data=True,
+    install_requires=(
+        'pandas',
+        'faker',
+        'google-cloud-bigquery',
+        'google-cloud-storage',
+    ),
+    setup_requires=(
+        'pytest-runner',
+    ),
+    tests_require=(
+        'pytest-cov',
+    ),
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ),
+)
