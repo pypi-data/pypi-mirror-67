714e843afc15d6ffd488513467139e854360c14a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:14 +0000

    Add package pygogo/0.13.2/pygogo-0.13.2.tar.gz

---
+++ packages/pygogo/pygogo-0.13.2.tar.gz/pygogo-0.13.2/setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import pkutils
+
+from os import path as p
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+sys.dont_write_bytecode = True
+dev_requirements = list(pkutils.parse_requirements("dev-requirements.txt"))
+readme = pkutils.read("README.rst")
+changes = pkutils.read(p.join("docs", "CHANGES.rst"))
+module = pkutils.parse_module(p.join("pygogo", "__init__.py"))
+license = module.__license__
+version = module.__version__
+project = module.__title__
+description = module.__description__
+user = "reubano"
+
+# Setup requirements
+setup_require = [r for r in dev_requirements if "pkutils" in r]
+
+setup(
+    name=project,
+    version=version,
+    description=description,
+    long_description="%s\n\n%s" % (readme, changes),
+    author=module.__author__,
+    author_email=module.__email__,
+    url=pkutils.get_url(project, user),
+    download_url=pkutils.get_dl_url(project, user, version),
+    packages=find_packages(exclude=["tests"]),
+    include_package_data=True,
+    package_data={"helpers": ["helpers/*"], "docs": ["docs/*"]},
+    extras_require={"develop": dev_requirements},
+    setup_requires=setup_require,
+    test_suite="nose.collector",
+    tests_require=dev_requirements,
+    license=license,
+    zip_safe=False,
+    keywords=[project] + description.split(" "),
+    classifiers=[
+        pkutils.get_license(license),
+        pkutils.get_status(version),
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Environment :: Console",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    platforms=["MacOS X", "Windows", "Linux"],
+    scripts=[p.join("bin", "gogo")],
+)
---
+++ packages/pygogo/pygogo-0.13.2.tar.gz/pygogo-0.13.2/tests/__init__.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+# vim: sw=4:ts=4:expandtab
+
+"""
+tests
+~~~~~
+
+Provides application unit tests
+"""
+
+import unittest
+import re
+
+
+import pygogo as gogo
+
+module_logger = gogo.Gogo(__name__).logger
+initialized = False
+
+
+def setup_package():
+    """database context creation"""
+    global initialized
+    initialized = True
+    module_logger.debug("Package Setup\n")
+
+
+def teardown_package():
+    """database context removal"""
+    global initialized
+    initialized = False
+    module_logger.debug("Package Teardown\n")
+
+
+class BaseTest(unittest.TestCase):
+    def runTest(self, *args, **kwargs):
+        pass
+
+    def assertEqualEllipsis(self, expected, actual, marker="...", msg=None):
+        """Checks whether actual is equal to expected while ignoring ellipsis
+        content.
+
+        # https://gist.github.com/harobed/5845674
+
+        Args:
+            expected (scalar): The expected value
+            actual (scalar): The actual value
+
+        Example:
+            >>> BaseTest().assertEqualEllipsis('foo...bar', 'foo123bar')
+        """
+        if marker not in expected:
+            self.assertEqual(expected, actual, msg)
+
+        replaced = re.escape(expected).replace(re.escape(marker), "(.*?)")
+
+        if re.match(replaced, actual, re.M | re.S) is None:
+            self.assertMultiLineEqual(expected, actual, msg)
+
+    def assertIsSubset(self, expected, actual):
+        """Checks whether actual is a superset of expected.
+
+        Args:
+            expected (scalar): The expected value
+            actual (scalar): The actual value
+
+        Example:
+            >>> BaseTest().assertIsSubset([1,2], range(5))
+        """
+        self.assertTrue(set(expected).issubset(actual))
+
+    def assertIsNotSubset(self, expected, actual):
+        """Checks whether actual is a superset of expected.
+
+        Args:
+            expected (scalar): The expected value
+            actual (scalar): The actual value
+
+        Example:
+            >>> BaseTest().assertIsNotSubset([11,12], range(5))
+        """
+        self.assertFalse(set(expected).issubset(actual))
