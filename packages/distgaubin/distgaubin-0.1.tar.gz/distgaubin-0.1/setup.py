df90027601af88449335fdb45f05d96d11a636ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:45 +0000

    Add package distgaubin/0.1/distgaubin-0.1.tar.gz

---
+++ packages/distgaubin/distgaubin-0.1.tar.gz/distgaubin-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distgaubin',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distgaubin'],
+      author = "AnmolJM",
+      author_email = 'anmoljm@gmail.com',
+      zip_safe=False)
