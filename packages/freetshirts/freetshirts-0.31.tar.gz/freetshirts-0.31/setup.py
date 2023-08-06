84f98697ffe643125bec8b3a88c32d3e087f21dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:43 +0000

    Add package freetshirts/0.31/freetshirts-0.31.tar.gz

---
+++ packages/freetshirts/freetshirts-0.31.tar.gz/freetshirts-0.31/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from glob import glob
+
+data = glob('freetshirts/*.csv')
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='freetshirts',
+      version='0.31',
+      description='Package to generate email with personalized message to 1500+ colleges.',
+      url='http://github.com/iiradia/freetshirts',
+      author='Ishaan Radia',
+      author_email='iiradia@ncsu.edu',
+      license='MIT',
+      packages=['freetshirts'],
+      install_requires = ['pandas'],
+      include_package_data = True,
+      package_data = {"freetshirts": 
+            data
+      },
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      zip_safe=False)
