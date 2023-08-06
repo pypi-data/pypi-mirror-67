f84065e00cde07af452d9a611412e5d29692b19e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:29 +0000

    Add package torch-summary/1.1.8/torch-summary-1.1.8.tar.gz

---
+++ packages/torch-summary/torch-summary-1.1.8.tar.gz/torch-summary-1.1.8/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="torch-summary",
+    version="1.1.8",
+    author="Tyler Yep @tyleryep",
+    author_email="tyep10@gmail.com",
+    description="Model summary in PyTorch, based off of the original torchsummary.",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tyleryep/torch-summary",
+    packages=["torchsummary"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
