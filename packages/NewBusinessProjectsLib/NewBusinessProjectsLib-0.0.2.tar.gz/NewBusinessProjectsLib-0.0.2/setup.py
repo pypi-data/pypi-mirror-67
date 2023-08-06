0b4fc0e9b47a7ce0371a0db77c36e3c4f36ee08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:21 +0000

    Add package NewBusinessProjectsLib/0.0.2/NewBusinessProjectsLib-0.0.2.tar.gz

---
+++ packages/NewBusinessProjectsLib/NewBusinessProjectsLib-0.0.2.tar.gz/NewBusinessProjectsLib-0.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='NewBusinessProjectsLib',
+    version='0.0.2',
+    description='Keywords for facile newbusiness project!',
+    py_module=["NewBusinessLib"],
+    package_dir={'': 'src'},
+    long_description="Package which centralized python newbusiness keywords",
+    long_description_content_type="text/markdown",
+    url="https://github.com/f46io",
+    author="Fabio Lima",
+    author_email="f46io@icloud.com",
+    install_requires=[
+        "selenium ~= 3.141.0",
+        "robotframework ~= 3.2",
+    ],
+    Classfiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: MIT",
+        "Operating System  ::  OS Independent",
+        "Facile.it New Business help out lib "
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=3.7",
+        ],
+    },
+)
