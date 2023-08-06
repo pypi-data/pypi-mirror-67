8d89a29deb995e912a9309c3651c4b9d8451c30c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:00 +0000

    Add package vkbotfather/0.1.4/vkbotfather-0.1.4.tar.gz

---
+++ packages/vkbotfather/vkbotfather-0.1.4.tar.gz/vkbotfather-0.1.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='vkbotfather',
+    description='Father for creating VK Bots',
+    version='0.1.4',
+    url='https://github.com/wultes/vkbotfather',
+    license='MIT',
+    author='Wultes (Kirill)',
+    author_email='wultesu@gmail.com',
+    packages=['vkbotfather', 'vkbotfather.fatherbot', 'vkbotfather.fatherbot', 'vkbotfather.fatherbot.models', 'vkbotfather.utils'],
+    install_requires=['toml', 'Pillow']
+)
