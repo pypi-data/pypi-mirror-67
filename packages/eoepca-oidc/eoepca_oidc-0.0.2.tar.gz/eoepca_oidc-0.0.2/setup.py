24aaca2d8138938d8da3160d07140c569b0a4fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:22 +0000

    Add package eoepca-oidc/0.0.2/eoepca_oidc-0.0.2.tar.gz

---
+++ packages/eoepca-oidc/eoepca_oidc-0.0.2.tar.gz/eoepca_oidc-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name = 'eoepca_oidc',
+  version = '0.0.2',
+  author = 'EOEPCA',
+  author_email = 'alvaro.villanueva@deimos-space.com',
+  description = 'Open ID Connect python library developed by the EOEPCA team',
+  long_description = long_description,
+  long_description_content_type="text/markdown",
+  url = 'https://github.com/EOEPCA/um-common-oidc-client',
+  packages=setuptools.find_packages(),
+  license='apache-2.0',
+  keywords = ['OIDC', 'EOEPCA', 'client'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',                      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+  ],
+  python_requires='>=3.6',
+)
