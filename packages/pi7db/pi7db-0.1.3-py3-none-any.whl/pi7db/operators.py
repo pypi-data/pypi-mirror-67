0c0782cea85245a8f59651a5e7894fd668849dc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:34 +0000

    Add package pi7db/0.1.3/pi7db-0.1.3-py3-none-any.whl

---
+++ packages/pi7db/pi7db-0.1.3-py3-none-any.whl/pi7db/operators.py
@@ -0,0 +1,14 @@
+# Operators
+OR = "OR"
+GRT = "GRT"
+LET = "LET"
+ASC=asc= False
+DES=des= True
+increment_v = "Add++"
+decrement_v = "Subtract--"
+
+def increment(num):
+    return {increment_v:num}
+
+def decrement(num):
+    return {decrement_v:num}
