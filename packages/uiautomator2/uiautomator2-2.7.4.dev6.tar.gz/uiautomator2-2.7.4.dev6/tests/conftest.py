54e29f69d7c7ff2f2597e74fee658b08edf97acc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:18 +0000

    Add package uiautomator2/2.7.4.dev6/uiautomator2-2.7.4.dev6.tar.gz

---
+++ packages/uiautomator2/uiautomator2-2.7.4.dev6.tar.gz/uiautomator2-2.7.4.dev6/tests/conftest.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+import uiautomator2 as u2
+import pytest
+
+
+@pytest.fixture(scope="module")
+def d():
+    return u2.connect()
+
+
+@pytest.fixture(scope="function")
+def sess(d) -> u2.Session:
+    d.watcher.reset()
+    
+    s = d.session("io.appium.android.apis")
+    yield s
+
+
+@pytest.fixture
+def package_name():
+    return "io.appium.android.apis"
