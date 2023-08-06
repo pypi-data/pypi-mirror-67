88fb5cbdfd8888036cdb20f0ba6a1f87a1cdda41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:43 +0000

    Add package xingapi/1.0.1/xingapi-1.0.1-py3-none-any.whl

---
+++ packages/xingapi/xingapi-1.0.1-py3-none-any.whl/xingapi/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '1.0.1'
+
+import sys
+if sys.platform != 'win32':
+    raise Exception('xingapi requires 32bit working environment')
+
+from xingapi.res import Res
+from xingapi.xasession import Session
+from xingapi.xaquery import Query
+from xingapi.xareal import Real, RealManager
