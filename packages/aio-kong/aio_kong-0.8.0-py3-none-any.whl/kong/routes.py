b3d5f0a4a8f05ffbf12caa55046f70215411d59f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:29 +0000

    Add package aio-kong/0.8.0/aio_kong-0.8.0-py3-none-any.whl

---
+++ packages/aio-kong/aio_kong-0.8.0-py3-none-any.whl/kong/routes.py
@@ -0,0 +1,41 @@
+from itertools import zip_longest
+
+from .components import CrudComponent
+from .plugins import KongEntityWithPlugins
+from .utils import as_list
+
+
+class Routes(CrudComponent):
+    """Kong Routes
+
+    Routes are always associated with a Service
+    """
+
+    Entity = KongEntityWithPlugins
+
+    async def delete(self, id_):
+        route = self.wrap({"id": id_})
+        await route.plugins.delete_all()
+        return await super().delete(id_)
+
+    async def apply_json(self, data):
+        if not isinstance(data, list):
+            data = [data]
+        routes = await self.get_list()
+        result = []
+        for d, route in zip_longest(data, routes):
+            if not d:
+                if route:
+                    await self.delete(route.id)
+                continue
+            plugins = d.pop("plugins", [])
+            as_list("hosts", d)
+            as_list("paths", d)
+            as_list("methods", d)
+            if not route:
+                route = await self.create(**d)
+            else:
+                route = await self.update(route.id, **d)
+            route.data["plugins"] = await route.plugins.apply_json(plugins)
+            result.append(route.data)
+        return result
