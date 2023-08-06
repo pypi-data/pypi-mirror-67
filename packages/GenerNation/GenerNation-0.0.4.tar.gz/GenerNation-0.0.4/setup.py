772dc84fef5df02a2f27819cf31e2d408591fc7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:44 +0000

    Add package GenerNation/0.0.4/GenerNation-0.0.4.tar.gz

---
+++ packages/GenerNation/GenerNation-0.0.4.tar.gz/GenerNation-0.0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='GenerNation',
+      version='0.0.4',
+      description='Tool for generating various characters',
+      packages=['GenerNation'],
+      author_email='artegoser@gmail.com',
+      zip_safe=False)
