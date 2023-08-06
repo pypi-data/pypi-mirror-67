e7c96f8a0c6411bbfd7a5af40d6dd19bb9b1494a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:30 +0000

    Add package plot-model/0.20/plot_model-0.20.tar.gz

---
+++ packages/plot-model/plot_model-0.20.tar.gz/plot_model-0.20/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from setuptools import find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = 'plot_model',      
+    version = '0.20',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author = 'QinBingFeng',        
+    author_email = '114479602@qq.com',
+    url = 'https://github.com/Qinbf/plot_model',
+    description = 'plot model',
+    packages=find_packages()
+    )
