472b4ea3322c427823fa3aabf0aae71464f0f637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:31 +0000

    Add package phydra/0.0.1/phydra-0.0.1.tar.gz

---
+++ packages/phydra/phydra-0.0.1.tar.gz/phydra-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+      name='phydra',
+      version='0.0.1',
+      description='',
+      long_description='flexible marine ecosystem modelling python package based on xarray-simlab',
+      url='https://github.com/ben1post/phydra',
+      author='Benjamin Post',
+      author_email='ben@anoutpost.com',
+      packages=[],
+      classifiers=['Development Status :: 1 - Planning'],
+      )
