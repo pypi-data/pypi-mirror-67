c9f7de703325a8f8e45b5ade79c08041018889c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:24 +0000

    Add package blec/0.0.3/blec-0.0.3.tar.gz

---
+++ packages/blec/blec-0.0.3.tar.gz/blec-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="blec",
+    version="0.0.3",
+    author="igrmk",
+    author_email="igrmkx@gmail.com",
+    description="Alpha blending calculator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/igrmk/blec",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+    scripts=['bin/blec'],
+)
