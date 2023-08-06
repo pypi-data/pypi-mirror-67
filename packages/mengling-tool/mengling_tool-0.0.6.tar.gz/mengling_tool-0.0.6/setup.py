eb444b63db53e71df047813752e75b7a5497b1f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:13 +0000

    Add package mengling-tool/0.0.6/mengling_tool-0.0.6.tar.gz

---
+++ packages/mengling-tool/mengling_tool-0.0.6.tar.gz/mengling_tool-0.0.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='mengling_tool',
+      version='0.0.6',
+      description='工具包',
+      # url='https://github.com/BigDataFounder/string2date',
+      author='mengling',
+      author_email='1321443305@qq.com',
+      license='MIT',
+      packages=['mengling_tool', 'mengling_tool.爬虫工具包'])
