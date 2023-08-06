98caa5fabb223295e604bbf2612303a3df522bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:54 +0000

    Add package simple-neural-net/0.1.0/simple-neural-net-0.1.0.tar.gz

---
+++ packages/simple-neural-net/simple-neural-net-0.1.0.tar.gz/simple-neural-net-0.1.0/README.md
@@ -0,0 +1,14 @@
+# simple_neural_net
+
+Minimal framework for creating a simple neural net
+
+## Installation
+
+`pip install simple-neural-net`
+
+## Tasks
+  * Consider changing shape of X and y inputs to match `sklearn` conventions
+  * Add method which automatically evaluates model accuracy
+  * Add cost model and prediction model for multi-class classification
+  * Add other layer geometries (will require splitting up `Layer` class)
+  * Add batching
---
+++ packages/simple-neural-net/simple-neural-net-0.1.0.tar.gz/simple-neural-net-0.1.0/simple_neural_net/activation_models.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+class ActivationModel:
+    def activation(
+        self,
+        Z
+    ):
+        # Z: (num_outputs, num_examples)
+        # outputs: (num_outputs, num_examples)
+        raise NotImplementedError('Method must be implemented by child class')
+
+    def d_activation_d_Z(
+        self,
+        Z,
+        outputs
+    ):
+        # Z: (num_outputs, num_examples)
+        # outputs: (num_outputs, num_examples)
+        # d_activation_d_Z: (num_outputs, num_examples)
+        raise NotImplementedError('Method must be implemented by child class')
+
+class SigmoidActivationModel(ActivationModel):
+    def activation(
+        self,
+        Z
+    ):
+        # Z: (num_outputs, num_examples)
+        # outputs: (num_outputs, num_examples)
+        return np.reciprocal(np.add(1.0, np.exp(-Z))) # (num_outputs, num_examples)
+
+    def d_activation_d_Z(
+        self,
+        Z,
+        outputs
+    ):
+        # Z: (num_outputs, num_examples)
+        # outputs: (num_outputs, num_examples)
+        # d_activation_d_Z: (num_outputs, num_examples)
+        return np.multiply(outputs, np.subtract(1, outputs)) # (num_outputs, num_examples)
+
+class TanhActivationModel(ActivationModel):
+    def activation(
+        self,
+        Z
+    ):
+        # Z: (num_outputs, num_examples)
+        # outputs: (num_outputs, num_examples)
+        return np.tanh(Z) # (num_outputs, num_examples)
+
+    def d_activation_d_Z(
+        self,
+        Z,
+        outputs
+    ):
+        # Z: (num_outputs, num_examples)
+        # outputs: (num_outputs, num_examples)
+        # d_activation_d_Z: (num_outputs, num_examples)
+        return np.subtract(1, np.square(outputs)) # (num_outputs, num_examples)
+
+class ReLUActivationModel(ActivationModel):
+    def activation(
+        self,
+        Z
+    ):
+        # Z: (num_outputs, num_examples)
+        # outputs: (num_outputs, num_examples)
+        return np.multiply(Z, Z > 0.0) # (num_outputs, num_examples)
+
+    def d_activation_d_Z(
+        self,
+        Z,
+        outputs
+    ):
+        # Z: (num_outputs, num_examples)
+        # outputs: (num_outputs, num_examples)
+        # d_activation_d_Z: (num_outputs, num_examples)
+        return np.multiply(1.0, Z > 0.0) # (num_outputs, num_examples)
