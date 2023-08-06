0696ec47c15ef9ae6fc35896284246d6cbb790ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:15 +0000

    Add package aws-cdk.aws-s3-deployment/1.36.1/aws-cdk.aws-s3-deployment-1.36.1.tar.gz

---
+++ packages/aws-cdk.aws-s3-deployment/aws-cdk.aws-s3-deployment-1.36.1.tar.gz/aws-cdk.aws-s3-deployment-1.36.1/src/aws_cdk/aws_s3_deployment/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.assets
+import aws_cdk.aws_cloudformation
+import aws_cdk.aws_cloudfront
+import aws_cdk.aws_iam
+import aws_cdk.aws_lambda
+import aws_cdk.aws_s3
+import aws_cdk.aws_s3_assets
+import aws_cdk.core
+import constructs
+
+__jsii_assembly__ = jsii.JSIIAssembly.load("@aws-cdk/aws-s3-deployment", "1.36.1", __name__[0:-6], "aws-s3-deployment@1.36.1.jsii.tgz")
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
