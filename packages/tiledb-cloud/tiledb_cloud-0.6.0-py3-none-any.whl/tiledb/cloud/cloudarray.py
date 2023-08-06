6f764cff8b3a50d2f65efb7eb2e377e7aac4fae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:34 +0000

    Add package tiledb-cloud/0.6.0/tiledb_cloud-0.6.0-py3-none-any.whl

---
+++ packages/tiledb-cloud/tiledb_cloud-0.6.0-py3-none-any.whl/tiledb/cloud/cloudarray.py
@@ -0,0 +1,77 @@
+from . import array
+
+last_udf_task_id = None
+
+
+class CloudArray(object):
+    def apply_async(
+        self,
+        func=None,
+        ranges=None,
+        name=None,
+        attrs=None,
+        layout=None,
+        image_name=None,
+        http_compressor="deflate",
+    ):
+        """
+        Apply a user defined function to an array asynchronous
+
+        :param func: user function to run
+        :param ranges: ranges to issue query on
+        :param attrs: list of attributes or dimensions to fetch in query
+        :param layout: tiledb query layout
+        :param image_name: udf image name to use, useful for testing beta features
+        :param http_compressor: set http compressor for results
+        :return: UDFResult object which is a future containing the results of the UDF
+
+        **Example**
+        >>> import tiledb, tiledb.cloud, numpy
+        >>> def median(df):
+        ...   return numpy.median(df["a"])
+        >>> # Open the array then run the UDF
+        >>> with tiledb.SparseArray("tiledb://TileDB-Inc/quickstart_dense", ctx=tiledb.cloud.ctx()) as A:
+        ...   A.apply(median, [(0,5), (0,5)], attrs=["a", "b", "c"])
+        2.0
+
+        """
+        return array.apply_async(
+            uri=self.uri,
+            func=func,
+            ranges=ranges,
+            name=name,
+            attrs=attrs,
+            layout=layout,
+            image_name=image_name,
+            http_compressor=http_compressor,
+        )
+
+    def apply(
+        self,
+        func=None,
+        ranges=None,
+        name=None,
+        attrs=None,
+        layout=None,
+        image_name=None,
+        http_compressor="deflate",
+    ):
+        """
+        Apply a user defined function to an array
+        :param func: user function to run
+        :param ranges: ranges to issue query on
+        :param attrs: list of attributes or dimensions to fetch in query
+        :param layout: tiledb query layout
+        :param image_name: udf image name to use, useful for testing beta features
+        :param http_compressor: set http compressor for results
+        :return: results of the UDF
+        """
+        return self.apply_async(
+            func=func,
+            ranges=ranges,
+            name=name,
+            attrs=attrs,
+            layout=layout,
+            image_name=image_name,
+            http_compressor=http_compressor,
+        ).get()
