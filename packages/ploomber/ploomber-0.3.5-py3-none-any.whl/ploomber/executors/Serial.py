3a605ca8092cb55f13f69cb3dc431c3e8d0d419f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:28 +0000

    Add package ploomber/0.3.5/ploomber-0.3.5-py3-none-any.whl

---
+++ packages/ploomber/ploomber-0.3.5-py3-none-any.whl/ploomber/executors/Serial.py
@@ -0,0 +1,139 @@
+"""
+Serial DAG executor: builds one task at a time
+"""
+import warnings
+from multiprocessing import Pool
+import traceback
+import logging
+
+from tqdm.auto import tqdm
+from ploomber.executors.Executor import Executor
+from ploomber.exceptions import DAGBuildError
+from ploomber.MessageCollector import MessageCollector
+from ploomber.constants import TaskStatus
+
+
+# TODO: add a SerialIterator executor
+
+
+class Serial(Executor):
+    """Runs a DAG one task at a time
+
+    Tries to run as many tasks as possible (even if some of them fail), when
+    tasks fail, a final traceback is shown at the end of the execution showing
+    error messages along with their corresponding task to ease debgging, the
+    same happens with warnings: they are captured and shown at the end of the
+    execution (but only when build_in_subprocess is False).
+
+    Parameters
+    ----------
+    build_in_subprocess : bool, optional
+        Determines whether tasks should be executed in a subprocess or in the
+        current process. For pipelines with a lot of PythonCallables loading
+        large objects such as pandas.DataFrame, this option is recommended as
+        it guarantees that memory will be cleared up upon task execution.
+        Defaults to True
+
+    """
+    # TODO: maybe add a parameter: stop on first exception, same for Parallel
+
+    def __init__(self, build_in_subprocess=True):
+        self._logger = logging.getLogger(__name__)
+        self._build_in_subprocess = build_in_subprocess
+
+    def __call__(self, dag, show_progress, task_kwargs):
+        super().__call__(dag)
+
+        exceptions = MessageCollector()
+        warnings_ = MessageCollector()
+        task_reports = []
+
+        if show_progress:
+            tasks = tqdm(dag.values(), total=len(dag))
+        else:
+            tasks = dag.values()
+
+        for t in tasks:
+            if t.exec_status in {TaskStatus.Skipped, TaskStatus.Aborted}:
+                continue
+
+            if show_progress:
+                tasks.set_description('Building task "{}"'.format(t.name))
+
+            try:
+                with warnings.catch_warnings(record=True) as warnings_current:
+                    if (callable(t.source.value)
+                            and self._build_in_subprocess):
+                        report = execute_in_subprocess(t, task_kwargs)
+                    else:
+                        report = t.build(**task_kwargs)
+            except Exception:
+                t.exec_status = TaskStatus.Errored
+                new_status = TaskStatus.Errored
+                tr = traceback.format_exc()
+                exceptions.append(message=tr, task_str=repr(t))
+            else:
+                new_status = TaskStatus.Executed
+
+                try:
+                    t.exec_status = new_status
+                except Exception:
+                    tr = traceback.format_exc()
+                    exceptions.append(message=tr, task_str=repr(t))
+
+                task_reports.append(report)
+
+            if warnings_current:
+                w = [str(a_warning.message) for a_warning
+                     in warnings_current]
+                warnings_.append(task_str=t.name,
+                                 message='\n'.join(w))
+
+        # end of for loop
+
+        if warnings_:
+            # FIXME: maybe raise one by one to keep the warning type
+            warnings.warn('Some tasks had warnings when rendering DAG '
+                          '"{}":\n{}'.format(dag.name, str(warnings_)))
+
+        if exceptions:
+            raise DAGBuildError('DAG build failed, the following '
+                                'tasks crashed '
+                                '(corresponding downstream tasks aborted '
+                                'execution):\n{}'
+                                .format(str(exceptions)))
+
+        # only close when tasks are executed in this process (otherwise
+        # this won't have any effect anyway)
+        if not self._build_in_subprocess:
+            for client in dag.clients.values():
+                client.close()
+
+        return task_reports
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        # _logger is not pickable, so we remove them and build
+        # them again in __setstate__
+        del state['_logger']
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+        self._logger = logging.getLogger(__name__)
+
+
+def execute_in_subprocess(task, build_kwargs):
+    p = Pool(processes=1)
+    res = p.apply_async(func=task.build, kwds=build_kwargs)
+    # calling this make sure we catch the exception, from the docs:
+    # Return the result when it arrives. If timeout is not None and
+    # the result does not arrive within timeout seconds then
+    # multiprocessing.TimeoutError is raised. If the remote call
+    # raised an exception then that exception will be reraised by
+    # get().
+    # https://docs.python.org/3/library/multiprocessing.html#multiprocessing.pool.AsyncResult.get
+    result = res.get()
+    p.close()
+    p.join()
+    return result
---
+++ packages/ploomber/ploomber-0.3.5-py3-none-any.whl/ploomber/tasks/notebook.py
@@ -0,0 +1,238 @@
+import warnings
+from tempfile import mktemp
+from pathlib import Path
+
+try:
+    import papermill as pm
+except ImportError:
+    pm = None
+
+try:
+    import jupytext
+except ImportError:
+    jupytext = None
+
+try:
+    import nbformat
+except ImportError:
+    nbformat = None
+
+try:
+    import nbconvert
+except ImportError:
+    nbconvert = None
+
+try:
+    import jupyter_client
+except ImportError:
+    jupyter_client = None
+
+
+from ploomber.exceptions import TaskBuildError, SourceInitializationError
+from ploomber.sources import GenericSource
+from ploomber.products import File, MetaProduct
+from ploomber.tasks.Task import Task
+from ploomber.util import requires
+
+
+@requires(['jupytext'])
+def _to_ipynb(source, extension, kernelspec_name=None):
+    """Convert to jupyter notebook via jupytext
+
+    Parameters
+    ----------
+    source : str
+        Jupyter notebook (or jupytext compatible formatted) document
+
+    extension : str
+        Document format
+    """
+    nb = jupytext.reads(source, fmt={'extension': extension})
+
+    has_parameters_tag = False
+
+    for c in nb.cells:
+        cell_tags = c.metadata.get('tags')
+
+        if cell_tags:
+            if 'parameters' in cell_tags:
+                has_parameters_tag = True
+                break
+
+    if not has_parameters_tag:
+        warnings.warn('Notebook does not have any cell with tag "parameters"'
+                      'which is required by papermill')
+
+    if nb.metadata.get('kernelspec') is None and kernelspec_name is None:
+        raise ValueError('juptext could not load kernelspec from file and '
+                         'kernelspec_name was not specified, either add '
+                         'kernelspec info to your source file or specify '
+                         'a kernelspec by name')
+
+    if kernelspec_name is not None:
+        k = jupyter_client.kernelspec.get_kernel_spec(kernelspec_name)
+
+        nb.metadata.kernelspec = {
+            "display_name": k.display_name,
+            "language": k.language,
+            "name": kernelspec_name
+        }
+
+    out = mktemp('.ipynb')
+    Path(out).write_text(nbformat.v4.writes(nb))
+
+    return out
+
+
+def _from_ipynb(path_to_nb, extension, nbconvert_exporter_name):
+    if nbconvert_exporter_name is not None:
+        exporter = nbconvert.get_exporter(nbconvert_exporter_name)
+    else:
+        try:
+            exporter = nbconvert.get_exporter(extension.replace('.', ''))
+        except ValueError:
+            raise TaskBuildError('Could not determine nbconvert exporter '
+                                 'either specify in the path extension '
+                                 'or pass a valid exporter name in '
+                                 'the NotebookRunner constructor, '
+                                 'valid expoers are: {}'
+                                 .format(nbconvert.get_export_names()))
+
+    path = Path(path_to_nb)
+
+    nb = nbformat.v4.reads(path.read_text())
+    content, _ = nbconvert.export(exporter, nb,
+                                  exclude_input=True)
+
+    path.write_text(content)
+
+    return content
+
+
+class NotebookRunner(Task):
+    """
+    Run a Jupyter notebook using papermill. Support several input formats
+    via jupytext and several output formats via nbconvert
+
+    Parameters
+    ----------
+    source: str or pathlib.Path
+        Notebook source, if str, the content is interpreted as the actual
+        notebook, if pathlib.Path, the content of the file is loaded. When
+        loading from a str, ext_in must be passed
+    product: ploomber.File
+        The output file
+    dag: ploomber.DAG
+        A DAG to add this task to
+    name: str, optional
+        A str to indentify this task. Should not already exist in the dag
+    params: dict, optional
+        Notebook parameters. This are passed as the "parameters" argument
+        to the papermill.execute_notebook function, by default, "product"
+        and "upstream" are included
+    papermill_params : dict, optional
+        Other parameters passed to papermill.execute_notebook, defaults to None
+    kernelspec_name: str, optional
+        Kernelspec name to use, if the notebook already includes kernelspec
+        data (in metadata.kernelspec), this is ignored, otherwise, the kernel
+        is looked up using the jupyter_client.kernelspec.get_kernel_spec
+        function
+    nbconvert_exporter_name: str, optional
+        Once the notebook is run, this parameter controls whether to export
+        the notebook to a different parameter using the nbconvert package,
+        it is not needed unless the extension cannot be used to infer the
+        final output format, in which case the nbconvert.get_exporter is used
+    ext_in: str, optional
+        The input extension format. If source is a pathlib.Path, the extension
+        from there is used, if loaded from a str, this parameter is needed
+    nb_product_key: str, optional
+        If the notebook is expected to generate other products, pass the key
+        to identify the output notebook (i.e. if product is a list with 3
+        ploomber.File, pass the index pointing to the notebook path). If the
+        only output is the notebook itself, this parameter is not needed
+    """
+    PRODUCT_CLASSES_ALLOWED = (File, )
+
+    @requires(['jupyter', 'papermill'], 'NotebookRunner')
+    def __init__(self, source, product, dag, name=None, params=None,
+                 papermill_params=None, kernelspec_name=None,
+                 nbconvert_exporter_name=None, ext_in=None,
+                 nb_product_key=None):
+        self.papermill_params = papermill_params or {}
+        self.kernelspec_name = kernelspec_name
+        self.nbconvert_exporter_name = nbconvert_exporter_name
+        self.ext_in = ext_in
+        self.nb_product_key = nb_product_key
+        super().__init__(source, product, dag, name, params)
+
+        if isinstance(self.product, MetaProduct) and nb_product_key is None:
+            raise KeyError('More than one product was passed but '
+                           'nb_product_key '
+                           'is None, pass a value to locate the notebook '
+                           'save location')
+
+    def _init_source(self, source):
+        source = GenericSource(source)
+
+        if source.needs_render:
+            raise SourceInitializationError('The source for this task "{}"'
+                                            ' must be a literal '
+                                            .format(source.value.raw))
+
+        return source
+
+    def run(self):
+        if isinstance(self.product, MetaProduct):
+            path_to_out = Path(str(self.product[self.nb_product_key]))
+        else:
+            path_to_out = Path(str(self.product))
+
+        source = str(self.source)
+
+        if self.source.loc is None:
+            if self.ext_in is None:
+                raise ValueError('If the source was loaded from a string '
+                                 'you need to pass the ext_in parameter')
+
+            ext_in = '.'+self.ext_in
+        else:
+            ext_in = Path(self.source.loc).suffix
+
+        ext_out = path_to_out.suffix
+        # we will run the notebook with this extension, regardless of the
+        # user's choice, if any error happens, this will allow them to debug
+        # we will change the extension after the notebook runs successfully
+        path_to_out_nb = path_to_out.with_suffix('.ipynb')
+
+        # need to convert to ipynb using jupytext
+        if ext_in != '.ipynb':
+            path_to_in = _to_ipynb(source, ext_in, self.kernelspec_name)
+        else:
+            # otherwise just save rendered code in a tmp file
+            path_to_in = mktemp('.ipynb')
+            Path(path_to_in).write_text(source)
+
+        # papermill only allows JSON serializable parameters
+        params = self.params.to_dict()
+        params['product'] = params['product'].to_json_serializable()
+
+        if params.get('upstream'):
+            params['upstream'] = {k: n.to_json_serializable() for k, n
+                                  in params['upstream'].items()}
+
+        try:
+            pm.execute_notebook(path_to_in, str(path_to_out_nb),
+                                parameters=params,
+                                **self.papermill_params)
+        except Exception as e:
+            raise TaskBuildError('An error ocurred when calling'
+                                 ' papermil.execute_notebook, partially'
+                                 ' executed notebook with traceback '
+                                 'available at {}'
+                                 .format(str(path_to_out_nb))) from e
+
+        # if output format other than ipynb, convert using nbconvert
+        # and overwrite
+        if ext_out != '.ipynb' or self.nbconvert_exporter_name is not None:
+            path_to_out_nb.rename(path_to_out)
+            _from_ipynb(path_to_out, ext_out, self.nbconvert_exporter_name)
