7ec78fbce1e289913d387184ecc154ae920f4807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:56 +0000

    Add package lapras/0.0.5/lapras-0.0.5-py3-none-any.whl

---
+++ packages/lapras/lapras-0.0.5-py3-none-any.whl/lapras/ModelCode/utils/decisionTree.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+
+import numpy as np
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+import re
+
+def decisionTreeSplit(datax, datay, DepthOfTree, MinSamplesLeaf):
+    groupdt = DecisionTreeClassifier(criterion='entropy', splitter='best', max_depth=DepthOfTree,
+                                     min_samples_leaf=MinSamplesLeaf)
+    groupdt.fit(datax.reshape((datax.shape[0], 1)), datay.reshape((datay.shape[0], 1)))
+    dot_data = tree.export_graphviz(groupdt, out_file=None, )
+    pattern = re.compile('<= (.*?)\\\\nentropy', re.S)
+    split_num = re.findall(pattern, dot_data)
+    a = [float(b) for b in split_num]
+    return sorted(a)
+
+if __name__ == "__main__":
+    datax=np.array([int(line.strip()) for line in open('datax.csv')])
+    datay=np.array([int(line.strip()) for line in open('datay.csv')])
+    aaa = decisionTreeSplit(datax,datay,10,100)
+    print (aaa)
+
