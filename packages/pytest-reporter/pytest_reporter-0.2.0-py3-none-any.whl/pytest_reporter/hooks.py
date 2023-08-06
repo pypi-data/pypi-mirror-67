34206c3c17aca46249052c37f2d60f24843fa802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:52 +0000

    Add package pytest-reporter/0.2.0/pytest_reporter-0.2.0-py3-none-any.whl

---
+++ packages/pytest-reporter/pytest_reporter-0.2.0-py3-none-any.whl/pytest_reporter/hooks.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+def pytest_reporter_template_dirs(config):
+    pass
+
+
+def pytest_reporter_loader(dirs, config):
+    pass
+
+
+def pytest_reporter_context(context, config):
+    pass
+
+
+@pytest.hookspec(firstresult=True)
+def pytest_reporter_render(template_name, dirs, context):
+    pass
+
+
+def pytest_reporter_save(config):
+    pass
+
+
+def pytest_reporter_finish(path, context, config):
+    pass
