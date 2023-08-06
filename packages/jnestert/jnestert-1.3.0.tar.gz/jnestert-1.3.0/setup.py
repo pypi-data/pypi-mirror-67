922a4c2ae63e637ca76cb71baab44dbcd003472a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:11 +0000

    Add package jnestert/1.3.0/jnestert-1.3.0.tar.gz

---
+++ packages/jnestert/jnestert-1.3.0.tar.gz/jnestert-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+
+	name		= 'jnestert',
+	version		= '1.3.0',
+	py_modules	= ['jnestert'],
+	author		= 'hfpython',
+	author_email	= 'hfpython@headfirstlabs.com',
+	url		= 'http://www.headfirstlabs.com',
+	description	= ' A simple printer of nested lists',
+	)
