74bf864a1afb01aa235ed109f76e4b0080c186e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:21 +0000

    Add package web-downloader/0.0.2/web-downloader-0.0.2.tar.gz

---
+++ packages/web-downloader/web-downloader-0.0.2.tar.gz/web-downloader-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="web-downloader", # Replace with your own username
+    version="0.0.2",
+    author="Gabriel Chung",
+    author_email="gabrielchung1128@gmail.com",
+    description="A Python package using Selenium to download web content",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    url="https://github.com/gabrielchung/WebDownloader",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
