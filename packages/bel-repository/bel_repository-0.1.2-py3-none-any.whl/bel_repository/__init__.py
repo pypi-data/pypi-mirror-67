d210f70d5a741ecdf5d65967145dfb6de7dc4b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:10 +0000

    Add package bel-repository/0.1.2/bel_repository-0.1.2-py3-none-any.whl

---
+++ packages/bel-repository/bel_repository-0.1.2-py3-none-any.whl/bel_repository/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""A utility for loading data from repositories of BEL documents."""
+
+from .metadata import BELMetadata  # noqa: F401
+from .repository import BELRepository  # noqa: F401
+from .version import get_version  # noqa: F401
