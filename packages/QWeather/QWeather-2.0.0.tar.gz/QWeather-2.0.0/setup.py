794660eb45425e577bd4fc6d51c6e4dca816ec82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:40 +0000

    Add package QWeather/2.0.0/QWeather-2.0.0.tar.gz

---
+++ packages/QWeather/QWeather-2.0.0.tar.gz/QWeather-2.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='QWeather',
+    version='2.0.0',
+    packages=['qweather',],
+    author='A. Arvad Joergensen',
+    author_email='Asbjorn.Arvad@nbi.ku.dk',
+    license='MIT',
+    url='https://github.com/Arvad/QWeather',
+    description='A zeromq based distributed messaging platform',
+    long_description=open('README.rst').read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "pyzmq >= 17.0.0.b3",
+    ],
+    rpython_requires='~=3.5'
+)
