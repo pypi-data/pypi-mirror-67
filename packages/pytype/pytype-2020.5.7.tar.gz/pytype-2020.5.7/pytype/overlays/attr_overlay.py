0e5f5415164dcdd551a3d8348faa943c1c378d90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:41 +0000

    Add package pytype/2020.5.7/pytype-2020.5.7.tar.gz

---
+++ packages/pytype/pytype-2020.5.7.tar.gz/pytype-2020.5.7/pytype/overlays/attr_overlay.py
@@ -0,0 +1,198 @@
+"""Support for the 'attrs' library."""
+
+import logging
+
+from pytype import abstract
+from pytype import abstract_utils
+from pytype import function
+from pytype import mixin
+from pytype import overlay
+from pytype import overlay_utils
+from pytype.overlays import classgen
+
+log = logging.getLogger(__name__)
+
+# type aliases for convenience
+Param = overlay_utils.Param
+Attribute = classgen.Attribute
+
+
+_ATTRS_METADATA_KEY = "__attrs_attrs__"
+
+
+class AttrOverlay(overlay.Overlay):
+  """A custom overlay for the 'attr' module."""
+
+  def __init__(self, vm):
+    member_map = {
+        "s": Attrs.make,
+        "ib": Attrib.make,
+        "Factory": Factory.make,
+    }
+    ast = vm.loader.import_name("attr")
+    super(AttrOverlay, self).__init__(vm, "attr", member_map, ast)
+
+
+class Attrs(classgen.Decorator):
+  """Implements the @attr.s decorator."""
+
+  @classmethod
+  def make(cls, name, vm):
+    return super(Attrs, cls).make(name, vm, "attr")
+
+  def init_name(self, attr):
+    # attrs removes leading underscores from attrib names when generating kwargs
+    # for __init__.
+    return attr.name.lstrip("_")
+
+  def decorate(self, node, cls):
+    """Processes the attrib members of a class."""
+    # Collect classvars to convert them to attrs.
+    if self.args[cls]["auto_attribs"]:
+      ordering = classgen.Ordering.FIRST_ANNOTATE
+    else:
+      ordering = classgen.Ordering.LAST_ASSIGN
+    ordered_locals = self.get_class_locals(
+        cls, allow_methods=False, ordering=ordering)
+    own_attrs = []
+    for name, (value, orig) in ordered_locals.items():
+      if is_attrib(orig):
+        if not is_attrib(value) and orig.data[0].has_type:
+          # We cannot have both a type annotation and a type argument.
+          self.vm.errorlog.invalid_annotation(self.vm.frames, value.data[0].cls)
+          attr = Attribute(
+              name=name,
+              typ=self.vm.new_unsolvable(node),
+              init=orig.data[0].init,
+              default=orig.data[0].default)
+        else:
+          if is_attrib(value):
+            # Replace the attrib in the class dict with its type.
+            attr = Attribute(
+                name=name,
+                typ=value.data[0].typ,
+                init=value.data[0].init,
+                default=value.data[0].default)
+            cls.members[name] = attr.typ
+          else:
+            # cls.members[name] has already been set via a typecomment
+            attr = Attribute(
+                name=name,
+                typ=value,
+                init=orig.data[0].init,
+                default=orig.data[0].default)
+        own_attrs.append(attr)
+      elif self.args[cls]["auto_attribs"]:
+        if not match_classvar(value):
+          attr = Attribute(name=name, typ=value, init=True, default=orig)
+          cls.members[name] = value
+          own_attrs.append(attr)
+
+    base_attrs = self.get_base_class_attrs(cls, own_attrs, _ATTRS_METADATA_KEY)
+    attrs = base_attrs + own_attrs
+    # Stash attributes in class metadata for subclasses.
+    cls.metadata[_ATTRS_METADATA_KEY] = attrs
+
+    # Add an __init__ method
+    if self.args[cls]["init"]:
+      init_method = self.make_init(node, cls, attrs)
+      cls.members["__init__"] = init_method
+
+
+class AttribInstance(abstract.SimpleAbstractValue, mixin.HasSlots):
+  """Return value of an attr.ib() call."""
+
+  def __init__(self, vm, typ, has_type, init, default=None):
+    super(AttribInstance, self).__init__("attrib", vm)
+    mixin.HasSlots.init_mixin(self)
+    self.typ = typ
+    self.has_type = has_type
+    self.init = init
+    self.default = default
+    # TODO(rechen): attr.ib() returns an instance of attr._make._CountingAttr.
+    self.cls = vm.convert.unsolvable
+    self.set_slot("default", self.default_slot)
+    self.set_slot("validator", self.validator_slot)
+
+  def default_slot(self, node, default):
+    self.default = default
+    # If we don't have a type, and the default function has an explicit return
+    # annotation, use it for the type.
+    # TODO(mdemello): If there is no annotation but we can infer a return type
+    # for the default, use that as the type too.
+    if not self.has_type:
+      func = default.data[0]
+      if (isinstance(func, abstract.SignedFunction) and
+          func.signature.has_return_annotation):
+        ret = func.signature.annotations["return"]
+        self.typ = ret.instantiate(node)
+    return node, default
+
+  def validator_slot(self, node, validator):
+    return node, validator
+
+
+class Attrib(classgen.FieldConstructor):
+  """Implements attr.ib."""
+
+  @classmethod
+  def make(cls, name, vm):
+    return super(Attrib, cls).make(name, vm, "attr")
+
+  def call(self, node, unused_func, args):
+    """Returns a type corresponding to an attr."""
+    self.match_args(node, args)
+    node, default_var = self._get_default_var(node, args)
+    type_var = args.namedargs.get("type")
+    init = self.get_kwarg(args, "init", True)
+    has_type = type_var is not None
+    if type_var:
+      typ = self._instantiate_type(node, args, type_var)
+    elif default_var:
+      typ = self.get_type_from_default(node, default_var)
+    else:
+      typ = self.vm.new_unsolvable(node)
+    typ = AttribInstance(self.vm, typ, has_type, init,
+                         default_var).to_variable(node)
+    return node, typ
+
+  def _get_default_var(self, node, args):
+    if "default" in args.namedargs and "factory" in args.namedargs:
+      # attr.ib(factory=x) is syntactic sugar for attr.ib(default=Factory(x)).
+      raise function.DuplicateKeyword(self.signatures[0].signature, args,
+                                      self.vm, "default")
+    elif "default" in args.namedargs:
+      default_var = args.namedargs["default"]
+    elif "factory" in args.namedargs:
+      mod = self.vm.import_module("attr", "attr", 0)
+      node, attr = self.vm.attribute_handler.get_attribute(node, mod, "Factory")
+      # We know there is only one value because Factory is in the overlay.
+      factory, = attr.data
+      factory_args = function.Args(posargs=(args.namedargs["factory"],))
+      node, default_var = factory.call(node, attr.bindings[0], factory_args)
+    else:
+      default_var = None
+    return node, default_var
+
+  def _instantiate_type(self, node, args, type_var):
+    cls = self.vm.annotations_util.extract_annotation(
+        node, type_var, "attr.ib", self.vm.simple_stack())
+    _, instance = self.vm.init_class(node, cls)
+    return instance
+
+
+def is_attrib(var):
+  return var and isinstance(var.data[0], AttribInstance)
+
+
+def match_classvar(var):
+  """Unpack the type parameter from ClassVar[T]."""
+  return abstract_utils.match_type_container(var, "typing.ClassVar")
+
+
+class Factory(abstract.PyTDFunction):
+  """Implementation of attr.Factory."""
+
+  @classmethod
+  def make(cls, name, vm):
+    return super(Factory, cls).make(name, vm, "attr")
---
+++ packages/pytype/pytype-2020.5.7.tar.gz/pytype-2020.5.7/pytype/overlays/dataclass_overlay.py
@@ -0,0 +1,172 @@
+"""Support for dataclasses."""
+
+# TODO(mdemello):
+# - Raise an error if we see a duplicate annotation, even though python allows
+#     it, since there is no good reason to do that.
+
+import logging
+
+from pytype import abstract
+from pytype import abstract_utils
+from pytype import function
+from pytype import overlay
+from pytype.overlays import classgen
+
+log = logging.getLogger(__name__)
+
+
+_DATACLASS_METADATA_KEY = "__dataclass_fields__"
+
+
+class DataclassOverlay(overlay.Overlay):
+  """A custom overlay for the 'dataclasses' module."""
+
+  def __init__(self, vm):
+    member_map = {
+        "dataclass": Dataclass.make,
+        "field": Field.make,
+    }
+    ast = vm.loader.import_name("dataclasses")
+    super(DataclassOverlay, self).__init__(vm, "dataclasses", member_map, ast)
+
+
+class Dataclass(classgen.Decorator):
+  """Implements the @dataclass decorator."""
+
+  @classmethod
+  def make(cls, name, vm):
+    return super(Dataclass, cls).make(name, vm, "dataclasses")
+
+  def _check_default(self, node, name, value, orig):
+    if not orig:
+      return
+    typ = self.vm.convert.merge_classes(value.data)
+    bad = self.vm.matcher.bad_matches(orig, typ, node)
+    if bad:
+      binding = bad[0][orig]
+      self.vm.errorlog.annotation_type_mismatch(
+          self.vm.frames, typ, binding, name)
+
+  def _handle_initvar(self, node, cls, name, value, orig):
+    """Unpack or delete an initvar in the class annotations."""
+    initvar = match_initvar(value)
+    if not initvar:
+      return None
+    annots = abstract_utils.get_annotations_dict(cls.members)
+    # The InitVar annotation is not retained as a class member, but any default
+    # value is retained.
+    del annots[name]
+    value = initvar.instantiate(node)
+    if orig is not None:
+      cls.members[name] = value
+    return value
+
+  def decorate(self, node, cls):
+    """Processes class members."""
+
+    # Collect classvars to convert them to attrs. @dataclass collects vars with
+    # an explicit type annotation, in order of annotation, so that e.g.
+    # class A:
+    #   x: int
+    #   y: str = 'hello'
+    #   x = 10
+    # would have init(x:int = 10, y:str = 'hello')
+    own_attrs = []
+    cls_locals = self.get_class_locals(
+        cls, allow_methods=True, ordering=classgen.Ordering.FIRST_ANNOTATE)
+    for name, (value, orig) in cls_locals.items():
+      clsvar = match_classvar(value)
+      if clsvar:
+        continue
+      initvar_value = self._handle_initvar(node, cls, name, value, orig)
+      if initvar_value:
+        value = initvar_value
+        init = True
+      else:
+        cls.members[name] = value
+        if is_field(orig):
+          field = orig.data[0]
+          orig = field.typ if field.default else None
+          init = field.init
+        else:
+          init = True
+
+      # Check that default matches the declared type
+      self._check_default(node, name, value, orig)
+
+      attr = classgen.Attribute(name=name, typ=value, init=init, default=orig)
+      own_attrs.append(attr)
+
+    base_attrs = self.get_base_class_attrs(
+        cls, own_attrs, _DATACLASS_METADATA_KEY)
+    attrs = base_attrs + own_attrs
+    # Stash attributes in class metadata for subclasses.
+    cls.metadata[_DATACLASS_METADATA_KEY] = attrs
+
+    # Add an __init__ method if one doesn't exist already (dataclasses do not
+    # overwrite an explicit __init__ method).
+    if "__init__" not in cls.members and self.args[cls]["init"]:
+      init_method = self.make_init(node, cls, attrs)
+      cls.members["__init__"] = init_method
+
+
+class FieldInstance(abstract.SimpleAbstractValue):
+  """Return value of a field() call."""
+
+  def __init__(self, vm, typ, init, default=None):
+    super(FieldInstance, self).__init__("field", vm)
+    self.typ = typ
+    self.init = init
+    self.default = default
+    self.cls = vm.convert.unsolvable
+
+
+class Field(classgen.FieldConstructor):
+  """Implements dataclasses.field."""
+
+  @classmethod
+  def make(cls, name, vm):
+    return super(Field, cls).make(name, vm, "dataclasses")
+
+  def call(self, node, unused_func, args):
+    """Returns a type corresponding to a field."""
+    self.match_args(node, args)
+    node, default_var = self._get_default_var(node, args)
+    init = self.get_kwarg(args, "init", True)
+    if default_var:
+      typ = self.get_type_from_default(node, default_var)
+    else:
+      typ = self.vm.new_unsolvable(node)
+    typ = FieldInstance(self.vm, typ, init, default_var).to_variable(node)
+    return node, typ
+
+  def _get_default_var(self, node, args):
+    if "default" in args.namedargs and "default_factory" in args.namedargs:
+      # The pyi signatures should prevent this; check left in for safety.
+      raise function.DuplicateKeyword(self.signatures[0].signature, args,
+                                      self.vm, "default")
+    elif "default" in args.namedargs:
+      default_var = args.namedargs["default"]
+    elif "default_factory" in args.namedargs:
+      factory_var = args.namedargs["default_factory"]
+      factory, = factory_var.data
+      f_args = function.Args(posargs=())
+      node, default_var = factory.call(node, factory_var.bindings[0], f_args)
+    else:
+      default_var = None
+
+    return node, default_var
+
+
+def is_field(var):
+  return var and isinstance(var.data[0], FieldInstance)
+
+
+def match_initvar(var):
+  """Unpack the type parameter from InitVar[T]."""
+  return abstract_utils.match_type_container(var, "dataclasses.InitVar")
+
+
+def match_classvar(var):
+  """Unpack the type parameter from ClassVar[T]."""
+  return abstract_utils.match_type_container(var, "typing.ClassVar")
