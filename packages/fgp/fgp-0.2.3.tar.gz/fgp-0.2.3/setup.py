efb27716bd4e6657660df92d09d59a1d00c42c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:21 +0000

    Add package fgp/0.2.3/fgp-0.2.3.tar.gz

---
+++ packages/fgp/fgp-0.2.3.tar.gz/fgp-0.2.3/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+
+import setuptools
+
+import pathlib
+import os
+
+pathlib.Path(__file__).parent.absolute()
+
+with open(os.path.join(pathlib.Path(__file__).parent.absolute(), 'VERSION.txt')) as version_file:
+    version = version_file.read().strip()
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='fgp',
+    version=version,
+    author='Future Grid',
+    author_email='team@future-grid.com',
+    description='Future Grid API client',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'requests',
+        'loguru',
+        'pandas'
+    ],
+    url='https://gitlab.com/future-grid/fgp-api-python',
+    packages=setuptools.find_packages(),
+    package_data={'fgp': ['VERSION.txt']},
+    include_package_data=True,
+    classifiers=(                                 # Classifiers help people find your
+        'Programming Language :: Python :: 3',    # projects. See all possible classifiers
+        'License :: OSI Approved :: MIT License', # in https://pypi.org/classifiers/
+        'Operating System :: OS Independent',
+    ),
+    data_files=[('', ['VERSION.txt'])],
+    entry_points={
+        'console_scripts': [
+            "fgp=fgp:cli"
+        ]
+    }
+)
