9f42805bd2be1620ac60e9d99bdb5b66d05fdbf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:55 +0000

    Add package assert-info/0.1.2/assert-info-0.1.2.tar.gz

---
+++ packages/assert-info/assert-info-0.1.2.tar.gz/assert-info-0.1.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['assert_info']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['asttokens>=2.0.4,<3.0.0', 'docopt>=0.6.2,<0.7.0']
+
+entry_points = \
+{'console_scripts': ['assert-info = assert_info:main']}
+
+setup_kwargs = {
+    'name': 'assert-info',
+    'version': '0.1.2',
+    'description': 'A tool for fixing bare assert statements',
+    'long_description': None,
+    'author': 'Samuel Broster',
+    'author_email': 's.h.broster+gitlab@pm.me',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://gitlab.com/broster/assert-info',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=2.7,<3.0',
+}
+
+
+setup(**setup_kwargs)
