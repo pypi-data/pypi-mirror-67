6d511c17a45c193d46c0a4a54e289b3d6f24cf54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:10 +0000

    Add package kedro-pandas-profiling/0.1.0/kedro-pandas-profiling-0.1.0.tar.gz

---
+++ packages/kedro-pandas-profiling/kedro-pandas-profiling-0.1.0.tar.gz/kedro-pandas-profiling-0.1.0/README.md
@@ -0,0 +1,39 @@
+# Kedro-Pandas-Profiling
+
+This is a Kedro plugin that uses [Pandas-Profiling](https://github.com/pandas-profiling/pandas-profiling) to profile datasets.
+
+## Installation
+
+It can be installed via PyPI.
+
+``` sh
+
+pip install kedro-pandas-profiling
+
+```
+
+## How is Kedro-Pandas Profiling Used?
+You simply proceed with a Kedro project as normal.
+
+Once the data catalog is set up, you can run:
+
+``` sh
+kedro profile #this returns the list of things in the catalog
+
+kedro profile -n #with the name of the dataset
+```
+
+Kedro profile with no arguments returns the results of your catalog,
+and from that you can append a name of a dataset to profile. This 
+current iteration only supports .csv and .xlsx files.
+
+### Sample Output:
+
+Sample output based on the company dataset from the Kedro tutorial.
+
+![Sample Output](https://github.com/BrickFrog/kedro-pandas-profiling/blob/master/samples/sample.PNG)
+
+## What licence do you use?
+
+Kedro-Pandas-Profiling is licensed under the [Apache 2.0](LICENSE) License.
+
---
+++ packages/kedro-pandas-profiling/kedro-pandas-profiling-0.1.0.tar.gz/kedro-pandas-profiling-0.1.0/setup.py
@@ -0,0 +1,39 @@
+import re
+from codecs import open
+from os import path
+
+from setuptools import setup
+
+name = "kedro-pandas-profiling"
+here = path.abspath(path.dirname(__file__))
+
+# get package version
+package_name = name.replace("-", "_")
+with open(path.join(here, package_name, "__init__.py"), encoding="utf-8") as f:
+    version = re.search(r'__version__ = ["\']([^"\']+)', f.read()).group(1)
+
+# get the dependencies and installs
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requires = [x.strip() for x in f if x.strip()]
+
+# get test dependencies and installs
+with open("test_requirements.txt", "r", encoding="utf-8") as f:
+    test_requires = [x.strip() for x in f if x.strip() and not x.startswith("-r")]
+   
+
+setup(
+    name=name,
+    version=version,
+    description="Kedro-Pandas-Profiling is a small Kedro plugin for profiling dataframes",
+    long_description="Kedro-Pandas-Profiling is a small Kedro plugin that provides a wrapper to Pandas Profiling. This allows data in the catalog to be profiled, allowing better decisions to be made when building your Kedro pipeline.",
+    url="https://github.com/brickfrog/kedro-pandas-profiling",
+    license="Apache License 2.0",
+    python_requires=">=3.6, <3.8",
+    install_requires=requires,
+    tests_require=test_requires,
+    author="Justin Malloy",
+    packages=["kedro_pandas_profiling"],
+    entry_points={
+        "kedro.project_commands": ["profiling = kedro_pandas_profiling.plugin:commands"]
+    },
+)
