827404c7f614ebbe5f127df4d7779756a17c4559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:26 +0000

    Add package rcc/0.9.63/rcc-0.9.63-py3-none-any.whl

---
+++ packages/rcc/rcc-0.9.63-py3-none-any.whl/rcc/runner/reshard.py
@@ -0,0 +1,31 @@
+'''Tools to reshard a redis cluster
+
+Copyright (c) 2020 Machine Zone, Inc. All rights reserved.
+'''
+
+import resource
+import click
+
+from rcc.cluster.reshard import binPackingReshard
+
+DEFAULT_WEIGHTS_PATH = 'weights.csv'
+
+
+@click.command()
+@click.option(
+    '--redis_url', '-r', envvar='RCC_REDIS_URL', default='redis://localhost:11000'
+)
+@click.option('--password', '-a')
+@click.option('--user')
+@click.option('--port', default=6379)
+@click.option('--weight', '-w', required=True, default=DEFAULT_WEIGHTS_PATH)
+@click.option('--dry', is_flag=True)
+@click.option('--node_id')
+@click.option('--timeout', default=15, help='Max time to wait for consistency check')
+def reshard(port, redis_url, password, user, weight, timeout, dry, node_id):
+    '''Reshard a cluster using the binpacking technique'''
+
+    nofile = resource.getrlimit(resource.RLIMIT_NOFILE)[0]
+    click.secho(f'file descriptors ulimit: {nofile}', fg='cyan')
+
+    binPackingReshard(redis_url, password, user, weight, timeout, dry, node_id)
