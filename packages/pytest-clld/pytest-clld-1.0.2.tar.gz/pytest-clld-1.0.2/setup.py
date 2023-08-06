ae3a4701079394261726ee64132e7d571e8a7364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:06 +0000

    Add package pytest-clld/1.0.2/pytest-clld-1.0.2.tar.gz

---
+++ packages/pytest-clld/pytest-clld-1.0.2.tar.gz/pytest-clld-1.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+    name='pytest-clld',
+    version='1.0.2',
+    packages=['pytest_clld'],
+    install_requires=[
+        'pytest>=3.6', 
+        'clld',
+        'sqlalchemy>=1.0',
+        'WebTest>=1.3.1',
+        'pyramid>=1.6',
+        'mock',
+        'html5lib',
+        'webob',
+        'selenium>=3.8.1',
+    ],
+    entry_points={
+        'pytest11': [
+            'pytest_clld = pytest_clld.plugin',
+        ]
+    },
+    classifiers=[
+        'Framework :: Pytest',
+    ],
+)
