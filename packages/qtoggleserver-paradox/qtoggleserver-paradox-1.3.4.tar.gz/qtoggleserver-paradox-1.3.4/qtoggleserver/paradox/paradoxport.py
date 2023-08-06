dec46f201447dd743d69f07ad4cb8fb37e5b9933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:16 +0000

    Add package qtoggleserver-paradox/1.3.4/qtoggleserver-paradox-1.3.4.tar.gz

---
+++ packages/qtoggleserver-paradox/qtoggleserver-paradox-1.3.4.tar.gz/qtoggleserver-paradox-1.3.4/qtoggleserver/paradox/paradoxport.py
@@ -0,0 +1,27 @@
+
+import abc
+
+from typing import cast, Optional
+
+from qtoggleserver.peripherals import PeripheralPort
+
+from .paradoxalarm import ParadoxAlarm
+from .typing import Property
+
+
+class ParadoxPort(PeripheralPort, metaclass=abc.ABCMeta):
+    CMD_TIMEOUT = 60
+
+    def on_property_change(
+        self,
+        _type: str,
+        _id: Optional[str],
+        _property: str,
+        old_value: Property,
+        new_value: Property
+    ) -> None:
+
+        pass
+
+    def get_peripheral(self) -> ParadoxAlarm:
+        return cast(ParadoxAlarm, super().get_peripheral())
