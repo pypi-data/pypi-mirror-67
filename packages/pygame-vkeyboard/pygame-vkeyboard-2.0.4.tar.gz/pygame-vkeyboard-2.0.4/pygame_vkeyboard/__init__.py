03135805afa59113eadcbee8284006658cd9cf6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:25 +0000

    Add package pygame-vkeyboard/2.0.4/pygame-vkeyboard-2.0.4.tar.gz

---
+++ packages/pygame-vkeyboard/pygame-vkeyboard-2.0.4.tar.gz/pygame-vkeyboard-2.0.4/pygame_vkeyboard/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf8
+
+"""
+Visual keyboard for pygame.
+
+Aims to be easy to use as highly customizable as well.
+"""
+
+from .vkeyboard import VKeyboardLayout, VKeyboard
+from .vrenderers import VKeyboardRenderer
+
+__version__ = '2.0.4'
