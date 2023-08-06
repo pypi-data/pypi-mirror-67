766e85b65c0d53d2d59176f857410cd3e050d61f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:35 +0000

    Add package isopy/0.3.3/isopy-0.3.3.tar.gz

---
+++ packages/isopy/isopy-0.3.3.tar.gz/isopy-0.3.3/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="isopy",
+    version="0.3.3",
+    packages=setuptools.find_packages(include=['isopy', 'isopy.*']),
+    install_requires=['numpy', 'tables', 'pyperclip', 'xlrd', 'matplotlib'],
+
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+
+    package_data = {
+        "": ["*.txt", "*.rst", "*.csv", "*.xlsx"]},
+
+    author="Mattias Ek",
+    author_email="mattias.ek@bristol.ac.uk",
+    description="A python package for data processing in geo/cosmochemistry",
+    long_description=long_description,
+    keywords="array isotope geochemistry cosmochemisty",
+    long_description_content_type="text/markdown",
+    url="https://github.com/mattias-ek/isopy",
+    download_url = "https://github.com/mattias-ek/isopy/archive/v0.3.3.tar.gz",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha"
+    ],
+)
