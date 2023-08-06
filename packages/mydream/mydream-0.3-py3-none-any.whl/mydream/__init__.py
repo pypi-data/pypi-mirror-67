4809b8d5d593c0280e4c20ffd28fafa5bbcf16aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:10 +0000

    Add package mydream/0.3/mydream-0.3-py3-none-any.whl

---
+++ packages/mydream/mydream-0.3-py3-none-any.whl/mydream/__init__.py
@@ -0,0 +1,25 @@
+print("""
+
+  Do not answer!
+
+  Do not answer!!
+
+  Do not answer!!!
+
+  This world has received your message.
+
+  I am a pacifis
+  t in this world. It is the luck of your civilization that I am the first to receive your message. I am warning you: Do not answer! Do not answer!! Do not answer!!!
+
+  There are tens of millions of stars in your direction. As long as you do not answer, this world will not be able to ascertain the source of your transmission.
+
+  But if you do answer, the source will be located right away. Your planet will be invaded. Your world will be conquered!
+
+  Do not answer! Do not answer!! Do not answer!!!
+  
+  
+  --- 'Three Body Problem'
+
+  This boring package is made by JackM
+  
+  """)
