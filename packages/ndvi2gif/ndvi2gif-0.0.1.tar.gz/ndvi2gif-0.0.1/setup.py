48db1cbdd661c41b04221ac8c04d5b17ba3d6e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:34 +0000

    Add package ndvi2gif/0.0.1/ndvi2gif-0.0.1.tar.gz

---
+++ packages/ndvi2gif/ndvi2gif-0.0.1.tar.gz/ndvi2gif-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages, Extension
+
+with open('README.md', 'r') as fh:
+	long_description = fh.read()
+
+classifiers = [
+	'Intended Audience :: GIS & Remote Sensing Technicians',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Operating System :: OS Independent'
+]
+
+
+setup(
+	name='ndvi2gif',
+	version='0.0.1',
+	description='Python package to create ndvi seasonal composites, and download them as gif and geotiff',
+	#long_description=long_description,
+	#long_description_content_type='text/markdown',
+	url='https://github.com/Digdgeo/GEE_Playground',
+	python_requires='>=3.5',
+	author='Diego Garcia Diaz',
+	author_email='digd.geografo@gmail.com',
+	license='MIT',
+	install_requires=['geemap >= 0.6.10'],
+	packages=find_packages(include=['ndvi2gif', 'ndvi2gif.*']),
+	zip_safe=False
+)
