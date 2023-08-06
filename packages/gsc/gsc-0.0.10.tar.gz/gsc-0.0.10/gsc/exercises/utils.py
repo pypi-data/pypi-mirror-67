e7b4296265f8203087301ab53410fdf06aec0a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:34 +0000

    Add package gsc/0.0.10/gsc-0.0.10.tar.gz

---
+++ packages/gsc/gsc-0.0.10.tar.gz/gsc-0.0.10/gsc/exercises/utils.py
@@ -0,0 +1,63 @@
+import os
+import stat
+import shutil
+import subprocess
+import typing as t
+from gsc import cli
+
+
+def clean_status() -> bool:
+    res = subprocess.run(["git", "status"], capture_output=True)
+    return b"nothing to commit" in res.stdout
+
+
+def on_branch(branch: str) -> bool:
+    res = subprocess.run(["git", "branch"], capture_output=True)
+    return b"* " + branch.encode("utf-8") in res.stdout
+
+
+def tests_pass() -> bool:
+    res = subprocess.run(["pytest"], capture_output=True)
+    return res.returncode == 0
+
+
+def remove_hash(msg: str) -> str:
+    chunks = msg.split(" ", 1)[1:]
+    return " ".join(chunks).strip()
+
+
+def most_recent_commit_message() -> str:
+    res = subprocess.run(["git", "log", "-1", "--oneline"], capture_output=True)
+    return remove_hash(res.stdout.decode("utf-8"))
+
+
+def commit_messages() -> t.List[str]:
+    res = subprocess.run(["git", "log", "--oneline"], capture_output=True)
+    msgs = res.stdout.decode("utf-8").split("\n")[:-1]
+    return list(map(remove_hash, msgs))
+
+
+def check_commit_message(msg: str) -> None:
+    cli.info(f"Checking commit message: {msg}")
+
+    if msg.endswith("."):
+        cli.warn(
+            "You shouldn't put a full stop at the end of your Git commit messages.\n"
+            "Messages should be concise and not gramatically correct sentences."
+        )
+
+    if len(msg) >= 50:
+        cli.warn(
+            "Git commit messages should be fewer than 50 characters long.\n"
+            f"You've used {len(msg)}.\n"
+            "Try to make your commit messages more concise."
+        )
+
+
+def remove_readonly(func, path, execinfo):
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
+
+
+def rmtree_readonly(root):
+    shutil.rmtree(root, onerror=remove_readonly)
---
+++ packages/gsc/gsc-0.0.10.tar.gz/gsc-0.0.10/gsc/setup_exercise.py
@@ -0,0 +1,37 @@
+import os
+import pathlib
+from gsc import cli
+import gsc.exercises.push_and_pull, gsc.exercises.sync_error, gsc.exercises.merge_conflict
+
+
+class SetupError(Exception):
+    pass
+
+
+def setup(gsc_id: str):
+    while not os.path.exists(".git"):
+        os.chdir("..")
+        if os.getcwd() == "/":
+            raise SetupError("This is not a git repo.")
+
+    if not gsc_id:
+        if not os.path.exists(".gsc_id"):
+            raise SetupError(
+                "Could not find a Git Scientist exercise ID.\n"
+                "Did you mean to give an ID as an argument to gsc?"
+            )
+        gsc_id = pathlib.Path(".gsc_id").read_text().strip()
+
+    # Just in case someone decides to use dashes.
+    gsc_id = gsc_id.replace("-", "_")
+
+    cli.info(f"Setting up {gsc_id}")
+
+    if gsc_id == "push_and_pull":
+        gsc.exercises.push_and_pull.setup()
+    elif gsc_id == "sync_error":
+        gsc.exercises.sync_error.setup()
+    elif gsc_id == "merge_conflict":
+        gsc.exercises.merge_conflict.setup()
+    else:
+        raise SetupError("Unknown Git Scientist exercise. Try upgrading gsc.")
