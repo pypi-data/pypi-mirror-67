cc41ae7b487e49286651bba25af9e8084c79b432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:41 +0000

    Add package ont-bonito/0.1.5/ont-bonito-0.1.5.tar.gz

---
+++ packages/ont-bonito/ont-bonito-0.1.5.tar.gz/ont-bonito-0.1.5/bonito/basecaller.py
@@ -0,0 +1,73 @@
+"""
+Bonito Basecaller
+"""
+
+import sys
+import time
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+from bonito.util import load_model
+from bonito.io import DecoderWriter, PreprocessReader
+
+import torch
+import numpy as np
+
+
+def main(args):
+
+    sys.stderr.write("> loading model\n")
+    model = load_model(args.model_directory, args.device, weights=int(args.weights), half=args.half)
+
+    samples = 0
+    num_reads = 0
+    max_read_size = 4e6
+    dtype = np.float16 if args.half else np.float32
+    reader = PreprocessReader(args.reads_directory)
+    writer = DecoderWriter(model, beamsize=args.beamsize, fastq=args.fastq)
+
+    t0 = time.perf_counter()
+    sys.stderr.write("> calling\n")
+
+    with writer, reader, torch.no_grad():
+
+        while True:
+
+            read = reader.queue.get()
+            if read is None:
+                break
+
+            read_id, raw_data = read
+
+            if len(raw_data) > max_read_size:
+                sys.stderr.write("> skipping long read %s (%s samples)\n" % (read_id, len(raw_data)))
+                continue
+
+            num_reads += 1
+            samples += len(raw_data)
+
+            raw_data = raw_data[np.newaxis, np.newaxis, :].astype(dtype)
+            gpu_data = torch.tensor(raw_data).to(args.device)
+            posteriors = model(gpu_data).exp().cpu().numpy().squeeze()
+
+            writer.queue.put((read_id, posteriors.astype(np.float32)))
+
+    duration = time.perf_counter() - t0
+
+    sys.stderr.write("> completed reads: %s\n" % num_reads)
+    sys.stderr.write("> samples per second %.1E\n" % (samples  / duration))
+    sys.stderr.write("> done\n")
+
+
+def argparser():
+    parser = ArgumentParser(
+        formatter_class=ArgumentDefaultsHelpFormatter,
+        add_help=False
+    )
+    parser.add_argument("model_directory")
+    parser.add_argument("reads_directory")
+    parser.add_argument("--device", default="cuda")
+    parser.add_argument("--weights", default="0", type=str)
+    parser.add_argument("--beamsize", default=5, type=int)
+    parser.add_argument("--half", action="store_true", default=False)
+    parser.add_argument("--fastq", action="store_true", default=False)
+    return parser
