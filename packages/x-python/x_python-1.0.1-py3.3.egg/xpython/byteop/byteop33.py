5147a1ebbb07afc747344054bc63dbe2888a2bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:40 +0000

    Add package x-python/1.0.1/x_python-1.0.1-py3.3.egg

---
+++ packages/x-python/x_python-1.0.1-py3.3.egg/xpython/byteop/byteop33.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Byte Interpreter operations for Python 3.3
+"""
+from __future__ import print_function, division
+
+from xpython.byteop.byteop32 import ByteOp32
+
+class ByteOp33(ByteOp32):
+    def __init__(self, vm):
+        self.vm = vm
+        self.version = 3.3
+
+    # Right now 3.3 is largely the same as 3.2 here. How nice!
+
+if __name__ == "__main__":
+    x = ByteOp33(None)
