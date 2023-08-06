426cdda02113614defb1455d281c775f768d06be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:35 +0000

    Add package wasd/1.0.78/wasd-1.0.78.tar.gz

---
+++ packages/wasd/wasd-1.0.78.tar.gz/wasd-1.0.78/wasd/core/session.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import os
+
+
+root_dir = Path(os.getcwd())
+test_dir = root_dir.joinpath('tests')
+env_dir = root_dir.joinpath('_env')
+output_dir = root_dir.joinpath('_output')
+
+env = None
+use_listener = None
+save_screenshot = None
+steps = None
