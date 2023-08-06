f05c08668f60dd4d8dfe460d9e7bbb9955b9846a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:04 +0000

    Add package continuum_ci/0.2.0/continuum_ci-0.2.0.tar.gz

---
+++ packages/continuum_ci/continuum_ci-0.2.0.tar.gz/continuum_ci-0.2.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: UTF-8
+
+from distutils.core import setup
+
+
+setup(
+    name = 'continuum_ci',
+    version = '0.2.0',
+    author = 'Michel Casabianca',
+    author_email = 'casa@sweetohm.net',
+    packages = ['continuum'],
+    url = 'https://pypi.org/project/continuum_ci/',
+    license = 'Apache Software License',
+    description = 'Continuum is a minimalist continuous integration tool',
+    long_description=open('README.rst', encoding='UTF-8').read(),
+    install_requires=[
+        'PyYAML',
+        'mail1',
+    ],
+    entry_points = {
+        'console_scripts': [
+            'continuum = continuum:run',
+        ],
+    },
+)
