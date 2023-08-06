19f8cbafb2fcc7bc7ccdbe18a122349196ad1f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:30 +0000

    Add package germanetpy/0.2.1/germanetpy-0.2.1.tar.gz

---
+++ packages/germanetpy/germanetpy-0.2.1.tar.gz/germanetpy-0.2.1/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+import setuptools
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+requirements = ["numpy>=1.18.1", "lxml>=4.4.2", "pytest>=5.3.2", "fastenum>=0.0.1", "tqdm>=4.14",
+                "python-Levenshtein==0.12.0"]
+
+# The text of the README file
+long_description = (HERE / "README.md").read_text()
+setuptools.setup(
+    name="germanetpy",
+    version="0.2.1",
+    author="Neele Falk",
+    author_email="neele.witte@uni-tuebingen.de",
+    description="Python API for GermaNet",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Germanet-sfs/germanetpy.git",
+    install_requires=requirements,
+    packages=["germanetpy"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
