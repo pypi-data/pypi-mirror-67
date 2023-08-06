d025f5c5aee443e2ba897543d1271da2cda3d34c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:58 +0000

    Add package crdatamgt/0.2.10/crdatamgt-0.2.10-py3-none-any.whl

---
+++ packages/crdatamgt/crdatamgt-0.2.10-py3-none-any.whl/crdatamgt/runner.py
@@ -0,0 +1,30 @@
+import crdatamgt
+import yaml
+import os
+
+def main():
+    def represent_none(self, _):
+        return self.represent_scalar('tag:yaml.org,2002:null', '')
+
+    yaml.add_representer(type(None), represent_none)
+
+    try:
+        with open("parameters.yaml", 'r') as stream:
+            data_loaded = yaml.safe_load(stream)
+            try:
+                crdatamgt.project.project_load(**data_loaded)
+            except TypeError as e:
+                print("Please double-check your parameters.yaml document'")
+                os.startfile(os.path.join(os.getcwd(), 'parameters.yaml'))
+                raise e
+    except FileNotFoundError as e:
+        data_loaded = crdatamgt.helpers.write_yaml()
+        with open('parameters.yaml', 'w') as outfile:
+            yaml.dump(data_loaded, outfile, default_flow_style=False)
+        print("Please fill out the parameter.yaml document")
+        os.startfile(os.path.join(os.getcwd(), 'parameters.yaml'))
+        raise e
+
+
+if __name__ == "__main__":
+    main()
