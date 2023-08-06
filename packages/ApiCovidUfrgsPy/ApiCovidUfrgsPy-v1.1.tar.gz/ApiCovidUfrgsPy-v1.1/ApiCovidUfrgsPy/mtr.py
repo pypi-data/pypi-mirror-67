68f3fab25be161f3201696fdabc0976f3b866ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:20 +0000

    Add package ApiCovidUfrgsPy/v1.1/ApiCovidUfrgsPy-v1.1.tar.gz

---
+++ packages/ApiCovidUfrgsPy/ApiCovidUfrgsPy-v1.1.tar.gz/ApiCovidUfrgsPy-v1.1/ApiCovidUfrgsPy/mtr.py
@@ -0,0 +1,113 @@
+import requests as r
+import numpy as np
+
+url = r.get('https://api-covid-ufrgs.herokuapp.com/links').json()[-1]['link']
+
+class Municipios:
+	def __init__(self, url=url):
+		self.url = url + '/index/municipios'
+		self.data = r.get(self.url).json()
+
+	def matriz_municipios(self):
+		d = self.data
+		arr = []
+		for mp in d:
+			arr.append([mp['nome'], mp['pop_senso_2010'], mp['pop_est_2019'], mp['mesoregiao']])
+		return np.asarray(arr)
+
+class Distancias:
+	def __init__(self,url=url):
+		self.url = url + '/index/distancias'
+		try:
+			self.mps = r.get(self.url).json()
+		except NameError as err:
+			print(err);
+
+
+	def matriz_distancia(self):
+		arr_arr = []
+		for i in range(0,167):
+			arr = []
+			for mp in self.mps[i*167:(i+1)*167]:
+				arr.append(mp['distancia'])
+			arr_arr.append(arr)
+		return np.asarray(arr_arr)
+
+	def matriz_limitrofes(self):
+		arr_arr = []
+		for i in range(0,167):
+			arr = []
+			for mp in self.mps[i*167:(i+1)*167]:
+				if(mp['limitrofes'] == 1):
+					arr.append(mp['distancia'])
+				elif(mp['limitrofes'] == 0):
+					arr.append(0)
+			arr_arr.append(arr)
+		return np.asarray(arr_arr)
+
+	def matriz_distancia_raio(self, raio):
+		arr_arr = []
+		for i in range(0,167):
+			arr = []
+			for mp in self.mps[i*167:(i+1)*167]:
+				if(mp['distancia'] <= raio):
+					arr.append(mp['distancia'])
+				else:
+					arr.append(0)
+			arr_arr.append(arr)
+		return np.array(arr_arr)
+
+class Rodoviarias:
+	def __init__(self, url=url):
+		self.url = url+'/index/rodoviarias/linhas'
+		self.mps = r.get(self.url).json()
+
+	def matriz_rodoviarias(self):
+		arr_arr = []
+		for i in range(0,167):
+			arr = []
+			for mp in self.mps[i*167:(i+1)*167]:
+				arr.append(mp['quantidade'])
+			arr_arr.append(arr)
+		return np.asarray(arr_arr)
+	def status(self):
+		pass
+	def detalhes(self):
+		pass
+
+class CovidStatus:
+	def __init__(self, url=url):
+		self.url = url + '/index/infectados'
+		self.data = r.get(self.url).json()
+	def matriz_infectados(self):
+		d= self.data
+		self.medicoes = int(len(self.data)/167)
+		arr_arr = []
+		for i in range(0,self.medicoes):
+			arr = []
+			for j in d[i*self.medicoes:(i+1)*self.medicoes]:
+				arr.append(j['numero_infectados'])
+			arr_arr.append(arr)
+
+		return np.asarray(arr_arr)
+
+	def matriz_obitos(self):
+		pass
+
+class Localizacao:
+	def __init__(self,url=url):
+		self.url = url + '/index/localizacao'
+		self.data = r.get(self.url).json()
+
+	def matriz_localizacao(self):
+		arr = []
+		arr_arr = []
+		for d in self.data:
+			arr.append(d['lat'])
+			arr.append(d['lng'])
+			arr_arr.append(arr)
+			arr = []
+		return np.asarray(arr_arr)
+
+
+
---
+++ packages/ApiCovidUfrgsPy/ApiCovidUfrgsPy-v1.1.tar.gz/ApiCovidUfrgsPy-v1.1/setup.py
@@ -0,0 +1,28 @@
+
+from distutils.core import setup
+setup(
+  name = 'ApiCovidUfrgsPy',         # How you named your package folder (MyLib)
+  packages = ['ApiCovidUfrgsPy'],   # Chose the same as "name"
+  version = 'v1.1',
+  license='MIT',
+  description = 'Uma interface para acessar dados do servidor relacionado a covid no RS',
+  author = 'Matheus Fachini',                   # Type in your name
+  author_email = 'matheus.fachinis@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/Boardstroke/api-covid-ufrgs-py',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/Boardstroke/api-covid-ufrgs-py/archive/v1.1.tar.gz',    # I explain this later on
+  keywords = ['API', 'COVID', 'RS'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'numpy',
+          'requests',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
