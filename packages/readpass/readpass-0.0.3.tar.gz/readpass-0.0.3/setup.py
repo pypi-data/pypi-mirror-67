b0e10696929e657f52f83ffe2a0525104926f50c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:40 +0000

    Add package readpass/0.0.3/readpass-0.0.3.tar.gz

---
+++ packages/readpass/readpass-0.0.3.tar.gz/readpass-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="readpass",
+    version="0.0.3",
+    author="ChezCoder",
+    author_email="mrpizzaguyytb@gmail.com",
+    description="function to modify user input",
+    long_description=long_description, # don't touch this, this is your README.md
+    long_description_content_type="text/markdown",
+    url="https://repl.it/@ChezCoder/readpass",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
