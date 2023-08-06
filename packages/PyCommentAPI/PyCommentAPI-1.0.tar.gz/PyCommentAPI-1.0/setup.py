bfaa8ee850949f0e5301644be21bb971db9447cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:59 +0000

    Add package PyCommentAPI/1.0/PyCommentAPI-1.0.tar.gz

---
+++ packages/PyCommentAPI/PyCommentAPI-1.0.tar.gz/PyCommentAPI-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='PyCommentAPI',
+      version='1.0',
+      description='A simple, but extensible Python implementation for the comments.bot API',
+      packages=['PyCommentAPI'],
+      author_email='FenaFenol@gmail.com',
+      zip_safe=False)
