985b266977124a61ffd04818a63933c3926fb13a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:36 +0000

    Add package ssaha-distributions/0.2/ssaha_distributions-0.2.tar.gz

---
+++ packages/ssaha-distributions/ssaha_distributions-0.2.tar.gz/ssaha_distributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ssaha_distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['ssaha_distributions'],
+      zip_safe=False)
