6cadd24eed973cdcca51f035a85b7909e181f5ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:46 +0000

    Add package wagtail-birdsong/0.0.1/wagtail_birdsong-0.0.1-py3-none-any.whl

---
+++ packages/wagtail-birdsong/wagtail_birdsong-0.0.1-py3-none-any.whl/birdsong/backends/__init__.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+
+class BaseEmailBackend:
+    def send_email(self, request, campaign, subject, contacts):
+        raise NotImplementedError
+
+    @property
+    def from_email(self):
+        if hasattr(settings, 'BIRDSONG_FROM_EMAIL'):
+            return settings.BIRDSONG_FROM_EMAIL
+        return settings.DEFAULT_FROM_EMAIL
