34b49e291b0cbc15611a0fc9358496bd024bc679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:05 +0000

    Add package NlpToolkit-PropBank/1.0.14/NlpToolkit-PropBank-1.0.14.tar.gz

---
+++ packages/NlpToolkit-PropBank/NlpToolkit-PropBank-1.0.14.tar.gz/NlpToolkit-PropBank-1.0.14/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='NlpToolkit-PropBank',
+    version='1.0.14',
+    packages=['PropBank'],
+    url='https://github.com/olcaytaner/TurkishPropbank-Py',
+    license='',
+    author='olcaytaner',
+    author_email='olcaytaner@isikun.edu.tr',
+    description='Turkish PropBank'
+)
