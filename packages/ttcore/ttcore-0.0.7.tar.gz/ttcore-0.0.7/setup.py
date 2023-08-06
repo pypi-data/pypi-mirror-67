55cb42142f2e301fa5a8afdf4678de4160f93e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:32 +0000

    Add package ttcore/0.0.7/ttcore-0.0.7.tar.gz

---
+++ packages/ttcore/ttcore-0.0.7.tar.gz/ttcore-0.0.7/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ttcore",
+    version="0.0.7",
+    author="Martin F",
+    author_email="pypi.org@tigerteamx.com",
+    description="Django functionality to speedup development",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tigerteamx/ttcore",
+    packages=setuptools.find_packages(),
+    install_requires=['Django >= 2.0',
+                      "django-cron==0.5.1"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
