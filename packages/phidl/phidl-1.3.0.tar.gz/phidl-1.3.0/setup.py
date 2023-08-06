68ccf1c038c20f236aefa483fe20216478f521d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:42 +0000

    Add package phidl/1.3.0/phidl-1.3.0.tar.gz

---
+++ packages/phidl/phidl-1.3.0.tar.gz/phidl-1.3.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+install_requires=[
+   'gdspy>=1.5',
+   'numpy',
+   'matplotlib',
+]
+
+setup(name='phidl',
+      version='1.3.0',
+      description='PHIDL',
+      install_requires=install_requires,
+      author='Adam McCaughan',
+      author_email='amccaugh@gmail.com',
+      packages=['phidl'],
+      py_modules=['phidl.geometry', 'phidl.routing', 'phidl.utilities'],
+      package_dir = {'phidl': 'phidl'},
+     )
