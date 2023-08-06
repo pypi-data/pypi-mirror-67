c52eeb8d97ee52f89614cf287afa4a72a25f81c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:32 +0000

    Add package umongo/3.0.0b5/umongo-3.0.0b5.tar.gz

---
+++ packages/umongo/umongo-3.0.0b5.tar.gz/umongo-3.0.0b5/umongo/__init__.py
@@ -0,0 +1,74 @@
+from marshmallow import missing
+from .instance import Instance
+from .frameworks import (
+    PyMongoInstance,
+    TxMongoInstance,
+    MotorAsyncIOInstance,
+    MotorTornadoInstance,
+    MongoMockInstance
+)
+from .document import (
+    Document,
+    pre_load,
+    post_load,
+    pre_dump,
+    post_dump,
+    validates_schema
+)
+from .exceptions import (
+    UMongoError,
+    ValidationError,
+    UpdateError,
+    DeleteError,
+    AlreadyCreatedError,
+    NotCreatedError,
+    NoneReferenceError,
+    UnknownFieldInDBError,
+)
+from . import fields, validate
+from .schema import Schema
+from .data_objects import Reference
+from .embedded_document import EmbeddedDocument
+from .i18n import set_gettext
+
+
+__author__ = 'Emmanuel Leblond'
+__email__ = 'emmanuel.leblond@gmail.com'
+__version__ = '3.0.0b5'
+__all__ = (
+    'missing',
+
+    'Instance',
+    'PyMongoInstance',
+    'TxMongoInstance',
+    'MotorAsyncIOInstance',
+    'MotorTornadoInstance',
+    'MongoMockInstance',
+
+    'Document',
+    'pre_load',
+    'post_load',
+    'pre_dump',
+    'post_dump',
+    'validates_schema',
+    'EmbeddedDocument',
+
+    'UMongoError',
+    'ValidationError',
+    'UpdateError',
+    'DeleteError',
+    'AlreadyCreatedError',
+    'NotCreatedError',
+    'NoneReferenceError',
+    'UnknownFieldInDBError',
+
+    'fields',
+
+    'Schema',
+
+    'Reference',
+
+    'set_gettext',
+
+    'validate'
+)
