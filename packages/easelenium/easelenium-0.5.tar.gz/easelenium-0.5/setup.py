53e16cc969c33af09cdc723ffe066fd8e6997222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:51 +0000

    Add package easelenium/0.5/easelenium-0.5.tar.gz

---
+++ packages/easelenium/easelenium-0.5.tar.gz/easelenium-0.5/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from os import path
+from setuptools import setup
+
+
+with open(
+    path.join(path.abspath(path.dirname(__file__)), "README.md"), encoding="utf-8"
+) as f:
+    long_description = f.read()
+
+setup(
+    name="easelenium",
+    version="0.5",
+    description="easelenium - Selenium-based Test Automation Framework",
+    author="Kirill Strelkov",
+    author_email="kirillstrelkov@users.noreply.github.com",
+    url="https://github.com/kirillstrelkov/easelenium",
+    packages=[
+        "easelenium",
+        "easelenium.ui",
+        "easelenium.ui.editor",
+        "easelenium.ui.generator",
+        "easelenium.ui.widgets",
+        "easelenium.ui.widgets.image",
+        "easelenium.ui.selector_finder",
+        "easelenium.ui.parser",
+    ],
+    python_requires=">=3.6",
+    scripts=[
+        "easelenium/scripts/easelenium_cli.py",
+        "easelenium/scripts/easelenium_ui.py",
+    ],
+    requires=["selenium", "wxPython", "pytest", "pytest_html"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
