998e9959c0e9920fb3016c5e6f30b21fd77c487c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:35 +0000

    Add package distributions-syou/2020.5.3/distributions-syou-2020.5.3.tar.gz

---
+++ packages/distributions-syou/distributions-syou-2020.5.3.tar.gz/distributions-syou-2020.5.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions-syou',
+      version='2020.05.03',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = 'syou',
+      author_email = 'syouhung1904@gmail.com',
+      zip_safe=False)
