2487564e3dc16b6a8a0389b1c0cac96a30db4449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:49 +0000

    Add package readpass/0.0.1/readpass-0.0.1.tar.gz

---
+++ packages/readpass/readpass-0.0.1.tar.gz/readpass-0.0.1/README.md
@@ -0,0 +1,14 @@
+# Usage
+`readpass(prompt,replacement=None)`
+
+## Examples
+***
+Code:
+```python
+print(readpass("What is your username?: "))
+print(readpass("What is your password?: ","*"))
+print(readpass("Doesn't show: ",""))
+print(readpass("Troll: ","lol"))
+```
+Result:
+![result](https://storage.googleapis.com/replit/images/1588714358415_93e285e48a92005926f1f9eaf7ec52fa.png)
---
+++ packages/readpass/readpass-0.0.1.tar.gz/readpass-0.0.1/readpass/__init__.py
@@ -0,0 +1,38 @@
+from getkey import getkey
+from AnsiPrint import *
+
+def readpass(prompt,replacement=None):
+    class ReplacementLengthError(Exception):
+        pass
+    prompt = str(prompt)
+    if replacement != None and len(str(replacement)) > 5:
+        raise ReplacementLengthError("Length of replacement must not exceed 5.")
+    replacement = str(replacement)
+    print(prompt,end="",flush=True)
+    output = ""
+    while True:
+        inp = getkey()
+        if len(inp) == 1 and ord(inp) == 127:
+            if output != "":
+                if len(output) == 1:
+                    output = ""
+                else:
+                    output = output[:-1]
+                
+                if replacement != "None":
+                    print("\b \b" * len(replacement),end="",flush=True)
+                else:
+                    print("\b \b" * 1,end="",flush=True)
+            else:
+                print("\a",end="",flush=True)
+        elif len(inp) == 1 and ord(inp) == 10:
+            print("\n",end="",flush=True)
+            return output
+        elif len(inp) == 1:
+            output = output + inp
+            print(inp,end="",flush=True)
+        if replacement != "None" and ord(inp) != 127 and len(inp) == 1:
+            if replacement != "":
+                print("\b" + replacement,end="",flush=True)
+            else:
+                print("\b \b",end="",flush=True)
