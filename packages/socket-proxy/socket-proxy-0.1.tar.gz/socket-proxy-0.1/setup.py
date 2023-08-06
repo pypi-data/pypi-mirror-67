528678fb60700ffca10c9f8138479ff49381f852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:50 +0000

    Add package socket-proxy/0.1/socket-proxy-0.1.tar.gz

---
+++ packages/socket-proxy/socket-proxy-0.1.tar.gz/socket-proxy-0.1/setup.py
@@ -0,0 +1,32 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
+setup(
+    name="socket-proxy",
+    version="0.1",
+    author="Florian Kantelberg",
+    author_email="florian.kantelberg@mailbox.org",
+    description="Proxy TCP ports of local systems",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    keywords="proxy socket",
+    url="https://github.com/fkantelberg/socket-proxy",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    entry_points={"console_scripts": ["socket_proxy = socket_proxy.main:main"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
